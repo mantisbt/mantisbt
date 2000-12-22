8c0624f300aa285a2aeaf1f3b050716522a8d853
Kenzaburo Ito <prescience@users.sourceforge.net>
2000-12-22 21:45:05 +0000

    localization for english
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@98 f5dc347c-c33d-0410-90a0-b07cc1902cb9

---
+++ strings_english.php
@@ -0,0 +1,271 @@
+<?
+	# Mantis - a php based bugtracking system
+	# Copyright (C) 2000  Kenzaburo Ito - kenito@300baud.org
+	# This program is distributed under the terms and conditions of the GPL
+	# See the README and LICENSE files for details
+?>
+<?
+	# This file is to aid in localization
+
+	### account_delete_page.php3
+	$s_confirm_delete = "Are you sure you wish to delete your account?";
+
+	### account_delete.php3
+	$s_account_protected = "Account protected. Cannot change settings...";
+	$s_account_removed = "Your account has been removed...";
+
+	### account_page.php3
+	$s_manage_profiles_link = "Manage Profiles";
+	$s_change_preferences_link = "Change Preferences";
+	$s_edit_account_title = "Edit Account";
+	$s_username = "Username";
+	$s_email = "Email";
+	$s_password = "Password";
+	$s_confirm_password  = "Confirm Password";
+	$s_access_level = "Access Level";
+	$s_update_user_button = "Update User";
+	$s_delete_account_button = "Delete Account";
+
+	### account_prefs_page.php3
+	$s_default_account_preferences = "Default Account Preferences";
+	$s_advanced_report = "Advanced Report";
+	$s_advanced_report = "Advanced View";
+	$s_update_prefs = "Update Prefs";
+	$s_reset_prefs = "Reset Prefs";
+
+	### account_prefs_update.php3
+	$s_prefs_updated = "Preferences have been successfully updated...";
+
+	### account_profile_add.php3
+	$s_profile_added = "Profile added...";
+
+	### account_profile_delete.php3
+	$s_profile_deleted = "Deleted profile...";
+
+	### account_profile_edit_page.php3
+	$s_edit_profile_title = "Edit Profile";
+	$s_platform = "Platform";
+	$s_operating_system = "Operating System";
+	$s_version = "Version/Build";
+	$s_additional_description = "Additional Description";
+	$s_update_profile_button = "Update Profile";
+
+	### account_profile_make_default.php3
+	$s_profile_defaulted = "Default profile updated...";
+
+	### account_profile_manage_page.php3
+	$s_add_profile = "Add Profile";
+	$s_add_profile_button = "Add Profile";
+	$s_edit_or_delete_profiles = "Edit or Delete Profiles";
+	$s_edit_profile = "Edit Profile";
+	$s_make_default = "Make Default";
+	$s_delete_profile = "Delete Profile";
+	$s_select_profile = "Select Profile";
+	$s_submit = "Submit";
+
+	### account_profile_update.php3
+	$s_profile_updated = "Profile updated...";
+
+	### account_update.php3
+	$s_account_protected = "Account protected. Cannot change settings...";
+	$s_account_updated = "Your account has been successfully updated...";
+
+	### bug_delete.php3
+	$s_bug_deleted = "Bug has been deleted...";
+
+	### bug_delete_page.php3
+	$s_delete_bug_sure = "Are you sure you wish to delete this bug?";
+	$s_delete_bug_button = "Delete Bug";
+
+	### bug_reopen.php3
+	$s_bug_reopened = "Bug has been reopened...";
+
+	### bug_update.php3
+	$s_bug_updated = "Bug has been successfully updated...";
+
+	### bug_update_advanced_page.php3
+
+	### bug_update_page.php3
+
+	### bug_vote_add.php3
+	$s_vote_added = "Vote has been added...";
+
+	### bugnote_add.php3
+	$s_bugnote_added = "Bugnote added...";
+
+	### bugnote_add_inc.php
+	$s_add_bugnote_button = "Add Bugnote";
+
+	### bugnote_add_page.php3
+	$s_add_bugnote_title = "Add Bugnote";
+	$s_add_note_button = "Add Note";
+
+	### bugnote_delete.php3
+	$s_bugnote_deleted = "Bugnote has been successfully deleted...";
+	$s_bug_notes_title = "Bug Notes";
+
+	### bugnote_inc.php
+	$s_no_bugnotes = "There are no notes attached to this bug.";
+
+	### index.php3
+	$s_click_to_login = "Click here to login";
+
+	### login.php3
+
+	### login_error_page.php3
+	$s_login_error = "There was an error: your acount may be disabled or the username/password you entered is incorrect.";
+	$s_login_title = "Login";
+	$s_username = "Username";
+	$s_password = "Password";
+	$s_save_login = "Save Login";
+	$s_login_button = "Login";
+
+	### login_page.php3
+	### logout_page.php3
+	$s_logged_out = "Logged Out...";
+	$s_redirecting = "...Redirecting";
+	$s_here = "Here";
+
+	### main_page.php3
+	$s_newer_news = "Newer News";
+	$s_older_news = "Older News";
+
+	### manage_category_page.php3
+	$s_edit_categories_title = "Edit Categories";
+	$s_category_names = "Category Names";
+	$s_update_categories_button = "Update Categories";
+
+	### manage_category_update.php3
+	$s_categories_updated = "Cateogries successfully updated...";
+
+	### manage_create_new_user.php3
+	$s_created_user_part1 = "Created user";
+	$s_created_user_part2 = "with an access level of";
+
+	### manage_create_user_page.php3
+	$s_create_new_account_title = "Create New Account";
+	$s_verify_password = "Verify Password";
+	$s_access_level = "Access Level";
+	$s_enabled = "Enabled";
+	$s_protected = "Protected";
+	$s_create_user_button = "Create User";
+
+	### manage_page.php3
+	$s_create_new_account_link = "Create New Account";
+	$s_manage_categories_link = "Manage Categories";
+	$s_manage_product_versions_link = "Manage Product Versions";
+	$s_documentation = "Documentation";
+	$s_manage_accounts_title = "Manage Accounts";
+	$s_date_created = "Date Created";
+	$s_last_visit = "Last Visit";
+	$s_edit_user_link = "Edit User";
+
+	### manage_product_versions_page.php3
+	$s_edit_product_versions_title = "Edit Product Versions";
+	$s_versions = "Versions";
+	$s_update_versions_button = "Update Versions";
+
+	### manage_product_versions_update.php3
+	$s_product_version_updated = "Product versions successfully updated...";
+
+	### manage_user_delete.php3
+	$s_account_delete_protected = "Account protected. Cannot delete this account.";
+	$s_account_deleted = "Account deleted...";
+
+	### manage_user_delete_page.php3
+	$s_delete_account_sure = "Are you sure you wish to delete this account?";
+	$s_delete_account_button = "Delete Account";
+
+	### manage_user_page.php3
+	$s_edit_user_title = "Edit User";
+	$s_update_user_button = "Update User";
+	$s_reset_password_button = "Reset Password";
+	$s_delete_user_button = "Delete User";
+	$s_reset_password_msg = "Reset Password sets the password to be blank.";
+
+	### manage_user_reset.php3
+	$s_account_reset_protected = "Account protected. Cannot reset the password.";
+	$s_account_reset = "Account password reset...";
+
+	### manage_user_update.php3
+	$s_manage_user_protected = "Account protected. Access level and enabled protected. Otherwise, account has been updated...";
+	$s_manage_user_updated = "Account successfully updated...";
+
+	### menu_inc.php
+	$s_main_link = "Main";
+	$s_view_bugs_link = "View Bugs";
+	$s_report_bug_link = "Report Bug";
+	$s_summary_link = "Summary";
+	$s_account_link = "Account";
+	$s_manage_link = "Manage";
+	$s_edit_news_link = "Edit News";
+	$s_docs_link = "Docs";
+	$s_logout_link = "Logout";
+
+	### meta_inc.php
+	### mysql_error_page.php3
+	### news_add.php3
+	### news_delete.php3
+	$s_news_deleted = "Deleted news item...";
+
+	### news_delete_page.php3
+	$s_delete_news_sure = "Are you sure you wish to delete this news item?";
+	$s_delete_news_item_button = "Delete News Item";
+
+	### news_edit_page.php3
+	$s_edit_news_title = "Edit News";
+	$s_headline = "Headline";
+	$s_body = "Body";
+	$s_update_news_button = "Update News";
+
+	### news_menu_page.php3
+	$s_add_news_title = "Add News";
+	$s_headline = "Headline";
+	$s_do_not_use = "Do not use";
+	$s_post_news_button = "Post News";
+
+	$s_edit_or_delete_news_title = "Edit or Delete News";
+	$s_edit_post = "Edit Post";
+	$s_delete_post = "Delete Post";
+	$s_select_post = "Select Post";
+	$s_submit = "Submit";
+
+	### news_update.php3
+	$s_news_updated = "News item updated...";
+
+	### report_add.php3
+	$s_report_add_error = "ERROR: There was an error in your report.";
+	$s_hit_back = "Please hit back and renter the required fields.";
+	$s_submission_thanks = "Thank you for your submission.";
+
+	### report_bug_advanced_page.php3
+	### report_bug_page.php3
+	### show_source_page.php3
+	$s_show_source_for = "Showing source for";
+	$s_not_supported_part1 = "This version";
+	$s_not_supported_part2 = "of php does not support";
+
+	### summary_page.php3
+	$s_summary_title = "Summary";
+	$s_by_status = "by status";
+	$s_by_date = "by date";
+	$s_by_severity = "by severity";
+	$s_by_resolution = "by resolution";
+	$s_by_category = "by category";
+	$s_by_priority = "by priority";
+	$s_time_stats = "time stats for resolved bugs(days)";
+	$s_longest_open_bug = "longest open bug";
+	$s_longest_open = "longest open";
+	$s_average_time = "average time";
+	$s_total_time = "total time";
+
+	### view_bug_advanced_page.php3
+	### view_bug_all_page.php3
+	### view_bug_page.php3
+	### view_user_assigned_bug_page.php3
+	### view_user_reported_bug_page.php3
+
+	### General Strings
+	$s_go_back = "Go Back";
+	$s_proceed = "Click here to proceed";
+?>
