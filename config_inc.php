45c32c38b9fe09da09c1ba73c8f09dad370581c1
Kenzaburo Ito <prescience@users.sourceforge.net>
2001-01-25 15:07:43 +0000

    additions for meta, projects, and more
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@130 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- config_inc.php
+++ config_inc.php
@@ -31,13 +31,20 @@
 	#--------------------
 	# table name prefix
 	# if you change this remember to reflect the changes in the database
-	$g_db_table_prefix   = "mantis";
+	$g_db_table_prefix = "mantis";
 	#--------------------
 
 	#--------------------
 	# email variables
 	$g_administrator_email  = "administrator@mydomain.com";   # set this
 	$g_webmaster_email      = "webmaster@mydomain.com";       # set this
+	$g_from_email           = "nobody@300baud.org";
+	#--------------------
+
+	#--------------------
+	# enter the exact url where the bugtracker is located
+	# used in the emailing system
+	$g_login_url            = "http://www.mydomain.com/mantis/";
 	#--------------------
 
 	#--------------------
@@ -50,6 +57,8 @@
 	# toggling advanced interfaces      # 1 to enable - 0 to disable
 	$g_show_advanced_report      = 1;
 	$g_show_advanced_update      = 1;
+
+	$g_allow_signup              = 1;
 
 	$g_show_version              = 0;
 
@@ -68,10 +77,12 @@
 
 	#--------------------
 	# time for coookie to live in seconds
-	$g_time_length               = 30000000;     # 1 year
+	$g_cookie_time_length        = 30000000;     # 1 year
 	# time to delay between page redirects
 	$g_wait_time                 = 2;            # in seconds
 	#--------------------
+	### minutes to wait before document is stale
+	$g_content_expire            = 0;			 # in minutes
 
 	#--------------------
 	# limit the number of news items to be displayed on the main page
@@ -99,28 +110,35 @@
 	#--------------------
 
 	#--------------------
+	# date format strings
+	$g_short_date_format = "m-d";
+	$g_normal_date_format = "m-d H:i";
+	$g_complete_date_format = "m-d-y H:i T";
+	#--------------------
+
+	#--------------------
 	# html table appearance variables
-	$g_primary_table_tags        = "";
+	$g_primary_table_tags          = "";
 	#--------------------
 
 	#--------------------
 	# color values
-	$g_white_color           = "#ffffff";    # white
+	$g_white_color                 = "#ffffff";    # white
 
-	$g_primary_color_dark    = "#d8d8d8";    # gray
-	$g_primary_color_light   = "#e8e8e8";    # light gray
-	$g_primary_border_color  = "#aaaaaa";    # dark gray
-	$g_category_title_color	 = "#c8c8e8";    # bluish
-	$g_category_title_color2 = "#c0c0c8";    # gray bluish
+	$g_primary_color_dark          = "#d8d8d8";    # gray
+	$g_primary_color_light         = "#e8e8e8";    # light gray
+	$g_primary_border_color        = "#aaaaaa";    # dark gray
+	$g_category_title_color        = "#c8c8e8";    # bluish
+	$g_category_title_color2       = "#c0c0c8";    # gray bluish
 
-	$g_table_title_color     = "#ffffff";    # white
+	$g_table_title_color           = "#ffffff";    # white
 
-	$g_new_color             = "#ffa0a0";    # red
-	$g_feedback_color        = "#ff50a8";    # purple
-	$g_acknowledged_color    = "#ffd850";    # orange
-	$g_confirmed_color       = "#ffffb0";    # yellow
-	$g_assigned_color        = "#c8c8ff";    # blue
-	$g_resolved_color        = "#ffffff";    # not used in default
+	$g_new_color                   = "#ffa0a0";    # red
+	$g_feedback_color              = "#ff50a8";    # purple
+	$g_acknowledged_color          = "#ffd850";    # orange
+	$g_confirmed_color             = "#ffffb0";    # yellow
+	$g_assigned_color              = "#c8c8ff";    # blue
+	$g_resolved_color              = "#ffffff";    # not used in default
 	#--------------------
 
 	#--------------------
@@ -128,91 +146,106 @@
 	$g_cookie_prefix     = "MANTIS";
 
 	# cookie names
-	$g_string_cookie            = $g_cookie_prefix."_STRING_COOKIE";
+	$g_string_cookie                  = $g_cookie_prefix."_STRING_COOKIE";
+	$g_project_cookie                 = $g_cookie_prefix."_PROJECT_COOKIE";
+	$g_view_all_cookie                = $g_cookie_prefix."_VIEW_ALL_COOKIE";
+	$g_view_reported_cookie           = $g_cookie_prefix."_VIEW_REPORTED_COOKIE";
+	$g_view_assigned_cookie           = $g_cookie_prefix."_VIEW_ASSIGNED_COOKIE";
 
 	# cookie values
-	$g_string_cookie_val        = $HTTP_COOKIE_VARS[$g_string_cookie];
+	$g_string_cookie_val              = $HTTP_COOKIE_VARS[$g_string_cookie];
+	$g_project_cookie_val             = $HTTP_COOKIE_VARS[$g_project_cookie];
+	$g_view_all_cookie_val            = $HTTP_COOKIE_VARS[$g_view_all_cookie];
+	$g_view_reported_cookie_val       = $HTTP_COOKIE_VARS[$g_view_reported_cookie];
+	$g_view_assigned_cookie_val       = $HTTP_COOKIE_VARS[$g_view_assigned_cookie];
 	#--------------------
 
 	#--------------------
 	# database table names
-	$g_mantis_bug_table            = $g_db_table_prefix."_bug_table";
-	$g_mantis_bug_text_table       = $g_db_table_prefix."_bug_text_table";
-	$g_mantis_bugnote_table        = $g_db_table_prefix."_bugnote_table";
-	$g_mantis_bugnote_text_table   = $g_db_table_prefix."_bugnote_text_table";
-	$g_mantis_news_table           = $g_db_table_prefix."_news_table";
-	$g_mantis_user_table           = $g_db_table_prefix."_user_table";
-	$g_mantis_user_profile_table   = $g_db_table_prefix."_user_profile_table";
-	$g_mantis_user_pref_table      = $g_db_table_prefix."_user_pref_table";
+	$g_mantis_bug_table               = $g_db_table_prefix."_bug_table";
+	$g_mantis_bug_text_table          = $g_db_table_prefix."_bug_text_table";
+	$g_mantis_bugnote_table           = $g_db_table_prefix."_bugnote_table";
+	$g_mantis_bugnote_text_table      = $g_db_table_prefix."_bugnote_text_table";
+	$g_mantis_news_table              = $g_db_table_prefix."_news_table";
+	$g_mantis_projects_table          = $g_db_table_prefix."_projects_table";
+	$g_mantis_user_table              = $g_db_table_prefix."_user_table";
+	$g_mantis_user_profile_table      = $g_db_table_prefix."_user_profile_table";
+	$g_mantis_user_pref_table         = $g_db_table_prefix."_user_pref_table";
 	#--------------------
 
 	#--------------------
 	# core file variables
-	$g_core_API_file             = "core_API.php";
-	$g_meta_include_file         = "meta_inc.php";
-	$g_menu_include_file         = "menu_inc.php";
+	$g_core_API_file                  = "core_API.php";
+	$g_meta_include_file              = "meta_inc.php";
+	$g_menu_include_file              = "menu_inc.php";
 	#--------------------
 
 	#--------------------
 	# bugnote includes
-	$g_bugnote_include_file      = "bugnote_inc.php";
-	$g_bugnote_add_include_file  = "bugnote_add_inc.php";
+	$g_bugnote_include_file           = "bugnote_inc.php";
+	$g_bugnote_add_include_file       = "bugnote_add_inc.php";
 	#--------------------
 
 	#--------------------
 	# css
-	$g_css_include_file          = "css_inc.php";
+	$g_css_include_file               = "css_inc.php";
 	#--------------------
 
 	#--------------------
 	# page names
-	$g_index                       = "index".$g_php;
-	$g_main_page                   = "main_page".$g_php;
+	$g_index                          = "index".$g_php;
+	$g_main_page                      = "main_page".$g_php;
 
 	# bug view/update
-	$g_view_bug_all_page           = "view_bug_all_page".$g_php;
-	$g_view_user_assigned_bug_page = "view_user_assigned_bug_page".$g_php;
-	$g_view_user_reported_bug_page = "view_user_reported_bug_page".$g_php;
+	$g_view_bug_all_page              = "view_bug_all_page".$g_php;
+	$g_view_user_assigned_bug_page    = "view_user_assigned_bug_page".$g_php;
+	$g_view_user_reported_bug_page    = "view_user_reported_bug_page".$g_php;
 
-	$g_view_bug_page               = "view_bug_page".$g_php;
-	$g_view_bug_advanced_page      = "view_bug_advanced_page".$g_php;
+	$g_view_bug_page                  = "view_bug_page".$g_php;
+	$g_view_bug_advanced_page         = "view_bug_advanced_page".$g_php;
 
-	$g_bug_delete_page             = "bug_delete_page".$g_php;
-	$g_bug_delete                  = "bug_delete".$g_php;
-	$g_bug_update_page             = "bug_update_page".$g_php;
-	$g_bug_update_advanced_page    = "bug_update_advanced_page".$g_php;
-	$g_bug_update                  = "bug_update".$g_php;
+	$g_bug_assign                     = "bug_assign".$g_php;
 
-	$g_bug_reopen_page             = "bug_reopen_page".$g_php;
+	$g_bug_delete_page                = "bug_delete_page".$g_php;
+	$g_bug_delete                     = "bug_delete".$g_php;
+	$g_bug_update_page                = "bug_update_page".$g_php;
+	$g_bug_update_advanced_page       = "bug_update_advanced_page".$g_php;
+	$g_bug_update                     = "bug_update".$g_php;
 
-	$g_bug_resolve_page            = "bug_resolve_page".$g_php;
-	$g_bug_resolve_page2           = "bug_resolve_page2".$g_php;
+	$g_bug_reopen_page                = "bug_reopen_page".$g_php;
+
+	$g_bug_resolve_page               = "bug_resolve_page".$g_php;
+	$g_bug_resolve_page2              = "bug_resolve_page2".$g_php;
 
 	# vote
-	$g_bug_vote_add                = "bug_vote_add".$g_php;
+	$g_bug_vote_add                   = "bug_vote_add".$g_php;
 
 	# bugnote
-	$g_bugnote_add_page            = "bugnote_add_page".$g_php;
-	$g_bugnote_add                 = "bugnote_add".$g_php;
-	$g_bugnote_delete              = "bugnote_delete".$g_php;
+	$g_bugnote_add_page               = "bugnote_add_page".$g_php;
+	$g_bugnote_add                    = "bugnote_add".$g_php;
+	$g_bugnote_delete                 = "bugnote_delete".$g_php;
 
 	# report bug
-	$g_report_bug_page             = "report_bug_page".$g_php;
-	$g_report_bug_advanced_page    = "report_bug_advanced_page".$g_php;
-	$g_report_add                  = "report_add".$g_php;
+	$g_report_bug_page                = "report_bug_page".$g_php;
+	$g_report_bug_advanced_page       = "report_bug_advanced_page".$g_php;
+	$g_report_add                     = "report_add".$g_php;
 
 	# summary
-	$g_summary_page                = "summary_page".$g_php;
+	$g_summary_page                   = "summary_page".$g_php;
 
 	# user feedback
-	$g_view_user_reported_bug_page = "view_user_reported_bug_page".$g_php;
-	$g_view_user_assigned_bug_page = "view_user_assigned_bug_page".$g_php;
+	$g_view_user_reported_bug_page    = "view_user_reported_bug_page".$g_php;
+	$g_view_user_assigned_bug_page    = "view_user_assigned_bug_page".$g_php;
+
+	#signup
+	$g_signup_page                    = "signup_page".$g_php;
+	$g_signup                         = "signup".$g_php;
 
 	# account
-	$g_account_page                = "account_page".$g_php;
-	$g_account_update              = "account_update".$g_php;
-	$g_account_delete_page         = "account_delete_page".$g_php;
-	$g_account_delete              = "account_delete".$g_php;
+	$g_account_page                   = "account_page".$g_php;
+	$g_account_update                 = "account_update".$g_php;
+	$g_account_delete_page            = "account_delete_page".$g_php;
+	$g_account_delete                 = "account_delete".$g_php;
 
 	$g_account_profile_manage_page    = "account_profile_manage_page".$g_php;
 	$g_account_profile_add            = "account_profile_add".$g_php;
@@ -226,53 +259,64 @@
 	$g_account_prefs_reset            = "account_prefs_reset".$g_php;
 
 	# site management
-	$g_manage_page                 = "manage_page".$g_php;
-	$g_manage_create_new_user      = "manage_create_new_user".$g_php;
-	$g_manage_create_user_page     = "manage_create_user_page".$g_php;
+	$g_manage_page                    = "manage_page".$g_php;
+	$g_manage_create_new_user         = "manage_create_new_user".$g_php;
+	$g_manage_create_user_page        = "manage_create_user_page".$g_php;
 
-	$g_manage_user_page            = "manage_user_page".$g_php;
-	$g_manage_user_update          = "manage_user_update".$g_php;
-	$g_manage_user_reset           = "manage_user_reset".$g_php;
-	$g_manage_user_delete_page     = "manage_user_delete_page".$g_php;
-	$g_manage_user_delete          = "manage_user_delete".$g_php;
+	$g_manage_user_page               = "manage_user_page".$g_php;
+	$g_manage_user_update             = "manage_user_update".$g_php;
+	$g_manage_user_reset              = "manage_user_reset".$g_php;
+	$g_manage_user_delete_page        = "manage_user_delete_page".$g_php;
+	$g_manage_user_delete             = "manage_user_delete".$g_php;
 
-	$g_documentation_page          = "documentation_page".$g_php;
+	$g_documentation_page             = "documentation_page".$g_php;
 
 	# category management
-	$g_manage_category_page        = "manage_category_page".$g_php;
-	$g_manage_category_update      = "manage_category_update".$g_php;
+	$g_manage_category_page           = "manage_category_page".$g_php;
+	$g_manage_category_update         = "manage_category_update".$g_php;
 
+	# product versions
 	$g_manage_product_versions_page   = "manage_product_versions_page".$g_php;
 	$g_manage_product_versions_update = "manage_product_versions_update".$g_php;
 
+	# multiple projects
+	$g_project_menu_page              = "project_menu_page".$g_php;
+	$g_project_select                 = "project_select".$g_php;
+	$g_project_reset                  = "project_reset".$g_php;
+	$g_project_add_page               = "project_add_page".$g_php;
+	$g_project_add                    = "project_add".$g_php;
+	$g_project_edit_page              = "project_edit_page".$g_php;
+	$g_project_update                 = "project_update".$g_php;
+	$g_project_delete                 = "project_delete".$g_php;
+
 	# news
-	$g_news_menu_page              = "news_menu_page".$g_php;
-	$g_news_edit_page              = "news_edit_page".$g_php;
-	$g_news_add                    = "news_add".$g_php;
-	$g_news_update                 = "news_update".$g_php;
-	$g_news_delete_page            = "news_delete_page".$g_php;
-	$g_news_delete                 = "news_delete".$g_php;
+	$g_news_menu_page                 = "news_menu_page".$g_php;
+	$g_news_edit_page                 = "news_edit_page".$g_php;
+	$g_news_add                       = "news_add".$g_php;
+	$g_news_update                    = "news_update".$g_php;
+	$g_news_delete_page               = "news_delete_page".$g_php;
+	$g_news_delete                    = "news_delete".$g_php;
 
 	# userland documentation
-	$g_usage_doc_page              = "documentation.html";
+	$g_usage_doc_page                 = "documentation.html";
 
 	# login
-	$g_login                       = "login".$g_php;
-	$g_login_page                  = "login_page".$g_php;
-	$g_login_error_page            = "login_error_page".$g_php;
-	$g_login_success_page          = "index".$g_php;
-	$g_logout_page                 = "logout_page".$g_php;
-	$g_logout_redirect_page        = ".";
+	$g_login                          = "login".$g_php;
+	$g_login_page                     = "login_page".$g_php;
+	$g_login_error_page               = "login_error_page".$g_php;
+	$g_login_success_page             = "index".$g_php;
+	$g_logout_page                    = "logout_page".$g_php;
+	$g_logout_redirect_page           = ".";
 
 	# debug only
-	$g_show_source_page            = "show_source_page".$g_php;
+	$g_show_source_page               = "show_source_page".$g_php;
 
 	# errors
-	$g_mysql_error_page            = "mysql_error_page".$g_php;
+	$g_mysql_error_page               = "mysql_error_page".$g_php;
 	#--------------------
 
 	#--------------------
 	#version
-	$g_mantis_version       = "0.13.0";
+	$g_mantis_version       = "0.14.0";
 	#--------------------
 ?>
