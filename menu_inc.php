f85edf79b48fc96dc9e2b055e8960010f9c035f5
Kenzaburo Ito <prescience@users.sourceforge.net>
2001-02-03 20:39:27 +0000

    tweaks
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@137 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- menu_inc.php
+++ menu_inc.php
@@ -1,6 +1,6 @@
 <?
 	# Mantis - a php based bugtracking system
-	# Copyright (C) 2000  Kenzaburo Ito - kenito@300baud.org
+	# Copyright (C) 2000, 2001  Kenzaburo Ito - kenito@300baud.org
 	# This program is distributed under the terms and conditions of the GPL
 	# See the files README and LICENSE for details
 ?>
@@ -10,12 +10,14 @@
 		global 	$g_string_cookie_val, $g_path,
 				$g_mantis_user_table, $g_mantis_user_pref_table,
 				$g_hostname, $g_db_username, $g_db_password, $g_database_name,
+
 				$g_main_page, $g_view_bug_all_page,
 				$g_report_bug_page, $g_report_bug_advanced_page,
 				$g_summary_page, $g_account_page, $g_manage_page,
 				$g_news_menu_page, $g_usage_doc_page, $g_logout_page,
+
 				$s_main_link, $s_view_bugs_link, $s_report_bug_link,
-				$s_summary_link, $s_account_link,
+				$s_summary_link, $s_account_link, $g_manage_project_menu_page,
 				$s_manage_link, $s_edit_news_link, $s_docs_link,
 				$s_logout_link;
 	}
@@ -24,12 +26,8 @@
 
 	### grab the access level and protected information for the
 	### currently logged in user
-    $query = "SELECT access_level, protected
-    		FROM $g_mantis_user_table
-			WHERE cookie_string='$g_string_cookie_val'";
-    $result = db_query($query);
-    $t_access_level = db_result( $result, 0, 0 );
-    $t_protected = db_result( $result, 0, 1 );
+    $t_access_level = get_current_user_field( "access_level" );
+    $t_protected = get_current_user_field( "protected" );
 ?>
 <div align=center>
 	<a href="<? echo $g_path.$g_main_page ?>"><? echo $s_main_link ?></a> |
