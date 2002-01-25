485cf62c8da4cc7b713dc7d2aff67e4ad42233fb
aspinei <aspinei@users.sourceforge.net>
2002-01-25 16:00:49 +0000

    fixed summary for "all projects" option
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@698 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- summary_page.php
+++ summary_page.php
@@ -9,11 +9,14 @@
 <?php
 	db_connect( $g_hostname, $g_db_username, $g_db_password, $g_database_name );
 
+	#checking if it's a per project statistic or all projects
+	if ($g_project_cookie_val=='0000000') $specific_where = " 1=1";
+	else $specific_where = " project_id='$g_project_cookie_val'";
 	$t_res_val = RESOLVED;
 	$query = "SELECT id, UNIX_TIMESTAMP(date_submitted) as date_submitted,
 			UNIX_TIMESTAMP(last_updated) as last_updated
 			FROM $g_mantis_bug_table
-			WHERE project_id='$g_project_cookie_val' AND status='$t_res_val'";
+			WHERE $specific_where AND status='$t_res_val'";
 	$result = db_query( $query );
 	$bug_count = db_num_rows( $result );
 
