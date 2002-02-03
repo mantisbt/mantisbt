a42c79f35ef80dcd3722006d90923659c726eb40
Kenzaburo Ito <prescience@users.sourceforge.net>
2002-02-03 21:58:51 +0000

    Modified default/config_inc1.php to have more more css global variables.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@708 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- css_inc.php
+++ css_inc.php
@@ -8,23 +8,24 @@
 			$g_table_border_color, $g_category_title_color,
 			$g_primary_color1, $g_primary_color2,
 			$g_form_title_color, $g_spacer_color,
-			$g_menu_color;
+			$g_menu_color, $g_fonts,
+			$g_font_small, $g_font_normal, $g_font_large;
 ?>
 <style type="text/css">
-body 				{ background-color: <?php echo $g_background_color ?>; font-family:Verdana, Arial; font-size: 10pt; margin: 0px; margin-top: 6px; margin-bottom: 6px; }
-p 					{ font-family: Verdana, Arial, Helvetica; font-size: 10pt }
+body 				{ background-color: <?php echo $g_background_color ?>; font-family:Verdana, Arial; font-size: <?php echo $g_font_normal ?>; margin: 0px; margin-top: 6px; margin-bottom: 6px; }
+p 					{ font-family: <?php echo $g_fonts ?>; font-size: <?php echo $g_font_normal ?> }
 p.center			{ text-align: center }
-address 			{ font-family: Verdana, Arial, Helvetica; font-size: 8pt }
-span				{ font-family: Verdana, Arial, Helvetica; font-size: 10pt; }
+address 			{ font-family: <?php echo $g_fonts ?>; font-size: <?php echo $g_font_small ?> }
+span				{ font-family: <?php echo $g_fonts ?>; font-size: <?php echo $g_font_normal ?>; }
 table				{}
-td 					{ font-family: Verdana, Arial, Helvetica; font-size: 10pt; padding: 4px; text-align: left }
+td 					{ font-family: <?php echo $g_fonts ?>; font-size: <?php echo $g_font_normal ?>; padding: 4px; text-align: left }
 a					{}
-a.small-link		{ font-size: 8pt; }
-span.print			{ font-size: 8pt; }
+a.small-link		{ font-size: <?php echo $g_font_small ?>; }
+span.print			{ font-size: <?php echo $g_font_small ?>; }
 
-span.required 		{ font-size: 8pt; color: <?php echo $g_required_color ?>; }
-span.small 			{ font-size: 8pt }
-span.pagetitle		{ font-size: 12pt; font-weight: bold; text-align: center }
+span.required 		{ font-size: <?php echo $g_font_small ?>; color: <?php echo $g_required_color ?>; }
+span.small 			{ font-size: <?php echo $g_font_small ?> }
+span.pagetitle		{ font-size: <?php echo $g_font_large ?>; font-weight: bold; text-align: center }
 span.bold			{ font-weight: bold; }
 span.italic			{ font-style: italic; }
 
@@ -43,8 +44,8 @@ td.col-2			{ background-color: <?php echo $g_primary_color2 ?>; }
 td.form-title		{ background-color: <?php echo $g_form_title_color ?>; font-weight: bold; }
 td.nopad			{ padding: 0px; }
 td.spacer			{ background-color: <?php echo $g_spacer_color ?>; font-size: 1pt; line-height: 0.1; }
-td.small-caption	{ font-size: 8pt; }
-td.print			{ font-size: 8pt; text-align: center; }
+td.small-caption	{ font-size: <?php echo $g_font_small ?>; }
+td.print			{ font-size: <?php echo $g_font_small ?>; text-align: center; }
 
 tr.center			{ text-align: center; }
 tr.row-1			{ background-color: <?php echo $g_primary_color1 ?>; }
@@ -57,7 +58,7 @@ td.login-info-left	{ width: 33%; padding: 0px; text-align: left; }
 td.login-info-middle{ width: 33%; padding: 0px; text-align: center; }
 td.login-info-right	{ width: 33%; padding: 0px; text-align: right; }
 span.login-username	{ font-style: italic; }
-span.login-time		{ font-size: 8pt; font-style: italic; }
+span.login-time		{ font-size: <?php echo $g_font_small ?>; font-style: italic; }
 
 /* Menu */
 td.menu				{ background-color: <?php echo $g_menu_color ?>; text-align: center; }
@@ -70,7 +71,7 @@ td.quick-summary-right	{ width: 50%; text-align: right; }
 td.news-heading		{ background-color: <?php echo $g_primary_color1 ?>; text-align: left; border-bottom: 1px solid <?php echo $g_table_border_color ?>; }
 td.news-body		{ background-color: <?php echo $g_spacer_color ?>; padding: 16px; }
 span.news-headline	{ font-weight: bold; }
-span.news-date		{ font-style: italic; font-size: 8pt; }
-a.news-email		{ font-size: 8pt; }
+span.news-date		{ font-style: italic; font-size: <?php echo $g_font_small ?>; }
+a.news-email		{ font-size: <?php echo $g_font_small ?>; }
 
 </style>
