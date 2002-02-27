cca66454fd857b19412cb6ae9a06e78c9f4106b9
aspinei <aspinei@users.sourceforge.net>
2002-02-27 09:53:54 +0000

    added LDAP functionality (crypted password so that /etc/passwd or /etc/shadow info could be used)
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@738 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- core_ldap_API.php
+++ core_ldap_API.php
@@ -52,29 +52,46 @@
 	}
 	# --------------------
 	# Return true if the $uid has $password (salt soon!)
-	function ldap_uid_pass($uid, $pass) {
-		global $g_ldap_organisation,$g_ldap_server,$g_ldap_root_dn;
+	function ldap_uid_pass($login, $pass) {
+	global $g_ldap_organisation,$g_ldap_server,$g_ldap_root_dn,$g_ldapauth_type;
 
-	# @@@ Add MD5/SALT/OTHER one-way-encryption support for the password <leighm@linuxbandwagon.com>
+	$search_dn = "(&$g_ldap_organisation(uid=$login))";
+	$ds        = ldap_connect( "$g_ldap_server" );
 
-		$search_dn = "(&$g_ldap_organisation(uid=$uid)(userpassword=$pass))";
-		$ds        = ldap_connect( "$g_ldap_server" );
+	if ( $ds ) {
+		$r       = ldap_bind( $ds ); # bind to server
 
-		if ( $ds ) {
-			$r       = ldap_bind( $ds ); # bind to server
-			$sr      = ldap_search( $ds, $g_ldap_root_dn, $search_dn ); # query
-			$entries = ldap_count_entries( $ds, $sr );
-
-			ldap_close( $ds ); # clean up
-			if ( $entries >= 1 ) {
-				return true;
-			} else {
-				return false;
-			}
-		} else {
-			echo "<h4>Unable to connect to LDAP server</h4>";
-			die;
+		if ("CLEAR" == $g_ldapauth_type)
+		{
+			$crypted_pass = $pass;
 		}
+		elseif ("CRYPT" == $g_ldapauth_type)
+		{
+			$sr	= ldap_search( $ds, $g_ldap_root_dn, $search_dn ); # query without password
+			$entry	= ldap_first_entry($ds, $sr);
+			if (!($entry)) return false;
+			$values	= ldap_get_values($ds, $entry,"userpassword");
+			$salt	= $values[0][0].$values[0][1];
+			$crypted_pass=crypt($pass,$salt);
+		}
+		else
+		{
+			die ("wrong LDAP parameter g_ldapauth_type : [$g_ldapauth_type]");
+		}
+
+		$search_dn = "(&$g_ldap_organisation(uid=$login)(userpassword=$crypted_pass))";
+		$sr      = ldap_search( $ds, $g_ldap_root_dn, $search_dn ); # query with password matching
+		#---------------------------
+		$entries = ldap_count_entries( $ds, $sr );
+		ldap_close( $ds ); # clean up
+		if ( $entries >= 1 ) {
+			return true;
+		} else {
+			return false;
+		}
+	} else {
+		die ("Unable to connect to LDAP server");
 	}
+}
 	# --------------------
 ?>
