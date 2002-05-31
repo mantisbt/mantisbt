d55bb41ea8cd2ef2215f98bb49d4c999bde0eeb1
Kenzaburo Ito <prescience@users.sourceforge.net>
2002-05-31 23:59:48 +0000

    modified BC_Timer to only use mark_time().
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@1056 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- core_timer_API.php
+++ core_timer_API.php
@@ -13,16 +13,23 @@
 	# Charles Killian, modified by Kenzaburo Ito
 
 	# set to ON if you want to see debug level timings
-	$debug_timer = OFF;
+	$g_debug_timer = ON;
 
-	# USAGE: set #debug_timer to ON and just call
-	# $g_timer->mark_time( 'token name' );
+	# USAGE: set #debug_timer to ON and just call mark_time( 'token name' );
 	# where 'token name' is descriptive of what is happening at that point
 
 	# Normally you would mark_time() before and after a critical section of
 	# code that you are timing.  Remember to test more than once since various
 	# factors can affect actual runtime.
 
+	# --------------------
+	# You should use this function instead of the class function
+	function mark_time( $p_marker_name ) {
+		global $g_timer;
+
+		$g_timer->mark_time( $p_marker_name );
+	}
+	# --------------------
 	class BC_Timer {
 		var $atime; # this is an array of ( string token => time ) array elements.
 
@@ -45,19 +52,26 @@
 
 		# print out the timings.  If not in debug then just print out the total time.
 		function print_times() {
-			global $debug_timer;
+			global $g_debug_timer;
+
+			$t_total_time = 0;
+
+			# store end time
 			$this->atime[] = array( "END", $this->get_microtime() );
-			if ( ON == $debug_timer ) {
+
+			# if debug then display debug timings
+			if ( ON == $g_debug_timer ) {
 				for ($i=0;$i+1<count($this->atime);$i++) {
 					$time = $this->atime[$i+1][1]-$this->atime[$i][1];
 					#echo number_format( $time, 5 )." = ".$this->atime[$i][0]." : ".$this->atime[$i+1][0]."<br />";
-					PRINT '<span class="italic">Time: '.number_format( $time, 5 ).' seconds for '.$this->atime[$i][0].' -to- '.$this->atime[$i+1][0].'</span><br />';
+					PRINT '<span class="italic">Time: '.number_format( $time, 6 ).' seconds for '.$this->atime[$i][0].' -to- '.$this->atime[$i+1][0].'</span><br />';
 				}
-			} else {
-				$end = count( $this->atime )-1;
-				$time = $this->atime[$end][1]-$this->atime[0][1];
-				PRINT '<span class="italic">Time: '.number_format( $time, 6 ).' seconds.</span><br />';
 			}
+
+			# display total time
+			$end = count( $this->atime )-1;
+			$time = $this->atime[$end][1]-$this->atime[0][1];
+			PRINT '<span class="italic">Time: '.number_format( $time, 6 ).' seconds.</span><br />';
 		}
 	}
 	# --------------------
