701d2e2c30087fd490f13403e66b5c605a7f4eab
Kenzaburo Ito <prescience@users.sourceforge.net>
2002-06-10 00:03:59 +0000

    code formatting
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@1119 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- core_compress_API.php
+++ core_compress_API.php
@@ -1,25 +1,25 @@
 <?php
+	# Starts the buffering/compression (only if the compression option is ON)
+	# This method should be called after all possible re-directs and access
+	# level checks.
+	# ----------------
+	function start_compression() {
+		global $g_compress_html;
 
-# Starts the buffering/compression (only if the compression option is ON)
-# This method should be called after all possible re-directs and access
-# level checks.
-function start_compression() {
-	global $g_compress_html;
-
-	if ( ON == $g_compress_html ) {
-		ob_implicit_flush( 0 );
-		ob_start( 'ob_gzhandler' );
+		if ( ON == $g_compress_html ) {
+			ob_implicit_flush( 0 );
+			ob_start( 'ob_gzhandler' );
+		}
 	}
-}
+	# ----------------
+	# Stop buffering and flush buffer contents.
+	function stop_compression() {
+		global $g_compress_html;
 
-# Stop buffering and flush buffer contents.
-function stop_compression() {
-	global $g_compress_html;
-
-	if ( ON == $g_compress_html ) {
-		ob_end_flush();
-		ob_implicit_flush();
+		if ( ON == $g_compress_html ) {
+			ob_end_flush();
+			ob_implicit_flush();
+		}
 	}
-}
-
+	# ----------------
 ?>
