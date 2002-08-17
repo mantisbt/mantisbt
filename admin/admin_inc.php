d66b904c8b04b3950f50732a9e518ba98dd9eead
Victor Boctor <vboctor@gmail.com>
2002-08-17 03:16:53 +0000

    Fixed a problem where admin upgrade scripts were not working after the configuration defaults were moved from defaults/ to mantis main directory.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@1223 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- admin/admin_inc.php
+++ admin/admin_inc.php
@@ -1,25 +1,19 @@
 <?php
-	$t_mantis_path = dirname( dirname(__FILE__) ) . '/';
+	$t_mantis_path = '../';
 
 	require ( $t_mantis_path . 'constant_inc.php' );
-	
+
 	$t_custom_constants = $t_mantis_path . 'custom_constant_inc.php';
 	if ( file_exists( $t_custom_constants )) {
 		include ( $t_custom_constants );
 	}
 
-	require $t_mantis_path . 'default/config_inc1.php';
+	require $t_mantis_path . 'config_defaults_inc.php';
 
 	$t_custom_config = $t_mantis_path . 'custom_config_inc.php';
 	if ( !file_exists( $t_custom_config )) {
 		$t_custom_config = $t_mantis_path . 'config_inc.php';
 	}
 	
-	if ( file_exists( $t_custom_config ) ) {
-		include ( $t_custom_config );
-	}
-
-	require $t_mantis_path . 'default/config_inc2.php';
-
 	require $t_mantis_path . 'core_database_API.php';
 ?>
