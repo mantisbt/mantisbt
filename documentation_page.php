ca91d1037c92d548023801146e40c0a40e60775c
Julian Fitzell <jfitzell@users.sourceforge.net>
2003-01-11 22:29:38 +0000

    * documentation_page.php   Make this page display valid html.  We strip out the style block and the bdoy contents from phpinfo() output and display them in the appropriate places
    The catch and original patch were by Ondrej Jombik <nepto@pobox.sk> but I modified it to get the style block as well
    
    
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@1727 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- documentation_page.php
+++ documentation_page.php
@@ -12,6 +12,20 @@
 ?>
 <?php 
 	print_page_top1();
+
+	# get the phpinfo() content
+	ob_start();
+	phpinfo();
+	$content = ob_get_contents();
+	ob_end_clean();
+
+	# get the <style> block
+	$style = preg_replace( '|^.*(<style.*</style>).*$|si', '\1', $content );
+	# add '.phpinfo' before each style definition
+	$style = preg_replace( '/(.*\{.*\}.*)/', '.phpinfo \1', $style );
+	# output the <style> block
+	echo $style;
+
 	print_page_top2();
 
 	print_manage_menu( 'documentation_page.php' );
@@ -20,6 +34,10 @@
 
 	echo '<br />';
 
-	phpinfo();
+	# output the contents of the <body> block inside a div with class phpinfo
+	echo '<div class="phpinfo">';
+	$body = preg_replace( '|^.*<body>(.*)</body>.*$|si', '\1', $content );
+	echo $body;
+	echo '</div>';
 ?>
 <?php print_page_bot1( __FILE__ ) ?>
