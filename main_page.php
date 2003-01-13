2f0a1c35891c26bc429ce229bf22d3c4f4341214
int2str <int2str@users.sourceforge.net>
2003-01-13 21:23:58 +0000

    Removed top bug summary links for anonymous users.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@1738 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- main_page.php
+++ main_page.php
@@ -19,12 +19,20 @@
 <?php print_page_top2() ?>
 
 <div class="quick-summary-left">
-		<?php echo lang_get( 'open_and_assigned_to_me' ) ?>:
-		<?php echo '<a href="view_all_set.php?type=1&amp;reporter_id=any&amp;show_status=any&amp;show_severity=any&amp;show_category=any&amp;handler_id=' .  auth_get_current_user_id() . '&amp;hide_closed=on">' . current_user_get_assigned_open_bug_count() . '</a>' ?>
+		<?php 
+        	if ( !current_user_is_anonymous() ) {
+            	echo lang_get( 'open_and_assigned_to_me' ) . ':';
+		    	echo '<a href="view_all_set.php?type=1&amp;reporter_id=any&amp;show_status=any&amp;show_severity=any&amp;show_category=any&amp;handler_id=' .  auth_get_current_user_id() . '&amp;hide_closed=on">' . current_user_get_assigned_open_bug_count() . '</a>';
+			}
+		?>
 </div>
 <div class="quick-summary-right">
-		<?php echo lang_get( 'open_and_reported_to_me' ) ?>:
-		<?php PRINT '<a href="view_all_set.php?type=1&amp;reporter_id=' . auth_get_current_user_id() . '&amp;show_status=any&amp;show_severity=any&amp;show_category=any&amp;handler_id=any&amp;hide_closed=on">' . current_user_get_reported_open_bug_count() . '</a>' ?>
+		<?php 
+        	if ( !current_user_is_anonymous() ) {
+				echo lang_get( 'open_and_reported_to_me' ) . ':';
+				echo '<a href="view_all_set.php?type=1&amp;reporter_id=' . auth_get_current_user_id() . '&amp;show_status=any&amp;show_severity=any&amp;show_category=any&amp;handler_id=any&amp;hide_closed=on">' . current_user_get_reported_open_bug_count() . '</a>';
+			}
+		?>
 </div>
 <br />
 
