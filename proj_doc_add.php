a8a7fac392490a6d5b3bf3dc3a91a1830a46347e
Julian Fitzell <jfitzell@users.sourceforge.net>
2003-02-09 01:43:04 +0000

    Thanks to a patch from Ondrej Jombik <nepto@pobox.sk> from ages ago. Most of his patch had been fixed elsewhere by the time I got to it but this part was missed.
    * proj_doc_add.php: correct a variable reference so database uploads work
    
    
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@1809 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- proj_doc_add.php
+++ proj_doc_add.php
@@ -54,7 +54,7 @@
 						}
 						break;
 			case DATABASE:
-						$t_content = addslashes( fread ( fopen( $f_file, 'rb' ), $t_file_size ) );
+						$t_content = addslashes( fread ( fopen( $f_tmp_name, 'rb' ), $t_file_size ) );
 						$query = "INSERT INTO mantis_project_file_table
 								(id, project_id, title, description, diskfile, filename, folder, filesize, file_type, date_added, content)
 								VALUES
