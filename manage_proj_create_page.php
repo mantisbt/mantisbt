f1dcd2c93dd3b261f234e81ad21c31703656cb9a
Julian Fitzell <jfitzell@users.sourceforge.net>
2003-02-09 10:20:38 +0000

    Move project creation to a separate page instead of having it on the project list page (for clarity and consistency with the user management)
    * manage_proj_create_page.php: new page that prompts for project info when
        creating a new project
    
    * manage_proj_page.php: remove project creation code and replace it with a
        link to manage_proj_create_page.php
    
    
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@1812 f5dc347c-c33d-0410-90a0-b07cc1902cb9

---
+++ manage_proj_create_page.php
@@ -0,0 +1,90 @@
+<?php
+	# Mantis - a php based bugtracking system
+	# Copyright (C) 2000 - 2002  Kenzaburo Ito - kenito@300baud.org
+	# Copyright (C) 2002 - 2003  Mantis Team   - mantisbt-dev@lists.sourceforge.net
+	# This program is distributed under the terms and conditions of the GPL
+	# See the README and LICENSE files for details
+
+	# --------------------------------------------------------
+	# $Id: manage_proj_create_page.php,v 1.1 2003-02-09 10:20:38 jfitzell Exp $
+	# --------------------------------------------------------
+?>
+<?php require_once( 'core.php' ) ?>
+<?php login_cookie_check() ?>
+<?php
+	check_access( config_get( 'create_project_threshold' ) );
+?>
+<?php print_page_top1() ?>
+<?php print_page_top2() ?>
+
+<?php print_manage_menu( 'manage_proj_create_page.php' ) ?>
+
+<br />
+<div align="center">
+<form method="post" action="manage_proj_add.php">
+<table class="width75" cellspacing="1">
+<tr>
+	<td class="form-title" colspan="2">
+		<?php echo lang_get( 'add_project_title' ) ?>
+	</td>
+</tr>
+<tr class="row-1">
+	<td class="category" width="25%">
+		<?php echo lang_get( 'project_name' )?>
+	</td>
+	<td width="75%">
+		<input type="text" name="name" size="64" maxlength="128" />
+	</td>
+</tr>
+<tr class="row-2">
+	<td class="category">
+		<?php echo lang_get( 'status' ) ?>
+	</td>
+	<td>
+		<select name="status">
+		<?php print_enum_string_option_list( 'project_status' ) ?>
+		</select>
+	</td>
+</tr>
+<tr class="row-1">
+	<td class="category">
+		<?php echo lang_get( 'view_status' ) ?>
+	</td>
+	<td>
+		<select name="view_state">
+			<?php print_enum_string_option_list( 'view_state' ) ?>
+		</select>
+	</td>
+</tr>
+<?php
+	if ( config_get( 'allow_file_upload' ) ) {
+	?>
+		<tr class="row-2">
+			<td class="category">
+				<?php echo lang_get( 'upload_file_path' ) ?>
+			</td>
+			<td>
+				<input type="text" name="file_path" size="70" maxlength="250" />
+			</td>
+		</tr>
+		<?php
+	}
+?>
+<tr class="row-1">
+	<td class="category">
+		<?php echo lang_get( 'description' ) ?>
+	</td>
+	<td>
+		<textarea name="description" cols="60" rows="5" wrap="virtual"></textarea>
+	</td>
+</tr>
+<tr>
+	<td class="center" colspan="2">
+		<input type="submit" value="<?php echo lang_get( 'add_project_button' ) ?>" />
+	</td>
+</tr>
+</table>
+</form>
+</div>
+
+<?php print_page_bot1( __FILE__ ) ?>
--- manage_proj_page.php
+++ manage_proj_page.php
@@ -6,7 +6,7 @@
 	# See the README and LICENSE files for details
 
 	# --------------------------------------------------------
-	# $Id: manage_proj_page.php,v 1.2 2003-01-30 09:41:23 jfitzell Exp $
+	# $Id: manage_proj_page.php,v 1.3 2003-02-09 10:20:38 jfitzell Exp $
 	# --------------------------------------------------------
 ?>
 <?php
@@ -37,82 +37,17 @@
 
 <?php print_manage_menu( 'manage_proj_page.php' ) ?>
 
-<?php if ( access_level_check_greater_or_equal ( config_get( 'create_project_threshold' ) ) ) { # Add Project Form BEGIN ?>
-<br />
-<div align="center">
-<form method="post" action="manage_proj_add.php">
-<table class="width75" cellspacing="1">
-<tr>
-	<td class="form-title" colspan="2">
-		<?php echo lang_get( 'add_project_title' ) ?>
-	</td>
-</tr>
-<tr class="row-1">
-	<td class="category" width="25%">
-		<?php echo lang_get( 'project_name' )?>
-	</td>
-	<td width="75%">
-		<input type="text" name="name" size="64" maxlength="128" />
-	</td>
-</tr>
-<tr class="row-2">
-	<td class="category">
-		<?php echo lang_get( 'status' ) ?>
-	</td>
-	<td>
-		<select name="status">
-		<?php print_enum_string_option_list( 'project_status' ) ?>
-		</select>
-	</td>
-</tr>
-<tr class="row-1">
-	<td class="category">
-		<?php echo lang_get( 'view_status' ) ?>
-	</td>
-	<td>
-		<select name="view_state">
-			<?php print_enum_string_option_list( 'view_state' ) ?>
-		</select>
-	</td>
-</tr>
-<?php
-	if ( config_get( 'allow_file_upload' ) ) {
-	?>
-		<tr class="row-2">
-			<td class="category">
-				<?php echo lang_get( 'upload_file_path' ) ?>
-			</td>
-			<td>
-				<input type="text" name="file_path" size="70" maxlength="250" />
-			</td>
-		</tr>
-		<?php
-	}
-?>
-<tr class="row-1">
-	<td class="category">
-		<?php echo lang_get( 'description' ) ?>
-	</td>
-	<td>
-		<textarea name="description" cols="60" rows="5" wrap="virtual"></textarea>
-	</td>
-</tr>
-<tr>
-	<td class="center" colspan="2">
-		<input type="submit" value="<?php echo lang_get( 'add_project_button' ) ?>" />
-	</td>
-</tr>
-</table>
-</form>
-</div>
-<?php } # Add Project Form END ?>
-
 <?php # Project Menu Form BEGIN ?>
 <br />
 <table class="width100" cellspacing="1">
 <tr>
 	<td class="form-title" colspan="5">
 		<?php echo lang_get( 'projects_title' ) ?>
+		<?php
+		if ( access_level_check_greater_or_equal ( config_get( 'create_project_threshold' ) ) ) {
+			print_bracket_link( 'manage_proj_create_page.php', lang_get( 'create_new_project_link' ) );
+		}
+		?>
 	</td>
 </tr>
 <tr class="row-category">
