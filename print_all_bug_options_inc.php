c9e686f64e4b8f9939bb623258f96020bec6d748
Julian Fitzell <jfitzell@users.sourceforge.net>
2003-02-22 18:29:22 +0000

    * print_all_bug_options_inc.php   (edit_printing_prefs):     + a global was being used for the table name without being imported into       the function.  I replace it with a config_get() call.     + changed the default for $p_user_id to null instead of 0     + use auth_get_current_user_id()     + use is_blank()     + clean up parentheses spacing in several spaces   - generally clean up spacing and add a use of string_attribute()
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@1991 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- print_all_bug_options_inc.php
+++ print_all_bug_options_inc.php
@@ -45,33 +45,34 @@ function get_field_names()
 }
 
 
-function edit_printing_prefs($p_user_id = 0, $p_error_if_protected = true, $p_redirect_url = '')
+function edit_printing_prefs( $p_user_id = null, $p_error_if_protected = true, $p_redirect_url = '' )
 {
-	$c_user_id = (integer)$p_user_id;
-
-	if ($c_user_id == 0) {
-		$c_user_id = current_user_get_field( 'id' );
+	if ( null === $p_user_id ) {
+		$p_user_id = auth_get_current_user_id();
 	}
 
-	$t_redirect_url = $p_redirect_url;
-	if ( strlen ($t_redirect_url)== 0 ) {
-		$t_redirect_url = 'print_all_bug_page.php';
-	}
+	$c_user_id = db_prepare_int( $p_user_id );
 
 	# protected account check
 	if ( $p_error_if_protected ) {
 		user_ensure_unprotected( $p_user_id );
 	}
 
+	$t_user_print_pref_table = config_get( 'mantis_user_print_pref_table' );
+
+	if ( is_blank( $p_redirect_url ) ) {
+		$p_redirect_url = 'print_all_bug_page.php';
+	}
+
 	# get the fields list
 	$t_field_name_arr = get_field_names();
-	$field_name_count = count($t_field_name_arr);
+	$field_name_count = count( $t_field_name_arr );
 
 	# Grab the data
     $query = "SELECT print_pref
-    		FROM $g_mantis_user_print_pref_table
+    		FROM $t_user_print_pref_table
 			WHERE user_id='$c_user_id'";
-    $result = db_query($query);
+    $result = db_query( $query );
 
     ## OOPS, No entry in the database yet.  Lets make one
     if ( 0 == db_num_rows( $result ) ) {
@@ -80,26 +81,26 @@ function edit_printing_prefs($p_user_id = 0, $p_error_if_protected = true, $p_re
 		for ($i=0 ; $i<$field_name_count ; $i++) {
 			$t_default_arr[$i] = 1 ;
 		}
-		$t_default = implode('',$t_default_arr) ;
+		$t_default = implode( '', $t_default_arr ) ;
 
 		# all fields are added by default
 		$query = "INSERT
-				INTO $g_mantis_user_print_pref_table
+				INTO $t_user_print_pref_table
 				(user_id, print_pref)
 				VALUES
 				('$c_user_id','$t_default')";
 
-		$result = db_query($query);
+		$result = db_query( $query );
 
 		# Rerun select query
 	    $query = "SELECT print_pref
-	    		FROM $g_mantis_user_print_pref_table
+	    		FROM $t_user_print_pref_table
 				WHERE user_id='$c_user_id'";
-	    $result = db_query($query);
+	    $result = db_query( $query );
     }
 
     # putting the query result into an array with the same size as $t_fields_arr
-	$row = db_fetch_array($result);
+	$row = db_fetch_array( $result );
 	$t_prefs = $row[0];
 
 ?>
@@ -109,8 +110,8 @@ function edit_printing_prefs($p_user_id = 0, $p_error_if_protected = true, $p_re
 <br />
 <div align="center">
 <form method="post" action="print_all_bug_options_update.php">
-<input type="hidden" name="user_id" value="<?php echo $c_user_id ?>" />
-<input type="hidden" name="redirect_url" value="<?php echo $t_redirect_url ?>" />
+<input type="hidden" name="user_id" value="<?php echo $p_user_id ?>" />
+<input type="hidden" name="redirect_url" value="<?php echo string_attribute( $p_redirect_url ) ?>" />
 <table class="width75" cellspacing="1">
 <tr>
 	<td class="form-title">
@@ -134,8 +135,8 @@ for ($i=0 ; $i <$field_name_count ; $i++) {
 	<td>
 		<?php # @@@ REWORK Code should not span two lines except in extreme cases.  Build this into a variable then print it out.  ?>
 		<?php //echo 'print_'.strtolower(str_replace(' ','_', $t_field_name_arr[$i])); ?>
-		<input type="checkbox" name="<?php echo 'print_'.strtolower(str_replace(' ','_', $t_field_name_arr[$i])); ?>"
-		<?php if ( isset($t_prefs[$i])&&($t_prefs[$i]==1) ) echo 'checked="checked"' ?> />
+		<input type="checkbox" name="<?php echo 'print_' . strtolower( str_replace( ' ', '_', $t_field_name_arr[$i] ) ); ?>"
+		<?php if ( isset( $t_prefs[$i] ) && ( $t_prefs[$i]==1 ) ) echo 'checked="checked"' ?> />
 	</td>
 </tr>
 
