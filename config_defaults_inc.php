3b192b41ef7db1337349f1e0b099b04b26c0c5e1
Jeroen Latour <jlatour@users.sourceforge.net>
2003-02-26 14:14:56 +0000

    Preparing for 0.18.0a3 release...
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@2044 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- config_defaults_inc.php
+++ config_defaults_inc.php
@@ -6,7 +6,7 @@
 	# See the README and LICENSE files for details
 
 	# --------------------------------------------------------
-	# $Id: config_defaults_inc.php,v 1.104 2003-02-26 08:23:58 int2str Exp $
+	# $Id: config_defaults_inc.php,v 1.105 2003-02-26 14:14:56 jlatour Exp $
 	# --------------------------------------------------------
 
 	
@@ -257,7 +257,7 @@
 	#############################
 
 	# --- version variables -----------
-	$g_mantis_version		= '0.18.0-CVS';
+	$g_mantis_version		= '0.18.0a3';
 	$g_show_version			= ON;
 
 	################################
