79588ba68fcfbe15073f9ec09604a9ddeb6e252e
Julian Fitzell <jfitzell@users.sourceforge.net>
2003-02-26 08:48:42 +0000

    Fix issue #2997
    * manage_user_proj_add.php: properly check project permissions inside the loop.
        We were accidentaly passing in the whole project_id array
    
    
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@2037 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- manage_user_proj_add.php
+++ manage_user_proj_add.php
@@ -6,7 +6,7 @@
 	# See the README and LICENSE files for details
 
 	# --------------------------------------------------------
-	# $Id: manage_user_proj_add.php,v 1.18 2003-02-15 10:25:17 jfitzell Exp $
+	# $Id: manage_user_proj_add.php,v 1.19 2003-02-26 08:48:42 jfitzell Exp $
 	# --------------------------------------------------------
 ?>
 <?php require_once( 'core.php' ) ?>
@@ -17,10 +17,11 @@
 	$f_project_id	= gpc_get_int_array( 'project_id', array() );
 
 	access_ensure_global_level( config_get( 'manage_user_threshold' ) );
-	access_ensure_project_level( config_get( 'project_user_threshold' ), $f_project_id );
 
 	foreach ( $f_project_id as $t_proj_id ) {
-		project_add_user( $t_proj_id, $f_user_id, $f_access_level );
+		if ( access_has_project_level( config_get( 'project_user_threshold' ), $t_proj_id ) ) {
+			project_add_user( $t_proj_id, $f_user_id, $f_access_level );
+		}
 	}
 
 	print_header_redirect( 'manage_user_edit_page.php?user_id=' . $f_user_id );
