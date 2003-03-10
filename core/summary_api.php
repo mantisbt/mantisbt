f450f74de605d01a056d306a014b1db3b57e53ff
int2str <int2str@users.sourceforge.net>
2003-03-10 10:55:57 +0000

    *** empty log message ***
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@2080 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- core/summary_api.php
+++ core/summary_api.php
@@ -6,20 +6,20 @@
 	# See the README and LICENSE files for details
 
 	# --------------------------------------------------------
-	# $Id: summary_api.php,v 1.13 2003-03-10 09:40:55 int2str Exp $
+	# $Id: summary_api.php,v 1.14 2003-03-10 10:55:57 int2str Exp $
 	# --------------------------------------------------------
 
 	###########################################################################
 	# Summary printing API
 	###########################################################################
 
-	function summary_helper_print_row( $row, $label, $open, $resolved, $closed, $total ) {
-		printf( "<tr %s>",  helper_alternate_class( $row ) );
-		printf( "<td width=\"50%%\">%s</td>", $label );
-		printf( "<td width=\"12%%\" class=\"right\">%d</td>", $open );
-		printf( "<td width=\"12%%\" class=\"right\">%d</td>", $resolved );
-		printf( "<td width=\"12%%\" class=\"right\">%d</td>", $closed );
-		printf( "<td width=\"12%%\" class=\"right\">%d</td>", $total );
+	function summary_helper_print_row( $p_label, $p_open, $p_resolved, $p_closed, $p_total ) {
+		printf( "<tr %s>",  helper_alternate_class() );
+		printf( "<td width=\"50%%\">%s</td>", $p_label );
+		printf( "<td width=\"12%%\" class=\"right\">%d</td>", $p_open );
+		printf( "<td width=\"12%%\" class=\"right\">%d</td>", $p_resolved );
+		printf( "<td width=\"12%%\" class=\"right\">%d</td>", $p_closed );
+		printf( "<td width=\"12%%\" class=\"right\">%d</td>", $p_total );
 		print( "</tr>\n" );
 	}
 	# --------------------
@@ -27,8 +27,8 @@
 	# Given the enum string this function prints out the summary 
 	# for each enum setting
 	# The enum field name is passed in through $p_enum
-	function print_bug_enum_summary( $p_enum_string, $p_enum ) {
-		global 	$g_mantis_bug_table;
+	function summary_print_by_enum( $p_enum_string, $p_enum ) {
+		$t_mantis_bug_table = config_get( 'mantis_bug_table' );
 
 		$t_arr = explode_enum_string( $p_enum_string );
 		$enum_count = count( $t_arr );
@@ -37,67 +37,67 @@
 
 		#checking if it's a per project statistic or all projects
 		if ( ALL_PROJECTS == $t_project_id ) 
-			$specific_where = ' 1=1';
+			$t_project_filter = ' 1=1';
 		else 
-			$specific_where = " project_id='$t_project_id'";
+			$t_project_filter = " project_id='$t_project_id'";
 
-		$query = "SELECT status, $p_enum FROM $g_mantis_bug_table"
-			. " WHERE $specific_where ORDER BY $p_enum";
+		$query = "SELECT status, $p_enum FROM $t_mantis_bug_table"
+			. " WHERE $t_project_filter ORDER BY $p_enum";
 
 		$result = db_query( $query );
 
-		$last_value = -1;
-		$bugs_open = 0;
-		$bugs_resolved = 0;
-		$bugs_closed = 0;
-		$bugs_total = 0;
+		$t_last_value = -1;
+		$t_bugs_open = 0;
+		$t_bugs_resolved = 0;
+		$t_bugs_closed = 0;
+		$t_bugs_total = 0;
 
 		$t_resolved_val = RESOLVED;
 		$t_closed_val = CLOSED;
 
 		while ( $row = db_fetch_array( $result ) ) {
-			if ( $row[$p_enum] != $last_value 
-			  && $last_value != -1 ) {
-				summary_helper_print_row( $i 
-				  , get_enum_element( $p_enum, $last_value)
-			  	  , $bugs_open, $bugs_resolved
-				  , $bugs_closed, $bugs_total );
+			if ( $row[$p_enum] != $t_last_value 
+			  && $t_last_value != -1 ) {
+				summary_helper_print_row( 
+				  get_enum_element( $p_enum, $t_last_value)
+			  	  , $t_bugs_open, $t_bugs_resolved
+				  , $t_bugs_closed, $t_bugs_total );
 
-				$bugs_open = 0;
-				$bugs_resolved = 0;
-				$bugs_closed = 0;
-				$bugs_total = 0;
+				$t_bugs_open = 0;
+				$t_bugs_resolved = 0;
+				$t_bugs_closed = 0;
+				$t_bugs_total = 0;
 			}
 
-			$bugs_total++;
+			$t_bugs_total++;
 
 			switch( $row['status'] ) {
 				case $t_resolved_val:
-					$bugs_resolved++;
+					$t_bugs_resolved++;
 					break;
 				case $t_closed_val:
-					$bugs_closed++;
+					$t_bugs_closed++;
 					break;
 				default:
-					$bugs_open++;
+					$t_bugs_open++;
 					break;
 			}
 
-			$last_value = $row[$p_enum];
+			$t_last_value = $row[$p_enum];
 		}
 
-		if ( 0 < $bugs_total ) {
-			summary_helper_print_row( $i 
-			  , get_enum_element( $p_enum, $last_value)
-			  , $bugs_open, $bugs_resolved
-			  , $bugs_closed, $bugs_total );
+		if ( 0 < $t_bugs_total ) {
+			summary_helper_print_row( 
+			  get_enum_element( $p_enum, $t_last_value)
+			  , $t_bugs_open, $t_bugs_resolved
+			  , $t_bugs_closed, $t_bugs_total );
 		}
 	}
 	# --------------------
 	# prints the bugs submitted in the last X days (default is 1 day) for the
 	# current project
-	function get_bug_count_by_date( $p_time_length=1 ) {
-		global $g_mantis_bug_table;
+	function summary_bug_count_by_date( $p_time_length=1 ) {
+		$t_mantis_bug_table = config_get( 'mantis_bug_table' );
 
 		$c_time_length = (integer)$p_time_length;
 
@@ -108,7 +108,7 @@
 		else $specific_where = " project_id='$t_project_id'";
 
 		$query = "SELECT COUNT(*)
-				FROM $g_mantis_bug_table
+				FROM $t_mantis_bug_table
 				WHERE TO_DAYS(NOW()) - TO_DAYS(date_submitted) <= '$c_time_length' AND $specific_where";
 		$result = db_query( $query );
 		return db_result( $result, 0 );
@@ -116,12 +116,12 @@
 	# --------------------
 	# This function shows the number of bugs submitted in the last X days
 	# An array of integers representing days is passed in
-	function print_bug_date_summary( $p_date_array ) {
+	function summary_print_by_date( $p_date_array ) {
 		$arr_count = count( $p_date_array );
 		for ($i=0;$i<$arr_count;$i++) {
-			$t_enum_count = get_bug_count_by_date( $p_date_array[$i] );
+			$t_enum_count = summary_bug_count_by_date( $p_date_array[$i] );
 
-			printf( "<tr %s>",  helper_alternate_class( $row ) );
+			printf( "<tr %s>",  helper_alternate_class() );
 			printf( "<td width=\"50%%\">%s</td>", $p_date_array[$i] );
 			printf( "<td class=\"right\">%s</td>", $t_enum_count );
 			print(  "</tr>\n" );
@@ -129,8 +129,9 @@
 	}
 	# --------------------
 	# print bug counts by assigned to each developer
-	function print_developer_summary() {
-		global 	$g_mantis_bug_table, $g_mantis_user_table;
+	function summary_print_by_developer() {
+		$t_mantis_bug_table = config_get( 'mantis_bug_table' );
+		$t_mantis_user_table = config_get( 'mantis_user_table' );
 		
 		$t_project_id = helper_get_current_project();
 
@@ -139,17 +140,17 @@
 		else 
 			$specific_where = " project_id='$t_project_id'";
 
-		$query = "SELECT handler_id, status FROM $g_mantis_bug_table"
+		$query = "SELECT handler_id, status FROM $t_mantis_bug_table"
 			. " WHERE handler_id>0 AND $specific_where"
 			. " ORDER BY handler_id";
 
 		$result = db_query( $query );
 
 		$last_handler = -1;
-		$bugs_open = 0;
-		$bugs_resolved = 0;
-		$bugs_closed = 0;
-		$bugs_total = 0;
+		$t_bugs_open = 0;
+		$t_bugs_resolved = 0;
+		$t_bugs_closed = 0;
+		$t_bugs_total = 0;
 
 		$t_resolved_val = RESOLVED;
 		$t_closed_val = CLOSED;
@@ -160,55 +161,56 @@
 			if ( $v_handler_id != $last_handler 
 			  && $last_handler != -1 ) {
 				$query = "SELECT username"
-					. " FROM $g_mantis_user_table"
+					. " FROM $t_mantis_user_table"
 					. " WHERE id=$last_handler";
 				$result2 = db_query( $query );
 				$row2 = db_fetch_array( $result2 );
-				summary_helper_print_row( $i 
-				  , $row2['username']
-			  	  , $bugs_open, $bugs_resolved
-				  , $bugs_closed, $bugs_total );
+				summary_helper_print_row( 
+				  $row2['username']
+			  	  , $t_bugs_open, $t_bugs_resolved
+				  , $t_bugs_closed, $t_bugs_total );
 
-				$bugs_open = 0;
-				$bugs_resolved = 0;
-				$bugs_closed = 0;
-				$bugs_total = 0;
+				$t_bugs_open = 0;
+				$t_bugs_resolved = 0;
+				$t_bugs_closed = 0;
+				$t_bugs_total = 0;
 			}
 
-			$bugs_total++;
+			$t_bugs_total++;
 
 			switch( $v_status ) {
 				case $t_resolved_val:
-					$bugs_resolved++;
+					$t_bugs_resolved++;
 					break;
 				case $t_closed_val:
-					$bugs_closed++;
+					$t_bugs_closed++;
 					break;
 				default:
-					$bugs_open++;
+					$t_bugs_open++;
 					break;
 			}
 
 			$last_handler = $v_handler_id;
 		}
 
-		if ( 0 < $bugs_total ) {
+		if ( 0 < $t_bugs_total ) {
 			$query = "SELECT username"
-				. " FROM $g_mantis_user_table"
+				. " FROM $t_mantis_user_table"
 				. " WHERE id=$last_handler";
 			$result2 = db_query( $query );
 			$row2 = db_fetch_array( $result2 );
-			summary_helper_print_row( $i 
-			  , $row2['username']
-			  , $bugs_open, $bugs_resolved
-			  , $bugs_closed, $bugs_total );
+			summary_helper_print_row(
+			  $row2['username']
+			  , $t_bugs_open, $t_bugs_resolved
+			  , $t_bugs_closed, $t_bugs_total );
 		}
 	}
 	# --------------------
 	# print bug counts by reporter id
-	function print_reporter_summary() {
-		global $g_mantis_bug_table, $g_mantis_user_table;
-		global $g_reporter_summary_limit;
+	function summary_print_by_reporter() {
+		$t_mantis_bug_table = config_get( 'mantis_bug_table' );
+		$t_mantis_user_table = config_get( 'mantis_user_table' );
+		$t_reporter_summary_limit = config_get( 'reporter_summary_limit' );
 		
 		$t_project_id = helper_get_current_project();
 
@@ -218,63 +220,64 @@
 			$specific_where = " project_id='$t_project_id'";
 
 		$query = "SELECT reporter_id, COUNT(*) as num"
-			. " FROM $g_mantis_bug_table"
+			. " FROM $t_mantis_bug_table"
 			. " WHERE $specific_where"
 			. " GROUP BY reporter_id"
 			. " ORDER BY num DESC"
-			. " LIMIT $g_reporter_summary_limit";
+			. " LIMIT $t_reporter_summary_limit";
 		$result = db_query( $query );
 		while ( $row = db_fetch_array( $result ) ) {
 			$v_reporter_id = $row['reporter_id'];
-			$query = "SELECT status FROM $g_mantis_bug_table"
+			$query = "SELECT status FROM $t_mantis_bug_table"
 				. " WHERE reporter_id=$v_reporter_id"
 				. " AND $specific_where";
 			$result2 = db_query( $query );
 
 			$last_reporter = -1;
-			$bugs_open = 0;
-			$bugs_resolved = 0;
-			$bugs_closed = 0;
-			$bugs_total = 0;
+			$t_bugs_open = 0;
+			$t_bugs_resolved = 0;
+			$t_bugs_closed = 0;
+			$t_bugs_total = 0;
 
 			$t_resolved_val = RESOLVED;
 			$t_closed_val = CLOSED;
 
 			while ( $row2 = db_fetch_array( $result2 ) ) {
-				$bugs_total++;
+				$t_bugs_total++;
 
 				switch( $row2['status'] ) {
 					case $t_resolved_val:
-						$bugs_resolved++;
+						$t_bugs_resolved++;
 						break;
 					case $t_closed_val:
-						$bugs_closed++;
+						$t_bugs_closed++;
 						break;
 					default:
-						$bugs_open++;
+						$t_bugs_open++;
 						break;
 				}
 			}
 
-			if ( 0 < $bugs_total ) {
+			if ( 0 < $t_bugs_total ) {
 				$query = "SELECT username"
-					. " FROM $g_mantis_user_table"
+					. " FROM $t_mantis_user_table"
 					. " WHERE id=$v_reporter_id";
 				$result3 = db_query( $query );
 				$row3 = db_fetch_array( $result3 );
-				summary_helper_print_row( $i 
-				, $row3['username']
-				, $bugs_open, $bugs_resolved
-				, $bugs_closed, $bugs_total );
+				summary_helper_print_row(
+				$row3['username']
+				, $t_bugs_open, $t_bugs_resolved
+				, $t_bugs_closed, $t_bugs_total );
 			}
 		}
 	}
 	# --------------------
 	# print a bug count per category
-	function print_category_summary() {
-		global 	$g_mantis_bug_table;
-		global	$g_mantis_project_table;
-		global	$g_summary_category_include_project;
+	function summary_print_by_category() {
+		$t_mantis_bug_table = config_get( 'mantis_bug_table' );
+		$t_mantis_project_table = config_get( 'mantis_project_table' );
+		$t_summary_category_include_project = 
+			config_get( 'summary_category_include_project' );
 		
 		$t_project_id = helper_get_current_project();
 
@@ -283,7 +286,7 @@
 		else 
 			$specific_where = " project_id='$t_project_id'";
 
-		$query = "SELECT project_id, category, status FROM $g_mantis_bug_table"
+		$query = "SELECT project_id, category, status FROM $t_mantis_bug_table"
 			. " WHERE category>'' AND $specific_where"
 			. " ORDER BY project_id, category, status";
 
@@ -291,10 +294,10 @@
 
 		$last_category = -1;
 		$last_project = -1;
-		$bugs_open = 0;
-		$bugs_resolved = 0;
-		$bugs_closed = 0;
-		$bugs_total = 0;
+		$t_bugs_open = 0;
+		$t_bugs_resolved = 0;
+		$t_bugs_closed = 0;
+		$t_bugs_total = 0;
 
 		$t_resolved_val = RESOLVED;
 		$t_closed_val = CLOSED;
@@ -305,38 +308,38 @@
 			if ( $v_category != $last_category 
 			  && $last_category != -1 ) {
 			$label = $last_category;
-				if ( ( ON == $g_summary_category_include_project ) 
+				if ( ( ON == $t_summary_category_include_project ) 
 				  && ( ALL_PROJECTS == $t_project_id ) ) {
 					$query = "SELECT name"
-						. " FROM $g_mantis_project_table"
+						. " FROM $t_mantis_project_table"
 						. " WHERE id=$last_project";
 					$result2 = db_query( $query );
 					$row2 = db_fetch_array( $result2 );
 
 					$label = sprintf( "[%s] %s", $row2['name'], $label );
 				}
-				summary_helper_print_row( $i 
-				, $label
-				, $bugs_open, $bugs_resolved
-				, $bugs_closed, $bugs_total );
+				summary_helper_print_row(
+				$label
+				, $t_bugs_open, $t_bugs_resolved
+				, $t_bugs_closed, $t_bugs_total );
 
-				$bugs_open = 0;
-				$bugs_resolved = 0;
-				$bugs_closed = 0;
-				$bugs_total = 0;
+				$t_bugs_open = 0;
+				$t_bugs_resolved = 0;
+				$t_bugs_closed = 0;
+				$t_bugs_total = 0;
 			}
 
-			$bugs_total++;
+			$t_bugs_total++;
 
 			switch( $v_status ) {
 				case $t_resolved_val:
-					$bugs_resolved++;
+					$t_bugs_resolved++;
 					break;
 				case $t_closed_val:
-					$bugs_closed++;
+					$t_bugs_closed++;
 					break;
 				default:
-					$bugs_open++;
+					$t_bugs_open++;
 					break;
 			}
 
@@ -344,22 +347,22 @@
 			$last_project = $v_project_id;
 		}
 
-		if ( 0 < $bugs_total ) {
+		if ( 0 < $t_bugs_total ) {
 			$label = $last_category;
-			if ( ( ON == $g_summary_category_include_project ) 
+			if ( ( ON == $t_summary_category_include_project ) 
 			  && ( ALL_PROJECTS == $t_project_id ) ) {
 				$query = "SELECT name"
-					. " FROM $g_mantis_project_table"
+					. " FROM $t_mantis_project_table"
 					. " WHERE id=$last_project";
 				$result2 = db_query( $query );
 				$row2 = db_fetch_array( $result2 );
 				
 				$label = sprintf( "[%s] %s", $row2['name'], $label );
 			}
-			summary_helper_print_row( $i 
-			  , $label
-			  , $bugs_open, $bugs_resolved
-			  , $bugs_closed, $bugs_total );
+			summary_helper_print_row( 
+			  $label
+			  , $t_bugs_open, $t_bugs_resolved
+			  , $t_bugs_closed, $t_bugs_total );
 		}
 	}
 	# --------------------
