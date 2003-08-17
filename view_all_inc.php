26fe8f576a23c8f400a8abf6fd70731df33829d6
Victor Boctor <vboctor@gmail.com>
2003-08-17 14:41:17 +0000

    Fix #2981: Resolution in overview (show resolution in View Bugs page).
    M view_all_inc.php
    - Added a title (bubble) to the status to indicate the current resolution.  Resolution examples, include open, fixed, not-a-bug, won't-fix, ...etc.
    
    
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@2222 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- view_all_inc.php
+++ view_all_inc.php
@@ -413,7 +413,7 @@
 	<?php # -- Status / Handler -- ?>
 	<td class="center">
 		<?php
-			echo get_enum_element( 'status', $v_status );
+			echo '<a title="' . get_enum_element( 'resolution', $v_resolution ) . '">' . get_enum_element( 'status', $v_status ) . '</a>';
 			# print username instead of status
 			if ( $v_handler_id > 0 && ON == config_get( 'show_assigned_names' ) ) {
 				echo ' (';
