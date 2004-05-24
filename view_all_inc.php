4ac9951db839ce70ee8caa35ac4f4ae64421039a
int2str <int2str@users.sourceforge.net>
2004-05-24 08:12:45 +0000

    HTML and PHP code cleanup.
    - Reduced number of times we drop in and out of <?php ?> mode to
      avoid whitespace created in the output. (And maybe speed up
      parsing?)
    - Standardized table cell creation a little
    - Removed whitespace in output
    - removed class="center" and made center default alignment, since
      its used in all but 2/3 columns in the list
    
    
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@2561 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- view_all_inc.php
+++ view_all_inc.php
@@ -6,7 +6,7 @@
 	# See the README and LICENSE files for details
 
 	# --------------------------------------------------------
-	# $Id: view_all_inc.php,v 1.138 2004-05-21 21:42:50 int2str Exp $
+	# $Id: view_all_inc.php,v 1.139 2004-05-24 08:12:44 int2str Exp $
 	# --------------------------------------------------------
 ?>
 <?php
@@ -17,8 +17,7 @@
 	require_once( $t_core_path.'string_api.php' );
 	require_once( $t_core_path.'date_api.php' );
 	require_once( $t_core_path.'icon_api.php' );
-?>
-<?php
+
 	$t_filter = current_user_get_bug_filter();
 
 	$t_sort = $t_filter['sort'];
@@ -28,13 +27,13 @@
 
 	$t_icon_path = config_get( 'icon_path' );
 	$t_update_bug_threshold = config_get( 'update_bug_threshold' );
-?>
-<?php # -- ====================== FILTER FORM ========================= -- ?>
-<?php filter_draw_selection_area( $f_page_number ); ?>
-<?php # -- ====================== end of FILTER FORM ========================= -- ?>
 
-<?php # -- ====================== BUG LIST ========================= -- ?>
-<?php
+	# -- ====================== FILTER FORM ========================= --
+	filter_draw_selection_area( $f_page_number );
+	# -- ====================== end of FILTER FORM ================== --
+
+	# -- ====================== BUG LIST ============================ -- 
+
 	$col_count = 10;
 
 	if ( STATUS_LEGEND_POSITION_TOP == config_get( 'status_legend_position' ) ) {
@@ -54,120 +53,121 @@
 
 <br />
 <form method="get" action="bug_actiongroup_page.php">
-<table class="width100" cellspacing="1">
-<?php # -- Navigation header row -- ?>
+<table id="buglist" class="width100" cellspacing="1">
 <tr>
-	<?php # -- Viewing range info -- ?>
-	<td class="form-title" colspan="<?php echo $col_count - 2; ?>">
-		<?php echo lang_get( 'viewing_bugs_title' ) ?>
-		<?php
+	<td class="form-title" colspan="<?php print( $col_count - 2 ); ?>">
+		<?php 
+			# -- Viewing range info --
+
+			$v_start = 0;
+			$v_end   = 0;
+
 			if ( sizeof( $rows ) > 0 ) {
 				$v_start = $t_filter['per_page'] * ($f_page_number-1) +1;
 				$v_end   = $v_start + sizeof( $rows ) -1;
-			} else {
-				$v_start = 0;
-				$v_end   = 0;
 			}
-			echo "($v_start - $v_end / $t_bug_count)";
+
+			print( lang_get( 'viewing_bugs_title' ) );
+			print( "($v_start - $v_end / $t_bug_count)" );
 		?>
 
-		<?php # -- Print and Export links -- ?>
-		<span class="small">
-			<?php
+		<span class="small"> <?php
+				# -- Print and Export links --
+
 				print_bracket_link( 'print_all_bug_page.php', lang_get( 'print_all_bug_page_link' ) );
-				echo '&nbsp;';
+				print( '&nbsp;' );
 				print_bracket_link( 'csv_export.php', lang_get( 'csv_export' ) );
-			?>
-		</span>
-		<?php # -- end Print and Export links -- ?>
+		?> </span>
 	</td>
 
-	<?php # -- Page number links -- ?>
 	<td class="right" colspan="2">
-		<span class="small">
-			<?php print_page_links( 'view_all_bug_page.php', 1, $t_page_count, $f_page_number ) ?>
-		</span>
+		<span class="small"> <?php 
+			# -- Page number links -- 
+
+			print_page_links( 'view_all_bug_page.php', 1, $t_page_count, $f_page_number ); 
+		?> </span>
 	</td>
 </tr>
 <?php # -- Bug list column header row -- ?>
 <tr class="row-category">
-	<td class="center">&nbsp;</td>
+	<td> &nbsp; </td>
+	<td> &nbsp; </td>
+	<td> <?php 
+		# -- Priority column -- 
 
-	<td class="center">&nbsp;</td>
+		print_view_bug_sort_link( 'P', 'priority', $t_sort, $t_dir ); 
+		print_sort_icon( $t_dir, $t_sort, 'priority' );
+	?> </td>
+	<td> <?php 
+		# -- Bug ID column --
 
-	<?php # -- Priority column -- ?>
-	<td class="center">
-		<?php print_view_bug_sort_link( 'P', 'priority', $t_sort, $t_dir ) ?>
-		<?php print_sort_icon( $t_dir, $t_sort, 'priority' ) ?>
-	</td>
+		print_view_bug_sort_link( lang_get( 'id' ), 'id', $t_sort, $t_dir );
+		print_sort_icon( $t_dir, $t_sort, 'id' );
+	?> </td>
+<?php
+		# -- Sponsorship Amount -- 
 
-	<?php # -- Bug ID column -- ?>
-	<td class="center">
-		<?php print_view_bug_sort_link( lang_get( 'id' ), 'id', $t_sort, $t_dir ) ?>
-		<?php print_sort_icon( $t_dir, $t_sort, 'id' ) ?>
-	</td>
-
-	<?php	# -- Sponsorship Amount -- 
-		if ( $t_enable_sponsorship == ON ) {
-	?>
-	<td class="center">
-		<?php
+		if ( ON == $t_enable_sponsorship ) {
+			print( "\t<td>" );
 			print_view_bug_sort_link( sponsorship_get_currency(), 'sponsorship_total', $t_sort, $t_dir );
 			print_sort_icon( $t_dir, $t_sort, 'sponsorship_total' );
-		?>
-	</td>
-	<?php } ?>
-
-	<?php # -- Bugnote count column -- ?>
-	<td class="center">
-		#
-	</td>
-
-	<?php # -- Attachment indicator --
-		if ( ON == $t_show_attachments ) {
-		  echo '<td class="center">';
-			echo '<img src="' . $t_icon_path . 'attachment.png' . '" alt="" />';
-			echo '</td>';
+			print( "</td>\n" );
 		}
-	?>
+	
+		# -- Bugnote count column -- 
+?>
+	<td> # </td>
+<?php 
+		# -- Attachment indicator --
 
-	<?php # -- Category column -- ?>
-	<td class="center">
-		<?php print_view_bug_sort_link( lang_get( 'category' ), 'category', $t_sort, $t_dir ) ?>
-		<?php print_sort_icon( $t_dir, $t_sort, 'category' ) ?>
-	</td>
+		if ( ON == $t_show_attachments ) {
+			print( "\t<td>" );
+			print( '<img src="' . $t_icon_path . 'attachment.png' . '" alt="" />' );
+			print( "</td>\n" );
+		}
+?>
+	<td> <?php 
+		# -- Category column -- 
 
-	<?php # -- Severity column -- ?>
-	<td class="center">
-		<?php print_view_bug_sort_link( lang_get( 'severity' ), 'severity', $t_sort, $t_dir ) ?>
-		<?php print_sort_icon( $t_dir, $t_sort, 'severity' ) ?>
-	</td>
+		print_view_bug_sort_link( lang_get( 'category' ), 'category', $t_sort, $t_dir );
+		print_sort_icon( $t_dir, $t_sort, 'category' );
+	?> </td>
+	<td> <?php 
+		# -- Severity column -- 
 
-	<?php # -- Status column -- ?>
-	<td class="center">
-		<?php print_view_bug_sort_link( lang_get( 'status' ), 'status', $t_sort, $t_dir ) ?>
-		<?php print_sort_icon( $t_dir, $t_sort, 'status' ) ?>
-	</td>
+		print_view_bug_sort_link( lang_get( 'severity' ), 'severity', $t_sort, $t_dir );
+		print_sort_icon( $t_dir, $t_sort, 'severity' );
+	?> </td>
+	<td> <?php
+		# -- Status column --
 
-	<?php # -- Last Updated column -- ?>
-	<td class="center">
-		<?php print_view_bug_sort_link( lang_get( 'updated' ), 'last_updated', $t_sort, $t_dir ) ?>
-		<?php print_sort_icon( $t_dir, $t_sort, 'last_updated' ) ?>
-	</td>
+		print_view_bug_sort_link( lang_get( 'status' ), 'status', $t_sort, $t_dir );
+		print_sort_icon( $t_dir, $t_sort, 'status' );
+	?> </td>
+	<td> <?php 
+		# -- Last Updated column -- 
 
-	<?php # -- Summary column -- ?>
-	<td class="center">
-		<?php print_view_bug_sort_link( lang_get( 'summary' ), 'summary', $t_sort, $t_dir ) ?>
-		<?php print_sort_icon( $t_dir, $t_sort, 'summary' ) ?>
-	</td>
+		print_view_bug_sort_link( lang_get( 'updated' ), 'last_updated', $t_sort, $t_dir );
+		print_sort_icon( $t_dir, $t_sort, 'last_updated' );
+	?> </td>
+	<td> <?php 
+		# -- Summary column -- 
+
+		print_view_bug_sort_link( lang_get( 'summary' ), 'summary', $t_sort, $t_dir );
+		print_sort_icon( $t_dir, $t_sort, 'summary' );
+	?> </td>
 </tr>
+
 <?php # -- Spacer row -- ?>
 <tr>
-	<td class="spacer" colspan="<?php echo $col_count ?>">&nbsp;</td>
+	<td class="spacer" colspan="<?php print( $col_count ); ?>"> &nbsp; </td>
 </tr>
-<?php mark_time( 'begin loop' ); ?>
-<?php # -- Loop over bug rows and create $v_* variables -- ?>
-<?php
+
+<?php 
+	mark_time( 'begin loop' );
+
+	# -- Loop over bug rows and create $v_* variables -- 
+
 	for($i=0; $i < sizeof( $rows ); $i++) {
 		# prefix bug data with v_
 
@@ -190,172 +190,173 @@
 
 		# Check for attachments
 		$t_attachment_count = 0;
-		if ( ( ON == $t_show_attachments ) && ( file_can_view_bug_attachments( $v_id ) ) ) {
-		   $t_attachment_count = file_bug_attachment_count( $v_id );
+		if ( ( ON == $t_show_attachments ) 
+		  && ( file_can_view_bug_attachments( $v_id ) ) ) {
+			$t_attachment_count = file_bug_attachment_count( $v_id );
 		}
 
 		# grab the project name
 		$project_name = project_get_field( $v_project_id, 'name' );
 ?>
-<tr bgcolor="<?php echo $status_color ?>">
-	<?php # -- Checkbox -- ?>
-<?php
-	if ( access_has_bug_level( $t_update_bug_threshold, $v_id ) ) {
-		$t_checkboxes_exist = true;
-?>
-	<td>
-		<input type="checkbox" name="bug_arr[]" value="<?php echo "$v_id" ?>" />
-	</td>
-<?php
-	} else {
-		echo '<td>&nbsp;</td>';
-}
-?>
-	
-	<?php # -- Pencil shortcut -- ?>
-	<td class="center">
-	<?php
-		if ( !bug_is_readonly( $v_id ) && access_has_bug_level( $t_update_bug_threshold, $v_id ) ) {
-			echo '<a href="' . string_get_bug_update_url( $v_id ) . '"><img border="0" src="' . $t_icon_path . 'update.png' . '" alt="' . lang_get( 'update_bug_button' ) . '" /></a>';
+<tr bgcolor="<?php print( $status_color ); ?>">
+	<td> <?php
+		# -- Checkbox -- 
+
+		if ( access_has_bug_level( $t_update_bug_threshold, $v_id ) ) {
+			$t_checkboxes_exist = true;
+			printf( "<input type=\"checkbox\" name=\"bug_arr[]\" value=\"%d\" />" , $v_id );
 		} else {
-			echo '&nbsp;';
+			print( "&nbsp;" );
 		}
-	?>
-	</td>
+	?> </td>
+	<td> <?php 
+		# -- Pencil shortcut --
 
-	<?php # -- Priority -- ?>
-	<td class="center">
-		<?php
-			if ( ON == config_get( 'show_priority_text' ) ) {
-				print_formatted_priority_string( $v_status, $v_priority );
-			} else {
-				print_status_icon( $v_priority );
-			}
-		?>
-	</td>
+		if ( !bug_is_readonly( $v_id ) 
+		  && access_has_bug_level( $t_update_bug_threshold, $v_id ) ) {
+			print( '<a href="' . string_get_bug_update_url( $v_id ) . '">' );
+			print( '<img border="0" src="' . $t_icon_path . 'update.png' );
+			print( '" alt="' . lang_get( 'update_bug_button' ) . '" /></a>' );
+		} else {
+			print( '&nbsp;' );
+		}
+	?> </td>
+	<td> <?php
+		# -- Priority --
 
-	<?php # -- Bug ID and details link -- ?>
-	<td class="center">
-		<?php print_bug_link( $v_id ) ?>
-	</td>
+		if ( ON == config_get( 'show_priority_text' ) ) {
+			print_formatted_priority_string( $v_status, $v_priority );
+		} else {
+			print_status_icon( $v_priority );
+		}
+	?> </td>
+	<td> <?php 
+		# -- Bug ID and details link -- 
 
-	<?php	# -- Sponsorship Amount -- 
+		print_bug_link( $v_id );
+	?> </td>
+<?php
+		# -- Sponsorship Amount -- 
+
 		if ( $t_enable_sponsorship == ON ) {
-	?>
-	<td class="right">
-		<?php
+			print( "\t<td class=\"right\">" );
 			if ( $v_sponsorship_total > 0 ) {
-				echo sponsorship_format_amount( $v_sponsorship_total );
+				print( sponsorship_format_amount( $v_sponsorship_total ) );
 			}
-		?>
-	</td>
-	<?php } ?>
+			print( "</td>\n" );
+		}
+?>
+	<td> <?php
+		# -- Bugnote count --
 
-	<?php # -- Bugnote count -- ?>
-	<td class="center">
-		<?php
-			if ( $bugnote_count > 0 ) {
-				$t_bugnote_link = '<a href="' . string_get_bug_view_url( $v_id ) . '&amp;nbn=' . $bugnote_count . '#bugnotes">' . $bugnote_count . '</a>'; 
-				if ( $v_bugnote_updated > strtotime( '-'.$t_filter['highlight_changed'].' hours' ) ) {
-					echo '<span class="bold">';
-					echo $t_bugnote_link;
-					echo '</span>';
-				} else {
-					echo $t_bugnote_link;
-				}
+		if ( $bugnote_count > 0 ) {
+			$t_bugnote_link = '<a href="' . string_get_bug_view_url( $v_id ) 
+				. '&amp;nbn=' . $bugnote_count . '#bugnotes">' 
+				. $bugnote_count . '</a>'; 
+
+			if ( $v_bugnote_updated > strtotime( '-'.$t_filter['highlight_changed'].' hours' ) ) {
+				printf( '<span class="bold">%s</span>', $t_bugnote_link );
 			} else {
-				echo '&nbsp;';
-			}
-		?>
-	</td>
-
-	<?php # -- Attachment indicator --
+				print( $t_bugnote_link );
+				}
+		} else {
+			print( '&nbsp;' );
+		}
+	?> </td>
+<?php 
+		# -- Attachment indicator --
 	  
 		if ( ON == $t_show_attachments ) {
-		  echo '<td class="center">';
+			print( "\t<td>" );
 			if ( 0 < $t_attachment_count ) {
-				echo '<a href="' . string_get_bug_view_url( $v_id ) . '#attachments">';
-				echo '<img border="0" src="' . $t_icon_path . 'attachment.png' . '"';
-				echo ' alt="' . lang_get( 'attachment_alt' ) . '"';
-				echo ' title="' . $t_attachment_count . ' ' . lang_get( 'attachments' ) . '"';
-				echo ' />';
-				echo '</a>';
+				print( '<a href="' . string_get_bug_view_url( $v_id ) . '#attachments">' );
+				print( '<img border="0" src="' . $t_icon_path . 'attachment.png' . '"' );
+				print( ' alt="' . lang_get( 'attachment_alt' ) . '"' );
+				print( ' title="' . $t_attachment_count . ' ' . lang_get( 'attachments' ) . '"' );
+				print( ' />' );
+				print( '</a>' );
 			} else {
-			  echo '&nbsp;';
+			  print( ' &nbsp; ' );
 			}
-			echo '</td>';
+			print( "</td>\n" );
 		}
-	?>
-
-	<?php # -- Category -- ?>
-	<td class="center">
-		<?php
-			# type project name if viewing 'all projects'
-			if ( ON == config_get( 'show_bug_project_links' ) &&
-				 helper_get_current_project() == ALL_PROJECTS ) {
-				echo '<small>[';
-				print_view_bug_sort_link( $project_name, 'project_id', $t_sort, $t_dir );
-				echo ']</small><br />';
-			}
-
-			echo string_display( $v_category );
-		?>
-	</td>
-	<?php # -- Severity -- ?>
-	<td class="center">
-		<?php print_formatted_severity_string( $v_status, $v_severity ) ?>
-	</td>
-	<?php # -- Status / Handler -- ?>
-	<td class="center">
-		<?php
-			echo '<u><a title="' . get_enum_element( 'resolution', $v_resolution ) . '">' . get_enum_element( 'status', $v_status ) . '</a></u>';
-			# print username instead of status
-			if ( $v_handler_id > 0 && ON == config_get( 'show_assigned_names' ) ) {
-				echo ' (';
-				print_user( $v_handler_id );
-				echo ')';
-			}
-		?>
-	</td>
-	<?php # -- Last Updated -- ?>
-	<td class="center">
-		<?php
-			if ( $v_last_updated > strtotime( '-'.$t_filter['highlight_changed'].' hours' ) ) {
-				echo '<span class="bold">'.$t_last_updated.'</span>';
-			} else {
-				echo $t_last_updated;
-			}
-		?>
-	</td>
-	<?php # -- Summary -- ?>
-	<td class="left">
-		<?php
-			echo $v_summary;
-			if ( VS_PRIVATE == $v_view_state ) {
-				echo ' <img src="' . $t_icon_path . 'protected.gif" width="8" height="15" alt="' . lang_get( 'private' ) . '" />';
-			}
-		 ?>
-	</td>
-</tr>
-<?php # -- end of Repeating bug row -- ?>
-<?php
-	}
 ?>
-<?php # -- ====================== end of BUG LIST ========================= -- ?>
+	<td> <?php 
+		# -- Category -- 
 
-<?php # -- ====================== MASS BUG MANIPULATION ========================= -- ?>
+		# type project name if viewing 'all projects'
+		if ( ON == config_get( 'show_bug_project_links' ) 
+		  && helper_get_current_project() == ALL_PROJECTS ) {
+			print( '<small>[' );
+			print_view_bug_sort_link( $project_name, 'project_id', $t_sort, $t_dir );
+			print( ']</small><br />' );
+		}
+
+		print( string_display( $v_category ) );
+	?> </td>
+	<td> <?php 
+		# -- Severity --
+
+		print_formatted_severity_string( $v_status, $v_severity );
+	?> </td>
+	<td> <?php 
+		# -- Status / Handler --
+
+		printf( '<u><a title="%s">%s</a></u>'
+			, get_enum_element( 'resolution', $v_resolution ) 
+			, get_enum_element( 'status', $v_status )
+		);
+
+		# print username instead of status
+		if ( ON == config_get( 'show_assigned_names' ) 
+		  && $v_handler_id > 0 ) {
+			print( ' (' );
+			print_user( $v_handler_id );
+			print( ')' );
+		}
+	?> </td>
+	<td> <?php 
+		# -- Last Updated -- 
+	
+		if ( $v_last_updated > strtotime( '-'.$t_filter['highlight_changed'].' hours' ) ) {
+			printf( '<span class="bold">%s</span>', $t_last_updated );
+		} else {
+			print( $t_last_updated );
+		}
+	?> </td>
+	<td class="left"> <?php 
+		# -- Summary --
+
+		print( $v_summary );
+		if ( VS_PRIVATE == $v_view_state ) {
+			printf( ' <img src="%s" alt="(%s)" title="%s" />'
+				, $t_icon_path . 'protected.gif'
+				, lang_get( 'private' )
+				, lang_get( 'private' )
+			);
+		}
+	 ?> </td>
+</tr>
+<?php 
+	# -- end of Repeating bug row --
+	}
+
+	# -- ====================== end of BUG LIST ========================= --
+
+	# -- ====================== MASS BUG MANIPULATION =================== -- 
+?>
 	<tr>
-		<td colspan="<?php echo $col_count-2 ?>">
+		<td class="left" colspan="<?php print( $col_count-2 ); ?>">
 <?php
 		if ( $t_checkboxes_exist ) {
 ?>
 			<select name="action">
 				<?php print_all_bug_action_option_list() ?>
 			</select>
-			<input type="submit" class="button" value="<?php echo lang_get( 'ok' );  ?>" />
+			<input type="submit" class="button" value="<?php print( lang_get( 'ok' ) ); ?>" />
 <?php
 		} else {
-			echo '&nbsp;';
+			print( '&nbsp;' );
 		}
 ?>
 		</td>
@@ -370,9 +371,10 @@
 </table>
 </form>
 
-<?php mark_time( 'end loop' ); ?>
+<?php 
 
-<?php
+	mark_time( 'end loop' ); 
+
 	if ( STATUS_LEGEND_POSITION_BOTTOM == config_get( 'status_legend_position' ) ) {
 		html_status_legend();
 	}
