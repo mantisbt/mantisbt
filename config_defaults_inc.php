4f8eef2e20762ff81b7cd06469c782a934edb945
Victor Boctor <vboctor@gmail.com>
2004-07-07 14:39:40 +0000

    Back to development mode.  Updated version and add new section in changelog.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@2655 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- config_defaults_inc.php
+++ config_defaults_inc.php
@@ -6,7 +6,7 @@
 	# See the README and LICENSE files for details
 
 	# --------------------------------------------------------
-	# $Id: config_defaults_inc.php,v 1.171 2004-07-07 12:50:17 vboctor Exp $
+	# $Id: config_defaults_inc.php,v 1.172 2004-07-07 14:39:40 vboctor Exp $
 	# --------------------------------------------------------
 
 
@@ -245,7 +245,7 @@
 	#############################
 
 	# --- version variables -----------
-	$g_mantis_version		= '0.19.0a1';
+	$g_mantis_version		= '0.19.0-CVS';
 	$g_show_version			= ON;
 
 	################################
