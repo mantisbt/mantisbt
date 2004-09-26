442b8541e2b7ecc3ce791a73e18bade688fa7963
Glenn Henshaw <thraxisp@logicaloutcome.ca>
2004-09-26 22:57:04 +0000

    fix for  0004258: Username used instead of real name (in history for monitor/unmonitor) - switch user_get_name for user_get_field('username')
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@3002 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- core/history_api.php
+++ core/history_api.php
@@ -6,7 +6,7 @@
 	# See the README and LICENSE files for details
 
 	# --------------------------------------------------------
-	# $Id: history_api.php,v 1.28 2004-08-17 18:01:18 thraxisp Exp $
+	# $Id: history_api.php,v 1.29 2004-09-26 22:57:04 thraxisp Exp $
 	# --------------------------------------------------------
 
 	### History API ###
@@ -279,11 +279,11 @@
 					$t_note = lang_get( 'bugnote_view_state' ) . ": " . $p_old_value . ": " . $p_new_value;
 					break;
 				case BUG_MONITOR:
-					$p_old_value = user_get_field( $p_old_value, 'username' );
+					$p_old_value = user_get_name( $p_old_value );
 					$t_note = lang_get( 'bug_monitor' ) . ": " . $p_old_value;
 					break;
 				case BUG_UNMONITOR:
-					$p_old_value = user_get_field( $p_old_value, 'username' );
+					$p_old_value = user_get_name( $p_old_value );
 					$t_note = lang_get( 'bug_end_monitor' ) . ": " . $p_old_value;
 					break;
 				case BUG_DELETED:
