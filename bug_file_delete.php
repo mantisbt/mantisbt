3082bf891d5cc839ffe2f69baa1f245c823b5957
marcelloscata <marcelloscata@users.sourceforge.net>
2004-12-06 20:23:33 +0000

    Added user confirmation when deleting an attachment (bug #4889)
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@3140 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- bug_file_delete.php
+++ bug_file_delete.php
@@ -6,25 +6,24 @@
 	# See the README and LICENSE files for details
 
 	# --------------------------------------------------------
-	# $Id: bug_file_delete.php,v 1.28 2004-10-05 14:59:08 thraxisp Exp $
+	# $Id: bug_file_delete.php,v 1.29 2004-12-06 20:23:03 marcelloscata Exp $
 	# --------------------------------------------------------
-?>
-<?php
+
 	# Delete a file from a bug and then view the bug
-?>
-<?php
+
 	require_once( 'core.php' );
-	
+
 	$t_core_path = config_get( 'core_path' );
-	
+
 	require_once( $t_core_path.'file_api.php' );
-?>
-<?php
+
 	$f_file_id = gpc_get_int( 'file_id' );
 
 	$t_bug_id = file_get_field( $f_file_id, 'bug_id' );
 
 	access_ensure_bug_level( config_get( 'update_bug_threshold' ), $t_bug_id );
+
+	helper_ensure_confirmed( lang_get( 'delete_attachment_sure_msg' ), lang_get( 'delete_attachment_button' ) );
 
 	file_delete( $f_file_id, 'bug' );
 
