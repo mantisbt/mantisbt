ccf6b99d212c6a20d7a7151235179c5fa15dcb5a
Glenn Henshaw <thraxisp@logicaloutcome.ca>
2005-04-24 13:27:03 +0000

    fix for 0002686: switch project often goes back to the main page
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@3468 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- set_project.php
+++ set_project.php
@@ -6,7 +6,7 @@
 	# See the README and LICENSE files for details
 
 	# --------------------------------------------------------
-	# $Id: set_project.php,v 1.53 2005-04-22 21:26:22 prichards Exp $
+	# $Id: set_project.php,v 1.54 2005-04-24 13:27:03 thraxisp Exp $
 	# --------------------------------------------------------
 ?>
 <?php
@@ -36,47 +36,37 @@
 
 	helper_set_current_project( $f_project_id );
 
-	#@@@ we really need to make this more general... it is never intuitive
-	#  to redirect to the main page as far as I can see. Is there a reason
-	#  we can't just redirect back to the referrer in all cases?  See
-	#  issue #2686 about this... -jf
-
 	# redirect to 'same page' when switching projects.
-	# view_all_* pages, and summary
+
 	# for proxies that clear out HTTP_REFERER
 	if ( !is_blank( $f_ref ) ) {
 		$t_redirect_url = $f_ref;
 	} else if ( !isset( $_SERVER['HTTP_REFERER'] ) || is_blank( $_SERVER['HTTP_REFERER'] ) ) {
 		$t_redirect_url = config_get( 'default_home_page' );
-	} else if ( eregi( 'view_all_bug_page.php', $_SERVER['HTTP_REFERER'] ) ){
-		$t_source_filter_id = filter_db_get_project_current( $f_project_id );
-		$t_redirect_url = 'view_all_set.php?type=4';
-
-		if ( $t_source_filter_id != null ) {
-			$t_redirect_url = 'view_all_set.php?type=3&amp;source_query_id=' . $t_source_filter_id;
-		}
-	} else if ( eregi( 'changelog_page.php', $_SERVER['HTTP_REFERER'] ) ){
-		$t_redirect_url =  'changelog_page.php';
-	} else if ( eregi( 'summary_page.php', $_SERVER['HTTP_REFERER'] ) ){
-		$t_redirect_url =  'summary_page.php';
-	} else if ( eregi( 'proj_user_menu_page.php', $_SERVER['HTTP_REFERER'] ) ){
-		$t_redirect_url = 'proj_user_menu_page.php';
-	} else if ( eregi( 'manage_user_page.php', $_SERVER['HTTP_REFERER'] ) ){
-		$t_redirect_url = 'manage_user_page.php';
-	} else if ( eregi( 'bug_report_page.php', $_SERVER['HTTP_REFERER'] ) ){
-		$t_redirect_url = 'bug_report_page.php';
-	} else if ( eregi( 'bug_report_advanced_page.php', $_SERVER['HTTP_REFERER'] ) ){
-		$t_redirect_url = 'bug_report_advanced_page.php';
-	} else if ( eregi( 'summary_jpgraph_page.php', $_SERVER['HTTP_REFERER'] ) ){
-		$t_redirect_url = 'summary_jpgraph_page.php';
-	} else if ( eregi( 'view_filters_page.php', $_SERVER['HTTP_REFERER'] ) ){
-		$t_redirect_url = 'view_filters_page.php';
-	} else if ( eregi( 'my_view_page.php', $_SERVER['HTTP_REFERER'] ) ){
-		$t_redirect_url = 'my_view_page.php';
-	} else if ( eregi( 'main_page.php', $_SERVER['HTTP_REFERER'] ) ){
-		$t_redirect_url = 'main_page.php';
 	} else {
-		$t_redirect_url = config_get( 'default_home_page' );
+		$t_home_page = config_get( 'default_home_page' );
+
+		# Check that referrer matches our address after squashing case (case insensitive compare)
+		$t_path = config_get( 'path' );
+		if ( strtolower( $t_path ) == strtolower( substr( $_SERVER['HTTP_REFERER'], 0, strlen( $t_path ) ) ) ) {
+			$t_referrer_page = substr( $_SERVER['HTTP_REFERER'], strlen( $t_path ) );
+			# if view_all_bug_page, pass on filter	
+			if ( eregi( 'view_all_bug_page.php', $t_referrer_page ) ) {
+				$t_source_filter_id = filter_db_get_project_current( $f_project_id );
+				$t_redirect_url = 'view_all_set.php?type=4';
+
+				if ( $t_source_filter_id !== null ) {
+					$t_redirect_url = 'view_all_set.php?type=3&amp;source_query_id=' . $t_source_filter_id;
+				}
+			} else if ( eregi( '_page.php', $t_referrer_page ) ) {
+				# if any other page, return to that page
+				$t_redirect_url = $t_referrer_page;
+			} else {
+				$t_redirect_url = $t_home_page;
+			}
+		} else {
+			$t_redirect_url = $t_home_page;
+		}
 	}
 
 	print_header_redirect( $t_redirect_url );
