36b8d72770eed6410ac470d8c7250956a9bc884b
Glenn Henshaw <thraxisp@logicaloutcome.ca>
2005-05-03 14:16:42 +0000

    fix for  0005562: db_stats doesn't use "localized" table names
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@3505 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- admin/db_stats.php
+++ admin/db_stats.php
@@ -6,7 +6,7 @@
 	# See the README and LICENSE files for details
 
 	# --------------------------------------------------------
-	# $Id: db_stats.php,v 1.1 2005-05-02 10:18:08 vboctor Exp $
+	# $Id: db_stats.php,v 1.2 2005-05-03 14:16:42 thraxisp Exp $
 	# --------------------------------------------------------
 
 	require_once ( dirname( dirname( __FILE__ ) ) . DIRECTORY_SEPARATOR . 'core.php' );
@@ -36,33 +36,33 @@
 	echo '<h1>Mantis Database Statistics</h1>';
 	# echo '<table border="1" width="50%" cellpadding="3" cellspacing="0">';
 
-	print_table_stats( 'mantis_bug_file_table' );
-	print_table_stats( 'mantis_bug_history_table' );
-	print_table_stats( 'mantis_bug_monitor_table' );
-	print_table_stats( 'mantis_bug_relationship_table' );
-	print_table_stats( 'mantis_bug_table' );
-	print_table_stats( 'mantis_bug_text_table' );
-	print_table_stats( 'mantis_bugnote_table' );
-	print_table_stats( 'mantis_bugnote_text_table' );
-	print_table_stats( 'mantis_config_table' );
-	print_table_stats( 'mantis_custom_field_project_table' );
-	print_table_stats( 'mantis_custom_field_string_table' );
-	print_table_stats( 'mantis_custom_field_table' );
-	print_table_stats( 'mantis_filters_table' );
-	print_table_stats( 'mantis_news_table' );
-	print_table_stats( 'mantis_project_category_table' );
-	print_table_stats( 'mantis_project_file_table' );
-	print_table_stats( 'mantis_project_hierarchy_table' );
-	print_table_stats( 'mantis_project_table' );
-	print_table_stats( 'mantis_project_user_list_table' );
-	print_table_stats( 'mantis_project_version_table' );
-	print_table_stats( 'mantis_sponsorship_table' );
-	print_table_stats( 'mantis_tokens_table' );
-	print_table_stats( 'mantis_upgrade_table' );
-	print_table_stats( 'mantis_user_pref_table' );
-	print_table_stats( 'mantis_user_print_pref_table' );
-	print_table_stats( 'mantis_user_profile_table' );
-	print_table_stats( 'mantis_user_table' );
+	print_table_stats( config_get( 'mantis_bug_file_table' ) );
+	print_table_stats( config_get( 'mantis_bug_history_table' ) );
+	print_table_stats( config_get( 'mantis_bug_monitor_table' ) );
+	print_table_stats( config_get( 'mantis_bug_relationship_table' ) );
+	print_table_stats( config_get( 'mantis_bug_table' ) );
+	print_table_stats( config_get( 'mantis_bug_text_table' ) );
+	print_table_stats( config_get( 'mantis_bugnote_table' ) );
+	print_table_stats( config_get( 'mantis_bugnote_text_table' ) );
+	print_table_stats( config_get( 'mantis_config_table' ) );
+	print_table_stats( config_get( 'mantis_custom_field_project_table' ) );
+	print_table_stats( config_get( 'mantis_custom_field_string_table' ) );
+	print_table_stats( config_get( 'mantis_custom_field_table' ) );
+	print_table_stats( config_get( 'mantis_filters_table' ) );
+	print_table_stats( config_get( 'mantis_news_table' ) );
+	print_table_stats( config_get( 'mantis_project_category_table' ) );
+	print_table_stats( config_get( 'mantis_project_file_table' ) );
+	print_table_stats( config_get( 'mantis_project_hierarchy_table' ) );
+	print_table_stats( config_get( 'mantis_project_table' ) );
+	print_table_stats( config_get( 'mantis_project_user_list_table' ) );
+	print_table_stats( config_get( 'mantis_project_version_table' ) );
+	print_table_stats( config_get( 'mantis_sponsorship_table' ) );
+	print_table_stats( config_get( 'mantis_tokens_table' ) );
+	print_table_stats( config_get( 'mantis_upgrade_table' ) );
+	print_table_stats( config_get( 'mantis_user_pref_table' ) );
+	print_table_stats( config_get( 'mantis_user_print_pref_table' ) );
+	print_table_stats( config_get( 'mantis_user_profile_table' ) );
+	print_table_stats( config_get( 'mantis_user_table' ) );
 
 	# echo '</table>';
 	echo '</body></html>';
