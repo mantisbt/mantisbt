e2d6bdf0a38386df2da4cc323e64ec6ae47cf5a2
Glenn Henshaw <thraxisp@logicaloutcome.ca>
2005-05-23 13:55:38 +0000

    add additional indexes on bug, bugnote, project, project_user_list, and user tables
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@3551 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- admin/upgrades/1_00_inc.php
+++ admin/upgrades/1_00_inc.php
@@ -8,7 +8,7 @@
 	# Changes applied to 0.18 database
 
 	# --------------------------------------------------------
-	# $Id: 1_00_inc.php,v 1.2 2005-04-20 13:54:56 vboctor Exp $
+	# $Id: 1_00_inc.php,v 1.3 2005-05-23 13:55:38 thraxisp Exp $
 	# --------------------------------------------------------
 ?>
 <?php
@@ -39,6 +39,54 @@
 			'note_bug_id_index',
 			'Add index on bug_id in bugnotes table',
 			"ALTER TABLE $t_bugnote_table ADD INDEX ( bug_id )"
+		);
+
+	$upgrades[] = new SQLUpgrade(
+			'project_child_index',
+			'Add index on child_id in project heirarchy table',
+			"ALTER TABLE $t_project_hierarchy_table ADD INDEX ( child_id )"
+		);
+
+	$upgrades[] = new SQLUpgrade(
+			'bug_status_index',
+			'Add index on status in bug table',
+			"ALTER TABLE $t_bug_table ADD INDEX ( status )"
+		);
+
+	$upgrades[] = new SQLUpgrade(
+			'bug_project_index',
+			'Add index on project_id in bug table',
+			"ALTER TABLE $t_bug_table ADD INDEX ( project_id )"
+		);
+
+	$upgrades[] = new SQLUpgrade(
+			'note_updated_index',
+			'Add index on last_modified in bugnotes table',
+			"ALTER TABLE $t_bugnote_table ADD INDEX ( last_modified )"
+		);
+
+	$upgrades[] = new SQLUpgrade(
+			'project_viewstate_index',
+			'Add index on view_state in project table',
+			"ALTER TABLE $t_project_table ADD INDEX ( view_state )"
+		);
+
+	$upgrades[] = new SQLUpgrade(
+			'project_user_id_index',
+			'Add index on user_id in project_user table',
+			"ALTER TABLE $t_project_user_list_table ADD INDEX ( user_id )"
+		);
+
+	$upgrades[] = new SQLUpgrade(
+			'user_enabled_index',
+			'Add index on enabled in user table',
+			"ALTER TABLE $t_user_table ADD INDEX ( enabled )"
+		);
+
+	$upgrades[] = new SQLUpgrade(
+			'user_access_index',
+			'Add index on access_level in user table',
+			"ALTER TABLE $t_user_table ADD INDEX ( access_level )"
 		);
 
 	return $upgrades;
