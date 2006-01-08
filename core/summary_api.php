3a6364feaccd8ecca209a8b7aeeff90f831dbb87
Glenn Henshaw <thraxisp@logicaloutcome.ca>
2006-01-08 14:46:13 +0000

    fixed un-initialized variables
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@3953 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- core/summary_api.php
+++ core/summary_api.php
@@ -6,7 +6,7 @@
 	# See the README and LICENSE files for details
 
 	# --------------------------------------------------------
-	# $Id: summary_api.php,v 1.43 2005-11-21 17:26:39 prichards Exp $
+	# $Id: summary_api.php,v 1.44 2006-01-08 14:46:13 thraxisp Exp $
 	# --------------------------------------------------------
 
 	### Summary printing API ###
@@ -511,11 +511,23 @@
 			while ( $row = db_fetch_array( $result ) ) {
 				extract( $row, EXTR_PREFIX_ALL, 'v' );
                 if ( $t_closed_val <= $v_status ) {
-                    $p_cache[ $v_project_id ][ 'closed'   ]  += $v_count;
+                    if ( isset( $p_cache[ $v_project_id ][ 'closed'   ] ) ) {
+                        $p_cache[ $v_project_id ][ 'closed'   ]  += $v_count;
+                    } else {
+                        $p_cache[ $v_project_id ][ 'closed'   ]  = $v_count;
+                    }
                 } else if ( $t_resolved_val <= $v_status ) {
-                    $p_cache[ $v_project_id ][ 'resolved' ]  += $v_count;
+                    if ( isset( $p_cache[ $v_project_id ][ 'resolved' ] ) ) {
+                        $p_cache[ $v_project_id ][ 'resolved' ]  += $v_count;
+                    } else {
+                        $p_cache[ $v_project_id ][ 'resolved' ]  = $v_count;
+                    }
                 } else {
-                    $p_cache[ $v_project_id ][ 'open'     ]  += $v_count;
+                    if ( isset( $p_cache[ $v_project_id ][ 'open'     ] ) ) {
+                        $p_cache[ $v_project_id ][ 'open'     ]  += $v_count;
+                    } else {
+                        $p_cache[ $v_project_id ][ 'open'     ]  = $v_count;
+                    }
                 }
 			}
 		}
