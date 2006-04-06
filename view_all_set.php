7df5ef5fce0bf72e34ce5561b0de734184a2a418
Glenn Henshaw <thraxisp@logicaloutcome.ca>
2006-04-06 00:28:52 +0000

    fix for #0006902: XSS in mantis bug track system ....
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/branches/BRANCH_1_0_0@4022 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- view_all_set.php
+++ view_all_set.php
@@ -6,7 +6,7 @@
 	# See the README and LICENSE files for details
 
 	# --------------------------------------------------------
-	# $Id: view_all_set.php,v 1.57.4.1.2.1 2005-12-18 14:22:02 thraxisp Exp $
+	# $Id: view_all_set.php,v 1.57.4.1.2.1.4.1 2006-04-06 00:28:52 thraxisp Exp $
 	# --------------------------------------------------------
 ?>
 <?php require_once( 'core.php' ) ?>
@@ -152,12 +152,12 @@
 	$f_dir_1				= gpc_get_string( 'dir_1', '' );
 
 	# date values
-	$f_start_month			= gpc_get_string( 'start_month', date( 'm' ) );
-	$f_end_month			= gpc_get_string( 'end_month', date( 'm' ) );
-	$f_start_day			= gpc_get_string( 'start_day', 1 );
-	$f_end_day				= gpc_get_string( 'end_day', date( 'd' ) );
-	$f_start_year			= gpc_get_string( 'start_year', date( 'Y' ) );
-	$f_end_year				= gpc_get_string( 'end_year', date( 'Y' ) );
+	$f_start_month			= gpc_get_int( 'start_month', date( 'm' ) );
+	$f_end_month			= gpc_get_int( 'end_month', date( 'm' ) );
+	$f_start_day			= gpc_get_int( 'start_day', 1 );
+	$f_end_day				= gpc_get_int( 'end_day', date( 'd' ) );
+	$f_start_year			= gpc_get_int( 'start_year', date( 'Y' ) );
+	$f_end_year				= gpc_get_int( 'end_year', date( 'Y' ) );
 	$f_search				= gpc_get_string( 'search', '' );
 	$f_and_not_assigned		= gpc_get_bool( 'and_not_assigned' );
 	$f_do_filter_by_date	= gpc_get_bool( 'do_filter_by_date' );
