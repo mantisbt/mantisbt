95c4f03dc928d7bf2d27e8f620b20e1fdc62ff77
Victor Boctor <vboctor@gmail.com>
2006-05-07 06:12:23 +0000

    Preparing for releasing 1.0.3.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/branches/BRANCH_1_0_0@4088 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- config_defaults_inc.php
+++ config_defaults_inc.php
@@ -6,7 +6,7 @@
 	# See the README and LICENSE files for details
 
 	# --------------------------------------------------------
-	# $Id: config_defaults_inc.php,v 1.283.2.1.2.1.2.1.2.2.2.3 2006-05-05 15:48:30 vboctor Exp $
+	# $Id: config_defaults_inc.php,v 1.283.2.1.2.1.2.1.2.2.2.4 2006-05-07 06:12:22 vboctor Exp $
 	# --------------------------------------------------------
 
 
@@ -312,7 +312,7 @@
 	#############################
 
 	# --- version variables -----------
-	$g_mantis_version		= '1.0.3-CVS';
+	$g_mantis_version		= '1.0.3';
 	$g_show_version			= ON;
 
 	################################
