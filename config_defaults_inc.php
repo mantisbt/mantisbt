4837d9b396855173629291e98bbe2cf1c90d0419
Victor Boctor <vboctor@gmail.com>
2006-09-09 05:33:38 +0000

    Update Mantis 1.1 to refer to mantisbugtracker.com rather than mantisbt.org.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@4144 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- config_defaults_inc.php
+++ config_defaults_inc.php
@@ -6,7 +6,7 @@
 	# See the README and LICENSE files for details
 
 	# --------------------------------------------------------
-	# $Id: config_defaults_inc.php,v 1.308 2006-08-14 08:32:57 vboctor Exp $
+	# $Id: config_defaults_inc.php,v 1.309 2006-09-09 05:33:38 vboctor Exp $
 	# --------------------------------------------------------
 
 
@@ -23,7 +23,7 @@
 	# In general a value of OFF means the feature is disabled and ON means the
 	# feature is enabled.  Any other cases will have an explanation.
 
-	# For more details see http://manual.mantisbt.org/
+	# For more details see http://manual.mantisbugtracker.com/
 
 	################################
 	# Mantis Database Settings
@@ -103,7 +103,7 @@
 	$g_core_path			= $g_absolute_path . 'core' . DIRECTORY_SEPARATOR;
 
 	# Used to link to manual for User Documentation.
-	$g_manual_url = 'http://manual.mantisbt.org/';
+	$g_manual_url = 'http://manual.mantisbugtracker.com/';
 
 	#############################
 	# Web Server
