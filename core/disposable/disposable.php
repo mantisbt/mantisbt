c22590f76623f05ad05a8580c0bb99cebce6649f
zakman <zakman@users.sourceforge.net>
2007-08-01 23:10:09 +0000

    Fixed Bug #:0008138: Add more services to disposable email address checker
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@4523 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- core/disposable/disposable.php
+++ core/disposable/disposable.php
@@ -8,10 +8,10 @@
 	# This program is distributed under the terms and conditions of the LGPL
 	# See the README and LICENSE files for details
 	#
-	# Version 1.0.0 - Release Date: 16-Jun-2007 
+	# Version 1.0.1 - Release Date: 31-July-2007 
 
 	# --------------------------------------------------------
-	# $Id: disposable.php,v 1.1 2007-06-16 23:04:35 vboctor Exp $
+	# $Id: disposable.php,v 1.2 2007-08-01 23:10:09 zakman Exp $
 	# --------------------------------------------------------
 
 /**
@@ -58,6 +58,34 @@ class DisposableEmailChecker
 			case 'spamhole.com':
 			case 'spammotel.com':
 			case 'trashmail.net':
+			case 'despammed.com':
+			case 'e4ward.com':
+			case 'emailias.com':
+			case 'hidemail.de':
+			case 'kasmail.com':
+			case 'mailfreeonline.com':
+			case 'mailmoat.com':
+			case 'mailnull.com':
+			case 'mailshell.com':
+			case 'mailzilla.com':
+			case 'mintemail.com':
+			case 'netzidiot.de':
+			case 'punkass.com':
+			case 'safersignup.de':
+			case 'sneakemail.com':
+			case 'spamslicer.com':
+			case 'spamtrail.com':
+			case 'temporaryforwarding.com':
+			case 'xemaps.com':
+			case 'xmaily.com':
+			case 'fakemailz.com':
+			case 'shiftmail.com':
+			case '1chuan.com': 
+			case '1zhuan.com': 
+			case '4warding.com': 
+			case '4warding.net': 
+			case '4warding.org': 
+			case 'imstations.com': 
 				return true;
 		}
 
@@ -89,6 +117,105 @@ class DisposableEmailChecker
 			case 'spambog.com':
 			case 'spam.la':
 			case 'tempinbox.com':
+			case 'discardmail.com':
+			case 'dodgeit.com':
+			case 'dontsendmespam.de':
+			case 'emaildienst.de':
+			case 'getonemail.com':
+			case 'haltospam.com':
+			case 'ipoo.org':
+			case 'killmail.net':
+			case 'link2mail.net':
+			case 'lortemail.dk':
+			case 'maileater.com':
+			case 'mytrashmail.com':
+			case 'nobulk.com':
+			case 'noclickemail.com':
+			case 'nospamfor.us':
+			case 'oneoffemail.com':
+			case 'oopi.org':
+			case 'pookmail.com':
+			case 'rejectmail.com':
+			case 'shortmail.net':
+			case 'sofort-mail.de':
+			case 'spamavert.com':
+			case 'spamfree24.com':
+			case 'spamfree24.org':
+			case 'spamfree24.net':
+			case 'spaml.com':
+			case 'tempemail.net':
+			case 'temporaryinbox.com':
+			case 'trashmail.net':
+			case 'trash-mail.de':
+			case 'twinmail.de':
+			case 'willselfdestruct.com':
+			case 'yopmail.com':
+			case 'mailinator.com':
+			case 'mailinator2.com': 
+			case 'sogetthis.com': 
+			case 'mailin8r.com': 
+			case 'mailinator.net': 
+			case 'klassmaster.com':
+			case 'fastacura.com':
+			case 'fastchevy.com':
+			case 'fastchrysler.com': 
+			case 'fastkawasaki.com':
+			case 'fastmazda.com':
+			case 'fastmitsubishi.com': 
+			case 'fastnissan.com':
+			case 'fastsubaru.com':
+			case 'fastsuzuki.com':
+			case 'fasttoyota.com':
+			case 'fastyamaha.com':
+			case '675hosting.com': 
+			case '675hosting.net': 
+			case '675hosting.org': 
+			case '75hosting.com': 
+			case '75hosting.net': 
+			case '75hosting.org': 
+			case 'ajaxapp.net': 
+			case 'amiri.net': 
+			case 'amiriindustries.com': 
+			case 'emailmiser.com': 
+			case 'etranquil.com': 
+			case 'etranquil.net': 
+			case 'etranquil.org': 
+			case 'gowikibooks.com': 
+			case 'gowikicampus.com': 
+			case 'gowikicars.com': 
+			case 'gowikifilms.com': 
+			case 'gowikigames.com': 
+			case 'gowikimusic.com': 
+			case 'gowikinetwork.com': 
+			case 'gowikitravel.com': 
+			case 'gowikitv.com':
+			case 'myspaceinc.com': 
+			case 'myspaceinc.net': 
+			case 'myspaceinc.org': 
+			case 'myspacepimpedup.com': 
+			case 'ourklips.com': 
+			case 'pimpedupmyspace.com': 
+			case 'rklips.com': 
+			case 'turual.com': 
+			case 'upliftnow.com': 
+			case 'uplipht.com': 
+			case 'viditag.com': 
+			case 'viewcastmedia.com': 
+			case 'viewcastmedia.net': 
+			case 'viewcastmedia.org': 
+			case 'wetrainbayarea.com': 
+			case 'wetrainbayarea.org': 
+			case 'xagloo.com': 
+			case 'buyusedlibrarybooks.org':
+			case 'mailquack.com':
+			case 'mailslapping.com':
+			case 'oneoffmail.com':
+			case 'recyclemail.dk':
+			case 'anonymail.dk':
+			case 'trashdevil.com':
+			case 'trashdevil.de':
+			case 'whopy.com':
+			case 'wilemail.com':
 				return true;
 		}
 
@@ -132,6 +259,43 @@ class DisposableEmailChecker
 			case 'no-spam.hu':
 			case 'spamhole.com':
 			case 'trashmail.net':
+			case 'despam.it':
+			case 'dontreg.com':
+			case 'emailto.de':
+			case 'getonemail.com':
+			case 'guerrillamail.com':
+			case 'guerrillamail.net':
+			case 'haltospam.com':
+			case 'jetable.com':
+			case 'jetable.net':
+			case 'jetable.org':
+			case 'kasmail.com':
+			case 'link2mail.net':
+			case 'mailexpire.com':
+			case 'mailzilla.com':
+			case 'mintemail.com':
+			case 'noclickemail.com':
+			case 'oneoffemail.com':
+			case 'oopi.org':
+			case 'pookmail.com':
+			case 'shortmail.net':
+			case 'spambox.us':
+			case 'spamfree24.com':
+			case 'spamfree24.org':
+			case 'spamfree24.net':
+			case 'spamify.com':
+			case 'tempemail.net':
+			case 'tempinbox.com':
+			case 'temporarily.de':
+			case 'temporaryinbox.com':
+			case 'wh4f.org':
+			case 'yopmail.com':
+			case 'buyusedlibrarybooks.org':
+			case 'dotmsg.com':
+			case 'lovemeleaveme.com':
+			case 'trashdevil.com':
+			case 'trashdevil.de':
+			case 'walala.org':
 				return true;
 		}
 
@@ -153,11 +317,75 @@ class DisposableEmailChecker
 	function is_free_email( $p_email ) {
 		$t_domain = DisposableEmailChecker::_get_domain_from_address( $p_email );
 
-		# @@@ use regular expressions to handle potentially yahoo.com, yahoo.com.au, etc.
 		switch ( $t_domain ) {
 			case 'gmail.com':
+			case 'googlemail.com':
 			case 'hotmail.com':
 			case 'yahoo.com':
+			case 'msn.com':
+			case 'hotmail.de':
+			case 'hotmail.fr':
+			case 'hotmail.it':
+			case 'hotmail.co.uk':
+			case 'msn.co.uk':
+			case 'pancakemail.com':
+			case 'gawab.com':
+			case 'yahoo.com.au':
+			case 'yahoo.com.cn':
+			case 'yahoo.co.uk':
+			case 'yahoo.com.hk':
+			case 'yahoo.com.ar':
+			case 'yahoo.com.br':
+			case 'yahoo.com.mx':
+			case 'yahoo.com.asia':
+			case 'yahoo.co.jp':
+			case 'yahoo.com.malaysia':
+			case 'yahoo.com.ph':
+			case 'yahoo.com.sg':
+			case 'yahoo.com.tw':
+			case 'yahoo.com.vn':
+			case 'yahoo.com.es':
+			case 'yahoo.fr':
+			case 'yahoo.ie':
+			case 'yahoo.de':
+			case 'yahoo.ca':
+			case 'talk21.com':
+			case 'BTinternet.com':
+			case 'lycos.co.uk':
+			case 'lycos.it':
+			case 'lycos.es':
+			case 'lycos.de':
+			case 'lycos.at':
+			case 'lycos.nl':
+			case 'caramail.com':
+			case 'mail.com':
+			case 'libero.it':
+			case 'iol.it':
+			case 'blu.it':
+			case 'aol.com':
+			case 'aim.com':
+			case 'netscape.com':
+			case 'netscape.net':
+			case 'mail.ru':
+			case 'inbox.ru':
+			case 'bk.ru':
+			case 'list.ru':
+			case 'rediffmail.com':
+			case 'hanmail.net':
+			case 'webmail.co.za':
+			case 'exclusivemail.co.za':
+			case 'executive.co.za':
+			case 'homemail.co.za':
+			case 'magicmail.co.za':
+			case 'mailbox.co.za':
+			case 'ravemail.co.za':
+			case 'starmail.co.za':
+			case 'thecricket.co.za':
+			case 'thegolf.co.za':
+			case 'thepub.co.za':
+			case 'therugby.co.za':
+			case 'websurfer.co.za':
+			case 'workmail.co.za':
 				return true;
 		}
 
