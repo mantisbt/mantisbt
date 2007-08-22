0d0c2a266d1e68c1bed0fc0e73003a94e63df9d4
Victor Boctor <vboctor@gmail.com>
2007-08-22 05:44:03 +0000

    Fixed #8237: Error in dependency graphs.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@4555 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- bug_relationship_graph.php
+++ bug_relationship_graph.php
@@ -1,12 +1,12 @@
 <?php
 	# Mantis - a php based bugtracking system
 	# Copyright (C) 2000 - 2002  Kenzaburo Ito - kenito@300baud.org
-	# Copyright (C) 2002 - 2004  Mantis Team   - mantisbt-dev@lists.sourceforge.net
+	# Copyright (C) 2002 - 2007  Mantis Team   - mantisbt-dev@lists.sourceforge.net
 	# This program is distributed under the terms and conditions of the GPL
 	# See the README and LICENSE files for details
 
 	# --------------------------------------------------------
-	# $Id: bug_relationship_graph.php,v 1.5 2007-07-15 22:16:59 prichards Exp $
+	# $Id: bug_relationship_graph.php,v 1.6 2007-08-22 05:44:03 vboctor Exp $
 	# --------------------------------------------------------
 
 	require_once( 'core.php' );
@@ -72,7 +72,7 @@
 		if ( $t_graph_relation ) 
 			echo lang_get( 'viewing_bug_relationship_graph_title' );
 		else
-			echo lang_get( 'viewing_bug_dependancy_graph_title' );
+			echo lang_get( 'viewing_bug_dependency_graph_title' );
 		?>
 	</td>
 	<!-- Links -->
