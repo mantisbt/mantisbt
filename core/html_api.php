bfce5230c81a92940c3db880d43c778b943ca11f
Victor Boctor <vboctor@gmail.com>
2007-09-20 07:18:27 +0000

    Fixed #5093: Rights in custom menu does not work.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@4579 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- core/html_api.php
+++ core/html_api.php
@@ -6,7 +6,7 @@
 	# See the README and LICENSE files for details
 
 	# --------------------------------------------------------
-	# $Id: html_api.php,v 1.214 2007-09-08 23:21:00 prichards Exp $
+	# $Id: html_api.php,v 1.215 2007-09-20 07:18:27 vboctor Exp $
 	# --------------------------------------------------------
 
 	###########################################################################
@@ -476,7 +476,7 @@
 
 		foreach( $t_custom_menu_options as $t_custom_option ) {
 			$t_access_level = $t_custom_option[1];
-			if ( access_has_global_level( $t_access_level ) ) {
+			if ( access_has_project_level( $t_access_level ) ) {
 				$t_caption = lang_get_defaulted( $t_custom_option[0] );
 				$t_link = $t_custom_option[2];
 				$t_options[] = "<a href=\"$t_link\">$t_caption</a>";
