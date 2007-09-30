32d16c42cc33679d06e6ceea9ccf98a8317b7dbb
Victor Boctor <vboctor@gmail.com>
2007-09-30 02:55:32 +0000

    Clearup of bug_delete.php and removed an extra bug_get() call.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@4607 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- bug_delete.php
+++ bug_delete.php
@@ -1,39 +1,33 @@
 <?php
 	# Mantis - a php based bugtracking system
 	# Copyright (C) 2000 - 2002  Kenzaburo Ito - kenito@300baud.org
-	# Copyright (C) 2002 - 2004  Mantis Team   - mantisbt-dev@lists.sourceforge.net
+	# Copyright (C) 2002 - 2007  Mantis Team   - mantisbt-dev@lists.sourceforge.net
 	# This program is distributed under the terms and conditions of the GPL
 	# See the README and LICENSE files for details
 
 	# --------------------------------------------------------
-	# $Id: bug_delete.php,v 1.41 2007-02-20 05:24:14 vboctor Exp $
+	# $Id: bug_delete.php,v 1.42 2007-09-30 02:55:32 vboctor Exp $
 	# --------------------------------------------------------
-?>
-<?php
-	# Deletes the bug and re-directs to view_all_bug_page.php
-?>
-<?php
+
 	require_once( 'core.php' );
 
 	$t_core_path = config_get( 'core_path' );
 
 	require_once( $t_core_path.'bug_api.php' );
-?>
-<?php
+
 	$f_bug_id = gpc_get_int( 'bug_id' );
 
 	access_ensure_bug_level( config_get( 'delete_bug_threshold' ), $f_bug_id );
 
 	$t_bug = bug_get( $f_bug_id, true );
-	if( $t_bug->project_id != helper_get_current_project() ) {
+
+	if ( $t_bug->project_id != helper_get_current_project() ) {
 		# in case the current project is not the same project of the bug we are viewing...
 		# ... override the current project. This to avoid problems with categories and handlers lists etc.
 		$g_project_override = $t_bug->project_id;
 	}
 
 	helper_ensure_confirmed( lang_get( 'delete_bug_sure_msg' ), lang_get( 'delete_bug_button' ) );
-
-	$t_bug = bug_get( $f_bug_id, true );
 
 	bug_delete( $f_bug_id );
 
