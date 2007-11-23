7068eeb0d95e5df2e91e3aa49cb57d3353393a68
Victor Boctor <vboctor@gmail.com>
2007-11-23 19:13:49 +0000

    Fixed #8519: Name collision between NuSoap SoapClient and PHP 5 Soap extension.  Done by upgrading to nusoap 0.7.3.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/branches/BRANCH_1_1_0@4780 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- api/soap/nusoap/class.soap_val.php
+++ api/soap/nusoap/class.soap_val.php
@@ -11,7 +11,7 @@
 * xsd:anyType and user-defined types.
 *
 * @author   Dietrich Ayala <dietrich@ganx4.com>
-* @version  $Id: class.soap_val.php,v 1.1 2007-07-18 06:53:06 vboctor Exp $
+* @version  $Id: class.soap_val.php,v 1.11 2007/04/06 13:56:32 snichol Exp $
 * @access   public
 */
 class soapval extends nusoap_base {
@@ -87,7 +87,7 @@ class soapval extends nusoap_base {
 	* @access   public
 	*/
 	function serialize($use='encoded') {
-		return $this->serialize_val($this->value,$this->name,$this->type,$this->element_ns,$this->type_ns,$this->attributes,$use);
+		return $this->serialize_val($this->value, $this->name, $this->type, $this->element_ns, $this->type_ns, $this->attributes, $use, true);
     }
 
 	/**
