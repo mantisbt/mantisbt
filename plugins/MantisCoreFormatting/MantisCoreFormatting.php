106e45638dbbb97a43068e814aa8490953e76c10
Victor Boctor <vboctor@gmail.com>
2008-02-16 01:46:45 +0000

    Added missing copyright header.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@5010 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- plugins/MantisCoreFormatting/MantisCoreFormatting.php
+++ plugins/MantisCoreFormatting/MantisCoreFormatting.php
@@ -1,9 +1,28 @@
 <?php
+# Mantis - a php based bugtracking system
+
+# Copyright (C) 2000 - 2002  Kenzaburo Ito - kenito@300baud.org
+# Copyright (C) 2002 - 2007  Mantis Team   - mantisbt-dev@lists.sourceforge.net
+
+# Mantis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Mantis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mantis.  If not, see <http://www.gnu.org/licenses/>.
 
 require_once( config_get( 'class_path' ) . 'MantisFormattingPlugin.class.php' );
 
 class MantisCoreFormattingPlugin extends MantisFormattingPlugin {
-
+	/**
+	 *  A method that populates the plugin information and minimum requirements.
+	 */
 	function register() {
 		$this->name			= lang_get( 'plugin_format_title' );
 		$this->description	= lang_get( 'plugin_format_description' );
