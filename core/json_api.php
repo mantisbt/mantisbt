2572b28e7e1033b12533ac6d78eb3057fd1d4438
John Reese <jreese@leetcode.net>
2008-04-15 17:37:07 +0000

    Added a JSON manipulation library for use with external service plugins (like GitHub integration).
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@5164 f5dc347c-c33d-0410-90a0-b07cc1902cb9

---
+++ core/json_api.php
@@ -0,0 +1,40 @@
+<?php
+# Mantis - a php based bugtracking system
+
+# Copyright (C) 2000 - 2002  Kenzaburo Ito - kenito@300baud.org
+# Copyright (C) 2002 - 2007  Mantis Team   - mantisbt-dev@lists.sourceforge.net
+
+# Mantis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Mantis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mantis.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * API for simplifying some JSON interactions.
+ */
+
+/**
+ * Get a chunk of JSON from a given URL.
+ * @param string URL
+ * @param string Top-level member to retrieve
+ * @return multi JSON class structure
+ */
+function json_url( $p_url, $p_member=null ) {
+	$t_data = file_get_contents( $p_url );
+	$t_json = json_decode( $t_data );
+
+	if ( is_null( $p_member ) ) {
+		return $t_json;
+	} else {
+		return $t_json->$p_member;
+	}
+}
+
