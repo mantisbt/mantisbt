918333723e641aa65bfab4c2ed491991e2a1c6c9
Paul Richards <paul@mantisforge.org>
2008-04-27 21:12:11 +0000

    being pedantic
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@5212 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- bug_monitor_list_view_inc.php
+++ bug_monitor_list_view_inc.php
@@ -20,36 +20,32 @@
 	# --------------------------------------------------------
 	# $Id$
 	# --------------------------------------------------------
-?>
-<?php
+
 	# This include file prints out the list of users monitoring the current
 	# bug.	$f_bug_id must be set and be set to the bug id
-?>
-<?php	if ( access_has_bug_level( config_get( 'show_monitor_list_threshold' ), $f_bug_id ) ) { ?>
-<?php
-	$c_bug_id = db_prepare_int( $f_bug_id );
-	$t_bug_monitor_table = db_get_table( 'mantis_bug_monitor_table' );
-	$t_user_table = db_get_table( 'mantis_user_table' );
 
-	# get the bugnote data
-	$query = "SELECT user_id, enabled
-			FROM $t_bug_monitor_table m, $t_user_table u
-			WHERE m.bug_id=" . db_param(0) . " AND m.user_id = u.id
-			ORDER BY u.realname, u.username";
-	$result = db_query_bound($query, Array( $c_bug_id ) );
-	$num_users = db_num_rows($result);
+	if ( access_has_bug_level( config_get( 'show_monitor_list_threshold' ), $f_bug_id ) ) { 
+		$c_bug_id = db_prepare_int( $f_bug_id );
+		$t_bug_monitor_table = db_get_table( 'mantis_bug_monitor_table' );
+		$t_user_table = db_get_table( 'mantis_user_table' );
 
-	$t_users = array();
-	for ( $i = 0; $i < $num_users; $i++ ) {
-		$row = db_fetch_array( $result );
-		$t_users[$i] = $row['user_id'];
+		# get the bugnote data
+		$query = "SELECT user_id, enabled
+				FROM $t_bug_monitor_table m, $t_user_table u
+				WHERE m.bug_id=" . db_param(0) . " AND m.user_id = u.id
+				ORDER BY u.realname, u.username";
+		$result = db_query_bound($query, Array( $c_bug_id ) );
+		$num_users = db_num_rows($result);
+
+		$t_users = array();
+		for ( $i = 0; $i < $num_users; $i++ ) {
+			$row = db_fetch_array( $result );
+			$t_users[$i] = $row['user_id'];
 	}
 	user_cache_array_rows( $t_users );
 
 	echo '<a name="monitors" id="monitors" /><br />';
-?>
 
-<?php
 	collapse_open( 'monitoring' );
 ?>
 <table class="width100" cellspacing="1">
