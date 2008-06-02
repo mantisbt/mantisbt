7931e1dc65a2068d535f0c84b61b92dc53bf626e
John Reese <jreese@leetcode.net>
2008-06-02 14:49:35 +0000

    Enhance Form API to work for both POST forms and GET urls.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@5308 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- bug_update_page.php
+++ bug_update_page.php
@@ -65,7 +65,7 @@
 
 <br />
 <form name="update_bug_form" method="post" action="bug_update.php">
-<?php echo form_security_token( 'bug_update' ) ?>
+<?php echo form_security_field( 'bug_update' ) ?>
 <table class="width100" cellspacing="1">
 
 
