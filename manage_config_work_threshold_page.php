fef533f9f85c2b44d47e0e1f72d26a3993cc8bbf
John Reese <jreese@leetcode.net>
2008-07-30 19:16:16 +0000

    Fix part of #9437: Added form security token.
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@5468 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- manage_config_work_threshold_page.php
+++ manage_config_work_threshold_page.php
@@ -322,6 +322,7 @@
 	
 	if ( $t_show_submit && ( 0 < count( $t_overrides ) ) ) {
         echo "<div class=\"right\"><form name=\"threshold_config_action\" method=\"post\" action=\"manage_config_revert.php\">\n";
+		echo form_security_field( 'manage_config_revert' );
         echo "<input name=\"revert\" type=\"hidden\" value=\"" . implode( ',', $t_overrides ) . "\"></input>";
         echo "<input name=\"project\" type=\"hidden\" value=\"$t_project_id\"></input>";
         echo "<input name=\"return\" type=\"hidden\" value=\"" . string_sanitize_url( $_SERVER['PHP_SELF'] ) ."\"></input>";
