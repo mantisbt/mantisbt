f8bdae9ffacf06124e52c6a0c7dfab179f988d57
Gianluca Sforna <giallu@gmail.com>
2008-08-23 22:33:41 +0000

    Fix main menu link (wasn't really broken...)
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@5525 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- core/html_api.php
+++ core/html_api.php
@@ -570,7 +570,7 @@
 				$t_menu_options = array();
 
 				# Main Page
-				$t_menu_options[] = '<a href="' . helper_mantis_url( 'main_page.php">' ) . lang_get( 'main_link' ) . '</a>';
+				$t_menu_options[] = '<a href="' . helper_mantis_url( 'main_page.php' ) . '">' . lang_get( 'main_link' ) . '</a>';
 
 				# Plugin / Event added options
 				$t_event_menu_options = event_signal( 'EVENT_MENU_MAIN_FRONT' );
