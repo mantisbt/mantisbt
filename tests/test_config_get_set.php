fe05ab1bc9da855516bfed3aaf6573c9d0114330
Glenn Henshaw <thraxisp@logicaloutcome.ca>
2008-10-10 19:40:26 +0000

    submit unit tests
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/trunk@5653 f5dc347c-c33d-0410-90a0-b07cc1902cb9

---
+++ tests/test_config_get_set.php
@@ -0,0 +1,67 @@
+<?php
+# Mantis - a php based bugtracking system
+
+# Mantis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Mantis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mantis.  If not, see <http://www.gnu.org/licenses/>.
+
+	/**
+	 * @package MantisBT
+	 * @version $Id$
+	 * @copyright Copyright (C) 2002 - 2008  Mantis Team   - mantisbt-dev@lists.sourceforge.net
+	 * @link http://www.mantisbt.org
+	 */
+	 /**
+	  * Mantis Unit Test
+	  */
+	 /**
+	  * test config_get and config_set 
+	  *
+	  * run from command line using: php -q test_config_get_set.php 
+	  *  inspect results manually
+	  * Notes:
+	  *  data set from the test may need to be removed from the database manually
+	  */
+require_once( 'core.php' );
+
+$t_config = 'main_menu_custom_options';
+$t_test = config_get( $t_config );
+print_r( $t_config);
+print_r( $t_test );
+$t_test[0][1] = 20;
+config_set( $t_config, $t_test );
+$t_test = config_get( $t_config );
+print_r( $t_test );
+
+$t_config = 'default_home_page';
+$t_test = config_get( $t_config );
+print_r( $t_config);
+print_r( $t_test );
+$t_test .= '?test';
+config_set( $t_config, $t_test );
+$t_test = config_get( $t_config );
+print_r( $t_test );
+
+$g_test_config = array();
+$t_config = 'test_config';
+$t_test = config_get( $t_config );
+print_r( $t_config);
+print_r( $t_test );
+echo " ".(isset($t_test[0])?"set":"not set")." ".count($t_test)." ";
+$t_test[0] = 20;
+config_set( $t_config, $t_test );
+$t_test = config_get( $t_config );
+print_r( $t_test );
+echo " ".(isset($t_test[0])?"set":"not set")." ".count($t_test)." ";
+
+
+?>
---
+++ tests/test_string_sanitize_url.php
@@ -0,0 +1,48 @@
+<?php
+# Mantis - a php based bugtracking system
+
+# Mantis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Mantis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mantis.  If not, see <http://www.gnu.org/licenses/>.
+
+	/**
+	 * @package MantisBT
+	 * @version $Id$
+	 * @copyright Copyright (C) 2002 - 2008  Mantis Team   - mantisbt-dev@lists.sourceforge.net
+	 * @link http://www.mantisbt.org
+	 */
+	 /**
+	  * Mantis Unit Test
+	  */
+	 /**
+	  * test string_sanitize_url 
+	  *
+	  * run from command line using: php -q test_string_sanitize_url.php 
+	  *  inspect results manually
+	  */
+require_once( 'core.php' );
+
+$t_test = array(
+    '',
+    'abc.php',
+    'abc.php#a',
+    'abc.php?abc=def',
+    'abc.php?abc=def#a',
+    'abc.php?abc=def&z=xyz',
+    'abc.php?abc=def&z=xyz#a',
+    'abc.php?abc=def&z=<script>alert("foo")</script>z#a',
+    'abc.php?abc=def&z=z#<script>alert("foo")</script>a'
+    );
+    
+foreach($t_test as $t_url) {
+    echo $t_url . ' => ' . string_sanitize_url($t_url, false) . "\n";
+}
