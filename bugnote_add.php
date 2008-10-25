3316d3f8b59f11c3c1ea251895da5386269cb79e
Victor Boctor <vboctor@gmail.com>
2008-10-25 19:20:39 +0000

    Fixed #9748: Port 9737: bugnote_add.php contains undefined t_note_type
    git-svn-id: http://mantisbt.svn.sourceforge.net/svnroot/mantisbt/branches/BRANCH_1_1_0@5734 f5dc347c-c33d-0410-90a0-b07cc1902cb9

--- bugnote_add.php
+++ bugnote_add.php
@@ -51,8 +51,9 @@
 		$g_project_override = $t_bug->project_id;
 	}
 
-	# @@@ VB: Do we want to differentiate email notifications for normal notes from time tracking entries?
-	$t_bugnote_added = bugnote_add( $f_bug_id, $f_bugnote_text, $f_time_tracking, $f_private, $t_note_type );
+	// We always set the note time to BUGNOTE, and the API will overwrite it with TIME_TRACKING
+	// if $f_time_tracking is not 0 and the time tracking feature is enabled.
+	$t_bugnote_added = bugnote_add( $f_bug_id, $f_bugnote_text, $f_time_tracking, $f_private, BUGNOTE );
 	if ( !$t_bugnote_added ) {
 		error_parameters( lang_get( 'bugnote' ) );
 		trigger_error( ERROR_EMPTY_FIELD, ERROR );
