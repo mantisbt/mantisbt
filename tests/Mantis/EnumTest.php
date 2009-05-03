d4f4e2136a522f457f2c1890458c0d035e18e7f6
Gianluca Sforna <giallu@gmail.com>
2009-05-03 17:15:15 +0200

    New stuff is not copyright kenito

--- tests/Mantis/EnumTest.php
+++ tests/Mantis/EnumTest.php
@@ -17,7 +17,6 @@
 /**
  * @package Tests
  * @subpackage UnitTests
- * @copyright Copyright (C) 2000 - 2002  Kenzaburo Ito - kenito@300baud.org
  * @copyright Copyright (C) 2002 - 2009  MantisBT Team - mantisbt-dev@lists.sourceforge.net
  * @link http://www.mantisbt.org
  */
