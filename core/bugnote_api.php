6412a921eccf0d3ebe8bb8f27b3eac92bde62bfd
Paul Richards <paul@mantisforge.org>
2009-05-04 01:14:41 +0100

    typo

--- core/bugnote_api.php
+++ core/bugnote_api.php
@@ -628,7 +628,7 @@ function bugnote_stats_get_project_array( $p_project_id, $p_from, $p_to, $p_cost
 	$c_to = strtotime( $p_to, SECONDS_PER_DAY - 1); // @23:59:59
 	$c_from = strtotime( $p_from );
 
-	if ( $c_to === false || $c_form === false ) {
+	if ( $c_to === false || $c_from === false ) {
 		error_parameters( array( $p_form, $p_to ) );
 		trigger_error( ERROR_GENERIC, ERROR );
 	}
