d6cffa48fcaa116a2f85d51c3f077ff1d78abf2c
John Reese <jreese@leetcode.net>
2009-06-08 15:54:17 -0400

    Updating for 1.1.8 release.

--- core/constant_inc.php
+++ core/constant_inc.php
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Mantis.  If not, see <http://www.gnu.org/licenses/>.
 
-	define( 'MANTIS_VERSION', '1.1.7' );
+	define( 'MANTIS_VERSION', '1.1.8' );
 
 	# --- constants -------------------
 
