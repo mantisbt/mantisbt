mantisbt/mantisbt

commit eba305694fcf9191bad01e5d5d4065cfb04914a0

Adjust how we cache loaded bugnotes Adjust how we generate relationship text in emails

core/bugnote_api.php
@@ -288,6 +288,12 @@ function bugnote_get_text( $p_bugnote_id ) {
  * @access public
  */
 function bugnote_get_field( $p_bugnote_id, $p_field_name ) {
+	global $g_cache_bugnote;
+	
+	if( isset( $g_cache_bugnote[(int)$p_bugnote_id] ) ) {
+		return $g_cache_bugnote[(int)$p_bugnote_id]->$p_field_name;
+	}
+
 	$c_bugnote_id = db_prepare_int( $p_bugnote_id );
 	$c_field_name = db_prepare_string( $p_field_name );
 	$t_bugnote_table = db_get_table( 'mantis_bugnote_table' );
@@ -385,14 +391,18 @@ function bugnote_get_all_visible_bugnotes( $p_bug_id, $p_user_bugnote_order, $p_
  * @access public
  */
 function bugnote_get_all_bugnotes( $p_bug_id ) {
-	global $g_cache_bugnotes;
+	global $g_cache_bugnotes, $g_cache_bugnote;
 
 	if( !isset( $g_cache_bugnotes ) ) {
 		$g_cache_bugnotes = array();
 	}
+	
+	if( !isset( $g_cache_bugnote ) ) {
+		$g_cache_bugnote = array();
+	}
 
 	# the cache should be aware of the sorting order
-	if( !isset( $g_cache_bugnotes[$p_bug_id] ) ) {
+	if( !isset( $g_cache_bugnotes[(int)$p_bug_id] ) ) {
 		$t_bugnote_table = db_get_table( 'mantis_bugnote_table' );
 		$t_bugnote_text_table = db_get_table( 'mantis_bugnote_text_table' );
 
@@ -423,12 +433,13 @@ function bugnote_get_all_bugnotes( $p_bug_id ) {
 			$t_bugnote->time_tracking = $row['time_tracking'];
 
 			$t_bugnotes[] = $t_bugnote;
+			$g_cache_bugnote[(int)$t_bugnote->id] = $t_bugnote;
 		}
 
-		$g_cache_bugnotes[$p_bug_id] = $t_bugnotes;
+		$g_cache_bugnotes[(int)$p_bug_id] = true;
 	}
 
-	return $g_cache_bugnotes[$p_bug_id];
+	return $t_bugnotes;
 }
 
 /**
@@ -673,13 +684,14 @@ function bugnote_stats_get_project_array( $p_project_id, $p_from, $p_to, $p_cost
  * @access public
  */
 function bugnote_clear_cache( $p_bugnote_id = null ) {
-	global $g_cache_bugnotes;
+	global $g_cache_bugnote, $g_cache_bugnotes;
 
 	if( null === $p_bugnote_id ) {
-		$g_cache_bugnotes = array();
+		$g_cache_bugnote = array();
 	} else {
-		unset( $g_cache_bugnotes[(int) $p_bugnote_id] );
+		unset( $g_cache_bugnote[(int) $p_bugnote_id] );
 	}
-
+	$g_cache_bugnotes = array();
+	
 	return true;
 }

core/email_api.php
@@ -477,6 +477,9 @@ function email_generic( $p_bug_id, $p_notify_type, $p_message_id = null, $p_head
 		$t_recipients = email_collect_recipients( $p_bug_id, $p_notify_type, $p_extra_user_ids_to_email );
 
 		$t_project_id = bug_get_field( $p_bug_id, 'project_id' );
+		
+		bugnote_get_all_bugnotes( $p_bug_id );
+		
 		if( is_array( $t_recipients ) ) {
 			# send email to every recipient
 			foreach( $t_recipients as $t_user_id => $t_user_email ) {
@@ -1065,7 +1068,9 @@ function email_format_bug_message( $p_visible_bug_data ) {
 	}
 
 	if( isset( $p_visible_bug_data['relations'] ) ) {
-		$t_message .= $p_visible_bug_data['relations'];
+		if( $p_visible_bug_data['relations'] != "" ) {
+			$t_message .= $t_email_separator1 . "\n" . str_pad( lang_get( 'bug_relationships' ), 20 ) . str_pad( lang_get( 'id' ), 8 ) . lang_get( 'summary' ) . "\n" . $t_email_separator2 . "\n" . $p_visible_bug_data['relations'];
+		}
 	}
 
 	# Sponsorship

core/relationship_api.php
@@ -745,9 +745,6 @@ function relationship_get_summary_html_preview( $p_bug_id ) {
  * @return string
  */
 function relationship_get_summary_text( $p_bug_id ) {
-	$t_email_separator1 = config_get( 'email_separator1' );
-	$t_email_separator2 = config_get( 'email_separator2' );
-
 	$t_summary = "";
 	$t_show_project = false;
 
@@ -757,10 +754,6 @@ function relationship_get_summary_text( $p_bug_id ) {
 	# prepare the relationships table
 	for( $i = 0;$i < $t_relationship_all_count;$i++ ) {
 		$t_summary .= relationship_get_details( $p_bug_id, $t_relationship_all[$i], false );
-	}
-
-	if( $t_summary != "" ) {
-		$t_summary = $t_email_separator1 . "\n" . str_pad( lang_get( 'bug_relationships' ), 20 ) . str_pad( lang_get( 'id' ), 8 ) . lang_get( 'summary' ) . "\n" . $t_email_separator2 . "\n" . $t_summary;
 	}
 
 	return $t_summary;

commit 545da5fc106c1c0919799319b23cae8b774ddb29

Fix broken bugnote caching introduced in eba30569

core/bugnote_api.php
@@ -429,10 +429,10 @@ function bugnote_get_all_bugnotes( $p_bug_id ) {
 			$g_cache_bugnote[(int)$t_bugnote->id] = $t_bugnote;
 		}
 
-		$g_cache_bugnotes[(int)$p_bug_id] = true;
+		$g_cache_bugnotes[(int)$p_bug_id] = $t_bugnotes;
 	}
 
-	return $t_bugnotes;
+	return $g_cache_bugnotes[(int)$p_bug_id];
 }
 
 /**