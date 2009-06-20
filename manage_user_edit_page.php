mantisbt/mantisbt

commit 5824bb70420b2ca0a474d2cde06d6b9118957c4d

Remove hardcoded uses of ADMINISTRATOR constant
In custom installations it may be possible to have different
administrator access levels. An existing configuration option
$g_admin_site_threshold is used, alongside existing
user_is_administrator and current_user_is_administrator functions to
remove all hardcoded uses of the ADMINISTRATOR constant.

It is now possible to create your own administrator access level using
the custom_*_inc.php files. In some setups, you may even want to define
multiple different administrator access levels - even if it's just for
cosmetic reasons (different access level names appearing under each
user).

Signed-off-by: John Reese <jreese@leetcode.net>

manage_user_edit_page.php
@@ -149,7 +149,7 @@
 	</form>
 
 <!-- Delete Button -->
-<?php if ( !( ( ADMINISTRATOR <= $t_user['access_level'] ) && ( 1 >= user_count_level( ADMINISTRATOR ) ) ) ) { ?>
+<?php if ( !( ( user_is_administrator( $t_user ) && ( user_count_level( config_get_global( 'admin_site_threshold' ) ) <= 1 ) ) ) ) { ?>
 	<form method="post" action="manage_user_delete.php">
 <?php echo form_security_field( 'manage_user_delete' ) ?>
 
@@ -172,7 +172,7 @@
 
 <!-- PROJECT ACCESS (if permissions allow) and user is not ADMINISTRATOR -->
 <?php if ( access_has_global_level( config_get( 'manage_user_threshold' ) ) &&
-    !access_has_global_level( ADMINISTRATOR, $t_user['id'] ) ){
+    !user_is_administrator( $t_user_id ) ) {
 ?>
 <br />
 <div align="center">

account_prefs_update.php
@@ -86,7 +86,7 @@
 
 	# prevent users from changing other user's accounts
 	if ( $f_user_id != auth_get_current_user_id() ) {
-		access_ensure_project_level( ADMINISTRATOR );
+		access_ensure_global_level( config_get_global( 'admin_site_threshold' ) );
 	}
 
 	# make sure the delay isn't too low

admin/check.php
@@ -317,7 +317,7 @@ $t_anon_user = false;
 print_test_row( 'check configuration: anonymous_account is a valid username if set',
 	( (strlen( config_get_global( 'anonymous_account') ) > 0 ) ? ( ($t_anon_user = user_get_id_by_name( config_get_global( 'anonymous_account') ) ) !== false ) : TRUE ) );
 print_test_row( 'check configuration: anonymous_account should not be an administrator',
-	( $t_anon_user ? ( !access_compare_level( user_get_field( $t_anon_user, 'access_level' ), ADMINISTRATOR) ) : TRUE ) );
+	( $t_anon_user ? ( !user_is_administrator( $t_anon_user ) ) : TRUE ) );
 print_test_row( '$g_bug_link_tag is not empty ("' . config_get_global( 'bug_link_tag' ) . '")',
 	'' <> config_get_global( 'bug_link_tag' ) );
 print_test_row( '$g_bugnote_link_tag is not empty ("' . config_get_global( 'bugnote_link_tag' ) . '")',

admin/copy_field.php
@@ -26,7 +26,7 @@
  */
 require_once( dirname( dirname( __FILE__ ) ) . DIRECTORY_SEPARATOR . 'core.php' );
 
-access_ensure_global_level( ADMINISTRATOR );
+access_ensure_global_level( config_get_global( 'admin_site_threshold' ) );
 
 $f_source_field_id = gpc_get_int( 'source_id' );
 $f_dest_field = gpc_get( 'dest_id' );

admin/db_stats.php
@@ -25,7 +25,7 @@
  */
 require_once( dirname( dirname( __FILE__ ) ) . DIRECTORY_SEPARATOR . 'core.php' );
 
-access_ensure_global_level( ADMINISTRATOR );
+access_ensure_global_level( config_get_global( 'admin_site_threshold' ) );
 
 # --------------------
 function helper_table_row_count( $p_table ) {

admin/email_queue.php
@@ -25,7 +25,7 @@
  */
 require_once( dirname( dirname( __FILE__ ) ) . DIRECTORY_SEPARATOR . 'core.php' );
 
-access_ensure_global_level( ADMINISTRATOR );
+access_ensure_global_level( config_get_global( 'admin_site_threshold' ) );
 
 $f_to = gpc_get( 'send', null );
 

admin/index.php
@@ -26,7 +26,7 @@
 require_once( dirname( dirname( __FILE__ ) ) . DIRECTORY_SEPARATOR . 'core.php' );
 require_once( 'schema.php' );
 
-access_ensure_global_level( ADMINISTRATOR );
+access_ensure_global_level( config_get_global( 'admin_site_threshold' ) );
 
 html_page_top( 'MantisBT Administration' );
 

admin/move_db2disk.php
@@ -26,7 +26,7 @@
  */
 require_once( dirname( dirname( __FILE__ ) ) . DIRECTORY_SEPARATOR . 'core.php' );
 
-access_ensure_global_level( ADMINISTRATOR );
+access_ensure_global_level( config_get_global( 'admin_site_threshold' ) );
 
 // Move type should be attachment or project.
 $f_move_type = gpc_get( 'doc' );

commit 70a687a08c69da83fb3a5dd09a806f5e1dfd1e46

Fix invalid argument to user_is_administrator
A bug was introduced in 5824bb70420b2ca0a474d2cde06d6b9118957c4d where
an entire bug array was sent to the user_is_administrator function when
it only accepts a user ID. The rest of the before mentioned commit has
been double checked to make sure this was a one off (it was).

manage_user_edit_page.php
@@ -153,7 +153,7 @@
 	</form>
 
 <!-- Delete Button -->
-<?php if ( !( ( user_is_administrator( $t_user ) && ( user_count_level( config_get_global( 'admin_site_threshold' ) ) <= 1 ) ) ) ) { ?>
+<?php if ( !( ( user_is_administrator( $t_user_id ) && ( user_count_level( config_get_global( 'admin_site_threshold' ) ) <= 1 ) ) ) ) { ?>
 	<form method="post" action="manage_user_delete.php">
 <?php echo form_security_field( 'manage_user_delete' ) ?>
 