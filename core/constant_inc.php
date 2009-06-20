ce10860b00c93e87935727958c49e18b949e88e4
John Reese <jreese@leetcode.net>
2009-06-20 17:26:08 -0400

    Version bump.

--- core/constant_inc.php
+++ core/constant_inc.php
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
 
-define( 'MANTIS_VERSION', '1.2.0a3' );
+define( 'MANTIS_VERSION', '1.2.0rc1' );
 
 # --- constants -------------------
 # magic numbers
