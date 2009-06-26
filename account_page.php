58dad315491ea482c95e597203b6db1bb936f7d6
David Hicks <hickseydr@optusnet.com.au>
2009-06-26 19:40:10 +1000

    Add CSRF protection for account_delete

--- account_page.php
+++ account_page.php
@@ -266,6 +266,7 @@ if ( $t_ldap && ON == config_get( 'use_ldap_email' ) ) { ?> <!-- With LDAP Email
 <!-- Delete Button -->
 <div class="border center">
 	<form method="post" action="account_delete.php">
+	<?php echo form_security_field( 'account_delete' ) ?>
 	<input type="submit" class="button" value="<?php echo lang_get( 'delete_account_button' ) ?>" />
 	</form>
 </div>
