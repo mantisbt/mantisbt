mantisbt/mantisbt

commit 97b67e4bbb2f2df5f6fb24eea5b4bb37e1c28acd

Changes to bug object

bug_view_page.php
@@ -42,7 +42,7 @@
 
 	access_ensure_bug_level( VIEWER, $f_bug_id );
 
-	$t_bug = bug_prepare_display( bug_get( $f_bug_id, true ) );
+	$t_bug = bug_get( $f_bug_id, true );
 
 	$t_selected_project = helper_get_current_project();
 	if( $t_bug->project_id != $t_selected_project ) {
@@ -306,7 +306,7 @@
 		<?php echo lang_get( 'product_version' ) ?>
 	</td>
 	<td>
-		<?php echo $t_bug->version ?>
+		<?php echo string_display_line( $t_bug->version ) ?>
 	</td>
 	<?php
 		} else {
@@ -346,7 +346,7 @@
 		<?php echo lang_get( 'description' ) ?>
 	</td>
 	<td colspan="5">
-		<?php echo $t_bug->description ?>
+		<?php echo string_display_line_links( $t_bug->description ) ?>
 	</td>
 </tr>
 
@@ -357,7 +357,7 @@
 		<?php echo lang_get( 'additional_information' ) ?>
 	</td>
 	<td colspan="5">
-		<?php echo $t_bug->additional_information ?>
+		<?php echo string_display_line_links( $t_bug->additional_information ) ?>
 	</td>
 </tr>
 
@@ -458,7 +458,7 @@
 	include( $t_mantis_dir . 'bug_sponsorship_list_view_inc.php' );
 
 	# Bug Relationships
-	relationship_view_box ( $f_bug_id );
+	relationship_view_box ( $t_bug->id );
 
 	# File upload box
 	if ( !bug_is_readonly( $f_bug_id ) ) {

api/soap/mc_issue_api.php
@@ -531,7 +531,7 @@ function mc_issue_add( $p_username, $p_password, $p_issue ) {
 	$t_bug_data->additional_information = isset( $p_issue['additional_information'] ) ? $p_issue['additional_information'] : '';
 
 	# submit the issue
-	$t_issue_id = bug_create( $t_bug_data );
+	$t_issue_id = $t_bug_data->create();
 
 	mci_issue_set_custom_fields( $t_issue_id, $p_issue['custom_fields'] );
 

bug_relationship_graph.php
@@ -60,7 +60,7 @@
 
 	access_ensure_bug_level( VIEWER, $f_bug_id );
 
-	$t_bug = bug_prepare_display( bug_get( $f_bug_id, true ) );
+	$t_bug = bug_get( $f_bug_id, true );
 
 	if( $t_bug->project_id != helper_get_current_project() ) {
 		# in case the current project is not the same project of the bug we are viewing...

bug_relationship_graph_img.php
@@ -44,7 +44,7 @@
 
 	access_ensure_bug_level( VIEWER, $f_bug_id );
 
-	$t_bug = bug_prepare_display( bug_get( $f_bug_id, true ) );
+	$t_bug = bug_get( $f_bug_id, true );
 
 	compress_enable();
 

bug_report.php
@@ -114,7 +114,7 @@
 	$t_bug_data = event_signal( 'EVENT_REPORT_BUG_DATA', $t_bug_data );
 
 	# Create the bug
-	$t_bug_id = bug_create( $t_bug_data );
+	$t_bug_id = $t_bug_data->create();
 
 	# Mark the added issue as visited so that it appears on the last visited list.
 	last_visited_issue( $t_bug_id );

bug_report_advanced_page.php
@@ -60,7 +60,7 @@
 			trigger_error( ERROR_BUG_READ_ONLY_ACTION_DENIED, ERROR );
 		}
 
-		$t_bug = bug_prepare_edit( bug_get( $f_master_bug_id, true ) );
+		$t_bug = bug_get( $f_master_bug_id, true );
 
 		# the user can at least update the master bug (needed to add the relationship)...
 		access_ensure_bug_level( config_get( 'update_bug_threshold', null, $t_bug->project_id ), $f_master_bug_id );
@@ -347,7 +347,7 @@
 			<?php echo lang_get( 'product_build' ) ?>
 		</td>
 		<td>
-			<input <?php echo helper_get_tab_index() ?> type="text" name="build" size="32" maxlength="32" value="<?php echo $f_build ?>" />
+			<input <?php echo helper_get_tab_index() ?> type="text" name="build" size="32" maxlength="32" value="<?php echo string_attribute( $f_build ) ?>" />
 		</td>
 	</tr>
 <?php } ?>
@@ -390,7 +390,7 @@
 			<span class="required">*</span><?php print_documentation_link( 'summary' ) ?>
 		</td>
 		<td>
-			<input <?php echo helper_get_tab_index() ?> type="text" name="summary" size="105" maxlength="128" value="<?php echo $f_summary ?>" />
+			<input <?php echo helper_get_tab_index() ?> type="text" name="summary" size="105" maxlength="128" value="<?php echo string_attribute( $f_summary ) ?>" />
 		</td>
 	</tr>
 	<tr <?php echo helper_alternate_class() ?>>
@@ -398,7 +398,7 @@
 			<span class="required">*</span><?php print_documentation_link( 'description' ) ?>
 		</td>
 		<td>
-			<textarea <?php echo helper_get_tab_index() ?> name="description" cols="80" rows="10"><?php echo $f_description ?></textarea>
+			<textarea <?php echo helper_get_tab_index() ?> name="description" cols="80" rows="10"><?php echo string_textarea( $f_description ) ?></textarea>
 		</td>
 	</tr>
 	<tr <?php echo helper_alternate_class() ?>>
@@ -406,7 +406,7 @@
 			<?php print_documentation_link( 'steps_to_reproduce' ) ?>
 		</td>
 		<td>
-			<textarea <?php echo helper_get_tab_index() ?> name="steps_to_reproduce" cols="80" rows="10"><?php echo $f_steps_to_reproduce ?></textarea>
+			<textarea <?php echo helper_get_tab_index() ?> name="steps_to_reproduce" cols="80" rows="10"><?php echo string_textarea( $f_steps_to_reproduce ) ?></textarea>
 		</td>
 	</tr>
 	<tr <?php echo helper_alternate_class() ?>>
@@ -414,7 +414,7 @@
 			<?php print_documentation_link( 'additional_information' ) ?>
 		</td>
 		<td>
-			<textarea <?php echo helper_get_tab_index() ?> name="additional_info" cols="80" rows="10"><?php echo $f_additional_info ?></textarea>
+			<textarea <?php echo helper_get_tab_index() ?> name="additional_info" cols="80" rows="10"><?php echo string_textarea( $f_additional_info ) ?></textarea>
 		</td>
 	</tr>
 	<tr class="spacer">

bug_report_page.php
@@ -55,7 +55,7 @@
 			trigger_error( ERROR_BUG_READ_ONLY_ACTION_DENIED, ERROR );
 		}
 
-		$t_bug = bug_prepare_edit( bug_get( $f_master_bug_id, true ) );
+		$t_bug = bug_get( $f_master_bug_id, true );
 
 		# the user can at least update the master bug (needed to add the relationship)...
 		access_ensure_bug_level( config_get( 'update_bug_threshold', null, $t_bug->project_id ), $f_master_bug_id );
@@ -216,7 +216,7 @@
 			<span class="required">*</span><?php print_documentation_link( 'summary' ) ?>
 		</td>
 		<td>
-			<input <?php echo helper_get_tab_index() ?> type="text" name="summary" size="105" maxlength="128" value="<?php echo $f_summary ?>" />
+			<input <?php echo helper_get_tab_index() ?> type="text" name="summary" size="105" maxlength="128" value="<?php echo string_attribute( $f_summary ) ?>" />
 		</td>
 	</tr>
 	<tr <?php echo helper_alternate_class() ?>>
@@ -224,7 +224,7 @@
 			<span class="required">*</span><?php print_documentation_link( 'description' ) ?>
 		</td>
 		<td>
-			<textarea <?php echo helper_get_tab_index() ?> name="description" cols="80" rows="10"><?php echo $f_description ?></textarea>
+			<textarea <?php echo helper_get_tab_index() ?> name="description" cols="80" rows="10"><?php echo string_textarea( $f_description ) ?></textarea>
 		</td>
 	</tr>
 	<tr <?php echo helper_alternate_class() ?>>
@@ -232,7 +232,7 @@
 			<?php print_documentation_link( 'additional_information' ) ?>
 		</td>
 		<td>
-			<textarea <?php echo helper_get_tab_index() ?> name="additional_info" cols="80" rows="10"><?php echo $f_additional_info ?></textarea>
+			<textarea <?php echo helper_get_tab_index() ?> name="additional_info" cols="80" rows="10"><?php echo string_textarea( $f_additional_info ) ?></textarea>
 		</td>
 	</tr>
 	<tr class="spacer">

bug_update.php
@@ -212,7 +212,7 @@
 	}
 
 	# Update the bug entry, notify if we haven't done so already
-	bug_update( $f_bug_id, $t_bug_data, true, ( false == $t_notify ) );
+	$t_bug_data->update( true, ( false == $t_notify ) );
 
 	form_security_purge( 'bug_update' );
 

commit 37171c8c5c83ee2544f46cfd1f9fd7a3cc5ff13e

Fix #10646: description printed as single line
Commit 97b67e4bbb2f2df5f6fb24eea5b4bb37e1c28acd introduced two little
bugs into the simple bug view page (bug_view_page.php) where multi-line
text fields (description, additional information) were shown to the user
as only a single line of text.

bug_view_page.php
@@ -356,7 +356,7 @@
 		<?php echo lang_get( 'description' ) ?>
 	</td>
 	<td colspan="5">
-		<?php echo string_display_line_links( $t_bug->description ) ?>
+		<?php echo string_display_links( $t_bug->description ) ?>
 	</td>
 </tr>
 
@@ -367,7 +367,7 @@
 		<?php echo lang_get( 'additional_information' ) ?>
 	</td>
 	<td colspan="5">
-		<?php echo string_display_line_links( $t_bug->additional_information ) ?>
+		<?php echo string_display_links( $t_bug->additional_information ) ?>
 	</td>
 </tr>
 