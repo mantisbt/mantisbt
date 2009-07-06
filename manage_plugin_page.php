4fadaf737d95d5f9ae8faea3b7f40a542f509686
David Hicks <hickseydr@optusnet.com.au>
2009-07-07 00:57:49 +1000

    Add CSRF protection for manage_plugin_upgrade

--- manage_plugin_page.php
+++ manage_plugin_page.php
@@ -146,7 +146,7 @@ foreach ( $t_plugins_installed as $t_basename => $t_plugin ) {
 		echo '<td>&nbsp;</td><td>&nbsp;</td>';
 	}
 	echo '<td class="center">';
-	if ( $t_upgrade ) { print_bracket_link( 'manage_plugin_upgrade.php?name=' . $t_basename, lang_get( 'plugin_upgrade' ) ); }
+	if ( $t_upgrade ) { print_bracket_link( 'manage_plugin_upgrade.php?name=' . $t_basename . form_security_param( 'manage_plugin_upgrade' ), lang_get( 'plugin_upgrade' ) ); }
 	if ( $t_uninstall ) { print_bracket_link( 'manage_plugin_uninstall.php?name=' . $t_basename . form_security_param( 'manage_plugin_uninstall' ), lang_get( 'plugin_uninstall' ) ); }
 	echo '</td></tr>';
 } ?>
