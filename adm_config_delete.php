mantisbt/mantisbt

commit 108db3df79ec71419de550d35658ab5a41097791

Add CSRF protection for adm_config_delete

adm_config_delete.php
@@ -25,7 +25,7 @@
 	  */
 	require_once( 'core.php' );
 
-	# helper_ensure_post();
+	form_security_validate( 'adm_config_delete' );
 
 	$f_user_id = gpc_get_int( 'user_id' );
 	$f_project_id = gpc_get_int( 'project_id' );
@@ -46,6 +46,8 @@
 	helper_ensure_confirmed( lang_get( 'delete_config_sure_msg' ), lang_get( 'delete_link' ) );
 
 	config_delete( $f_config_option, $f_user_id, $f_project_id );
+
+	form_security_purg( 'adm_config_delete' );
 
 	print_successful_redirect( 'adm_config_report.php' );
 

commit 1df92f4cbc7719cb2d09e62a8b6b3560621d4826

Fix typo in adm_config_delete
Typo introduced in 108db3df79ec71419de550d35658ab5a41097791

adm_config_delete.php
@@ -47,7 +47,7 @@
 
 	config_delete( $f_config_option, $f_user_id, $f_project_id );
 
-	form_security_purg( 'adm_config_delete' );
+	form_security_purge( 'adm_config_delete' );
 
 	print_successful_redirect( 'adm_config_report.php' );
 