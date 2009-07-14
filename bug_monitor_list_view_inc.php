5800ee2cc204b7e22f23e47d11b665d8440e4fa1
John Reese <jreese@leetcode.net>
2009-07-14 16:26:09 -0400

    Fix syntax error and formatting.

--- bug_monitor_list_view_inc.php
+++ bug_monitor_list_view_inc.php
@@ -14,33 +14,33 @@
 # You should have received a copy of the GNU General Public License
 # along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
 
-	/**
-	 * This include file prints out the list of users monitoring the current
-	 * bug.	$f_bug_id must be set and be set to the bug id
-	 *
-	 * @package MantisBT
-	 * @copyright Copyright (C) 2000 - 2002  Kenzaburo Ito - kenito@300baud.org
-	 * @copyright Copyright (C) 2002 - 2009  MantisBT Team - mantisbt-dev@lists.sourceforge.net
-	 * @link http://www.mantisbt.org
-	 */
+/**
+ * This include file prints out the list of users monitoring the current
+ * bug.	$f_bug_id must be set and be set to the bug id
+ *
+ * @package MantisBT
+ * @copyright Copyright (C) 2000 - 2002  Kenzaburo Ito - kenito@300baud.org
+ * @copyright Copyright (C) 2002 - 2009  MantisBT Team - mantisbt-dev@lists.sourceforge.net
+ * @link http://www.mantisbt.org
+ */
 
-	if ( access_has_bug_level( config_get( 'show_monitor_list_threshold' ), $f_bug_id ) ) {
-		$c_bug_id = db_prepare_int( $f_bug_id );
-		$t_bug_monitor_table = db_get_table( 'mantis_bug_monitor_table' );
-		$t_user_table = db_get_table( 'mantis_user_table' );
+if ( access_has_bug_level( config_get( 'show_monitor_list_threshold' ), $f_bug_id ) ) {
+	$c_bug_id = db_prepare_int( $f_bug_id );
+	$t_bug_monitor_table = db_get_table( 'mantis_bug_monitor_table' );
+	$t_user_table = db_get_table( 'mantis_user_table' );
 
-		# get the bugnote data
-		$query = "SELECT user_id, enabled
-				FROM $t_bug_monitor_table m, $t_user_table u
-				WHERE m.bug_id=" . db_param() . " AND m.user_id = u.id
-				ORDER BY u.realname, u.username";
-		$result = db_query_bound($query, Array( $c_bug_id ) );
-		$num_users = db_num_rows($result);
+	# get the bugnote data
+	$query = "SELECT user_id, enabled
+			FROM $t_bug_monitor_table m, $t_user_table u
+			WHERE m.bug_id=" . db_param() . " AND m.user_id = u.id
+			ORDER BY u.realname, u.username";
+	$result = db_query_bound($query, Array( $c_bug_id ) );
+	$num_users = db_num_rows($result);
 
-		$t_users = array();
-		for ( $i = 0; $i < $num_users; $i++ ) {
-			$row = db_fetch_array( $result );
-			$t_users[$i] = $row['user_id'];
+	$t_users = array();
+	for ( $i = 0; $i < $num_users; $i++ ) {
+		$row = db_fetch_array( $result );
+		$t_users[$i] = $row['user_id'];
 	}
 	user_cache_array_rows( $t_users );
 
@@ -82,7 +82,7 @@
  			<input type="text" name="username" />
 			<input type="submit" class="button" value="<?php echo lang_get( 'add_user_to_monitor' ) ?>" />
  		</form>
-		<? } ?>
+		<?php } ?>
 	</td>
 </tr>
 </table>
@@ -102,3 +102,4 @@
 
 <?php 
 } # show monitor list
+
