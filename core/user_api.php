mantisbt/mantisbt

commit 3803c90c340dd20a0736af381fe50bfd3cfa838d

Anonymous accounts: fixes and doc updates
Updated documentation of configuration options relating to anonymous
access to Mantis. Fixes #10626.

Added new user_is_anonymous function to the user API. The purpose of
this function is to check if the current user is the anonymous user
account.

Changes user_is_protected function in the user API to always return true
when the user is the anonymous user account. The anonymous user should
never be allowed to update the preferences and settings on the anonymous
account! Fixes #10265.

Fixed a major security bug in account_prefs_reset.php that allowed
anyone to reset the account preferences of another person. Fixes #10264.

core/user_api.php
@@ -345,14 +345,37 @@ function user_is_administrator( $p_user_id ) {
 	}
 }
 
-# --------------------
-# return true is the user account is protected, false otherwise
+/*
+ * Check if a user has a protected user account.
+ * Protected user accounts cannot be updated without manage_user_threshold
+ * permission. If the user ID supplied is that of the anonymous user, this
+ * function will always return true. The anonymous user account is always
+ * considered to be protected.
+ *
+ * @param int $p_user_id
+ * @return true: user is protected; false: user is not protected.
+ * @access public
+ */
 function user_is_protected( $p_user_id ) {
-	if( ON == user_get_field( $p_user_id, 'protected' ) ) {
+	if( !user_is_anonymous( $p_user_id ) && ON == user_get_field( $p_user_id, 'protected' ) ) {
 		return true;
-	} else {
-		return false;
 	}
+	return false;
+}
+
+/*
+ * Check if a user is the anonymous user account.
+ * When anonymous logins are disabled this function will always return false.
+ *
+ * @param int $p_user_id
+ * @return true: user is the anonymous user; false: user is not the anonymous user.
+ * @access public
+ */
+function user_is_anonymous( $p_user_id ) {
+	if( ON == config_get( 'allow_anonymous_login' ) && user_get_field( 'username' ) == config_get( 'anonymous_account' ) ) {
+		return true;
+	}
+	return false;
 }
 
 # --------------------

account_manage_columns_page.php
@@ -35,10 +35,7 @@
 	# Define constant that will be checked by the include page.
 	define ( 'ACCOUNT_COLUMNS', '' );
 
-	# Anonymous / Protected users must not be able to customize the columns associated with the shared account.
-	if ( current_user_is_anonymous() || current_user_is_protected() ) {
-		access_denied();
-	}
+	current_user_ensure_unprotected();
 
 	include ( dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'manage_columns_inc.php' );
 

account_prefs_reset.php
@@ -51,7 +51,19 @@
 
 	auth_ensure_user_authenticated();
 
-	user_ensure_unprotected( $f_user_id );
+	user_ensure_exists( $f_user_id );
+
+	# This page is currently called from the manage_* namespace and thus we
+	# have to allow authorised users to update the accounts of other users.
+	# TODO: split this functionality into manage_user_prefs_reset.php
+	if ( auth_get_current_user_id() != $f_user_id ) {
+		access_ensure_global_level( config_get( 'manage_user_threshold' ) );
+	} else {
+		# Protected users should not be able to update the preferences of their
+		# user account. The anonymous user is always considered a protected
+		# user and hence will also not be allowed to update preferences.
+		user_ensure_unprotected( $f_user_id );
+	}
 
 	user_pref_delete( $f_user_id );
 

account_prefs_update.php
@@ -37,16 +37,17 @@
 
 	user_ensure_exists( $f_user_id );
 
-	# If the user is trying to modify an account other than their own
-	#  they must have high enough permissions to do so
-	# @@@ should we really be sharing this file between the manage section
-	#  and the account section.  The account section should always be operating
-	#  on the current user, so passing in a user ID here is a little odd.
+	# This page is currently called from the manage_* namespace and thus we
+	# have to allow authorised users to update the accounts of other users.
+	# TODO: split this functionality into manage_user_prefs_update.php
 	if ( auth_get_current_user_id() != $f_user_id ) {
 		access_ensure_global_level( config_get( 'manage_user_threshold' ) );
+	} else {
+		# Protected users should not be able to update the preferences of their
+		# user account. The anonymous user is always considered a protected
+		# user and hence will also not be allowed to update preferences.
+		user_ensure_unprotected( $f_user_id );
 	}
-
-	user_ensure_unprotected( $f_user_id );
 
 	$t_prefs = user_pref_get( $f_user_id );
 

config_defaults_inc.php
@@ -2379,14 +2379,22 @@
 	$g_allow_account_delete		= OFF;
 
 	/**
-	 * anonymous login
-	 * Allow anonymous login
+	 * Enable anonymous access to Mantis. You must also specify
+	 * $g_anonymous_account as the account which anonymous users will browse
+	 * Mantis with. The default setting is OFF.
 	 * @global int $g_allow_anonymous_login
 	 */
 	$g_allow_anonymous_login	= OFF;
 
 	/**
-	 *
+	 * Define the account which anonymous users will assume when using Mantis.
+	 * You only need to define this setting when $g_allow_anonymous_login is
+	 * set to ON. This account will always be treated as a protected account
+	 * and thus anonymous users will not be able to update the preferences or
+	 * settings of this account. It is suggested that the access level of this
+	 * account have read only access to your Mantis installation (VIEWER).
+	 * Please read the documentation on this topic before setting up anonymous
+	 * access to your Mantis installation.
 	 * @global string $g_anonymous_account
 	 */
 	$g_anonymous_account		= '';

core/access_api.php
@@ -91,7 +91,7 @@ function access_denied() {
 			print_header_redirect( 'login_page.php' . '?return=' . $t_return_page );
 		}
 	} else {
-		if( auth_get_current_user_id() == user_get_id_by_name( config_get_global( 'anonymous_account' ) ) ) {
+		if( current_user_is_anonymous() ) {
 			if( basename( $_SERVER['SCRIPT_NAME'] ) != 'login_page.php' ) {
 				$t_return_page = $_SERVER['PHP_SELF'];
 				if( isset( $_SERVER['QUERY_STRING'] ) ) {

core/authentication_api.php
@@ -213,11 +213,8 @@ function auth_attempt_login( $p_username, $p_password, $p_perm_login = false ) {
 		return false;
 	}
 
-	$t_anon_account = config_get( 'anonymous_account' );
-	$t_anon_allowed = config_get( 'allow_anonymous_login' );
-
 	# check for anonymous login
-	if( !(( ON == $t_anon_allowed ) && ( $t_anon_account == $p_username ) ) ) {
+	if( !user_is_anonymous( $t_user_id ) ) {
 		# anonymous login didn't work, so check the password
 
 		if( !auth_does_password_match( $t_user_id, $p_password ) ) {

core/current_user_api.php
@@ -154,9 +154,10 @@ function current_user_is_administrator() {
 }
 
 /**
- * Returns true if the currently logged in user protected, false otherwise
+ * Returns true if the current user is a protected user, false otherwise.
+ * The $g_anonymous_account user is always considered protected.
  *
- * @return true: user protected; false: otherwise.
+ * @return true: user is protected; false: otherwise.
  * @access public
  */
 function current_user_is_protected() {
@@ -164,21 +165,18 @@ function current_user_is_protected() {
 }
 
 /**
- * Returns true if the currently user is the anonymous user
+ * Returns true if the current user is the anonymous user.
  *
- * @return true: user anonymous; false: otherwise.
+ * @return true: user is anonymous; false: otherwise.
  * @access public
  */
 function current_user_is_anonymous() {
-	if( auth_is_user_authenticated() ) {
-		return(( ON == config_get( 'allow_anonymous_login' ) ) && ( current_user_get_field( 'username' ) == config_get( 'anonymous_account' ) ) );
-	} else {
-		return false;
-	}
+	return user_is_anonymous( auth_get_current_user_id() );
 }
 
 /**
- * Triggers an ERROR if the current user account is protected
+ * Triggers an ERROR if the current user account is protected.
+ * The $g_anonymous_account user is always considered protected.
  *
  * @access public
  */

commit 7b794dfc91b733a5899412997cd3cb8ffc433280

Fix #10889: user_is_protected() incorrect logic for anon accounts
Commit 3803c90c340dd20a0736af381fe50bfd3cfa838d introduced a bug whereby
the anonymous account would never be considered protected. This was due
to inversed/invalid logic. How embarrassing!

core/user_api.php
@@ -357,7 +357,7 @@ function user_is_administrator( $p_user_id ) {
  * @access public
  */
 function user_is_protected( $p_user_id ) {
-	if( !user_is_anonymous( $p_user_id ) && ON == user_get_field( $p_user_id, 'protected' ) ) {
+	if( user_is_anonymous( $p_user_id ) || ON == user_get_field( $p_user_id, 'protected' ) ) {
 		return true;
 	}
 	return false;