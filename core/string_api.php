aea59ec80dcb147ea5763668fce7256ac0205ba2
John Reese <jreese@leetcode.net>
2009-09-04 15:04:35 -0400

    Fix #10663: Make email regex case-insensitive

--- core/string_api.php
+++ core/string_api.php
@@ -475,7 +475,7 @@ function string_insert_hrefs( $p_string ) {
 		ini_set( 'magic_quotes_sybase', true );
 	}
 
-	$p_string = preg_replace( '/\s' . email_regex_simple() . '\s/i', '<a href="mailto:\0">\0</a>', $p_string );
+	$p_string = preg_replace( email_regex_simple(), '<a href="mailto:\0">\0</a>', $p_string );
 
 	return $p_string;
 }
