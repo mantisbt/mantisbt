b4fe91cbbe761ee43817481cef7fc7fb852132cd
Victor Boctor <vboctor@gmail.com>
2009-10-25 23:35:38 -0700

    Fixes #11053: Using real names and monitoring.

--- bug_monitor_add.php
+++ bug_monitor_add.php
@@ -42,8 +42,12 @@
 	} else {
 		$t_user_id = user_get_id_by_name( $f_username );
 		if ( $t_user_id === false ) {
-			error_parameters( $f_username );
-			trigger_error( ERROR_USER_BY_NAME_NOT_FOUND, E_USER_ERROR );
+			$t_user_id = user_get_id_by_realname( $f_username );
+
+			if ( $t_user_id === false ) {
+				error_parameters( $f_username );
+				trigger_error( ERROR_USER_BY_NAME_NOT_FOUND, E_USER_ERROR );
+			}
 		}
 	}
 
