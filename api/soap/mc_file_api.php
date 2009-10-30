77e6eac90f32455582308559d628f9f655b874ce
Robert Munteanu <robert.munteanu@gmail.com>
2009-10-30 11:20:24 +0200

    Fix project attachment handling using the SOAP API
    This commit fixes 2 issues with the project attachment handling:
    
    - in mc_project_attachment_add the contents was not base64 decoded;
    - in mci_file_get the $row array expected to contain a bug id, bug
      that only holds for issue attachments.
    
    A test has been added to verify the behaviour of project
    attachments.

--- api/soap/mc_file_api.php
+++ api/soap/mc_file_api.php
@@ -159,10 +159,10 @@ function mci_file_get( $p_file_id, $p_type, $p_user_id ) {
 	$result = db_query( $query );
 	$row = db_fetch_array( $result );
 
-	$t_bug_id = $row['bug_id'];
-
 	if ( $p_type == 'doc' ) {
 		$t_project_id = $row['project_id'];
+	} else if ( $p_type == 'bug' ) {
+		$t_bug_id = $row['bug_id'];
 	}
 
 	$t_diskfile = $row['diskfile'];
