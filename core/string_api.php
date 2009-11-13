mantisbt/mantisbt

commit 193ae8b94faa104d3d74a8e169eda99e10cbcf8c

Fix #10535: Escape path and short_path configuration options
As these two options can contain slashes, periods and other characters
with special meaning in PRCE, they must be escaped prior to use within
regular expression patterns.

core/string_api.php
@@ -240,11 +240,11 @@ function string_sanitize_url( $p_url, $p_return_absolute = false ) {
 
 	# Break the given URL into pieces for path, script, query, and anchor
 	$t_type = 0;
-	if ( preg_match( "@^(?P<path>$t_path)$t_pattern\$@", $t_url, $t_matches ) ) {
+	if ( preg_match( '@^(?P<path>' . preg_quote( $t_path, '/' ) . ')' . $t_pattern . '\$@', $t_url, $t_matches ) ) {
 		$t_type = 1;
-	} else if ( preg_match( "@^(?P<path>$t_short_path)$t_pattern\$@", $t_url, $t_matches ) ) {
+	} else if ( preg_match( '@^(?P<path>' . preg_quote( $t_short_path, '/' ) . ')' . $t_pattern . '\$@', $t_url, $t_matches ) ) {
 		$t_type = 2;
-	} else if ( preg_match( "@^(?P<path>)$t_pattern\$@", $t_url, $t_matches ) ) {
+	} else if ( preg_match( '@^(?P<path>)$t_pattern\$@', $t_url, $t_matches ) ) {
 		$t_type = 3;
 	}
 

commit 9e25918824f85ffa790832d58b490d566d8e2609

Fix #11155: Broken PCRE regex strings broke page redirects
Commit 193ae8b94faa104d3d74a8e169eda99e10cbcf8c broke page redirects due
to completely broken PCRE regex strings. My fault, was in a hurry. Sorry!

core/string_api.php
@@ -240,11 +240,11 @@ function string_sanitize_url( $p_url, $p_return_absolute = false ) {
 
 	# Break the given URL into pieces for path, script, query, and anchor
 	$t_type = 0;
-	if ( preg_match( '@^(?P<path>' . preg_quote( $t_path, '@' ) . ')' . $t_pattern . '\$@', $t_url, $t_matches ) ) {
+	if ( preg_match( '@^(?P<path>' . preg_quote( $t_path, '@' ) . ')' . $t_pattern . '$@', $t_url, $t_matches ) ) {
 		$t_type = 1;
-	} else if ( preg_match( '@^(?P<path>' . preg_quote( $t_short_path, '@' ) . ')' . $t_pattern . '\$@', $t_url, $t_matches ) ) {
+	} else if ( preg_match( '@^(?P<path>' . preg_quote( $t_short_path, '@' ) . ')' . $t_pattern . '$@', $t_url, $t_matches ) ) {
 		$t_type = 2;
-	} else if ( preg_match( '@^(?P<path>)$t_pattern\$@', $t_url, $t_matches ) ) {
+	} else if ( preg_match( '@^(?P<path>)' . $t_pattern . '$@', $t_url, $t_matches ) ) {
 		$t_type = 3;
 	}
 