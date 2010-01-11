mantisbt/mantisbt

commit 2d5455cce290531359a55d353010258c6f3f6f34

Use require_api/require_lib to pull in dependencies
Following up on ea3cd38976a5b2af0ff0615b91d2f5d51970d783 which
introduced require_once calls within the core API files to included
dependencies in-place, this commit does a similar thing for user
viewable pages.

Replacements for require_once have been created within core.php to help
optimise the increased number of dependency checks.

require_api and require_lib have the same syntax as require_once however
the first will look within the core directory only, and the second will
look within the library directory only. The library directory can now be
relocated elsewhere by use of the $g_library_path configuration option.

require_api and require_lib use a global array to keep track of which
files have already been included. This is a much simpler check than what
is performed by require_once and hence avoids expensive checking for
symlinks with stat() calls and so forth.

view_all_set.php
@@ -14,16 +14,44 @@
 # You should have received a copy of the GNU General Public License
 # along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
 
-	/**
-	 * @package MantisBT
-	 * @copyright Copyright (C) 2000 - 2002  Kenzaburo Ito - kenito@300baud.org
-	 * @copyright Copyright (C) 2002 - 2010  MantisBT Team - mantisbt-dev@lists.sourceforge.net
-	 * @link http://www.mantisbt.org
-	 */
-	 /**
-	  * MantisBT Core API's
-	  */
-	require_once( 'core.php' );
+/**
+ * @package MantisBT
+ * @copyright Copyright (C) 2000 - 2002  Kenzaburo Ito - kenito@300baud.org
+ * @copyright Copyright (C) 2002 - 2010  MantisBT Team - mantisbt-dev@lists.sourceforge.net
+ * @link http://www.mantisbt.org
+ *
+ * @uses core.php
+ * @uses authentication_api.php
+ * @uses config_api.php
+ * @uses constant_inc.php
+ * @uses custom_field_api.php
+ * @uses error_api.php
+ * @uses filter_api.php
+ * @uses filter_constants_inc.php
+ * @uses gpc_api.php
+ * @uses helper_api.php
+ * @uses html_api.php
+ * @uses logging_api.php
+ * @uses print_api.php
+ * @uses tokens_api.php
+ * @uses utility_api.php
+ */
+
+require_once( 'core.php' );
+require_api( 'authentication_api.php' );
+require_api( 'config_api.php' );
+require_api( 'constant_inc.php' );
+require_api( 'custom_field_api.php' );
+require_api( 'error_api.php' );
+require_api( 'filter_api.php' );
+reuqire_api( 'filter_constants_inc.php' );
+require_api( 'gpc_api.php' );
+require_api( 'helper_api.php' );
+require_api( 'html_api.php' );
+require_api( 'logging_api.php' );
+require_api( 'print_api.php' );
+require_api( 'tokens_api.php' );
+require_api( 'utility_api.php' );
 
 	auth_ensure_user_authenticated();
 

commit 94dc37c3a3c21b58de4bf81ac2f517914b2a8d78

Fix #11382: Typo in includes within view_all_set.php
Commit 2d5455cce290531359a55d353010258c6f3f6f34 introduced
require_api/require_lib to the start of each PHP file so that
dependencies are included where they're used.

There was however a typo in view_all_set.php that led to a non-existant
function being called.

view_all_set.php
@@ -44,7 +44,7 @@ require_api( 'constant_inc.php' );
 require_api( 'custom_field_api.php' );
 require_api( 'error_api.php' );
 require_api( 'filter_api.php' );
-reuqire_api( 'filter_constants_inc.php' );
+require_api( 'filter_constants_inc.php' );
 require_api( 'gpc_api.php' );
 require_api( 'helper_api.php' );
 require_api( 'html_api.php' );