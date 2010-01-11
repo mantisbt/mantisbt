5e2d92c9f712d04c311febc6f070943f63e64bfc
David Hicks <hickseydr@optusnet.com.au>
2010-01-11 13:01:33 +1100

    Fix #11373: Page links have incorrect spacing (first, prev, 1, 2...)
    The page navigation links printed to view_all_bug_page.php had incorrect
    spacing around the special text links for first, previous, next and last
    page.

--- core/print_api.php
+++ core/print_api.php
@@ -1404,7 +1404,7 @@ function print_page_link( $p_page_url, $p_text = '', $p_page_no = 0, $p_page_cur
 			print_link( "$p_page_url?page_number=$p_page_no", $p_text );
 		}
 	} else {
-		echo " $p_text ";
+		echo $p_text;
 	}
 }
 
@@ -1432,7 +1432,9 @@ function print_page_links( $p_page, $p_start, $p_end, $p_current, $p_temp_filter
 
 	# First and previous links
 	print_page_link( $p_page, $t_first, 1, $p_current, $p_temp_filter_id );
+	echo '&nbsp;';
 	print_page_link( $p_page, $t_prev, $p_current - 1, $p_current, $p_temp_filter_id );
+	echo '&nbsp;';
 
 	# Page numbers ...
 
@@ -1465,11 +1467,13 @@ function print_page_links( $p_page, $p_start, $p_end, $p_current, $p_temp_filter
 	}
 
 	# Next and Last links
+	echo '&nbsp;';
 	if( $p_current < $p_end ) {
 		print_page_link( $p_page, $t_next, $p_current + 1, $p_current, $p_temp_filter_id );
 	} else {
 		print_page_link( $p_page, $t_next, null, null, $p_temp_filter_id );
 	}
+	echo '&nbsp;';
 	print_page_link( $p_page, $t_last, $p_end, $p_current, $p_temp_filter_id );
 
 	print( ' ]' );
