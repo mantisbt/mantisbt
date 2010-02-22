8f89da5c5bc449718ed9618af5ce5fef51870d6f
John Reese <jreese@leetcode.net>
2010-02-22 14:24:52 -0500

    Preparing for 1.2.0 release
    Release notes updated to be relevant for an official 1.2.0 release.  The
    internal version number for MantisBT has also been updated.
    
    This marks the official end of feature work on the 1.2.x branch.
    Commits to the master-1.2.x branch are restricted to bug fixes and
    security issues.

--- core/constant_inc.php
+++ core/constant_inc.php
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
 
-define( 'MANTIS_VERSION', '1.2.0rc2' );
+define( 'MANTIS_VERSION', '1.2.0' );
 
 # --- constants -------------------
 # magic numbers
