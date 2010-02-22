616d1768197c5f685ddb1466aa5ff6947f1a3677
John Reese <jreese@leetcode.net>
2010-02-21 21:49:48 -0500

    Remove verbose constants for bug fields

--- config_defaults_inc.php
+++ config_defaults_inc.php
@@ -2101,84 +2101,78 @@ $g_enable_product_build = OFF;
  * An array of the fields to show on the bug report page.
  *
  * The following fields can not be included:
- * BUG_FIELD_ID, BUG_FIELD_PROJECT, BUG_FIELD_DATE_SUBMITTED,
- * BUG_FIELD_LAST_UPDATED, BUG_FIELD_STATUS, BUG_FIELD_RESOLUTION,
- * BUG_FIELD_TAGS, BUG_FIELD_FIXED_IN_VERSION, BUG_FIELD_PROJECTION,
- * BUG_FIELD_ETA, BUG_FIELD_REPORTER.
+ * id, project, date_submitted,
+ * last_updated, status, resolution,
+ * tags, fixed_in_version, projection,
+ * eta, reporter.
  *
  * The following fields must be included:
- * BUG_FIELD_CATEGORY, BUG_FIELD_SUMMARY, BUG_FIELD_DESCRIPTION.
+ * category_id, summary, description.
  *
  * To overload this setting per project, then the settings must be included in
- * the database through the generic configuration form.  Note that the array in
- * the database should consistent of the values of the constants.  For example,
- * replace BUG_FIELD_CATEGORY with 'category_id'.  See constant_inc.php for the
- * values of the constants.
+ * the database through the generic configuration form.
  *
  * @global array $g_bug_report_page_fields
  */
 $g_bug_report_page_fields = array(
-	BUG_FIELD_CATEGORY,
-	BUG_FIELD_VIEW_STATE,
-	BUG_FIELD_HANDLER,
-	BUG_FIELD_PRIORITY,
-	BUG_FIELD_SEVERITY,
-	BUG_FIELD_REPRODUCIBILITY,
-	BUG_FIELD_PLATFORM,
-	BUG_FIELD_OS,
-	BUG_FIELD_OS_VERSION,
-	BUG_FIELD_PRODUCT_VERSION,
-	BUG_FIELD_PRODUCT_BUILD,
-	BUG_FIELD_TARGET_VERSION,
-	BUG_FIELD_SUMMARY,
-	BUG_FIELD_DESCRIPTION,
-	BUG_FIELD_ADDITIONAL_INFO,
-	BUG_FIELD_STEPS_TO_REPRODUCE,
-	BUG_FIELD_ATTACHMENTS,
-	BUG_FIELD_DUE_DATE,
+	'category_id',
+	'view_state',
+	'handler',
+	'priority',
+	'severity',
+	'reproducibility',
+	'platform',
+	'os',
+	'os_version',
+	'product_version',
+	'product_build',
+	'target_version',
+	'summary',
+	'description',
+	'additional_info',
+	'steps_to_reproduce',
+	'attachments',
+	'due_date',
 );
 
 /**
  * An array of the fields to show on the bug view page.
  *
  * To overload this setting per project, then the settings must be included in
- * the database through the generic configuration form.  Note that the array in
- * the database should consistent of the values of the constants.  For example,
- * replace BUG_FIELD_CATEGORY with 'category_id'.  See constant_inc.php for the
- * values of the constants.
+ * the database through the generic configuration form.
  *
  * @global array $g_bug_view_page_fields
  */
 $g_bug_view_page_fields = array (
-	BUG_FIELD_ID,
-	BUG_FIELD_PROJECT,
-	BUG_FIELD_CATEGORY,
-	BUG_FIELD_VIEW_STATE,
-	BUG_FIELD_DATE_SUBMITTED,
-	BUG_FIELD_LAST_UPDATED,
-	BUG_FIELD_REPORTER,
-	BUG_FIELD_HANDLER,
-	BUG_FIELD_PRIORITY,
-	BUG_FIELD_SEVERITY,
-	BUG_FIELD_REPRODUCIBILITY,
-	BUG_FIELD_STATUS,
-	BUG_FIELD_RESOLUTION,
-	BUG_FIELD_PROJECTION,
-	BUG_FIELD_ETA,
-	BUG_FIELD_PLATFORM,
-	BUG_FIELD_OS,
-	BUG_FIELD_OS_VERSION,
-	BUG_FIELD_PRODUCT_VERSION,
-	BUG_FIELD_PRODUCT_BUILD,
-	BUG_FIELD_TARGET_VERSION,
-	BUG_FIELD_FIXED_IN_VERSION,
-	BUG_FIELD_SUMMARY,
-	BUG_FIELD_DESCRIPTION,
-	BUG_FIELD_ADDITIONAL_INFO,
-	BUG_FIELD_STEPS_TO_REPRODUCE,
-	BUG_FIELD_TAGS,
-	BUG_FIELD_ATTACHMENTS,
-	BUG_FIELD_DUE_DATE,
+	'id',
+	'project',
+	'category_id',
+	'view_state',
+	'date_submitted',
+	'last_updated',
+	'reporter',
+	'handler',
+	'priority',
+	'severity',
+	'reproducibility',
+	'status',
+	'resolution',
+	'projection',
+	'eta',
+	'platform',
+	'os',
+	'os_version',
+	'product_version',
+	'product_build',
+	'target_version',
+	'fixed_in_version',
+	'summary',
+	'description',
+	'additional_info',
+	'steps_to_reproduce',
+	'tags',
+	'attachments',
+	'due_date',
 );
 
 /**
@@ -2186,120 +2180,114 @@ $g_bug_view_page_fields = array (
  * @global array $g_bug_print_page_fields
  */
 $g_bug_print_page_fields = array (
-	BUG_FIELD_ID,
-	BUG_FIELD_PROJECT,
-	BUG_FIELD_CATEGORY,
-	BUG_FIELD_VIEW_STATE,
-	BUG_FIELD_DATE_SUBMITTED,
-	BUG_FIELD_LAST_UPDATED,
-	BUG_FIELD_REPORTER,
-	BUG_FIELD_HANDLER,
-	BUG_FIELD_PRIORITY,
-	BUG_FIELD_SEVERITY,
-	BUG_FIELD_REPRODUCIBILITY,
-	BUG_FIELD_STATUS,
-	BUG_FIELD_RESOLUTION,
-	BUG_FIELD_PROJECTION,
-	BUG_FIELD_ETA,
-	BUG_FIELD_PLATFORM,
-	BUG_FIELD_OS,
-	BUG_FIELD_OS_VERSION,
-	BUG_FIELD_PRODUCT_VERSION,
-	BUG_FIELD_PRODUCT_BUILD,
-	BUG_FIELD_TARGET_VERSION,
-	BUG_FIELD_FIXED_IN_VERSION,
-	BUG_FIELD_SUMMARY,
-	BUG_FIELD_DESCRIPTION,
-	BUG_FIELD_ADDITIONAL_INFO,
-	BUG_FIELD_STEPS_TO_REPRODUCE,
-	BUG_FIELD_TAGS,
-	BUG_FIELD_ATTACHMENTS,
-	BUG_FIELD_DUE_DATE,
+	'id',
+	'project',
+	'category_id',
+	'view_state',
+	'date_submitted',
+	'last_updated',
+	'reporter',
+	'handler',
+	'priority',
+	'severity',
+	'reproducibility',
+	'status',
+	'resolution',
+	'projection',
+	'eta',
+	'platform',
+	'os',
+	'os_version',
+	'product_version',
+	'product_build',
+	'target_version',
+	'fixed_in_version',
+	'summary',
+	'description',
+	'additional_info',
+	'steps_to_reproduce',
+	'tags',
+	'attachments',
+	'due_date',
 );
 
 /**
  * An array of the fields to show on the bug update page.
  *
  * To overload this setting per project, then the settings must be included in
- * the database through the generic configuration form.  Note that the array in
- * the database should consistent of the values of the constants.  For example,
- * replace BUG_FIELD_CATEGORY with 'category_id'.  See constant_inc.php for the
- * values of the constants.
+ * the database through the generic configuration form.
  *
  * @global array $g_bug_update_page_fields
  */
 $g_bug_update_page_fields = array (
-	BUG_FIELD_ID,
-	BUG_FIELD_PROJECT,
-	BUG_FIELD_CATEGORY,
-	BUG_FIELD_VIEW_STATE,
-	BUG_FIELD_DATE_SUBMITTED,
-	BUG_FIELD_LAST_UPDATED,
-	BUG_FIELD_REPORTER,
-	BUG_FIELD_HANDLER,
-	BUG_FIELD_PRIORITY,
-	BUG_FIELD_SEVERITY,
-	BUG_FIELD_REPRODUCIBILITY,
-	BUG_FIELD_STATUS,
-	BUG_FIELD_RESOLUTION,
-	BUG_FIELD_PROJECTION,
-	BUG_FIELD_ETA,
-	BUG_FIELD_PLATFORM,
-	BUG_FIELD_OS,
-	BUG_FIELD_OS_VERSION,
-	BUG_FIELD_PRODUCT_VERSION,
-	BUG_FIELD_PRODUCT_BUILD,
-	BUG_FIELD_TARGET_VERSION,
-	BUG_FIELD_FIXED_IN_VERSION,
-	BUG_FIELD_SUMMARY,
-	BUG_FIELD_DESCRIPTION,
-	BUG_FIELD_ADDITIONAL_INFO,
-	BUG_FIELD_STEPS_TO_REPRODUCE,
-	BUG_FIELD_ATTACHMENTS,
-	BUG_FIELD_DUE_DATE,
+	'id',
+	'project',
+	'category_id',
+	'view_state',
+	'date_submitted',
+	'last_updated',
+	'reporter',
+	'handler',
+	'priority',
+	'severity',
+	'reproducibility',
+	'status',
+	'resolution',
+	'projection',
+	'eta',
+	'platform',
+	'os',
+	'os_version',
+	'product_version',
+	'product_build',
+	'target_version',
+	'fixed_in_version',
+	'summary',
+	'description',
+	'additional_info',
+	'steps_to_reproduce',
+	'attachments',
+	'due_date',
 );
 
 /**
  * An array of the fields to show on the bug change status page.
  *
  * To overload this setting per project, then the settings must be included in
- * the database through the generic configuration form.  Note that the array in
- * the database should consistent of the values of the constants.  For example,
- * replace BUG_FIELD_CATEGORY with 'category_id'.  See constant_inc.php for the
- * values of the constants.
+ * the database through the generic configuration form.
  *
  * @global array $g_bug_change_status_page_fields
  */
 $g_bug_change_status_page_fields = array (
-	BUG_FIELD_ID,
-	BUG_FIELD_PROJECT,
-	BUG_FIELD_CATEGORY,
-	BUG_FIELD_VIEW_STATE,
-	BUG_FIELD_DATE_SUBMITTED,
-	BUG_FIELD_LAST_UPDATED,
-	BUG_FIELD_REPORTER,
-	BUG_FIELD_HANDLER,
-	BUG_FIELD_PRIORITY,
-	BUG_FIELD_SEVERITY,
-	BUG_FIELD_REPRODUCIBILITY,
-	BUG_FIELD_STATUS,
-	BUG_FIELD_RESOLUTION,
-	BUG_FIELD_PROJECTION,
-	BUG_FIELD_ETA,
-	BUG_FIELD_PLATFORM,
-	BUG_FIELD_OS,
-	BUG_FIELD_OS_VERSION,
-	BUG_FIELD_PRODUCT_VERSION,
-	BUG_FIELD_PRODUCT_BUILD,
-	BUG_FIELD_TARGET_VERSION,
-	BUG_FIELD_FIXED_IN_VERSION,
-	BUG_FIELD_SUMMARY,
-	BUG_FIELD_DESCRIPTION,
-	BUG_FIELD_ADDITIONAL_INFO,
-	BUG_FIELD_STEPS_TO_REPRODUCE,
-	BUG_FIELD_TAGS,
-	BUG_FIELD_ATTACHMENTS,
-	BUG_FIELD_DUE_DATE,
+	'id',
+	'project',
+	'category_id',
+	'view_state',
+	'date_submitted',
+	'last_updated',
+	'reporter',
+	'handler',
+	'priority',
+	'severity',
+	'reproducibility',
+	'status',
+	'resolution',
+	'projection',
+	'eta',
+	'platform',
+	'os',
+	'os_version',
+	'product_version',
+	'product_build',
+	'target_version',
+	'fixed_in_version',
+	'summary',
+	'description',
+	'additional_info',
+	'steps_to_reproduce',
+	'tags',
+	'attachments',
+	'due_date',
 );
 
 /**************************
