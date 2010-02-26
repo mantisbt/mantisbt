mantisbt/mantisbt

commit f838e89dc6460f3f2e65d20e4bd904203bd6f880

Fix #11482: Fix delimiter for links generated with print_page_link[s]
The functions print_page_link/print_page_links take a URL and add extra
parameters to the end of the URL for page navigation. The problem is
that plugin pages already contain parameters. Thus we shouldn't assume
that URLs passed to these functions don't already contain parameters.

This patch checks for existing parameters and if any are found, page
navigation parameters are appended to the end with an ampersand
delimiter instead of question mark.

Signed-off-by: David Hicks <hickseydr@optusnet.com.au>

core/print_api.php
@@ -1378,11 +1378,12 @@ function print_page_link( $p_page_url, $p_text = '', $p_page_no = 0, $p_page_cur
 		$p_text = $p_page_no;
 	}
 
-	if(( 0 < $p_page_no ) && ( $p_page_no != $p_page_cur ) ) {
+	if( ( 0 < $p_page_no ) && ( $p_page_no != $p_page_cur ) ) {
+		$t_delimiter = ( strpos( $p_page_url, "?" ) ? "&" : "?" );
 		if( $p_temp_filter_id !== 0 ) {
-			print_link( "$p_page_url?filter=$p_temp_filter_id&page_number=$p_page_no", $p_text );
+			print_link( "$p_page_url${t_delimiter}filter=$p_temp_filter_id&page_number=$p_page_no", $p_text );
 		} else {
-			print_link( "$p_page_url?page_number=$p_page_no", $p_text );
+			print_link( "$p_page_url${t_delimiter}page_number=$p_page_no", $p_text );
 		}
 	} else {
 		echo $p_text;
@@ -1434,10 +1435,11 @@ function print_page_links( $p_page, $p_start, $p_end, $p_current, $p_temp_filter
 		if( $i == $p_current ) {
 			array_push( $t_items, $i );
 		} else {
+			$t_delimiter = ( strpos( $p_page, "?" ) ? "&" : "?" ) ;
 			if( $p_temp_filter_id !== 0 ) {
-				array_push( $t_items, "<a href=\"$p_page?filter=$p_temp_filter_id&page_number=$i\">$i</a>" );
+				array_push( $t_items, "<a href=\"$p_page${t_delimiter}filter=$p_emp_filter_id&page_number=$i\">$i</a>" );
 			} else {
-				array_push( $t_items, "<a href=\"$p_page?page_number=$i\">$i</a>" );
+				array_push( $t_items, "<a href=\"$p_page${t_delimiter}page_number=$i\">$i</a>" );
 			}
 		}
 	}

commit 6a424345436880517c92601083a78ce9b1a9bbfc

Fix #11569: Incorrect variable name in print_page_links()
Commit f838e89dc6460f3f2e65d20e4bd904203bd6f880 introduced a bug whereby
a variable name was incorrect ($p_emp_filter_id instead of
$p_temp_filter_id), thereby causing errors.

core/print_api.php
@@ -1437,7 +1437,7 @@ function print_page_links( $p_page, $p_start, $p_end, $p_current, $p_temp_filter
 		} else {
 			$t_delimiter = ( strpos( $p_page, "?" ) ? "&" : "?" ) ;
 			if( $p_temp_filter_id !== 0 ) {
-				array_push( $t_items, "<a href=\"$p_page${t_delimiter}filter=$p_emp_filter_id&page_number=$i\">$i</a>" );
+				array_push( $t_items, "<a href=\"$p_page${t_delimiter}filter=$p_temp_filter_id&page_number=$i\">$i</a>" );
 			} else {
 				array_push( $t_items, "<a href=\"$p_page${t_delimiter}page_number=$i\">$i</a>" );
 			}