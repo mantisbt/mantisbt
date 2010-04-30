0ac6cac8e5c0d07425a62150c5e10e487a284c09
Kornel <kornel@aardvarkmedia.co.uk>
2010-04-30 16:48:16 +0100

    Added xmlns required for XHTML
    Signed-off-by: David Hicks <hickseydr@optusnet.com.au>

--- core/html_api.php
+++ core/html_api.php
@@ -307,7 +307,7 @@ function html_page_bottom1a( $p_file = null ) {
  */
 function html_begin() {
 	echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">', "\n";
-	echo '<html>', "\n";
+	echo '<html xmlns="http://www.w3.org/1999/xhtml">', "\n";
 }
 
 /**
