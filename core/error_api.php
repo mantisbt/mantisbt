7f1534f93b1d7eb2f4333008961136ce00d27212
John Reese <jreese@leetcode.net>
2010-05-17 21:27:29 -0400

    Fix edge case with event_clear_callbacks
    In cases where an error would occur before Mantis had fully
    bootstrapped, it was possible for the error API to generate misleading
    errors when the event API had not yet been loaded.  By wrapping the call
    to event_clear_callbacks() with a check for function_exists(), it should
    fix this sort of behavior.

--- core/error_api.php
+++ core/error_api.php
@@ -132,7 +132,9 @@ function error_handler( $p_type, $p_error, $p_file, $p_line, $p_context ) {
 	switch( $t_method ) {
 		case 'halt':
 			# disable any further event callbacks
-			event_clear_callbacks();
+			if ( function_exists( 'event_clear_callbacks' ) ) {
+				event_clear_callbacks();
+			}
 
 			$t_oblen = ob_get_length();
 			if( error_handled() && $t_oblen > 0 ) {
