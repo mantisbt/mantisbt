bba266b10624034e31c61c90b802d7f4ca077d9e
David Hicks <hickseydr@optusnet.com.au>
2010-08-07 15:54:27 +1000

    Issue #11826: Add dynamic JavaScript configuration script
    This script is loaded prior to any other scripts and will allow all
    scripts to read selected MantisBT configuration values.

---
+++ javascript_config.php
@@ -0,0 +1,36 @@
+<?php
+# MantisBT - A PHP based bugtracking system
+
+# MantisBT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# MantisBT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package MantisBT
+ * @copyright Copyright (C) 2000 - 2002  Kenzaburo Ito - kenito@300baud.org
+ * @copyright Copyright (C) 2002 - 2010  MantisBT Team - mantisbt-dev@lists.sourceforge.net
+ * @link http://www.mantisbt.org
+ *
+ * @uses config_api.php
+ */
+
+require_once( 'core.php' );
+require_api( 'config_api.php' );
+
+function print_config_value( $p_config_key ) {
+	echo "config['" . $p_config_key . "'] = '" . addslashes( config_get( $p_config_key ) ) . "';\n";
+}
+
+echo "var config = new Array();\n";
+print_config_value( 'calendar_js_date_format' );
+print_config_value( 'icon_path' );
+print_config_value( 'short_path' );
