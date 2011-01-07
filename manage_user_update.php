mantisbt/mantisbt

commit 7672ca3d7f0008fd582e9f58a308fe1005d7d33c

Fix #11351: Real name and email should not be updated via GPC (LDAP)
When LDAP is being used for retrieving the user real name and/or email
address we should not provide any way for MantisBT forms to update these
fields manually via GPC parameters to account_update and
manage_user_update. Thus we don't need (and never wanted to) send hidden
fields with forms containing the current LDAP details. A user could
maliciously change these values by crafting their own HTTP POST queries
to the server.

manage_user_update.php
@@ -71,24 +71,34 @@
 		trigger_error( ERROR_USER_NAME_NOT_UNIQUE, ERROR );
 	}
 
-    # strip extra space from real name
-    $t_realname = string_normalize( $f_realname );
-
 	user_ensure_name_valid( $f_username );
-	user_ensure_realname_valid( $f_realname );
-	user_ensure_realname_unique( $f_username, $f_realname );
 
-	$f_email = email_append_domain( $f_email );
-	email_ensure_valid( $f_email );
-	email_ensure_not_disposable( $f_email );
+	$t_ldap = ( LDAP == config_get( 'login_method' ) );
 
-	$c_email		= $f_email;
-	$c_username		= $f_username;
-	$c_realname		= $t_realname;
-	$c_protected	= db_prepare_bool( $f_protected );
-	$c_enabled		= db_prepare_bool( $f_enabled );
-	$c_user_id		= db_prepare_int( $f_user_id );
-	$c_access_level	= db_prepare_int( $f_access_level );
+	if ( $t_ldap && config_get( 'use_ldap_realname' ) ) {
+		$t_realname = ldap_realname_from_username( $f_username );
+	} else {
+		# strip extra space from real name
+		$t_realname = string_normalize( $f_realname );
+		user_ensure_realname_valid( $t_realname );
+		user_ensure_realname_unique( $f_username, $t_realname );
+	}
+
+	if ( $t_ldap && config_get( 'use_ldap_email' ) ) {
+		$t_email = ldap_email( $f_user_id );
+	} else {
+		$t_email = email_append_domain( $f_email );
+		email_ensure_valid( $t_email );
+		email_ensure_not_disposable( $t_email );
+	}
+
+	$c_email = $t_email;
+	$c_username = $f_username;
+	$c_realname = $t_realname;
+	$c_protected = db_prepare_bool( $f_protected );
+	$c_enabled = db_prepare_bool( $f_enabled );
+	$c_user_id = db_prepare_int( $f_user_id );
+	$c_access_level = db_prepare_int( $f_access_level );
 
 	$t_user_table = db_get_table( 'mantis_user_table' );
 

account_page.php
@@ -114,7 +114,7 @@
 			<?php echo lang_get( 'username' ) ?>
 		</td>
 		<td width="75%">
-			<?php echo $u_username ?>
+			<?php echo string_display_line( $u_username ) ?>
 		</td>
 	</tr>
 
@@ -139,7 +139,7 @@
 			<?php echo lang_get( 'username' ) ?>
 		</td>
 		<td width="75%">
-			<?php echo $u_username ?>
+			<?php echo string_display_line( $u_username ) ?>
 		</td>
 	</tr>
 
@@ -186,9 +186,6 @@
 			// With LDAP
 			if ( $t_ldap && ON == config_get( 'use_ldap_email' ) ) {
 				echo string_display_line( $u_email );
-		?>
-			<input type="hidden" name="email" value="<?php echo string_attribute( $u_email ) ?>" />
-		<?php
 			}
 			// Without LDAP
 			else {
@@ -209,9 +206,6 @@
 			// With LDAP
 			if ( $t_ldap && ON == config_get( 'use_ldap_realname' ) ) {
 				echo string_display_line( ldap_realname_from_username( $u_username ) );
-		?>
-			<input type="hidden" name="realname" value="<?php echo string_attribute( ldap_realname_from_username( $u_username ) ) ?>" />
-		<?php
 			}
 			// Without LDAP
 			else {

account_update.php
@@ -48,13 +48,15 @@
 
 	$t_redirect = 'account_page.php';
 
+	/** @todo Listing what fields were updated is not standard behaviour of MantisBT - it also complicates the code. */
 	$t_email_updated = false;
 	$t_password_updated = false;
 	$t_realname_updated = false;
 
-	/** @todo Listing what fields were updated is not standard behaviour of MantisBT - it also complicates the code. */
+	$t_ldap = ( LDAP == config_get( 'login_method' ) );
 
-	if ( OFF == config_get( 'use_ldap_email' ) ) {
+	# Update email (but only if LDAP isn't being used)
+	if ( !( $t_ldap && config_get( 'use_ldap_email' ) ) ) {
 		$f_email = email_append_domain( $f_email );
 		email_ensure_valid( $f_email );
 		email_ensure_not_disposable( $f_email );
@@ -65,15 +67,18 @@
 		}
 	}
 
-    # strip extra spaces from real name
-    $t_realname = string_normalize( $f_realname );
-	if ( $t_realname != user_get_field( $t_user_id, 'realname' ) ) {
-		# checks for problems with realnames
-		user_ensure_realname_valid( $t_realname );
-		$t_username = user_get_field( $t_user_id, 'username' );
-		user_ensure_realname_unique( $t_username, $t_realname );
-		user_set_realname( $t_user_id, $t_realname );
-		$t_realname_updated = true;
+	# Update real name (but only if LDAP isn't being used)
+	if ( !( $t_ldap && config_get( 'use_ldap_realname' ) ) ) {
+		# strip extra spaces from real name
+		$t_realname = string_normalize( $f_realname );
+		if ( $t_realname != user_get_field( $t_user_id, 'realname' ) ) {
+			# checks for problems with realnames
+			user_ensure_realname_valid( $t_realname );
+			$t_username = user_get_field( $t_user_id, 'username' );
+			user_ensure_realname_unique( $t_username, $t_realname );
+			user_set_realname( $t_user_id, $t_realname );
+			$t_realname_updated = true;
+		}
 	}
 
 	# Update password if the two match and are not empty

manage_user_edit_page.php
@@ -91,9 +91,6 @@
 			// With LDAP
 			if ( $t_ldap && ON == config_get( 'use_ldap_realname' ) ) {
 				echo string_display_line( user_get_realname( $f_user_id ) );
-		?>
-			<input type="hidden" name="realname" value="<?php echo string_attribute( user_get_realname( $f_user_id ) ) ?>" />
-		<?php
 			}
 			// Without LDAP
 			else {
@@ -115,9 +112,6 @@
 			// With LDAP
 			if ( $t_ldap && ON == config_get( 'use_ldap_email' ) ) {
 				echo string_display_line( user_get_email( $f_user_id ) );
-		?>
-			<input type="hidden" name="email" value="<?php echo string_attribute( user_get_email( $f_user_id ) ) ?>" />
-		<?php
 			}
 			// Without LDAP
 			else {

commit 132fd5db7b2b57bf26902fa3210d80a5b2cc3d49

Fix #12646: notification email not sent to user when updating profile and LDAP=on
Problem was introduced in commit 7672ca3d7f000, changing from
$f_email to $t_email: one reference to $f_email was left out in the call to
email_store.

Signed-off-by: David Hicks <hickseydr@optusnet.com.au>

manage_user_update.php
@@ -50,7 +50,6 @@
 
 	$t_user = user_get_row( $f_user_id );
 
-	$f_email	= trim( $f_email );
 	$f_username	= trim( $f_username );
 
 	$t_old_username = $t_user['username'];
@@ -87,7 +86,7 @@
 	if ( $t_ldap && config_get( 'use_ldap_email' ) ) {
 		$t_email = ldap_email( $f_user_id );
 	} else {
-		$t_email = email_append_domain( $f_email );
+		$t_email = email_append_domain( trim( $f_email ) );
 		email_ensure_valid( $t_email );
 		email_ensure_not_disposable( $t_email );
 	}
@@ -165,7 +164,7 @@
 			$t_subject = '[' . config_get( 'window_title' ) . '] ' . lang_get( 'email_user_updated_subject' );
 			$t_updated_msg = lang_get( 'email_user_updated_msg' );
 			$t_message = $t_updated_msg . "\n\n" . config_get( 'path' ) . 'account_page.php' . "\n\n" . $t_changes;
-			email_store( $f_email, $t_subject, $t_message );
+			email_store( $t_email, $t_subject, $t_message );
 			log_event( LOG_EMAIL, sprintf( 'Account update notification sent to ' . $f_username . ' (' . $t_email . ')' ) );
 			if ( config_get( 'email_send_using_cronjob' ) == OFF ) {
 				email_send_all();