mantisbt/mantisbt

commit f081ed1f0171435e5a61880d46ca2e95a8c6b01e

Fix #12641: Internet Explorer does not support xhtml+xml MIME type
Internet Explorer prior to version 9 does not support the
application/xhtml+xml MIME type and will therefore not render pages
served up in this way.

We should check the "Accept" header sent by the browser to ensure that
the browser can handle application/xhtml+xml before we use this MIME
type. Otherwise the default should be to use the text/html MIME type for
older browsers.

Signed-off-by: David Hicks <hickseydr@optusnet.com.au>

core/http_api.php
@@ -120,7 +120,15 @@ function http_caching_headers( $p_allow_caching=false ) {
  */
 function http_content_headers() {
 	if ( !headers_sent() ) {
-		header( 'Content-Type: application/xhtml+xml; charset=UTF-8' );
+		// Only use the application/xhtml+xml MIME type if the browser
+		// has indicated support for this type. Internet Explorer
+		// prior to version 9 only supports the text/html MIME type.
+		if ( stristr( $_SERVER[HTTP_ACCEPT], 'application/xhtml+xml' ) ) {
+			header( 'Content-Type: application/xhtml+xml; charset=UTF-8' );
+		} else {
+			header( 'Content-Type: text/html; charset=UTF-8' );
+		}
+
 		// Disallow Internet Explorer from attempting to second guess the Content-Type
 		// header as per http://blogs.msdn.com/ie/archive/2008/07/02/ie8-security-part-v-comprehensive-protection.aspx
 		header( 'X-Content-Type-Options: nosniff' );

commit 8d464c02df511fe49185627a23a31d2fe0f322e3

Fix #12641: HTTP_ACCEPT is not a constant
Commit f081ed1f0171435e5a61880d46ca2e95a8c6b01e accidentally referenced
$_SERVER[HTTP_ACCEPT] where HTTP_ACCEPT is not a valid constant. It
should be $_SERVER['HTTP_ACCEPT'] instead (ie. the array key is a
string).

core/http_api.php
@@ -123,7 +123,7 @@ function http_content_headers() {
 		// Only use the application/xhtml+xml MIME type if the browser
 		// has indicated support for this type. Internet Explorer
 		// prior to version 9 only supports the text/html MIME type.
-		if ( stristr( $_SERVER[HTTP_ACCEPT], 'application/xhtml+xml' ) ) {
+		if ( stristr( $_SERVER['HTTP_ACCEPT'], 'application/xhtml+xml' ) ) {
 			header( 'Content-Type: application/xhtml+xml; charset=UTF-8' );
 		} else {
 			header( 'Content-Type: text/html; charset=UTF-8' );