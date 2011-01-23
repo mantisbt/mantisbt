b6dd45e605695ce4be8c73995fa4f0760255de3c
Roland Becker <roland@atrol.de>
2011-01-23 16:26:09 +0100

    Fix validation error at "Workflow Thresholds" page

--- manage_config_work_threshold_page.php
+++ manage_config_work_threshold_page.php
@@ -80,7 +80,7 @@ function get_section_begin_mcwt( $p_section_name ) {
 	echo '<table class="width100">';
 	echo '<tr><td class="form-title" colspan="' . ( count( $t_access_levels ) + 2 ) . '">' . $p_section_name . '</td></tr>' . "\n";
 	echo '<tr><td class="form-title" width="40%" rowspan="2">' . lang_get( 'perm_rpt_capability' ) . '</td>';
-	echo '<td class="form-title"style="text-align:center"  width="40%" colspan="' . count( $t_access_levels ) . '">' . lang_get( 'access_levels' ) . '</td>';
+	echo '<td class="form-title" style="text-align:center"  width="40%" colspan="' . count( $t_access_levels ) . '">' . lang_get( 'access_levels' ) . '</td>';
 	echo '<td class="form-title" style="text-align:center" rowspan="2">&#160;' . lang_get( 'alter_level' ) . '&#160;</td></tr><tr>';
 	foreach( $t_access_levels as $t_access_level => $t_access_label ) {
 		echo '<td class="form-title" style="text-align:center">&#160;' . MantisEnum::getLabel( lang_get( 'access_levels_enum_string' ), $t_access_level ) . '&#160;</td>';
