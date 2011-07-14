0c6d013558abb08ff4dcd8042361e6c2ffa68c92
Damien Regad <damien.regad@merckgroup.com>
2011-07-14 19:22:46 +0200

    Fix #7952: Add a label next to time spent in bug notes
    Also reduced the font size back to normal, because with the added label the
    large font put too much emphasis on the time spent vs the bugnote text.

--- bugnote_view_inc.php
+++ bugnote_view_inc.php
@@ -186,7 +186,7 @@ $num_notes = count( $t_bugnotes );
 					break;
 				case TIME_TRACKING:
 					if ( access_has_bug_level( config_get( 'time_tracking_view_threshold' ), $f_bug_id ) ) {
-						echo '<b><big>', $t_time_tracking_hhmm, '</big></b><br /><br />';
+						echo '<b>', lang_get( 'time_tracking_time_spent' ) . ' ' . $t_time_tracking_hhmm, '</b><br /><br />';
 					}
 					break;
 			}
