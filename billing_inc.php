mantisbt/mantisbt

commit c4378b821a54d7ef30184d3144459b18673c1f42

Fix #13132: Print the time tracking information sorted by username
Previously the list was printed in whatever order returned by the SQL

billing_inc.php
@@ -126,16 +126,24 @@ if ( !config_get('time_tracking_enabled') )
 
 <?php
 	if ( !is_blank( $f_get_bugnote_stats_button ) ) {
+		# Retrieve time tracking information
 		$t_from = "$t_bugnote_stats_from_y-$t_bugnote_stats_from_m-$t_bugnote_stats_from_d";
 		$t_to = "$t_bugnote_stats_to_y-$t_bugnote_stats_to_m-$t_bugnote_stats_to_d";
 		$t_bugnote_stats = bugnote_stats_get_project_array( $f_project_id, $t_from, $t_to, $f_bugnote_cost );
 
+		# Sort the array by bug_id, user/real name
 		if ( ON == config_get( 'show_realname' ) ) {
 			$t_name_field = 'realname';
 		}
 		else {
 			$t_name_field = 'username';
 		}
+		foreach ( $t_bugnote_stats as $t_key => $t_item ) {
+			$t_sort_bug[$t_key] = $t_item['bug_id'];
+			$t_sort_name[$t_key] = $t_item[$t_name_field];
+		}
+		array_multisort( $t_sort_bug, SORT_NUMERIC, $t_sort_name, $t_bugnote_stats );
+		unset( $t_sort_bug, $t_sort_name );
 
 		if ( is_blank( $f_bugnote_cost ) || ( (double)$f_bugnote_cost == 0 ) ) {
 			$t_cost_col = false;

bugnote_stats_inc.php
@@ -112,12 +112,18 @@ if ( ON != config_get('time_tracking_enabled') ) {
 		$t_to = "$t_bugnote_stats_to_y-$t_bugnote_stats_to_m-$t_bugnote_stats_to_d";
 		$t_bugnote_stats = bugnote_stats_get_events_array( $f_bug_id, $t_from, $t_to );
 
+		# Sort the array by user/real name
 		if ( ON == config_get( 'show_realname' ) ) {
 			$t_name_field = 'realname';
 		}
 		else {
 			$t_name_field = 'username';
 		}
+		foreach ( $t_bugnote_stats as $t_key => $t_item ) {
+			$t_sort_name[$t_key] = $t_item[$t_name_field];
+		}
+		array_multisort( $t_sort_name, $t_bugnote_stats );
+		unset( $t_sort_name );
 ?>
 <br />
 <table border=0 class="width100" cellspacing="0">

commit a8bc7ff0c1504649de21429ce3ad6dffcf355b42

Fix #0013150: Getting time tracking info when none exists generates warning
This was caused by sort key arrays not being initialized before the foreach
loop, resulting in a call to array_multisort with an undefined variable.

Bug was introduced by commit c4378b821a54d7ef30184d3144459b18673c1f42

billing_inc.php
@@ -138,6 +138,7 @@ if ( !config_get('time_tracking_enabled') )
 		else {
 			$t_name_field = 'username';
 		}
+		$t_sort_bug = $t_sort_name = array();
 		foreach ( $t_bugnote_stats as $t_key => $t_item ) {
 			$t_sort_bug[$t_key] = $t_item['bug_id'];
 			$t_sort_name[$t_key] = $t_item[$t_name_field];

bugnote_stats_inc.php
@@ -119,6 +119,7 @@ if ( ON != config_get('time_tracking_enabled') ) {
 		else {
 			$t_name_field = 'username';
 		}
+		$t_sort_name = array();
 		foreach ( $t_bugnote_stats as $t_key => $t_item ) {
 			$t_sort_name[$t_key] = $t_item[$t_name_field];
 		}