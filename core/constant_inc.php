89738d55b8e3c261d2a69e816b236d63d1a6c206
John Reese <john@noswap.com>
2011-07-26 09:36:57 -0400

    Bump

--- core/constant_inc.php
+++ core/constant_inc.php
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
 
-define( 'MANTIS_VERSION', '1.2.5' );
+define( 'MANTIS_VERSION', '1.2.6' );
 
 # --- constants -------------------
 # magic numbers
