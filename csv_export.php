277681c1f575d64b5bf9fa83c9b39ac586959037
Robert Munteanu <robert.munteanu@gmail.com>
2011-10-06 01:50:02 +0300

    Fix #13370 : CSV export specifies incorrect MIME type
    Conflicts:
    	csv_export.php

--- csv_export.php
+++ csv_export.php
@@ -69,7 +69,7 @@ $t_filename = csv_get_default_filename();
 # Make sure that IE can download the attachments under https.
 header( 'Pragma: public' );
 
-header( 'Content-Type: text/plain; name=' . urlencode( file_clean_name( $t_filename ) ) );
+header( 'Content-Type: text/csv; name=' . urlencode( file_clean_name( $t_filename ) ) );
 header( 'Content-Transfer-Encoding: BASE64;' );
 
 # Added Quotes (") around file name.
