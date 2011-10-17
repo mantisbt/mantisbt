58c0cd9af0404a0104b9332371e359b5ed3605ed
Paul Richards <paul@mantisforge.org>
2011-10-17 17:57:41 +0100

    silence deprecated warning

--- admin/check/check_php_inc.php
+++ admin/check/check_php_inc.php
@@ -81,7 +81,7 @@ check_print_test_row(
 
 check_print_test_row(
 	'magic_quotes_runtime php.ini directive is disabled',
-	!( function_exists( 'get_magic_quotes_runtime' ) && get_magic_quotes_runtime() ),
+	!( function_exists( 'get_magic_quotes_runtime' ) && @get_magic_quotes_runtime() ),
 	array( false => 'PHP\'s magic quotes feature is <a href="http://www.php.net/manual/en/security.magicquotes.whynot.php">deprecated in PHP 5.3.0</a> and should not be used.' )
 );
 
