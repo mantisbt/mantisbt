mantisbt/mantisbt

commit ce56bd5706ebf98a2b8a55624e4b492a52ae8a05

Obsolete $g_show_attachment_indicator option
Following discussion with dhx on IRC on 31-Aug-2011, this setting is
not necessary.

There is no point or advantage in displaying a paperclip bitmap as opposed to
the actual number of attachments; furthermore, the option was inconsistently
used, in one instance it was actually hiding the attachments column completely

Affects issue #13276

Porting 3323204d0c3e6031b182b7113e7afb99d861588d to master

core/helper_api.php
@@ -494,7 +494,7 @@ function helper_get_columns_to_view( $p_columns_target = COLUMNS_TARGET_VIEW_PAG
 		$t_keys_to_remove[] = 'overdue';
 	}
 
-	if( $p_columns_target == COLUMNS_TARGET_CSV_PAGE || $p_columns_target == COLUMNS_TARGET_EXCEL_PAGE || OFF == config_get( 'show_attachment_indicator' ) ) {
+	if( $p_columns_target == COLUMNS_TARGET_CSV_PAGE || $p_columns_target == COLUMNS_TARGET_EXCEL_PAGE ) ) {
 		$t_keys_to_remove[] = 'attachment_count';
 	}
 

config_defaults_inc.php
@@ -2063,16 +2063,6 @@ $g_preview_max_width = 0;
 $g_preview_max_height = 250;
 
 /**
- * Show an attachment indicator on bug list. Show a clickable attachment
- * indicator on the bug list page if the bug has one or more files attached.
- * Note: This option is disabled by default since it adds 1 database query per
- * bug listed and thus might slow down the page display.
- *
- * @global int $g_show_attachment_indicator
- */
-$g_show_attachment_indicator = OFF;
-
-/**
  * access level needed to view bugs attachments.  View means to see the file
  * names, sizes, and timestamps of the attachments.
  * @global int $g_view_attachments_threshold

core/columns_api.php
@@ -1074,12 +1074,7 @@ function print_column_attachment_count( $p_bug, $p_columns_target = COLUMNS_TARG
 	if ( $t_attachment_count > 0 ) {
 		$t_href = string_get_bug_view_url( $p_bug->id ) . '#attachments';
 		$t_href_title = sprintf( lang_get( 'view_attachments_for_issue' ), $t_attachment_count, $p_bug->id );
-		if ( config_get( 'show_attachment_indicator' ) ) {
-			$t_alt_text = $t_attachment_count . lang_get( 'word_separator' ) . lang_get( 'attachments' );
-			echo "<a href=\"$t_href\" title=\"$t_href_title\"><img src=\"${t_icon_path}attachment.png\" alt=\"$t_alt_text\" title=\"$t_alt_text\" /></a>";
-		} else {
-			echo "<a href=\"$t_href\" title=\"$t_href_title\">$t_attachment_count</a>";
-		}
+		echo "<a href=\"$t_href\" title=\"$t_href_title\">$t_attachment_count</a>";
 	} else {
 		echo ' &#160; ';
 	}

core/obsolete.php
@@ -139,7 +139,10 @@ config_obsolete( 'default_graph_type', '' );
 config_obsolete( 'graph_font', '' );
 config_obsolete( 'graph_colors', '' );
 
-#changes in 1.3.0dev
+# changes in 1.2.8
+config_obsolete( 'show_attachment_indicator' );
+
+# changes in 1.3.0dev
 config_obsolete( 'bugnote_allow_user_edit_delete', '' );
 config_obsolete( 'password_confirm_hash_magic_string', 'crypto_master_salt' );
 config_obsolete( 'rss_key_seed', 'crypto_master_salt' );

commit 05947d3556a6a4549a79b6275e5c131526981b32

Fix syntax error
Introduced in ce56bd5706ebf98a2b8a55624e4b492a52ae8a05

core/helper_api.php
@@ -494,7 +494,7 @@ function helper_get_columns_to_view( $p_columns_target = COLUMNS_TARGET_VIEW_PAG
 		$t_keys_to_remove[] = 'overdue';
 	}
 
-	if( $p_columns_target == COLUMNS_TARGET_CSV_PAGE || $p_columns_target == COLUMNS_TARGET_EXCEL_PAGE ) ) {
+	if( $p_columns_target == COLUMNS_TARGET_CSV_PAGE || $p_columns_target == COLUMNS_TARGET_EXCEL_PAGE ) {
 		$t_keys_to_remove[] = 'attachment_count';
 	}
 