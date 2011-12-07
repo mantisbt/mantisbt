dd286bf898f3e9ee330f7c4512cf5ca9437d521e
Robert Munteanu <robert.munteanu@gmail.com>
2011-12-07 21:45:25 +0200

    Fix #12029 : minimum length of custom fields of type numeric is not checked

--- core/custom_field_api.php
+++ core/custom_field_api.php
@@ -1171,10 +1171,20 @@ function custom_field_validate( $p_field_id, $p_value ) {
 			break;
 		case CUSTOM_FIELD_TYPE_NUMERIC:
 			$t_valid &= ( $t_length == 0 ) || is_numeric( $p_value );
+			
+			# Check the length of the number
+			$t_valid &= ( 0 == $t_length_min ) || ( $t_length >= $t_length_min );
+			$t_valid &= ( 0 == $t_length_max ) || ( $t_length <= $t_length_max );
+			
 			break;
 		case CUSTOM_FIELD_TYPE_FLOAT:
 			# Allow both integer and float numbers
 			$t_valid &= ( $t_length == 0 ) || is_numeric( $p_value ) || is_float( $p_value );
+			
+			# Check the length of the number
+			$t_valid &= ( 0 == $t_length_min ) || ( $t_length >= $t_length_min );
+			$t_valid &= ( 0 == $t_length_max ) || ( $t_length <= $t_length_max );
+			
 			break;
 		case CUSTOM_FIELD_TYPE_DATE:
 			# gpc_get_cf for date returns the value from strftime
