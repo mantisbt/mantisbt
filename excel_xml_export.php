88d7fa689e864ac446d95b41daedfc31e0db66b0
Damien Regad <damien.regad@merckgroup.com>
2012-01-19 11:26:02 +0100

    Fix #13765: infinite loop in Excel export
    If the number of records in the View Issues page was a multiple of 100,
    the Excel export ended up in an infinite loop.
    
    Instead of testing on the number of rows returned vs the number of rows
    per page, the code now uses the current page number vs the total number
    of pages returned by filter_get_bug_rows() to set the loop's exit flag.

--- excel_xml_export.php
+++ excel_xml_export.php
@@ -55,7 +55,7 @@
 	if ( $result === false ) {
 		print_header_redirect( 'view_all_set.php?type=0&print=1' );
 	}
-	
+
 	# pre-cache custom column data
 	columns_plugin_cache_issue_data( $result );
 
@@ -107,14 +107,13 @@
 			} #in_array
 		} #for loop
 
-		// If got a full page, then attempt for the next one.
+		// Get the next page if we are not processing the last one
 		// @@@ Note that since we are not using a transaction, there is a risk that we get a duplicate record or we miss
 		// one due to a submit or update that happens in parallel.
-		if ( $t_row_count == $t_per_page ) {
+		if ( $t_page_number < $t_page_count ) {
 			$t_page_number++;
 			$t_bug_count = null;
 			$t_page_count = null;
-
 			$result = filter_get_bug_rows( $t_page_number, $t_per_page, $t_page_count, $t_bug_count );
 			if ( $result === false ) {
 				$t_more = false;
