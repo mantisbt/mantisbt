mantisbt/mantisbt

commit 735506cfc14aab75cacb3adaf79d21a16dfe3992

More fixes of custom enum types across projects
Fixes #13728

core/columns_api.php
@@ -1082,7 +1082,7 @@ function print_column_severity( $p_bug, $p_columns_target = COLUMNS_TARGET_VIEW_
  * @access public
  */
 function print_column_eta( $p_bug, $p_columns_target = COLUMNS_TARGET_VIEW_PAGE ) {
-	echo '<td class="center">', get_enum_element( 'eta', $p_bug->eta ), '</td>';
+	echo '<td class="center">', get_enum_element( 'eta', $p_bug->eta, auth_get_current_user_id(), $t_bug->project_id ), '</td>';
 }
 
 /**
@@ -1093,7 +1093,7 @@ function print_column_eta( $p_bug, $p_columns_target = COLUMNS_TARGET_VIEW_PAGE 
  * @access public
  */
 function print_column_projection( $p_bug, $p_columns_target = COLUMNS_TARGET_VIEW_PAGE ) {
-	echo '<td class="center">', get_enum_element( 'projection', $p_bug->projection ), '</td>';
+	echo '<td class="center">', get_enum_element( 'projection', $p_bug->projection, auth_get_current_user_id(), $t_bug->project_id ), '</td>';
 }
 
 /**
@@ -1104,7 +1104,7 @@ function print_column_projection( $p_bug, $p_columns_target = COLUMNS_TARGET_VIE
  * @access public
  */
 function print_column_reproducibility( $p_bug, $p_columns_target = COLUMNS_TARGET_VIEW_PAGE ) {
-	echo '<td class="center">', get_enum_element( 'reproducibility', $p_bug->reproducibility ), '</td>';
+	echo '<td class="center">', get_enum_element( 'reproducibility', $p_bug->reproducibility, auth_get_current_user_id(), $t_bug->project_id ), '</td>';
 }
 
 /**

account_sponsor_page.php
@@ -128,7 +128,7 @@
 
 			# describe bug
 			$t_status = string_attribute( get_enum_element( 'status', $t_bug->status, auth_get_current_user_id(), $t_bug->project_id ) );
-			$t_resolution = string_attribute( get_enum_element( 'resolution', $t_bug->resolution ) );
+			$t_resolution = string_attribute( get_enum_element( 'resolution', $t_bug->resolution, auth_get_current_user_id(), $t_bug->project_id ) );
 			$t_version_id = version_get_id( $t_bug->fixed_in_version, $t_project );
 			if ( ( false !== $t_version_id ) && ( VERSION_RELEASED == version_get_field( $t_version_id, 'released' ) ) ) {
 				$t_released_label = '<a title="' . lang_get( 'released' ) . '">' . $t_bug->fixed_in_version . '</a>';
@@ -238,7 +238,7 @@
 
 			# describe bug
 			$t_status = string_attribute( get_enum_element( 'status', $t_bug->status, auth_get_current_user_id(), $t_bug->project_id ) );
-			$t_resolution = string_attribute( get_enum_element( 'resolution', $t_bug->resolution ) );
+			$t_resolution = string_attribute( get_enum_element( 'resolution', $t_bug->resolution, auth_get_current_user_id(), $t_bug->project_id ) );
 			$t_version_id = version_get_id( $t_bug->fixed_in_version, $t_project );
 			if ( ( false !== $t_version_id ) && ( VERSION_RELEASED == version_get_field( $t_version_id, 'released' ) ) ) {
 				$t_released_label = '<a title="' . lang_get( 'released' ) . '">' . $t_bug->fixed_in_version . '</a>';

core/relationship_api.php
@@ -626,7 +626,7 @@ function relationship_get_details( $p_bug_id, $p_relationship, $p_html = false, 
 	# get the information from the related bug and prepare the link
 	$t_bug = bug_get( $t_related_bug_id, false );
 	$t_status_string = get_enum_element( 'status', $t_bug->status, auth_get_current_user_id(), $t_bug->project_id );
-	$t_resolution_string = get_enum_element( 'resolution', $t_bug->resolution );
+	$t_resolution_string = get_enum_element( 'resolution', $t_bug->resolution, auth_get_current_user_id(), $t_bug->project_id );
 
 	$t_relationship_info_html = $t_td . string_no_break( $t_relationship_descr ) . '&#160;</td>';
 	if( $p_html_preview == false ) {

commit 8d115d9fdeefd5c07086bb95261dadb2163339a9

Fix use of undefined $t_bug variable instead of $p_bug
Copy/paste error introduced in 735506cfc14aab75cacb3adaf79d21a16dfe3992

Affects #11323

core/columns_api.php
@@ -1082,7 +1082,7 @@ function print_column_severity( $p_bug, $p_columns_target = COLUMNS_TARGET_VIEW_
  * @access public
  */
 function print_column_eta( $p_bug, $p_columns_target = COLUMNS_TARGET_VIEW_PAGE ) {
-	echo '<td class="center">', get_enum_element( 'eta', $p_bug->eta, auth_get_current_user_id(), $t_bug->project_id ), '</td>';
+	echo '<td class="center">', get_enum_element( 'eta', $p_bug->eta, auth_get_current_user_id(), $p_bug->project_id ), '</td>';
 }
 
 /**
@@ -1093,7 +1093,7 @@ function print_column_eta( $p_bug, $p_columns_target = COLUMNS_TARGET_VIEW_PAGE 
  * @access public
  */
 function print_column_projection( $p_bug, $p_columns_target = COLUMNS_TARGET_VIEW_PAGE ) {
-	echo '<td class="center">', get_enum_element( 'projection', $p_bug->projection, auth_get_current_user_id(), $t_bug->project_id ), '</td>';
+	echo '<td class="center">', get_enum_element( 'projection', $p_bug->projection, auth_get_current_user_id(), $p_bug->project_id ), '</td>';
 }
 
 /**
@@ -1104,7 +1104,7 @@ function print_column_projection( $p_bug, $p_columns_target = COLUMNS_TARGET_VIE
  * @access public
  */
 function print_column_reproducibility( $p_bug, $p_columns_target = COLUMNS_TARGET_VIEW_PAGE ) {
-	echo '<td class="center">', get_enum_element( 'reproducibility', $p_bug->reproducibility, auth_get_current_user_id(), $t_bug->project_id ), '</td>';
+	echo '<td class="center">', get_enum_element( 'reproducibility', $p_bug->reproducibility, auth_get_current_user_id(), $p_bug->project_id ), '</td>';
 }
 
 /**