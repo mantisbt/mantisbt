mantisbt/mantisbt

commit a68398245663d25229800638b6965ffe6e7ab0d3

Prevent unwanted clearing version fields when editing bugs
When editing issues having Version information set to some value that is
no longer available in the selection list (i.e. because the version has
been marked as obsolete), Mantis defaulted the corresponding Version
field to blank in the bug_update_page. If the user then saved the bug,
the version information was consequently cleared in the DB, possibly
without the user even noticing the change.

This commit ensures that the version selection list always includes the
currently set version to avoid this behavior.

Fixes #13096

core/print_api.php
@@ -764,6 +764,11 @@ function print_version_option_list( $p_version = '', $p_project_id = null, $p_re
 		null );
 	}
 
+	# Ensure the current version (if specified) is included in the list
+	if( !empty( $p_version ) ) {
+		$versions[] = version_cache_row( version_get_id( $p_version, $c_project_id ) );
+	}
+
 	if( $p_leading_blank ) {
 		echo '<option value=""></option>';
 	}

commit 081ca2874b96dcde64d9911e339971d0f91ecd65

Avoid error when editing bug with invalid version
When editing an issue having the Product Version field set to a version
number which is not defined in the issue's current project (which can
happen when the issue has been moved from another project, or a version
has been deleted from the current project), APPLICATION ERROR 1601 -
Version "" not found occurs.

The error occurs because the version field in the bug table is not
cleared when the issue is moved, so when Mantis is building the version
selection list to edit the bug, it tries to add the version defined in
it to the list, which triggers the error since that version does not
exist in the current project.

Regression caused by commit a68398245663d25229800638b6965ffe6e7ab0d3

Fixes #13938

core/print_api.php
@@ -768,7 +768,10 @@ function print_version_option_list( $p_version = '', $p_project_id = null, $p_re
 	# Note: Filter API specifies selected versions as an array
 	if( !is_array( $p_version ) ) {
 		if( !empty( $p_version ) ) {
-			$versions[] = version_cache_row( version_get_id( $p_version, $c_project_id ) );
+			$t_version_id = version_get_id( $p_version, $c_project_id );
+			if( $t_version_id !== false ) {
+				$versions[] = version_cache_row( t_version_id );
+			}
 		}
 	}
 