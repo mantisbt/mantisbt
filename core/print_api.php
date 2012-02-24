mantisbt/mantisbt

commit 081ca2874b96dcde64d9911e339971d0f91ecd65

Avoid error when editing bug with invalid version
When editing an issue having the Product Version field set to a version
number which is not defined in the issue's current project (which can
happen when the issue has been moved from another project, or a version
has been deleted from the current project), APPLICATION ERROR 1601 -
Version "" not found occurs.

The error occurs because the version field in the bug table is not
cleared when the issue is moved, so when Mantis is building the version
selection list to edit the bug, it tries to add the version defined in
it to the list, which triggers the error since that version does not
exist in the current project.

Regression caused by commit a68398245663d25229800638b6965ffe6e7ab0d3

Fixes #13938

core/print_api.php
@@ -768,7 +768,10 @@ function print_version_option_list( $p_version = '', $p_project_id = null, $p_re
 	# Note: Filter API specifies selected versions as an array
 	if( !is_array( $p_version ) ) {
 		if( !empty( $p_version ) ) {
-			$versions[] = version_cache_row( version_get_id( $p_version, $c_project_id ) );
+			$t_version_id = version_get_id( $p_version, $c_project_id );
+			if( $t_version_id !== false ) {
+				$versions[] = version_cache_row( t_version_id );
+			}
 		}
 	}
 

commit 37c2b0acd801a3cc650d496a4cc9770df2f0b01a

Fix typo
Added missing '$' in front of variable name
Error introduced by 081ca2874b96dcde64d9911e339971d0f91ecd65

Affects #13938

core/print_api.php
@@ -770,7 +770,7 @@ function print_version_option_list( $p_version = '', $p_project_id = null, $p_re
 		if( !empty( $p_version ) ) {
 			$t_version_id = version_get_id( $p_version, $c_project_id );
 			if( $t_version_id !== false ) {
-				$versions[] = version_cache_row( t_version_id );
+				$versions[] = version_cache_row( $t_version_id );
 			}
 		}
 	}