mantisbt/mantisbt

commit 169b882b97b0c6cf1babbf072ce326f0779c6c0b

Fix #12772: Add function to handle CSV formatting of sponsorship totals
When exporting issues in CSV format a fatal error is thrown if the
export includes sponsorship totals. This is due to a missing
csv_format_sponsorship_total function.

Signed-off-by: David Hicks <hickseydr@optusnet.com.au>

core/csv_api.php
@@ -412,3 +412,13 @@ function csv_format_due_date( $p_due_date ) {
 		$s_date_format = config_get( 'short_date_format' );
 	return csv_escape_string( date( $s_date_format, $p_due_date ) );
 }
+
+/**
+ * return the sponsorship total for an issue
+ * @param int $p_sponsorship_total
+ * @return string
+ * @access public
+ */
+function csv_format_sponsorship_total( $p_sponsorship_total ) {
+	return number_format( $p_sponsorship_total );
+}

commit 1d2a0355f2c61a9bf26869b6678cb5d4a8947781

Extra column in csv export when sponsorship_total >= 1000
Bug introduced by commit 169b882b97b0c6cf1babbf072ce326f0779c6c0b.

Due to the use of number_format() function with default parameters, PHP
outputs the sponsorship amount formatted with ',' as a thousands
separator, which also happens to be MantisBT's default csv separator.
Since the fields are not escaped, this causes an extra column to be
generated.

Call to number_format() was removed, as it makes more sense to output
raw data in csv exports.

Fixes #13958

core/csv_api.php
@@ -420,5 +420,5 @@ function csv_format_due_date( $p_bug ) {
  * @access public
  */
 function csv_format_sponsorship_total( $p_bug ) {
-	return number_format( $p_bug->sponsorship_total );
+	return csv_escape_string( $p_bug->sponsorship_total );
 }