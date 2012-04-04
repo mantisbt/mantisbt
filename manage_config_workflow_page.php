052606d21aa302142288bf5eceeaa1377ef56dac
Damien Regad <damien.regad@merckgroup.com>
2012-04-04 18:53:16 +0200

    Fix type check error in manage_config_workflow_page.php
    Strict type check in function check_selected() caused an error comparing
    false with the status level, so replaced with 0

--- manage_config_workflow_page.php
+++ manage_config_workflow_page.php
@@ -287,9 +287,9 @@ function access_row() {
 				}
 			}
 		} else {
-			$t_level = ( isset( $t_project_set[$t_status] ) ? $t_project_set[$t_status] : false );
-			$t_level_global = ( isset( $t_global_set[$t_status] ) ? $t_global_set[$t_status] : false );
-			$t_level_file = ( isset( $t_file_set[$t_status] ) ? $t_file_set[$t_status] : false );
+			$t_level = ( isset( $t_project_set[$t_status] ) ? $t_project_set[$t_status] : 0 );
+			$t_level_global = ( isset( $t_global_set[$t_status] ) ? $t_global_set[$t_status] : 0 );
+			$t_level_file = ( isset( $t_file_set[$t_status] ) ? $t_file_set[$t_status] : 0 );
 			$t_can_change = ( $t_access >= config_get_access( 'set_status_threshold' ) );
 			$t_colour = '';
 			if ( $t_level_global != $t_level_file ) {
