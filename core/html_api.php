7a45762d6122dbeef188075796314cd1b2ec1f2a
Victor Boctor <vboctor@gmail.com>
2012-05-06 12:55:24 -0700

    Upgrade jQuery and jQuery UI to latest versions.
    - jQuery UI from v1.8.14 to v1.8.20
    - jQuery from v1.6.2 to v1.7.2
    - Use filenames that reflect the version, makes versioning more explicit and avoids caching of such files across versions.
    - Added the non-minified version of jQuery for reference.

--- core/html_api.php
+++ core/html_api.php
@@ -354,7 +354,7 @@ function require_css( $p_stylesheet_path ) {
 function html_css() {
 	global $g_stylesheets_included;
 	html_css_link( config_get( 'css_include_file' ) );
-	html_css_link( 'jquery-ui.css' );
+	html_css_link( 'jquery-ui-1.8.20.custom.css' );
 	html_css_link( 'common_config.php' );
 	# Add right-to-left css if needed
 	if ( lang_get( 'directionality' ) == 'rtl' ) {
@@ -423,8 +423,8 @@ function html_head_javascript() {
 		global $g_scripts_included;
 		echo "\t<script type=\"text/javascript\" src=\"" . helper_mantis_url( 'javascript_config.php' ) . '"></script>' . "\n";
 		echo "\t<script type=\"text/javascript\" src=\"" . helper_mantis_url( 'javascript_translations.php' ) . '"></script>' . "\n";
-		html_javascript_link( 'jquery.js' );
-		html_javascript_link( 'jquery-ui.js' );
+		html_javascript_link( 'jquery-1.7.2.min.js' );
+		html_javascript_link( 'jquery-ui-1.8.20.custom.min.js' );
 		html_javascript_link( 'common.js' );
 		foreach ( $g_scripts_included as $t_script_path ) {
 			html_javascript_link( $t_script_path );
