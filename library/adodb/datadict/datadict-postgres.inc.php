3fc6200386a975a204f6951acba4067b40ecabd0
Damien Regad <damien.regad@merckgroup.com>
2012-06-20 14:17:16 +0200

    ADOdb pgsql fix: AlterColumnSQL conversion from bool to int
    When changing type of a column from boolean to integer, PostgreSQL does
    not provide implicit type cast, so the library needs to specify it via
    USING statement when generating the SQL·
    
    Porting part b) of commit 10871e5afc0bffc546b1a3d60bd7a459fa37e762
    
    This issue exists in ADOdb <= 5.17. Upstream bug report
    http://phplens.com/lens/lensforum/msgs.php?id=19204
    
    Fixes #14398

--- library/adodb/datadict/datadict-postgres.inc.php
+++ library/adodb/datadict/datadict-postgres.inc.php
@@ -188,6 +188,7 @@ class ADODB2_postgres extends ADODB_DataDict {
 	      $tabname = $this->TableName($tabname);
 	      $sql = array();
 	      list($lines,$pkey) = $this->_GenFields($flds);
+		  $set_null = false;
 	      $alter = 'ALTER TABLE ' . $tabname . $this->alterCol . ' ';
 	      foreach($lines as $v) {
 	         if ($not_null = preg_match('/NOT NULL/i',$v)) {
@@ -201,10 +202,19 @@ class ADODB2_postgres extends ADODB_DataDict {
 	         }
 
 	         if (preg_match('/^([^ ]+) .*DEFAULT ([^ ]+)/',$v,$matches)) {
-	            list(,$colname,$default) = $matches;
-	            $v = preg_replace('/^' . preg_quote($colname) . '\s/', '', $v);
-	            $sql[] = $alter . $colname . ' TYPE ' . str_replace('DEFAULT '.$default,'',$v);
-	            $sql[] = 'ALTER TABLE '.$tabname.' ALTER COLUMN '.$colname.' SET DEFAULT ' . $default;
+				$existing = $this->MetaColumns($tabname);
+				list(,$colname,$default) = $matches;
+				$v = preg_replace('/^' . preg_quote($colname) . '\s/', '', $v);
+				$t = trim(str_replace('DEFAULT '.$default,'',$v));
+				// Type change from bool to int
+				if ( $existing[strtoupper($colname)]->type == 'bool' && $t == 'INTEGER' ) {
+					$sql[] = 'ALTER TABLE '.$tabname.' ALTER COLUMN '.$colname.' DROP DEFAULT';
+					$sql[] = $alter . $colname . ' TYPE ' . $t . ' USING (' . $colname . '::BOOL)::INT ';
+					$sql[] = 'ALTER TABLE '.$tabname.' ALTER COLUMN '.$colname.' SET DEFAULT ' . $default;
+				} else {
+					$sql[] = $alter . $colname . ' TYPE ' . $t;
+					$sql[] = 'ALTER TABLE '.$tabname.' ALTER COLUMN '.$colname.' SET DEFAULT ' . $default;
+				}
 	         }
 	         else {
 	            // drop default?
