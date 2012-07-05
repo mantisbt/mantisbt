6da5164e35ffa80afeb8a04dcc4b8d2fa978f172
Damien Regad <damien.regad@merckgroup.com>
2012-07-05 17:07:52 +0200

    Fix email validation regex rejects addresses with single subdomain
    Per RFC 5322, e.g. user@localhost is a valid e-mail address but the
    previously implemented regex rejected it.
    
    Fixes #14453

--- core/email_api.php
+++ core/email_api.php
@@ -63,8 +63,18 @@ require_once( 'relationship_api.php' );
  * @return string
  */
 function email_regex_simple() {
-	return "/([a-z0-9!#*+\/=?^_{|}~-]+(?:\.[a-z0-9!#*+\/=?^_{|}~-]+)*)" . 				# recipient
-			"\@((?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)/i";	# @domain
+	static $s_email_regex = null;
+
+	if( is_null( $s_email_regex ) ) {
+		$t_recipient = "([a-z0-9!#*+\/=?^_{|}~-]+(?:\.[a-z0-9!#*+\/=?^_{|}~-]+)*)";
+
+		# a domain is one or more subdomains
+		$t_subdomain = "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)";
+		$t_domain    = "(${t_subdomain}(?:\.${t_subdomain})*)";
+
+		$s_email_regex = "/${t_recipient}\@${t_domain}/";
+	}
+	return $s_email_regex;
 }
 
 /**
