9051f84686f016e8b678b0ff7ce6141fddbd30ef
Robert Munteanu <robert@lmn.ro>
2012-08-04 15:31:55 +0300

    Fix #14550: Incorrect values for date_order field when using mc_project_version_add()

--- api/soap/mc_project_api.php
+++ api/soap/mc_project_api.php
@@ -321,7 +321,7 @@ function mc_project_version_add( $p_username, $p_password, $p_version ) {
 	if ( is_blank( $t_date_order ) ) 
 	    $t_date_order = null;
 	else 
-	    $t_date_order = date( "Y-m-d H:i:s", strtotime( $t_date_order ) );
+		$t_date_order = iso8601_to_timestamp($t_date_order);
 	
 	$t_obsolete = isset ( $p_version['obsolete'] ) ? $p_version['obsolete'] : false;
 	
@@ -389,7 +389,7 @@ function mc_project_version_update( $p_username, $p_password, $p_version_id, $p_
 	$t_name = $p_version['name'];
 	$t_released = $p_version['released'];
 	$t_description = $p_version['description'];
-	$t_date_order = $p_version['date_order'];
+	$t_date_order = isset ( $p_version['date_order']) ? iso8601_to_timestamp($p_version['date_order']) : null;
 	$t_obsolete = isset ( $p_version['obsolete'] ) ? $p_version['obsolete'] : false;
 
 	if ( is_blank( $t_project_id ) ) {
@@ -430,7 +430,7 @@ function mc_project_version_update( $p_username, $p_password, $p_version_id, $p_
 	$t_version_data->version = $t_name;
 	$t_version_data->description = $t_description;
 	$t_version_data->released = $t_released;
-	$t_version_data->date_order = date( "Y-m-d H:i:s", strtotime( $t_date_order ) );
+	$t_version_data->date_order = $t_date_order;
 	$t_version_data->obsolete = $t_obsolete;
 
 	return version_update( $t_version_data );
