bc3701b259748ef79a04c433fd8b1d1f958bd474
Kirill Krasnov <krasnovforum@gmail.com>
2012-09-21 16:57:13 +0300

    Renamed event to 'EVENT_VIEW_BUG_ATTACHMENT'

--- core/print_api.php
+++ core/print_api.php
@@ -1541,7 +1541,7 @@ function print_bug_attachment_header( $p_attachment ) {
 		}
 		echo lang_get( 'word_separator' ) . '(' . number_format( $p_attachment['size'] ) . lang_get( 'word_separator' ) . lang_get( 'bytes' ) . ')';
 		echo lang_get( 'word_separator' ) . '<span class="italic">' . date( config_get( 'normal_date_format' ), $p_attachment['date_added'] ) . '</span>';
-		event_signal('EVENT_PRINT_ATTACHMENT', array($p_attachment));
+		event_signal('EVENT_VIEW_BUG_ATTACHMENT', array($p_attachment));
 	} else {
 		print_file_icon( $p_attachment['display_name'] );
 		echo lang_get( 'word_separator' ) . '<span class="strike">' . string_display_line( $p_attachment['display_name'] ) . '</span>' . lang_get( 'word_separator' ) . '(' . lang_get( 'attachment_missing' ) . ')';
