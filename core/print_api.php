mantisbt/mantisbt

commit 935c106f7657bacfdac1f3ad5641a9d55f6f3ca3

Fixes #14756: mismatched types passed on to check_selected()
When trying to report an issue in a setup having subprojects, and no
specific project is selected (current project = 'All projects'), an
error is triggered in check_selected() due to mismatched types.

Statically casting the project_id as string to solve the problem.

Thanks to phyllisl for detecting and proposing a solution to this issue.

core/print_api.php
@@ -525,7 +525,7 @@ function print_subproject_option_list( $p_parent_id, $p_project_id = null, $p_fi
 			}
 			echo $t_full_id . '"';
 			if ( $p_project_id !== null ) {
-				check_selected( $p_project_id, $t_full_id );
+				check_selected( (string)$p_project_id, $t_full_id );
 			}
 			echo '>' . str_repeat( '&#160;', count( $p_parents ) ) . str_repeat( '&#187;', count( $p_parents ) ) . ' ' . string_attribute( project_get_field( $t_id, 'name' ) ) . '</option>' . "\n";
 			print_subproject_option_list( $t_id, $p_project_id, $p_filter_project_id, $p_trace, $p_parents );

commit 9f302620f17fb1b11ec6308d941c9b447128c23c

Fix XML error in adm_config_report.php
Regression introduced by 935c106f7657bacfdac1f3ad5641a9d55f6f3ca3.

Fixes #14756

core/print_api.php
@@ -525,7 +525,7 @@ function print_subproject_option_list( $p_parent_id, $p_project_id = null, $p_fi
 			}
 			echo $t_full_id . '"';
 			if ( $p_project_id !== null ) {
-				check_selected( (string)$p_project_id, $t_full_id );
+				check_selected( (string)$p_project_id, (string)$t_full_id );
 			}
 			echo '>' . str_repeat( '&#160;', count( $p_parents ) ) . str_repeat( '&#187;', count( $p_parents ) ) . ' ' . string_attribute( project_get_field( $t_id, 'name' ) ) . '</option>' . "\n";
 			print_subproject_option_list( $t_id, $p_project_id, $p_filter_project_id, $p_trace, $p_parents );