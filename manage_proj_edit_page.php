mantisbt/mantisbt

commit aec06762f253cfc5c8f3177fbbc77888f9b2ac80

Fix #9159: Added missing CSS 'button' class

manage_proj_edit_page.php
@@ -205,7 +205,7 @@ if ( access_has_global_level ( config_get( 'delete_project_threshold' ) ) ) { ?>
 					<option value="<?php echo $t_project['id'] ?>"><?php echo string_attribute( $t_project['name'] ) ?></option><?php
 				} # End looping over projects ?>
 				</select>
-				<input type="submit" value="<?php echo lang_get('add_subproject'); ?>" />
+				<input type="submit" class="button" value="<?php echo lang_get( 'add_subproject' ); ?>">
 			</fieldset>
 		</form>
 	<?php
@@ -258,7 +258,9 @@ if ( access_has_global_level ( config_get( 'delete_project_threshold' ) ) ) { ?>
 				</tr><?php
 				} # End of foreach loop over subprojects ?>
 			</table>
-			<span class="submit-button"><input type="submit" value="<?php echo lang_get( 'update_subproject_inheritance' ) ?>" /></span>
+			<span class="submit-button">
+				<input type="submit" class="button" value="<?php echo lang_get( 'update_subproject_inheritance' ) ?>" />
+			</span>
 		</fieldset>
 	</form><?php
 		# End of subprojects listing / update form

admin/install.php
@@ -531,7 +531,7 @@ if( !$g_database_upgrade ) {?>
 		Attempt Installation
 	</td>
 	<td>
-		<input name="go" type="submit" value="Install/Upgrade Database"></input>
+		<input name="go" type="submit" class="button" value="Install/Upgrade Database"></input>
 	</td>
 </tr>
 <input name="install" type="hidden" value="2"></input>
@@ -1020,8 +1020,8 @@ if( $g_failed ) {
 		<input name="admin_username" type="hidden" value="<?php echo $f_admin_username?>"></input>
 		<input name="admin_password" type="hidden" value="<?php echo $f_admin_password?>"></input>
 		<input name="log_queries" type="hidden" value="<?php echo( $f_log_queries ? 1 : 0 )?>"></input>
-		<input name="retry" type="submit" value="Retry"></input>
 		<input name="db_exists" type="hidden" value="<?php echo( $f_db_exists ? 1 : 0 )?>"></input>
+		<input name="retry" type="submit" class="button" value="Retry"></input>
 	</td>
 </tr>
 </table>

admin/test_email.php
@@ -64,7 +64,7 @@ html_page_top();
 ?>
 		<form method="post" action="<?php echo $_SERVER['SCRIPT_NAME']?>#email">
 		Email Address: <?php echo config_get_global( 'webmaster_email' );?><br />
-		<input type="submit" value="Send Mail" name="mail_test" />
+		<input type="submit" class="button" value="Send Mail" name="mail_test" />
 		</form>
 	</td>
 </tr>

manage_plugin_page.php
@@ -183,7 +183,9 @@ foreach ( $t_plugins_installed as $t_basename => $t_plugin ) {
 
 <tr>
 	<td colspan="3"></td>
-	<td colspan="2" class="center"><input type="submit" value="<?php echo lang_get( 'plugin_update' ) ?>"/></td>
+	<td colspan="2" class="center">
+		<input type="submit" class="button" value="<?php echo lang_get( 'plugin_update' ) ?>"/>
+	</td>
 	<td></td>
 </tr>
 

plugins/MantisGraph/pages/bug_graph_page.php
@@ -84,7 +84,7 @@ $t_show = array(
 						<?php echo get_dropdown( $t_show, 'show_table', $f_show_as_table ? 1 : 0 ); ?>
 					</td>
 					<td>
-						<input type="submit" name="show" value="<?php echo plugin_lang_get( 'show_graph' ); ?>" />
+						<input type="submit" class="button" name="show" value="<?php echo plugin_lang_get( 'show_graph' ); ?>"/>
 					</td>
 				</tr>
 			</table>

commit bc942a7013578a036392d9809ea0bfc791750058

Fix XML error in manage_proj_edit_page.php
Regression introduced by aec06762f253cfc5c8f3177fbbc77888f9b2ac80.

Affects #9159

manage_proj_edit_page.php
@@ -205,7 +205,7 @@ if ( access_has_global_level ( config_get( 'delete_project_threshold' ) ) ) { ?>
 					<option value="<?php echo $t_project['id'] ?>"><?php echo string_attribute( $t_project['name'] ) ?></option><?php
 				} # End looping over projects ?>
 				</select>
-				<input type="submit" class="button" value="<?php echo lang_get( 'add_subproject' ); ?>">
+				<input type="submit" class="button" value="<?php echo lang_get( 'add_subproject' ); ?>" />
 			</fieldset>
 		</form>
 	<?php