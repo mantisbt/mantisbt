mantisbt/mantisbt

commit 2b5d66217bd4ecf5e7271f1a4b2b339d7681e91c

Remove invalid chars from displayed string per XML specification
Strict XHTML requires that data comply with XML 1.0 specification [1],
which only allows a subset of the UTF-8 charset.

Function string_html_specialchars() has been modified to remove from the
string to print, any character which is not in the defined range

Char ::= #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] |
         [#x10000-#x10FFFF]

Fixes #14744

[1] http://www.w3.org/TR/xml/

core/string_api.php
@@ -910,6 +910,10 @@ function string_html_entities( $p_string ) {
  * @return string
  */
 function string_html_specialchars( $p_string ) {
+	# Remove any invalid character from the string per XML 1.0 specification
+	# http://www.w3.org/TR/2008/REC-xml-20081126/#NT-Char
+	$p_string = preg_replace( '/[^\x9\xA\xD\x20-\xD7FF\xE000-\xFFFD\x{10000}-\x{10FFFF}]/u', '', $p_string );
+
 	# achumakov: @ added to avoid warning output in unsupported codepages
 	# e.g. 8859-2, windows-1257, Korean, which are treated as 8859-1.
 	# This is VERY important for Eastern European, Baltic and Korean languages

commit ff2e6506406a20cd53c0c6147f9c9adad71690d7

Fix regex to remove UTF-8 chars invalid in XML 1.0
The regex introduced in string_html_specialchars() function with commit
2b5d66217bd4ecf5e7271f1a4b2b339d7681e91c caused problems with multibyte
UTF-8 chars, as PCRE require that they are specified like '\x{NNNN}';
the syntax without braces '\xNN' only supports up to 2 hex digits [1].

Fixes #14744

[1] http://php.net/regexp.reference.escape

core/string_api.php
@@ -912,7 +912,7 @@ function string_html_entities( $p_string ) {
 function string_html_specialchars( $p_string ) {
 	# Remove any invalid character from the string per XML 1.0 specification
 	# http://www.w3.org/TR/2008/REC-xml-20081126/#NT-Char
-	$p_string = preg_replace( '/[^\x9\xA\xD\x20-\xD7FF\xE000-\xFFFD\x{10000}-\x{10FFFF}]/u', '', $p_string );
+	$p_string = preg_replace( '/[^\x9\xA\xD\x20-\x{D7FF}\x{E000}-\x{FFFD}\x{10000}-\x{10FFFF}]+/u', '', $p_string );
 
 	# achumakov: @ added to avoid warning output in unsupported codepages
 	# e.g. 8859-2, windows-1257, Korean, which are treated as 8859-1.