37a2fee2fb1affc2a3a16242109f44b14cb82723
Damien Regad <damien.regad@merckgroup.com>
2012-11-29 10:31:34 +0100

    Don't send notif mail when updating Protected account's Access Level
    Prior to this, Mantis would effectively not update the protected user
    account's 'Access Level' field, but would still send a notification to
    the user informing them that the field was modified, causing confusion.
    
    The confirmation message for protected account update was reworded.
    
    Fixes #15247

--- manage_user_update.php
+++ manage_user_update.php
@@ -164,6 +164,8 @@ if ( $f_protected && $t_old_protected ) {
 				protected=" . db_param() . ", realname=" . db_param() . "
 			WHERE id=" . db_param();
 	$query_params = Array( $c_username, $c_email, $c_protected, $c_realname, $c_user_id );
+	# Prevent e-mail notification for a change that did not happen
+	$f_access_level = $t_old_access_level;
 } else {
 	$query = "UPDATE $t_user_table
 			SET username=" . db_param() . ", email=" . db_param() . ",
