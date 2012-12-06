mantisbt/mantisbt

commit 6a7db3403d42cf58e8c5ebb50b60a7c090906c36

Fix SQL error when sorting by custom field containing special char
Incorrect behavior is due to unnecessarily escaping of special chars by
calling db_prepare_string() on the custom field's name before attempting
to retrieve it's id with custom_field_get_id_from_name(). This causes a
double-escaping which prevents a match.

Even though this is not strictly necessary to fix the issue at hand,
this commit also replaces db_query() call by db_query_bound() in
custom_field_get_id_from_name().

Fixes #12170

core/custom_field_api.php
@@ -777,10 +777,9 @@ function custom_field_get_id_from_name( $p_field_name, $p_truncated_length = nul
 
 	$t_custom_field_table = db_get_table( 'mantis_custom_field_table' );
 
-	$c_field_name = db_prepare_string( $p_field_name );
-
-	if(( null === $p_truncated_length ) || ( utf8_strlen( $c_field_name ) != $p_truncated_length ) ) {
-		$query = "SELECT id FROM $t_custom_field_table WHERE name = '$c_field_name'";
+	if(( null === $p_truncated_length ) || ( utf8_strlen( $p_field_name ) != $p_truncated_length ) ) {
+		$query = "SELECT id FROM $t_custom_field_table WHERE name = " . db_param();
+		$c_field_name = $p_field_name;
 	} else {
 		# This is to handle the case where we potentially only have a
 		# truncated part of the custom field name.  This happens when we
@@ -789,10 +788,11 @@ function custom_field_get_id_from_name( $p_field_name, $p_truncated_length = nul
 		# This is needed to handle legacy database entries, as any
 		# history record created after 1.1.0a4 has the correct field
 		# size (see #8002)
-		$query = "SELECT id FROM $t_custom_field_table WHERE name LIKE '$c_field_name%'";
+		$query = "SELECT id FROM $t_custom_field_table WHERE name LIKE " . db_param();
+		$c_field_name = $p_field_name . '%';
 	}
 
-	$t_result = db_query( $query, 1 );
+	$t_result = db_query_bound( $query, $c_field_name );
 
 	if( db_num_rows( $t_result ) == 0 ) {
 		return false;

core/filter_api.php
@@ -907,7 +907,7 @@ function filter_get_query_sort_data( &$p_filter, $p_show_sticky, $p_query_clause
 
 	$t_count = count( $t_sort_fields );
 	for( $i = 0;$i < $t_count;$i++ ) {
-		$c_sort = db_prepare_string( $t_sort_fields[$i] );
+		$c_sort = $t_sort_fields[$i];
 		$c_dir = 'DESC' == $t_dir_fields[$i] ? 'DESC' : 'ASC';
 
 		if( !in_array( $t_sort_fields[$i], array_slice( $t_sort_fields, $i + 1 ) ) ) {

commit d0807051dfd6cef2db961a747aa2c31198e812ef

Fix custom_field_get_id_from_name() always returning false
Commit 6a7db3403d42cf58e8c5ebb50b60a7c090906c36 introduced an error in
the call to db_query_bound(), passing the param as a scalar value
instead of an array.

Thanks to user nimmich for noticing the issue and providing a patch.

Fixes #15264

core/custom_field_api.php
@@ -642,7 +642,7 @@ function custom_field_get_id_from_name( $p_field_name ) {
 	$t_custom_field_table = db_get_table( 'custom_field' );
 
 	$query = "SELECT id FROM $t_custom_field_table WHERE name = " . db_param();
-	$t_result = db_query_bound( $query, $p_field_name );
+	$t_result = db_query_bound( $query, array( $p_field_name ) );
 
 	if( db_num_rows( $t_result ) == 0 ) {
 		return false;