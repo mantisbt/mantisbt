b74efee1fadead671ab2ff9eaa24aea80fc917b0
Damien Regad <damien.regad@merckgroup.com>
2012-12-31 14:27:10 +0100

    Use date() for current copyright year in html_footer

--- core/html_api.php
+++ core/html_api.php
@@ -667,7 +667,7 @@ function html_footer( $p_file = null ) {
 	$t_copyright_years = '';
 	if ( config_get( 'show_version' ) ) {
 		$t_version_suffix = htmlentities( ' ' . MANTIS_VERSION . config_get_global( 'version_suffix' ) );
-		$t_copyright_years = ' 2000 - 2012';
+		$t_copyright_years = ' 2000 - ' . date('Y');
 	}
 	echo "\t<address id=\"mantisbt-copyright\">Powered by <a href=\"http://www.mantisbt.org\" title=\"Mantis Bug Tracker: a free and open source web based bug tracking system.\">Mantis Bug Tracker</a> (MantisBT)$t_version_suffix. Copyright &copy;$t_copyright_years MantisBT contributors. Licensed under the terms of the <a href=\"http://www.gnu.org/licenses/old-licenses/gpl-2.0.html\" title=\"GNU General Public License (GPL) version 2\">GNU General Public License (GPL) version 2</a> or a later version.</address>\n";
 
