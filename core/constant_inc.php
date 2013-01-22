d38abf9556a379bdf43502edad076d554a30166d
Damien Regad <damien.regad@merckgroup.com>
2013-01-22 13:43:36 +0100

    Bump version and update release notes for 1.2.13

--- core/constant_inc.php
+++ core/constant_inc.php
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
 
-define( 'MANTIS_VERSION', '1.2.13dev' );
+define( 'MANTIS_VERSION', '1.2.13' );
 
 # --- constants -------------------
 # magic numbers
