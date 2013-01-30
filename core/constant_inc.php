a685ee50b5c5bee5a93344b47656ea3a91f5ad9b
Damien Regad <damien.regad@merckgroup.com>
2013-01-30 10:04:30 +0100

    Changed version to 1.2.15dev

--- core/constant_inc.php
+++ core/constant_inc.php
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
 
-define( 'MANTIS_VERSION', '1.2.14' );
+define( 'MANTIS_VERSION', '1.2.15dev' );
 
 # --- constants -------------------
 # magic numbers
