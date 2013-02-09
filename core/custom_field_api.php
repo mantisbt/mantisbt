mantisbt/mantisbt

commit b5abce1f917da688c86aec17dee11111ced5f0b7

Improve comments for custom field length check
Function custom_field_get_id_from_name() contains code to check for
truncated custom fields names. This dates back to when the history
table's field_name column was only 32 chars long, whereas the custom
field's size is 64.

Even though the history's field_name size has been increased to 64 since
1.1.0a4 (see issue #8002), the check must be maintained to ensure
compatibility with upgraded legacy systems.

The fix ce3450aaabbd9d9feceb03d77d4358f195e7d966 implemented in master
branch cannot be applied here as it contains a schema update. Instead,
comments have been reworded and obsolete reference to old schema
removed.

Fixes #14650

core/custom_field_api.php
@@ -782,10 +782,13 @@ function custom_field_get_id_from_name( $p_field_name, $p_truncated_length = nul
 	if(( null === $p_truncated_length ) || ( utf8_strlen( $c_field_name ) != $p_truncated_length ) ) {
 		$query = "SELECT id FROM $t_custom_field_table WHERE name = '$c_field_name'";
 	} else {
-		/** @todo This is to handle the case where we only have a truncated part of the name.  This happens in the case where
-		 * we are getting the custom field name from the history logs, since history is 32 and custom field name is 64.
-		 * This fix will handle entries already in the database, future entries should be handled by making the field name max lengths match.
-		 */
+		# This is to handle the case where we potentially only have a
+		# truncated part of the custom field name.  This happens when we
+		# are getting the field from the history logs (as the history's
+		# field_name column used to be 32 while custom field name is 64).
+		# This is needed to handle legacy database entries, as any
+		# history record created after 1.1.0a4 has the correct field
+		# size (see #8002)
 		$query = "SELECT id FROM $t_custom_field_table WHERE name LIKE '$c_field_name%'";
 	}
 

core/history_api.php
@@ -172,9 +172,12 @@ function history_get_raw_events_array( $p_bug_id, $p_user_id = null ) {
 
 		if ( $v_type == NORMAL_TYPE ) {
 			if ( !in_array( $v_field_name, $t_standard_fields ) ) {
+				# check that the item should be visible to the user
 
-				// check that the item should be visible to the user
-				// custom fields - we are passing 32 here to notify the API that the custom field name is truncated by the history column from 64 to 32 characters.
+				# We are passing 32 here to notify the custom field API
+				# that legacy history entries for field names longer than
+				# 32 chars created when the db column was of that size were
+				# truncated (no longer the case since 1.1.0a4, see #8002)
 				$t_field_id = custom_field_get_id_from_name( $v_field_name, 32 );
 				if( false !== $t_field_id && !custom_field_has_read_access( $t_field_id, $p_bug_id, $t_user_id ) ) {
 					continue;

commit 645ac2e973b99940226414299e3d8c515feea062

Fix #15481: custom field values sort order in view all page filter
Commit b5abce1f917da688c86aec17dee11111ced5f0b7 introduced a regression
in the sort order of custom field values because the query retrieving
the custom field values was rewritten with a SELECT DISTINCT instead of
using a GROUP BY clause. With MySQL, group by sorts the results, whereas
distinct does not (not tested with other RDBMS).

This fixes the issue by adding an ORDER BY clause.

core/custom_field_api.php
@@ -1161,7 +1161,8 @@ function custom_field_distinct_values( $p_field_def, $p_project_id = ALL_PROJECT
 		$t_query = "
 			SELECT DISTINCT cfst.value
 			FROM $t_from
-			WHERE $t_where1 $t_where2";
+			WHERE $t_where1 $t_where2
+			ORDER BY cfst.value";
 
 		$t_result = db_query_bound( $t_query, $t_params );
 		$t_row_count = db_num_rows( $t_result );