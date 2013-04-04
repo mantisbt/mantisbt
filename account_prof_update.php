mantisbt/mantisbt

commit fe8e3672f555063fadbf652e3c557f5a438a3313

Fix #15698: system warning when editing profile
When user clicks Submit button to edit an existing profile, without
first selecting one from the list, the following system warning was
triggered: 'extract() expects parameter 1 to be array, boolean given'.

The code now ensures that a profile has been selected before redirecting
to account_prof_edit_page.php, and issues a meaningful error message if
not.

account_prof_update.php
@@ -36,10 +36,18 @@
 	current_user_ensure_unprotected();
 
 	$f_action = gpc_get_string('action');
+	if( $f_action != 'add') {
+		$f_profile_id = gpc_get_int( 'profile_id' );
+
+		# Make sure user did select an existing profile from the list
+		if( $f_profile_id == 0 ) {
+			error_parameters( lang_get( 'select_profile' ) );
+			trigger_error( ERROR_EMPTY_FIELD, ERROR );
+		}
+	}
 
 	switch ( $f_action ) {
 		case 'edit':
-			$f_profile_id = gpc_get_int( 'profile_id' );
 			form_security_purge('profile_update');
 			print_header_redirect( 'account_prof_edit_page.php?profile_id=' . $f_profile_id );
 			break;
@@ -72,7 +80,6 @@
 			break;
 
 		case 'update':
-			$f_profile_id = gpc_get_int( 'profile_id' );
 			$f_platform = gpc_get_string( 'platform' );
 			$f_os = gpc_get_string( 'os' );
 			$f_os_build = gpc_get_string( 'os_build' );
@@ -92,7 +99,6 @@
 			break;
 
 		case 'delete':
-			$f_profile_id = gpc_get_int( 'profile_id' );
 			if ( profile_is_global( $f_profile_id ) ) {
 				access_ensure_global_level( config_get( 'manage_global_profile_threshold' ) );
 
@@ -107,7 +113,6 @@
 			break;
 
 		case 'make_default':
-			$f_profile_id = gpc_get_int( 'profile_id' );
 			current_user_set_pref( 'default_profile', $f_profile_id );
 			form_security_purge('profile_update');
 			print_header_redirect( 'account_prof_menu_page.php' );

commit 5c7bcc716e00d35e053972ad6e4e925b7a14f8a9

Reenable ability to clear default profile
Regression introduced by fe8e3672f555063fadbf652e3c557f5a438a3313

Fixes #15698

account_prof_update.php
@@ -40,7 +40,7 @@
 		$f_profile_id = gpc_get_int( 'profile_id' );
 
 		# Make sure user did select an existing profile from the list
-		if( $f_profile_id == 0 ) {
+		if( $f_action != 'make_default' && $f_profile_id == 0 ) {
 			error_parameters( lang_get( 'select_profile' ) );
 			trigger_error( ERROR_EMPTY_FIELD, ERROR );
 		}