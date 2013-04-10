49406e529d1276f042ac308a3ccd40a377fd4b98
Damien Regad <damien.regad@merckgroup.com>
2013-04-10 17:49:16 +0200

    Create reminder bugnote even if no message text provided
    When adding a reminder, a bugnote was actually NOT created when the
    reminder text is empty, even if $g_store_reminders = ON.  This was due
    to bugnote_add() returning false when $p_bugnote_text is blank as the
    code did not take REMINDER type notes into consideration.
    
    Fixes #15744

--- core/bugnote_api.php
+++ core/bugnote_api.php
@@ -143,33 +143,35 @@ function bugnote_is_user_reporter( $p_bugnote_id, $p_user_id ) {
  * @return false|int false or indicating bugnote id added
  * @access public
  */
-function bugnote_add( $p_bug_id, $p_bugnote_text, $p_time_tracking = '0:00', $p_private = false, $p_type = 0, $p_attr = '', $p_user_id = null, $p_send_email = TRUE, $p_date_submitted = 0, $p_last_modified = 0, $p_skip_bug_update = FALSE, $p_log_history = TRUE ) {
+function bugnote_add( $p_bug_id, $p_bugnote_text, $p_time_tracking = '0:00', $p_private = false, $p_type = BUGNOTE, $p_attr = '', $p_user_id = null, $p_send_email = TRUE, $p_date_submitted = 0, $p_last_modified = 0, $p_skip_bug_update = FALSE, $p_log_history = TRUE ) {
 	$c_bug_id = db_prepare_int( $p_bug_id );
 	$c_time_tracking = helper_duration_to_minutes( $p_time_tracking );
 	$c_type = db_prepare_int( $p_type );
 	$c_date_submitted = $p_date_submitted <= 0 ? db_now() : db_prepare_int( $p_date_submitted );
 	$c_last_modified = $p_last_modified <= 0 ? db_now() : db_prepare_int( $p_last_modified );
 
-	$t_bugnote_text_table = db_get_table( 'bugnote_text' );
-	$t_bugnote_table = db_get_table( 'bugnote' );
-
-	$t_time_tracking_enabled = config_get( 'time_tracking_enabled' );
-	$t_time_tracking_without_note = config_get( 'time_tracking_without_note' );
-
-	if( ON == $t_time_tracking_enabled && $c_time_tracking > 0 ) {
-		if( is_blank( $p_bugnote_text ) && OFF == $t_time_tracking_without_note ) {
-			error_parameters( lang_get( 'bugnote' ) );
-			trigger_error( ERROR_EMPTY_FIELD, ERROR );
+	if( REMINDER !== $p_type ) {
+		# Check if this is a time-tracking note
+		$t_time_tracking_enabled = config_get( 'time_tracking_enabled' );
+		if( ON == $t_time_tracking_enabled && $c_time_tracking > 0 ) {
+			$t_time_tracking_without_note = config_get( 'time_tracking_without_note' );
+			if( is_blank( $p_bugnote_text ) && OFF == $t_time_tracking_without_note ) {
+				error_parameters( lang_get( 'bugnote' ) );
+				trigger_error( ERROR_EMPTY_FIELD, ERROR );
+			}
+			$c_type = TIME_TRACKING;
+		} else if( is_blank( $p_bugnote_text ) ) {
+			# This is not time tracking (i.e. it's a normal bugnote)
+			# @todo should we not trigger an error in this case ?
+			return false;
 		}
-		$c_type = TIME_TRACKING;
-	} else if( is_blank( $p_bugnote_text ) ) {
-		return false;
 	}
 
-	$t_bugnote_text = $p_bugnote_text;
+	$t_bugnote_text_table = db_get_table( 'bugnote_text' );
+	$t_bugnote_table = db_get_table( 'bugnotE' );
 
 	# Event integration
-	$t_bugnote_text = event_signal( 'EVENT_BUGNOTE_DATA', $t_bugnote_text, $c_bug_id );
+	$t_bugnote_text = event_signal( 'EVENT_BUGNOTE_DATA', $p_bugnote_text, $c_bug_id );
 
 	# insert bugnote text
 	$query = 'INSERT INTO ' . $t_bugnote_text_table . ' ( note ) VALUES ( ' . db_param() . ' )';
