mantisbt/mantisbt

commit c9bc0646d69b2e39cd5e48320be82a83d8e39ebc

Filter api: systematic use JOIN when building SQL
Do not join tables using the where clause, for better readability and
avoiding risk of issues with operator precedence and 'any condition'
filtering mode.

This commit also removes an unnecessary LEFT JOIN between the bugnote
and bugnote_text tables; since this is a strict 1:1 relationship, an
inner join is sufficient and yields better performance.

core/filter_api.php
@@ -1092,7 +1092,6 @@ function filter_get_bug_rows( &$p_page_number, &$p_per_page, &$p_page_count, &$p
 
 	$t_project_where_clauses =  array(
 		"$t_project_table.enabled = " . db_param(),
-		"$t_project_table.id = $t_bug_table.project_id",
 	);
 	$t_where_params = array(
 		1,
@@ -1101,8 +1100,13 @@ function filter_get_bug_rows( &$p_page_number, &$p_per_page, &$p_page_count, &$p
 		"$t_bug_table.*",
 	);
 
-	$t_join_clauses = array();
-	$t_from_clauses = array();
+	$t_from_clauses = array(
+		$t_bug_table,
+	);
+
+	$t_join_clauses = array(
+		"JOIN $t_project_table ON $t_project_table.id = $t_bug_table.project_id",
+	);
 
 	// normalize the project filtering into an array $t_project_ids
 	if( 'simple' == $t_view_type ) {
@@ -1997,7 +2001,7 @@ function filter_get_bug_rows( &$p_page_number, &$p_per_page, &$p_page_count, &$p
 		if ( !$t_first ) {
 			$t_join_clauses[] = "JOIN $t_bug_text_table ON $t_bug_table.bug_text_id = $t_bug_text_table.id";
 			$t_join_clauses[] = "LEFT JOIN $t_bugnote_table ON $t_bug_table.id = $t_bugnote_table.bug_id";
-			$t_join_clauses[] = "LEFT JOIN $t_bugnote_text_table ON $t_bugnote_table.bugnote_text_id = $t_bugnote_text_table.id";
+			$t_join_clauses[] = "JOIN $t_bugnote_text_table ON $t_bugnote_table.bugnote_text_id = $t_bugnote_text_table.id";
 			$t_where_clauses[] = $t_textsearch_where_clause;
 		}
 	}
@@ -2011,9 +2015,6 @@ function filter_get_bug_rows( &$p_page_number, &$p_per_page, &$p_page_count, &$p
 		$t_join_operator = ' AND ';
 
 	log_event(LOG_FILTERING, 'Join operator : ' . $t_join_operator);
-
-	$t_from_clauses[] = $t_project_table;
-	$t_from_clauses[] = $t_bug_table;
 
 	$t_query_clauses['select'] = $t_select_clauses;
 	$t_query_clauses['from'] = $t_from_clauses;

commit cb4e22cb5c44a308f1f3f88bc7f9e21601e2a62d

Fix regression in filter_api.php
Commit c9bc0646d69b2e39cd5e48320be82a83d8e39ebc introduced a regression,
whereby the filter would exclude issues without bugnotes when using text
search.

This was caused by removal of left outer join between bugnote_table and
bugnote_text_table, which was in fact required because the join clause
fails to return any record when the bugnote_text_id is null (which is
the case when there are no bugnotes).

Thanks to user Kitzberger for reporting this and atrol for providing the
steps to reproduce.

core/filter_api.php
@@ -2001,7 +2001,8 @@ function filter_get_bug_rows( &$p_page_number, &$p_per_page, &$p_page_count, &$p
 		if ( !$t_first ) {
 			$t_join_clauses[] = "JOIN $t_bug_text_table ON $t_bug_table.bug_text_id = $t_bug_text_table.id";
 			$t_join_clauses[] = "LEFT JOIN $t_bugnote_table ON $t_bug_table.id = $t_bugnote_table.bug_id";
-			$t_join_clauses[] = "JOIN $t_bugnote_text_table ON $t_bugnote_table.bugnote_text_id = $t_bugnote_text_table.id";
+			# Outer join required otherwise we don't retrieve issues without notes
+			$t_join_clauses[] = "LEFT JOIN $t_bugnote_text_table ON $t_bugnote_table.bugnote_text_id = $t_bugnote_text_table.id";
 			$t_where_clauses[] = $t_textsearch_where_clause;
 		}
 	}