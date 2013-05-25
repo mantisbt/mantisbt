18ff872ec765dca5f823c362219c12b5d2ca455a
Damien Regad <dregad@mantisbt.org>
2013-05-26 01:35:40 +0200

    Issue #13740: use require_api() instead of require_once()

--- core.php
+++ core.php
@@ -156,7 +156,7 @@ function __autoload( $className ) {
 # Register the autoload function to make it effective immediately
 spl_autoload_register( '__autoload' );
 
-require_once( 'core/mobile_api.php' );
+require_api( 'mobile_api.php' );
 
 if ( strlen( $GLOBALS['g_mantistouch_url'] ) > 0 && mobile_is_mobile_browser() ) {
 	$t_url = sprintf( $GLOBALS['g_mantistouch_url'], $GLOBALS['g_path'] );
