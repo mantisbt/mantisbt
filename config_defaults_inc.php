82bc5a7998a1de09d636dda5c4dba6d55988dbdc
Damien Regad <dregad@mantisbt.org>
2013-05-27 11:51:50 +0200

    Fix #15921: Unset $t_hosts after use

--- config_defaults_inc.php
+++ config_defaults_inc.php
@@ -4200,4 +4200,4 @@ $g_mantistouch_url = '';
 
 
 # Temporary variables should not remain defined in global scope
-unset( $t_protocol, $t_host, $t_port, $t_self, $t_path, $t_use_iis );
+unset( $t_protocol, $t_host, $t_hosts, $t_port, $t_self, $t_path, $t_use_iis );
