mantisbt/mantisbt

commit 564f92a02ff738645d68b6e7f5ace1009b5c5f9f

Fix validation issues for my view and for the status legends.  Add css/status_config.php         to allow configuring statuses and colors in config_inc but output with css.

css/status_config.php
@@ -1,0 +1,72 @@
+<?php
+# MantisBT - A PHP based bugtracking system
+
+# MantisBT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# MantisBT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package MantisBT
+ * @copyright Copyright (C) 2000 - 2002  Kenzaburo Ito - kenito@300baud.org
+ * @copyright Copyright (C) 2002 - 2010  MantisBT Team - mantisbt-dev@lists.sourceforge.net
+ * @link http://www.mantisbt.org
+ *
+ * @uses config_api.php
+ */
+
+/**
+ * MantisBT Core API's
+ */
+@require_once( dirname( dirname( __FILE__ ) ) . DIRECTORY_SEPARATOR . 'core.php' );
+require_api( 'config_api.php' );
+
+/**
+ * Send correct MIME Content-Type header for css content.
+ */
+header( 'Content-Type: text/css; charset=UTF-8' );
+
+/**
+ * Disallow Internet Explorer from attempting to second guess the Content-Type
+ * header as per http://blogs.msdn.com/ie/archive/2008/07/02/ie8-security-part-v-comprehensive-protection.aspx
+ */
+header( 'X-Content-Type-Options: nosniff' );
+
+/**
+ * WARNING: DO NOT EXPOSE SENSITIVE CONFIGURATION VALUES!
+ *
+ * All configuration values below are publicly available to visitors of the bug
+ * tracker regardless of whether they're authenticated. Server paths should not
+ * be exposed. It is OK to expose paths that the user sees directly (short
+ * paths) but you do need to be careful in your selections. Consider servers
+ * using URL rewriting engines to mask/convert user-visible paths to paths that
+ * should only be known internally to the server.
+ */
+
+/**
+ *	@todo Modify to run sections only on certain pages.
+ *	eg. status colors are only necessary on a few pages.(my view, view all bugs, bug view, etc. )
+ *	other pages my need to include dynamic css styles as well
+ */
+$t_status_string = config_get( 'status_enum_string' );
+$t_statuses = MantisEnum::getAssocArrayIndexedByValues( $t_status_string );
+$t_colors = config_get( 'status_colors' );
+$t_color_count = count( $t_colors );
+$t_color_width = ( $t_color_count > 0 ? ( round( 100/$t_color_count ) ) : 0 );
+$t_status_percents = get_percentage_by_status();
+foreach( $t_statuses AS $t_id=>$t_label ) {
+	if( array_key_exists( $t_label, $t_colors ) ) { 
+		echo ".$t_label-color { background-color: {$t_colors[$t_label]}; width: $t_color_width%; }\n";
+	}
+	if( array_key_exists( $t_id, $t_status_percents ) ) {
+		echo ".$t_label-percentage { width: {$t_status_percents[$t_id]}%; }\n";
+	}
+}

core/helper_api.php
@@ -116,6 +116,41 @@ function get_status_color( $p_status ) {
 }
 
 /**
+ * get the status percentages
+ * @return array key is the status value, value is the percentage of bugs for the status
+ */
+function get_percentage_by_status() {
+	$t_mantis_bug_table = db_get_table( 'bug' );
+	$t_project_id = helper_get_current_project();
+	$t_user_id = auth_get_current_user_id();
+
+	# checking if it's a per project statistic or all projects
+	$t_specific_where = helper_project_specific_where( $t_project_id, $t_user_id );
+
+	$query = "SELECT status, COUNT(*) AS number
+				FROM $t_mantis_bug_table
+				WHERE $t_specific_where";
+	if ( !access_has_project_level( config_get( 'private_bug_threshold' ) ) ) {
+		$query .= ' AND view_state < ' . VS_PRIVATE;
+	}
+	$query .= ' GROUP BY status';
+	$result = db_query_bound( $query );
+
+	$t_bug_count = 0;
+	$t_status_count_array = array();
+
+	while( $row = db_fetch_array( $result ) ) {
+		$t_status_count_array[$row['status']] = $row['number'];
+	}
+	$t_bug_count = array_sum( $t_status_count_array );
+	foreach( $t_status_count_array AS $t_status=>$t_value ) {
+		$t_status_count_array[$t_status] = round(( $t_value / $t_bug_count ) * 100 );
+	}
+
+	return $t_status_count_array;
+}
+
+/**
  * Given a enum string and num, return the appropriate string
  * @param string $p_enum_name
  * @param int $p_val

core/html_api.php
@@ -1284,11 +1284,12 @@ function html_status_legend() {
 
 	# draw the status bar
 	$width = (int)( 100 / count( $t_status_array ) );
+	$t_status_enum_string = config_get('status_enum_string' );
 	foreach( $t_status_array as $t_status => $t_name ) {
 		$t_val = $t_status_names[$t_status];
-		$t_color = get_status_color( $t_status );
+		$t_status_label = MantisEnum::getLabel( $t_status_enum_string, $t_status );
 
-		echo "<td class=\"small-caption\" width=\"$width%\" bgcolor=\"$t_color\">$t_val</td>";
+		echo "<td class=\"small-caption $t_status_label-color\">$t_val</td>";
 	}
 
 	echo '</tr>';
@@ -1303,32 +1304,12 @@ function html_status_legend() {
  * @return null
  */
 function html_status_percentage_legend() {
-	$t_mantis_bug_table = db_get_table( 'bug' );
-	$t_project_id = helper_get_current_project();
-	$t_user_id = auth_get_current_user_id();
-
-	# checking if it's a per project statistic or all projects
-	$t_specific_where = helper_project_specific_where( $t_project_id, $t_user_id );
-
-	$query = "SELECT status, COUNT(*) AS number
-				FROM $t_mantis_bug_table
-				WHERE $t_specific_where";
-	if ( !access_has_project_level( config_get( 'private_bug_threshold' ) ) ) {
-		$query .= ' AND view_state < ' . VS_PRIVATE;
-	}
-	$query .= ' GROUP BY status';
-	$result = db_query_bound( $query );
-
-	$t_bug_count = 0;
-	$t_status_count_array = array();
-
-	while( $row = db_fetch_array( $result ) ) {
-		$t_status_count_array[$row['status']] = $row['number'];
-		$t_bug_count += $row['number'];
-	}
-
-	$t_enum_values = MantisEnum::getValues( config_get( 'status_enum_string' ) );
+	$t_status_percents = get_percentage_by_status();
+	$t_status_enum_string = config_get('status_enum_string' );
+	$t_enum_values = MantisEnum::getValues( $t_status_enum_string );
 	$enum_count = count( $t_enum_values );
+
+	$t_bug_count = array_sum( $t_status_percents );
 
 	if( $t_bug_count > 0 ) {
 		echo '<br />';
@@ -1339,16 +1320,11 @@ function html_status_percentage_legend() {
 		echo '<tr>';
 
 		foreach ( $t_enum_values as $t_status ) {
-			$t_color = get_status_color( $t_status );
+			$t_percent = ( isset( $t_status_percents[$t_status] ) ?  $t_status_percents[$t_status] : 0 );
 
-			if ( !isset( $t_status_count_array[$t_status] ) ) {
-				$t_status_count_array[$t_status] = 0;
-			}
-
-			$width = round(( $t_status_count_array[$t_status] / $t_bug_count ) * 100 );
-
-			if( $width > 0 ) {
-				echo "<td class=\"small-caption-center\" width=\"$width%\" bgcolor=\"$t_color\">$width%</td>";
+			if( $t_percent > 0 ) {
+				$t_status_label = MantisEnum::getLabel( $t_status_enum_string, $t_status );
+				echo "<td class=\"small-caption-center $t_status_label-color $t_status_label-percentage\">$t_percent%</td>";
 			}
 		}
 

css/default.css
@@ -358,3 +358,5 @@ label.even {
 	line-height: 1.7em;
 	background-color: #e8e8e8;
 }
+
+.myview-left-col, .myview-right-col { width: 50%; vertical-align: top; }

my_view_inc.php
@@ -438,15 +438,18 @@ echo "<span class=\"my-buglist-count\">($v_start - $v_end / $t_bug_count)</span>
 </thead><tbody>
 <?php
 # -- Loop over bug rows and create $v_* variables --
-	$t_count = count( $rows );
-	for( $i = 0;$i < $t_count; $i++ ) {
-		$t_bug = $rows[$i];
+$t_count = count( $rows );
+if( $t_count == 0 ) {
+	echo '<tr><td>&#160;</td></tr>';
+}
+for( $i = 0;$i < $t_count; $i++ ) {
+	$t_bug = $rows[$i];
 
 	$t_summary = string_display_line_links( $t_bug->summary );
 	$t_last_updated = date( config_get( 'normal_date_format' ), $t_bug->last_updated );
 
 	# choose color based on status
-	$status_color = get_status_color( $t_bug->status );
+	$status_label = MantisEnum::getLabel( config_get('status_enum_string' ), $t_bug->status );
 
 	# Check for attachments
 	$t_attachment_count = 0;
@@ -466,10 +469,10 @@ echo "<span class=\"my-buglist-count\">($v_start - $v_end / $t_bug_count)</span>
     }
 	?>
 
-<tr class="my-buglist-bug <?php echo $t_bug_class?>" bgcolor="<?php echo $status_color?>">
+<tr class="my-buglist-bug <?php echo $t_bug_class?> <?php echo $status_label . '-color'; ?>">
 	<?php
 	# -- Bug ID and details link + Pencil shortcut --?>
-	<td class="center nowrap my-buglist-id" width="0">
+	<td class="center nowrap my-buglist-id">
 		<span class="small">
 		<?php
 			print_bug_link( $t_bug->id );
@@ -502,7 +505,7 @@ echo "<span class=\"my-buglist-count\">($v_start - $v_end / $t_bug_count)</span>
 
 	<?php
 	# -- Summary --?>
-	<td class="left my-buglist-description" width="100%">
+	<td class="left my-buglist-description">
 		<?php
 		 	if( ON == config_get( 'show_bug_project_links' ) && helper_get_current_project() != $t_bug->project_id ) {
 				echo '<span class="small project">[', string_display_line( project_get_name( $t_bug->project_id ) ), '] </span>';

my_view_page.php
@@ -53,6 +53,7 @@ require_api( 'html_api.php' );
 require_api( 'lang_api.php' );
 require_api( 'print_api.php' );
 require_api( 'user_api.php' );
+require_css( 'status_config.php' );
 
 auth_ensure_user_authenticated();
 
@@ -140,14 +141,14 @@ while (list ($t_box_title, $t_box_display) = each ($t_boxes)) {
 		if ( ON == $t_boxes_position ) {
 			# for even box number start new row and column
 			if ( 1 == $t_counter%2 ) {
-				echo '<tr><td width="50%">';
+				echo '<tr><td class="myview-left-col">';
 				include( dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'my_view_inc.php' );
 				echo '</td>';
 			}
 
 			# for odd box number only start new column
 			else if ( 0 == $t_counter%2 ) {
-				echo '<td width="50%">';
+				echo '<td class="myview-right-col">';
 				include( dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'my_view_inc.php' );
 				echo '</td></tr>';
 			}
@@ -155,12 +156,12 @@ while (list ($t_box_title, $t_box_display) = each ($t_boxes)) {
 		else if ( OFF == $t_boxes_position ) {
 			# start new table row and column for first box
 			if ( 1 == $t_counter ) {
-				echo '<tr><td width="50%">';
+				echo '<tr><td class="myview-left-col">';
 			}
 
 			# start new table column for the second half of boxes
 			if ( $t_counter == ceil ($t_number_of_boxes/2) + 1 ) {
-				echo '<td width="50%">';
+				echo '<td class="myview-right-col">';
 			}
 
 			# display the required box
@@ -179,7 +180,7 @@ while (list ($t_box_title, $t_box_display) = each ($t_boxes)) {
 # is required to pad the number of cells in the last row to the full width of
 # the table.
 if ( ON == $t_boxes_position && $t_counter == $t_number_of_boxes && 1 == $t_counter%2 ) {
-	echo '<td width="50%"></td></tr>';
+	echo '<td class="myview-right-col"></td></tr>';
 } else if ( OFF == $t_boxes_position && $t_counter == $t_number_of_boxes ) {
 	echo '</td></tr>';
 }

view_all_bug_page.php
@@ -51,6 +51,7 @@ require_api( 'project_api.php' );
 require_api( 'user_api.php' );
 
 require_js( 'bugFilter.js' );
+require_css( 'status_config.php' );
 
 auth_ensure_user_authenticated();
 

commit c2dd477aed6ea9989f98ea9adc1ebee83d240466

Fix #16039: Login page causes error in web server log
Regression introduced by 564f92a02ff738645d68b6e7f5ace1009b5c5f9f.

Dynamic CSS 'status_config.php' page now exits without generating any
stylesheets when called from login_page.php (status colors are not
needed there anyway).

css/status_config.php
@@ -54,8 +54,16 @@ header( 'X-Content-Type-Options: nosniff' );
 /**
  *	@todo Modify to run sections only on certain pages.
  *	eg. status colors are only necessary on a few pages.(my view, view all bugs, bug view, etc. )
- *	other pages my need to include dynamic css styles as well
+ *	other pages may need to include dynamic css styles as well
  */
+$t_referer_page = basename( parse_url( $_SERVER['HTTP_REFERER'], PHP_URL_PATH ) );
+switch( $t_referer_page ) {
+	case 'login_page.php':
+		# We don't need custom status colors on login page, and this is
+		# actually causing an error since we're not authenticated yet.
+		exit;
+}
+
 $t_status_string = config_get( 'status_enum_string' );
 $t_statuses = MantisEnum::getAssocArrayIndexedByValues( $t_status_string );
 $t_colors = config_get( 'status_colors' );
@@ -63,7 +71,7 @@ $t_color_count = count( $t_colors );
 $t_color_width = ( $t_color_count > 0 ? ( round( 100/$t_color_count ) ) : 0 );
 $t_status_percents = get_percentage_by_status();
 foreach( $t_statuses AS $t_id=>$t_label ) {
-	if( array_key_exists( $t_label, $t_colors ) ) { 
+	if( array_key_exists( $t_label, $t_colors ) ) {
 		echo ".$t_label-color { background-color: {$t_colors[$t_label]}; width: $t_color_width%; }\n";
 	}
 	if( array_key_exists( $t_id, $t_status_percents ) ) {