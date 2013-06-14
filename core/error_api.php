cec454931df5a99d03ff22aabf8901648ff3d549
Damien Regad <dregad@mantisbt.org>
2013-06-14 23:49:42 +0200

    Force reporting of E_USER_* in error api
    Solves the problem of Mantis silently proceeding through errors that
    should effectively stop code execution, when error_reporting is disabled
    in php.ini or apache mod config.
    
    This follows @davidhicks recommendation in comment ~22998.
    
    Fixes #10966

--- core/error_api.php
+++ core/error_api.php
@@ -43,6 +43,10 @@ $g_error_handled = false;
 $g_error_proceed_url = null;
 $g_error_send_page_header = true;
 
+# Make sure we always capture User-defined errors regardless of ini settings
+# These can be disabled in config_inc.php, see $g_display_errors
+error_reporting( error_reporting() | E_USER_ERROR | E_USER_WARNING | E_USER_NOTICE );
+
 set_error_handler( 'error_handler' );
 
 /**
