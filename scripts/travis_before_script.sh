f8deab614ba8c1b8bc73c2d62776134aefa8109d
Roland Becker <roland@atrol.de>
2013-07-21 13:28:14 +0200

    Fix #16202: Travis CI: set up PHP 5.3 build

--- scripts/travis_before_script.sh
+++ scripts/travis_before_script.sh
@@ -10,12 +10,45 @@ elif [ $DB = 'pgsql' ]; then
 	DB_USER='postgres'
 fi
 
-# start embedded server
-php -S localhost:8000 &
-sleep 5
+if [ $TRAVIS_PHP_VERSION = '5.3' ]; then
+	# install Apache as PHP 5.3 does not come with an embedded web server
+	sudo apt-get update -qq
+	sudo apt-get install -qq apache2 libapache2-mod-php5 php5-mysql php5-pgsql
+	
+	WEBROOT="$(pwd)"
+
+	echo "<VirtualHost *:80>
+        DocumentRoot $WEBROOT
+        <Directory />
+                Options FollowSymLinks
+                AllowOverride All
+        </Directory>
+        <Directory $WEBROOT >
+                Options Indexes FollowSymLinks MultiViews
+                AllowOverride All
+                Order allow,deny
+                allow from all
+        </Directory>
+
+	</VirtualHost>" | sudo tee /etc/apache2/sites-available/default > /dev/null
+	
+	sudo service apache2 restart
+
+	# needed to allow web server to create config_inc.php
+	chmod 777 .
+else
+	# use PHP's embedded server
+	# get path of PHP as the path is not in $PATH for sudo
+	myphp=$(which php)
+	# sudo needed for port 80
+	sudo $myphp -S localhost:80 &
+fi
+
+#  wait until server is up
+sleep 10
 
 # trigger installation
-curl --data "install=2&hostname=localhost&db_username=${DB_USER}&db_type=${DB}&db_password=&database_name=bugtracker&admin_username=${DB_USER}&admin_password=" http://localhost:8000/admin/install.php
+curl --data "install=2&hostname=localhost&db_username=${DB_USER}&db_type=${DB}&db_password=&database_name=bugtracker&admin_username=${DB_USER}&admin_password=" http://localhost/admin/install.php
 
 # create the first project
 if [ $DB = 'mysql' ]; then
@@ -25,4 +58,4 @@ elif [ $DB = 'pgsql' ]; then
 fi
 
 # enable SOAP tests
-echo "<?php \$GLOBALS['MANTIS_TESTSUITE_SOAP_ENABLED'] = true;  \$GLOBALS['MANTIS_TESTSUITE_SOAP_HOST'] = 'http://localhost:8000/api/soap/mantisconnect.php?wsdl';?>" > ./tests/bootstrap.php
+echo "<?php \$GLOBALS['MANTIS_TESTSUITE_SOAP_ENABLED'] = true;  \$GLOBALS['MANTIS_TESTSUITE_SOAP_HOST'] = 'http://localhost/api/soap/mantisconnect.php?wsdl';?>" > ./tests/bootstrap.php
