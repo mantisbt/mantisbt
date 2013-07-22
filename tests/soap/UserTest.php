f84b783ee2d6c131793a4481902b2244b9eeb7d3
Damien Regad <dregad@mantisbt.org>
2013-07-22 10:25:36 +0200

    UserTest: preferences should compare against default value
    Previously the test was comparing the retrieved user preference
    'bugnote_order' with a hardcoded value, so the test would fail if the
    config_inc settings were different.
    
    Fixes #16204

--- tests/soap/UserTest.php
+++ tests/soap/UserTest.php
@@ -32,10 +32,11 @@ class UserTest extends SoapBase {
      * Tests getting a user preference
      */
     public function testGetPreference() {
-        
+
+        $default_order = $this->client->mc_config_get_string( $this->userName, $this->password, 'default_bugnote_order' );
         $bugnote_order = $this->client->mc_user_pref_get_pref( $this->userName, $this->password, 0 /*ALL_PROJECTS*/, 'bugnote_order' );
 
-        $this->assertEquals( 'ASC', $bugnote_order );
-        
+        $this->assertEquals( $default_order, $bugnote_order );
+
     }
 }
