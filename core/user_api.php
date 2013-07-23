mantisbt/mantisbt

commit cc7703acc8d05f253d52a152a7d8cd0c1c43815d

Optimize user_get_all_accessible_projects()
On instances having a large number of projects, this function would
consume significant resources while processing all the subprojects to
determine if one is accessible to the user (about 25 seconds to load
main_page.php for 5'000 projects, without subprojects).

The performance bottleneck was the array_merge() call in the loop. This
has been replaced by a foreach working on an associative array. The same
page now loads under 1 second.

Fixes #9876

core/user_api.php
@@ -1008,12 +1008,18 @@ function user_get_all_accessible_subprojects( $p_user_id, $p_project_id ) {
 
 function user_get_all_accessible_projects( $p_user_id, $p_project_id ) {
 	if( ALL_PROJECTS == $p_project_id ) {
-		$t_topprojects = $t_project_ids = user_get_accessible_projects( $p_user_id );
-		foreach( $t_topprojects as $t_project ) {
-			$t_project_ids = array_merge( $t_project_ids, user_get_all_accessible_subprojects( $p_user_id, $t_project ) );
-		}
+		$t_topprojects = user_get_accessible_projects( $p_user_id );
 
-		$t_project_ids = array_unique( $t_project_ids );
+		# Create a combined array where key = value
+		$t_project_ids = array_combine( $t_topprojects, $t_topprojects );
+
+		# Add all subprojects user has access to
+		foreach( $t_topprojects as $t_project ) {
+			$t_subprojects_ids = user_get_all_accessible_subprojects( $p_user_id, $t_project );
+			foreach( $t_subprojects_ids as $t_id ) {
+				$t_project_ids[$t_id] = $t_id;
+			}
+		}
 	} else {
 		access_ensure_project_level( VIEWER, $p_project_id );
 		$t_project_ids = user_get_all_accessible_subprojects( $p_user_id, $p_project_id );

commit f9a07a0dabae07d1c7559400fd4862e97d2264e6

Fix #16187: error caused by array_combine() with PHP<5.4
array_combine() function behavior was changed in PHP 5.4 [1]; in earlier
versions it would issue a warning and return false when working with
empty arrays, which then causes Mantis to trigger an application error.

This is a regression introduced by cc7703acc8d05f253d52a152a7d8cd0c1c43815d

[1] http://php.net/function.array-combine.php

core/user_api.php
@@ -1010,6 +1010,12 @@ function user_get_all_accessible_projects( $p_user_id, $p_project_id ) {
 	if( ALL_PROJECTS == $p_project_id ) {
 		$t_topprojects = user_get_accessible_projects( $p_user_id );
 
+		# Cover the case for PHP < 5.4 where array_combine() returns
+		# false and triggers warning if arrays are empty (see #16187)
+		if( empty( $t_topprojects ) ) {
+			return array();
+		}
+
 		# Create a combined array where key = value
 		$t_project_ids = array_combine( $t_topprojects, $t_topprojects );
 