mantisbt/mantisbt

commit b1a1bbaeb690b7611899ae8075586d655155d18c

User with access to single project defaults to it, not all projects
Since 1.2, if a user has access to only one project, the project
selection box at top right isn't shown. This was causing issues as new
users accounts have 'All projects' as their default:

 - user can't report issues (no link for all projects)
 - categories displayed are prefixed with [project]

Until this commit, users had to define their (single) project as default
using preferences to work around the problem; this is no longer needed.

Function html_login_info() now sets the current project as appropriate
if user has only access to a single one, and also defines that as the
default.

The account preferences page has also been modified to only include 'All
projects' in the selection list when user has more than one project
available.

Fixes #9826, #14268

core/html_api.php
@@ -515,11 +515,11 @@ function html_login_info() {
 	echo "<span class=\"italic\">$t_now</span>";
 	echo '</td>';
 	echo '<td class="login-info-right">';
-	$t_show_project_selector = true;
-	if( count( current_user_get_accessible_projects() ) == 1 ) {
 
-		// >1
-		$t_project_ids = current_user_get_accessible_projects();
+	# Project Selector hidden if only one project visisble to user
+	$t_show_project_selector = true;
+	$t_project_ids = current_user_get_accessible_projects();
+	if( count( $t_project_ids ) == 1 ) {
 		$t_project_id = (int) $t_project_ids[0];
 		if( count( current_user_get_accessible_subprojects( $t_project_id ) ) == 0 ) {
 			$t_show_project_selector = false;
@@ -544,7 +544,17 @@ function html_login_info() {
 		}
 		echo '<input type="submit" class="button-small" value="' . lang_get( 'switch' ) . '" />';
 		echo '</form>';
+	} else {
+		# User has only one project, set it as both current and default
+		if( ALL_PROJECTS == helper_get_current_project() ) {
+			helper_set_current_project( $t_project_id );
+			current_user_set_default_project( $t_project_id );
+			# Force reload of current page
+			$t_redirect_url = str_replace( config_get( 'short_path' ), '', $_SERVER['REQUEST_URI'] );
+			html_meta_redirect( $t_redirect_url, 0, false );
+		}
 	}
+
 	if( OFF != config_get( 'rss_enabled' ) ) {
 
 		# Link to RSS issues feed for the selected project, including authentication details.

account_prefs_inc.php
@@ -75,7 +75,16 @@
 	</td>
 	<td width="50%">
 		<select name="default_project">
-			<?php print_project_option_list( $t_pref->default_project ) ?>
+<?php
+	# Count number of available projects
+	$t_projects = current_user_get_accessible_projects();
+	$t_num_proj = count( $t_projects );
+	if( $t_num_proj == 1 ) {
+		$t_num_proj += count( current_user_get_accessible_subprojects( $t_projects[0] ) );
+	}
+	# Only display "All projects" in selection list if there is more than 1
+	print_project_option_list( $t_pref->default_project, $t_num_proj > 1 );
+?>
 		</select>
 	</td>
 </tr>

commit 9ef5537b45b9474c20dd68893775c5755c692506

Do not redirect after creating the first project
When the first project is created after installing MantisBT, an
application error 2800 occurs when submitting the changes from
manage_proj_create_page.php, although the project is created
successfully.

Regression introduced by b1a1bbaeb690b7611899ae8075586d655155d18c

Fixes #16337

core/html_api.php
@@ -553,9 +553,12 @@ function html_login_info() {
 				current_user_set_default_project( $t_project_id );
 			}
 
-			# Force reload of current page
+			# Force reload of current page, except if we got here after
+			# creating the first project
 			$t_redirect_url = str_replace( config_get( 'short_path' ), '', $_SERVER['REQUEST_URI'] );
-			html_meta_redirect( $t_redirect_url, 0, false );
+			if( 'manage_proj_create.php' != $t_redirect_url ) {
+				html_meta_redirect( $t_redirect_url, 0, false );
+			}
 		}
 	}
 