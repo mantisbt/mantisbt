f286a94228a235145955cf5ff03fd7bcbbe62120
Paul Richards <paul@mantisforge.org>
2013-10-13 19:32:44 +0100

    Correctly use db_helper_like instead of LIKE within sql query

--- manage_user_page.php
+++ manage_user_page.php
@@ -191,8 +191,8 @@ if ( $f_filter === 'ALL' ) {
 } else if ( $f_filter === 'NEW' ) {
 	$t_where = db_helper_compare_days("" . db_now() . "","date_created","<= $days_old");
 } else {
-	$c_prefix = db_prepare_string($f_filter);
-	$t_where = "(UPPER(username) LIKE '$c_prefix%')";
+	$t_where_params[] = $f_filter . '%';
+	$t_where = db_helper_like( '(UPPER(username)' );
 }
 
 $p_per_page = 50;
