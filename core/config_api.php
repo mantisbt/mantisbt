mantisbt/mantisbt

commit a61aed3b0b1f89e7e978be486beeac97492a83c0

Further removal of legacy db_prepare_int calls

core/config_api.php
@@ -363,18 +363,13 @@ function config_set( $p_option, $p_value, $p_user = NO_USER, $p_project = ALL_PR
 		$c_value = (float) $p_value;
 	} else if( is_int( $p_value ) || is_numeric( $p_value ) ) {
 		$t_type = CONFIG_TYPE_INT;
-		$c_value = db_prepare_int( $p_value );
+		$c_value = (int) $p_value;
 	} else {
 		$t_type = CONFIG_TYPE_STRING;
 		$c_value = $p_value;
 	}
 
 	if( config_can_set_in_database( $p_option ) ) {
-		$c_option = $p_option;
-		$c_user = db_prepare_int( $p_user );
-		$c_project = db_prepare_int( $p_project );
-		$c_access = db_prepare_int( $p_access );
-
 		$t_config_table = db_get_table( 'config' );
 		$query = "SELECT COUNT(*) from $t_config_table
 				WHERE config_id = " . db_param() . " AND
@@ -390,12 +385,12 @@ function config_set( $p_option, $p_value, $p_user = NO_USER, $p_project = ALL_PR
 						project_id = " . db_param() . " AND
 						user_id = " . db_param();
 			$t_params = array(
-				(string)$c_value,
+				$c_value,
 				$t_type,
-				$c_access,
-				$c_option,
-				$c_project,
-				$c_user,
+				(int)$p_access,
+				$p_option,
+				(int)$p_project,
+				(int)$p_user,
 			);
 		} else {
 			$t_set_query = "INSERT INTO $t_config_table
@@ -403,12 +398,12 @@ function config_set( $p_option, $p_value, $p_user = NO_USER, $p_project = ALL_PR
 					VALUES
 					(" . db_param() . ', ' . db_param() . ', ' . db_param() . ', ' . db_param() . ', ' . db_param() . ',' . db_param() . ' )';
 			$t_params = array(
-				(string)$c_value,
+				$c_value,
 				$t_type,
-				$c_access,
-				$c_option,
-				$c_project,
-				$c_user,
+				(int)$p_access,
+				$p_option,
+				(int)$p_project,
+				(int)$p_user,
 			);
 		}
 
@@ -527,23 +522,22 @@ function config_delete( $p_option, $p_user = ALL_USERS, $p_project = ALL_PROJECT
 		# @@ debug @@ echo "lu table=" . ( db_table_exists( $t_config_table ) ? "yes" : "no" );
 		# @@ debug @@ error_print_stack_trace();
 
-		$c_user = db_prepare_int( $p_user );
-		$c_project = db_prepare_int( $p_project );
 		$query = "DELETE FROM $t_config_table
 				WHERE config_id = " . db_param() . " AND
 					project_id=" . db_param() . " AND
 					user_id=" . db_param();
 
-		$result = @db_query_bound( $query, array( $p_option, $c_project, $c_user ) );
+		$result = @db_query_bound( $query, array( $p_option, $p_project, $p_user ) );
 	}
 
 	config_flush_cache( $p_option, $p_user, $p_project );
 }
 
 /**
- * Delete the specified option for the specified user.across all projects.
- * @param $p_option - The configuration option to be deleted.
- * @param $p_user_id - The user id
+ * Delete the specified option for the specified user across all projects.
+ *
+ * @param string $p_option - The configuration option to be deleted.
+ * @param int $p_user_id - The user id
  */
 function config_delete_for_user( $p_option, $p_user_id ) {
 	if( !config_can_delete( $p_option ) ) {
@@ -551,12 +545,11 @@ function config_delete_for_user( $p_option, $p_user_id ) {
 	}
 
 	$t_config_table = db_get_table( 'config' );
-	$c_user_id = db_prepare_int( $p_user_id );
 
 	# Delete the corresponding bugnote texts
 	$query = "DELETE FROM $t_config_table
 					WHERE config_id=" . db_param() . " AND user_id=" . db_param();
-	db_query_bound( $query, array( $p_option, $c_user_id ) );
+	db_query_bound( $query, array( $p_option, $p_user_id ) );
 }
 
 /**
@@ -567,11 +560,10 @@ function config_delete_for_user( $p_option, $p_user_id ) {
 function config_delete_project( $p_project = ALL_PROJECTS ) {
 	global $g_cache_config, $g_cache_config_access;
 	$t_config_table = db_get_table( 'config' );
-	$c_project = db_prepare_int( $p_project );
 	$query = "DELETE FROM $t_config_table
 				WHERE project_id=" . db_param();
 
-	$result = @db_query_bound( $query, array( $c_project ) );
+	$result = @db_query_bound( $query, array( $p_project ) );
 
 	# flush cache here in case some of the deleted configs are in use.
 	config_flush_cache();

api/soap/mc_api.php
@@ -379,13 +379,13 @@ function mci_filter_db_get_available_queries( $p_project_id = null, $p_user_id =
 	if( null === $p_project_id ) {
 		$t_project_id = helper_get_current_project();
 	} else {
-		$t_project_id = db_prepare_int( $p_project_id );
+		$t_project_id = (int)$p_project_id;
 	}
 
 	if( null === $p_user_id ) {
 		$t_user_id = auth_get_current_user_id();
 	} else {
-		$t_user_id = db_prepare_int( $p_user_id );
+		$t_user_id = (int)$p_user_id;
 	}
 
 	# If the user doesn't have access rights to stored queries, just return

core/bug_api.php
@@ -1696,10 +1696,7 @@ function bug_set_field( $p_bug_id, $p_field_name, $p_value ) {
  * @uses database_api.php
  */
 function bug_assign( $p_bug_id, $p_user_id, $p_bugnote_text = '', $p_bugnote_private = false ) {
-	$c_bug_id = db_prepare_int( $p_bug_id );
-	$c_user_id = db_prepare_int( $p_user_id );
-
-	if(( $c_user_id != NO_USER ) && !access_has_bug_level( config_get( 'handle_bug_threshold' ), $p_bug_id, $p_user_id ) ) {
+	if(( $p_user_id != NO_USER ) && !access_has_bug_level( config_get( 'handle_bug_threshold' ), $p_bug_id, $p_user_id ) ) {
 		trigger_error( ERROR_USER_DOES_NOT_HAVE_REQ_ACCESS );
 	}
 
@@ -1721,11 +1718,11 @@ function bug_assign( $p_bug_id, $p_user_id, $p_bugnote_text = '', $p_bugnote_pri
 		$query = "UPDATE $t_bug_table
 					  SET handler_id=" . db_param() . ", status=" . db_param() . "
 					  WHERE id=" . db_param();
-		db_query_bound( $query, array( $c_user_id, $t_ass_val, $c_bug_id ) );
+		db_query_bound( $query, array( $p_user_id, $t_ass_val, $p_bug_id ) );
 
 		# log changes
-		history_log_event_direct( $c_bug_id, 'status', $h_status, $t_ass_val );
-		history_log_event_direct( $c_bug_id, 'handler_id', $h_handler_id, $p_user_id );
+		history_log_event_direct( $p_bug_id, 'status', $h_status, $t_ass_val );
+		history_log_event_direct( $p_bug_id, 'handler_id', $h_handler_id, $p_user_id );
 
 		# Add bugnote if supplied ignore false return
 		bugnote_add( $p_bug_id, $p_bugnote_text, 0, $p_bugnote_private, 0, '', NULL, FALSE );
@@ -1961,7 +1958,7 @@ function bug_get_monitors( $p_bug_id ) {
 		return array();
 	}
 
-	$c_bug_id = db_prepare_int( $p_bug_id );
+	$c_bug_id = (int)$p_bug_id;
 	$t_bug_monitor_table = db_get_table( 'bug_monitor' );
 	$t_user_table = db_get_table( 'user' );
 

core/bugnote_api.php
@@ -126,13 +126,12 @@ class BugnoteData {
  * @access public
  */
 function bugnote_exists( $p_bugnote_id ) {
-	$c_bugnote_id = db_prepare_int( $p_bugnote_id );
 	$t_bugnote_table = db_get_table( 'bugnote' );
 
 	$query = "SELECT COUNT(*)
 		          	FROM $t_bugnote_table
 		          	WHERE id=" . db_param();
-	$result = db_query_bound( $query, array( $c_bugnote_id ) );
+	$result = db_query_bound( $query, array( $p_bugnote_id ) );
 
 	if( 0 == db_result( $result ) ) {
 		return false;
@@ -187,11 +186,11 @@ function bugnote_is_user_reporter( $p_bugnote_id, $p_user_id ) {
  * @access public
  */
 function bugnote_add( $p_bug_id, $p_bugnote_text, $p_time_tracking = '0:00', $p_private = false, $p_type = BUGNOTE, $p_attr = '', $p_user_id = null, $p_send_email = TRUE, $p_date_submitted = 0, $p_last_modified = 0, $p_skip_bug_update = FALSE, $p_log_history = TRUE ) {
-	$c_bug_id = db_prepare_int( $p_bug_id );
+	$c_bug_id = (int)$p_bug_id;
 	$c_time_tracking = helper_duration_to_minutes( $p_time_tracking );
-	$c_type = db_prepare_int( $p_type );
-	$c_date_submitted = $p_date_submitted <= 0 ? db_now() : db_prepare_int( $p_date_submitted );
-	$c_last_modified = $p_last_modified <= 0 ? db_now() : db_prepare_int( $p_last_modified );
+	$c_type = (int)$p_type;
+	$c_date_submitted = $p_date_submitted <= 0 ? db_now() : (int)$p_date_submitted;
+	$c_last_modified = $p_last_modified <= 0 ? db_now() : (int)$p_last_modified;
 
 	if( REMINDER !== $p_type ) {
 		# Check if this is a time-tracking note
@@ -225,9 +224,7 @@ function bugnote_add( $p_bug_id, $p_bugnote_text, $p_time_tracking = '0:00', $p_
 
 	# get user information
 	if( $p_user_id === null ) {
-		$c_user_id = auth_get_current_user_id();
-	} else {
-		$c_user_id = db_prepare_int( $p_user_id );
+		$p_user_id = auth_get_current_user_id();
 	}
 
 	# Check for private bugnotes.
@@ -238,11 +235,11 @@ function bugnote_add( $p_bug_id, $p_bugnote_text, $p_time_tracking = '0:00', $p_
 	}
 
 	# insert bugnote info
-	$query = "INSERT INTO $t_bugnote_table
+	$t_query = "INSERT INTO $t_bugnote_table
 				(bug_id, reporter_id, bugnote_text_id, view_state, date_submitted, last_modified, note_type, note_attr, time_tracking )
 			VALUES
 				(" . db_param() . ', ' . db_param() . ',' . db_param() . ', ' . db_param() . ', ' . db_param() . ',' . db_param() . ', ' . db_param() . ', ' . db_param() . ', ' . db_param() . ' )';
-	db_query_bound( $query, array( $c_bug_id, $c_user_id, $t_bugnote_text_id, $t_view_state, $c_date_submitted, $c_last_modified, $c_type, $p_attr, $c_time_tracking ) );
+	db_query_bound( $t_query, array( $c_bug_id, $p_user_id, $t_bugnote_text_id, $t_view_state, $c_date_submitted, $c_last_modified, $c_type, $p_attr, $c_time_tracking ) );
 
 	# get bugnote id
 	$t_bugnote_id = db_insert_id( $t_bugnote_table );
@@ -274,7 +271,6 @@ function bugnote_add( $p_bug_id, $p_bugnote_text, $p_time_tracking = '0:00', $p_
  * @access public
  */
 function bugnote_delete( $p_bugnote_id ) {
-	$c_bugnote_id = db_prepare_int( $p_bugnote_id );
 	$t_bug_id = bugnote_get_field( $p_bugnote_id, 'bug_id' );
 	$t_bugnote_text_id = bugnote_get_field( $p_bugnote_id, 'bugnote_text_id' );
 	$t_bugnote_text_table = db_get_table( 'bugnote_text' );
@@ -282,7 +278,7 @@ function bugnote_delete( $p_bugnote_id ) {
 
 	# Remove the bugnote
 	$query = 'DELETE FROM ' . $t_bugnote_table . ' WHERE id=' . db_param();
-	db_query_bound( $query, array( $c_bugnote_id ) );
+	db_query_bound( $query, array( (int)$p_bugnote_id ) );
 
 	# Remove the bugnote text
 	$query = 'DELETE FROM ' . $t_bugnote_text_table . ' WHERE id=' . db_param();
@@ -301,7 +297,6 @@ function bugnote_delete( $p_bugnote_id ) {
  * @access public
  */
 function bugnote_delete_all( $p_bug_id ) {
-	$c_bug_id = db_prepare_int( $p_bug_id );
 	$t_bugnote_table = db_get_table( 'bugnote' );
 	$t_bugnote_text_table = db_get_table( 'bugnote_text' );
 
@@ -309,7 +304,7 @@ function bugnote_delete_all( $p_bug_id ) {
 	$query = "SELECT bugnote_text_id
 		          	FROM $t_bugnote_table
 		          	WHERE bug_id=" . db_param();
-	$result = db_query_bound( $query, array( $c_bug_id ) );
+	$result = db_query_bound( $query, array( (int)$p_bug_id ) );
 	$bugnote_count = db_num_rows( $result );
 	for( $i = 0;$i < $bugnote_count;$i++ ) {
 		$row = db_fetch_array( $result );
@@ -324,7 +319,7 @@ function bugnote_delete_all( $p_bug_id ) {
 	# Delete the corresponding bugnotes
 	$query = "DELETE FROM $t_bugnote_table
 		          	WHERE bug_id=" . db_param();
-	$result = db_query_bound( $query, array( $c_bug_id ) );
+	$result = db_query_bound( $query, array( (int)$p_bug_id ) );
 
 	# db_query errors on failure so:
 	return true;
@@ -390,16 +385,15 @@ function bugnote_get_field( $p_bugnote_id, $p_field_name ) {
  * @access public
  */
 function bugnote_get_latest_id( $p_bug_id ) {
-	$c_bug_id = db_prepare_int( $p_bug_id );
 	$t_bugnote_table = db_get_table( 'bugnote' );
 
-	$query = "SELECT id
+	$t_query = "SELECT id
 		          	FROM $t_bugnote_table
 		          	WHERE bug_id=" . db_param() . "
 		          	ORDER by last_modified DESC";
-	$result = db_query_bound( $query, array( $c_bug_id ), 1 );
+	$t_result = db_query_bound( $t_query, array( (int)$p_bug_id ), 1 );
 
-	return (int)db_result( $result );
+	return (int)db_result( $t_result );
 }
 
 /**
@@ -529,14 +523,13 @@ function bugnote_get_all_bugnotes( $p_bug_id ) {
  * @access public
  */
 function bugnote_set_time_tracking( $p_bugnote_id, $p_time_tracking ) {
-	$c_bugnote_id = db_prepare_int( $p_bugnote_id );
 	$c_bugnote_time_tracking = helper_duration_to_minutes( $p_time_tracking );
 	$t_bugnote_table = db_get_table( 'bugnote' );
 
 	$query = "UPDATE $t_bugnote_table
 				SET time_tracking = " . db_param() . "
 				WHERE id=" . db_param();
-	db_query_bound( $query, array( $c_bugnote_time_tracking, $c_bugnote_id ) );
+	db_query_bound( $query, array( $c_bugnote_time_tracking, $p_bugnote_id ) );
 
 	# db_query errors if there was a problem so:
 	return true;
@@ -549,13 +542,12 @@ function bugnote_set_time_tracking( $p_bugnote_id, $p_time_tracking ) {
  * @access public
  */
 function bugnote_date_update( $p_bugnote_id ) {
-	$c_bugnote_id = db_prepare_int( $p_bugnote_id );
 	$t_bugnote_table = db_get_table( 'bugnote' );
 
 	$query = "UPDATE $t_bugnote_table
 					SET last_modified=" . db_param() . "
 					WHERE id=" . db_param();
-	db_query_bound( $query, array( db_now(), $c_bugnote_id ) );
+	db_query_bound( $query, array( db_now(), $p_bugnote_id ) );
 
 	# db_query errors if there was a problem so:
 	return true;
@@ -612,7 +604,6 @@ function bugnote_set_text( $p_bugnote_id, $p_bugnote_text ) {
  * @access public
  */
 function bugnote_set_view_state( $p_bugnote_id, $p_private ) {
-	$c_bugnote_id = db_prepare_int( $p_bugnote_id );
 	$t_bug_id = bugnote_get_field( $p_bugnote_id, 'bug_id' );
 
 	if( $p_private ) {
@@ -623,10 +614,10 @@ function bugnote_set_view_state( $p_bugnote_id, $p_private ) {
 
 	$t_bugnote_table = db_get_table( 'bugnote' );
 
-	$query = "UPDATE $t_bugnote_table
+	$t_query = "UPDATE $t_bugnote_table
 		          	SET view_state=" . db_param() . "
 		          	WHERE id=" . db_param();
-	db_query_bound( $query, array( $t_view_state, $c_bugnote_id ) );
+	db_query_bound( $t_query, array( $t_view_state, $p_bugnote_id ) );
 
 	history_log_event_special( $t_bug_id, BUGNOTE_STATE_CHANGED, $t_view_state, bugnote_format_id( $p_bugnote_id ) );
 
@@ -654,7 +645,6 @@ function bugnote_format_id( $p_bugnote_id ) {
  * @access public
  */
 function bugnote_stats_get_events_array( $p_bug_id, $p_from, $p_to ) {
-	$c_bug_id = db_prepare_int( $p_bug_id );
 	$c_to = strtotime( $p_to ) + SECONDS_PER_DAY - 1;
 	$c_from = strtotime( $p_from );
 
@@ -675,17 +665,17 @@ function bugnote_stats_get_events_array( $p_bug_id, $p_from, $p_to ) {
 
 	$t_results = array();
 
-	$query = "SELECT username, realname, SUM(time_tracking) AS sum_time_tracking
+	$t_query = "SELECT username, realname, SUM(time_tracking) AS sum_time_tracking
 				FROM $t_user_table u, $t_bugnote_table bn
 				WHERE u.id = bn.reporter_id AND bn.time_tracking != 0 AND
 				bn.bug_id = '$c_bug_id'
 				$t_from_where $t_to_where
 				GROUP BY u.username, u.realname";
 
-	$result = db_query( $query );
+	$t_result = db_query_bound( $t_query, array( $p_bug_id ) );
 
-	while( $row = db_fetch_array( $result ) ) {
-		$t_results[] = $row;
+	while( $t_row = db_fetch_array( $t_result ) ) {
+		$t_results[] = $t_row;
 	}
 
 	return $t_results;

core/category_api.php
@@ -61,15 +61,12 @@ function category_exists( $p_category_id ) {
 		return true;
 	}
 
-	$c_category_id = db_prepare_int( $p_category_id );
-
 	$t_category_table = db_get_table( 'category' );
 
-	$query = "SELECT COUNT(*) FROM $t_category_table
-					WHERE id=" . db_param();
-	$count = db_result( db_query_bound( $query, array( $c_category_id ) ) );
+	$t_query = "SELECT COUNT(*) FROM $t_category_table WHERE id=" . db_param();
+	$t_count = db_result( db_query_bound( $t_query, array( (int)$p_category_id ) ) );
 
-	if( 0 < $count ) {
+	if( 0 < $t_count ) {
 		return true;
 	} else {
 		return false;
@@ -96,13 +93,11 @@ function category_exists( $p_category_id ) {
  * @access public
  */
  function category_is_unique( $p_project_id, $p_name ) {
-	$c_project_id = db_prepare_int( $p_project_id );
-
 	$t_category_table = db_get_table( 'category' );
 
 	$query = "SELECT COUNT(*) FROM $t_category_table
 					WHERE project_id=" . db_param() . " AND " . db_helper_like( 'name' );
-	$count = db_result( db_query_bound( $query, array( $c_project_id, $p_name ) ) );
+	$count = db_result( db_query_bound( $query, array( $p_project_id, $p_name ) ) );
 
 	if( 0 < $count ) {
 		return false;
@@ -133,8 +128,6 @@ function category_exists( $p_category_id ) {
  * @access public
  */
  function category_add( $p_project_id, $p_name ) {
-	$c_project_id = db_prepare_int( $p_project_id );
-
 	if( is_blank( $p_name ) ) {
 		error_parameters( lang_get( 'category' ) );
 		trigger_error( ERROR_EMPTY_FIELD, ERROR );
@@ -144,11 +137,11 @@ function category_exists( $p_category_id ) {
 
 	$t_category_table = db_get_table( 'category' );
 
-	$query = "INSERT INTO $t_category_table
+	$t_query = "INSERT INTO $t_category_table
 					( project_id, name )
 				  VALUES
 					( " . db_param() . ', ' . db_param() . ' )';
-	db_query_bound( $query, array( $c_project_id, $p_name ) );
+	db_query_bound( $query, array( $p_project_id, $p_name ) );
 
 	# db_query errors on failure so:
 	return db_insert_id( $t_category_table );
@@ -170,22 +163,19 @@ function category_exists( $p_category_id ) {
 
 	$t_old_category = category_get_row( $p_category_id );
 
-	$c_category_id = db_prepare_int( $p_category_id );
-	$c_assigned_to = db_prepare_int( $p_assigned_to );
-
 	$t_category_table = db_get_table( 'category' );
-	$t_bug_table = db_get_table( 'bug' );
 
 	$query = "UPDATE $t_category_table
 				  SET name=" . db_param() . ',
 					user_id=' . db_param() . '
 				  WHERE id=' . db_param();
-	db_query_bound( $query, array( $p_name, $c_assigned_to, $c_category_id ) );
+	db_query_bound( $query, array( $p_name, $p_assigned_to, $p_category_id ) );
 
 	# Add bug history entries if we update the category's name
 	if( $t_old_category['name'] != $p_name ) {
+		$t_bug_table = db_get_table( 'bug' );
 		$query = "SELECT id FROM $t_bug_table WHERE category_id=" . db_param();
-		$t_result = db_query_bound( $query, array( $c_category_id ) );
+		$t_result = db_query_bound( $query, array( $p_category_id ) );
 
 		while( $t_bug_row = db_fetch_array( $t_result ) ) {
 			history_log_event_direct( $t_bug_row['id'], 'category', $t_old_category['name'], $p_name );
@@ -206,9 +196,6 @@ function category_exists( $p_category_id ) {
  function category_remove( $p_category_id, $p_new_category_id = 0 ) {
 	$t_category_row = category_get_row( $p_category_id );
 
-	$c_category_id = db_prepare_int( $p_category_id );
-	$c_new_category_id = db_prepare_int( $p_new_category_id );
-
 	category_ensure_exists( $p_category_id );
 	if( 0 != $p_new_category_id ) {
 		category_ensure_exists( $p_new_category_id );
@@ -217,23 +204,23 @@ function category_exists( $p_category_id ) {
 	$t_category_table = db_get_table( 'category' );
 	$t_bug_table = db_get_table( 'bug' );
 
-	$query = "DELETE FROM $t_category_table
+	$t_query = "DELETE FROM $t_category_table
 				  WHERE id=" . db_param();
-	db_query_bound( $query, array( $c_category_id ) );
+	db_query_bound( $t_query, array( $p_category_id ) );
 
 	# update bug history entries
 	$query = "SELECT id FROM $t_bug_table WHERE category_id=" . db_param();
-	$t_result = db_query_bound( $query, array( $c_category_id ) );
+	$t_result = db_query_bound( $query, array( $p_category_id ) );
 
 	while( $t_bug_row = db_fetch_array( $t_result ) ) {
 		history_log_event_direct( $t_bug_row['id'], 'category', $t_category_row['name'], category_full_name( $p_new_category_id, false ) );
 	}
 
 	# update bug data
-	$query = "UPDATE $t_bug_table
+	$t_query = "UPDATE $t_bug_table
 				  SET category_id=" . db_param() . "
 				  WHERE category_id=" . db_param();
-	db_query_bound( $query, array( $c_new_category_id, $c_category_id ) );
+	db_query_bound( $t_query, array( $p_new_category_id, $p_category_id ) );
 
 	# db_query errors on failure so:
 	return true;
@@ -247,7 +234,6 @@ function category_exists( $p_category_id ) {
  * @access public
  */
  function category_remove_all( $p_project_id, $p_new_category_id = 0 ) {
-
 	project_ensure_exists( $p_project_id );
 	if( 0 != $p_new_category_id ) {
 		category_ensure_exists( $p_new_category_id );
@@ -302,18 +288,19 @@ function category_exists( $p_category_id ) {
  */
  function category_get_row( $p_category_id ) {
 	global $g_category_cache;
+
+	$p_category_id = (int)$p_category_id;
+
 	if( isset( $g_category_cache[$p_category_id] ) ) {
 		return $g_category_cache[$p_category_id];
 	}
-
-	$c_category_id = db_prepare_int( $p_category_id );
 
 	$t_category_table = db_get_table( 'category' );
 	$t_project_table = db_get_table( 'project' );
 
 	$query = "SELECT * FROM $t_category_table
 				WHERE id=" . db_param();
-	$result = db_query_bound( $query, array( $c_category_id ) );
+	$result = db_query_bound( $query, array( $p_category_id ) );
 	$count = db_num_rows( $result );
 	if( 0 == $count ) {
 		trigger_error( ERROR_CATEGORY_NOT_FOUND, ERROR );
@@ -335,7 +322,6 @@ function category_exists( $p_category_id ) {
  function category_sort_rows_by_project( $p_category1, $p_category2 = null ) {
 	static $p_project_id = null;
 	if( is_null( $p_category2 ) ) {
-
 		# Set a target project
 		$p_project_id = $p_category1;
 		return;
@@ -383,12 +369,12 @@ function category_cache_array_rows_by_project( $p_project_id_array ) {
 	$t_category_table = db_get_table( 'category' );
 	$t_project_table = db_get_table( 'project' );
 
-	$query = "SELECT c.*, p.name AS project_name FROM $t_category_table c
+	$t_query = "SELECT c.*, p.name AS project_name FROM $t_category_table c
 				LEFT JOIN $t_project_table p
 					ON c.project_id=p.id
 				WHERE project_id IN ( " . implode( ', ', $c_project_id_array ) . " )
 				ORDER BY c.name ";
-	$result = db_query_bound( $query );
+	$t_result = db_query_bound( $t_query );
 
 	$rows = array();
 	while( $row = db_fetch_array( $result ) ) {
@@ -522,7 +508,7 @@ function category_get_filter_list( $p_project_id = null ) {
 }
 
 /**
- *
+ * Cache an set of category ids
  * @param array $p_cat_id_array array of category id's
  * @return null
  * @access public
@@ -560,7 +546,7 @@ function category_cache_array_rows( $p_cat_id_array ) {
  * Given a category id and a field name, this function returns the field value.
  * An error will be triggered for a non-existent category id or category id = 0.
  * @param int $p_category_id category id
- * @param string $p_name field name
+ * @param string $p_field_name field name
  * @return string field value
  * @access public
  */

commit c30a1085379c55e63e3fcf0291c24079836d5219

Fix broken config_set()
Commit a61aed3b0b1f89e7e978be486beeac97492a83c0 introduced a major
regression, preventing installation and potentially any update of
existing config keys.

This was due to calling db_query_bound() with uninitialized variables.

Fixes #16480

core/config_api.php
@@ -375,7 +375,12 @@ function config_set( $p_option, $p_value, $p_user = NO_USER, $p_project = ALL_PR
 				WHERE config_id = " . db_param() . " AND
 					project_id = " . db_param() . " AND
 					user_id = " . db_param();
-		$result = db_query_bound( $query, array( $c_option, $c_project, $c_user ) );
+		$t_params = array(
+			$p_option,
+			(int)$p_project,
+			(int)$c_user,
+		);
+		$result = db_query_bound( $query, $t_params );
 
 		$t_params = array();
 		if( 0 < db_result( $result ) ) {