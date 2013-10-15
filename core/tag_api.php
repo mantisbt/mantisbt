mantisbt/mantisbt

commit a80f447687722b52160d00c96c21a5be5f4329e1

Further backports of db_int*/phpdoc*

core/tag_api.php
@@ -80,22 +80,25 @@ function tag_ensure_exists( $p_tag_id ) {
 /**
  * Determine if a given name is unique (not already used).
  * Uses a case-insensitive search of the database for existing tags with the same name.
- * @param string Tag name
+ * @param string $p_name Tag name
  * @return boolean True if name is unique
  */
 function tag_is_unique( $p_name ) {
 	$c_name = trim( $p_name );
 	$t_tag_table = db_get_table( 'tag' );
 
-	$query = 'SELECT id FROM ' . $t_tag_table . ' WHERE ' . db_helper_like( 'name' );
-	$result = db_query_bound( $query, array( $c_name ) );
+	$t_query = 'SELECT id FROM ' . $t_tag_table . ' WHERE ' . db_helper_like( 'name' );
+	$result = db_query_bound( $t_query, array( $c_name ) );
 
-	return db_num_rows( $result ) == 0;
+	if ( db_result( $result ) ) {
+		return false;
+	}
+	return true;
 }
 
 /**
  * Ensure that a name is unique.
- * @param string Tag name
+ * @param string $p_name Tag name
  */
 function tag_ensure_unique( $p_name ) {
 	if( !tag_is_unique( $p_name ) ) {
@@ -287,14 +290,15 @@ function tag_get( $p_tag_id ) {
 
 	$t_tag_table = db_get_table( 'tag' );
 	$query = "SELECT * FROM $t_tag_table WHERE id=" . db_param();
-	$result = db_query_bound( $query, array( $p_tag_id ) );
+	$t_result = db_query_bound( $query, array( $p_tag_id ) );
 
-	if( 0 == db_num_rows( $result ) ) {
+	$t_row = db_fetch_array( $t_result );
+
+	if( !$t_row ) {
 		return false;
 	}
-	$row = db_fetch_array( $result );
 
-	return $row;
+	return $t_row;
 }
 
 /**
@@ -304,17 +308,17 @@ function tag_get( $p_tag_id ) {
  */
 function tag_get_by_name( $p_name ) {
 	$t_tag_table = db_get_table( 'tag' );
-
 	$query = "SELECT * FROM $t_tag_table
 					WHERE " . db_helper_like( 'name' );
 	$result = db_query_bound( $query, array( $p_name ) );
 
-	if( 0 == db_num_rows( $result ) ) {
+	$t_row = db_fetch_array( $t_result );
+
+	if( !$t_row ) {
 		return false;
 	}
-	$row = db_fetch_array( $result );
 
-	return $row;
+	return $t_row;
 }
 
 /**
@@ -513,20 +517,18 @@ function tag_get_candidates_for_bug( $p_bug_id ) {
 	return $t_results_to_return;
 }
 
-# Associative
 /**
  * Determine if a tag is attached to a bug.
- * @param integer Tag ID
- * @param integer Bug ID
+ * @param integer $p_tag_id Tag ID
+ * @param integer $p_bug_id Bug ID
  * @return boolean True if the tag is attached
  */
 function tag_bug_is_attached( $p_tag_id, $p_bug_id ) {
 	$t_bug_tag_table = db_get_table( 'bug_tag' );
-
-	$query = "SELECT * FROM $t_bug_tag_table
+	$query = "SELECT id FROM $t_bug_tag_table
 					WHERE tag_id=" . db_param() . " AND bug_id=" . db_param();
 	$result = db_query_bound( $query, array( $p_tag_id, $p_bug_id ) );
-	return( db_num_rows( $result ) > 0 );
+	return( db_result( $result ) > 0 );
 }
 
 /**
@@ -537,21 +539,21 @@ function tag_bug_is_attached( $p_tag_id, $p_bug_id ) {
  */
 function tag_bug_get_row( $p_tag_id, $p_bug_id ) {
 	$t_bug_tag_table = db_get_table( 'bug_tag' );
-
 	$query = "SELECT * FROM $t_bug_tag_table
 					WHERE tag_id=" . db_param() . " AND bug_id=" . db_param();
 	$result = db_query_bound( $query, array( $p_tag_id, $p_bug_id ) );
 
-	if( db_num_rows( $result ) == 0 ) {
+	$t_row = db_fetch_array( $result );
+	if( !$t_row ) {
 		trigger_error( TAG_NOT_ATTACHED, ERROR );
 	}
-	return db_fetch_array( $result );
+	return $t_row;
 }
 
 /**
  * Return an array of tags attached to a given bug sorted by tag name.
- * @param Bug ID
- * @return array array of tag rows with attachement information
+ * @param int $p_bug_id Bug ID
+ * @return array Array of tag rows with attachement information
  */
 function tag_bug_get_attached( $p_bug_id ) {
 	$t_tag_table = db_get_table( 'tag' );
@@ -595,9 +597,10 @@ function tag_get_bugs_attached( $p_tag_id ) {
 
 /**
  * Attach a tag to a bug.
- * @param integer Tag ID
- * @param integer Bug ID
- * @param integer User ID
+ * @param integer $p_tag_id Tag ID
+ * @param integer $p_bug_id Bug ID
+ * @param integer $p_user_id User ID
+ * @return bool
  */
 function tag_bug_attach( $p_tag_id, $p_bug_id, $p_user_id = null ) {
 	access_ensure_bug_level( config_get( 'tag_attach_threshold' ), $p_bug_id, $p_user_id );
@@ -641,10 +644,11 @@ function tag_bug_attach( $p_tag_id, $p_bug_id, $p_user_id = null ) {
 
 /**
  * Detach a tag from a bug.
- * @param integer Tag ID
- * @param integer Bug ID
- * @param boolean Add history entries to bug
- * @param integer User Id (or null for current logged in user)
+ * @param integer $p_tag_id Tag ID
+ * @param integer $p_bug_id Bug ID
+ * @param boolean $p_add_history Add history entries to bug
+ * @param integer $p_user_id User Id (or null for current logged in user)
+ * @return bool
  */
 function tag_bug_detach( $p_tag_id, $p_bug_id, $p_add_history = true, $p_user_id = null ) {
 	if( $p_user_id === null ) {
@@ -684,9 +688,9 @@ function tag_bug_detach( $p_tag_id, $p_bug_id, $p_add_history = true, $p_user_id
 
 /**
  * Detach all tags from a given bug.
- * @param integer Bug ID
- * @param boolean Add history entries to bug
- * @param integer User Id (or null for current logged in user)
+ * @param integer $p_bug_id Bug ID
+ * @param boolean $p_add_history Add history entries to bug
+ * @param integer $p_user_id User Id (or null for current logged in user)
  */
 function tag_bug_detach_all( $p_bug_id, $p_add_history = true, $p_user_id = null ) {
 	$t_tags = tag_bug_get_attached( $p_bug_id );
@@ -730,7 +734,8 @@ function tag_display_link( $p_tag_row, $p_bug_id = 0 ) {
 
 /**
  * Display a list of attached tag hyperlinks separated by the configured hyperlinks.
- * @param Bug ID
+ * @param int $p_bug_id Bug ID
+ * @return bool
  */
 function tag_display_attached( $p_bug_id ) {
 	$t_tag_rows = tag_bug_get_attached( $p_bug_id );
@@ -751,16 +756,15 @@ function tag_display_attached( $p_bug_id ) {
 
 /**
  * Get the number of bugs a given tag is attached to.
- * @param integer Tag ID
+ * @param integer $p_tag_id Tag ID
  * @return integer Number of attached bugs
  */
 function tag_stats_attached( $p_tag_id ) {
-	$c_tag_id = (int)$p_tag_id;
 	$t_bug_tag_table = db_get_table( 'bug_tag' );
 
 	$query = "SELECT COUNT(*) FROM $t_bug_tag_table
 					WHERE tag_id=" . db_param();
-	$result = db_query_bound( $query, array( $c_tag_id ) );
+	$result = db_query_bound( $query, array( $p_tag_id ) );
 
 	return db_result( $result );
 }
@@ -770,9 +774,9 @@ function tag_stats_attached( $p_tag_id ) {
  * Returns a list of tags that are the most related to the given tag,
  * based on the number of times they have been attached to the same bugs.
  * Defaults to a list of five tags.
- * @param integer Tag ID
- * @param integer List size
- * @return array array of tag rows, with share count added
+ * @param integer $p_tag_id Tag ID
+ * @param integer $p_limit List size
+ * @return array Array of tag rows, with share count added
  */
 function tag_stats_related( $p_tag_id, $p_limit = 5 ) {
 	$t_bug_table = db_get_table( 'bug' );

admin/install.php
@@ -203,7 +203,9 @@ if( $t_config_exists ) {
 	if( $g_db->IsConnected() ) {
 		$g_db_connected = true;
 	}
+
 	$t_cur_version = config_get( 'database_version', -1 );
+	
 	if( $t_cur_version > 1 ) {
 		$g_database_upgrade = true;
 		$f_db_exists = true;

core/print_api.php
@@ -1632,23 +1632,30 @@ function print_lost_password_link() {
 	}
 }
 
-# ===============================
-# Deprecated Functions
-# ===============================
-
-# Get icon corresponding to the specified filename
+/**
+ * Get icon corresponding to the specified filename
+ *
+ * @param string $p_filename Filename for which to retrieve icon link
+ */
 function print_file_icon( $p_filename ) {
 	$t_icon = file_get_icon_url( $p_filename );
 	echo '<img src="' . string_attribute( $t_icon['url'] ) . '" alt="' . string_attribute( $t_icon['alt'] ) . ' file icon" width="16" height="16" />';
 }
 
-# Prints an RSS image that is hyperlinked to an RSS feed.
+/**
+ * Prints an RSS image that is hyperlinked to an RSS feed.
+ *
+ * @param string $p_feed_url URI to an RSS feed
+ * @param string $p_title Title to use for hyperlink
+ */
 function print_rss( $p_feed_url, $p_title = '' ) {
 	$t_path = config_get( 'path' );
 	echo '<a class="rss" rel="alternate" href="', htmlspecialchars( $p_feed_url ), '" title="', $p_title, '"><img src="', $t_path, '/images/', 'rss.png" width="16" height="16" alt="', $p_title, '" /></a>';
 }
 
-# Prints the recently visited issues.
+/**
+ * Prints the recently visited issues.
+ */
 function print_recently_visited() {
 	if( !last_visited_enabled() ) {
 		return;

core/profile_api.php
@@ -212,13 +212,12 @@ function profile_get_all_rows( $p_user_id, $p_all_users = false ) {
 				  FROM $t_user_profile_table
 				  WHERE $query_where
 				  ORDER BY platform, os, os_build";
-	$result = db_query_bound( $query, $param );
+	$t_result = db_query_bound( $query, $param );
 
 	$t_rows = array();
-	$t_row_count = db_num_rows( $result );
 
-	for( $i = 0;$i < $t_row_count;$i++ ) {
-		array_push( $t_rows, db_fetch_array( $result ) );
+	while( $t_row = db_fetch_array( $t_result ) ) {
+		array_push( $t_rows, $t_row );
 	}
 
 	return $t_rows;
@@ -263,14 +262,11 @@ function profile_get_field_all_for_user( $p_field, $p_user_id = null ) {
 				  FROM $t_user_profile_table
 				  WHERE ( user_id=" . db_param() . " ) OR ( user_id = 0 )
 				  ORDER BY $c_field";
-	$result = db_query_bound( $query, array( $c_user_id ) );
+	$t_result = db_query_bound( $query, array( $c_user_id ) );
 
 	$t_rows = array();
 
-	$t_row_count = db_num_rows( $result );
-
-	for( $i = 0;$i < $t_row_count;$i++ ) {
-		$t_row = db_fetch_array( $result );
+	while( $t_row = db_fetch_array( $t_result ) ) {
 		array_push( $t_rows, $t_row[$c_field] );
 	}
 
@@ -294,13 +290,12 @@ function profile_get_all_for_project( $p_project_id ) {
 				  WHERE $t_project_where
 				  AND up.id = b.profile_id
 				  ORDER BY platform, os, os_build";
-	$result = db_query_bound( $query );
+	$t_result = db_query_bound( $query );
 
 	$t_rows = array();
-	$t_row_count = db_num_rows( $result );
 
-	for( $i = 0;$i < $t_row_count;$i++ ) {
-		array_push( $t_rows, db_fetch_array( $result ) );
+	while( $t_row = db_fetch_array( $t_result ) ) {
+		array_push( $t_rows, $t_row );
 	}
 
 	return $t_rows;

core/project_hierarchy_api.php
@@ -161,10 +161,10 @@ function project_hierarchy_cache( $p_show_disabled = false ) {
 	}
 	$g_cache_show_disabled = $p_show_disabled;
 
-	$t_project_table = db_get_table( 'project' );
-	$t_project_hierarchy_table = db_get_table( 'project_hierarchy' );
 	$t_enabled_clause = $p_show_disabled ? '1=1' : 'p.enabled = ' . db_param();
 
+	$t_project_table = db_get_table( 'project' );
+	$t_project_hierarchy_table = db_get_table( 'project_hierarchy' );
 	$query = "SELECT DISTINCT p.id, ph.parent_id, p.name, p.inherit_global, ph.inherit_parent
 				  FROM $t_project_table p
 				  LEFT JOIN $t_project_hierarchy_table ph
@@ -173,14 +173,11 @@ function project_hierarchy_cache( $p_show_disabled = false ) {
 				  ORDER BY p.name";
 
 	$result = db_query_bound( $query, ( $p_show_disabled ? null : array( true ) ) );
-	$row_count = db_num_rows( $result );
 
 	$g_cache_project_hierarchy = array();
 	$g_cache_project_inheritance = array();
 
-	for( $i = 0;$i < $row_count;$i++ ) {
-		$row = db_fetch_array( $result );
-
+	while ( $row = db_fetch_array( $result ) ) {
 		if( null === $row['parent_id'] ) {
 			$row['parent_id'] = ALL_PROJECTS;
 		}

core/relationship_api.php
@@ -318,17 +318,12 @@ function relationship_copy_all( $p_bug_id, $p_new_bug_id ) {
  */
 function relationship_get( $p_relationship_id ) {
 	$t_mantis_bug_relationship_table = db_get_table( 'bug_relationship' );
-
-	$query = "SELECT *
-				FROM $t_mantis_bug_relationship_table
-				WHERE id=" . db_param();
+	$query = "SELECT * FROM $t_mantis_bug_relationship_table WHERE id=" . db_param();
 	$result = db_query_bound( $query, array( (int) $p_relationship_id ) );
 
-	$t_relationship_count = db_num_rows( $result );
+	$t_relationship = db_fetch_array( $result );
 
-	if( $t_relationship_count == 1 ) {
-		$t_relationship = db_fetch_array( $result );
-
+	if( $t_relationship ) {
 		$t_bug_relationship_data = new BugRelationshipData;
 		$t_bug_relationship_data->id = $t_relationship['id'];
 		$t_bug_relationship_data->src_bug_id = $t_relationship['source_bug_id'];
@@ -344,14 +339,11 @@ function relationship_get( $p_relationship_id ) {
 /**
  * get all relationships with the given bug as source
  * @param int $p_src_bug_id Source Bug id
- * @return array array of BugRelationshipData objects
+ * @return array Array of BugRelationshipData objects
  */
 function relationship_get_all_src( $p_src_bug_id ) {
-	$c_src_bug_id = db_prepare_int( $p_src_bug_id );
-
 	$t_mantis_bug_relationship_table = db_get_table( 'bug_relationship' );
 	$t_mantis_bug_table = db_get_table( 'bug' );
-
 	$query = "SELECT $t_mantis_bug_relationship_table.id, $t_mantis_bug_relationship_table.relationship_type,
 				$t_mantis_bug_relationship_table.source_bug_id, $t_mantis_bug_relationship_table.destination_bug_id,
 				$t_mantis_bug_table.project_id
@@ -359,15 +351,15 @@ function relationship_get_all_src( $p_src_bug_id ) {
 				INNER JOIN $t_mantis_bug_table ON $t_mantis_bug_relationship_table.destination_bug_id = $t_mantis_bug_table.id
 				WHERE source_bug_id=" . db_param() . "
 				ORDER BY relationship_type, $t_mantis_bug_relationship_table.id";
-	$result = db_query_bound( $query, array( $c_src_bug_id ) );
+	$result = db_query_bound( $query, array( $p_src_bug_id ) );
 
 	$t_src_project_id = bug_get_field( $p_src_bug_id, 'project_id' );
 
 	$t_bug_relationship_data = array();
-	$t_relationship_count = db_num_rows( $result );
 	$t_bug_array = array();
-	for( $i = 0;$i < $t_relationship_count;$i++ ) {
-		$row = db_fetch_array( $result );
+	$i = 0;
+
+	while( $row = db_fetch_array( $result ) ) {
 		$t_bug_relationship_data[$i] = new BugRelationshipData;
 		$t_bug_relationship_data[$i]->id = $row['id'];
 		$t_bug_relationship_data[$i]->src_bug_id = $row['source_bug_id'];
@@ -376,8 +368,11 @@ function relationship_get_all_src( $p_src_bug_id ) {
 		$t_bug_relationship_data[$i]->dest_project_id = $row['project_id'];
 		$t_bug_relationship_data[$i]->type = $row['relationship_type'];
 		$t_bug_array[] = $row['destination_bug_id'];
+		$i++;
 	}
-	unset( $t_bug_relationship_data[$t_relationship_count] );
+
+	unset( $t_bug_relationship_data[$i] );
+
 	if( !empty( $t_bug_array ) ) {
 		bug_cache_array_rows( $t_bug_array );
 	}
@@ -388,7 +383,7 @@ function relationship_get_all_src( $p_src_bug_id ) {
 /**
  * get all relationships with the given bug as destination
  * @param int $p_dest_bug_id Destination Bug id
- * @return array array of BugRelationshipData objects
+ * @return array Array of BugRelationshipData objects
  */
 function relationship_get_all_dest( $p_dest_bug_id ) {
 	$c_dest_bug_id = db_prepare_int( $p_dest_bug_id );
@@ -408,10 +403,10 @@ function relationship_get_all_dest( $p_dest_bug_id ) {
 	$t_dest_project_id = bug_get_field( $p_dest_bug_id, 'project_id' );
 
 	$t_bug_relationship_data = array();
-	$t_relationship_count = db_num_rows( $result );
 	$t_bug_array = array();
-	for( $i = 0;$i < $t_relationship_count;$i++ ) {
-		$row = db_fetch_array( $result );
+	$i = 0;
+
+	while( $row = db_fetch_array( $result ) ) {
 		$t_bug_relationship_data[$i] = new BugRelationshipData;
 		$t_bug_relationship_data[$i]->id = $row['id'];
 		$t_bug_relationship_data[$i]->src_bug_id = $row['source_bug_id'];
@@ -420,8 +415,9 @@ function relationship_get_all_dest( $p_dest_bug_id ) {
 		$t_bug_relationship_data[$i]->dest_project_id = $t_dest_project_id;
 		$t_bug_relationship_data[$i]->type = $row['relationship_type'];
 		$t_bug_array[] = $row['source_bug_id'];
+		$i++;
 	}
-	unset( $t_bug_relationship_data[$t_relationship_count] );
+	unset( $t_bug_relationship_data[$i] );
 
 	if( !empty( $t_bug_array ) ) {
 		bug_cache_array_rows( $t_bug_array );
@@ -433,7 +429,7 @@ function relationship_get_all_dest( $p_dest_bug_id ) {
  * get all relationships associated with the given bug
  * @param int $p_bug_id  Bug id
  * @param bool &$p_is_different_projects Returned Boolean value indicating if some relationships cross project boundaries
- * @return array array of BugRelationshipData objects
+ * @return array Array of BugRelationshipData objects
  */
 function relationship_get_all( $p_bug_id, &$p_is_different_projects ) {
 	$t_src = relationship_get_all_src( $p_bug_id );
@@ -456,30 +452,22 @@ function relationship_get_all( $p_bug_id, &$p_is_different_projects ) {
  * @return int Relationship ID
  */
 function relationship_exists( $p_src_bug_id, $p_dest_bug_id ) {
-	$c_src_bug_id = db_prepare_int( $p_src_bug_id );
-	$c_dest_bug_id = db_prepare_int( $p_dest_bug_id );
+	$c_src_bug_id = (int)$p_src_bug_id;
+	$c_dest_bug_id = (int)$p_dest_bug_id;
 
 	$t_mantis_bug_relationship_table = db_get_table( 'bug_relationship' );
 
-	$t_query = "SELECT *
-				FROM $t_mantis_bug_relationship_table
-				WHERE
-				(source_bug_id=" . db_param() . "
-				AND destination_bug_id=" . db_param() . ")
+	$t_query = "SELECT * FROM $t_mantis_bug_relationship_table
+				WHERE (source_bug_id=" . db_param() . " AND destination_bug_id=" . db_param() . ")
 				OR
 				(source_bug_id=" . db_param() . "
 				AND destination_bug_id=" . db_param() . ')';
 	$result = db_query_bound( $t_query, array( $c_src_bug_id, $c_dest_bug_id, $c_dest_bug_id, $c_src_bug_id ), 1 );
 
-	$t_relationship_count = db_num_rows( $result );
-
-	if( $t_relationship_count == 1 ) {
-
+	if( $row = db_fetch_array( $result ) ) {
 		# return the first id
-		$row = db_fetch_array( $result );
 		return $row['id'];
 	} else {
-
 		# no relationship found
 		return 0;
 	}

core/string_api.php
@@ -591,16 +591,13 @@ function string_restore_valid_html_tags( $p_string, $p_multiline = true ) {
  * @return string
  */
 function string_get_bug_page( $p_action, $p_user_id = null ) {
-	if ( $p_action == 'view' ) {
-		return 'bug_view_page.php';
-	}
-
-	if ( $p_action == 'update' ) {
-		return 'bug_update_page.php';
-	}
-
-	if ( $p_action == 'report' ) {
-		return 'bug_report_page.php';
+	switch( $p_action ) {
+		case 'view':
+			return 'bug_view_page.php';
+		case 'update':
+			return 'bug_update_page.php';
+		case 'report':
+			return 'bug_report_page.php';
 	}
 
 	trigger_error( ERROR_GENERIC, ERROR );

core/summary_api.php
@@ -90,9 +90,13 @@ function summary_helper_get_developer_label ( $p_user_id ) {
 
 }
 
-# Used in summary reports
-# this function prints out the summary for the given enum setting
-# The enum field name is passed in through $p_enum
+/**
+ * Used in summary reports - this function prints out the summary for the given enum setting
+ * The enum field name is passed in through $p_enum
+ *
+ * @param string $p_enum enum field name
+ * @throws MantisBT\Exception\Database\FieldNotFound
+ */
 function summary_print_by_enum( $p_enum ) {
 	$t_project_id = helper_get_current_project();
 	$t_user_id = auth_get_current_user_id();
@@ -238,8 +242,12 @@ function summary_print_by_enum( $p_enum ) {
 	}
 }
 
-# prints the bugs submitted in the last X days (default is 1 day) for the
-# current project
+/**
+ * prints the bugs submitted in the last X days (default is 1 day) for the current project
+ *
+ * @param int $p_num_days number of days
+ * @return int
+ */
 function summary_new_bug_count_by_date( $p_time_length = 1 ) {
 	$t_mantis_bug_table = db_get_table( 'bug' );
 
@@ -260,8 +268,12 @@ function summary_new_bug_count_by_date( $p_time_length = 1 ) {
 	return db_result( $result, 0 );
 }
 
-# returns the number of bugs resolved in the last X days (default is 1 day) for the
-# current project
+/**
+ * returns the number of bugs resolved in the last X days (default is 1 day) for the current project
+ *
+ * @param int $p_num_days number of days
+ * @return int
+ */
 function summary_resolved_bug_count_by_date( $p_time_length = 1 ) {
 	$t_bug_table = db_get_table( 'bug' );
 	$t_bug_history_table = db_get_table( 'bug_history' );
@@ -292,8 +304,11 @@ function summary_resolved_bug_count_by_date( $p_time_length = 1 ) {
 	return db_result( $result, 0 );
 }
 
-# This function shows the number of bugs submitted in the last X days
-# An array of integers representing days is passed in
+/**
+ * This function shows the number of bugs submitted in the last X days
+ *
+ * @param array $p_date_array An array of integers representing days is passed in
+ */
 function summary_print_by_date( $p_date_array ) {
 	$arr_count = count( $p_date_array );
 	foreach( $p_date_array as $t_days ) {
@@ -331,13 +346,12 @@ function summary_print_by_date( $p_date_array ) {
 		print( "\n<td class=\"right$t_style\">$t_balance</td>\n" );
 		print( "</tr>\n" );
 	}
-
-	# end foreach
 }
 
-# Print list of open bugs with the highest activity score
-# the score is calculated assigning one "point" for each history event
-# associated with the bug
+/**
+ * Print list of open bugs with the highest activity score the score is calculated assigning 
+ * one "point" for each history event associated with the bug
+ */
 function summary_print_by_activity() {
 	$t_mantis_bug_table = db_get_table( 'bug' );
 	$t_mantis_history_table = db_get_table( 'bug_history' );

commit 80ae71ed37ee081d095400aa02c10d171c8e5485

Fix SQL error in tag_bug_is_attached()
Commit a80f447687722b52160d00c96c21a5be5f4329e1 introduced a regression
by changing SELECT * to SELECT id, but id is not a valid column in the
bug_tag table.

core/tag_api.php
@@ -525,10 +525,10 @@ function tag_get_candidates_for_bug( $p_bug_id ) {
  */
 function tag_bug_is_attached( $p_tag_id, $p_bug_id ) {
 	$t_bug_tag_table = db_get_table( 'bug_tag' );
-	$query = "SELECT id FROM $t_bug_tag_table
+	$query = "SELECT bug_id FROM $t_bug_tag_table
 					WHERE tag_id=" . db_param() . " AND bug_id=" . db_param();
 	$result = db_query_bound( $query, array( $p_tag_id, $p_bug_id ) );
-	return( db_result( $result ) > 0 );
+	return( db_result( $result ) !== false );
 }
 
 /**