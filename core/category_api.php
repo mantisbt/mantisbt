mantisbt/mantisbt

commit ef0bbb87aecdc54e2b9112bf9f3a11a592dfdc4d

remove db_num_rows where possible (part 1)

core/category_api.php
@@ -583,8 +583,8 @@ function category_get_field( $p_category_id, $p_field_name ) {
 	$t_query = "SELECT id FROM $t_category_table
 				WHERE name=" . db_param() . " AND project_id=" . db_param();
 	$t_result = db_query_bound( $t_query, array( $p_category_name, (int) $p_project_id ) );
-	$t_count = db_num_rows( $t_result );
-	if( 1 > $t_count ) {
+	$t_id = db_result( $t_result );
+	if( $t_id ) {
 		if( $p_trigger_errors ) {
 			error_parameters( $p_category_name, $t_project_name );
 			trigger_error( ERROR_CATEGORY_NOT_FOUND_FOR_PROJECT, ERROR );
@@ -593,7 +593,7 @@ function category_get_field( $p_category_id, $p_field_name ) {
 		}
 	}
 
-	return db_result( $t_result );
+	return $t_id;
 }
 
 /**

core/access_api.php
@@ -130,12 +130,9 @@ function access_cache_matrix_project( $p_project_id ) {
 		$query = "SELECT user_id, access_level
 					  FROM $t_project_user_list_table
 					  WHERE project_id=" . db_param();
-		$result = db_query_bound( $query, array( (int)$p_project_id ) );
-		$count = db_num_rows( $result );
-		for( $i = 0;$i < $count;$i++ ) {
-			$row = db_fetch_array( $result );
-
-			$g_cache_access_matrix[(int) $row['user_id']][(int) $p_project_id] = (int) $row['access_level'];
+		$t_result = db_query_bound( $query, array( (int)$p_project_id ) );
+		while( $t_row = db_fetch_array( $t_result ) ) {
+			$g_cache_access_matrix[(int) $t_row['user_id']][(int) $p_project_id] = (int) $t_row['access_level'];
 		}
 
 		$g_cache_access_matrix_project_ids[] = (int) $p_project_id;
@@ -164,19 +161,16 @@ function access_cache_matrix_user( $p_user_id ) {
 	if( !in_array( (int) $p_user_id, $g_cache_access_matrix_user_ids ) ) {
 		$t_project_user_list_table = db_get_table( 'project_user_list' );
 
-		$query = "SELECT project_id, access_level
+		$t_query = "SELECT project_id, access_level
 					  FROM $t_project_user_list_table
 					  WHERE user_id=" . db_param();
-		$result = db_query_bound( $query, array( (int)$p_user_id ) );
-
-		$count = db_num_rows( $result );
+		$t_result = db_query_bound( $t_query, array( (int)$p_user_id ) );
 
 		# make sure we always have an array to return
 		$g_cache_access_matrix[(int) $p_user_id] = array();
 
-		for( $i = 0;$i < $count;$i++ ) {
-			$row = db_fetch_array( $result );
-			$g_cache_access_matrix[(int) $p_user_id][(int) $row['project_id']] = (int) $row['access_level'];
+		while( $t_row = db_fetch_array( $t_result ) ) {
+			$g_cache_access_matrix[(int) $p_user_id][(int) $t_row['project_id']] = (int) $t_row['access_level'];
 		}
 
 		$g_cache_access_matrix_user_ids[] = (int) $p_user_id;

core/authentication_api.php
@@ -614,15 +614,14 @@ function auth_get_current_user_cookie( $p_login_anonymous=true ) {
 				if( function_exists( 'db_is_connected' ) && db_is_connected() ) {
 
 					# get anonymous information if database is available
-					$query = 'SELECT id, cookie_string FROM ' . db_get_table( 'user' ) . ' WHERE username = ' . db_param();
-					$result = db_query_bound( $query, array( config_get( 'anonymous_account' ) ) );
+					$t_query = 'SELECT id, cookie_string FROM ' . db_get_table( 'user' ) . ' WHERE username = ' . db_param();
+					$t_result = db_query_bound( $t_query, array( config_get( 'anonymous_account' ) ) );
 
-					if( 1 == db_num_rows( $result ) ) {
-						$row = db_fetch_array( $result );
-						$t_cookie = $row['cookie_string'];
+					if( $t_row = db_fetch_array( $t_result ) ) {						
+						$t_cookie = $t_row['cookie_string'];
 
 						$g_cache_anonymous_user_cookie_string = $t_cookie;
-						$g_cache_current_user_id = $row['id'];
+						$g_cache_current_user_id = $t_row['id'];
 					}
 				}
 			} else {
@@ -814,20 +813,21 @@ function auth_get_current_user_id() {
 	$t_user_table = db_get_table( 'user' );
 
 	/** @todo error with an error saying they aren't logged in? Or redirect to the login page maybe? */
-	$query = "SELECT id
+	$t_query = "SELECT id
 				  FROM $t_user_table
 				  WHERE cookie_string=" . db_param();
-	$result = db_query_bound( $query, array( $t_cookie_string ) );
+	$t_result = db_query_bound( $t_query, array( $t_cookie_string ) );
+
+	$t_user_id = (int) db_result( $t_result );
 
 	# The cookie was invalid. Clear the cookie (to allow people to log in again)
 	# and give them an Access Denied message.
-	if( db_num_rows( $result ) < 1 ) {
+	if( !$t_user_id ) {
 		auth_clear_cookies();
 		access_denied();
 		exit();
 	}
 
-	$t_user_id = (int) db_result( $result );
 	$g_cache_current_user_id = $t_user_id;
 
 	return $t_user_id;

core/bug_api.php
@@ -490,7 +490,7 @@ class BugData {
 						  WHERE id=" . db_param();
 			$result = db_query_bound( $query, array( $this->category_id ) );
 
-			if( db_num_rows( $result ) > 0 ) {
+			if( $t_result = db_result( $result ) ) {
 				$this->handler_id = db_result( $result );
 			}
 		}
@@ -1121,11 +1121,8 @@ function bug_copy( $p_bug_id, $p_target_project_id = null, $p_copy_custom_fields
 					   FROM $t_mantis_custom_field_string_table
 					   WHERE bug_id=" . db_param();
 		$result = db_query_bound( $query, array( $t_bug_id ) );
-		$t_count = db_num_rows( $result );
 
-		for( $i = 0;$i < $t_count;$i++ ) {
-			$t_bug_custom = db_fetch_array( $result );
-
+		while( $t_bug_custom = db_fetch_array( $result ) ) {
 			$c_field_id = (int)$t_bug_custom['field_id'];
 			$c_new_bug_id = (int)$t_new_bug_id;
 			$c_value = $t_bug_custom['value'];
@@ -1148,22 +1145,17 @@ function bug_copy( $p_bug_id, $p_target_project_id = null, $p_copy_custom_fields
 					  FROM $t_mantis_bugnote_table
 					  WHERE bug_id=" . db_param();
 		$result = db_query_bound( $query, array( $t_bug_id ) );
-		$t_count = db_num_rows( $result );
 
-		for( $i = 0;$i < $t_count;$i++ ) {
-			$t_bug_note = db_fetch_array( $result );
+		while( $t_bug_note = db_fetch_array( $result ) ) {
 			$t_bugnote_text_id = $t_bug_note['bugnote_text_id'];
 
 			$query2 = "SELECT *
 						   FROM $t_mantis_bugnote_text_table
 						   WHERE id=" . db_param();
 			$result2 = db_query_bound( $query2, array( $t_bugnote_text_id ) );
-			$t_count2 = db_num_rows( $result2 );
 
 			$t_bugnote_text_insert_id = -1;
-			if( $t_count2 > 0 ) {
-				$t_bugnote_text = db_fetch_array( $result2 );
-
+			if( $t_bugnote_text = db_fetch_array( $result2 ) ) {
 				$query2 = "INSERT INTO $t_mantis_bugnote_text_table
 							   ( note )
 							   VALUES ( " . db_param() . ' )';
@@ -1202,10 +1194,8 @@ function bug_copy( $p_bug_id, $p_target_project_id = null, $p_copy_custom_fields
 					  FROM $t_mantis_bug_history_table
 					  WHERE bug_id = " . db_param();
 		$result = db_query_bound( $query, array( $t_bug_id ) );
-		$t_count = db_num_rows( $result );
 
-		for( $i = 0;$i < $t_count;$i++ ) {
-			$t_bug_history = db_fetch_array( $result );
+		while( $t_bug_history = db_fetch_array( $result ) ) {
 			$query = "INSERT INTO $t_mantis_bug_history_table
 						  ( user_id, bug_id, date_modified, field_name, old_value, new_value, type )
 						  VALUES ( " . db_param() . ",
@@ -1357,17 +1347,13 @@ function bug_delete_all( $p_project_id ) {
 
 	$t_bug_table = db_get_table( 'bug' );
 
-	$query = "SELECT id
+	$t_query = "SELECT id
 				  FROM $t_bug_table
 				  WHERE project_id=" . db_param();
-	$result = db_query_bound( $query, array( $c_project_id ) );
+	$t_result = db_query_bound( $t_query, array( $c_project_id ) );
 
-	$bug_count = db_num_rows( $result );
-
-	for( $i = 0;$i < $bug_count;$i++ ) {
-		$row = db_fetch_array( $result );
-
-		bug_delete( $row['id'] );
+	while( $t_row = db_fetch_array( $t_result ) ) {
+		bug_delete( $t_row['id'] );
 	}
 
 	# @todo should we check the return value of each bug_delete() and
@@ -1535,20 +1521,24 @@ function bug_get_bugnote_stats( $p_bug_id ) {
 	}
 
 	$t_bugnote_table = db_get_table( 'bugnote' );
-
+	// @todo - optimise - max(), count()
 	$query = "SELECT last_modified
 				  FROM $t_bugnote_table
 				  WHERE bug_id=" . db_param() . "
-				  ORDER BY last_modified DESC";
-	$result = db_query_bound( $query, array( $c_bug_id ) );
-	$row = db_fetch_array( $result );
+				  ORDER BY last_modified ASC";
+	$t_result = db_query_bound( $query, array( $c_bug_id ) );
 
-	if( false === $row ) {
+	$t_bugnote_count = 0;
+	while ( $t_row = db_fetch_array( $t_result ) ) {
+		$t_bugnote_count++;
+	}
+
+	if( $t_bugnote_count === 0 ) {
 		return false;
 	}
 
-	$t_stats['last_modified'] = $row['last_modified'];
-	$t_stats['count'] = db_num_rows( $result );
+	$t_stats['last_modified'] = $t_row['last_modified'];
+	$t_stats['count'] = $t_bugnote_count;
 
 	return $t_stats;
 }
@@ -1571,12 +1561,11 @@ function bug_get_attachments( $p_bug_id ) {
 		                WHERE bug_id=" . db_param() . "
 		                ORDER BY date_added";
 	$db_result = db_query_bound( $query, array( $p_bug_id ) );
-	$num_files = db_num_rows( $db_result );
 
 	$t_result = array();
 
-	for( $i = 0;$i < $num_files;$i++ ) {
-		$t_result[] = db_fetch_array( $db_result );
+	while( $t_row = db_fetch_array( $db_result ) ) {
+		$t_result[] = $t_row;
 	}
 
 	return $t_result;
@@ -2001,10 +1990,8 @@ function bug_monitor_copy( $p_source_bug_id, $p_dest_bug_id ) {
 		FROM ' . $t_bug_monitor_table . '
 		WHERE bug_id = ' . db_param();
 	$result = db_query_bound( $query, array( $c_source_bug_id ) );
-	$t_count = db_num_rows( $result );
 
-	for( $i = 0; $i < $t_count; $i++ ) {
-		$t_bug_monitor = db_fetch_array( $result );
+	while( $t_bug_monitor = db_fetch_array( $result ) ) {
 		if ( user_exists( $t_bug_monitor['user_id'] ) &&
 			!user_is_monitoring_bug( $t_bug_monitor['user_id'], $c_dest_bug_id ) ) {
 			$query = 'INSERT INTO ' . $t_bug_monitor_table . ' ( user_id, bug_id )

core/bug_revision_api.php
@@ -95,11 +95,10 @@ function bug_revision_add( $p_bug_id, $p_user_id, $p_type, $p_value, $p_bugnote_
  */
 function bug_revision_exists( $p_revision_id ) {
 	$t_bug_rev_table = db_get_table( 'bug_revision' );
-
-	$t_query = "SELECT * FROM $t_bug_rev_table WHERE id=" . db_param();
+	$t_query = "SELECT id FROM $t_bug_rev_table WHERE id=" . db_param();
 	$t_result = db_query_bound( $t_query, array( $p_revision_id ) );
 
-	if ( db_num_rows( $t_result ) < 1 ) {
+	if ( !db_result( $t_result ) ) {
 		return false;
 	}
 
@@ -117,11 +116,12 @@ function bug_revision_get( $p_revision_id ) {
 	$t_query = "SELECT * FROM $t_bug_rev_table WHERE id=" . db_param();
 	$t_result = db_query_bound( $t_query, array( $p_revision_id ) );
 
-	if ( db_num_rows( $t_result ) < 1 ) {
+	$t_row = db_fetch_array( $t_result );
+	if ( !$t_row ) {
 		trigger_error( ERROR_BUG_REVISION_NOT_FOUND, ERROR );
 	}
 
-	return db_fetch_array( $t_result );
+	return $t_row;
 }
 
 /**
@@ -266,8 +266,9 @@ function bug_revision_last( $p_bug_id, $p_type=REV_ANY, $p_bugnote_id=0 ) {
 	$t_query .= ' ORDER BY timestamp DESC';
 	$t_result = db_query_bound( $t_query, $t_params, 1 );
 
-	if ( db_num_rows( $t_result ) > 0 ) {
-		return db_fetch_array( $t_result );
+	$t_row = db_fetch_array( $t_result );
+	if ( $t_row ) {
+		return $t_row;
 	} else {
 		return null;
 	}
@@ -322,11 +323,12 @@ function bug_revision_like( $p_rev_id ) {
 	$t_query = "SELECT bug_id, bugnote_id, type FROM $t_bug_rev_table WHERE id=" . db_param();
 	$t_result = db_query_bound( $t_query, array( $p_rev_id ) );
 
-	if ( db_num_rows( $t_result ) < 1 ) {
+	$t_row = db_fetch_array( $t_result );
+	
+	if ( !$t_row ) {
 		trigger_error( ERROR_BUG_REVISION_NOT_FOUND, ERROR );
 	}
 
-	$t_row = db_fetch_array( $t_result );
 	$t_bug_id = $t_row['bug_id'];
 	$t_bugnote_id = $t_row['bugnote_id'];
 	$t_type = $t_row['type'];

core/bugnote_api.php
@@ -305,9 +305,7 @@ function bugnote_delete_all( $p_bug_id ) {
 		          	FROM $t_bugnote_table
 		          	WHERE bug_id=" . db_param();
 	$result = db_query_bound( $query, array( (int)$p_bug_id ) );
-	$bugnote_count = db_num_rows( $result );
-	for( $i = 0;$i < $bugnote_count;$i++ ) {
-		$row = db_fetch_array( $result );
+	while( $row = db_fetch_array( $result ) ) {
 		$t_bugnote_text_id = $row['bugnote_text_id'];
 
 		# Delete the corresponding bugnote texts

core/cfdefs/cfdef_standard.php
@@ -402,18 +402,17 @@ function cfdef_prepare_list_value_to_database($p_value) {
 function cfdef_prepare_list_distinct_values($p_field_def) {
 	$t_custom_field_table = db_get_table( 'custom_field' );
 
-	$query = "SELECT possible_values
+	$t_query = "SELECT possible_values
 			  FROM $t_custom_field_table
 			  WHERE id=" . db_param();
-	$result = db_query_bound( $query, array( $p_field_def['id'] ) );
+	$t_result = db_query_bound( $t_query, array( $p_field_def['id'] ) );
 
-	$t_row_count = db_num_rows( $result );
-	if ( 0 == $t_row_count ) {
+	$t_row = db_fetch_array( $t_result );
+	if ( !$t_row ) {
 		return false;
 	}
-	$row = db_fetch_array( $result );
 
-	$t_possible_values = custom_field_prepare_possible_values( $row['possible_values'] );
+	$t_possible_values = custom_field_prepare_possible_values( $t_row['possible_values'] );
 	$t_values_arr = explode( '|', $t_possible_values );
 	$t_return_arr = array();
 

core/custom_field_api.php
@@ -118,7 +118,8 @@ function custom_field_cache_row( $p_field_id, $p_trigger_errors = true ) {
 				  WHERE id=" . db_param();
 	$result = db_query_bound( $t_query, array( $p_field_id ) );
 
-	if( 0 == db_num_rows( $result ) ) {
+	$t_row = db_fetch_array( $t_result );
+	if( !$t_row ) {
 		if( $p_trigger_errors ) {
 			error_parameters( 'Custom ' . $p_field_id );
 			trigger_error( ERROR_CUSTOM_FIELD_NOT_FOUND, ERROR );
@@ -127,12 +128,10 @@ function custom_field_cache_row( $p_field_id, $p_trigger_errors = true ) {
 		}
 	}
 
-	$row = db_fetch_array( $result );
+	$g_cache_custom_field[$p_field_id] = $t_row;
+	$g_cache_name_to_id_map[$t_row['name']] = $p_field_id;
 
-	$g_cache_custom_field[$c_field_id] = $row;
-	$g_cache_name_to_id_map[$row['name']] = $c_field_id;
-
-	return $row;
+	return $t_row;
 }
 
 /**
@@ -624,18 +623,19 @@ function custom_field_get_id_from_name( $p_field_name ) {
 
 	$t_custom_field_table = db_get_table( 'custom_field' );
 
-	$query = "SELECT id FROM $t_custom_field_table WHERE name = " . db_param();
-	$t_result = db_query_bound( $query, array( $p_field_name ) );
+	$t_query = "SELECT id FROM $t_custom_field_table WHERE name = " . db_param();
+	$t_result = db_query_bound( $t_query, array( $p_field_name ) );
 
-	if( db_num_rows( $t_result ) == 0 ) {
+	$t_row = db_fetch_array( $t_result );
+
+	if( !$t_row ) {
 		$g_cache_name_to_id_map[$p_field_name] = false;
 		return false;
 	}
 
-	$row = db_fetch_array( $t_result );
-	$g_cache_name_to_id_map[$p_field_name] = $row['id'];
+	$g_cache_name_to_id_map[$p_field_name] = $t_row['id'];
 
-	return $row['id'];
+	return $t_row['id'];
 }
 
 /**
@@ -723,13 +723,11 @@ function custom_field_get_linked_ids( $p_project_id = ALL_PROJECTS ) {
 				ORDER BY sequence ASC, name ASC";
 		}
 
-		$result = db_query_bound( $t_query, $t_params );
-		$t_row_count = db_num_rows( $result );
+		$t_result = db_query_bound( $t_query, $t_params );
 		$t_ids = array();
 
-		for( $i = 0;$i < $t_row_count;$i++ ) {
-			$row = db_fetch_array( $result );
-			array_push( $t_ids, $row['id'] );
+		while( $t_row = db_fetch_array( $t_result ) ) {
+			array_push( $t_ids, $t_row['id'] );
 		}
 		custom_field_cache_array_rows( $t_ids );
 
@@ -754,15 +752,12 @@ function custom_field_get_ids() {
 				  FROM $t_custom_field_table
 				  ORDER BY name ASC";
 		$result = db_query_bound( $query );
-		$t_row_count = db_num_rows( $result );
 		$t_ids = array();
 
-		for( $i = 0;$i < $t_row_count;$i++ ) {
-			$row = db_fetch_array( $result );
+		while( $t_row = db_fetch_array( $result ) ) {
+			$g_cache_custom_field[(int) $t_row['id']] = $t_row;
 
-			$g_cache_custom_field[(int) $row['id']] = $row;
-
-			array_push( $t_ids, $row['id'] );
+			array_push( $t_ids, $t_row['id'] );
 		}
 		$g_cache_cf_list = $t_ids;
 	} else {

commit 5dcf07a5fbdd91c3c3d64c73f3b3e76fd7e6cb93

RE-Fix category_get_id_by_name() doesn't find category
This is a cherry-pick of 3bd0e1ba64770a5e4faa67866cd822920523e2fa - the
fix had been reverted by fbd654083f061fcfcc8dc7229953cc44737889fa.

Commit ef0bbb87aecdc54e2b9112bf9f3a11a592dfdc4d introduced a regression.
Testing on the return value as if it were a boolean triggers an error
ERROR_CATEGORY_NOT_FOUND_FOR_PROJECT even if the category exists.

Now we do a strict check === false to ensure the error is only triggered
when the query returns no rows.

core/category_api.php
@@ -566,7 +566,7 @@ function category_get_field( $p_category_id, $p_field_name ) {
 				WHERE name=" . db_param() . " AND project_id=" . db_param();
 	$t_result = db_query_bound( $t_query, array( $p_category_name, (int) $p_project_id ) );
 	$t_id = db_result( $t_result );
-	if( $t_id > 0 ) {
+	if( $t_id === false ) {
 		if( $p_trigger_errors ) {
 			error_parameters( $p_category_name, $t_project_name );
 			trigger_error( ERROR_CATEGORY_NOT_FOUND_FOR_PROJECT, ERROR );