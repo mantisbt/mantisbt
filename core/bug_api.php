mantisbt/mantisbt

commit ad7009ca4a6810ddd559a8613c8a5b05e2fc5c6b

Sync in another batch of db_prepare_int/numrows/phpdoc stuff

core/bug_api.php
@@ -789,14 +789,13 @@ function bug_cache_array_rows( $p_bug_id_array ) {
 	}
 
 	$t_bug_table = db_get_table( 'bug' );
-
-	$query = "SELECT *
+	$t_query = "SELECT *
 				  FROM $t_bug_table
 				  WHERE id IN (" . implode( ',', $c_bug_id_array ) . ')';
-	$result = db_query_bound( $query );
+	$t_result = db_query_bound( $t_query );
 
-	while( $row = db_fetch_array( $result ) ) {
-		bug_add_to_cache( $row );
+	while( $t_row = db_fetch_array( $t_result ) ) {
+		bug_add_to_cache( $t_row );
 	}
 	return;
 }
@@ -1880,16 +1879,12 @@ function bug_reopen( $p_bug_id, $p_bugnote_text = '', $p_time_tracking = '0:00',
  * @uses database_api.php
  */
 function bug_update_date( $p_bug_id ) {
-	$c_bug_id = (int) $p_bug_id;
-
 	$t_bug_table = db_get_table( 'bug' );
-
-	$query = "UPDATE $t_bug_table
-				  SET last_updated= " . db_param() . "
+	$query = "UPDATE $t_bug_table SET last_updated= " . db_param() . "
 				  WHERE id=" . db_param();
-	db_query_bound( $query, array( db_now(), $c_bug_id ) );
+	db_query_bound( $query, array( db_now(), $p_bug_id ) );
 
-	bug_clear_cache( $c_bug_id );
+	bug_clear_cache( $p_bug_id );
 
 	return true;
 }
@@ -1918,9 +1913,9 @@ function bug_monitor( $p_bug_id, $p_user_id ) {
 		return false;
 	}
 
-	$t_bug_monitor_table = db_get_table( 'bug_monitor' );
 
 	# Insert monitoring record
+	$t_bug_monitor_table = db_get_table( 'bug_monitor' );
 	$query = 'INSERT INTO ' . $t_bug_monitor_table . '( user_id, bug_id ) VALUES (' . db_param() . ',' . db_param() . ')';
 	db_query_bound( $query, array( $c_user_id, $c_bug_id ) );
 
@@ -1947,22 +1942,19 @@ function bug_get_monitors( $p_bug_id ) {
 		return array();
 	}
 
-	$c_bug_id = (int)$p_bug_id;
 	$t_bug_monitor_table = db_get_table( 'bug_monitor' );
 	$t_user_table = db_get_table( 'user' );
 
 	# get the bugnote data
-	$query = "SELECT user_id, enabled
+	$t_query = "SELECT user_id, enabled
 			FROM $t_bug_monitor_table m, $t_user_table u
 			WHERE m.bug_id=" . db_param() . " AND m.user_id = u.id
 			ORDER BY u.realname, u.username";
-	$result = db_query_bound($query, array( $c_bug_id ) );
-	$num_users = db_num_rows($result);
+	$t_result = db_query_bound($t_query, array( $p_bug_id ) );
 
 	$t_users = array();
-	for ( $i = 0; $i < $num_users; $i++ ) {
-		$row = db_fetch_array( $result );
-		$t_users[$i] = $row['user_id'];
+	while( $t_row = db_fetch_array( $t_result ) ) {
+		$t_users[] = $t_row['user_id'];
 	}
 
 	user_cache_array_rows( $t_users );
@@ -1985,7 +1977,6 @@ function bug_monitor_copy( $p_source_bug_id, $p_dest_bug_id ) {
 	$c_dest_bug_id = (int)$p_dest_bug_id;
 
 	$t_bug_monitor_table = db_get_table( 'bug_monitor' );
-
 	$query = 'SELECT user_id
 		FROM ' . $t_bug_monitor_table . '
 		WHERE bug_id = ' . db_param();
@@ -2013,24 +2004,20 @@ function bug_monitor_copy( $p_source_bug_id, $p_dest_bug_id ) {
  * @uses history_api.php
  */
 function bug_unmonitor( $p_bug_id, $p_user_id ) {
-	$c_bug_id = (int) $p_bug_id;
-	$c_user_id = (int) $p_user_id;
-
-	$t_bug_monitor_table = db_get_table( 'bug_monitor' );
-
 	# Delete monitoring record
+	$t_bug_monitor_table = db_get_table( 'bug_monitor' );
 	$query = 'DELETE FROM ' . $t_bug_monitor_table . ' WHERE bug_id = ' . db_param();
-	$db_query_params[] = $c_bug_id;
+	$db_query_params[] = $p_bug_id;
 
 	if( $p_user_id !== null ) {
 		$query .= " AND user_id = " . db_param();
-		$db_query_params[] = $c_user_id;
+		$db_query_params[] = $p_user_id;
 	}
 
 	db_query_bound( $query, $db_query_params );
 
 	# log new un-monitor action
-	history_log_event_special( $c_bug_id, BUG_UNMONITOR, $c_user_id );
+	history_log_event_special( $p_bug_id, BUG_UNMONITOR, $p_user_id );
 
 	# updated the last_updated date
 	bug_update_date( $p_bug_id );

admin/copy_field.php
@@ -16,13 +16,12 @@
 # This upgrade moves attachments from the database to the disk
 
 /**
+ * Copy Custom fields to built in fields
+ *
  * @package MantisBT
  * @copyright Copyright 2000 - 2002  Kenzaburo Ito - kenito@300baud.org
  * @copyright Copyright 2002  MantisBT Team - mantisbt-dev@lists.sourceforge.net
  * @link http://www.mantisbt.org
- */
-/**
- * MantisBT Core API's
  */
 require_once( dirname( dirname( __FILE__ ) ) . '/core.php' );
 

api/soap/mantisconnect.php
@@ -1,10 +1,18 @@
 <?php
 # MantisConnect - A webservice interface to Mantis Bug Tracker
-# Copyright 2004  Victor Boctor - vboctor@users.sourceforge.net
+# 
 # This program is distributed under dual licensing.  These include
 # GPL and a commercial licenses.  Victor Boctor reserves the right to
 # change the license of future releases.
 # See docs/ folder for more details
+
+/**
+ * MantisConnect - A webservice interface to Mantis Bug Tracker
+ *
+ * @package MantisBT
+ * @copyright Copyright 2004  Victor Boctor - vboctor@users.sourceforge.net
+ * @link http://www.mantisbt.org
+ */
 
 set_include_path( '../../library' );
 

api/soap/mc_account_api.php
@@ -7,6 +7,14 @@
 # See docs/ folder for more details
 
 /**
+ * MantisConnect - A webservice interface to Mantis Bug Tracker
+ *
+ * @package MantisBT
+ * @copyright Copyright 2004  Victor Boctor - vboctor@users.sourceforge.net
+ * @link http://www.mantisbt.org
+ */
+
+/**
  * Get username, realname and email from for a given user id
  */
 function mci_account_get_array_by_id( $p_user_id ) {

api/soap/mc_api.php
@@ -6,6 +6,14 @@
 # change the license of future releases.
 # See docs/ folder for more details
 
+/**
+ * MantisConnect - A webservice interface to Mantis Bug Tracker
+ *
+ * @package MantisBT
+ * @copyright Copyright 2004  Victor Boctor - vboctor@users.sourceforge.net
+ * @link http://www.mantisbt.org
+ */
+
 # set up error_handler() as the new default error handling function
 set_error_handler( 'mc_error_handler' );
 

api/soap/mc_config_api.php
@@ -6,6 +6,20 @@
 # change the license of future releases.
 # See docs/ folder for more details
 
+/**
+ * MantisConnect - A webservice interface to Mantis Bug Tracker
+ *
+ * @package MantisBT
+ * @copyright Copyright 2004  Victor Boctor - vboctor@users.sourceforge.net
+ * @link http://www.mantisbt.org
+ */
+
+/**
+ * Get config string
+ * @param string $p_username username
+ * @param string $p_password password
+ * @param string $p_config_var config variable
+ */
 function mc_config_get_string( $p_username, $p_password, $p_config_var ) {
 	$t_user_id = mci_check_login( $p_username, $p_password );
 	if( $t_user_id === false ) {

api/soap/mc_config_defaults_inc.php
@@ -6,6 +6,14 @@
 # change the license of future releases.
 # See docs/ folder for more details
 
+/**
+ * MantisConnect - A webservice interface to Mantis Bug Tracker
+ *
+ * @package MantisBT
+ * @copyright Copyright 2004  Victor Boctor - vboctor@users.sourceforge.net
+ * @link http://www.mantisbt.org
+ */
+
 # Minimum global access level required to access webservice for readonly operations.
 $g_mc_readonly_access_level_threshold = REPORTER;
 

commit fe4875e2ead14533c86878df652aa34ffe5ecf84

Fix issue preventing bug deletion
Removal of $c_user_id variable caused bug_unmonitor() to call
history_log_event_special() with a NULL user id, causing an SQL error.

Regression introduced by ad7009ca4a6810ddd559a8613c8a5b05e2fc5c6b

Fixes #16502

core/bug_api.php
@@ -2017,7 +2017,7 @@ function bug_unmonitor( $p_bug_id, $p_user_id ) {
 	db_query_bound( $query, $db_query_params );
 
 	# log new un-monitor action
-	history_log_event_special( $p_bug_id, BUG_UNMONITOR, $p_user_id );
+	history_log_event_special( $p_bug_id, BUG_UNMONITOR, (int)$p_user_id );
 
 	# updated the last_updated date
 	bug_update_date( $p_bug_id );