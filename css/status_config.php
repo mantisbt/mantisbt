cfd1d1a732d0cc05d29aef7658c5b61e9329e046
Roland Becker <roland@atrol.de>
2013-10-24 08:54:11 +0200

    Additional fix #16039: Signup page causes error in web server log

--- css/status_config.php
+++ css/status_config.php
@@ -58,6 +58,7 @@ header( 'X-Content-Type-Options: nosniff' );
 $t_referer_page = basename( parse_url( $_SERVER['HTTP_REFERER'], PHP_URL_PATH ) );
 switch( $t_referer_page ) {
 	case 'login_page.php':
+	case 'signup_page.php':
 		# We don't need custom status colors on login page, and this is
 		# actually causing an error since we're not authenticated yet.
 		exit;
