mantisbt/mantisbt

commit 527d9faf95884c7d260eeaaf27aa926c0ea5c194

Admin checks: Simplify code to generate verbosity links

admin/check/index.php
@@ -60,8 +60,15 @@ set_time_limit( 60 * 5 );
 $g_show_all = gpc_get_bool( 'show_all', false );
 $g_show_errors = gpc_get_bool( 'show_errors', false );
 
-$t_show_all_mode_link = '<a href="index.php?show_all=' . ($g_show_all ? '0' : '1') . '&amp;show_errors=' . ($g_show_errors ? '1' : '0') . '">' . ($g_show_all ? 'Hide passed tests' : 'Show passed tests') . '</a>';
-$t_show_errors_mode_link = '<a href="index.php?show_all=' . ($g_show_all ? '1' : '0') . '&amp;show_errors=' . ($g_show_errors ? '0' : '1') . '">' . ($g_show_errors ? 'Hide verbose error messages' : 'Show verbose error messages') . '</a>';
+$t_url = basename( __FILE__ ) . '?' . http_build_query(
+	array(
+		'show_all' => (int)!$g_show_all,
+		'show_errors' => (int)!$g_show_errors,
+	)
+);
+$t_link = '<a href="%s">%s %s</a>';
+$t_show_all_mode_link = sprintf( $t_link, $t_url, ($g_show_all ? 'Hide' : 'Show'), 'passed tests' );
+$t_show_errors_mode_link = sprintf( $t_link, $t_url, ($g_show_errors ? 'Hide' : 'Show'), 'verbose error messages' );
 
 http_content_headers();
 

commit 8dfd21d1afc1a1c2bf953658f0b16f1e40cb6025

Fix bug with verbosity links
Regression introduced by 527d9faf95884c7d260eeaaf27aa926c0ea5c194,
prevented user from swiching the 2 flags individually.

admin/check/index.php
@@ -60,15 +60,26 @@ set_time_limit( 60 * 5 );
 $g_show_all = gpc_get_bool( 'show_all', false );
 $g_show_errors = gpc_get_bool( 'show_errors', false );
 
-$t_url = basename( __FILE__ ) . '?' . http_build_query(
-	array(
-		'show_all' => (int)!$g_show_all,
-		'show_errors' => (int)!$g_show_errors,
-	)
-);
+function mode_url( $p_all, $p_errors ) {
+	return basename( __FILE__ ) . '?' . http_build_query(
+		array(
+			'show_all' => (int)$p_all,
+			'show_errors' => (int)$p_errors,
+		)
+	);
+}
+
 $t_link = '<a href="%s">%s %s</a>';
-$t_show_all_mode_link = sprintf( $t_link, $t_url, ($g_show_all ? 'Hide' : 'Show'), 'passed tests' );
-$t_show_errors_mode_link = sprintf( $t_link, $t_url, ($g_show_errors ? 'Hide' : 'Show'), 'verbose error messages' );
+$t_show_all_mode_link = sprintf( $t_link,
+	mode_url( !$g_show_all, $g_show_errors ),
+	($g_show_all ? 'Hide' : 'Show'),
+	'passed tests'
+);
+$t_show_errors_mode_link = sprintf( $t_link,
+	mode_url( $g_show_all, !$g_show_errors ),
+	($g_show_errors ? 'Hide' : 'Show'),
+	'verbose error messages'
+);
 
 http_content_headers();
 