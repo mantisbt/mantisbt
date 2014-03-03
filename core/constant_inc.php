687aa3afcf05853a15ef91a57b2c6d68f40cfaac
Damien Regad <dregad@mantisbt.org>
2014-03-03 11:22:54 +0100

    Bump version, update credits and release notes for 1.2.17

--- core/constant_inc.php
+++ core/constant_inc.php
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
 
-define( 'MANTIS_VERSION', '1.2.17dev' );
+define( 'MANTIS_VERSION', '1.2.17' );
 
 # --- constants -------------------
 # magic numbers
