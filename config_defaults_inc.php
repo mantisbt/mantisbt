569b6e559f0a73ecff72b8462439e4812d63a646
Victor Boctor <victor@mantishub.com>
2014-04-15 18:34:10 -0700

    Merged latest form master 2.

--- config_defaults_inc.php
+++ config_defaults_inc.php
@@ -4201,7 +4201,7 @@ unset( $t_protocol, $t_host, $t_hosts, $t_port, $t_self, $t_path );
  ************/
 
 # Minimum global access level required to access webservice for readonly operations.
-$g_mc_readonly_access_level_threshold = REPORTER;
+$g_mc_readonly_access_level_threshold = VIEWER;
 
 # Minimum global access level required to access webservice for read/write operations.
 $g_mc_readwrite_access_level_threshold = REPORTER;
