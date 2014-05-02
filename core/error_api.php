987b6adcd45b2be3697063d8fe294eb39f9aa97f
Damien Regad <dregad@mantisbt.org>
2014-05-02 16:15:13 +0200

    Separate error type from description with colon
    This makes the error message look more like standard PHP errors.

--- core/error_api.php
+++ core/error_api.php
@@ -145,7 +145,7 @@ function error_handler( $p_type, $p_error, $p_file, $p_line, $p_context ) {
 	$t_error_description = nl2br( $t_error_description );
 
 	if( php_sapi_name() == 'cli' ) {
-		echo $t_error_type . " " . $t_error_description . "\n";
+		echo $t_error_type . ": " . $t_error_description . "\n";
 		exit();
 	}
 
