mantisbt/mantisbt

commit 5a84e9db76162e378b9a1b0dea129d1f7ce127e9

Add new global config for plugin table prefix
This allows users on Oracle to shorten the table identifier, to avoid hitting
the 30-char size limit.

Also modified comments for the existing db prefix/suffix to explain that they
should be blank or shortened for Oracle.

Affects #13227

Porting to 1.3 - Conflicts:
	config_defaults_inc.php
	core/plugin_api.php

core/plugin_api.php
@@ -194,7 +194,17 @@ function plugin_table( $p_name, $p_basename = null ) {
 	} else {
 		$t_current = $p_basename;
 	}
-	return db_get_table( 'plugin_' . $t_current . '_' . $p_name );
+
+	$t_table_name = config_get_global( 'db_table_prefix' );
+	if( !isempty( $t_table_name ) ) {
+		$t_table_name .= '_';
+	}
+	$t_table_name .=
+		config_get_global( 'db_table_plugin_prefix' ) .
+		$t_current . '_' . $p_name .
+		config_get_global( 'db_table_suffix' );
+
+	return $t_table_name;
 }
 
 /**

config_defaults_inc.php
@@ -3207,12 +3207,24 @@ $g_create_short_url = 'http://tinyurl.com/create.php?url=%s';
 
 /**
  * table prefix
+ * To avoid the 30-char limit on identifiers in Oracle, the prefix
+ * should be set to blank or kept as short as possible (e.g. 'm')
  * @global string $g_db_table_prefix
  */
 $g_db_table_prefix = 'mantis';
 
 /**
+ * plugin table prefix
+ * To avoid the 30-char limit on identifiers in Oracle, the prefix
+ * should be kept as short as possible (e.g. 'plg')
+ * @global string $g_db_table_prefix
+ */
+$g_db_table_plugin_prefix	= 'plugin';
+
+/**
  * table suffix
+ * To avoid the 30-char limit on identifiers in Oracle, the suffix
+ * should be set to blank or kept as short as possible
  * @global string $g_db_table_suffix
  */
 $g_db_table_suffix = '_table';

commit 92e9d5d0416db8141c50872acebea489c85eb9b6

Make sure db prefix / suffix is consistently applied
Modifies the db_get_table() and plugin_table() functions to ensure that
we build a correct table name based on the user's config settings, e.g.

- trim leading/trailing whitespace
- do not add underscores if already present

Fixes #17355, regression issue introduced by commit
5a84e9db76162e378b9a1b0dea129d1f7ce127e9 where plugin_table() no longer
inserts an underscore after $g_db_table_plugin_prefix

core/database_api.php
@@ -959,16 +959,20 @@ function db_get_table( $p_name ) {
 		$t_table = $p_name;
 	}
 
-	$t_prefix = config_get_global( 'db_table_prefix' );
-	$t_suffix = config_get_global( 'db_table_suffix' );
-
-	if( $t_prefix ) {
-		$t_table = $t_prefix . '_' . $t_table;
+	# Determine table prefix including trailing '_'
+	$t_prefix = trim( config_get_global( 'db_table_prefix' ) );
+	if( !empty( $t_prefix ) && '_' != substr( $t_prefix, -1 ) ) {
+		$t_prefix .= '_';
 	}
-	$t_table .= $t_suffix;
+	# Determine table suffix including leading '_'
+	$t_suffix = trim( config_get_global( 'db_table_suffix' ) );
+	if( !empty( $t_suffix ) && '_' != substr( $t_suffix, 0, 1 ) ) {
+		$t_suffix = '_' . $t_suffix;
+	}
 
+	# Physical table name
+	$t_table = $t_prefix . $t_table . $t_suffix;
 	db_check_identifier_size( $t_table );
-
 	return $t_table;
 }
 

core/plugin_api.php
@@ -195,16 +195,13 @@ function plugin_table( $p_name, $p_basename = null ) {
 		$t_current = $p_basename;
 	}
 
-	$t_table_name = config_get_global( 'db_table_prefix' );
-	if( !empty( $t_table_name ) ) {
-		$t_table_name .= '_';
+	# Determine plugin table prefix including trailing '_'
+	$t_prefix = trim( config_get_global( 'db_table_plugin_prefix' ) );
+	if( !empty( $t_prefix ) && '_' != substr( $t_prefix, -1 ) ) {
+		$t_prefix .= '_';
 	}
-	$t_table_name .=
-		config_get_global( 'db_table_plugin_prefix' ) .
-		$t_current . '_' . $p_name .
-		config_get_global( 'db_table_suffix' );
 
-	return $t_table_name;
+	return db_get_table( $t_prefix . $t_current . '_' . $p_name );
 }
 
 /**