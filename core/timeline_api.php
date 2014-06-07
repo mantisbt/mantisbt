250c81ca32f48862d00aa4fa5046aa73cccd689c
Victor Boctor <victor@mantishub.com>
2014-06-06 19:40:34 -0700

    Replace if statements with switch for history event types.

--- core/timeline_api.php
+++ core/timeline_api.php
@@ -81,32 +81,40 @@ function timeline_events( $p_start_time, $p_end_time ) {
 			$t_user_id = $t_history_event['userid'];
 			$t_timestamp = $t_history_event['date'];
 
-			if ( $t_history_event['type'] == NEW_BUG ) {
-				$t_event = new IssueCreatedTimelineEvent( $t_timestamp, $t_user_id, $t_issue_id );
-			} else if ( $t_history_event['type'] == BUGNOTE_ADDED ) {
-				$t_bugnote_id = $t_history_event['old_value'];
-				$t_event = new IssueNoteCreatedTimelineEvent( $t_timestamp, $t_user_id, $t_issue_id, $t_bugnote_id );
-			} else if ( $t_history_event['type'] == BUG_MONITOR ) {
-				# Skip monitors added for others due to reminders, only add monitor events where added
-				# user is the same as the logged in user.
-				if ( (int)$t_history_event['old_value'] == (int)$t_history_event['userid'] ) {
-					$t_event = new IssueMonitorTimelineEvent( $t_timestamp, $t_user_id, $t_issue_id, true );
-				}
-			} else if ( $t_history_event['type'] == BUG_UNMONITOR ) {
-				$t_event = new IssueMonitorTimelineEvent( $t_timestamp, $t_user_id, $t_issue_id, false );
-			} else if ( $t_history_event['type'] == TAG_ATTACHED ) {
-				$t_event = new IssueTagTimelineEvent( $t_timestamp, $t_user_id, $t_issue_id, $t_history_event['old_value'], true );
-			} else if ( $t_history_event['type'] == TAG_DETACHED ) {
-				$t_event = new IssueTagTimelineEvent( $t_timestamp, $t_user_id, $t_issue_id, $t_history_event['old_value'], false );
-			} else if ( $t_history_event['type'] == NORMAL_TYPE ) {
-				switch ( $t_history_event['field'] ) {
-					case 'status':
-						$t_event = new IssueStatusChangeTimelineEvent( $t_timestamp, $t_user_id, $t_issue_id, $t_history_event['old_value'], $t_history_event['new_value'] );
-						break;
-					case 'handler_id':
-						$t_event = new IssueAssignedTimelineEvent( $t_timestamp, $t_user_id, $t_issue_id, $t_history_event['new_value'] );
-						break;
-				}
+			switch ( $t_history_event['type'] ) {
+				case NEW_BUG:
+					$t_event = new IssueCreatedTimelineEvent( $t_timestamp, $t_user_id, $t_issue_id );
+					break;
+				case BUGNOTE_ADDED:
+					$t_bugnote_id = $t_history_event['old_value'];
+					$t_event = new IssueNoteCreatedTimelineEvent( $t_timestamp, $t_user_id, $t_issue_id, $t_bugnote_id );
+					break;
+				case BUG_MONITOR:
+					# Skip monitors added for others due to reminders, only add monitor events where added
+					# user is the same as the logged in user.
+					if ( (int)$t_history_event['old_value'] == (int)$t_history_event['userid'] ) {
+						$t_event = new IssueMonitorTimelineEvent( $t_timestamp, $t_user_id, $t_issue_id, true );
+					}
+					break;
+				case BUG_UNMONITOR:
+					$t_event = new IssueMonitorTimelineEvent( $t_timestamp, $t_user_id, $t_issue_id, false );
+					break;
+				case TAG_ATTACHED:
+					$t_event = new IssueTagTimelineEvent( $t_timestamp, $t_user_id, $t_issue_id, $t_history_event['old_value'], true );
+					break;
+				case TAG_DETACHED:
+					$t_event = new IssueTagTimelineEvent( $t_timestamp, $t_user_id, $t_issue_id, $t_history_event['old_value'], false );
+					break;
+				case NORMAL_TYPE:
+					switch ( $t_history_event['field'] ) {
+						case 'status':
+							$t_event = new IssueStatusChangeTimelineEvent( $t_timestamp, $t_user_id, $t_issue_id, $t_history_event['old_value'], $t_history_event['new_value'] );
+							break;
+						case 'handler_id':
+							$t_event = new IssueAssignedTimelineEvent( $t_timestamp, $t_user_id, $t_issue_id, $t_history_event['new_value'] );
+							break;
+					}
+					break;
 			}
 
 			if ( $t_event != null ) {
