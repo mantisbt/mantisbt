mantisbt/mantisbt

commit a80f447687722b52160d00c96c21a5be5f4329e1

Further backports of db_int*/phpdoc*

core/version_api.php
@@ -131,19 +131,19 @@ $g_cache_versions = array();
 function version_cache_row( $p_version_id, $p_trigger_errors = true ) {
 	global $g_cache_versions;
 
-	$c_version_id = db_prepare_int( $p_version_id );
-	$t_project_version_table = db_get_table( 'project_version' );
+	$c_version_id = (int)$p_version_id;
 
 	if( isset( $g_cache_versions[$c_version_id] ) ) {
 		return $g_cache_versions[$c_version_id];
 	}
 
-	$query = "SELECT *
-				  FROM $t_project_version_table
-				  WHERE id=" . db_param();
+	$t_project_version_table = db_get_table( 'project_version' );
+	$query = "SELECT * FROM $t_project_version_table WHERE id=" . db_param();
 	$result = db_query_bound( $query, array( $c_version_id ) );
 
-	if( 0 == db_num_rows( $result ) ) {
+	$row = db_fetch_array( $result );
+
+	if( !$row ) {
 		$g_cache_versions[$c_version_id] = false;
 
 		if( $p_trigger_errors ) {
@@ -154,7 +154,6 @@ function version_cache_row( $p_version_id, $p_trigger_errors = true ) {
 		}
 	}
 
-	$row = db_fetch_array( $result );
 	$g_cache_versions[$c_version_id] = $row;
 
 	return $row;
@@ -217,9 +216,8 @@ function version_ensure_unique( $p_version, $p_project_id = null ) {
  * @return int
  */
 function version_add( $p_project_id, $p_version, $p_released = VERSION_FUTURE, $p_description = '', $p_date_order = null, $p_obsolete = false ) {
-	$c_project_id = db_prepare_int( $p_project_id );
-	$c_released = db_prepare_int( $p_released );
-	$c_obsolete = db_prepare_bool( $p_obsolete );
+	$c_project_id = (int)$p_project_id ;
+	$c_released = (int)$p_released;
 
 	if( null === $p_date_order ) {
 		$c_date_order = db_now();
@@ -235,16 +233,18 @@ function version_add( $p_project_id, $p_version, $p_released = VERSION_FUTURE, $
 					( project_id, version, date_order, description, released, obsolete )
 				  VALUES
 					(" . db_param() . ', ' . db_param() . ', ' . db_param() . ', ' . db_param() . ', ' . db_param() . ', ' . db_param() . ' )';
-	db_query_bound( $query, array( $c_project_id, $p_version, $c_date_order, $p_description, $c_released, $c_obsolete ) );
+	db_query_bound( $query, array( $c_project_id, $p_version, $c_date_order, $p_description, $c_released, $p_obsolete ) );
+
+	$t_version_id = db_insert_id( $t_project_version_table );
+	
 
 	# db_query errors on failure so:
-	return db_insert_id( $t_project_version_table );
+	return $t_version_id;
 }
 
 /**
  * Update the definition of a version
  * @param VersionData @p_version_info
- * @return true
  */
 function version_update( $p_version_info ) {
 	version_ensure_exists( $p_version_info->id );
@@ -320,9 +320,6 @@ function version_update( $p_version_info ) {
 		 * @todo We probably need to update the saved filters too?
 		 */
 	}
-
-	// db_query errors on failure so:
-	return true;
 }
 
 /**
@@ -366,9 +363,6 @@ function version_remove( $p_version_id, $p_new_version = '' ) {
 				  SET target_version=" . db_param() . "
 				  WHERE ( project_id IN ( $t_project_list ) ) AND ( target_version=" . db_param() . ')';
 	db_query_bound( $query, array( $p_new_version, $t_old_version ) );
-
-	# db_query errors on failure so:
-	return true;
 }
 
 /**
@@ -377,18 +371,18 @@ function version_remove( $p_version_id, $p_new_version = '' ) {
  * @return bool
  */
 function version_remove_all( $p_project_id ) {
-	$c_project_id = db_prepare_int( $p_project_id );
+	$c_project_id = (int)$p_project_id;
 
-	$t_project_version_table = db_get_table( 'project_version' );
-	$t_bug_table = db_get_table( 'bug' );
 
 	# remove all references to versions from verison, fixed in version and target version.
+	$t_bug_table = db_get_table( 'bug' );
 	$query = "UPDATE $t_bug_table
 				  SET version='', fixed_in_version='', target_version=''
 				  WHERE project_id=" . db_param();
 	db_query_bound( $query, array( $c_project_id ) );
 
 	# remove the actual versions associated with the project.
+	$t_project_version_table = db_get_table( 'project_version' );
 	$query = "DELETE FROM $t_project_version_table
 				  WHERE project_id=" . db_param();
 	db_query_bound( $query, array( $c_project_id ) );
@@ -421,7 +415,6 @@ function version_cache_array_rows( $p_project_id_array ) {
 	}
 
 	$t_project_version_table = db_get_table( 'project_version' );
-
 	$query = "SELECT *
 				  FROM $t_project_version_table
 				  WHERE project_id IN (" . implode( ',', $c_project_id_array ) . ')
@@ -490,16 +483,14 @@ function version_get_all_rows( $p_project_id, $p_released = null, $p_obsolete = 
 
 	$query .= " ORDER BY date_order DESC";
 
-	$result = db_query_bound( $query, $query_params );
-	$count = db_num_rows( $result );
-	$rows = array();
-	for( $i = 0;$i < $count;$i++ ) {
-		$row = db_fetch_array( $result );
-		$g_cache_versions[(int) $row['id']] = $row;
+	$t_result = db_query_bound( $query, $query_params );
+	$t_rows = array();
+	while( $t_row = db_fetch_array( $t_result ) ) {
+		$g_cache_versions[(int) $t_row['id']] = $t_row;
 
-		$rows[] = $row;
+		$t_rows[] = $t_row;
 	}
-	return $rows;
+	return $t_rows;
 }
 
 /**
@@ -518,7 +509,7 @@ function version_get_all_rows_with_subs( $p_project_id, $p_released = null, $p_o
 	if( $p_released === null ) {
 		$t_released_where = '';
 	} else {
-		$c_released = db_prepare_int( $p_released );
+		$c_released = (int)$p_released;
 		$t_released_where = "AND ( released = " . db_param( $t_param_count++ ) . " )";
 		$t_query_params[] = $c_released;
 	}
@@ -526,22 +517,18 @@ function version_get_all_rows_with_subs( $p_project_id, $p_released = null, $p_o
 	if( $p_obsolete === null ) {
 		$t_obsolete_where = '';
 	} else {
-		$c_obsolete = db_prepare_bool( $p_obsolete );
 		$t_obsolete_where = "AND ( obsolete = " . db_param( $t_param_count++ ) . " )";
-		$t_query_params[] = $c_obsolete;
+		$t_query_params[] = $p_obsolete;
 	}
 
 	$t_project_version_table = db_get_table( 'project_version' );
 
-	$query = "SELECT *
-				  FROM $t_project_version_table
+	$query = "SELECT * FROM $t_project_version_table
 				  WHERE $t_project_where $t_released_where $t_obsolete_where
 				  ORDER BY date_order DESC";
 	$result = db_query_bound( $query, $t_query_params );
-	$count = db_num_rows( $result );
 	$rows = array();
-	for( $i = 0;$i < $count;$i++ ) {
-		$row = db_fetch_array( $result );
+	while( $row = db_fetch_array( $result ) ) {
 		$rows[] = $row;
 	}
 	return $rows;
@@ -561,7 +548,7 @@ function version_get_id( $p_version, $p_project_id = null, $p_inherit = null ) {
 	if( $p_project_id === null ) {
 		$c_project_id = helper_get_current_project();
 	} else {
-		$c_project_id = db_prepare_int( $p_project_id );
+		$c_project_id = (int)$p_project_id;
 	}
 
 	foreach( $g_cache_versions as $t_version ) {
@@ -580,10 +567,10 @@ function version_get_id( $p_version, $p_project_id = null, $p_inherit = null ) {
 
 	$result = db_query_bound( $query, array( $p_version ) );
 
-	if( 0 == db_num_rows( $result ) ) {
-		return false;
+	if( $t_row = db_result( $result ) ) {
+		return $t_row;
 	} else {
-		return db_result( $result );
+		return false;
 	}
 }
 
@@ -711,7 +698,7 @@ function version_get_project_where_clause( $p_project_id, $p_inherit ) {
 		}
 	}
 
-	$c_project_id = db_prepare_int( $p_project_id );
+	$c_project_id = (int)$p_project_id;
 
 	if ( $t_inherit ) {
 		$t_project_ids = project_hierarchy_inheritance( $p_project_id );

admin/install.php
@@ -203,7 +203,9 @@ if( $t_config_exists ) {
 	if( $g_db->IsConnected() ) {
 		$g_db_connected = true;
 	}
+
 	$t_cur_version = config_get( 'database_version', -1 );
+	
 	if( $t_cur_version > 1 ) {
 		$g_database_upgrade = true;
 		$f_db_exists = true;

core/print_api.php
@@ -1632,23 +1632,30 @@ function print_lost_password_link() {
 	}
 }
 
-# ===============================
-# Deprecated Functions
-# ===============================
-
-# Get icon corresponding to the specified filename
+/**
+ * Get icon corresponding to the specified filename
+ *
+ * @param string $p_filename Filename for which to retrieve icon link
+ */
 function print_file_icon( $p_filename ) {
 	$t_icon = file_get_icon_url( $p_filename );
 	echo '<img src="' . string_attribute( $t_icon['url'] ) . '" alt="' . string_attribute( $t_icon['alt'] ) . ' file icon" width="16" height="16" />';
 }
 
-# Prints an RSS image that is hyperlinked to an RSS feed.
+/**
+ * Prints an RSS image that is hyperlinked to an RSS feed.
+ *
+ * @param string $p_feed_url URI to an RSS feed
+ * @param string $p_title Title to use for hyperlink
+ */
 function print_rss( $p_feed_url, $p_title = '' ) {
 	$t_path = config_get( 'path' );
 	echo '<a class="rss" rel="alternate" href="', htmlspecialchars( $p_feed_url ), '" title="', $p_title, '"><img src="', $t_path, '/images/', 'rss.png" width="16" height="16" alt="', $p_title, '" /></a>';
 }
 
-# Prints the recently visited issues.
+/**
+ * Prints the recently visited issues.
+ */
 function print_recently_visited() {
 	if( !last_visited_enabled() ) {
 		return;

core/profile_api.php
@@ -212,13 +212,12 @@ function profile_get_all_rows( $p_user_id, $p_all_users = false ) {
 				  FROM $t_user_profile_table
 				  WHERE $query_where
 				  ORDER BY platform, os, os_build";
-	$result = db_query_bound( $query, $param );
+	$t_result = db_query_bound( $query, $param );
 
 	$t_rows = array();
-	$t_row_count = db_num_rows( $result );
 
-	for( $i = 0;$i < $t_row_count;$i++ ) {
-		array_push( $t_rows, db_fetch_array( $result ) );
+	while( $t_row = db_fetch_array( $t_result ) ) {
+		array_push( $t_rows, $t_row );
 	}
 
 	return $t_rows;
@@ -263,14 +262,11 @@ function profile_get_field_all_for_user( $p_field, $p_user_id = null ) {
 				  FROM $t_user_profile_table
 				  WHERE ( user_id=" . db_param() . " ) OR ( user_id = 0 )
 				  ORDER BY $c_field";
-	$result = db_query_bound( $query, array( $c_user_id ) );
+	$t_result = db_query_bound( $query, array( $c_user_id ) );
 
 	$t_rows = array();
 
-	$t_row_count = db_num_rows( $result );
-
-	for( $i = 0;$i < $t_row_count;$i++ ) {
-		$t_row = db_fetch_array( $result );
+	while( $t_row = db_fetch_array( $t_result ) ) {
 		array_push( $t_rows, $t_row[$c_field] );
 	}
 
@@ -294,13 +290,12 @@ function profile_get_all_for_project( $p_project_id ) {
 				  WHERE $t_project_where
 				  AND up.id = b.profile_id
 				  ORDER BY platform, os, os_build";
-	$result = db_query_bound( $query );
+	$t_result = db_query_bound( $query );
 
 	$t_rows = array();
-	$t_row_count = db_num_rows( $result );
 
-	for( $i = 0;$i < $t_row_count;$i++ ) {
-		array_push( $t_rows, db_fetch_array( $result ) );
+	while( $t_row = db_fetch_array( $t_result ) ) {
+		array_push( $t_rows, $t_row );
 	}
 
 	return $t_rows;

core/project_hierarchy_api.php
@@ -161,10 +161,10 @@ function project_hierarchy_cache( $p_show_disabled = false ) {
 	}
 	$g_cache_show_disabled = $p_show_disabled;
 
-	$t_project_table = db_get_table( 'project' );
-	$t_project_hierarchy_table = db_get_table( 'project_hierarchy' );
 	$t_enabled_clause = $p_show_disabled ? '1=1' : 'p.enabled = ' . db_param();
 
+	$t_project_table = db_get_table( 'project' );
+	$t_project_hierarchy_table = db_get_table( 'project_hierarchy' );
 	$query = "SELECT DISTINCT p.id, ph.parent_id, p.name, p.inherit_global, ph.inherit_parent
 				  FROM $t_project_table p
 				  LEFT JOIN $t_project_hierarchy_table ph
@@ -173,14 +173,11 @@ function project_hierarchy_cache( $p_show_disabled = false ) {
 				  ORDER BY p.name";
 
 	$result = db_query_bound( $query, ( $p_show_disabled ? null : array( true ) ) );
-	$row_count = db_num_rows( $result );
 
 	$g_cache_project_hierarchy = array();
 	$g_cache_project_inheritance = array();
 
-	for( $i = 0;$i < $row_count;$i++ ) {
-		$row = db_fetch_array( $result );
-
+	while ( $row = db_fetch_array( $result ) ) {
 		if( null === $row['parent_id'] ) {
 			$row['parent_id'] = ALL_PROJECTS;
 		}

core/relationship_api.php
@@ -318,17 +318,12 @@ function relationship_copy_all( $p_bug_id, $p_new_bug_id ) {
  */
 function relationship_get( $p_relationship_id ) {
 	$t_mantis_bug_relationship_table = db_get_table( 'bug_relationship' );
-
-	$query = "SELECT *
-				FROM $t_mantis_bug_relationship_table
-				WHERE id=" . db_param();
+	$query = "SELECT * FROM $t_mantis_bug_relationship_table WHERE id=" . db_param();
 	$result = db_query_bound( $query, array( (int) $p_relationship_id ) );
 
-	$t_relationship_count = db_num_rows( $result );
+	$t_relationship = db_fetch_array( $result );
 
-	if( $t_relationship_count == 1 ) {
-		$t_relationship = db_fetch_array( $result );
-
+	if( $t_relationship ) {
 		$t_bug_relationship_data = new BugRelationshipData;
 		$t_bug_relationship_data->id = $t_relationship['id'];
 		$t_bug_relationship_data->src_bug_id = $t_relationship['source_bug_id'];
@@ -344,14 +339,11 @@ function relationship_get( $p_relationship_id ) {
 /**
  * get all relationships with the given bug as source
  * @param int $p_src_bug_id Source Bug id
- * @return array array of BugRelationshipData objects
+ * @return array Array of BugRelationshipData objects
  */
 function relationship_get_all_src( $p_src_bug_id ) {
-	$c_src_bug_id = db_prepare_int( $p_src_bug_id );
-
 	$t_mantis_bug_relationship_table = db_get_table( 'bug_relationship' );
 	$t_mantis_bug_table = db_get_table( 'bug' );
-
 	$query = "SELECT $t_mantis_bug_relationship_table.id, $t_mantis_bug_relationship_table.relationship_type,
 				$t_mantis_bug_relationship_table.source_bug_id, $t_mantis_bug_relationship_table.destination_bug_id,
 				$t_mantis_bug_table.project_id
@@ -359,15 +351,15 @@ function relationship_get_all_src( $p_src_bug_id ) {
 				INNER JOIN $t_mantis_bug_table ON $t_mantis_bug_relationship_table.destination_bug_id = $t_mantis_bug_table.id
 				WHERE source_bug_id=" . db_param() . "
 				ORDER BY relationship_type, $t_mantis_bug_relationship_table.id";
-	$result = db_query_bound( $query, array( $c_src_bug_id ) );
+	$result = db_query_bound( $query, array( $p_src_bug_id ) );
 
 	$t_src_project_id = bug_get_field( $p_src_bug_id, 'project_id' );
 
 	$t_bug_relationship_data = array();
-	$t_relationship_count = db_num_rows( $result );
 	$t_bug_array = array();
-	for( $i = 0;$i < $t_relationship_count;$i++ ) {
-		$row = db_fetch_array( $result );
+	$i = 0;
+
+	while( $row = db_fetch_array( $result ) ) {
 		$t_bug_relationship_data[$i] = new BugRelationshipData;
 		$t_bug_relationship_data[$i]->id = $row['id'];
 		$t_bug_relationship_data[$i]->src_bug_id = $row['source_bug_id'];
@@ -376,8 +368,11 @@ function relationship_get_all_src( $p_src_bug_id ) {
 		$t_bug_relationship_data[$i]->dest_project_id = $row['project_id'];
 		$t_bug_relationship_data[$i]->type = $row['relationship_type'];
 		$t_bug_array[] = $row['destination_bug_id'];
+		$i++;
 	}
-	unset( $t_bug_relationship_data[$t_relationship_count] );
+
+	unset( $t_bug_relationship_data[$i] );
+
 	if( !empty( $t_bug_array ) ) {
 		bug_cache_array_rows( $t_bug_array );
 	}
@@ -388,7 +383,7 @@ function relationship_get_all_src( $p_src_bug_id ) {
 /**
  * get all relationships with the given bug as destination
  * @param int $p_dest_bug_id Destination Bug id
- * @return array array of BugRelationshipData objects
+ * @return array Array of BugRelationshipData objects
  */
 function relationship_get_all_dest( $p_dest_bug_id ) {
 	$c_dest_bug_id = db_prepare_int( $p_dest_bug_id );
@@ -408,10 +403,10 @@ function relationship_get_all_dest( $p_dest_bug_id ) {
 	$t_dest_project_id = bug_get_field( $p_dest_bug_id, 'project_id' );
 
 	$t_bug_relationship_data = array();
-	$t_relationship_count = db_num_rows( $result );
 	$t_bug_array = array();
-	for( $i = 0;$i < $t_relationship_count;$i++ ) {
-		$row = db_fetch_array( $result );
+	$i = 0;
+
+	while( $row = db_fetch_array( $result ) ) {
 		$t_bug_relationship_data[$i] = new BugRelationshipData;
 		$t_bug_relationship_data[$i]->id = $row['id'];
 		$t_bug_relationship_data[$i]->src_bug_id = $row['source_bug_id'];
@@ -420,8 +415,9 @@ function relationship_get_all_dest( $p_dest_bug_id ) {
 		$t_bug_relationship_data[$i]->dest_project_id = $t_dest_project_id;
 		$t_bug_relationship_data[$i]->type = $row['relationship_type'];
 		$t_bug_array[] = $row['source_bug_id'];
+		$i++;
 	}
-	unset( $t_bug_relationship_data[$t_relationship_count] );
+	unset( $t_bug_relationship_data[$i] );
 
 	if( !empty( $t_bug_array ) ) {
 		bug_cache_array_rows( $t_bug_array );
@@ -433,7 +429,7 @@ function relationship_get_all_dest( $p_dest_bug_id ) {
  * get all relationships associated with the given bug
  * @param int $p_bug_id  Bug id
  * @param bool &$p_is_different_projects Returned Boolean value indicating if some relationships cross project boundaries
- * @return array array of BugRelationshipData objects
+ * @return array Array of BugRelationshipData objects
  */
 function relationship_get_all( $p_bug_id, &$p_is_different_projects ) {
 	$t_src = relationship_get_all_src( $p_bug_id );
@@ -456,30 +452,22 @@ function relationship_get_all( $p_bug_id, &$p_is_different_projects ) {
  * @return int Relationship ID
  */
 function relationship_exists( $p_src_bug_id, $p_dest_bug_id ) {
-	$c_src_bug_id = db_prepare_int( $p_src_bug_id );
-	$c_dest_bug_id = db_prepare_int( $p_dest_bug_id );
+	$c_src_bug_id = (int)$p_src_bug_id;
+	$c_dest_bug_id = (int)$p_dest_bug_id;
 
 	$t_mantis_bug_relationship_table = db_get_table( 'bug_relationship' );
 
-	$t_query = "SELECT *
-				FROM $t_mantis_bug_relationship_table
-				WHERE
-				(source_bug_id=" . db_param() . "
-				AND destination_bug_id=" . db_param() . ")
+	$t_query = "SELECT * FROM $t_mantis_bug_relationship_table
+				WHERE (source_bug_id=" . db_param() . " AND destination_bug_id=" . db_param() . ")
 				OR
 				(source_bug_id=" . db_param() . "
 				AND destination_bug_id=" . db_param() . ')';
 	$result = db_query_bound( $t_query, array( $c_src_bug_id, $c_dest_bug_id, $c_dest_bug_id, $c_src_bug_id ), 1 );
 
-	$t_relationship_count = db_num_rows( $result );
-
-	if( $t_relationship_count == 1 ) {
-
+	if( $row = db_fetch_array( $result ) ) {
 		# return the first id
-		$row = db_fetch_array( $result );
 		return $row['id'];
 	} else {
-
 		# no relationship found
 		return 0;
 	}

core/string_api.php
@@ -591,16 +591,13 @@ function string_restore_valid_html_tags( $p_string, $p_multiline = true ) {
  * @return string
  */
 function string_get_bug_page( $p_action, $p_user_id = null ) {
-	if ( $p_action == 'view' ) {
-		return 'bug_view_page.php';
-	}
-
-	if ( $p_action == 'update' ) {
-		return 'bug_update_page.php';
-	}
-
-	if ( $p_action == 'report' ) {
-		return 'bug_report_page.php';
+	switch( $p_action ) {
+		case 'view':
+			return 'bug_view_page.php';
+		case 'update':
+			return 'bug_update_page.php';
+		case 'report':
+			return 'bug_report_page.php';
 	}
 
 	trigger_error( ERROR_GENERIC, ERROR );

core/summary_api.php
@@ -90,9 +90,13 @@ function summary_helper_get_developer_label ( $p_user_id ) {
 
 }
 
-# Used in summary reports
-# this function prints out the summary for the given enum setting
-# The enum field name is passed in through $p_enum
+/**
+ * Used in summary reports - this function prints out the summary for the given enum setting
+ * The enum field name is passed in through $p_enum
+ *
+ * @param string $p_enum enum field name
+ * @throws MantisBT\Exception\Database\FieldNotFound
+ */
 function summary_print_by_enum( $p_enum ) {
 	$t_project_id = helper_get_current_project();
 	$t_user_id = auth_get_current_user_id();
@@ -238,8 +242,12 @@ function summary_print_by_enum( $p_enum ) {
 	}
 }
 
-# prints the bugs submitted in the last X days (default is 1 day) for the
-# current project
+/**
+ * prints the bugs submitted in the last X days (default is 1 day) for the current project
+ *
+ * @param int $p_num_days number of days
+ * @return int
+ */
 function summary_new_bug_count_by_date( $p_time_length = 1 ) {
 	$t_mantis_bug_table = db_get_table( 'bug' );
 
@@ -260,8 +268,12 @@ function summary_new_bug_count_by_date( $p_time_length = 1 ) {
 	return db_result( $result, 0 );
 }
 
-# returns the number of bugs resolved in the last X days (default is 1 day) for the
-# current project
+/**
+ * returns the number of bugs resolved in the last X days (default is 1 day) for the current project
+ *
+ * @param int $p_num_days number of days
+ * @return int
+ */
 function summary_resolved_bug_count_by_date( $p_time_length = 1 ) {
 	$t_bug_table = db_get_table( 'bug' );
 	$t_bug_history_table = db_get_table( 'bug_history' );
@@ -292,8 +304,11 @@ function summary_resolved_bug_count_by_date( $p_time_length = 1 ) {
 	return db_result( $result, 0 );
 }
 
-# This function shows the number of bugs submitted in the last X days
-# An array of integers representing days is passed in
+/**
+ * This function shows the number of bugs submitted in the last X days
+ *
+ * @param array $p_date_array An array of integers representing days is passed in
+ */
 function summary_print_by_date( $p_date_array ) {
 	$arr_count = count( $p_date_array );
 	foreach( $p_date_array as $t_days ) {
@@ -331,13 +346,12 @@ function summary_print_by_date( $p_date_array ) {
 		print( "\n<td class=\"right$t_style\">$t_balance</td>\n" );
 		print( "</tr>\n" );
 	}
-
-	# end foreach
 }
 
-# Print list of open bugs with the highest activity score
-# the score is calculated assigning one "point" for each history event
-# associated with the bug
+/**
+ * Print list of open bugs with the highest activity score the score is calculated assigning 
+ * one "point" for each history event associated with the bug
+ */
 function summary_print_by_activity() {
 	$t_mantis_bug_table = db_get_table( 'bug' );
 	$t_mantis_history_table = db_get_table( 'bug_history' );

commit 1a444345d5144a104214142f52723d8af65046a9

Consistently use db_prepare_bool() for boolean columns
In Version API, the released column was treated as an integer, despite
the fact that it's defined as 'L' in the schema. This can cause problems
with PostgreSQL.

Also fixes #17333, Oracle throwed an ORA-01400 error when adding a new
version (regression caused by a80f447687722b52160d00c96c21a5be5f4329e1,
which removed the call to db_prepare_bool()).

core/version_api.php
@@ -218,7 +218,8 @@ function version_ensure_unique( $p_version, $p_project_id = null ) {
  */
 function version_add( $p_project_id, $p_version, $p_released = VERSION_FUTURE, $p_description = '', $p_date_order = null, $p_obsolete = false ) {
 	$c_project_id = (int)$p_project_id ;
-	$c_released = (int)$p_released;
+	$c_released = db_prepare_bool( $p_released );
+	$c_obsolete = db_prepare_bool( $p_obsolete );
 
 	if( null === $p_date_order ) {
 		$c_date_order = db_now();
@@ -234,7 +235,7 @@ function version_add( $p_project_id, $p_version, $p_released = VERSION_FUTURE, $
 					( project_id, version, date_order, description, released, obsolete )
 				  VALUES
 					(" . db_param() . ', ' . db_param() . ', ' . db_param() . ', ' . db_param() . ', ' . db_param() . ', ' . db_param() . ' )';
-	db_query_bound( $t_query, array( $c_project_id, $p_version, $c_date_order, $p_description, $c_released, $p_obsolete ) );
+	db_query_bound( $t_query, array( $c_project_id, $p_version, $c_date_order, $p_description, $c_released, $c_obsolete ) );
 
 	$t_version_id = db_insert_id( $t_project_version_table );
 
@@ -260,7 +261,7 @@ function version_update( VersionData $p_version_info ) {
 	$c_version_name = $p_version_info->version;
 	$c_old_version_name = $t_old_version_name;
 	$c_description = $p_version_info->description;
-	$c_released = (int)$p_version_info->released;
+	$c_released = db_prepare_bool( $p_version_info->released );
 	$c_obsolete = db_prepare_bool( $p_version_info->obsolete );
 	$c_date_order = $p_version_info->date_order;
 	$c_project_id = (int)$p_version_info->project_id;
@@ -474,7 +475,7 @@ function version_get_all_rows( $p_project_id, $p_released = null, $p_obsolete = 
 
 	if( $p_released !== null ) {
 		$t_query .= " AND released = " . db_param();
-		$t_query_params[] = (int)$p_released;
+		$t_query_params[] = db_prepare_bool( $p_released );
 	}
 
 	if( $p_obsolete !== null ) {
@@ -509,7 +510,7 @@ function version_get_all_rows_with_subs( $p_project_id, $p_released = null, $p_o
 	if( $p_released === null ) {
 		$t_released_where = '';
 	} else {
-		$c_released = (int)$p_released;
+		$c_released = db_prepare_bool( $p_released );
 		$t_released_where = "AND ( released = " . db_param() . " )";
 		$t_query_params[] = $c_released;
 	}
@@ -518,7 +519,7 @@ function version_get_all_rows_with_subs( $p_project_id, $p_released = null, $p_o
 		$t_obsolete_where = '';
 	} else {
 		$t_obsolete_where = "AND ( obsolete = " . db_param() . " )";
-		$t_query_params[] = $p_obsolete;
+		$t_query_params[] = db_prepare_bool( $p_obsolete );
 	}
 
 	$t_project_version_table = db_get_table( 'project_version' );
@@ -666,7 +667,8 @@ function version_get( $p_version_id ) {
 function version_prepare_db( VersionData $p_version_info ) {
 	$p_version_info->id = (int)$p_version_info->id;
 	$p_version_info->project_id = (int)$p_version_info->project_id;
-	$p_version_info->released = (int)$p_version_info->released;
+	$p_version_info->released = db_prepare_bool( $p_version_info->released );
+	$p_version_info->obsolete = db_prepare_bool( $p_version_info->obsolete );
 
 	return $p_version_info;
 }