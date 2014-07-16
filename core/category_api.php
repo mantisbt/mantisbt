mantisbt/mantisbt

commit 5458f7020999317b35f403fa2a0e477d6182ec12

Code Standards/PHP Doc improvements
Improvements to code base to follow coding standards,
and improve phpdoc validation from PHP Codesniffer rules

core/category_api.php
@@ -51,13 +51,13 @@ $g_category_cache = array();
 
 /**
  * Check whether the category exists in the project
- * @param int $p_category_id category id
- * @return bool Return true if the category exists, false otherwise
+ * @param integer $p_category_id A Category identifier.
+ * @return boolean Return true if the category exists, false otherwise
  * @access public
  */
 function category_exists( $p_category_id ) {
 	global $g_category_cache;
-	if( isset( $g_category_cache[(int) $p_category_id] ) ) {
+	if( isset( $g_category_cache[(int)$p_category_id] ) ) {
 		return true;
 	}
 
@@ -75,7 +75,8 @@ function category_exists( $p_category_id ) {
 /**
  * Check whether the category exists in the project
  * Trigger an error if it does not
- * @param int $p_category_id category id
+ * @param integer $p_category_id A Category identifier.
+ * @return void
  * @access public
  */
 function category_ensure_exists( $p_category_id ) {
@@ -86,18 +87,18 @@ function category_ensure_exists( $p_category_id ) {
 
 /**
  * Check whether the category is unique within a project
- * @param int $p_project_id project id
- * @param string $p_name project name
- * @return bool Returns true if the category is unique, false otherwise
+ * @param integer $p_project_id A project identifier.
+ * @param string  $p_name       Project name.
+ * @return boolean Returns true if the category is unique, false otherwise
  * @access public
  */
 function category_is_unique( $p_project_id, $p_name ) {
 	$t_category_table = db_get_table( 'category' );
-	$query = "SELECT COUNT(*) FROM $t_category_table
+	$t_query = "SELECT COUNT(*) FROM $t_category_table
 					WHERE project_id=" . db_param() . " AND " . db_helper_like( 'name' );
-	$count = db_result( db_query_bound( $query, array( $p_project_id, $p_name ) ) );
+	$t_count = db_result( db_query_bound( $t_query, array( $p_project_id, $p_name ) ) );
 
-	if( 0 < $count ) {
+	if( 0 < $t_count ) {
 		return false;
 	} else {
 		return true;
@@ -107,9 +108,9 @@ function category_is_unique( $p_project_id, $p_name ) {
 /**
  * Check whether the category is unique within a project
  * Trigger an error if it is not
- * @param int $p_project_id Project id
- * @param string $p_name Category Name
- * @return null
+ * @param integer $p_project_id Project identifier.
+ * @param string  $p_name       Category Name.
+ * @return void
  * @access public
  */
 function category_ensure_unique( $p_project_id, $p_name ) {
@@ -120,9 +121,9 @@ function category_ensure_unique( $p_project_id, $p_name ) {
 
 /**
  * Add a new category to the project
- * @param int $p_project_id Project id
- * @param string $p_name Category Name
- * @return int Category ID
+ * @param integer $p_project_id Project identifier.
+ * @param string  $p_name       Category Name.
+ * @return integer Category ID
  * @access public
  */
 function category_add( $p_project_id, $p_name ) {
@@ -144,10 +145,10 @@ function category_add( $p_project_id, $p_name ) {
 
 /**
  * Update the name and user associated with the category
- * @param int $p_category_id Category id
- * @param string $p_name Category Name
- * @param int $p_assigned_to User ID that category is assigned to
- * @return bool
+ * @param integer $p_category_id Category identifier.
+ * @param string  $p_name        Category Name.
+ * @param integer $p_assigned_to User ID that category is assigned to.
+ * @return void
  * @access public
  */
 function category_update( $p_category_id, $p_name, $p_assigned_to ) {
@@ -166,23 +167,20 @@ function category_update( $p_category_id, $p_name, $p_assigned_to ) {
 	# Add bug history entries if we update the category's name
 	if( $t_old_category['name'] != $p_name ) {
 		$t_bug_table = db_get_table( 'bug' );
-		$query = "SELECT id FROM $t_bug_table WHERE category_id=" . db_param();
-		$t_result = db_query_bound( $query, array( $p_category_id ) );
+		$t_query = "SELECT id FROM $t_bug_table WHERE category_id=" . db_param();
+		$t_result = db_query_bound( $t_query, array( $p_category_id ) );
 
 		while( $t_bug_row = db_fetch_array( $t_result ) ) {
 			history_log_event_direct( $t_bug_row['id'], 'category', $t_old_category['name'], $p_name );
 		}
 	}
-
-	# db_query_bound() errors on failure so:
-	return true;
 }
 
 /**
  * Remove a category from the project
- * @param int $p_category_id Category id
- * @param int $p_new_category_id new category id (to replace existing category)
- * @return bool
+ * @param integer $p_category_id     Category identifier.
+ * @param integer $p_new_category_id New category id (to replace existing category).
+ * @return void
  * @access public
  */
 function category_remove( $p_category_id, $p_new_category_id = 0 ) {
@@ -199,8 +197,8 @@ function category_remove( $p_category_id, $p_new_category_id = 0 ) {
 
 	# update bug history entries
 	$t_bug_table = db_get_table( 'bug' );
-	$query = "SELECT id FROM $t_bug_table WHERE category_id=" . db_param();
-	$t_result = db_query_bound( $query, array( $p_category_id ) );
+	$t_query = "SELECT id FROM $t_bug_table WHERE category_id=" . db_param();
+	$t_result = db_query_bound( $t_query, array( $p_category_id ) );
 
 	while( $t_bug_row = db_fetch_array( $t_result ) ) {
 		history_log_event_direct( $t_bug_row['id'], 'category', $t_category_row['name'], category_full_name( $p_new_category_id, false ) );
@@ -210,16 +208,13 @@ function category_remove( $p_category_id, $p_new_category_id = 0 ) {
 	$t_query = "UPDATE $t_bug_table SET category_id=" . db_param() . "
 				  WHERE category_id=" . db_param();
 	db_query_bound( $t_query, array( $p_new_category_id, $p_category_id ) );
-
-	# db_query_bound() errors on failure so:
-	return true;
 }
 
 /**
  * Remove all categories associated with a project
- * @param int $p_project_id Project ID
- * @param int $p_new_category_id new category id (to replace existing category)
- * @return bool
+ * @param integer $p_project_id      A Project identifier.
+ * @param integer $p_new_category_id New category id (to replace existing category).
+ * @return boolean
  * @access public
  */
 function category_remove_all( $p_project_id, $p_new_category_id = 0 ) {
@@ -270,8 +265,8 @@ function category_remove_all( $p_project_id, $p_new_category_id = 0 ) {
 
 /**
  * Return the definition row for the category
- * @param int $p_category_id Category id
- * @return array array containing category details
+ * @param integer $p_category_id Category identifier.
+ * @return array An array containing category details.
  * @access public
  */
 function category_get_row( $p_category_id ) {
@@ -284,8 +279,7 @@ function category_get_row( $p_category_id ) {
 	}
 
 	$t_category_table = db_get_table( 'category' );
-	$t_query = "SELECT * FROM $t_category_table
-				WHERE id=" . db_param();
+	$t_query = "SELECT * FROM $t_category_table WHERE id=" . db_param();
 	$t_result = db_query_bound( $t_query, array( $p_category_id ) );
 	$t_row = db_fetch_array( $t_result );
 	if( !$t_row ) {
@@ -299,24 +293,24 @@ function category_get_row( $p_category_id ) {
 /**
  * Sort categories based on what project they're in.
  * Call beforehand with a single parameter to set a 'preferred' project.
- * @param array $p_category1 array containing category details
- * @param array $p_category2 array containing category details
- * @return int integer representing sort order
+ * @param array $p_category1 Array containing category details.
+ * @param array $p_category2 Array containing category details.
+ * @return integer|null An integer representing sort order.
  * @access public
  */
-function category_sort_rows_by_project( $p_category1, $p_category2 = null ) {
-	static $p_project_id = null;
+function category_sort_rows_by_project( array $p_category1, array $p_category2 = null ) {
+	static $s_project_id = null;
 	if( is_null( $p_category2 ) ) {
 		# Set a target project
-		$p_project_id = $p_category1;
-		return;
+		$s_project_id = $p_category1;
+		return null;
 	}
 
-	if( !is_null( $p_project_id ) ) {
-		if( $p_category1['project_id'] == $p_project_id && $p_category2['project_id'] != $p_project_id ) {
+	if( !is_null( $s_project_id ) ) {
+		if( $p_category1['project_id'] == $s_project_id && $p_category2['project_id'] != $s_project_id ) {
 			return -1;
 		}
-		if( $p_category1['project_id'] != $p_project_id && $p_category2['project_id'] == $p_project_id ) {
+		if( $p_category1['project_id'] != $s_project_id && $p_category2['project_id'] == $s_project_id ) {
 			return 1;
 		}
 	}
@@ -333,17 +327,18 @@ $g_cache_category_project = null;
 
 /**
  * Cache categories from multiple projects
- * @param array $p_project_id_array array of project ids
+ * @param array $p_project_id_array Array of project identifiers.
+ * @return void
  */
-function category_cache_array_rows_by_project( $p_project_id_array ) {
+function category_cache_array_rows_by_project( array $p_project_id_array ) {
 	global $g_category_cache, $g_cache_category_project;
 
 	$c_project_id_array = array();
 
 	foreach( $p_project_id_array as $t_project_id ) {
-		if( !isset( $g_cache_category_project[(int) $t_project_id] ) ) {
-			$c_project_id_array[] = (int) $t_project_id;
-			$g_cache_category_project[(int) $t_project_id] = array();
+		if( !isset( $g_cache_category_project[(int)$t_project_id] ) ) {
+			$c_project_id_array[] = (int)$t_project_id;
+			$g_cache_category_project[(int)$t_project_id] = array();
 		}
 	}
 
@@ -363,13 +358,13 @@ function category_cache_array_rows_by_project( $p_project_id_array ) {
 
 	$t_rows = array();
 	while( $t_row = db_fetch_array( $t_result ) ) {
-		$g_category_cache[(int) $t_row['id']] = $t_row;
+		$g_category_cache[(int)$t_row['id']] = $t_row;
 
-		$t_rows[ (int)$t_row[ 'project_id' ] ][] = $t_row['id'];
+		$t_rows[(int)$t_row['project_id']][] = $t_row['id'];
 	}
 
 	foreach( $t_rows as $t_project_id => $t_row ) {
-		$g_cache_category_project[ (int)$t_project_id ] = $t_row;
+		$g_cache_category_project[(int)$t_project_id] = $t_row;
 	}
 	return;
 }
@@ -379,7 +374,7 @@ function category_cache_array_rows_by_project( $p_project_id_array ) {
  *	the specified projects.  If no project is specified, use the current project.
  *	If the current project is ALL_PROJECTS get all categories for all accessible projects.
  *	For all cases, get global categories and subproject categories according to configured inheritance settings.
- *	@param int|null $p_project_id A specific project or null
+ *	@param integer|null $p_project_id A specific project or null.
  *	@return array A unique array of category names
  */
 function category_get_filter_list( $p_project_id = null ) {
@@ -403,12 +398,12 @@ function category_get_filter_list( $p_project_id = null ) {
 	$t_project_ids = array_merge( $t_project_ids, $t_subproject_ids );
 
 	$t_categories = array();
-	foreach( $t_project_ids AS $t_id ) {
+	foreach( $t_project_ids as $t_id ) {
 		$t_categories = array_merge( $t_categories, category_get_all_rows( $t_id ) );
 	}
 
 	$t_unique = array();
-	foreach( $t_categories AS $t_category ) {
+	foreach( $t_categories as $t_category ) {
 		if( !in_array( $t_category['name'], $t_unique ) ) {
 			$t_unique[] = $t_category['name'];
 		}
@@ -420,18 +415,18 @@ function category_get_filter_list( $p_project_id = null ) {
 /**
  * Return all categories for the specified project id.
  * Obeys project hierarchies and such.
- * @param int $p_project_id Project id
- * @param bool $p_inherit indicates whether to inherit categories from parent projects, or null to use configuration default.
- * @param bool $p_sort_by_project
+ * @param integer $p_project_id      A Project identifier.
+ * @param boolean $p_inherit         Indicates whether to inherit categories from parent projects, or null to use configuration default.
+ * @param boolean $p_sort_by_project Whether to sort by project.
  * @return array array of categories
  * @access public
  */
 function category_get_all_rows( $p_project_id, $p_inherit = null, $p_sort_by_project = false ) {
 	global $g_category_cache, $g_cache_category_project;
 
-	if( isset( $g_cache_category_project[ (int)$p_project_id ] ) ) {
-		if( !empty( $g_cache_category_project[ (int)$p_project_id ]) ) {
-			foreach( $g_cache_category_project[ (int)$p_project_id ] as $t_id ) {
+	if( isset( $g_cache_category_project[(int)$p_project_id] ) ) {
+		if( !empty( $g_cache_category_project[(int)$p_project_id]) ) {
+			foreach( $g_cache_category_project[(int)$p_project_id] as $t_id ) {
 				$t_categories[] = category_get_row( $t_id );
 			}
 
@@ -474,34 +469,34 @@ function category_get_all_rows( $p_project_id, $p_inherit = null, $p_sort_by_pro
 				WHERE $t_project_where
 				ORDER BY c.name ";
 	$t_result = db_query_bound( $t_query );
-	$rows = array();
-	while( $row = db_fetch_array( $t_result ) ) {
-		$rows[] = $row;
-		$g_category_cache[(int) $row['id']] = $row;
+	$t_rows = array();
+	while( $t_row = db_fetch_array( $t_result ) ) {
+		$t_rows[] = $t_row;
+		$g_category_cache[(int)$t_row['id']] = $t_row;
 	}
 
 	if( $p_sort_by_project ) {
 		category_sort_rows_by_project( $p_project_id );
-		usort( $rows, 'category_sort_rows_by_project' );
+		usort( $t_rows, 'category_sort_rows_by_project' );
 		category_sort_rows_by_project( null );
 	}
 
-	return $rows;
+	return $t_rows;
 }
 
 /**
  * Cache an set of category ids
- * @param array $p_cat_id_array array of category id's
- * @return null
+ * @param array $p_cat_id_array Array of category identifiers.
+ * @return void
  * @access public
  */
-function category_cache_array_rows( $p_cat_id_array ) {
+function category_cache_array_rows( array $p_cat_id_array ) {
 	global $g_category_cache;
 	$c_cat_id_array = array();
 
 	foreach( $p_cat_id_array as $t_cat_id ) {
-		if( !isset( $g_category_cache[(int) $t_cat_id] ) ) {
-			$c_cat_id_array[] = (int) $t_cat_id;
+		if( !isset( $g_category_cache[(int)$t_cat_id] ) ) {
+			$c_cat_id_array[] = (int)$t_cat_id;
 		}
 	}
 
@@ -512,14 +507,14 @@ function category_cache_array_rows( $p_cat_id_array ) {
 	$t_category_table = db_get_table( 'category' );
 	$t_project_table = db_get_table( 'project' );
 
-	$query = "SELECT c.*, p.name AS project_name FROM $t_category_table c
+	$t_query = "SELECT c.*, p.name AS project_name FROM $t_category_table c
 				LEFT JOIN $t_project_table p
 					ON c.project_id=p.id
 				WHERE c.id IN (" . implode( ',', $c_cat_id_array ) . ')';
-	$t_result = db_query_bound( $query );
+	$t_result = db_query_bound( $t_query );
 
-	while( $row = db_fetch_array( $t_result ) ) {
-		$g_category_cache[(int) $row['id']] = $row;
+	while( $t_row = db_fetch_array( $t_result ) ) {
+		$g_category_cache[(int)$t_row['id']] = $t_row;
 	}
 	return;
 }
@@ -527,8 +522,8 @@ function category_cache_array_rows( $p_cat_id_array ) {
 /**
  * Given a category id and a field name, this function returns the field value.
  * An error will be triggered for a non-existent category id or category id = 0.
- * @param int $p_category_id category id
- * @param string $p_field_name field name
+ * @param integer $p_category_id A category identifier.
+ * @param string  $p_field_name  Field name.
  * @return string field value
  * @access public
  */
@@ -540,7 +535,7 @@ function category_get_field( $p_category_id, $p_field_name ) {
 /**
  * Given a category id, this function returns the category name.
  * An error will be triggered for a non-existent category id or category id = 0.
- * @param int $p_category_id category id
+ * @param integer $p_category_id A category identifier.
  * @return string category name
  * @access public
  */
@@ -552,19 +547,18 @@ function category_get_name( $p_category_id ) {
  * Given a category name and project, this function returns the category id.
  * An error will be triggered if the specified project does not have a
  * category with that name.
- * @param string $p_category_name category name
- * @param int $p_project_id project id
- * @param bool $p_trigger_errors trigger error on failure
- * @return bool
+ * @param string  $p_category_name  Category name to retrieve.
+ * @param integer $p_project_id     A project identifier.
+ * @param boolean $p_trigger_errors Whether to trigger error on failure.
+ * @return boolean
  * @access public
  */
 function category_get_id_by_name( $p_category_name, $p_project_id, $p_trigger_errors = true ) {
 	$t_category_table = db_get_table( 'category' );
 	$t_project_name = project_get_name( $p_project_id );
 
-	$t_query = "SELECT id FROM $t_category_table
-				WHERE name=" . db_param() . " AND project_id=" . db_param();
-	$t_result = db_query_bound( $t_query, array( $p_category_name, (int) $p_project_id ) );
+	$t_query = "SELECT id FROM $t_category_table WHERE name=" . db_param() . " AND project_id=" . db_param();
+	$t_result = db_query_bound( $t_query, array( $p_category_name, (int)$p_project_id ) );
 	$t_id = db_result( $t_result );
 	if( $t_id === false ) {
 		if( $p_trigger_errors ) {
@@ -580,9 +574,9 @@ function category_get_id_by_name( $p_category_name, $p_project_id, $p_trigger_er
 
 /**
  * Retrieves category name (including project name if required)
- * @param string $p_category_id category id
- * @param bool $p_show_project show project details
- * @param int $p_current_project current project id override
+ * @param string  $p_category_id     Category identifier.
+ * @param boolean $p_show_project    Show project details.
+ * @param integer $p_current_project Current project id override.
  * @return string category full name
  * @access public
  */

commit 76f82ef55a36fed02b4230932a8fb38f55b8165b

Fix E_RECOVERABLE_ERROR in category API
category_sort_rows_by_project() is called in 2 steps:
1. with a project id in 1st param to set the "preferred" project (and
   2nd param null)
2. with 2 arrays containing category details

Because of this behavior, the 'array' type hint can't be used on the
first parameter as it throws an error when initializing the sort in
category_get_all_rows().

Regression introduced by 5458f7020999317b35f403fa2a0e477d6182ec12

Fixes #17515

core/category_api.php
@@ -293,12 +293,12 @@ function category_get_row( $p_category_id ) {
 /**
  * Sort categories based on what project they're in.
  * Call beforehand with a single parameter to set a 'preferred' project.
- * @param array $p_category1 Array containing category details.
+ * @param int|array $p_category1 Id of preferred project or array containing category details.
  * @param array $p_category2 Array containing category details.
  * @return integer|null An integer representing sort order.
  * @access public
  */
-function category_sort_rows_by_project( array $p_category1, array $p_category2 = null ) {
+function category_sort_rows_by_project( $p_category1, array $p_category2 = null ) {
 	static $s_project_id = null;
 	if( is_null( $p_category2 ) ) {
 		# Set a target project