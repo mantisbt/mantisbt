mantisbt/mantisbt

commit b78f966e013d494ea27d92bba39d797a1266b7b7

Code Standards/PHP Doc improvements
Improvements to code base to follow coding standards,
and improve phpdoc validation from PHP Codesniffer rules

core/profile_api.php
@@ -79,10 +79,10 @@ function profile_create( $p_user_id, $p_platform, $p_os, $p_os_build, $p_descrip
 	$t_user_profile_table = db_get_table( 'user_profile' );
 
 	# Add profile
-	$t_query = "INSERT INTO $t_user_profile_table
+	$t_query = 'INSERT INTO ' . $t_user_profile_table . '
 				    ( user_id, platform, os, os_build, description )
 				  VALUES
-				    ( " . db_param() . ', ' . db_param() . ', ' . db_param() . ', ' . db_param() . ', ' . db_param() . ' )';
+				    ( ' . db_param() . ', ' . db_param() . ', ' . db_param() . ', ' . db_param() . ', ' . db_param() . ' )';
 	db_query_bound( $t_query, array( $p_user_id, $p_platform, $p_os, $p_os_build, $p_description ) );
 
 	return db_insert_id( $t_user_profile_table );
@@ -106,7 +106,7 @@ function profile_delete( $p_user_id, $p_profile_id ) {
 	$t_user_profile_table = db_get_table( 'user_profile' );
 
 	# Delete the profile
-	$t_query = "DELETE FROM $t_user_profile_table WHERE id=" . db_param() . ' AND user_id=' . db_param();
+	$t_query = 'DELETE FROM ' . $t_user_profile_table . ' WHERE id=' . db_param() . ' AND user_id=' . db_param();
 	db_query_bound( $t_query, array( $p_profile_id, $p_user_id ) );
 }
 
@@ -146,8 +146,8 @@ function profile_update( $p_user_id, $p_profile_id, $p_platform, $p_os, $p_os_bu
 	$t_user_profile_table = db_get_table( 'user_profile' );
 
 	# Add item
-	$t_query = "UPDATE $t_user_profile_table
-				  SET platform=" . db_param() . ',
+	$t_query = 'UPDATE ' . $t_user_profile_table . '
+				  SET platform=' . db_param() . ',
 				  	  os=' . db_param() . ',
 					  os_build=' . db_param() . ',
 					  description=' . db_param() . '
@@ -164,7 +164,7 @@ function profile_update( $p_user_id, $p_profile_id, $p_platform, $p_os, $p_os_bu
 function profile_get_row( $p_user_id, $p_profile_id ) {
 	$t_user_profile_table = db_get_table( 'user_profile' );
 
-	$t_query = "SELECT * FROM $t_user_profile_table WHERE id=" . db_param() . ' AND user_id=' . db_param();
+	$t_query = 'SELECT * FROM ' . $t_user_profile_table . ' WHERE id=' . db_param() . ' AND user_id=' . db_param();
 	$t_result = db_query_bound( $t_query, array( $p_profile_id, $p_user_id ) );
 
 	return db_fetch_array( $t_result );
@@ -179,7 +179,7 @@ function profile_get_row( $p_user_id, $p_profile_id ) {
 function profile_get_row_direct( $p_profile_id ) {
 	$t_user_profile_table = db_get_table( 'user_profile' );
 
-	$t_query = "SELECT * FROM $t_user_profile_table WHERE id=" . db_param();
+	$t_query = 'SELECT * FROM ' . $t_user_profile_table . ' WHERE id=' . db_param();
 	$t_result = db_query_bound( $t_query, array( $p_profile_id ) );
 
 	return db_fetch_array( $t_result );
@@ -202,9 +202,9 @@ function profile_get_all_rows( $p_user_id, $p_all_users = false ) {
 		$t_param[] = ALL_USERS;
 	}
 
-	$t_query = "SELECT * FROM $t_user_profile_table
-				  WHERE $t_query_where
-				  ORDER BY platform, os, os_build";
+	$t_query = 'SELECT * FROM ' . $t_user_profile_table . '
+				  WHERE ' . $t_query_where . '
+				  ORDER BY platform, os, os_build';
 	$t_result = db_query_bound( $t_query, $t_param );
 
 	$t_rows = array();
@@ -251,10 +251,10 @@ function profile_get_field_all_for_user( $p_field, $p_user_id = null ) {
 
 	$t_user_profile_table = db_get_table( 'user_profile' );
 
-	$t_query = "SELECT DISTINCT $c_field
-				  FROM $t_user_profile_table
-				  WHERE ( user_id=" . db_param() . " ) OR ( user_id = 0 )
-				  ORDER BY $c_field";
+	$t_query = 'SELECT DISTINCT $c_field
+				  FROM ' . $t_user_profile_table . '
+				  WHERE ( user_id=' . db_param() . ' ) OR ( user_id = 0 )
+				  ORDER BY ' . $c_field;
 	$t_result = db_query_bound( $t_query, array( $c_user_id ) );
 
 	$t_rows = array();
@@ -277,11 +277,11 @@ function profile_get_all_for_project( $p_project_id ) {
 	$t_bug_table = db_get_table( 'bug' );
 	$t_user_profile_table = db_get_table( 'user_profile' );
 
-	$t_query = "SELECT DISTINCT(up.id), up.user_id, up.platform, up.os, up.os_build
-				  FROM $t_user_profile_table up, $t_bug_table b
-				  WHERE $t_project_where
+	$t_query = 'SELECT DISTINCT(up.id), up.user_id, up.platform, up.os, up.os_build
+				  FROM ' . $t_user_profile_table . ' up, ' . $t_bug_table . ' b
+				  WHERE ' . $t_project_where . '
 				  AND up.id = b.profile_id
-				  ORDER BY platform, os, os_build";
+				  ORDER BY platform, os, os_build';
 	$t_result = db_query_bound( $t_query );
 
 	$t_rows = array();
@@ -301,7 +301,7 @@ function profile_get_all_for_project( $p_project_id ) {
 function profile_get_default( $p_user_id ) {
 	$t_mantis_user_pref_table = db_get_table( 'user_pref' );
 
-	$t_query = "SELECT default_profile FROM $t_mantis_user_pref_table WHERE user_id=" . db_param();
+	$t_query = 'SELECT default_profile FROM ' . $t_mantis_user_pref_table . ' WHERE user_id=' . db_param();
 	$t_result = db_query_bound( $t_query, array( $p_user_id ) );
 
 	$t_default_profile = (int)db_result( $t_result, 0, 0 );

commit 17cb6c1f35781c030820efa46e35614aacb7eb97

Fix SQL error in profile_get_field_all_for_user()
Regression from b78f966e013d494ea27d92bba39d797a1266b7b7

core/profile_api.php
@@ -251,7 +251,7 @@ function profile_get_field_all_for_user( $p_field, $p_user_id = null ) {
 
 	$t_user_profile_table = db_get_table( 'user_profile' );
 
-	$t_query = 'SELECT DISTINCT $c_field
+	$t_query = 'SELECT DISTINCT ' . $c_field . '
 				  FROM ' . $t_user_profile_table . '
 				  WHERE ( user_id=' . db_param() . ' ) OR ( user_id = 0 )
 				  ORDER BY ' . $c_field;