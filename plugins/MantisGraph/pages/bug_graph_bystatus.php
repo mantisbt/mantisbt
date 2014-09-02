mantisbt/mantisbt

commit ed84127ac2e41bf0c429ec8f048b1f1d6309d6eb

Update db_get_table() calls for remaining tables
Commit ac49ad8 adds support for the new
table name syntax,allowing us to replace db_get_table calls within queries.

Calls to db_get_table that are used by other database function,
i.e. db_insert_id, db_field_exists are deliberately left unchanged at this
stage.

This is aimed at breaking down the future DB API changes into manageable
chunks, and to aid the review process after 1.3 for 2.0.

plugins/MantisGraph/pages/bug_graph_bystatus.php
@@ -61,9 +61,6 @@ if( count( $t_rows ) == 0 ) {
 	exit();
 }
 
-$t_bug_table = db_get_table( 'bug' );
-$t_bug_hist_table = db_get_table( 'bug_history' );
-
 $t_marker = array();
 $t_data = array();
 $t_ptr = 0;
@@ -97,7 +94,7 @@ foreach ( $t_rows as $t_row ) {
 # get the history for these bugs over the interval required to offset the data
 # type = 0 and field=status are status changes
 # type = 1 are new bugs
-$t_select = 'SELECT bug_id, type, old_value, new_value, date_modified FROM ' . $t_bug_hist_table .
+$t_select = 'SELECT bug_id, type, old_value, new_value, date_modified FROM {bug_history}
 	' WHERE bug_id in ('. implode( ',', $t_bug ) .
 	') and ( (type=' . NORMAL_TYPE . ' and field_name=\'status\')
 		or type=' . NEW_BUG . ' ) and date_modified >= ' . db_param() .

account_sponsor_page.php
@@ -109,13 +109,12 @@ $t_project = helper_get_current_project();
 # get issues user has sponsored
 $t_user = auth_get_current_user_id();
 $t_resolved = config_get( 'bug_resolved_status_threshold' );
-$t_bug_table = db_get_table( 'bug' );
 $t_payment = config_get( 'payment_enable', 0 );
 
 $t_project_clause = helper_project_specific_where( $t_project );
 
 $t_query = 'SELECT b.id as bug, s.id as sponsor, s.paid, b.project_id, b.fixed_in_version, b.status
-	FROM ' . $t_bug_table . ' b, {sponsorship} s
+	FROM {bug} b, {sponsorship} s
 	WHERE s.user_id=' . db_param() . ' AND s.bug_id = b.id ' .
 	( $t_show_all ? '' : 'AND ( b.status < ' . db_param() . ' OR s.paid < ' . SPONSORSHIP_PAID . ')' ) . '
 	AND ' . $t_project_clause . '
@@ -230,7 +229,7 @@ if( $t_sponsor_count === 0 ) {
 <?php } # end sponsored issues
 
 $t_query = 'SELECT b.id as bug, s.id as sponsor, s.paid, b.project_id, b.fixed_in_version, b.status
-	FROM ' . $t_bug_table . ' b, {sponsorship} s
+	FROM {bug} b, {sponsorship} s
 	WHERE b.handler_id=' . db_param() . ' AND s.bug_id = b.id ' .
 	( $t_show_all ? '' : 'AND ( b.status < ' . db_param() . ' OR s.paid < ' . SPONSORSHIP_PAID . ')' ) . '
 	AND ' . $t_project_clause . '

adm_config_report.php
@@ -211,10 +211,9 @@ $t_edit_type            = gpc_get_string( 'type', CONFIG_TYPE_DEFAULT );
 $t_edit_value           = gpc_get_string( 'value', '' );
 
 # Apply filters
-$t_config_table  = db_get_table( 'config' );
 
 # Get users in db having specific configs
-$t_query = 'SELECT DISTINCT user_id FROM ' . $t_config_table . ' WHERE user_id <> ' . db_param() ;
+$t_query = 'SELECT DISTINCT user_id FROM {config} WHERE user_id <> ' . db_param() ;
 $t_result = db_query_bound( $t_query, array( ALL_USERS ) );
 if( $t_filter_user_value != META_FILTER_NONE && $t_filter_user_value != ALL_USERS ) {
 	# Make sure the filter value exists in the list
@@ -236,7 +235,7 @@ $t_users_list = array(
 
 # Get projects in db with specific configs
 $t_query = 'SELECT DISTINCT project_id, pt.name as project_name
-	FROM ' . $t_config_table . ' ct
+	FROM {config} ct
 	JOIN {project} pt ON pt.id = ct.project_id
 	WHERE project_id!=0
 	ORDER BY project_name';
@@ -249,7 +248,7 @@ while( $t_row = db_fetch_array( $t_result ) ) {
 }
 
 # Get config list used in db
-$t_query = 'SELECT DISTINCT config_id FROM ' . $t_config_table . ' ORDER BY config_id';
+$t_query = 'SELECT DISTINCT config_id FROM {config} ORDER BY config_id';
 $t_result = db_query_bound( $t_query );
 $t_configs_list[META_FILTER_NONE] = '[' . lang_get( 'any' ) . ']';
 if( $t_filter_config_value != META_FILTER_NONE ) {
@@ -281,7 +280,7 @@ if( $t_where != '' ) {
 }
 
 $t_query = 'SELECT config_id, user_id, project_id, type, value, access_reqd
-	FROM ' . $t_config_table . $t_where . ' ORDER BY user_id, project_id, config_id ';
+	FROM {config} ' . $t_where . ' ORDER BY user_id, project_id, config_id ';
 $t_result = db_query_bound( $t_query, $t_param );
 ?>
 

admin/install.php
@@ -1191,8 +1191,7 @@ if( 6 == $t_install_state ) {
 		checking ability to SELECT records
 	</td>
 	<?php
-		$t_mantis_config_table = db_get_table( 'config' );
-	$t_query = 'SELECT COUNT(*) FROM ' . $t_mantis_config_table;
+	$t_query = 'SELECT COUNT(*) FROM {config}';
 	$t_result = @$g_db->Execute( $t_query );
 
 	if( $t_result != false ) {

admin/move_attachments.php
@@ -43,7 +43,7 @@ function move_attachments_to_db( $p_type, $p_projects ) {
 	}
 
 	# Build the SQL query based on attachment type
-	$t_file_table = db_get_table( "${p_type}_file" );
+	$t_file_table = '{' . $p_type . '_file}';
 	switch( $p_type ) {
 		case 'project':
 			$t_query = "SELECT f.*
@@ -53,11 +53,9 @@ function move_attachments_to_db( $p_type, $p_projects ) {
 				ORDER BY f.filename";
 			break;
 		case 'bug':
-			$t_bug_table = db_get_table( 'bug' );
-
 			$t_query = "SELECT f.*
 				FROM $t_file_table f
-				JOIN $t_bug_table b ON b.id = f.bug_id
+				JOIN {bug} b ON b.id = f.bug_id
 				WHERE content = ''
 				  AND b.project_id = " . db_param() . "
 				ORDER BY f.bug_id, f.filename";
@@ -167,11 +165,9 @@ function move_attachments_to_disk( $p_type, array $p_projects ) {
 				ORDER BY f.filename';
 			break;
 		case 'bug':
-			$t_bug_table = db_get_table( 'bug' );
-
 			$t_query = 'SELECT f.*
 				FROM ' . $t_file_table . ' f
-				JOIN ' . $t_bug_table . ' b ON b.id = f.bug_id
+				JOIN {bug} b ON b.id = f.bug_id
 				WHERE content <> \'\'
 				  AND b.project_id = ' . db_param() . '
 				ORDER BY f.bug_id, f.filename';

admin/move_attachments_page.php
@@ -38,8 +38,6 @@ echo '</p></div>';
 $f_file_type = gpc_get( 'type', 'bug' );
 
 function get_attachment_stats( $p_file_type, $p_in_db ) {
-	$t_bug_table = db_get_table( 'bug' );
-
 	if( $p_in_db ) {
 		$t_compare = "<> ''";
 	} else {
@@ -58,7 +56,7 @@ function get_attachment_stats( $p_file_type, $p_in_db ) {
 		default:
 			$t_query = "SELECT p.id, p.name, COUNT(f.id) stats
 				FROM {bug_file} f
-				JOIN $t_bug_table b ON b.id = f.bug_id
+				JOIN {bug} b ON b.id = f.bug_id
 				JOIN {project} p ON p.id = b.project_id
 				WHERE content $t_compare
 				GROUP BY p.id, p.name

api/soap/mc_issue_api.php
@@ -563,9 +563,7 @@ function mc_issue_get_id_from_summary( $p_username, $p_password, $p_summary ) {
 		return mci_soap_fault_login_failed();
 	}
 
-	$t_bug_table = db_get_table( 'bug' );
-
-	$t_query = 'SELECT id FROM ' . $t_bug_table . ' WHERE summary = ' . db_param();
+	$t_query = 'SELECT id FROM {bug} WHERE summary = ' . db_param();
 
 	$t_result = db_query_bound( $t_query, array( $p_summary ), 1 );
 

api/soap/mc_project_api.php
@@ -695,7 +695,6 @@ function mc_project_get_attachments( $p_username, $p_password, $p_project_id ) {
 		return mci_soap_fault_access_denied( $t_user_id );
 	}
 
-	$t_user_table = db_get_table( 'user' );
 	$t_pub = VS_PUBLIC;
 	$t_priv = VS_PRIVATE;
 	$t_admin = config_get_global( 'admin_site_threshold' );
@@ -728,7 +727,7 @@ function mc_project_get_attachments( $p_username, $p_password, $p_project_id ) {
 		LEFT JOIN {project} pt ON pft.project_id = pt.id
 		LEFT JOIN {project_user_list} pult
 		ON pft.project_id = pult.project_id AND pult.user_id = ' . db_param() . '
-		LEFT JOIN ' . $t_user_table . ' ut ON ut.id = ' . db_param() . '
+		LEFT JOIN {user} ut ON ut.id = ' . db_param() . '
 		WHERE pft.project_id in (' . implode( ',', $t_projects ) . ') AND
 		( ( ( pt.view_state = ' . db_param() . ' OR pt.view_state is null ) AND pult.user_id is null AND ut.access_level ' . $t_access_clause . ' ) OR
 		( ( pult.user_id = ' . db_param() . ' ) AND ( pult.access_level ' . $t_access_clause . ' ) ) OR

commit 838836ae6dba0f2f315caf8dd0801ea0b3f101a7

Fix syntax error in bug_graph_bystatus.php
Regression introduced by commit ed84127

plugins/MantisGraph/pages/bug_graph_bystatus.php
@@ -95,7 +95,7 @@ foreach ( $t_rows as $t_row ) {
 # type = 0 and field=status are status changes
 # type = 1 are new bugs
 $t_select = 'SELECT bug_id, type, old_value, new_value, date_modified FROM {bug_history}
-	' WHERE bug_id in ('. implode( ',', $t_bug ) .
+	WHERE bug_id in ('. implode( ',', $t_bug ) .
 	') and ( (type=' . NORMAL_TYPE . ' and field_name=\'status\')
 		or type=' . NEW_BUG . ' ) and date_modified >= ' . db_param() .
 	' order by date_modified DESC';