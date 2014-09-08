3d65e1a8201d1893cca5a976392aabe4ebf8ffc3
Paul Richards <paul@mantisforge.org>
2014-09-08 23:09:17 +0100

    remove non-required trim() statement

--- core/summary_api.php
+++ core/summary_api.php
@@ -623,7 +623,7 @@ function summary_print_by_category() {
 	$t_project_id = helper_get_current_project();
 	$t_user_id = auth_get_current_user_id();
 
-	$t_specific_where = trim( helper_project_specific_where( $t_project_id ) );
+	$t_specific_where = helper_project_specific_where( $t_project_id );
 	if( '1<>1' == $t_specific_where ) {
 		return;
 	}
