6f003878df5a94a96590d0443b830141a84d0017
Paul Richards <paul@mantisforge.org>
2014-09-08 23:12:24 +0100

    Simplify call to access_cache_matrix_project
    helper_get_current_project handles the check for
    project_override internally so we don't need to do it here.

--- bugnote_view_inc.php
+++ bugnote_view_inc.php
@@ -68,11 +68,7 @@ require_api( 'user_api.php' );
 $t_user_id = auth_get_current_user_id();
 
 #precache access levels
-if( isset( $g_project_override ) ) {
-	access_cache_matrix_project( $g_project_override );
-} else {
-	access_cache_matrix_project( helper_get_current_project() );
-}
+access_cache_matrix_project( helper_get_current_project() );
 
 # get the bugnote data
 $t_bugnote_order = current_user_get_pref( 'bugnote_order' );
