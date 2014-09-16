mantisbt/mantisbt

commit a38858a3accc5aea905cd8c913a0690f4fce7210

Fix #14631: Consistent email validation
PHPMailer uses filter_var() to check for an e-mail validity; this
function treats single-domain e-mail addresses (e.g. user@localhost) as
invalid. However, Mantis API function email_is_valid() relies on a
custom regex to validate e-mails, which does accept such addresses. As a
consequence, we accept addresses to which we are unable to send
messages.

To avoid this problem, we now rely on PHPMailer::ValidateAddress()
method to ensure that any email we store can be sent PHPMailer.

core/email_api.php
@@ -46,6 +46,8 @@
  * @uses user_api.php
  * @uses user_pref_api.php
  * @uses utility_api.php
+ *
+ * @uses class.phpmailer.php PHPMailer library
  */
 
 require_api( 'access_api.php' );
@@ -71,6 +73,8 @@ require_api( 'string_api.php' );
 require_api( 'user_api.php' );
 require_api( 'user_pref_api.php' );
 require_api( 'utility_api.php' );
+
+require_lib( 'phpmailer' . DIRECTORY_SEPARATOR . 'class.phpmailer.php' );
 
 /**
  * reusable object of class SMTP
@@ -117,11 +121,9 @@ function email_is_valid( $p_email ) {
 		return true;
 	}
 
-	# Use a regular expression to check to see if the email is in valid format
-	#  x-xx.xxx@yyy.zzz.abc etc.
-	if( preg_match( email_regex_simple(), $p_email, $t_check ) ) {
-		$t_local = $t_check[1];
-		$t_domain = $t_check[2];
+	# Delegate email validation to PHPMailer
+	if( PHPMailer::ValidateAddress( $p_email ) ) {
+		$t_domain = end( explode( '@', $p_email ) );
 
 		# see if we're limited to one domain
 		$t_limit_email_domain = config_get( 'limit_email_domain' );
@@ -912,10 +914,8 @@ function email_send( $p_email_data ) {
 	$t_mailer_method = config_get( 'phpMailer_method' );
 
 	if( is_null( $g_phpMailer ) ) {
-		if ( $t_mailer_method == PHPMAILER_METHOD_SMTP )
+		if ( $t_mailer_method == PHPMAILER_METHOD_SMTP ) {
 			register_shutdown_function( 'email_smtp_close' );
-		if( !class_exists( 'PHPMailer' ) ) {
-			require_lib( 'phpmailer' . DIRECTORY_SEPARATOR . 'class.phpmailer.php' );
 		}
 		$mail = new PHPMailer(true);
 	} else {

commit 92354171c3c27231626743a41d10a807f1c0767d

Fix E_STRICT notice in email_is_valid()
Regression from a38858a3accc5aea905cd8c913a0690f4fce7210

core/email_api.php
@@ -121,7 +121,7 @@ function email_is_valid( $p_email ) {
 	# check email address is a valid format
 	$t_email = filter_var( $p_email, FILTER_SANITIZE_EMAIL );
 	if( PHPMailer::ValidateAddress( $t_email ) ) {
-		$t_domain = end( explode( '@', $t_email ) );
+		$t_domain = substr( $t_email, strpos( $t_email, '@' ) + 1 );
 
 		# see if we're limited to a set of known domains
 		$t_limit_email_domains = config_get( 'limit_email_domains' );