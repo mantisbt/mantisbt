9a29f27e15032e8ad8c2adb70e068c0ae01b68d5
Damien Regad <dregad@mantisbt.org>
2014-10-14 17:20:12 +0200

    Revised admin check for $g_default_timezone
    - Clarification of messages, added links to PHP documentation
    - When default timezone is null, issue warning instead of failure and
      indicate defaulting to UTC

--- admin/check/check_i18n_inc.php
+++ admin/check/check_i18n_inc.php
@@ -42,18 +42,33 @@ if( $t_config_default_timezone ) {
 		'Default timezone has been specified in config_inc.php (default_timezone option)',
 		in_array( $t_config_default_timezone, timezone_identifiers_list() ),
 		array(
-			true => 'Default timezone is: ' . htmlentities( $t_config_default_timezone ),
-			false => 'Invalid timezone \'' . htmlentities( $t_config_default_timezone ) . '\' specified for the default_timezone configuration option.'
+			true => "Default timezone is '" . htmlentities( $t_config_default_timezone ) . "'",
+			false => "Invalid timezone '" . htmlentities( $t_config_default_timezone ) . "' specified. "
+				. 'Refer to the <a href="http://php.net/timezones">List of Supported Timezones</a>.'
 		)
 	);
 } else {
 	$t_php_default_timezone = ini_get( 'date.timezone' );
-	check_print_test_row(
-		'Default timezone has been specified in config_inc.php (default_timezone option) or php.ini (date.timezone directive)',
-		in_array( $t_php_default_timezone, timezone_identifiers_list() ),
-		array(
-			true => 'Default timezone (specified by the date.timezone directive in php.ini) is: ' . htmlentities( $t_php_default_timezone ),
-			false => 'Invalid timezone \'' . htmlentities( $t_php_default_timezone ) . '\' specified for the date.timezone php.ini directive.'
-		)
-	);
+	$t_msg = 'No timezone has been specified in config_inc.php (default_timezone option)';
+	$t_tz_link = '<a href="http://ch1.php.net/datetime.configuration#ini.date.timezone">date.timezone</a>';
+
+	if( $t_php_default_timezone ) {
+		check_print_test_row(
+			$t_msg,
+			in_array( $t_php_default_timezone, timezone_identifiers_list() ),
+			array(
+				true => "Default timezone (specified by the $t_tz_link directive in php.ini) "
+					. "is '" . htmlentities( $t_php_default_timezone ) . "'",
+				false => "Invalid timezone '" . htmlentities( $t_config_default_timezone ) . "' specified. "
+			)
+		);
+	} else {
+		check_print_test_warn_row(
+			$t_msg,
+			!empty( $t_php_default_timezone ),
+			array(
+				false => "Timezone has been defaulted to 'UTC'."
+			)
+		);
+	}
 }
