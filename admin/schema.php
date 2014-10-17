a146606fc2b2dbde36cc3a4dccf4f83ae6c80dea
Paul Richards <paul@mantisforge.org>
2014-10-18 01:22:00 +0200

    Additional timezone init fixes
    Implements additional fixes submitted by grangeway in PR #387
    
    - Set installer_db_now() timezone to UTC
    - Remove timezone_identifiers_list() existence checks
      These checks are obsolete, there were implemented to cover RHEL/CentOS
      under PHP 5.1 and we now require 5.3.
    - Travis: set timezone to UTC
    
    Signed-off-by: Damien Regad <dregad@mantisbt.org>

--- admin/schema.php
+++ admin/schema.php
@@ -50,7 +50,11 @@ if( !function_exists( 'db_null_date' ) ) {
 function installer_db_now() {
 	global $g_db;
 
-	return $g_db->BindTimeStamp( time() );
+	$t_timezone = @date_default_timezone_get();
+	date_default_timezone_set( 'UTC' );
+	$t_time = $g_db->BindTimeStamp( time() );
+	@date_default_timezone_set( $t_timezone );
+	return $t_time;
 }
 
 # Special handling for Oracle (oci8):
