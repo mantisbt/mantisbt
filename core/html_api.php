mantisbt/mantisbt

commit 0552366c6e2aff04efd4d3ada7c794b6300bb8f5

XHTML/CSS validation and rendering fixes

core/html_api.php
@@ -1008,14 +1008,16 @@ function print_summary_submenu() {
 		}
 	}
 
-	echo '<div id="summary-submenu">';
-	echo '<ul class="menu">';
-	# Plugins menu items - these are cooked links
-	foreach ( $t_menu_options as $t_menu_item ) {
-		echo '<li>', $t_menu_item, '</li>';
+	if( sizeof( $t_menu_options ) > 0 ) {
+		echo "\t" . '<div id="summary-submenu">' . "\n";
+		echo "\t\t" . '<ul class="menu">' . "\n";
+		# Plugins menu items - these are cooked links
+		foreach ( $t_menu_options as $t_menu_item ) {
+			echo "\t\t\t" . '<li>', $t_menu_item, '</li> . "\n"';
+		}
+		echo "\t\t" . '</ul>' . "\n";
+		echo "\t" . '</div>' . "\n";
 	}
-	echo '</ul>';
-	echo '</div>';
 }
 
 /**

account_sponsor_page.php
@@ -139,6 +139,17 @@ if( $t_sponsor_count === 0 ) {
 <br />
 <div>
 <table class="width100" cellspacing="1">
+	<colgroup>
+		<col style="width:10%" />
+		<col style="width:8%" />
+		<col style="width:7%" />
+		<col style="width:10%" />
+		<col style="width:10%" />
+		<col style="width:30%" />
+		<col style="width:8%" />
+		<col style="width:7%" />
+		<col style="width:10%" />
+	</colgroup>
 	<!-- Headings -->
 	<tr>
 		<td class="form-title" colspan="9">
@@ -146,15 +157,15 @@ if( $t_sponsor_count === 0 ) {
 		</td>
 	</tr>
 	<tr>
-		<td class="form-title" width="10%"><?php echo lang_get( 'email_bug' ) ?></td>
-		<td class="form-title" width="8%"><?php echo lang_get( 'email_project' ) ?></td>
-		<td class="form-title" width="7%"><?php echo lang_get( 'fixed_in_version' ) ?></td>
-		<td class="form-title" width="10%"><?php echo lang_get( 'email_status' ) ?></td>
-		<td class="form-title" width="10%"><?php echo lang_get( 'email_handler' ) ?></td>
-		<td class="form-title" width="30%"><?php echo lang_get( 'email_summary' ) ?></td>
-		<td class="form-title" width="8%"><?php echo lang_get( 'amount' ) ?></td>
-		<td class="form-title" width="7%"><?php echo lang_get( 'status' ) ?></td>
-		<td class="form-title" width="10%">&#160;</td>
+		<td class="form-title"><?php echo lang_get( 'email_bug' ) ?></td>
+		<td class="form-title"><?php echo lang_get( 'email_project' ) ?></td>
+		<td class="form-title"><?php echo lang_get( 'fixed_in_version' ) ?></td>
+		<td class="form-title"><?php echo lang_get( 'email_status' ) ?></td>
+		<td class="form-title"><?php echo lang_get( 'email_handler' ) ?></td>
+		<td class="form-title"><?php echo lang_get( 'email_summary' ) ?></td>
+		<td class="form-title"><?php echo lang_get( 'amount' ) ?></td>
+		<td class="form-title"><?php echo lang_get( 'status' ) ?></td>
+		<td class="form-title">&#160;</td>
 	</tr>
 <?php
 	$t_total_owing = 0;
@@ -256,7 +267,16 @@ if( $t_sponsor_count === 0 ) {
 <form method="post" action="account_sponsor_update.php">
 <?php echo form_security_field( 'account_sponsor_update' ) ?>
 <table class="width100" cellspacing="1">
-
+	<colgroup>
+		<col style="width:10%" />
+		<col style="width:8%" />
+		<col style="width:7%" />
+		<col style="width:10%" />
+		<col style="width:35%" />
+		<col style="width:10%" />
+		<col style="width:10%" />
+		<col style="width:10%" />
+	</colgroup>
 	<!-- Headings -->
 	<tr>
 		<td class="form-title" colspan="8">
@@ -264,14 +284,14 @@ if( $t_sponsor_count === 0 ) {
 		</td>
 	</tr>
 	<tr>
-		<td class="form-title" width="10%"><?php echo lang_get( 'email_bug' ) ?></td>
-		<td class="form-title" width="8%"><?php echo lang_get( 'email_project' ) ?></td>
-		<td class="form-title" width="7%"><?php echo lang_get( 'fixed_in_version' ) ?></td>
-		<td class="form-title" width="10%"><?php echo lang_get( 'email_status' ) ?></td>
-		<td class="form-title" width="35%"><?php echo lang_get( 'email_summary' ) ?></td>
-		<td class="form-title" width="10%"><?php echo lang_get( 'sponsor' ) ?></td>
-		<td class="form-title" width="10%"><?php echo lang_get( 'amount' ) ?></td>
-		<td class="form-title" width="10%"><?php echo lang_get( 'status' ) ?></td>
+		<td class="form-title"><?php echo lang_get( 'email_bug' ) ?></td>
+		<td class="form-title"><?php echo lang_get( 'email_project' ) ?></td>
+		<td class="form-title"><?php echo lang_get( 'fixed_in_version' ) ?></td>
+		<td class="form-title"><?php echo lang_get( 'email_status' ) ?></td>
+		<td class="form-title"><?php echo lang_get( 'email_summary' ) ?></td>
+		<td class="form-title"><?php echo lang_get( 'sponsor' ) ?></td>
+		<td class="form-title"><?php echo lang_get( 'amount' ) ?></td>
+		<td class="form-title"><?php echo lang_get( 'status' ) ?></td>
 	</tr>
 <?php
 	$t_bug_list = array();
@@ -341,12 +361,12 @@ if( $t_sponsor_count === 0 ) {
 	<td class="right"><?php echo sponsorship_format_amount( $t_total_paid ) ?></td>
 	<td></td>
 </tr>
-	<input type="hidden" name="buglist" value="<?php echo $t_hidden_bug_list ?>" />
 	<!-- BUTTONS -->
 	<tr>
 		<td colspan="5">&#160;</td>
 		<!-- Update Button -->
-		<td colspan="2">
+		<td colspan="3">
+			<input type="hidden" name="buglist" value="<?php echo $t_hidden_bug_list ?>" />
 			<input type="submit" class="button" value="<?php echo lang_get( 'update_sponsorship_button' ) ?>" />
 		</td>
 	</tr>

bug_sponsorship_list_view_inc.php
@@ -95,7 +95,7 @@ if( $t_show_sponsorships ) {
 
 			$t_details_url = lang_get( 'sponsorship_process_url' );
 			if( !is_blank( $t_details_url ) ) {
-				echo '&#160;[<a href="' . $t_details_url . '" target="_blank">'
+				echo '&#160;[<a href="' . $t_details_url . '">'
 					. lang_get( 'sponsorship_more_info' ) . '</a>]';
 			}
 		?>
@@ -169,7 +169,7 @@ if( $t_show_sponsorships ) {
 
 			$t_details_url = lang_get( 'sponsorship_process_url' );
 			if( !is_blank( $t_details_url ) ) {
-				echo '&#160;[<a href="' . $t_details_url . '" target="_blank">'
+				echo '&#160;[<a href="' . $t_details_url . '">'
 					. lang_get( 'sponsorship_more_info' ) . '</a>]';
 			}
 

core/summary_api.php
@@ -63,12 +63,12 @@ require_api( 'utility_api.php' );
  * @return void
  */
 function summary_helper_print_row( $p_label, $p_open, $p_resolved, $p_closed, $p_total ) {
-	printf( '<tr>' );
-	printf( '<td width="50%%">%s</td>', $p_label );
-	printf( '<td width="12%%" class="right">%s</td>', $p_open );
-	printf( '<td width="12%%" class="right">%s</td>', $p_resolved );
-	printf( '<td width="12%%" class="right">%s</td>', $p_closed );
-	printf( '<td width="12%%" class="right">%s</td>', $p_total );
+	echo '<tr>';
+	printf( '<td class="width50">%s</td>', $p_label );
+	printf( '<td class="width12 right">%s</td>', $p_open );
+	printf( '<td class="width12 right">%s</td>', $p_resolved );
+	printf( '<td class="width12 right">%s</td>', $p_closed );
+	printf( '<td class="width12 right">%s</td>', $p_total );
 	echo '</tr>';
 }
 
@@ -313,7 +313,7 @@ function summary_print_by_date( array $p_date_array ) {
 		$t_new_bugs_link = '<a class="subtle" href="' . config_get( 'bug_count_hyperlink_prefix' ) . '&amp;' . FILTER_PROPERTY_FILTER_BY_DATE . '=on&amp;' . FILTER_PROPERTY_START_YEAR . '=' . date( 'Y', $t_start_date ) . '&amp;' . FILTER_PROPERTY_START_MONTH . '=' . date( 'm', $t_start_date ) . '&amp;' . FILTER_PROPERTY_START_DAY . '=' . date( 'd', $t_start_date ) . '&amp;' . FILTER_PROPERTY_HIDE_STATUS . '=">';
 
 		echo '<tr>' . "\n";
-		echo '    <td width="50%">' . $t_days . '</td>' . "\n";
+		echo '    <td class="width50">' . $t_days . '</td>' . "\n";
 
 		if( $t_new_count > 0 ) {
 			echo '    <td class="right">' . $t_new_bugs_link . $t_new_count . '</a></td>' . "\n";

css/default.css
@@ -50,6 +50,7 @@ td.right			{ text-align: right; }
 td.category, th.category		{ background-color: #c8c8e8; color: #000000; font-weight: bold; vertical-align : top; }
 td.category2, th.category2		{ background-color: #c8c8e8; color: #000000; vertical-align : top; }
 td.overdue			{ background-color: #ff0000; color: #000000; font-weight: bold; }
+td.sticky-header 	{ background-color: #999999; }
 td.width30			{ width: 30%; }
 td.col-1			{ background-color: #d8d8d8; color: #000000; }
 td.col-2			{ background-color: #e8e8e8; color: #000000; }

manage_config_workflow_page.php
@@ -423,8 +423,10 @@ foreach ( $t_status_arr as $t_status => $t_status_label ) {
 	}
 }
 
-echo "<form name=\"workflow_config_action\" method=\"post\" action=\"manage_config_workflow_set.php\">\n";
+echo '<form name="workflow_config_action" method="post" action="manage_config_workflow_set.php">' . "\n";
+echo '<fieldset>';
 echo form_security_field( 'manage_config_workflow_set' );
+echo '</fieldset>';
 
 if( ALL_PROJECTS == $t_project ) {
 	$t_project_title = lang_get( 'config_all_projects' );
@@ -495,10 +497,11 @@ if( $g_can_change_flags ) {
 
 	if( 0 < count( $g_overrides ) ) {
 		echo '<div class="right"><form name="mail_config_action" method="post" action="manage_config_revert.php">' ."\n";
+		echo '<fieldset>' . "\n";
 		echo form_security_field( 'manage_config_revert' );
-		echo '<input name="revert" type="hidden" value="' . implode( ',', $g_overrides ) . '"></input>';
-		echo '<input name="project" type="hidden" value="' . $t_project . '"></input>';
-		echo '<input name="return" type="hidden" value="' . string_attribute( form_action_self() ) .'"></input>';
+		echo '<input name="revert" type="hidden" value="' . implode( ',', $g_overrides ) . '" />';
+		echo '<input name="project" type="hidden" value="' . $t_project . '" />';
+		echo '<input name="return" type="hidden" value="' . string_attribute( form_action_self() ) .'" />';
 		echo '<input type="submit" class="button" value=';
 		if( ALL_PROJECTS == $t_project ) {
 			echo lang_get( 'revert_to_system' );
@@ -506,6 +509,7 @@ if( $g_can_change_flags ) {
 			echo lang_get( 'revert_to_all_project' );
 		}
 		echo '" />' . "\n";
+		echo '</fieldset>' . "\n";
 		echo '</form></div>' . "\n";
 	}
 

manage_plugin_page.php
@@ -79,10 +79,18 @@ if( 0 < count( $t_plugins_installed ) ) {
 <div class="form-container">
 
 	<form action="manage_plugin_update.php" method="post">
-
+		<fieldset>
 		<?php echo form_security_field( 'manage_plugin_update' ) ?>
+		</fieldset>
 		<table>
-
+			<colgroup>
+				<col style="width:20%" />
+				<col style="width:35%" />
+				<col style="width:20%" />
+				<col style="width:7%" />
+				<col style="width:8%" />
+				<col style="width:10%" />
+			</colgroup>
 			<thead>
 				<!-- Title -->
 				<tr>
@@ -93,12 +101,12 @@ if( 0 < count( $t_plugins_installed ) ) {
 
 				<!-- Info -->
 				<tr class="row-category">
-					<th width="20%"><?php echo lang_get( 'plugin' ) ?></th>
-					<th width="35%"><?php echo lang_get( 'plugin_description' ) ?></th>
-					<th width="20%"><?php echo lang_get( 'plugin_depends' ) ?></th>
-					<th width="7%"><?php echo lang_get( 'plugin_priority' ) ?></th>
-					<th width="8%"><?php echo lang_get( 'plugin_protected' ) ?></th>
-					<th width="10%"><?php echo lang_get( 'plugin_actions' ) ?></th>
+					<th><?php echo lang_get( 'plugin' ) ?></th>
+					<th><?php echo lang_get( 'plugin_description' ) ?></th>
+					<th><?php echo lang_get( 'plugin_depends' ) ?></th>
+					<th><?php echo lang_get( 'plugin_priority' ) ?></th>
+					<th><?php echo lang_get( 'plugin_protected' ) ?></th>
+					<th><?php echo lang_get( 'plugin_actions' ) ?></th>
 				</tr>
 			</thead>
 
@@ -214,7 +222,12 @@ if( 0 < count( $t_plugins_available ) ) {
 <br/>
 <div class="table-container">
 	<table>
-
+		<colgroup>
+			<col style="width:25%" />
+			<col style="width:45%" />
+			<col style="width:20%" />
+			<col style="width:10%" />
+		</colgroup>
 		<thead>
 			<!-- Title -->
 			<tr>
@@ -225,10 +238,10 @@ if( 0 < count( $t_plugins_available ) ) {
 
 			<!-- Info -->
 			<tr class="row-category">
-				<td width="25%"><?php echo lang_get( 'plugin' ) ?></td>
-				<td width="45%"><?php echo lang_get( 'plugin_description' ) ?></td>
-				<td width="20%"><?php echo lang_get( 'plugin_depends' ) ?></td>
-				<td width="10%"><?php echo lang_get( 'plugin_actions' ) ?></td>
+				<td><?php echo lang_get( 'plugin' ) ?></td>
+				<td><?php echo lang_get( 'plugin_description' ) ?></td>
+				<td><?php echo lang_get( 'plugin_depends' ) ?></td>
+				<td><?php echo lang_get( 'plugin_actions' ) ?></td>
 			</tr>
 		</thead>
 

plugins/MantisCoreFormatting/pages/config.php
@@ -35,10 +35,16 @@ print_manage_menu( );
 <div class="form-container" style="width: 60%">
 
 <form action="<?php echo plugin_page( 'config_edit' )?>" method="post">
+<fieldset>
 <?php echo form_security_field( 'plugin_format_config_edit' ) ?>
+</fieldset>
 
 <table>
-
++<colgroup>
+	<col style="width:60%;" />
+	<col style="width:20%;" />
+	<col style="width:20%;" />
+</colgroup>
 <tr>
 	<td class="form-title" colspan="3">
 		<?php echo lang_get( 'plugin_format_title' ) . ': ' . lang_get( 'plugin_format_config' )?>
@@ -46,15 +52,15 @@ print_manage_menu( );
 </tr>
 
 <tr>
-	<th class="category" width="60%">
+	<th class="category">
 		<?php echo lang_get( 'plugin_format_process_text' )?>
 		<br /><span class="small"><?php echo lang_get( 'plugin_format_process_text_warning_notice' )?></span>
 	</th>
-	<td class="center" width="20%">
+	<td class="center">
 		<label><input type="radio" name="process_text" value="1" <?php echo( ON == plugin_config_get( 'process_text' ) ) ? 'checked="checked" ' : ''?>/>
 			<?php echo lang_get( 'plugin_format_enabled' )?></label>
 	</td>
-	<td class="center" width="20%">
+	<td class="center">
 		<label><input type="radio" name="process_text" value="0" <?php echo( OFF == plugin_config_get( 'process_text' ) ) ? 'checked="checked" ' : ''?>/>
 			<?php echo lang_get( 'plugin_format_disabled' )?></label>
 	</td>

commit d5b3f7cd909f5a7695e0ffd6117bac793666165d

Fix #17912: display of "\n" in summary submenu
Regression introduced by 0552366c6e2aff04efd4d3ada7c794b6300bb8f5.

core/html_api.php
@@ -1032,7 +1032,7 @@ function print_summary_submenu() {
 		echo "\t\t" . '<ul class="menu">' . "\n";
 		# Plugins menu items - these are cooked links
 		foreach ( $t_menu_options as $t_menu_item ) {
-			echo "\t\t\t" . '<li>', $t_menu_item, '</li> . "\n"';
+			echo "\t\t\t" . '<li>', $t_menu_item, '</li>' . "\n";
 		}
 		echo "\t\t" . '</ul>' . "\n";
 		echo "\t" . '</div>' . "\n";