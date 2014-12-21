mantisbt/mantisbt

commit 215968fa8ff33e327f0600765a5caa24de392cbc

Strip null bytes out of GPC input strings
Backporting commit fc02c46eea9d9e7cc472a7fc1801ea65d467db76 from master
branch to fix issue #17640

Signed-off-by: Damien Regad <dregad@mantisbt.org>

core/gpc_api.php
@@ -110,7 +110,7 @@ function gpc_get_string( $p_var_name, $p_default = null ) {
 		trigger_error( ERROR_GPC_ARRAY_UNEXPECTED, ERROR );
 	}
 
-	return $t_result;
+	return str_replace( "\0","",$t_result );
 }
 
 /**
@@ -255,7 +255,11 @@ function gpc_get_string_array( $p_var_name, $p_default = null ) {
 		trigger_error( ERROR_GPC_ARRAY_EXPECTED, ERROR );
 	}
 
-	return $t_result;
+	$t_array = array();
+	foreach( $t_result as $key => $val ) {
+		$t_array[$key] = str_replace( "\0", "", $val );
+	}
+	return $t_array;
 }
 
 /**

commit 99ada4de031b7cd308438144d3d68178055aef07

Fix system warning in gpc_get_string_array()
The fix for issue #17640 did not consider that the value returned by
gpc_get() is not necessarily an array - it can be the default value
(e.g. null) causing PHP to throw an 'Invalid argument supplied for
foreach()' warning.

Fixes #17967, regression from 215968fa8ff33e327f0600765a5caa24de392cbc

core/gpc_api.php
@@ -255,6 +255,9 @@ function gpc_get_string_array( $p_var_name, $p_default = null ) {
 		trigger_error( ERROR_GPC_ARRAY_EXPECTED, ERROR );
 	}
 
+	if( !is_array( $t_result ) ) {
+		return $t_result;
+	}
 	$t_array = array();
 	foreach( $t_result as $key => $val ) {
 		$t_array[$key] = str_replace( "\0", "", $val );