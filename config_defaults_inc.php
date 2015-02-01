9e5f3fec6924a6fcd8ff2a7fff08a3267e1fbc29
Victor Boctor <victor@mantishub.net>
2015-01-31 23:56:21 -0800

    Basic markdown support

--- config_defaults_inc.php
+++ config_defaults_inc.php
@@ -1214,6 +1214,16 @@ $g_news_view_limit_days = 30;
  */
 $g_private_news_threshold = DEVELOPER;
 
+############
+# Markdown #
+############
+
+/**
+ * Enables markdown support - once enabled all issue titles, text, and notes are formatted based on
+ * markdown syntax.
+ */
+$g_markdown_enabled = OFF;
+
 ################################
 # MantisBT Default Preferences #
 ################################
