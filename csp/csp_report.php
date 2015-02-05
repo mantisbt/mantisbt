7b5fa72a1dd47661f55e64a833749e2c287a06a2
mfoXen <mfoXen@users.noreply.github.com>
2015-02-05 14:30:27 +0100

    code-formating
    added spaces after opening braces and before closing braces

--- csp/csp_report.php
+++ csp/csp_report.php
@@ -1,16 +1,16 @@
 <?php
 
 $filename = './logs/csp_report.log';
-$logprefix = date('y-m-d') . ' - CSP Violation:' . PHP_EOL;
+$logprefix = date( 'y-m-d' ) . ' - CSP Violation:' . PHP_EOL;
 
 // Send `204 No Content` status code
-http_response_code(204);
+http_response_code( 204 );
 
 // Get the raw POST data
-$data = file_get_contents('php://input');
+$data = file_get_contents( 'php://input' );
 // Only continue if it’s valid JSON that is not just `null`, `0`, `false` or an
 // empty string, i.e. if it could be a CSP violation report.
-if ($data = json_decode($data)) {
+if ( $data = json_decode( $data ) ) {
 	// Prettify the JSON-formatted data
 	$data = json_encode(
 		$data,
