mantisbt/mantisbt

commit b78f966e013d494ea27d92bba39d797a1266b7b7

Code Standards/PHP Doc improvements
Improvements to code base to follow coding standards,
and improve phpdoc validation from PHP Codesniffer rules

set_project.php
@@ -84,7 +84,7 @@ if( !is_blank( $c_ref ) ) {
 
 	# Check that referrer matches our address after squashing case (case insensitive compare)
 	$t_path = rtrim( config_get( 'path' ), '/' );
-	if( preg_match( '@^(' . $t_path . ")/(?:/*([^\?#]*))(.*)?\$@", $_SERVER['HTTP_REFERER'], $t_matches ) ) {
+	if( preg_match( '@^(' . $t_path . ')/(?:/*([^\?#]*))(.*)?\$@', $_SERVER['HTTP_REFERER'], $t_matches ) ) {
 		$t_referrer_page = $t_matches[2];
 		$t_param = $t_matches[3];
 

commit 9ad6013cd225eb690ad943c64048e7337ff59bd6

Switching projects should maintain current page
When possible maintain the current page as user switches projects.
A bug was introduced in b78f966e013d494ea27d92bba39d797a1266b7b7
that made switching projects always redirects to default home page.

Fixes #19351

set_project.php
@@ -84,7 +84,7 @@ if( !is_blank( $c_ref ) ) {
 
 	# Check that referrer matches our address after squashing case (case insensitive compare)
 	$t_path = rtrim( config_get( 'path' ), '/' );
-	if( preg_match( '@^(' . $t_path . ')/(?:/*([^\?#]*))(.*)?\$@', $_SERVER['HTTP_REFERER'], $t_matches ) ) {
+	if( preg_match( '@^(' . $t_path . ')/(?:/*([^\?#]*))(.*)?$@', $_SERVER['HTTP_REFERER'], $t_matches ) ) {
 		$t_referrer_page = $t_matches[2];
 		$t_param = $t_matches[3];
 