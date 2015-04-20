e2391f6fa44349d14fdc0070208b65214851f035
Damien Regad <dregad@mantisbt.org>
2015-04-21 01:10:38 +0200

    Fix regression printing user lists for all projects
    This was introduced by commit c0976d063973a97b1ba4a5c3b017d3b3ddab59bc
    (see issue #19574); the while loop used to build the associative array
    to remove duplicate users from the list failed to take into
    consideration that the array returned by project_get_all_user_rows() is
    no longer 0-based...
    
    Fixes #19638

--- core/print_api.php
+++ core/print_api.php
@@ -283,12 +283,10 @@ function print_user_option_list( $p_user_id, $p_project_id = null, $p_access = A
 			$t_project_users_list = project_get_all_user_rows( $t_project_id, $p_access );
 			# Do a 'smart' merge of the project's user list, into an
 			# associative array (to remove duplicates)
-			# Use a while loop for better performance
-			$i = 0;
-			while( isset( $t_project_users_list[$i] ) ) {
-				$t_users[$t_project_users_list[$i]['id']] = $t_project_users_list[$i];
-				$i++;
+			foreach( $t_project_users_list as $t_id => $t_user ) {
+				$t_users[$t_id] = $t_user;
 			}
+			# Clear the array to release memory
 			unset( $t_project_users_list );
 		}
 		unset( $t_projects );
