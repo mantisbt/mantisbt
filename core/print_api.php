mantisbt/mantisbt

commit 1ddb400815dd847adbb302f1dda9b1fe3fdc5945

Ensure specified user exists in selection list
print_user_option_list() is used to display user selection lists. When
editing a bug having a handler that has been demoted, it is important
that the former developer is included in the list, otherwise the bug's
handler would be set to 0.

Fixes #19574

core/print_api.php
@@ -296,6 +296,11 @@ function print_user_option_list( $p_user_id, $p_project_id = null, $p_access = A
 		$t_users = project_get_all_user_rows( $p_project_id, $p_access );
 	}
 
+	# Ensure the specified user exists in the array
+	if( $p_user_id != NO_USER && !array_key_exists( $p_user_id, $t_users ) ) {
+		$t_users[$p_user_id] = user_get_row( $p_user_id );
+	}
+
 	$t_display = array();
 	$t_sort = array();
 	$t_show_realname = ( ON == config_get( 'show_realname' ) );

commit feb65adcdf7068a1c2cd0603b25d8b09fd593cfa

Fix printing user lists from filters
Commit 1ddb400815dd847adbb302f1dda9b1fe3fdc5945 introduced a regression,
preventing the users list from being printed on view_all_bug_page.php.

This is due to the fact that when called by filters, $p_user_id is
actually an array of id's, a case that was not documented and not
considered when developping the fix for issue #17574.

Fixes #19638

core/print_api.php
@@ -262,9 +262,9 @@ function print_captcha_input( $p_field_name ) {
 /**
  * This populates an option list with the appropriate users by access level
  * @todo from print_reporter_option_list
- * @param integer $p_user_id    A user identifier.
- * @param integer $p_project_id A project identifier.
- * @param integer $p_access     An access level.
+ * @param integer|array $p_user_id    A user identifier or a list of them.
+ * @param integer       $p_project_id A project identifier.
+ * @param integer       $p_access     An access level.
  * @return void
  */
 function print_user_option_list( $p_user_id, $p_project_id = null, $p_access = ANYBODY ) {
@@ -296,8 +296,13 @@ function print_user_option_list( $p_user_id, $p_project_id = null, $p_access = A
 		$t_users = project_get_all_user_rows( $p_project_id, $p_access );
 	}
 
-	# Ensure the specified user exists in the array
-	if( $p_user_id != NO_USER && !array_key_exists( $p_user_id, $t_users ) ) {
+	# Add the specified user ID to the list
+	# If we have an array of user IDs, then we've been called from a filter
+	# so don't add anything
+	if( !is_array( $p_user_id ) &&
+		$p_user_id != NO_USER &&
+		!array_key_exists( $p_user_id, $t_users )
+	) {
 		$t_users[$p_user_id] = user_get_row( $p_user_id );
 	}
 