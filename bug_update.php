mantisbt/mantisbt

commit 03c5a5d18cfef07dec45a1025072981f24a5f07c

Replace email_* calls with raw email_generic calls. This is to try and make a shorter patchset in a bit ;)

bug_update.php
@@ -395,16 +395,16 @@ helper_call_custom_function( 'issue_update_notify', array( $f_bug_id ) );
 
 # Send a notification of changes via email.
 if ( $t_resolve_issue ) {
-	email_resolved( $f_bug_id );
+	email_generic( $f_bug_id, 'resolved', 'The following issue has been RESOLVED.' );
 	email_relationship_child_resolved( $f_bug_id );
 } else if ( $t_close_issue ) {
-	email_close( $f_bug_id );
+	email_generic( $f_bug_id, 'closed', 'The following issue has been CLOSED' );
 	email_relationship_child_closed( $f_bug_id );
 } else if ( $t_reopen_issue ) {
-	email_reopen( $f_bug_id );
+	email_generic( $f_bug_id, 'reopened', 'email_notification_title_for_action_bug_reopened' );
 } else if ( $t_existing_bug->handler_id === NO_USER &&
             $t_updated_bug->handler_id !== NO_USER ) {
-	email_assign( $f_bug_id );
+	email_generic( $f_bug_id, 'owner', 'email_notification_title_for_action_bug_assigned' );
 } else if ( $t_existing_bug->status !== $t_updated_bug->status ) {
 	$t_new_status_label = MantisEnum::getLabel( config_get( 'status_enum_string' ), $t_updated_bug->status );
 	$t_new_status_label = str_replace( ' ', '_', $t_new_status_label );

core/bug_api.php
@@ -1278,7 +1278,7 @@ function bug_delete( $p_bug_id ) {
 	# log deletion of bug
 	history_log_event_special( $p_bug_id, BUG_DELETED, bug_format_id( $p_bug_id ) );
 
-	email_bug_deleted( $p_bug_id );
+	email_generic( $p_bug_id, 'deleted', 'email_notification_title_for_action_bug_deleted' );
 
 	# call post-deletion custom function.  We call this here to allow the custom function to access the details of the bug before
 	# they are deleted from the database given it's id.  The other option would be to move this to the end of the function and
@@ -1719,7 +1719,7 @@ function bug_assign( $p_bug_id, $p_user_id, $p_bugnote_text = '', $p_bugnote_pri
 		bug_clear_cache( $p_bug_id );
 
 		# send assigned to email
-		email_assign( $p_bug_id );
+		email_generic( $p_bug_id, 'owner', 'email_notification_title_for_action_bug_assigned' );
 	}
 
 	return true;
@@ -1744,7 +1744,7 @@ function bug_close( $p_bug_id, $p_bugnote_text = '', $p_bugnote_private = false,
 
 	bug_set_field( $p_bug_id, 'status', config_get( 'bug_closed_status_threshold' ) );
 
-	email_close( $p_bug_id );
+	email_generic( $p_bug_id, 'closed', 'The following issue has been CLOSED' );
 	email_relationship_child_closed( $p_bug_id );
 
 	return true;
@@ -1829,7 +1829,7 @@ function bug_resolve( $p_bug_id, $p_resolution, $p_fixed_in_version = '', $p_bug
 		bug_set_field( $p_bug_id, 'handler_id', $p_handler_id );
 	}
 
-	email_resolved( $p_bug_id );
+	email_generic( $p_bug_id, 'resolved', 'The following issue has been RESOLVED.' );
 	email_relationship_child_resolved( $p_bug_id );
 
 	return true;
@@ -1859,7 +1859,7 @@ function bug_reopen( $p_bug_id, $p_bugnote_text = '', $p_time_tracking = '0:00',
 	bug_set_field( $p_bug_id, 'status', config_get( 'bug_reopen_status' ) );
 	bug_set_field( $p_bug_id, 'resolution', config_get( 'bug_reopen_resolution' ) );
 
-	email_reopen( $p_bug_id );
+	email_generic( $p_bug_id, 'reopened', 'email_notification_title_for_action_bug_reopened' );
 
 	return true;
 }

api/soap/mc_issue_api.php
@@ -775,7 +775,7 @@ function mc_issue_add( $p_username, $p_password, $p_issue ) {
 		mci_tag_set_for_issue( $t_issue_id, $p_issue['tags'], $t_user_id );
 	}
 
-	email_new_bug( $t_issue_id );
+	email_generic( $t_issue_id, 'new', 'email_notification_title_for_action_bug_submitted' );
 
 	if ( $t_bug_data->status != config_get('bug_submit_status') )
 		history_log_event($t_issue_id, 'status', config_get('bug_submit_status') );

bug_report.php
@@ -268,7 +268,7 @@ helper_call_custom_function( 'issue_create_notify', array( $t_bug_id ) );
 # Allow plugins to post-process bug data with the new bug ID
 event_signal( 'EVENT_REPORT_BUG', array( $t_bug_data, $t_bug_id ) );
 
-email_new_bug( $t_bug_id );
+email_generic( $t_bug_id, 'new', 'email_notification_title_for_action_bug_submitted' );
 
 // log status and resolution changes if they differ from the default
 if ( $t_bug_data->status != config_get('bug_submit_status') )

core/bugnote_api.php
@@ -267,7 +267,7 @@ function bugnote_add( $p_bug_id, $p_bugnote_text, $p_time_tracking = '0:00', $p_
 
 	# only send email if the text is not blank, otherwise, it is just recording of time without a comment.
 	if( TRUE == $p_send_email && !is_blank( $t_bugnote_text ) ) {
-		email_bugnote_add( $p_bug_id );
+		email_generic( $p_bug_id, 'bugnote', 'email_notification_title_for_action_bugnote_submitted' );
 	}
 
 	return $t_bugnote_id;

core/email_api.php
@@ -697,96 +697,6 @@ function email_relationship_child_resolved_closed( $p_bug_id, $p_message_id ) {
 }
 
 /**
- * send notices when a bug is sponsored
- * @param int $p_bug_id
- * @return null
- */
-function email_sponsorship_added( $p_bug_id ) {
-	email_generic( $p_bug_id, 'sponsor', 'email_notification_title_for_action_sponsorship_added' );
-}
-
-/**
- * send notices when a sponsorship is modified
- * @param int $p_bug_id
- * @return null
- */
-function email_sponsorship_updated( $p_bug_id ) {
-	email_generic( $p_bug_id, 'sponsor', 'email_notification_title_for_action_sponsorship_updated' );
-}
-
-/**
- * send notices when a sponsorship is deleted
- * @param int $p_bug_id
- * @return null
- */
-function email_sponsorship_deleted( $p_bug_id ) {
-	email_generic( $p_bug_id, 'sponsor', 'email_notification_title_for_action_sponsorship_deleted' );
-}
-
-/**
- * send notices when a new bug is added
- * @param int $p_bug_id
- * @return null
- */
-function email_new_bug( $p_bug_id ) {
-	email_generic( $p_bug_id, 'new', 'email_notification_title_for_action_bug_submitted' );
-}
-
-/**
- * send notices when a new bugnote
- * @param int $p_bug_id
- * @return null
- */
-function email_bugnote_add( $p_bug_id ) {
-	email_generic( $p_bug_id, 'bugnote', 'email_notification_title_for_action_bugnote_submitted' );
-}
-
-/**
- * send notices when a bug is RESOLVED
- * @param int $p_bug_id
- * @return null
- */
-function email_resolved( $p_bug_id ) {
-	email_generic( $p_bug_id, 'resolved', 'email_notification_title_for_status_bug_resolved' );
-}
-
-/**
- * send notices when a bug is CLOSED
- * @param int $p_bug_id
- * @return null
- */
- function email_close( $p_bug_id ) {
-	email_generic( $p_bug_id, 'closed', 'email_notification_title_for_status_bug_closed' );
-}
-
-/**
- * send notices when a bug is REOPENED
- * @param int $p_bug_id
- * @return null
- */
-function email_reopen( $p_bug_id ) {
-	email_generic( $p_bug_id, 'reopened', 'email_notification_title_for_action_bug_reopened' );
-}
-
-/**
- * send notices when a bug is ASSIGNED
- * @param int $p_bug_id
- * @return null
- */
-function email_assign( $p_bug_id ) {
-	email_generic( $p_bug_id, 'owner', 'email_notification_title_for_action_bug_assigned' );
-}
-
-/**
- * send notices when a bug is DELETED
- * @param int $p_bug_id
- * @return null
- */
-function email_bug_deleted( $p_bug_id ) {
-	email_generic( $p_bug_id, 'deleted', 'email_notification_title_for_action_bug_deleted' );
-}
-
-/**
  * Store email in queue for sending
  *
  * @param string $p_recipient

core/sponsorship_api.php
@@ -379,9 +379,9 @@ function sponsorship_set( $p_sponsorship ) {
 	bug_monitor( $c_bug_id, $c_user_id );
 
 	if( $c_id == 0 ) {
-		email_sponsorship_added( $c_bug_id );
+		email_generic( $c_bug_id, 'sponsor', 'email_notification_title_for_action_sponsorship_added' );
 	} else {
-		email_sponsorship_updated( $c_bug_id );
+		email_generic( $c_bug_id, 'sponsor', 'email_notification_title_for_action_sponsorship_updated' );
 	}
 
 	return $t_sponsorship_id;
@@ -435,7 +435,7 @@ function sponsorship_delete( $p_sponsorship_id ) {
 	history_log_event_special( $t_sponsorship->bug_id, BUG_DELETE_SPONSORSHIP, $t_sponsorship->user_id, $t_sponsorship->amount );
 	sponsorship_update_bug( $t_sponsorship->bug_id );
 
-	email_sponsorship_deleted( $t_sponsorship->bug_id );
+	email_generic( $t_sponsorship->bug_id, 'sponsor', 'A sponsorship of the following issue was withdrawn.' );
 }
 
 /**

commit 3b78375117fbaaaefc66b9f73d3ed32c4cd6e848

Fix hardcoded email titles
The e-mail title language strings were replaced by hardcoded English
text.

Regression introduced by 03c5a5d18cfef07dec45a1025072981f24a5f07c.

Fixes #19984

bug_update.php
@@ -417,10 +417,10 @@ helper_call_custom_function( 'issue_update_notify', array( $f_bug_id ) );
 
 # Send a notification of changes via email.
 if( $t_resolve_issue ) {
-	email_generic( $f_bug_id, 'resolved', 'The following issue has been RESOLVED.' );
+	email_generic( $f_bug_id, 'resolved', 'email_notification_title_for_status_bug_resolved' );
 	email_relationship_child_resolved( $f_bug_id );
 } else if( $t_close_issue ) {
-	email_generic( $f_bug_id, 'closed', 'The following issue has been CLOSED' );
+	email_generic( $f_bug_id, 'closed', 'email_notification_title_for_status_bug_closed' );
 	email_relationship_child_closed( $f_bug_id );
 } else if( $t_reopen_issue ) {
 	email_generic( $f_bug_id, 'reopened', 'email_notification_title_for_action_bug_reopened' );

core/bug_api.php
@@ -1695,7 +1695,7 @@ function bug_close( $p_bug_id, $p_bugnote_text = '', $p_bugnote_private = false,
 
 	bug_set_field( $p_bug_id, 'status', config_get( 'bug_closed_status_threshold' ) );
 
-	email_generic( $p_bug_id, 'closed', 'The following issue has been CLOSED' );
+	email_generic( $p_bug_id, 'closed', 'email_notification_title_for_status_bug_closed' );
 	email_relationship_child_closed( $p_bug_id );
 
 	return true;
@@ -1781,7 +1781,7 @@ function bug_resolve( $p_bug_id, $p_resolution, $p_fixed_in_version = '', $p_bug
 		bug_set_field( $p_bug_id, 'handler_id', $p_handler_id );
 	}
 
-	email_generic( $p_bug_id, 'resolved', 'The following issue has been RESOLVED.' );
+	email_generic( $p_bug_id, 'resolved', 'email_notification_title_for_status_bug_resolved' );
 	email_relationship_child_resolved( $p_bug_id );
 
 	return true;