aa652758c0d2fca011ae9fe3a1b76fff20ce3f1a
Damien Regad <dregad@mantisbt.org>
2015-08-13 17:51:59 +0200

    Set bug attachment ownership when copying them
    file_copy_attachments() did not set user_id when inserting the new
    attachment, resulting in that field being set to 0.
    
    The user_id is now set to the original issue's.
    
    Fixes #20018
    
    Backport from master 8392a9b67f9c897849395d42a131fb979f45148f.

--- core/file_api.php
+++ core/file_api.php
@@ -930,19 +930,22 @@ function file_copy_attachments( $p_source_bug_id, $p_dest_bug_id ) {
 			}
 		}
 
-		$query = "INSERT INTO $t_mantis_bug_file_table
-							( bug_id, title, description, diskfile, filename, folder, filesize, file_type, date_added, content )
-							VALUES ( " . db_param() . ",
-									 " . db_param() . ",
-									 " . db_param() . ",
-									 " . db_param() . ",
-									 " . db_param() . ",
-									 " . db_param() . ",
-									 " . db_param() . ",
-									 " . db_param() . ",
-									 " . db_param() . ",
-									 " . db_param() . ");";
-		db_query_bound( $query, Array( $p_dest_bug_id, $t_bug_file['title'], $t_bug_file['description'], $t_new_diskfile_name, $t_new_file_name, $t_file_path, $t_bug_file['filesize'], $t_bug_file['file_type'], $t_bug_file['date_added'], $t_bug_file['content'] ) );
+		$t_query = 'INSERT INTO $t_mantis_bug_file_table (
+				bug_id, title, description, diskfile, filename, folder,
+				filesize, file_type, date_added, user_id, content
+			)
+			VALUES ( '
+			. db_param() . ', ' . db_param() . ', ' . db_param() . ', '
+			. db_param() . ', ' . db_param() . ', ' . db_param() . ', '
+			. db_param() . ', ' . db_param() . ', ' . db_param() . ', '
+			. db_param() . ', ' . db_param() .
+			')';
+		db_query_bound( $t_query, array(
+			$p_dest_bug_id, $t_bug_file['title'], $t_bug_file['description'],
+			$t_new_diskfile_name, $t_new_file_name, $t_file_path,
+			$t_bug_file['filesize'], $t_bug_file['file_type'], $t_bug_file['date_added'],
+			$t_bug_file['user_id'], $t_bug_file['content']
+		) );
 	}
 }
 
