1a04959d8ed2cf7883fcc17f341b5a9ad9af3dc8
Carlos Proensa <proensa@gmail.com>
2015-09-11 11:47:54 +0200

    adds ZWSP characters after each '|' to allow line wrapping

--- manage_custom_field_page.php
+++ manage_custom_field_page.php
@@ -76,7 +76,7 @@ print_manage_menu( 'manage_custom_field_page.php' );
 				</td>
 				<td><?php echo count( custom_field_get_project_ids( $t_field_id ) ) ?></td>
 				<td><?php echo get_enum_element( 'custom_field_type', $t_desc['type'] ) ?></td>
-				<td><?php echo string_display( $t_desc['possible_values'] ) ?></td>
+                                <td><?php echo str_replace( '|', '|&#x200b;', string_display( $t_desc['possible_values'] ) ) ?></td>
 				<td><?php echo string_display( $t_desc['default_value'] ) ?></td>
 			</tr><?php
 		} # Create Form END ?>
