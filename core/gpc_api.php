mantisbt/mantisbt

commit b53f7caace3513d544149a18beb8f1e422e9a96e

Fix gpc_get_xxx_array for associative arrays
Functions:
- gpc_get_int_array(),
- gpc_get_bool_array()
dont work properly for html fields named with associative indexes
(numeric out or order, or strings), because it assumes indexes as
zero-based sequential numbers.

The code is changed to keep the original keys as defined in the field
names.

This is not an issue in MantisBT core, but may affects plugins.

Fixes #20476

Signed-off-by: Damien Regad <dregad@mantisbt.org>

core/gpc_api.php
@@ -289,9 +289,8 @@ function gpc_get_int_array( $p_var_name, array $p_default = null ) {
 		trigger_error( ERROR_GPC_ARRAY_EXPECTED, ERROR );
 	}
 
-	$t_count = count( $t_result );
-	for( $i = 0; $i < $t_count; $i++ ) {
-		$t_result[$i] = (int)$t_result[$i];
+	foreach( $t_result as $t_key => $t_value ) {
+		$t_result[$t_key] = (int)$t_value;
 	}
 
 	return $t_result;
@@ -316,9 +315,8 @@ function gpc_get_bool_array( $p_var_name, array $p_default = null ) {
 		trigger_error( ERROR_GPC_ARRAY_EXPECTED, ERROR );
 	}
 
-	$t_count = count( $t_result );
-	for( $i = 0; $i < $t_count; $i++ ) {
-		$t_result[$i] = gpc_string_to_bool( $t_result[$i] );
+	foreach( $t_result as $t_key => $t_value ) {
+		$t_result[$t_key] = gpc_string_to_bool( $t_value );
 	}
 
 	return $t_result;

commit 2ed3c644e50c8e3959f06c0a8f2b49d874438c79

Fix gpc_get regression
This fixes a regression introduced by commit
b53f7caace3513d544149a18beb8f1e422e9a96e.

When loop is changed into 'foreach', null values produce error
if not checked.

Issue #20476

Signed-off-by: Damien Regad <dregad@mantisbt.org>

core/gpc_api.php
@@ -288,9 +288,10 @@ function gpc_get_int_array( $p_var_name, array $p_default = null ) {
 		error_parameters( $p_var_name );
 		trigger_error( ERROR_GPC_ARRAY_EXPECTED, ERROR );
 	}
-
-	foreach( $t_result as $t_key => $t_value ) {
-		$t_result[$t_key] = (int)$t_value;
+	if( is_array( $t_result ) ) {
+		foreach( $t_result as $t_key => $t_value ) {
+			$t_result[$t_key] = (int)$t_value;
+		}
 	}
 
 	return $t_result;
@@ -315,8 +316,10 @@ function gpc_get_bool_array( $p_var_name, array $p_default = null ) {
 		trigger_error( ERROR_GPC_ARRAY_EXPECTED, ERROR );
 	}
 
-	foreach( $t_result as $t_key => $t_value ) {
-		$t_result[$t_key] = gpc_string_to_bool( $t_value );
+	if( is_array( $t_result ) ) {
+		foreach( $t_result as $t_key => $t_value ) {
+			$t_result[$t_key] = gpc_string_to_bool( $t_value );
+		}
 	}
 
 	return $t_result;