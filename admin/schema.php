f581c8cd6d9a099a9bd2c33e6c9fa8c6bc2aed00
Victor Boctor <vboctor@gmail.com>
2016-01-25 16:29:20 +0000

    Install plugin if avatars enabled.

--- admin/schema.php
+++ admin/schema.php
@@ -862,11 +862,8 @@ $g_upgrade[203] = array( 'AlterColumnSQL', array( db_get_table( 'bug_file' ), "
 $g_upgrade[204] = array( 'AlterColumnSQL', array( db_get_table( 'project_file' ), "
 	content					B		NULL " . $t_blob_default ) );
 
-# Enable gravatar plugin by default
-$g_upgrade[205] = array( 'InsertData', array( db_get_table( 'plugin' ), "
-	( basename, enabled )
-	VALUES
-	( 'Gravatar', '1' )" ) );
+# Enable gravatar plugin if avatars are enabled
+$g_upgrade[205] = array( 'UpdateFunction', 'gravatar_plugin', array() );
 
 # Release marker: 1.3.0
 
