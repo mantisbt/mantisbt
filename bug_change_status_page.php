mantisbt/mantisbt

commit 5c4035bfe3bd652c8cd71a20939348127fcce7b3

HTML/CSS for bug change status page
Refactor the code to use "1.3-style" layout with form-container div and
fieldset, instead of html table.

Use 'required' CSS class for custom fields display instead of hardcoded
'*' with span.

bug_change_status_page.php
@@ -137,41 +137,42 @@ print_recently_visited();
 <br />
 <div id="bug-change-status-div" class="form-container">
 
-<form id="bug-change-status-form" name="bug_change_status_form" method="post" action="bug_update.php">
-
-	<?php echo form_security_field( 'bug_update' ) ?>
-	<table>
-		<thead>
-			<!-- Title -->
-			<tr>
-				<td class="form-title" colspan="2">
-					<input type="hidden" name="bug_id" value="<?php echo $f_bug_id ?>" />
-					<input type="hidden" name="status" value="<?php echo $f_new_status ?>" />
-					<input type="hidden" name="last_updated" value="<?php echo $t_bug->last_updated ?>" />
-					<?php echo lang_get( $t_status_label . '_bug_title' ) ?>
-				</td>
-			</tr>
+<h2><?php echo lang_get( $t_status_label . '_bug_title' ); ?></h2>
 <?php
 	if( $f_new_status >= $t_resolved ) {
 		if( relationship_can_resolve_bug( $f_bug_id ) == false ) {
-			echo '<tr><td colspan="2">' . lang_get( 'relationship_warning_blocking_bugs_not_resolved_2' ) . '</td></tr>';
+			echo '<div class="footer">';
+			echo lang_get( 'relationship_warning_blocking_bugs_not_resolved_2' );
+			echo '</div>';
 		}
 	}
 ?>
-		</thead>
-		<tbody>
+
+<form id="bug-change-status-form" name="bug_change_status_form" method="post" action="bug_update.php">
+
+<fieldset>
+
+	<?php echo form_security_field( 'bug_update' ) ?>
+
+	<input type="hidden" name="bug_id" value="<?php echo $f_bug_id ?>" />
+	<input type="hidden" name="status" value="<?php echo $f_new_status ?>" />
+	<input type="hidden" name="last_updated" value="<?php echo $t_bug->last_updated ?>" />
+	<input type="hidden" name="action_type" value="<?php echo $f_change_type; ?>" />
+
 <?php
-$t_current_resolution = $t_bug->resolution;
-$t_bug_is_open = $t_current_resolution < $t_resolved;
-if( ( $f_new_status >= $t_resolved ) && ( ( $f_new_status < $t_closed ) || ( $t_bug_is_open ) ) ) { ?>
-<!-- Resolution -->
-			<tr>
-				<th class="category">
-					<?php echo lang_get( 'resolution' ) ?>
-				</th>
-				<td>
-					<select name="resolution">
-			<?php
+	$t_current_resolution = $t_bug->resolution;
+	$t_bug_is_open = $t_current_resolution < $t_resolved;
+
+	if( ( $f_new_status >= $t_resolved ) && ( ( $f_new_status < $t_closed ) || ( $t_bug_is_open ) ) ) {
+?>
+	<!-- Resolution -->
+	<div class="field-container">
+		<label for="resolution">
+			<span><?php echo lang_get( 'resolution' ) ?></span>
+		</label>
+		<span class="select">
+ 			<select name="resolution">
+<?php
 				$t_resolution = $t_bug_is_open ? config_get( 'bug_resolution_fixed_threshold' ) : $t_current_resolution;
 
 				$t_relationships = relationship_get_all_src( $f_bug_id );
@@ -183,216 +184,235 @@ if( ( $f_new_status >= $t_resolved ) && ( ( $f_new_status < $t_closed ) || ( $t_
 				}
 
 				print_enum_string_option_list( 'resolution', $t_resolution );
-			?>
-					</select>
-				</td>
-			</tr>
-<?php } ?>
-
-<?php
-if( $f_new_status >= $t_resolved
-	&& $f_new_status < $t_closed
-	&& $t_resolution != config_get( 'bug_duplicate_resolution' ) ) { ?>
-<!-- Duplicate ID -->
-			<tr>
-				<th class="category">
-					<?php echo lang_get( 'duplicate_id' ) ?>
-				</th>
-				<td>
-					<input type="text" name="duplicate_id" maxlength="10" />
-				</td>
-			</tr>
-<?php } ?>
-
-<?php
-if( access_has_bug_level( config_get( 'update_bug_assign_threshold', config_get( 'update_bug_threshold' ) ), $f_bug_id ) ) {
-	$t_suggested_handler_id = $t_bug->handler_id;
-
-	if( $t_suggested_handler_id == NO_USER && access_has_bug_level( config_get( 'handle_bug_threshold' ), $f_bug_id ) ) {
-		$t_suggested_handler_id = $t_current_user_id;
-	}
 ?>
-<!-- Assigned To -->
-			<tr>
-				<th class="category">
-					<?php echo lang_get( 'assigned_to' ) ?>
-				</th>
-				<td>
-					<select name="handler_id">
-						<option value="0"></option>
-						<?php print_assign_to_option_list( $t_suggested_handler_id, $t_bug->project_id ) ?>
-					</select>
-				</td>
-			</tr>
-<?php } ?>
-
-<?php if( $t_can_update_due_date ) {
-	$t_date_to_display = '';
-	if( !date_is_null( $t_bug->due_date ) ) {
-		$t_date_to_display = date( config_get( 'calendar_date_format' ), $t_bug->due_date );
-	}
-?>
-<!-- Due date -->
-			<tr>
-				<th class="category">
-					<?php print_documentation_link( 'due_date' ) ?>
-				</th>
-				<td>
-					<?php echo '<input ' . helper_get_tab_index() . ' type="text" id="due_date" name="due_date" class="datetime" size="20" maxlength="16" value="' . $t_date_to_display . '" />' ?>
-				</td>
-			</tr>
-<?php } ?>
-
-<!-- Custom Fields -->
-<?php
-/** @todo thraxisp - I undid part of the change for #5068 for #5527
- * We really need to say what fields are shown in which statusses. For now,
- * this page will show required custom fields in update mode, or
- *  display or required fields on resolve or close
- */
-$t_custom_status_label = 'update'; # Don't show custom fields by default
-if( ( $f_new_status == $t_resolved ) && ( $f_new_status < $t_closed ) ) {
-	$t_custom_status_label = 'resolved';
-}
-if( $t_closed == $f_new_status ) {
-	$t_custom_status_label = 'closed';
-}
-
-$t_related_custom_field_ids = custom_field_get_linked_ids( $t_bug->project_id );
-
-foreach( $t_related_custom_field_ids as $t_id ) {
-	$t_def = custom_field_get_definition( $t_id );
-	$t_display = $t_def['display_' . $t_custom_status_label];
-	$t_require = $t_def['require_' . $t_custom_status_label];
-
-	if( ( 'update' == $t_custom_status_label ) && ( !$t_require ) ) {
-		continue;
-	}
-	if( in_array( $t_custom_status_label, array( 'resolved', 'closed' ) ) && !( $t_display || $t_require ) ) {
-		continue;
-	}
-	if( custom_field_has_write_access( $t_id, $f_bug_id ) ) {
-?>
-			<tr>
-				<th class="category">
-					<?php if( $t_require ) { ?>
-						<span class="required">*</span>
-					<?php }
-					echo lang_get_defaulted( $t_def['name'] )
-					?>
-				</th>
-				<td>
-					<?php
-						print_custom_field_input( $t_def, $f_bug_id );
-					?>
-				</td>
-			</tr>
-<?php
-	} else if( custom_field_has_read_access( $t_id, $f_bug_id ) ) {
-		#  custom_field_has_write_access( $t_id, $f_bug_id ) )
-?>
-			<tr>
-				<th class="category">
-					<?php echo lang_get_defaulted( $t_def['name'] ) ?>
-				</th>
-				<td>
-					<?php print_custom_field_value( $t_def, $t_id, $f_bug_id );			?>
-				</td>
-			</tr>
-<?php
-	} # custom_field_has_read_access( $t_id, $f_bug_id ) )
-} # foreach( $t_related_custom_field_ids as $t_id )
-?>
-
-<?php
-if( ( $f_new_status >= $t_resolved ) ) {
-	if( version_should_show_product_version( $t_bug->project_id )
-		&& !bug_is_readonly( $f_bug_id )
-		&& access_has_bug_level( config_get( 'update_bug_threshold' ), $f_bug_id )
-	) {
-?>
-			<!-- Fixed in Version -->
-			<tr>
-				<th class="category">
-					<?php echo lang_get( 'fixed_in_version' ) ?>
-				</th>
-				<td>
-					<select name="fixed_in_version">
-						<?php print_version_option_list( $t_bug->fixed_in_version, $t_bug->project_id, VERSION_ALL ) ?>
-					</select>
-				</td>
-			</tr>
+			</select>
+		</span>
+		<span class="label-style"></span>
+	</div>
 <?php
 	}
-}
+
+	if( $f_new_status >= $t_resolved
+		&& $f_new_status < $t_closed
+		&& $t_resolution != config_get( 'bug_duplicate_resolution' ) ) {
 ?>
-<?php event_signal( 'EVENT_UPDATE_BUG_STATUS_FORM', array( $f_bug_id ) ); ?>
-<?php if( $f_change_type == BUG_UPDATE_TYPE_REOPEN ) { ?>
-<!-- Bug was re-opened -->
+	<!-- Duplicate ID -->
+	<div class="field-container">
+		<label for="duplicate_id">
+			<span><?php echo lang_get( 'duplicate_id' ) ?></span>
+		</label>
+		<span class="input">
+ 			<input type="text" name="duplicate_id" maxlength="10" />
+		</span>
+		<span class="label-style"></span>
+	</div>
+
 <?php
-	printf( '	<input type="hidden" name="resolution" value="%s" />' . "\n", config_get( 'bug_reopen_resolution' ) );
-}
+	}
+
+	if( access_has_bug_level( config_get( 'update_bug_assign_threshold', config_get( 'update_bug_threshold' ) ), $f_bug_id ) ) {
+		$t_suggested_handler_id = $t_bug->handler_id;
+
+		if( $t_suggested_handler_id == NO_USER && access_has_bug_level( config_get( 'handle_bug_threshold' ), $f_bug_id ) ) {
+			$t_suggested_handler_id = $t_current_user_id;
+		}
+
 ?>
-			<!-- Bugnote -->
-			<tr id="bug-change-status-note">
-				<th class="category">
-					<?php echo lang_get( 'add_bugnote_title' ) ?>
-				</th>
-				<td>
-					<textarea name="bugnote_text" cols="80" rows="10"></textarea>
-				</td>
-			</tr>
-<?php if( access_has_bug_level( config_get( 'private_bugnote_threshold' ), $f_bug_id ) ) { ?>
-			<tr>
-				<th class="category">
-					<?php echo lang_get( 'view_status' ) ?>
-				</th>
-				<td>
+	<!-- Assigned To -->
+	<div class="field-container">
+		<label for="handler_id">
+			<span><?php echo lang_get( 'assigned_to' ) ?></span>
+		</label>
+		<span class="select">
+			<select name="handler_id">
+				<option value="0"></option>
+				<?php print_assign_to_option_list( $t_suggested_handler_id, $t_bug->project_id ) ?>
+			</select>
+		</span>
+		<span class="label-style"></span>
+	</div>
+
 <?php
+	}
+
+	if( true || $t_can_update_due_date ) {
+		$t_date_to_display = '';
+
+		if( !date_is_null( $t_bug->due_date ) ) {
+			$t_date_to_display = date( config_get( 'calendar_date_format' ), $t_bug->due_date );
+		}
+?>
+	<!-- Due date -->
+	<div class="field-container">
+		<label for="due_date">
+			<span><?php echo lang_get( 'due_date' ) ?></span>
+		</label>
+		<span class="input">
+ 			<input type="text" id="due_date" name="due_date"
+ 				class="datetime" size="20" maxlength="16"
+ 				<?php helper_get_tab_index() ?>
+ 				value="<?php echo $t_date_to_display ?>" />
+		</span>
+		<span class="label-style"></span>
+	</div>
+
+<?php
+	}
+?>
+
+	<!-- Custom Fields -->
+<?php
+	/**
+	 * @todo thraxisp - I undid part of the change for #5068 for #5527
+	 * We really need to say what fields are shown in which statusses. For now,
+	 * this page will show required custom fields in update mode, or
+	 * display or required fields on resolve or close
+	 */
+	$t_custom_status_label = 'update'; # Don't show custom fields by default
+	if( ( $f_new_status == $t_resolved ) && ( $f_new_status < $t_closed ) ) {
+		$t_custom_status_label = 'resolved';
+	}
+	if( $t_closed == $f_new_status ) {
+		$t_custom_status_label = 'closed';
+	}
+
+	$t_related_custom_field_ids = custom_field_get_linked_ids( $t_bug->project_id );
+
+	foreach( $t_related_custom_field_ids as $t_id ) {
+		$t_def = custom_field_get_definition( $t_id );
+		$t_display = $t_def['display_' . $t_custom_status_label];
+		$t_require = $t_def['require_' . $t_custom_status_label];
+
+		if( ( 'update' == $t_custom_status_label ) && ( !$t_require ) ) {
+			continue;
+		}
+		if( in_array( $t_custom_status_label, array( 'resolved', 'closed' ) ) && !( $t_display || $t_require ) ) {
+			continue;
+		}
+		$t_has_write_access = custom_field_has_write_access( $t_id, $f_bug_id );
+		$t_class_required = $t_require && $t_has_write_access ? 'class="required"' : '';
+?>
+	<div class="field-container">
+		<label <?php echo $t_class_required ?> for="due_date">
+			<span><?php echo lang_get_defaulted( $t_def['name'] ) ?></span>
+		</label>
+		<span class="input">
+<?php
+			if( $t_has_write_access ) {
+				print_custom_field_input( $t_def, $f_bug_id );
+			} elseif( custom_field_has_read_access( $t_id, $f_bug_id ) ) {
+				print_custom_field_value( $t_def, $t_id, $f_bug_id );
+			}
+?>
+		</span>
+		<span class="label-style"></span>
+	</div>
+
+<?php
+	} # foreach( $t_related_custom_field_ids as $t_id )
+
+	if( ( $f_new_status >= $t_resolved ) ) {
+		if( version_should_show_product_version( $t_bug->project_id )
+			&& !bug_is_readonly( $f_bug_id )
+			&& access_has_bug_level( config_get( 'update_bug_threshold' ), $f_bug_id )
+		) {
+?>
+	<!-- Fixed in Version -->
+	<div class="field-container">
+		<label for="due_date">
+			<span><?php echo lang_get( 'fixed_in_version' ) ?></span>
+		</label>
+		<span class="select">
+			<select name="fixed_in_version">
+				<?php print_version_option_list( $t_bug->fixed_in_version, $t_bug->project_id, VERSION_ALL ) ?>
+			</select>
+		</span>
+		<span class="label-style"></span>
+	</div>
+<?php
+		}
+	}
+
+	event_signal( 'EVENT_UPDATE_BUG_STATUS_FORM', array( $f_bug_id ) );
+
+	if( $f_change_type == BUG_UPDATE_TYPE_REOPEN ) {
+?>
+	<!-- Bug was re-opened -->
+<?php
+		printf( '	<input type="hidden" name="resolution" value="%s" />' . "\n", config_get( 'bug_reopen_resolution' ) );
+	}
+?>
+
+	<!-- Bugnote -->
+	<div class="field-container">
+		<label for="bugnote_text">
+			<span><?php echo lang_get( 'add_bugnote_title' ) ?></span>
+		</label>
+		<span class="textarea">
+			<textarea name="bugnote_text" cols="80" rows="10"></textarea>
+		</span>
+		<span class="label-style"></span>
+	</div>
+
+<?php
+	if( access_has_bug_level( config_get( 'private_bugnote_threshold' ), $f_bug_id ) ) {
 		$t_default_bugnote_view_status = config_get( 'default_bugnote_view_status' );
+?>
+	<!-- View status -->
+	<div class="field-container">
+		<label for="private">
+			<span><?php echo lang_get( 'view_status' ) ?></span>
+		</label>
+		<span class="checkbox">
+<?php
 		if( access_has_bug_level( config_get( 'set_view_status_threshold' ), $f_bug_id ) ) {
 ?>
-			<input type="checkbox" name="private" <?php check_checked( $t_default_bugnote_view_status, VS_PRIVATE ); ?> />
+			<input type="checkbox" name="private"
+				<?php check_checked( $t_default_bugnote_view_status, VS_PRIVATE ); ?> />
 <?php
 			echo lang_get( 'private' );
 		} else {
 			echo get_enum_element( 'project_view_state', $t_default_bugnote_view_status );
 		}
 ?>
-				</td>
-			</tr>
-<?php } ?>
+		</span>
+		<span class="label-style"></span>
+	</div>
 
-<?php if( config_get( 'time_tracking_enabled' ) ) { ?>
-<?php 	if( access_has_bug_level( config_get( 'private_bugnote_threshold' ), $f_bug_id ) ) { ?>
-<?php 		if( access_has_bug_level( config_get( 'time_tracking_edit_threshold' ), $f_bug_id ) ) { ?>
-			<tr>
-				<th class="category">
-					<?php echo lang_get( 'time_tracking' ) ?>
-				</th>
-				<td>
-					<input type="text" name="time_tracking" size="5" placeholder="hh:mm" />
-				</td>
-			</tr>
-<?php 		} ?>
-<?php 	} ?>
-<?php } ?>
+<?php
+	}
 
-<?php event_signal( 'EVENT_BUGNOTE_ADD_FORM', array( $f_bug_id ) ); ?>
+	if(     config_get( 'time_tracking_enabled' )
+		&& access_has_bug_level( config_get( 'private_bugnote_threshold' ), $f_bug_id )
+		&& access_has_bug_level( config_get( 'time_tracking_edit_threshold' ), $f_bug_id )
+	) {
+?>
+	<!-- Time tracking -->
+	<div class="field-container">
+		<label for="time_tracking">
+			<span><?php echo lang_get( 'time_tracking' ) ?></span>
+		</label>
+		<span class="input">
+			<input type="text" name="time_tracking" size="5" placeholder="hh:mm" />
+		</span>
+		<span class="label-style"></span>
+	</div>
+<?php
+	}
 
-			<!-- Submit Button -->
-			<tr>
-				<td class="center" colspan="2">
-					<input type="submit" class="button" value="<?php echo lang_get( $t_status_label . '_bug_button' ) ?>" />
-				</td>
-			</tr>
-		</tbody>
-	</table>
-	<input type="hidden" name="action_type" value="<?php echo $f_change_type; ?>" />
+	event_signal( 'EVENT_BUGNOTE_ADD_FORM', array( $f_bug_id ) );
+?>
+
+	<!-- Submit Button -->
+	<span class="submit-button">
+		<input type="submit" class="button" value="<?php echo lang_get( $t_status_label . '_bug_button' ) ?>" />
+	</span>
+
 </form>
-
 </div>
-<br />
+
+<br>
+
 <?php
 define( 'BUG_VIEW_INC_ALLOW', true );
 include( dirname( __FILE__ ) . '/bug_view_inc.php' );

commit a57d77a61dcc95247025546fb362b1db24169e75

Fix due date check on bug change status page
This is a regression from 5c4035bf which updates the page's
layout to the 1.3 approach.  Left over debug code causes the
field to be always visible.

bug_change_status_page.php
@@ -235,7 +235,7 @@ print_recently_visited();
 <?php
 	}
 
-	if( true || $t_can_update_due_date ) {
+	if( $t_can_update_due_date ) {
 		$t_date_to_display = '';
 
 		if( !date_is_null( $t_bug->due_date ) ) {