d063334922a23aadb103f9f75553ab2b0bddff6f
Victor Boctor <vboctor@gmail.com>
2016-02-28 05:22:23 +0000

    Fix indentation tabs for GravatarPlugin class

--- plugins/Gravatar/Gravatar.php
+++ plugins/Gravatar/Gravatar.php
@@ -81,7 +81,7 @@ class GravatarPlugin extends MantisPlugin {
 	 * @return array
 	 */
 	function config() {
-    	$t_default_avatar = config_get( 'show_avatar' );
+		$t_default_avatar = config_get( 'show_avatar' );
 
 		# Set default avatar for legacy configuration
 		if( ON === $t_default_avatar || OFF === $t_default_avatar) {
@@ -89,28 +89,31 @@ class GravatarPlugin extends MantisPlugin {
 		}
 
 		return array(
-	        /**
-	         * The rating of the avatar to show: 'G', 'PG', 'R', 'X'
-	         * @link http://en.gravatar.com/site/implement/images/
-	         */
-	       'rating' => self::GRAVATAR_RATING_G,
+			/**
+			 * The rating of the avatar to show: 'G', 'PG', 'R', 'X'
+			 * @link http://en.gravatar.com/site/implement/images/
+			 */
+			'rating' => self::GRAVATAR_RATING_G,
 
-           /**
-            * The kind of avatar to use:
-            *
-            * - One of Gravatar's defaults (mm, identicon, monsterid, wavatar, retro)
-            *   @link http://en.gravatar.com/site/implement/images/
-            * - An URL to the default image to be used (for example,
-            *   "http:/path/to/unknown.jpg" or "%path%images/avatar.png")
-	        */
-	       'default_avatar' => $t_default_avatar,
-	    );
+			/**
+			 * The kind of avatar to use:
+			 *
+			 * - One of Gravatar's defaults (mm, identicon, monsterid, wavatar, retro)
+			 *   @link http://en.gravatar.com/site/implement/images/
+			 * - An URL to the default image to be used (for example,
+			 *   "http:/path/to/unknown.jpg" or "%path%images/avatar.png")
+			 */
+			'default_avatar' => $t_default_avatar,
+		);
 	}
 
+	/**
+	 * Register event hooks for plugin.
+	 */
 	function hooks() {
 		return array(
-		    'EVENT_USER_AVATAR' => 'user_get_avatar',
-		    'EVENT_LAYOUT_RESOURCES' => 'csp_headers',
+			'EVENT_USER_AVATAR' => 'user_get_avatar',
+			'EVENT_LAYOUT_RESOURCES' => 'csp_headers',
 		);
 	}
 
@@ -129,53 +132,53 @@ class GravatarPlugin extends MantisPlugin {
 	}
 
 	/**
-     * Return the user avatar image URL
-     * in this first implementation, only gravatar.com avatars are supported
-     *
-     * This function returns an array( URL, width, height ) or an empty array when the given user has no avatar.
-     *
-     * @param string  $p_event   The name for the event.
-     * @param integer $p_user_id A valid user identifier.
-     * @param integer $p_size    The required number of pixel in the image to retrieve the link for.
-     *
-     * @return object An instance of class Avatar or null.
-     */
-    function user_get_avatar( $p_event, $p_user_id, $p_size = 80 ) {
-    	$t_default_avatar = config_get( 'show_avatar' );
+	 * Return the user avatar image URL
+	 * in this first implementation, only gravatar.com avatars are supported
+	 *
+	 * This function returns an array( URL, width, height ) or an empty array when the given user has no avatar.
+	 *
+	 * @param string  $p_event   The name for the event.
+	 * @param integer $p_user_id A valid user identifier.
+	 * @param integer $p_size    The required number of pixel in the image to retrieve the link for.
+	 *
+	 * @return object An instance of class Avatar or null.
+	 */
+	function user_get_avatar( $p_event, $p_user_id, $p_size = 80 ) {
+		$t_default_avatar = config_get( 'show_avatar' );
 
-    	# Set default avatar for legacy configuration
-    	if( ON === $t_default_avatar || OFF === $t_default_avatar ) {
-    		$t_default_avatar = 'identicon';
-    	}
+		# Set default avatar for legacy configuration
+		if( ON === $t_default_avatar || OFF === $t_default_avatar ) {
+			$t_default_avatar = 'identicon';
+		}
 
-    	# Default avatar is either one of Gravatar's options, or
-    	# assumed to be an URL to a default avatar image
-    	$t_default_avatar = urlencode( $t_default_avatar );
-    	$t_rating = plugin_config_get( 'rating' );
+		# Default avatar is either one of Gravatar's options, or
+		# assumed to be an URL to a default avatar image
+		$t_default_avatar = urlencode( $t_default_avatar );
+		$t_rating = plugin_config_get( 'rating' );
 
-    	if ( user_exists( $p_user_id ) ) {
-    		$t_email_hash = md5( strtolower( trim( user_get_email( $p_user_id ) ) ) );
-    	} else {
-    		$t_email_hash = md5( 'generic-avatar-since-user-not-found' );
-    	}
+		if( user_exists( $p_user_id ) ) {
+			$t_email_hash = md5( strtolower( trim( user_get_email( $p_user_id ) ) ) );
+		} else {
+			$t_email_hash = md5( 'generic-avatar-since-user-not-found' );
+		}
 
-    	# Build Gravatar URL
+		# Build Gravatar URL
 		$t_avatar_url = self::getAvatarUrl() .
 			'avatar/' . $t_email_hash . '?d=' . $t_default_avatar .
 			'&r=' . $t_rating . '&s=' . $p_size;
 
-        $t_avatar = new Avatar();
-        $t_avatar->image = $t_avatar_url;
+		$t_avatar = new Avatar();
+		$t_avatar->image = $t_avatar_url;
 
-    	return $t_avatar;
-    }
+		return $t_avatar;
+	}
 
-    /**
-     * Gets the gravatar base URL
-     *
-     * @return The gravatar URL.
-     */
-    private static function getAvatarUrl() {
+	/**
+	 * Gets the gravatar base URL
+	 *
+	 * @return The gravatar URL.
+	 */
+	private static function getAvatarUrl() {
 		if( http_is_protocol_https() ) {
 			$t_avatar_url = self::GRAVATAR_URL_SECURE;
 		} else {
