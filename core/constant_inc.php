7795b3020a00e0c28550f5dc4578bbb21a93d5ff
Damien Regad <dregad@mantisbt.org>
2016-05-23 22:04:11 +0200

    Upgrade jQuery from v1.11.3 to v1.12.4
    Fixes #21059

--- core/constant_inc.php
+++ core/constant_inc.php
@@ -607,7 +607,7 @@ define( 'FTP', 1 );                                 # DISK
 define( 'ERROR_FTP_CONNECT_ERROR', 16 );            # N/A
 
 # JQuery and JQuery UI
-define ( 'JQUERY_VERSION', '1.11.3' );
+define ( 'JQUERY_VERSION', '1.12.4' );
 define ( 'JQUERY_UI_VERSION', '1.11.4' );
 
 # Byte Order Markers
