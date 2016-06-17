cd54bcd35a8a8183a32c1a5c21686153e49af368
root <badfiles@mail.ru>
2016-06-17 19:03:23 +0300

    fix previous revert

--- core/layout_api.php
+++ core/layout_api.php
@@ -397,8 +397,8 @@ function layout_navbar() {
 
 	# mobile view
 	echo '<div class="hidden-sm hidden-md hidden-lg">';
-	echo '<ul class="nav navbar-nav">';
 	echo '<div class="navbar-menu pull-left navbar-collapse collapse" role="navigation" style="height: auto;">';
+	echo '<ul class="nav navbar-nav">';
 	if (auth_is_user_authenticated()) {
 		layout_navbar_user_menu(false);
 		layout_navbar_projects_menu();
