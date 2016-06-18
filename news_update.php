15d2e72682261400b27411c9e9ae306c96220e9e
badfiles <badfiles@mail.ru>
2016-06-18 04:26:07 +0300

    Fix news layout
    Remove div and make space between buttons.
    
    Commits from PR https://github.com/mantisbt/mantisbt/pull/795 squashed.
    
    Signed-off-by: Damien Regad <dregad@mantisbt.org>

--- news_update.php
+++ news_update.php
@@ -70,16 +70,14 @@ layout_page_begin( 'main_page.php' );
 echo '<div class="space-20"></div>';
 echo '<div class="alert alert-success">';
 echo lang_get( 'operation_successful' );
-
-echo '<br />';
+echo '<br /></div>';
 
 print_button( 'news_edit_page.php?news_id=' . $f_news_id . '&action=edit', lang_get( 'edit_link' ) );
+echo '&nbsp;&nbsp;&nbsp;&nbsp;';
 print_button( 'news_menu_page.php', lang_get( 'proceed' ) );
 
 echo '<br /><br />';
 
 print_news_entry( $f_headline, $f_body, $t_row['poster_id'], $f_view_state, $f_announcement, $t_row['date_posted'] );
-
-echo '</div>';
 
 layout_page_end();
