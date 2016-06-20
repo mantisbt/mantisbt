be4451cbd7305e4234553b0c5faded253356068f
root <badfiles@mail.ru>
2016-06-20 03:02:57 +0300

    activate buttons

--- core/layout_api.php
+++ core/layout_api.php
@@ -980,7 +980,6 @@ function layout_breadcrumbs() {
 	echo '<div id="breadcrumbs" class="breadcrumbs noprint">' , "\n";
 
 	# Login information
-	echo '<ul class="breadcrumb">' , "\n";
 	if( current_user_is_anonymous() ) {
 		$t_return_page = $_SERVER['SCRIPT_NAME'];
 		if( isset( $_SERVER['QUERY_STRING'] ) ) {
@@ -989,13 +988,13 @@ function layout_breadcrumbs() {
 
 		$t_return_page = string_url( $t_return_page );
 
-		echo ' <li><i class="fa fa-user home-icon active"></i> ' . lang_get( 'anonymous' ) . ' </li>' . "\n";
+		echo '<i class="fa fa-user home-icon active"></i> ' . lang_get( 'anonymous' ) . "\n";
 
 		echo '<div class="btn-group btn-corner">' . "\n";
-		echo '	<a href="' . helper_mantis_url( 'login_page.php?return=' . $t_return_page ) .
+		echo '<a href="' . helper_mantis_url( 'login_page.php?return=' . $t_return_page ) .
 			'" class="btn btn-primary btn-xs">' . lang_get( 'login_link' ) . '</a>' . "\n";
 		if( config_get_global( 'allow_signup' ) == ON ) {
-			echo '	<a href="' . helper_mantis_url( 'signup_page.php' ) . '" class="btn btn-primary btn-xs">' .
+			echo '<a href="' . helper_mantis_url( 'signup_page.php' ) . '" class="btn btn-primary btn-xs">' .
 				lang_get( 'signup_link' ) . '</a>' . "\n";
 		}
 		echo '</div>' . "\n";
@@ -1013,9 +1012,8 @@ function layout_breadcrumbs() {
 			$t_display_username . $t_display_realname . '</a>' . "\n";
 
 		$t_label = layout_is_rtl() ? 'arrowed-right' : 'arrowed';
-		echo '  <span class="label hidden-xs label-default ' . $t_label . '">' . $t_access_level . '</span></li>' . "\n";
+		echo '<span class="label hidden-xs label-default ' . $t_label . '">' . $t_access_level . '</span>' . "\n";
 	}
-	echo '</ul>' , "\n";
 
 	# Recently visited
 	if( last_visited_enabled() ) {
