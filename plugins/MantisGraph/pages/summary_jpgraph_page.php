01cc46351caa56078464eee1ee899b77e65d8941
Victor Boctor <victor@mantishub.net>
2016-07-02 22:50:02 -0700

    Remove redundant graphs from adv summary page

--- plugins/MantisGraph/pages/summary_jpgraph_page.php
+++ plugins/MantisGraph/pages/summary_jpgraph_page.php
@@ -36,9 +36,7 @@ layout_page_begin( 'summary_page.php' );
 
 print_summary_menu( 'summary_jpgraph_page.php' );
 
-$t_graphs = array( 'summary_graph_cumulative_bydate', 'summary_graph_bydeveloper', 'summary_graph_byreporter',
-		'summary_graph_byseverity', 'summary_graph_bystatus', 'summary_graph_byresolution',
-		'summary_graph_bycategory', 'summary_graph_bypriority' );
+$t_graphs = array( 'summary_graph_cumulative_bydate', 'summary_graph_bydeveloper', 'summary_graph_byreporter' );
 $t_wide = plugin_config_get( 'summary_graphs_per_row' );
 $t_width = plugin_config_get( 'window_width' );
 $t_graph_width = (int)( ( $t_width - 50 ) / $t_wide );
