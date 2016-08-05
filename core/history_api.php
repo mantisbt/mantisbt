e48e7375b49ca125d2cc690f7387b796de9a6cb2
Carlos Proensa <proensa@gmail.com>
2016-08-05 11:26:36 +0200

    Revert limit history range to current project
    Revert the changes made by Issue #19945 (commit adbd0882).
    
    The api function history_get_event_from_row should not impose a
    limitation for current project queries.
    
    The original timeline issue, which #19945 was addressing, has now been
    resolved in another way, see issue #21072.
    
    Fixes: #21146

--- core/history_api.php
+++ core/history_api.php
@@ -287,18 +287,12 @@ function history_get_range_result( $p_bug_id = null, $p_start_time = null, $p_en
 function history_get_event_from_row( $p_result, $p_user_id = null, $p_check_access_to_issue = true ) {
 	static $s_bug_visible = array();
 	$t_user_id = ( null === $p_user_id ) ? auth_get_current_user_id() : $p_user_id;
-	$t_project_id = helper_get_current_project();
 
 	while ( $t_row = db_fetch_array( $p_result ) ) {
 		extract( $t_row, EXTR_PREFIX_ALL, 'v' );
 
 		# Ignore entries related to non-existing bugs (see #20727)
 		if( !bug_exists( $v_bug_id ) ) {
-			continue;
-		}
-
-		# Make sure the entry belongs to current project.
-		if ( $t_project_id != ALL_PROJECTS && $t_project_id != bug_get_field( $v_bug_id, 'project_id' ) ) {
 			continue;
 		}
 
