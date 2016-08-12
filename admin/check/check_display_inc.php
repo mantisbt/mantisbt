7abcf3c1740cb1ae7eef0f39bbad2d8b8c3b1263
Damien Regad <dregad@mantisbt.org>
2016-08-13 01:17:13 +0200

    OpenSearch: add admin check for Shortname <= 16 chars
    Issue #11964

--- admin/check/check_display_inc.php
+++ admin/check/check_display_inc.php
@@ -36,6 +36,23 @@ require_api( 'config_api.php' );
 
 check_print_section_header_row( 'Display' );
 
+# Test length of OpenSearch ShortNames
+$t_prefix = config_get( 'search_title' );
+$t_shortname_text = sprintf( lang_get( 'opensearch_text_short' ), $t_prefix );
+$t_shortname_id = sprintf( lang_get( 'opensearch_id_short' ), $t_prefix );
+$t_shortname_length = max( strlen( $t_shortname_text ), strlen( $t_shortname_id ) );
+
+check_print_test_warn_row(
+	"Browser Search engine names must be 16 chars or less",
+	$t_shortname_length <= 16,
+	array( false => 'Either shorten the "search_title" configuration option to '
+		. 'a maximum  of ' . ( 16 - $t_shortname_length + strlen( $t_prefix ) )
+		. ' characters, or alter the "opensearch_XXX_short" language strings '
+		. 'as appropriate to meet the <a href="http://www.opensearch.org/">'
+		. 'OpenSearch 1.1</a> specification for the ShortName element.'
+	)
+);
+
 check_print_test_row(
 	'bug_link_tag is not blank/null',
 	config_get_global( 'bug_link_tag' ),
