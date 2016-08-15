06d0c7a2a28f79e9e6482cb824642a2af314c8ee
Carlos Proensa <proensa@gmail.com>
2016-08-16 01:27:25 +0200

    Remove unused include in excel_xml_export
    The page print_all_bug_options_inc.php is no longer used, as stated in
    Issue #19370

--- excel_xml_export.php
+++ excel_xml_export.php
@@ -52,9 +52,6 @@ require_api( 'helper_api.php' );
 require_api( 'print_api.php' );
 require_api( 'utility_api.php' );
 
-define( 'PRINT_ALL_BUG_OPTIONS_INC_ALLOW', true );
-include( dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'print_all_bug_options_inc.php' );
-
 auth_ensure_user_authenticated();
 
 $f_export = gpc_get_string( 'export', '' );
