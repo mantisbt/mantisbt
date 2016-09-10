7f63ed5ab499a21158592eb9b1d9101bd197afd6
Damien Regad <dregad@mantisbt.org>
2016-09-10 10:40:02 +0200

    Show realname in breadcrumbs if $g_show_realname = ON
    Prior to this, the username would always be displayed, regardless of
    $g_show_realname setting.
    
    Fixes #21681

--- core/layout_api.php
+++ core/layout_api.php
@@ -1000,7 +1000,7 @@ function layout_page_content_end() {
  * @return null
  */
 function layout_breadcrumbs() {
-	$t_username = current_user_get_field( 'username' );
+	$t_username = user_get_name( auth_get_current_user_id() );
 	$t_protected = current_user_get_field( 'protected' );
 	$t_access_level = get_enum_element( 'access_levels', current_user_get_access_level() );
 
