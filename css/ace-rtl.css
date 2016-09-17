a432c0b55c81f731f16c265cebfbc862e6e292d5
Rafik Robeal <rafik@mantishub.net>
2016-09-17 09:16:08 -0700

    Add author info to the license header
    Fixes #21603

--- css/ace-rtl.css
+++ css/ace-rtl.css
@@ -1,4 +1,7 @@
 /*
+ Ace Admin Theme v1.4
+ Copyright (c) 2016 Mohsen - (twitter.com/responsiweb)
+
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
