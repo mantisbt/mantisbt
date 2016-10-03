a5a945835320ae3c5d2ea15ff753dbe426e9fa57
Victor Boctor <victor@mantishub.net>
2016-10-03 04:44:54 +0000

    Add bug activity API + refactor

--- bugnote_view_inc.php
+++ bugnote_view_inc.php
@@ -48,6 +48,7 @@ if( !defined( 'BUGNOTE_VIEW_INC_ALLOW' ) ) {
 
 require_api( 'access_api.php' );
 require_api( 'authentication_api.php' );
+require_api( 'bug_activity_api.php' );
 require_api( 'bug_api.php' );
 require_api( 'bug_revision_api.php' );
 require_api( 'bugnote_api.php' );
@@ -64,21 +65,9 @@ require_api( 'print_api.php' );
 require_api( 'string_api.php' );
 require_api( 'user_api.php' );
 
-define( 'ENTRY_TYPE_NOTE', 'note' );
-define( 'ENTRY_TYPE_ATTACHMENT', 'attachment' );
-
-# grab the user id currently logged in
-$t_user_id = auth_get_current_user_id();
-
-# grab bug information
-$t_bug_readonly = bug_is_readonly( $f_bug_id );
-
 #precache access levels
 access_cache_matrix_project( helper_get_current_project() );
 
-# get the bugnote data
-$t_bugnote_order = current_user_get_pref( 'bugnote_order' );
-$t_bugnotes = bugnote_get_all_visible_bugnotes( $f_bug_id, $t_bugnote_order, 0, $t_user_id );
 $t_show_time_tracking = access_has_bug_level( config_get( 'time_tracking_view_threshold' ), $f_bug_id );
 
 # get attachments data
@@ -86,192 +75,20 @@ $t_fields = config_get( $t_fields_config_option );
 $t_fields = columns_filter_disabled( $t_fields );
 $t_show_attachments = in_array( 'attachments', $t_fields );
 
-if ( $t_show_attachments ) {
-	$t_attachments = file_get_visible_attachments( $f_bug_id );
-} else {
-	$t_attachments = array();
-}
-
-if( count( $t_bugnotes ) > 0 || count( $t_attachments ) > 0 ) {
-	# access level thresholds
-	$t_bugnote_user_edit_threshold = config_get( 'bugnote_user_edit_threshold' );
-	$t_bugnote_user_delete_threshold = config_get( 'bugnote_user_delete_threshold' );
-	$t_bugnote_user_change_view_state_threshold = config_get( 'bugnote_user_change_view_state_threshold' );
-	$t_can_edit_all_bugnotes = access_has_bug_level( config_get( 'update_bugnote_threshold' ), $f_bug_id );
-	$t_can_delete_all_bugnotes = access_has_bug_level( config_get( 'delete_bugnote_threshold' ), $f_bug_id );
-	$t_can_change_view_state_all_bugnotes = $t_can_edit_all_bugnotes && access_has_bug_level( config_get( 'change_view_status_threshold' ), $f_bug_id );
-}
-
-$t_entries = array();
-
-foreach( $t_attachments as $t_attachment ) {
-	$t_entry = array(
-		'type' => ENTRY_TYPE_ATTACHMENT,
-		'timestamp' => $t_attachment['date_added'],
-		'modified' => false,
-		'last_modified' => $t_attachment['date_added'],
-		'id' => $t_attachment['id'],
-		'id_formatted' => $t_attachment['id'],
-		'user_id' => $t_attachment['user_id'],
-		'private' => false,
-		'style' => 'bugnote-note',
-		'attachment' => $t_attachment );
-
-	$t_entry['can_edit'] = false;
-	$t_entry['can_delete'] = !$t_bug_readonly && $t_attachment['can_delete'];
-	$t_entry['can_change_view_state'] = false;
-
-	$t_entries[] = $t_entry;
-}
-
-foreach( $t_bugnotes as $t_bugnote ) {
-	$t_entry = array(
-		'type' => ENTRY_TYPE_NOTE,
-		'timestamp' => $t_bugnote->date_submitted,
-		'last_modified' => $t_bugnote->last_modified,
-		'modified' => $t_bugnote->date_submitted != $t_bugnote->last_modified,
-		'id' => $t_bugnote->id,
-		'id_formatted' => bugnote_format_id( $t_bugnote->id ),
-		'user_id' => $t_bugnote->reporter_id,
-		'private' => $t_bugnote->view_state != VS_PUBLIC,
-		'style' => 'bugnote-note',
-		'attachments' => array(),
-		'note' => $t_bugnote );
-
-	if( $t_entry['private'] ) {
-		$t_entry['style'] .= ' bugnote-private';
-	} else {
-		$t_entry['style'] .= ' bugnote-public';
-	}
-
-	if( TIME_TRACKING == $t_entry['note']->note_type ) {
-		$t_entry['style'] .= ' bugnote-time-tracking';
-	} else if( REMINDER == $t_entry['note']->note_type ) {
-		$t_entry['style'] .= ' bugnote-reminder';
-	}
-
-	if( $t_bug_readonly ) {
-		$t_can_edit_bugnote = false;
-		$t_can_delete_bugnote = false;
-		$t_can_change_view_state = false;
-	} else {
-		# check if the user can edit this bugnote
-		if( $t_user_id == $t_entry['user_id'] ) {
-			$t_can_edit_bugnote = access_has_bugnote_level( $t_bugnote_user_edit_threshold, $t_entry['id'] );
-		} else {
-			$t_can_edit_bugnote = $t_can_edit_all_bugnotes;
-		}
-
-		# check if the user can delete this bugnote
-		if( $t_user_id == $t_entry['user_id'] ) {
-			$t_can_delete_bugnote = access_has_bugnote_level( $t_bugnote_user_delete_threshold, $t_entry['id'] );
-		} else {
-			$t_can_delete_bugnote = $t_can_delete_all_bugnotes;
-		}
-
-		# check if the user can make this bugnote private
-		if( $t_user_id == $t_entry['user_id'] ) {
-			$t_can_change_view_state = access_has_bugnote_level( $t_bugnote_user_change_view_state_threshold, $t_entry['id'] );
-		} else {
-			$t_can_change_view_state = $t_can_change_view_state_all_bugnotes;
-		}
-	}
-
-	$t_entry['can_edit'] = $t_can_edit_bugnote;
-	$t_entry['can_delete'] = $t_can_delete_bugnote;
-	$t_entry['can_change_view_state'] = $t_can_change_view_state;
-
-	$t_entries[] = $t_entry;
-}
-
-/**
- * Sort bugnotes and attachments by timestamp then user_id.  If two entries have
- * the same timestamp and user id, then the note should be before the attachment.
- *
- * @param array $p_entries The array of entries.  The array will be updated.
- * @return void
- */
-function entries_sort( &$p_entries ) {
-	$t_order = config_get( 'bugnote_order' );
-	usort( $p_entries, function( $a, $b ) use( $t_order ) {
-		if( $a['timestamp'] < $b['timestamp'] ) {
-			return $t_order == 'DESC' ? 1 : -1;
-		}
-
-		if( $a['timestamp'] > $b['timestamp'] ) {
-			return $t_order == 'DESC' ? -1 : 1;
-		}
-
-		if( $a['user_id'] < $b['user_id'] ) {
-			return -1;
-		}
-
-		if( $a['user_id'] > $b['user_id'] ) {
-			return 1;
-		}
-
-		if( $a['type'] == ENTRY_TYPE_NOTE && $b['type'] == ENTRY_TYPE_ATTACHMENT ) {
-			return -1;
-		}
-
-		if( $a['type'] == ENTRY_TYPE_ATTACHMENT && $b['type'] == ENTRY_TYPE_NOTE ) {
-			return 1;
-		}
-
-		return 0;
-	} );
-}
-
-/**
- * Combine entries that were submitted together in one entry.  A user can
- * submit N attachments along with a note.  In such case, we want to have
- * a single entry that shows the note followed by the attachments.
- *
- * @param array $p_entries The array of entries.
- * @return The updated array of entries.
- */
-function entries_combine( $p_entries ) {
-	define( 'TIMESPAN_TO_COMBINE_ATTACHMENTS_IN_SECS', 10 );
-
-	$t_combined_entries = array();
-	$t_last_entry = null;
-
-	foreach( $p_entries as $t_entry ) {
-		if( $t_last_entry != null ) {
-			if( $t_last_entry['user_id'] == $t_entry['user_id'] &&
-			    $t_last_entry['type'] == ENTRY_TYPE_NOTE &&
-			    $t_entry['type'] == ENTRY_TYPE_ATTACHMENT &&
-			    abs( $t_entry['timestamp'] - $t_last_entry['timestamp'] ) <= TIMESPAN_TO_COMBINE_ATTACHMENTS_IN_SECS ) {
-			    $t_last_entry['attachments'][] = $t_entry['attachment'];
-			} else {
-				$t_combined_entries[] = $t_last_entry;
-				$t_last_entry = $t_entry;
-			}
-		} else {
-			$t_last_entry = $t_entry;
-		}
-	}
-
-	if( $t_last_entry !== null ) {
-		$t_combined_entries[] = $t_last_entry;
-	}
-
-	return $t_combined_entries;
-}
-
-entries_sort( $t_entries );
-$t_entries = entries_combine( $t_entries );
+$t_result = bug_activity_get_all( $f_bug_id, /* include_attachments */ $t_show_attachments );
+$t_activities = $t_result['activities'];
+$t_bugnotes = $t_result['bugnotes'];
 
 # Pre-cache users
 $t_users_to_cache = array();
 
-foreach( $t_entries as $t_entry ) {
-	$t_users_to_cache[$t_entry['user_id']] = true;
+foreach( $t_activities as $t_activity ) {
+	$t_users_to_cache[$t_activity['user_id']] = true;
 }
 
 user_cache_array_rows( array_keys( $t_users_to_cache ) );
 
-$t_num_entries = count( $t_entries );
+$t_activities_count = count( $t_activities );
 ?>
 
 <?php # Bugnotes BEGIN ?>
@@ -303,7 +120,7 @@ $t_block_icon = $t_collapse_block ? 'fa-chevron-down' : 'fa-chevron-up';
 	<table class="table table-bordered table-condensed table-striped">
 <?php
 	# no bugnotes
-	if( 0 == $t_num_entries ) {
+	if( 0 == $t_activities_count ) {
 ?>
 <tr class="bugnotes-empty">
 	<td class="center" colspan="2">
@@ -322,57 +139,57 @@ $t_block_icon = $t_collapse_block ? 'fa-chevron-down' : 'fa-chevron-up';
 	$t_security_token_notes_delete = null;
 	$t_security_token_attachments_delete = null;
 
-	for( $i=0; $i < $t_num_entries; $i++ ) {
-		$t_entry = $t_entries[$i];
+	for( $i=0; $i < $t_activities_count; $i++ ) {
+		$t_activity = $t_activities[$i];
 
-		if( $t_entry['type'] == ENTRY_TYPE_NOTE && $t_entry['note']->time_tracking != 0 ) {
-			$t_time_tracking_hhmm = db_minutes_to_hhmm( $t_bugnote->time_tracking );
-			$t_total_time += $t_entry['note']->time_tracking;
+		if( $t_activity['type'] == ENTRY_TYPE_NOTE && $t_activity['note']->time_tracking != 0 ) {
+			$t_time_tracking_hhmm = db_minutes_to_hhmm( $t_activity['note']->time_tracking );
+			$t_total_time += $t_activity['note']->time_tracking;
 		} else {
 			$t_time_tracking_hhmm = '';
 		}
 ?>
-<tr class="bugnote" id="c<?php echo $t_entry['id'] ?>">
+<tr class="bugnote" id="c<?php echo $t_activity['id'] ?>">
 		<td class="category">
-		<div class="pull-left padding-2"><?php print_avatar( $t_entry['user_id'] ); ?>
+		<div class="pull-left padding-2"><?php print_avatar( $t_activity['user_id'] ); ?>
 		</div>
 		<div class="pull-left padding-2">
 		<p class="no-margin">
 			<?php
 			echo '<i class="fa fa-user grey"></i> ';
-			print_user( $t_entry['user_id'] );
+			print_user( $t_activity['user_id'] );
 			?>
 		</p>
 		<p class="no-margin small lighter">
-			<i class="fa fa-clock-o grey"></i> <?php echo date( $t_normal_date_format, $t_entry['timestamp'] ); ?>
-			<?php if( $t_entry['private'] ) { ?>
+			<i class="fa fa-clock-o grey"></i> <?php echo date( $t_normal_date_format, $t_activity['timestamp'] ); ?>
+			<?php if( $t_activity['private'] ) { ?>
 				&#160;&#160;
 				<i class="fa fa-eye red"></i> <?php echo lang_get( 'private' ) ?>
 			<?php } ?>
 		</p>
 		<p class="no-margin">
 			<?php
-			if( user_exists( $t_entry['user_id'] ) ) {
-				$t_access_level = access_get_project_level( null, $t_entry['user_id'] );
+			if( user_exists( $t_activity['user_id'] ) ) {
+				$t_access_level = access_get_project_level( null, $t_activity['user_id'] );
 				$t_label = layout_is_rtl() ? 'arrowed-right' : 'arrowed-in-right';
 				echo '<span class="label label-sm label-default ' . $t_label . '">', access_level_get_string( $t_access_level ), '</span>';
 			}
 			?>
 			&#160;
-			<?php if( $t_entry['type'] == ENTRY_TYPE_NOTE ) { ?>
+			<?php if( $t_activity['type'] == ENTRY_TYPE_NOTE ) { ?>
 			<i class="fa fa-link grey"></i>
-			<a rel="bookmark" href="<?php echo string_get_bugnote_view_url( $t_entry['note']->bug_id, $t_entry['note']->id) ?>" class="lighter" title="<?php echo lang_get( 'bugnote_link_title' ) ?>">
-				<?php echo htmlentities( config_get_global( 'bugnote_link_tag' ) ) . $t_entry['id_formatted'] ?>
+			<a rel="bookmark" href="<?php echo string_get_bugnote_view_url( $t_activity['note']->bug_id, $t_activity['note']->id) ?>" class="lighter" title="<?php echo lang_get( 'bugnote_link_title' ) ?>">
+				<?php echo htmlentities( config_get_global( 'bugnote_link_tag' ) ) . $t_activity['id_formatted'] ?>
 			</a>
 			<?php } ?>
 		</p>
 		<?php
-		if( $t_entry['modified'] ) {
-			echo '<p class="no-margin small lighter"><i class="fa fa-retweet"></i> ' . lang_get( 'last_edited') . lang_get( 'word_separator' ) . date( $t_normal_date_format, $t_entry['last_modified'] ) . '</p>';
-			$t_revision_count = bug_revision_count( $f_bug_id, REV_BUGNOTE, $t_entry['id'] );
+		if( $t_activity['modified'] ) {
+			echo '<p class="no-margin small lighter"><i class="fa fa-retweet"></i> ' . lang_get( 'last_edited') . lang_get( 'word_separator' ) . date( $t_normal_date_format, $t_activity['last_modified'] ) . '</p>';
+			$t_revision_count = bug_revision_count( $f_bug_id, REV_BUGNOTE, $t_activity['id'] );
 			if( $t_revision_count >= 1 ) {
 				$t_view_num_revisions_text = sprintf( lang_get( 'view_num_revisions' ), $t_revision_count );
-				echo '<p class="no-margin"><span class="small bugnote-revisions-link"><a href="bug_revision_view_page.php?bugnote_id=' . $t_entry['id'] . '">' . $t_view_num_revisions_text . '</a></span></p>';
+				echo '<p class="no-margin"><span class="small bugnote-revisions-link"><a href="bug_revision_view_page.php?bugnote_id=' . $t_activity['id'] . '">' . $t_view_num_revisions_text . '</a></span></p>';
 			}
 		}
 		?>
@@ -381,21 +198,21 @@ $t_block_icon = $t_collapse_block ? 'fa-chevron-down' : 'fa-chevron-up';
 		<div class="btn-group-sm">
 		<?php
 			# show edit button if the user is allowed to edit this bugnote
-			if( $t_entry['can_edit'] ) {
+			if( $t_activity['can_edit'] ) {
 				echo '<div class="pull-left">';
 				print_form_button(
 					'bugnote_edit_page.php',
 					lang_get( 'bugnote_edit_link' ),
-					array( 'bugnote_id' => $t_entry['id'] ),
+					array( 'bugnote_id' => $t_activity['id'] ),
 					OFF );
 				echo '</div>';
 			}
 
 			# show delete button if the user is allowed to delete this bugnote
-			if( $t_entry['can_delete'] ) {
+			if( $t_activity['can_delete'] ) {
 				echo '<div class="pull-left">';
 
-				if( $t_entry['type'] == ENTRY_TYPE_NOTE ) {
+				if( $t_activity['type'] == ENTRY_TYPE_NOTE ) {
 					if ( !$t_security_token_notes_delete ) {
 						$t_security_token_notes_delete = form_security_token( 'bugnote_delete' );
 					}
@@ -403,15 +220,15 @@ $t_block_icon = $t_collapse_block ? 'fa-chevron-down' : 'fa-chevron-up';
 					print_form_button(
 						'bugnote_delete.php',
 						lang_get( 'delete_link' ),
-						array( 'bugnote_id' => $t_entry['id'] ),
+						array( 'bugnote_id' => $t_activity['id'] ),
 						$t_security_token_notes_delete );
 				} else {
 					if ( !$t_security_token_attachments_delete ) {
 						$t_security_token_attachments_delete = form_security_token( 'bug_file_delete' );
 					}
 
-					if( $t_entry['can_delete'] ) {
-						print_button( 'bug_file_delete.php?file_id=' . $t_entry['id'] . form_security_param( 'bug_file_delete', $t_security_token_attachments_delete ),
+					if( $t_activity['can_delete'] ) {
+						print_button( 'bug_file_delete.php?file_id=' . $t_activity['id'] . form_security_param( 'bug_file_delete', $t_security_token_attachments_delete ),
 							lang_get( 'delete_link' ), 'btn-xs' );
 					}
 				}
@@ -420,23 +237,23 @@ $t_block_icon = $t_collapse_block ? 'fa-chevron-down' : 'fa-chevron-up';
 			}
 
 			# show make public or make private button if the user is allowed to change the view state of this bugnote
-			if( $t_entry['can_change_view_state'] ) {
+			if( $t_activity['can_change_view_state'] ) {
 				if ( !$t_security_token_state ) {
 					$t_security_token_state = form_security_token( 'bugnote_set_view_state' );
 				}
 
 				echo '<div class="pull-left">';
-				if( $t_entry['private'] ) {
+				if( $t_activity['private'] ) {
 					print_form_button(
 						'bugnote_set_view_state.php',
 						lang_get( 'make_public' ),
-						array( 'private' => '0', 'bugnote_id' => $t_entry['id'] ),
+						array( 'private' => '0', 'bugnote_id' => $t_activity['id'] ),
 						$t_security_token_state );
 				} else {
 					print_form_button(
 						'bugnote_set_view_state.php',
 						lang_get( 'make_private' ),
-						array( 'private' => '1', 'bugnote_id' => $t_entry['id'] ),
+						array( 'private' => '1', 'bugnote_id' => $t_activity['id'] ),
 						$t_security_token_state );
 				}
 				echo '</div>';
@@ -445,21 +262,21 @@ $t_block_icon = $t_collapse_block ? 'fa-chevron-down' : 'fa-chevron-up';
 		</div>
 		</div>
 	</td>
-	<td class="<?php echo $t_entry['style'] ?>">
+	<td class="<?php echo $t_activity['style'] ?>">
 	<?php
-		if( $t_entry['type'] == ENTRY_TYPE_NOTE ) {
-			switch ( $t_entry['note']->note_type ) {
+		if( $t_activity['type'] == ENTRY_TYPE_NOTE ) {
+			switch ( $t_activity['note']->note_type ) {
 				case REMINDER:
 					echo '<strong>';
 
 					# List of recipients; remove surrounding delimiters
-					$t_recipients = trim( $t_entry['note']->note_attr, '|' );
+					$t_recipients = trim( $t_activity['note']->note_attr, '|' );
 
 					if( empty( $t_recipients ) ) {
 						echo lang_get( 'reminder_sent_none' );
 					} else {
 						# If recipients list's last char is not a delimiter, it was truncated
-						$t_truncated = ( '|' != utf8_substr( $t_entry['note']->note_attr, utf8_strlen( $t_entry['note']->note_attr ) - 1 ) );
+						$t_truncated = ( '|' != utf8_substr( $t_activity['note']->note_attr, utf8_strlen( $t_activity['note']->note_attr ) - 1 ) );
 
 						# Build recipients list for display
 						$t_to = array();
@@ -483,23 +300,23 @@ $t_block_icon = $t_collapse_block ? 'fa-chevron-down' : 'fa-chevron-up';
 					break;
 			}
 
-			echo string_display_links( $t_entry['note']->note );
+			echo string_display_links( $t_activity['note']->note );
 
-			if( isset( $t_entry['attachments'] ) && count( $t_entry['attachments'] ) > 0 ) {
+			if( isset( $t_activity['attachments'] ) && count( $t_activity['attachments'] ) > 0 ) {
 				echo '<br /><br />';
-				foreach( $t_entry['attachments'] as $t_attachment ) {
+				foreach( $t_activity['attachments'] as $t_attachment ) {
 					print_bug_attachment( $t_attachment );
 				}
 			}
 		} else {
-			print_bug_attachment( $t_entry['attachment'] );
+			print_bug_attachment( $t_activity['attachment'] );
 		}
 	?>
 	</td>
 </tr>
 <?php
-if( $t_entry['type'] == ENTRY_TYPE_NOTE ) {
-	event_signal( 'EVENT_VIEW_BUGNOTE', array( $f_bug_id, $t_entry['id'], $t_entry['private'] ) );
+if( $t_activity['type'] == ENTRY_TYPE_NOTE ) {
+	event_signal( 'EVENT_VIEW_BUGNOTE', array( $f_bug_id, $t_activity['id'], $t_activity['private'] ) );
 }
 ?>
 <tr class="spacer">
