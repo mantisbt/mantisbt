c9dbd7ea64fb2ddf96ec239723a917e6a317fb37
Victor Boctor <victor@mantishub.net>
2016-10-30 20:07:16 -0700

    Update version to 2.0.0-rc.1

--- core/constant_inc.php
+++ core/constant_inc.php
@@ -21,7 +21,7 @@
 /**
  * Mantis Version
  */
-define( 'MANTIS_VERSION', '2.0.0-beta.3' );
+define( 'MANTIS_VERSION', '2.0.0-rc.1' );
 define( 'FILTER_VERSION', 'v9' );
 
 # --- constants -------------------
