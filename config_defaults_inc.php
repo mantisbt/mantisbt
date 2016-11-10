e5952fd244d210c54527aec800f26d0fe9da8826
Victor Boctor <victor@mantishub.net>
2016-11-09 23:01:12 -0800

    Support a config option to use full issue format
    Add a configuration option that can be overriden per user, per project or
    per user + project to opt into full issue (verbose) notifications.
    
    Fixes #21876

--- config_defaults_inc.php
+++ config_defaults_inc.php
@@ -434,6 +434,15 @@ $g_return_path_email	= 'admin@example.com';
  */
 $g_enable_email_notification	= ON;
 
+/**
+ * When enabled, the email notifications will send the full issue with
+ * a hint about the change type at the top, rather than using dedicated
+ * notifications that are focused on what changed.  This change can be
+ * overridden in the database per user.
+ *
+ * @global integer $g_email_notifications_verbose
+ */
+$g_email_notifications_verbose = OFF;
 
 /**
  * The following two config options allow you to control who should get email
@@ -4420,6 +4429,7 @@ $g_public_config_names = array(
 	'due_date_view_threshold',
 	'email_ensure_unique',
 	'email_login_enabled',
+	'email_notifications_verbose',
 	'email_padding_length',
 	'email_receive_own',
 	'email_separator1',
