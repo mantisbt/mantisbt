ab951c31a43078680d4d6d813af6f4d2a976603d
Roland Becker <roland@atrol.de>
2017-01-11 21:41:03 +0100

    Bump the required MantisCore version in bundled plugins

--- plugins/XmlImportExport/XmlImportExport.php
+++ plugins/XmlImportExport/XmlImportExport.php
@@ -38,7 +38,7 @@ class XmlImportExportPlugin extends MantisPlugin {
 
 		$this->version = MANTIS_VERSION;
 		$this->requires = array(
-			'MantisCore' => '2.0.0',
+			'MantisCore' => '2.1.0',
 		);
 
 		$this->author = 'MantisBT Team';
