e597c5742c74da45b69daad1096c374c4e571b68
Carlos Proensa <proensa@gmail.com>
2017-01-29 20:17:23 +0100

    Remove colons from labels in edit filter page
    Remove colons from labels in edit filter form, to be consistent with
    other application forms.

--- manage_filter_edit_page.php
+++ manage_filter_edit_page.php
@@ -161,7 +161,7 @@ $t_filter_project_id = filter_get_field( $f_filter_id, 'project_id' );
 						?>
 						<tr>
 							<td class="category">
-								<?php echo lang_get( 'view_status' ) ?>:
+								<?php echo lang_get( 'view_status' ) ?>
 							</td>
 							<td>
 								<label>
@@ -178,7 +178,7 @@ $t_filter_project_id = filter_get_field( $f_filter_id, 'project_id' );
 						<?php } ?>
 						<tr>
 							<td class="category">
-								<?php echo lang_get( 'email_project' ) ?>:
+								<?php echo lang_get( 'email_project' ) ?>
 							</td>
 							<td>
 								<label class="inline">
