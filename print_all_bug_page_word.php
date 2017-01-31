17d6796a688e676efbbc4de82db7358191923986
Roland Becker <roland@atrol.de>
2017-01-31 21:30:57 +0100

    Add UTF-8 header to Office Word output
    Fixes #21798
    
    Signed-off-by: Damien Regad <dregad@mantisbt.org>
    
    Cherry-picked from 1.3.x (23da4e0c814e6ed5ee1fc51be6d2b5f15866b7ca)
    following incorrect merge conflict resolution in
    75ef07835bc3771c9632ed20aa6512ed9d6a17cc.

--- print_all_bug_page_word.php
+++ print_all_bug_page_word.php
@@ -110,7 +110,8 @@ if( $f_type_page == 'html' ) {
 } else {
 	echo '<html xmlns:o="urn:schemas-microsoft-com:office:office"
 		xmlns:w="urn:schemas-microsoft-com:office:word"
-		xmlns="http://www.w3.org/TR/REC-html40">';
+		xmlns="http://www.w3.org/TR/REC-html40">
+		<head><meta charset="utf-8"></head>';
 	echo '<body>';
 }
 
