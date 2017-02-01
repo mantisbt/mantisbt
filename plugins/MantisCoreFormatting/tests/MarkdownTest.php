1583471d43885ef0c58c8412453cdd5e33e15220
Joel C. Llano <joel@mantishub.com>
2017-02-01 15:26:45 +0800

    Markdown unit tests
    - refactor test suite, ensure to pass from text processing always
    
    Fixes #22231

--- plugins/MantisCoreFormatting/tests/MarkdownTest.php
+++ plugins/MantisCoreFormatting/tests/MarkdownTest.php
@@ -24,7 +24,9 @@
  */
 
 # Includes
-require_once( dirname( dirname( __FILE__ ) ) . '../../../tests/TestConfig.php' );
+//require_once dirname( dirname( __FILE__ ) ) . '/TestConfig.php';
+
+require_once  dirname( dirname( __FILE__ ) ) . '/../../tests/TestConfig.php' ;
 require_once( dirname( dirname( __FILE__ ) ) . '/core/MantisMarkdown.php' );
 
 # MantisBT Core API
@@ -41,113 +43,47 @@ require_mantis_core();
 class MantisMarkdownTest extends PHPUnit_Framework_TestCase {
 
 	/**
-	 * Test If string starts with hash character followed by letters
-	 * @return void
+	 * Mantis Text Processing
+	 *
+	 * @return string Formatted text
 	 */
-	public function testHashLetters() {
-		$this->assertEquals( '<h1>hello</h1>', MantisMarkdown::convert_text( '# hello' ) );
-		$this->assertEquals( '<h1>hello</h1>', MantisMarkdown::convert_text( '#hello' ) );
+	private function processText( $p_string ) {
+		
+		$t_string = $p_string;
+		
+		$t_string = string_strip_hrefs( $t_string );
+		$t_string = string_html_specialchars( $t_string );
+		$t_string = string_restore_valid_html_tags( $t_string, true );
+		
+		return $t_string;
 	}
 
 	/**
-	 * Test If string starts with hash character followed by number and letters
+	 * Test If string starts with hash(#) symbol
+	 *
 	 * @return void
 	 */
-	public function testHashNumberAny() {
-		$this->assertEquals( '<h1>1abcd</h1>', MantisMarkdown::convert_text( '# 1abcd' ) );
-		$this->assertEquals( '<h1>1abcd</h1>', MantisMarkdown::convert_text( '#1abcd' ) );
+	public function testHeader() {
+
+		$t_expected_string = '<h1>hello</h1>';
+		
+		# followed by letter
+		$t_input_string = '#hello';
+		$t_process_string = $this->processText( $t_input_string );
+		$this->assertEquals( $t_expected_string, MantisMarkdown::convert_text( $t_process_string ) );
+
+		# followed by space
+		$t_input_string = '# hello';
+		$t_process_string = $this->processText( $t_input_string );
+		$this->assertEquals( $t_expected_string, MantisMarkdown::convert_text( $t_process_string ) );
+
+		# followed by numeric, markdown must ignore this
+		# this is consider as bug/note link
+		$t_input_string = '#1';
+		
+		$t_process_string = $this->processText( $t_input_string );
+
+		$this->assertEquals( '<p>#1</p>', MantisMarkdown::convert_text( $t_process_string ) );
 	}
-
-	/**
-	 * Test If string starts with hash character followed by letters and numbers
-	 * @return void
-	 */
-	public function testHashLettersAny() {
-		$this->assertEquals( '<h1>abcd1234</h1>', MantisMarkdown::convert_text( '# abcd1234' ) );
-		$this->assertEquals( '<h1>abcd1234</h1>', MantisMarkdown::convert_text( '#abcd1234' ) );
-	}
-
-	/**
-	 * Test If string starts with hash character followed by numbers
-	 * since the class overrides the default Markdown parsing on Header
-	 * then the methods should return the standard text.
-	 * @return void
-	 */
-	public function testHashNumbers() {
-		$this->assertEquals( '<p>#1</p>', MantisMarkdown::convert_text( '#1' ) );
-	}
-
-	/**
-	* Test if table class attribute is defined
-	 * @return void
-	 */
-	public function testTableClassDefined() {
-		$markdown_table = <<<EOD
-| _header_ 1   | header 2     |
-| ------------ | ------------ |
-| _cell_ 1.1   | ~~cell~~ 1.2 |
-| `|` 2.1      | \| 2.2       |
-| `\|` 2.1     | [link](/)    |
-EOD;
-
-		$markdown_table_output = <<<EOD
-<table class="table table-nonfluid">
-<thead>
-<tr>
-<th><em>header</em> 1</th>
-<th>header 2</th>
-</tr>
-</thead>
-<tbody>
-<tr>
-<td><em>cell</em> 1.1</td>
-<td><del>cell</del> 1.2</td>
-</tr>
-<tr>
-<td><code>|</code> 2.1</td>
-<td>| 2.2</td>
-</tr>
-<tr>
-<td><code>\|</code> 2.1</td>
-<td><a href="/">link</a></td>
-</tr>
-</tbody>
-</table>
-EOD;
-
-		$this->assertEquals( $markdown_table_output, MantisMarkdown::convert_text( $markdown_table ) );
-	}
-
-	/**
-	 * Test the quote markdown if style attribute is defined
-	 * @return void
-	 */
-	public function testQuoteStyleAttribute() {
-		$markdown_quote = <<<EOD
-> quote
-
-indented:
-	> quote
-
-no space after `>`:
->quote
-EOD;
-
-		$markdown_quote_output = <<<EOD
-<blockquote style="border-color:#847d7d">
-<p>quote</p>
-</blockquote>
-<p>indented:</p>
-<blockquote style="border-color:#847d7d">
-<p>quote</p>
-</blockquote>
-<p>no space after <code>&gt;</code>:</p>
-<blockquote style="border-color:#847d7d">
-<p>quote</p>
-</blockquote>
-EOD;
-
-		$this->assertEquals( $markdown_quote_output, MantisMarkdown::convert_text( $markdown_quote ) );
-	}
-
+	
 }
