8b1af462881ded7374c1a81cc3a8f16564dbd082
Victor Boctor <vboctor@gmail.com>
2017-02-25 11:46:36 -0800

    Added Swagger meta data and UI
    The swagger will enable tools to generate client libraries for MantisBT.
    It also provides a nice sandbox to test the API from the browser, see
    http://petstore.swagger.io for an example.

--- api/rest/restcore/issues_rest.php
+++ api/rest/restcore/issues_rest.php
@@ -4,23 +4,26 @@ require_once( __DIR__ . '/AuthMiddleware.php' );
 $app->group('/issues', function() use ( $app ) {
 	# Path with URL re-writing: http://.../mantisbt/api/rest/issues/1234
 	# Path without URL re-writing: http://.../mantisbt/api/rest/index.php/issues/1234
-	$app->get('/{id:[0-9]+}', function ($request, $response, $args) {
-		# Username and password below are ignored, since middleware already done the auth.
-		$t_issue = mc_issue_get( /* username */ '', /* password */ '', $args['id'] );
-
-		# Dependency on SoapFault can be removed by refactoring mc_* code.
-		if( $t_issue instanceof SoapFault ) {
-			return $response->withStatus( 404 );
-		}
-
-		# Once we refactor mantisconnect code this step will not be needed.  For now this is
-		# needed since mc_* code emits SOAP structures and not just an array for types like
-		# DateTime.
-		$t_issue = soap2rest_issue( $t_issue );
-
-		return $response->withStatus( 200 )->withJson( $t_issue );
-	});
+	$app->get( '', 'rest_issue_get' );
+	$app->get( '/', 'rest_issue_get' );
 })->add( new AuthMiddleware() );
+
+function rest_issue_get( \Slim\Http\Request $p_request, \Slim\Http\Response $p_response, array $p_args ) {
+	# Username and password below are ignored, since middleware already done the auth.
+	$t_issue = mc_issue_get( /* username */ '', /* password */ '', $p_request->getParam( 'id' ) );
+
+	# Dependency on SoapFault can be removed by refactoring mc_* code.
+	if( $t_issue instanceof SoapFault ) {
+		return $p_response->withStatus( 404 );
+	}
+
+	# Once we refactor mantisconnect code this step will not be needed.  For now this is
+	# needed since mc_* code emits SOAP structures and not just an array for types like
+	# DateTime.
+	$t_issue = soap2rest_issue( $t_issue );
+
+	return $p_response->withStatus( 200 )->withJson( $t_issue );
+}
 
 function soap2rest_issue( $p_issue ) {
 	$t_issue = array(
