aa4137a50b206393a0d6ddcaaec0722b1768be4f
Victor Boctor <vboctor@gmail.com>
2017-02-25 12:30:30 -0800

    Make serialization code REST friendly
    This removes SOAP specific serialization logic in case of REST.

--- api/rest/index.php
+++ api/rest/index.php
@@ -27,6 +27,10 @@ require_once( __DIR__ . '/../../core.php' );
 require_once( __DIR__ . '/../soap/mc_core.php' );
 require_once( __DIR__ . '/restcore/VersionMiddleware.php' );
 
+# Hint to re-used mantisconnect code that it is being executed from REST rather than SOAP.
+# For example, this will disable logic like encoding dates with XSD meta-data.
+SoapObjectsFactory::$soap = false;
+
 $app = new \Slim\App();
 
 $app->add( new VersionMiddleware() );
