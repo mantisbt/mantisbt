9126d6165fe06fee3cb800c86d6b5e6478f6f96f
Victor Boctor <vboctor@gmail.com>
2017-03-01 00:02:33 -0800

    Adding headers plus phpdocs

--- api/rest/restcore/AuthMiddleware.php
+++ api/rest/restcore/AuthMiddleware.php
@@ -1,6 +1,33 @@
 <?php
+# MantisBT - A PHP based bugtracking system
+
+# MantisBT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# MantisBT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * A webservice interface to Mantis Bug Tracker
+ *
+ * @package MantisBT
+ * @copyright Copyright 2004  Victor Boctor - vboctor@users.sourceforge.net
+ * @copyright Copyright 2005  MantisBT Team - mantisbt-dev@lists.sourceforge.net
+ * @link http://www.mantisbt.org
+ */
+
 require_api( 'authentication_api.php' );
 
+/**
+ * A middleware class that handles authentication and authorization to access APIs.
+ */
 class AuthMiddleware {
 	public function __invoke( \Slim\Http\Request $request, \Slim\Http\Response $response, callable $next ) {
 		if( mci_is_mantis_offline() ) {
--- api/rest/restcore/issues_rest.php
+++ api/rest/restcore/issues_rest.php
@@ -1,4 +1,28 @@
 <?php
+# MantisBT - A PHP based bugtracking system
+
+# MantisBT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# MantisBT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * A webservice interface to Mantis Bug Tracker
+ *
+ * @package MantisBT
+ * @copyright Copyright 2004  Victor Boctor - vboctor@users.sourceforge.net
+ * @copyright Copyright 2005  MantisBT Team - mantisbt-dev@lists.sourceforge.net
+ * @link http://www.mantisbt.org
+ */
+
 require_once( __DIR__ . '/AuthMiddleware.php' );
 
 $app->group('/issues', function() use ( $app ) {
@@ -8,6 +32,14 @@ $app->group('/issues', function() use ( $app ) {
 	$app->get( '/', 'rest_issue_get' );
 })->add( new AuthMiddleware() );
 
+/**
+ * A method that does the work to handle getting an issue via REST API.
+ *
+ * @param \Slim\Http\Request $p_request   The request.
+ * @param \Slim\Http\Response $p_response The response.
+ * @param array $p_args Arguments
+ * @return \Slim\Http\Response The augmented response.
+ */
 function rest_issue_get( \Slim\Http\Request $p_request, \Slim\Http\Response $p_response, array $p_args ) {
 	# Username and password below are ignored, since middleware already done the auth.
 	$t_result = mc_issue_get( /* username */ '', /* password */ '', $p_request->getParam( 'id' ) );
