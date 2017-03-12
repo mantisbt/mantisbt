mantisbt/mantisbt

commit 42718557933d1e58fc134ef3bea7db97aeb00c65

Show "GOOD" status for installer DB connect checks
Since removal of DB2 support (see issue #0021140), the installer does
not display "GOOD" status anymore for Admin and User DB connection
checks.

This also improves the logic for the Admin account connection check, we
now capture a "BAD" status if the Admin can connect to the DB, but does
not have access to the database.

Fixes #22400

admin/install.php
@@ -421,16 +421,18 @@ if( 2 == $t_install_state ) {
 	$t_result = @$g_db->Connect( $f_hostname, $f_admin_username, $f_admin_password );
 
 	if( $t_result ) {
-
 		# check if db exists for the admin
-		$t_result = @$g_db->Connect( $f_hostname, $f_admin_username, $f_admin_password, $f_database_name );
-		if( $t_result ) {
-			$t_db_open = true;
-			$f_db_exists = true;
-		}
+		$t_result = @$g_db->Connect( $f_hostname, $f_admin_username, $f_admin_password, $f_database_name.'x' );
+	}
+	if( $t_result ) {
+		$t_db_open = true;
+		$f_db_exists = true;
+
 		# due to a bug in ADODB, this call prompts warnings, hence the @
 		# the check only works on mysql if the database is open
 		$t_version_info = @$g_db->ServerInfo();
+
+		print_test_result( GOOD );
 	} else {
 		print_test_result( BAD, true, 'Does administrative user have access to the database? ( ' . db_error_msg() . ' )' );
 		$t_version_info = null;
@@ -450,6 +452,7 @@ if( 2 == $t_install_state ) {
 
 		if( $t_result == true ) {
 			$t_db_open = true;
+			print_test_result( GOOD );
 		} else {
 			print_test_result( BAD, false, 'Database user doesn\'t have access to the database ( ' . db_error_msg() . ' )' );
 		}

commit cc19b5549366b0f1a3bfacb882a4f2b5eeb593a1

Install: fix db connection as admin check
Commit 42718557933d1e58fc134ef3bea7db97aeb00c65 (issue #22400)
introduced a regression in the installer, causing the 'Attempting to
connect to database as admin' check to fail for new installations, even
though the Admin user ID and password are correct.

Fixes #22510

admin/install.php
@@ -408,16 +408,16 @@ if( 2 == $t_install_state ) {
 	$t_result = @$g_db->Connect( $f_hostname, $f_admin_username, $f_admin_password );
 
 	if( $t_result ) {
-		# check if db exists for the admin
-		$t_result = @$g_db->Connect( $f_hostname, $f_admin_username, $f_admin_password, $f_database_name );
-	}
-	if( $t_result ) {
-		$t_db_open = true;
-		$f_db_exists = true;
-
 		# due to a bug in ADODB, this call prompts warnings, hence the @
 		# the check only works on mysql if the database is open
 		$t_version_info = @$g_db->ServerInfo();
+
+		# check if db exists for the admin
+		$t_result = @$g_db->Connect( $f_hostname, $f_admin_username, $f_admin_password, $f_database_name );
+		if( $t_result ) {
+			$t_db_open = true;
+			$f_db_exists = true;
+		}
 
 		print_test_result( GOOD );
 	} else {