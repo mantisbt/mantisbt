6a90fc83189b68dc29a0e6c899b1f7f96727022d
Roland Becker <roland@atrol.de>
2017-03-13 16:03:03 +0100

    Enhance layout of "Updating Issue Information"
    Fixes #22505

--- bug_update_page.php
+++ bug_update_page.php
@@ -269,13 +269,13 @@ if( $t_show_id || $t_show_project || $t_show_category || $t_show_view_state || $
 }
 
 #
-# Reporter
+# Reporter, Assigned To, Due Date
 #
 
-if( $t_show_reporter ) {
+if( $t_show_reporter || $t_show_handler || $t_show_due_date ) {
 	echo '<tr>';
 
-	$t_spacer = 4;
+	$t_spacer = 0;
 
 	if( $t_show_reporter ) {
 		# Reporter
@@ -285,7 +285,7 @@ if( $t_show_reporter ) {
 		# Do not allow the bug's reporter to edit the Reporter field
 		# when limit_reporters is ON
 		if( ON == config_get( 'limit_reporters' )
-		&&  !access_has_project_level( access_threshold_min_level( config_get( 'report_bug_threshold', null, null, $t_bug->project_id ) ) + 1, $t_bug->project_id )
+			&&  !access_has_project_level( access_threshold_min_level( config_get( 'report_bug_threshold', null, null, $t_bug->project_id ) ) + 1, $t_bug->project_id )
 		) {
 			echo string_attribute( user_get_name( $t_bug->reporter_id ) );
 		} else {
@@ -302,21 +302,6 @@ if( $t_show_reporter ) {
 	} else {
 		$t_spacer += 2;
 	}
-
-	# spacer
-	echo '<td colspan="', $t_spacer, '">&#160;</td>';
-
-	echo '</tr>';
-}
-
-#
-# Assigned To, Due Date
-#
-
-if( $t_show_handler || $t_show_due_date ) {
-	echo '<tr>';
-
-	$t_spacer = 0;
 
 	if ( $t_show_handler ) {
 		# Assigned To
