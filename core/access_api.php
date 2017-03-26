4195c59c6677d21c523c393fdb6c6d46c3da2028
Victor Boctor <victor@mantishub.net>
2017-03-25 21:03:52 -0700

    Use auth flags for reauthentication
    Fixes #4235

--- core/access_api.php
+++ core/access_api.php
@@ -89,7 +89,7 @@ function access_denied() {
 				}
 				$t_return_page = string_url( string_sanitize_url( $t_return_page ) );
 				echo '<p class="center">' . error_string( ERROR_ACCESS_DENIED ) . '</p><p class="center">';
-				print_link_button( helper_mantis_url( 'login_page.php' ) . '?return=' . $t_return_page, lang_get( 'click_to_login' ) );
+				print_link_button( auth_login_page( 'return=' . $t_return_page ), lang_get( 'click_to_login' ) );
 				echo '</p><p class="center">';
 				print_link_button(
 					helper_mantis_url( config_get( 'default_home_page' ) ),
