cc40d19bd164ed97ca7ec83e07ed6f02ca4ab69c
Victor Boctor <victor@mantishub.net>
2017-03-29 08:02:08 -0700

    Add DEPRECATED log msg when xmlhttprequest is used
    Fixes #22593

--- core/constant_inc.php
+++ core/constant_inc.php
@@ -545,7 +545,7 @@ define( 'LOG_NONE', 0 );            # no logging
 define( 'LOG_EMAIL', 1 );           # all emails sent
 define( 'LOG_EMAIL_RECIPIENT', 2 ); # details of email recipient determination
 define( 'LOG_FILTERING', 4 );       # logging for filtering.
-define( 'LOG_AJAX', 8 );            # logging for AJAX / XmlHttpRequests
+define( 'LOG_AJAX', 8 );            # logging for AJAX
 define( 'LOG_LDAP', 16 );           # logging for LDAP
 define( 'LOG_DATABASE', 32 );       # logging for Database
 define( 'LOG_WEBSERVICE', 64 );     # logging for Web Service Requests
