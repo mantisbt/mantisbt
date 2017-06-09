mantisbt/mantisbt

commit 624f9057798a010b15ca174e11b85ea0136637f1

Install: fix layout of check and options sections
Incorrect closing of tables and divs within the various conditional
blocks caused the title of the 'Installation Options' section to be
displayed as a "sub-table" under the 'Checking Installation' section,
followed by the actual install checks and finally the list of
installation options.

This commit moves the closing tags as appropriate, so that
- checks are listed as a table under 'Checking Installation' section
- 'Installation Options' is displayed as an independent section below
  the checks

Fixes #22850

admin/install.php
@@ -493,24 +493,26 @@ if( 2 == $t_install_state ) {
 		);
 ?>
 </tr>
-</table>
-<?php
-	}?>
-</table>
-</div>
-</div>
-</div>
-</div>
-</div>
 
 <?php
-	if( false == $g_failed ) {
-		$t_install_state++;
-	} else {
-		$t_install_state--; # a check failed, redisplay the questions
-	}
+		if( false == $g_failed ) {
+			$t_install_state++;
+		} else {
+			$t_install_state--; # a check failed, redisplay the questions
+		}
+	} # end if db open
 } # end 2 == $t_install_state
+?>
 
+</table>
+</table>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<?php
 # system checks have passed, get the database information
 if( 1 == $t_install_state ) {
 	?>

commit 0de67c37af9f4aa116721cda0e5c0b993ca9864d

Fix installer blocked at stage 2
Commit 624f9057798a010b15ca174e11b85ea0136637f1 introduced a regression
causing the installer to stop at stage 2, i.e. the database was never
created.

Fixes #22985

Signed-off-by: Damien Regad <dregad@mantisbt.org>

Rewrote original commit's message, correct author's e-mail.

admin/install.php
@@ -495,12 +495,12 @@ if( 2 == $t_install_state ) {
 </tr>
 
 <?php
-		if( false == $g_failed ) {
-			$t_install_state++;
-		} else {
-			$t_install_state--; # a check failed, redisplay the questions
-		}
 	} # end if db open
+	if( false == $g_failed ) {
+		$t_install_state++;
+	} else {
+		$t_install_state--; # a check failed, redisplay the questions
+	}
 } # end 2 == $t_install_state
 ?>
 