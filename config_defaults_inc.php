7f1d037b5385a87046bc395dc3817811400443cd
lucentminds <scott@lifecorp.com>
2017-07-25 19:36:52 -0500

    Fixed #23157: phpMailer SMTP Verify Peer

--- config_defaults_inc.php
+++ config_defaults_inc.php
@@ -680,6 +680,14 @@ $g_smtp_connection_mode = '';
  */
 $g_smtp_port = 25;
 
+/** 
+ * When sending e-mail via ssl or tls, the mail server you are sending to may be
+ * using a self-signed certificate. If so then you must set this value to false
+ * otherwise mantis will silently fail to send your e-mails.
+ * @global bool $g_smtp_ssl_verify_peer
+ */
+$g_smtp_ssl_verify_peer = true;
+
 /**
  * It is recommended to use a cronjob or a scheduler task to send emails. The
  * cronjob should typically run every 5 minutes.  If no cronjob is used,then
