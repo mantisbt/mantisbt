c57a735d513b0e36e41610d180791b08d3ea6488
Damien Regad <dregad@mantisbt.org>
2017-08-10 08:43:28 +0200

    XHR: send HTTP 400 error for invalid entry point
    Returning an HTTP error code from xmlhttprequest back to the caller
    instead of just outputting some arbitrary text message ensures that the
    client's call actually fails, without relying on the failure being
    caused by mismatch in output type, or detecting the error by parsing the
    output.
    
    HTTP 400 (Bad Request) error code was chosen following discussion in
    pull request https://github.com/mantisbt/mantisbt/pull/1139.
    
    Fixes #23184

--- xmlhttprequest.php
+++ xmlhttprequest.php
@@ -43,6 +43,12 @@ if( function_exists( $t_function ) ) {
 	log_event( LOG_AJAX, 'DEPRECATED: Calling {' . $t_function . '}. Use REST API instead.' );
 	call_user_func( $t_function );
 } else {
-	log_event( LOG_AJAX, 'Unknown function for entry point = ' . $t_function );
-	echo 'unknown entry point';
+	$t_msg = 'Unknown function for entry point: ' . $t_function;
+	log_event( LOG_AJAX, $t_msg );
+	header(
+		'HTTP/1.1 ' . HTTP_STATUS_BAD_REQUEST . ' ' . $t_msg,
+		false,
+		HTTP_STATUS_BAD_REQUEST
+	);
+	echo string_attribute( $t_msg );
 }
