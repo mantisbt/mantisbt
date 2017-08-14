ce5ba70413a4f11d4f8d16cd140dc2b83adf0a93
Roland Becker <roland@atrol.de>
2017-08-14 23:31:21 +0200

    Remove unneeded string copy operations from text processing
    Fixes #23411

--- plugins/MantisCoreFormatting/MantisCoreFormatting.php
+++ plugins/MantisCoreFormatting/MantisCoreFormatting.php
@@ -81,13 +81,9 @@ class MantisCoreFormattingPlugin extends MantisFormattingPlugin {
 	 */
 	private function processText( $p_string, $p_multiline = true ){
 
-		$t_string = $p_string;
-
-		$t_string = string_strip_hrefs( $t_string );
+		$t_string = string_strip_hrefs( $p_string );
 		$t_string = string_html_specialchars( $t_string );
-		$t_string = string_restore_valid_html_tags( $t_string, $p_multiline );
-
-		return $t_string;
+		return string_restore_valid_html_tags( $t_string, $p_multiline );
 	}
 
 	/**
@@ -98,12 +94,8 @@ class MantisCoreFormattingPlugin extends MantisFormattingPlugin {
 	 */
 	private function processBugAndNoteLinks( $p_string ){
 
-		$t_string = $p_string;
-
-		$t_string = string_process_bug_link( $t_string );
-		$t_string = string_process_bugnote_link( $t_string );
-
-		return $t_string;
+		$t_string = string_process_bug_link( $p_string );
+		return string_process_bugnote_link( $t_string );
 	}
 
 	/**
@@ -122,22 +114,21 @@ class MantisCoreFormattingPlugin extends MantisFormattingPlugin {
 	function text( $p_event, $p_string, $p_multiline = true ) {
 		static $s_text;
 
-		$t_string = $p_string;
-
 		if( null === $s_text ) {
 			$s_text = plugin_config_get( 'process_text' );
 		}
 
 		if( ON == $s_text ) {
-			$t_string = $this->processText( $t_string, $p_multiline );
+			$t_string = $this->processText( $p_string, $p_multiline );
 
 			if( $p_multiline ) {
 				$t_string = string_preserve_spaces_at_bol( $t_string );
 				$t_string = string_nl2br( $t_string );
 			}
+			return $t_string;
+		} else {
+			return $p_string;
 		}
-
-		return $t_string;
 	}
 
 	/**
