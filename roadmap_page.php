46aae5e53a80c26d6688aaacb8f828091f59cd0a
Roland Becker <roland@atrol.de>
2017-09-06 08:21:05 +0200

    Fix filter links on Change Log and Roadmap page
    Fixes #23311
    
    Minor PHPDoc correction

--- roadmap_page.php
+++ roadmap_page.php
@@ -113,7 +113,9 @@ function print_version_header( array $p_version_row ) {
 	}
 	echo '<div class="btn-toolbar pull-right">';
 	echo '<a class="btn btn-xs btn-primary btn-white btn-round" ';
-	echo 'href="view_all_set.php?type=1&temporary=y&' . FILTER_PROPERTY_PROJECT_ID . '=' . $t_project_id . '&' . filter_encode_field_and_value( FILTER_PROPERTY_TARGET_VERSION, $t_version_name ) . '">';
+	echo 'href="view_all_set.php?type=1&temporary=y&' . FILTER_PROPERTY_PROJECT_ID . '=' . $t_project_id .
+		 '&' . filter_encode_field_and_value( FILTER_PROPERTY_TARGET_VERSION, $t_version_name ) .
+		 '&' . FILTER_PROPERTY_HIDE_STATUS . '=' . META_FILTER_NONE . '">';
 	echo lang_get( 'view_bugs_link' );
 	echo '<a class="btn btn-xs btn-primary btn-white btn-round" href="roadmap_page.php?version_id=' . $t_version_id . '">' . string_display_line( $t_version_name ) . '</a>';
 	echo '<a class="btn btn-xs btn-primary btn-white btn-round" href="roadmap_page.php?project_id=' . $t_project_id . '">' . string_display_line( $t_project_name ) . '</a>';
@@ -130,7 +132,7 @@ function print_version_header( array $p_version_row ) {
  * @param int $p_issues_resolved number of issues in resolved state
  * @param int $p_issues_planned number of issues planned for this version
  * @param int $p_progress percentage progress
- * @return null
+ * @return void
  */
 function print_version_footer( $p_version_row, $p_issues_resolved, $p_issues_planned, $p_progress ) {
 	$t_project_id   = $p_version_row['project_id'];
@@ -143,7 +145,9 @@ function print_version_footer( $p_version_row, $p_issues_resolved, $p_issues_pla
 		echo '<div class="widget-toolbox padding-8 clearfix">';
 		echo sprintf( lang_get( 'resolved_progress' ), $p_issues_resolved, $p_issues_planned, $p_progress );
 		echo ' <a class="btn btn-xs btn-primary btn-white btn-round" ';
-		echo 'href="view_all_set.php?type=1&temporary=y&' . FILTER_PROPERTY_PROJECT_ID . '=' . $t_project_id . '&' . filter_encode_field_and_value( FILTER_PROPERTY_TARGET_VERSION, $t_version_name ) . '">';
+		echo 'href="view_all_set.php?type=1&temporary=y&' . FILTER_PROPERTY_PROJECT_ID . '=' . $t_project_id .
+			 '&' . filter_encode_field_and_value( FILTER_PROPERTY_TARGET_VERSION, $t_version_name ) .
+			 '&' . FILTER_PROPERTY_HIDE_STATUS . '=' . META_FILTER_NONE . '">';
 		echo lang_get( 'view_bugs_link' );
 		echo '</a>';
 		echo '</div>';
