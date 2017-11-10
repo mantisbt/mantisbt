eef3332ab95c478d5560ed55b6ba62996a2e3db9
Victor Boctor <victor@mantishub.net>
2017-11-09 19:49:02 -0800

    Make date value / email date value consistent
    # Conflicts:
    #	core/cfdefs/cfdef_standard.php

--- core/cfdefs/cfdef_standard.php
+++ core/cfdefs/cfdef_standard.php
@@ -286,13 +286,7 @@ function cfdef_prepare_email_value_for_email( $p_value ) {
  * @return string
  */
 function cfdef_prepare_date_value_for_email( $p_value ) {
-	if( $p_value != null ) {
-		if( is_numeric( $p_value ) ) {
-			return date( config_get( 'short_date_format' ), $p_value );
-		}
-
-		return $p_value;
-	}
+	return cfdef_prepare_date_value( $p_value );
 }
 
 /**
@@ -351,8 +345,12 @@ function cfdef_prepare_email_value( $p_value ) {
  */
 function cfdef_prepare_date_value( $p_value ) {
 	if( $p_value != null ) {
-		return date( config_get( 'short_date_format' ), $p_value );
+		if( is_numeric( $p_value ) ) {
+			return date( config_get( 'short_date_format' ), $p_value );
+		}
 	}
+
+	return '';
 }
 
 /**
