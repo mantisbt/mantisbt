409935d72247e6fd831e253fbab4c7906e36c9c5
Victor Boctor <victor@mantishub.net>
2017-11-16 22:35:05 -0800

    Command Pattern
    Use Command Pattern to achieve the following:
    
    - Re-using of business logic across Web UI, REST API, and SOAP API.
    - Having commands that execute a specific intent e.g. assign, close, monitor rather than just update.
    - Enable core code to hook into such commands to handle side effects like email messages.
    - Enable plugins code to hook in such commands to modify changes, block the change, post to slack, etc.
    - Use exceptions that can be surfaced to APIs or Web UI.
    - Enable having a clean model using ADODB or some higher level ORM library that is used by commands.

---
+++ core/commands/Context.php
@@ -0,0 +1,29 @@
+<?php
+
+class Context
+{
+	function userAuthenticated() {
+
+	}
+
+	function getUserId() {
+
+	}
+
+	function getProjectId() {
+
+	}
+
+	function getLanguage() {
+
+	}
+
+	function getConfig( $p_name ) {
+
+	}
+
+	function getLocalizedString( $p_name ) {
+
+	}
+}
+
