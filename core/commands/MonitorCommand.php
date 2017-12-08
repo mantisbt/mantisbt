80c8bf6082b85ae61830c3e3aa767d306c1f904d
Victor Boctor <victor@mantishub.net>
2017-12-08 00:16:43 -0800

    PHPDoc and Headers

--- core/commands/MonitorCommand.php
+++ core/commands/MonitorCommand.php
@@ -1,4 +1,19 @@
 <?php
+# MantisBT - A PHP based bugtracking system
+
+# MantisBT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# MantisBT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
+
 require_api( 'authentication_api.php' );
 require_api( 'bug_api.php' );
 require_api( 'constant_inc.php' );
@@ -7,9 +22,23 @@ require_api( 'user_api.php' );
 
 use Mantis\Exceptions\ClientException;
 
+/**
+ * A command that adds user to monitor an issue.
+ */
 class MonitorCommand extends Command {
+	/**
+	 * @var integer
+	 */
 	private $projectId;
+
+	/**
+	 * @var integer
+	 */
 	private $loggedInUserId;
+
+	/**
+	 * @var array array of user ids to add to monitor list of the issue
+	 */
 	private $userIdsToAdd;
 
 	/**
