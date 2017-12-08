fe97988a84dc0756db8602a5819feb26553e0922
Victor Boctor <victor@mantishub.net>
2017-12-07 17:45:40 -0800

    Add PHPDoc to MantisException

--- core/exceptions/MantisException.php
+++ core/exceptions/MantisException.php
@@ -1,9 +1,24 @@
 <?php
 namespace Mantis\Exceptions;
 
+/**
+ * A base exception for all Mantis exceptions.
+ */
 class MantisException extends \Exception {
+	/**
+	 * @var array array of parameters for localized exception message.
+	 */
 	protected $params;
 
+    /**
+     * Constructor
+     *
+     * @param string $p_message The internal non-localized error message.
+     * @param integer $p_code The Mantis error code.
+     * @param array $p_params Localized error message parameters.
+     * @param Throwable $p_previous The inner exception.
+     * @return void
+     */
 	function __construct( $p_message, $p_code, $p_params = array(), Throwable $p_previous = null ) {
 		parent::__construct( $p_message, $p_code, $p_previous );
 		$this->params = $p_params;
