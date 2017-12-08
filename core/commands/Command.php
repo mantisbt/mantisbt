2911555d6fe7f067857d6946178736acd115da64
Victor Boctor <victor@mantishub.net>
2017-12-08 00:16:43 -0800

    PHPDoc and Headers

--- core/commands/Command.php
+++ core/commands/Command.php
@@ -1,4 +1,19 @@
 <?php
+# MantisBT - A PHP based bugtracking system
+
+# MantisBT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# MantisBT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * A base class for intent based commands that can accomplish a task.
  * Such commands will be used from web ui action pages, REST API, SOAP API, etc.
@@ -10,6 +25,9 @@
  */
 abstract class Command
 {
+	/**
+	 * @var array The input data for the command.
+	 */
 	protected $data;
 
 	/**
