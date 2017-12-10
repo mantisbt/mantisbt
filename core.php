303ef0b7ae4fa5564fe62d4bccf99db03f69c7da
Roland Becker <roland@atrol.de>
2017-12-10 22:45:43 +0100

    Don't use deprecated __autoload
    Fixes #23710

--- core.php
+++ core.php
@@ -159,7 +159,7 @@ function http_is_protocol_https() {
  * @param string $p_class Class name being autoloaded.
  * @return void
  */
-function __autoload( $p_class ) {
+function autoload_mantis( $p_class ) {
 	global $g_class_path;
 	global $g_library_path;
 
@@ -179,7 +179,7 @@ function __autoload( $p_class ) {
 }
 
 # Register the autoload function to make it effective immediately
-spl_autoload_register( '__autoload' );
+spl_autoload_register( 'autoload_mantis' );
 
 # Load UTF8-capable string functions
 define( 'UTF8', $g_library_path . 'utf8' );
