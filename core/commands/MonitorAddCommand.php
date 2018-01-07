23e990f9a504f413fd7efa84abf7da9aca862577
Victor Boctor <victor@mantishub.net>
2018-01-07 14:29:50 -0800

    Fix defaulting of monitor users list

--- core/commands/MonitorAddCommand.php
+++ core/commands/MonitorAddCommand.php
@@ -59,7 +59,7 @@ class MonitorAddCommand extends Command {
 		$this->projectId = bug_get_field( $t_issue_id, 'project_id' );
 		$t_logged_in_user = auth_get_current_user_id();
 
-		$t_users = $this->payload( 'users', array( 'id' => $t_logged_in_user ) );
+		$t_users = $this->payload( 'users', array( array( 'id' => $t_logged_in_user ) ) );
 		if( !is_array( $t_users ) ) {
 			throw new ClientException( 'Invalid users array', ERROR_INVALID_FIELD_VALUE, array( 'users' ) );
 		}
