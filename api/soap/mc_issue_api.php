e68fcfedefb3bae9c276172fc040fad9eebdf71f
Victor Boctor <victor@mantishub.net>
2018-02-18 10:55:11 -0800

    Use exceptions in `mci_issue_set_custom_fields()`

--- api/soap/mc_issue_api.php
+++ api/soap/mc_issue_api.php
@@ -404,14 +404,19 @@ function mci_issue_set_custom_fields( $p_issue_id, array &$p_custom_fields = nul
 			}
 
 			if( !$t_valid_cf ) {
-				return ApiObjectFactory::faultBadRequest( $t_msg );
+				throw new ClientException(
+					$t_msg,
+					ERROR_EMPTY_FIELD,
+					"custom_field['id']" );
 			}
 
 			# get custom field id from object ref
 			$t_custom_field_id = mci_get_custom_field_id_from_objectref( (object)$t_custom_field['field'] );
-
 			if( $t_custom_field_id == 0 ) {
-				return ApiObjectFactory::faultNotFound( "Custom field '" . $t_field['name'] . "' not found." );
+				throw new ClientException(
+					"Custom field '" . $t_field['name'] . "' not found.",
+					ERROR_CUSTOM_FIELD_NOT_FOUND,
+					"custom_field['id']" );
 			}
 
 			# skip if current user doesn't have login access.
@@ -419,16 +424,22 @@ function mci_issue_set_custom_fields( $p_issue_id, array &$p_custom_fields = nul
 				continue;
 			}
 
+			$t_custom_field_def = custom_field_get_definition( $t_custom_field_id );
+			$t_name = $t_custom_field_def['name'];
 			$t_value = $t_custom_field['value'];
 
 			if( !custom_field_validate( $t_custom_field_id, $t_value ) ) {
-				return ApiObjectFactory::faultBadRequest( 'Invalid custom field value for field id ' .
-					$t_custom_field_id . '.' );
+				throw new ClientException(
+					"Invalid value for custom field '$t_name'",
+					ERROR_INVALID_FIELD_VALUE,
+					"value" );
 			}
 
 			if( !custom_field_set_value( $t_custom_field_id, $p_issue_id, $t_value, $p_log_insert ) ) {
-				return ApiObjectFactory::faultBadRequest( 'Unable to set custom field value for field id ' .
-					$t_custom_field_id . ' to issue ' . $p_issue_id . '.' );
+				throw new ClientException(
+					"Invalid value for custom field '$t_name'.",
+					ERROR_INVALID_FIELD_VALUE,
+					"value" );
 			}
 		}
 	}
