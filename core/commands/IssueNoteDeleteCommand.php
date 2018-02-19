ba0d8d30a08e25b63e7fad108e77826e48329b88
Victor Boctor <victor@mantishub.net>
2018-02-18 18:43:32 -0800

    Revert changes to `IssueNoteDeleteCommand`
    These changes were done in error.

--- core/commands/IssueNoteDeleteCommand.php
+++ core/commands/IssueNoteDeleteCommand.php
@@ -56,6 +56,43 @@ class IssueNoteDeleteCommand extends Command {
             throw new ClientException( "'id' must be >= 1", ERROR_INVALID_FIELD_VALUE, array( 'id' ) );
         }
 
+        bugnote_ensure_exists( $this->id );
+
+        $this->issueId = bugnote_get_field( $this->id, 'bug_id' );
+        $t_specified_issue_id = $this->query( 'issue_id' );
+        if( $t_specified_issue_id !== null && $t_specified_issue_id != $this->issueId ) {
+            throw new ClientException( "Issue note doesn't belong to issue", ERROR_INVALID_FIELD_VALUE, array( 'id' ) );
+        }
+
+        $t_project_id = bug_get_field( $this->issueId, 'project_id' );
+		if( $t_project_id != helper_get_current_project() ) {
+			# in case the current project is not the same project of the bug we are
+			# viewing, override the current project. This to avoid problems with
+			# categories and handlers lists etc.
+            global $g_project_override;
+            $g_project_override = $t_project_id;
+        }
+
+        $t_reporter_id = bugnote_get_field( $this->id, 'reporter_id' );
+        $t_user_id = auth_get_current_user_id();
+
+        # mirrors check from bugnote_delete.php
+        if( $t_user_id == $t_reporter_id ) {
+            $t_threshold_config_name =  'bugnote_user_delete_threshold';
+        } else {
+            $t_threshold_config_name =  'delete_bugnote_threshold';
+        }
+
+        if( !access_has_bugnote_level( config_get( $t_threshold_config_name ), $this->id ) ) {
+            throw new ClientException( 'Access denied', ERROR_ACCESS_DENIED );
+        }
+
+        if( bug_is_readonly( $this->issueId ) ) {
+			throw new ClientException(
+				sprintf( "Issue '%d' is read-only.", $this->issueId ),
+				ERROR_BUG_READ_ONLY_ACTION_DENIED,
+				array( $this->issueId ) );
+        }
     }
 
  	/**
