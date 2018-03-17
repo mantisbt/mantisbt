1eb5ff1c7e1d8d99e97eadbd54da087d38b0fa55
Roland Becker <roland@atrol.de>
2018-03-17 21:10:27 +0100

    Check early for minimum PHP version
    Fixes #24128

--- core.php
+++ core.php
@@ -63,13 +63,29 @@ if( file_exists( 'mantis_offline.php' ) && !isset( $_GET['mbadmin'] ) ) {
 
 $g_request_time = microtime( true );
 
+# Load supplied constants
+require_once( dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'constant_inc.php' );
+
+# Enforce our minimum PHP requirements
+if( version_compare( PHP_VERSION, PHP_MIN_VERSION, '<' ) ) {
+	echo '<strong>FATAL ERROR: Your version of PHP is too old. '
+		. 'MantisBT requires ' . PHP_MIN_VERSION . ' or newer</strong><br />'
+		. 'Your are running PHP version <em>' . PHP_VERSION . '</em>';
+	die();
+}
+
+# Enforce PHP mbstring extension
+if( !extension_loaded( 'mbstring' ) ) {
+	echo '<strong>FATAL ERROR: PHP mbstring extension is not enabled.</strong><br />'
+		. 'MantisBT requires this extension for Unicode (UTF-8) support<br />'
+		. 'http://www.php.net/manual/en/mbstring.installation.php';
+	die();
+}
+
 ob_start();
 
 # Load Composer autoloader
 require_once( dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'vendor/autoload.php' );
-
-# Load supplied constants
-require_once( dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'constant_inc.php' );
 
 # Include default configuration settings
 require_once( dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'config_defaults_inc.php' );
@@ -207,24 +223,6 @@ spl_autoload_register( 'autoload_mantis' );
 
 # Include PHP compatibility file
 require_api( 'php_api.php' );
-
-# Enforce our minimum PHP requirements
-if( version_compare( PHP_VERSION, PHP_MIN_VERSION, '<' ) ) {
-	@ob_end_clean();
-	echo '<strong>FATAL ERROR: Your version of PHP is too old. '
-		. 'MantisBT requires ' . PHP_MIN_VERSION . ' or newer</strong><br />'
-		. 'Your are running PHP version <em>' . PHP_VERSION . '</em>';
-	die();
-}
-
-# Enforce PHP mbstring extension
-if( !extension_loaded( 'mbstring' ) ) {
-	@ob_end_clean();
-	echo '<strong>FATAL ERROR: PHP mbstring extension is not enabled.</strong><br />'
-		. 'MantisBT requires this extension for Unicode (UTF-8) support<br />'
-		. 'http://www.php.net/manual/en/mbstring.installation.php';
-	die();
-}
 
 # Ensure that output is blank so far (output at this stage generally denotes
 # that an error has occurred)
