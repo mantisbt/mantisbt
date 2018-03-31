b19e905c8e0824c054a0d5031d9453e04727ed25
Victor Boctor <vboctor@gmail.com>
2018-04-01 09:26:23 +1000

    Update version to `2.13.0`

--- core/constant_inc.php
+++ core/constant_inc.php
@@ -21,7 +21,7 @@
 /**
  * Mantis Version
  */
-define( 'MANTIS_VERSION', '2.13.0-dev' );
+define( 'MANTIS_VERSION', '2.13.0' );
 define( 'FILTER_VERSION', 'v9' );
 
 # --- constants -------------------
