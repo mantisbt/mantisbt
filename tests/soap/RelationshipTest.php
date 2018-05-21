f2e2c9cc0e595f8c43847a3234958d287850aaa5
Roland Becker <roland@atrol.de>
2018-05-21 21:05:45 +0200

    Correct testDeleteRelation check
    Issue #24462

--- tests/soap/RelationshipTest.php
+++ tests/soap/RelationshipTest.php
@@ -104,7 +104,7 @@ class RelationshipTest extends SoapBase {
 		$t_retrieved_first_issue = $this->client->mc_issue_get( $this->userName, $this->password, $t_first_issue_id );
 		$t_retrieved_second_issue = $this->client->mc_issue_get( $this->userName, $this->password, $t_second_issue_id );
 
-		$this->assertObjectNotHasAttribute( 'relationships', $t_retrieved_first_issue );
-		$this->assertObjectNotHasAttribute( 'relationships', $t_retrieved_second_issue );
+		$this->assertEquals( 0, count( $t_retrieved_first_issue->relationships ) );
+		$this->assertEquals( 0, count( $t_retrieved_second_issue->relationships ) );
 	}
 }
