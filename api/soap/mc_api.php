9bf4e1dab9a1de7425880fc2491d185e459a27e9
Anders Sandblad <runeson@gmail.com>
2018-07-22 12:53:14 +0200

    Add getters for error message and http status code
    These getters are used by the throwIfFault function

--- api/soap/mc_api.php
+++ api/soap/mc_api.php
@@ -58,6 +58,24 @@ class RestFault {
 		$this->status_code = $p_status_code;
 		$this->fault_string = $p_fault_string === null ? '' : $p_fault_string;
 	}
+	
+	/**
+	 * Getter for error message
+	 *
+	 * @return string The error message.
+	 */
+	function getMessage() {
+		return $this->fault_string;
+	}
+
+	/**
+	 * Getter for http status code
+	 *
+	 * @return string The http status code.
+	 */
+	function getCode() {
+		return $this->status_code;
+	}
 }
 
 /**
