cf5c23fcf0ea5cedd0dc83c827ea3f211f2d9b22
Victor Boctor <victor@mantishub.net>
2018-07-29 21:37:25 -0700

    Update release to `2.16.0`

--- core/constant_inc.php
+++ core/constant_inc.php
@@ -21,7 +21,7 @@
 /**
  * Mantis Version
  */
-define( 'MANTIS_VERSION', '2.16.0-dev' );
+define( 'MANTIS_VERSION', '2.16.0' );
 define( 'FILTER_VERSION', 'v9' );
 
 # --- constants -------------------
