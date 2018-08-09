9d59867c7daab52438a10784a6105c18059262ef
CasN <cas@nuy.info>
2018-08-09 16:19:46 +0200

    Add realname case-insensitive wildcard search on manage_user_page.php
    Provide the option to exclude a pattern.
    Searchterm "jan" would lead to wildcard "%jan%"
    Searchterm "jan -dek" would lead to include  "%jan%" and to exclude "%dek%"
    
    Issue #12677

--- manage_user_page.php
+++ manage_user_page.php
@@ -64,6 +64,11 @@ $f_save          = gpc_get_bool( 'save' );
 $f_filter        = mb_strtoupper( gpc_get_string( 'filter', config_get( 'default_manage_user_prefix' ) ) );
 $f_page_number   = gpc_get_int( 'page_number', 1 );
 
+$f_findname		 = gpc_get_string( 'findname', '' ); 
+if( $f_findname <> "") {
+	$f_filter = 'SEARCH';
+}
+
 if( !$f_save && !is_blank( gpc_get_cookie( $t_cookie_name, '' ) ) ) {
 	$t_manage_arr = explode( ':', gpc_get_cookie( $t_cookie_name ) );
 
@@ -185,6 +190,20 @@ echo '<div class="space-10"></div >';
 $t_where_params = array();
 if( $f_filter === 'ALL' ) {
 	$t_where = '(1 = 1)';
+	} else if( $f_filter === 'SEARCH' ) {
+	$t_pos = strpos($f_findname, '-');
+	if( $t_pos ) {
+		$t_exclude			= trim( substr( $f_findname,$t_pos+1 ) );
+		$f_findname			= trim( substr( $f_findname, 0,$t_pos-1 ) );
+		$t_where_params[]	= '%'.$f_findname . '%';
+		$t_where			= db_helper_like( 'realname' );
+		$t_where			.= " AND NOT ";
+		$t_where_params[] 	= '%'.$t_exclude . '%';
+		$t_where 			.= db_helper_like( 'realname' );
+	} else {
+		$t_where_params[] 	= '%'.$f_findname . '%';
+		$t_where 			= db_helper_like( 'realname' );
+	}
 } else if( $f_filter === 'UNUSED' ) {
 	$t_where = '(login_count = 0) AND ( date_created = last_visit )';
 } else if( $f_filter === 'NEW' ) {
@@ -192,7 +211,7 @@ if( $f_filter === 'ALL' ) {
 	$t_where_params[] = db_now();
 } else {
 	$t_where_params[] = $f_filter . '%';
-	$t_where = db_helper_like( 'UPPER(username)' );
+	$t_where = db_helper_like( 'username' );
 }
 
 $p_per_page = 50;
@@ -363,10 +382,16 @@ $t_user_count = count( $t_users );
 		<form id="manage-user-edit-form" method="get" action="manage_user_edit_page.php" class="form-inline"
 			<?php # CSRF protection not required here - form does not result in modifications ?>>
 			<label class="inline" for="username"><?php echo lang_get( 'search' ) ?></label>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;			
 			<input id="username" type="text" name="username" class="input-sm" value="" />
 			<input type="submit" class="btn btn-primary btn-sm btn-white btn-round" value="<?php echo lang_get( 'manage_user' ) ?>" />
 		</form>
-	</div>
+		<form method="get" action="manage_user_page.php">
+			<?php # CSRF protection not required here - form does not result in modifications ?>
+			<label class="inline" for="findname"><?php echo lang_get( 'realname' ) ?></label>
+			<input id="findname" type="text" name="findname" class="input-sm" value="" />
+			<input type="submit" class="btn btn-primary btn-sm btn-white btn-round" value="<?php echo lang_get( 'search' ) ?>" />
+		</form>	</div>
 	<div class="btn-toolbar pull-right">
 		<?php
 		# @todo hack - pass in the hide inactive filter via cheating the actual filter value
