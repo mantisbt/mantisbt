1daf87051a5c5935178e11033f987f2ed1d13ee3
Roland Becker <roland@atrol.de>
2018-08-13 22:17:55 +0200

    Enhance user search UI on page manage_user_page.php
    - Replace label by placeholder
    - White space fixes
    - Remove unrelated changes from PR
    
    Fixes #12677

--- config_defaults_inc.php
+++ config_defaults_inc.php
@@ -4876,9 +4876,3 @@ $g_webservice_rest_enabled = ON;
  * the attachment is linked to the note.  Or 0 for disabling this feature.
  */
 $g_issue_activity_note_attachments_seconds_threshold = 3;
-
-/**
- * If a user submits a note with an attachments (with the specified # of seconds)
- * the attachment is linked to the note.  Or 0 for disabling this feature.
- */
-$g_issue_activity_note_attachments_seconds_threshold = 3;
