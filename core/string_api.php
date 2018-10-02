193d45d04002258cf0aefd13deb37c824fe93343
Roland Becker <roland@atrol.de>
2018-10-02 15:58:02 +0200

    Remove no longer invalid characters when displaying strings
    We finally do not use doctype XHTML [1], so there is no longer the need
    to remove characters from strings which are invalid per XML 1.0
    specification.
    
    [1] https://www.mantisbt.org/bugs/view.php?id=16025
    
    Issue #24823

--- core/string_api.php
+++ core/string_api.php
@@ -899,10 +899,6 @@ function string_html_entities( $p_string ) {
  * @return string
  */
 function string_html_specialchars( $p_string ) {
-	# Remove any invalid character from the string per XML 1.0 specification
-	# http://www.w3.org/TR/2008/REC-xml-20081126/#NT-Char
-	$p_string = preg_replace( '/[^\x9\xA\xD\x20-\x{D7FF}\x{E000}-\x{FFFD}\x{10000}-\x{10FFFF}]+/u', '', $p_string );
-
 	# achumakov: @ added to avoid warning output in unsupported codepages
 	# e.g. 8859-2, windows-1257, Korean, which are treated as 8859-1.
 	# This is VERY important for Eastern European, Baltic and Korean languages
