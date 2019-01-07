aaf4d8b05259099c48a2749f2b4d226b402a2ef7
Carlos Proensa <proensa@gmail.com>
2019-01-07 01:09:23 +0100

    MantisGraph, update includes and init
    Since 1.3, plugins don't need to explicitly include core apis.
    The proper way to include plugin's own apis is with
    plugin_require_api().
    For a better organization, plugin apis can be inluded in the plugin
    init() method, so it does not need to be included for each plugin's
    page. An additional benefit is that these apis are only included when
    the plugin is enabled, as opposed to having the includes in the plain
    php file.

--- plugins/MantisGraph/pages/issues_trend_page.php
+++ plugins/MantisGraph/pages/issues_trend_page.php
@@ -29,14 +29,6 @@
  * @uses plugin_api.php
  */
 
-require_once( 'core.php' );
-plugin_require_api( 'core/Period.php' );
-require_api( 'access_api.php' );
-require_api( 'config_api.php' );
-require_api( 'gpc_api.php' );
-require_api( 'html_api.php' );
-require_api( 'plugin_api.php' );
-
 access_ensure_project_level( config_get( 'view_summary_threshold' ) );
 
 $f_interval = gpc_get_int( 'interval', 0 );
--- plugins/MantisGraph/pages/priority_graph.php
+++ plugins/MantisGraph/pages/priority_graph.php
@@ -23,10 +23,6 @@
  * @link http://www.mantisbt.org
  */
 
-require_once( 'core.php' );
-
-plugin_require_api( 'core/graph_api.php' );
-
 access_ensure_project_level( config_get( 'view_summary_threshold' ) );
 
 layout_page_header();
