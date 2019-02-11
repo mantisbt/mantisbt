b8606858a55882d89fd8cd76bab9d97e9a4255b1
Carlos Proensa <proensa@gmail.com>
2019-02-11 01:19:12 +0100

    Redesign dropzone file previews
    Previous design was not very functional:
    - Did not properly support image thunbnails.
    - Fixed width makes the filename to get truncated
    - A permanent grey sqare that serves no purpose
    
    Redefine some markup and options to improve some of those:
    - Set scaling as method for image thumbnail, instead of cropping.
    - Fix image rendering for thumbnails.
    - Allow flexible width and height of the preview container. This way
      filenames are fully shown.

--- bug_report_page.php
+++ bug_report_page.php
@@ -639,6 +639,7 @@ if( $t_show_attachments ) {
 			<?php print_max_filesize( $t_max_file_size ); ?>
 		</th>
 		<td>
+			<?php print_dropzone_template() ?>
 			<input type="hidden" name="max_file_size" value="<?php echo $t_max_file_size ?>" />
 			<div class="dropzone center" <?php print_dropzone_form_data() ?>>
 				<i class="upload-icon ace-icon fa fa-cloud-upload blue fa-3x"></i><br>
@@ -647,7 +648,7 @@ if( $t_show_attachments ) {
 			</div>
 			<div class="fallback">
 				<div class="dz-message" data-dz-message></div>
-			<input <?php echo helper_get_tab_index() ?> id="ufile[]" name="ufile[]" type="file" size="60" />
+				<input <?php echo helper_get_tab_index() ?> id="ufile[]" name="ufile[]" type="file" size="60" />
 			</div>
 		</td>
 	</tr>
