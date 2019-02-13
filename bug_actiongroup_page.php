7e5adb5d9292688d8cdcb2ad461d87b7ccdfb22a
Carlos Proensa <proensa@gmail.com>
2019-02-14 00:32:08 +0100

    Evaluate versions for multiple projects
    - Make `version_get_all_rows()` evaluate mutiple projects, when we want to
      get all versions provided by a group of projects. Including parent
      projects if version inheritance is enabled.
    
    - Make `version_should_show_product_version()` evaluate multiple
      projects, as this conditions is needed in a context for several
      projects. Eg: filters, bug-action-group, ...
    
    - Make `print_version_option_list()` evaluate multiple projects. And
      remove the functionality for including version from subprojects.
    
    - Including subproject versions in `print_version_option_list()` is now
      a task for the caller, by passing the exact list of projects for which
      versions are needed. This project list is managed directly by the
      filter_api and the filter specific logic.
    
    Fixes: #12261

--- bug_actiongroup_page.php
+++ bug_actiongroup_page.php
@@ -330,11 +330,11 @@ if( $t_multiple_projects ) {
 					print_enum_string_option_list( 'view_state', config_get( 'default_bug_view_status' ) );
 					break;
 				case 'UP_TARGET_VERSION':
-					print_version_option_list( '', $t_project_id, VERSION_FUTURE, true, true );
+					print_version_option_list( '', $t_projects, VERSION_FUTURE, true );
 					break;
 				case 'UP_PRODUCT_VERSION':
 				case 'UP_FIXED_IN_VERSION':
-					print_version_option_list( '', $t_project_id, VERSION_ALL, true, true );
+					print_version_option_list( '', $t_projects, VERSION_ALL, true );
 					break;
 			}
 
