113a270b69b0299a240c746c525467ec21689bed
Jingshao Chen <jingshaochen@gmail.com>
2019-04-12 23:21:12 -0700

    Add active class in summary menu for custom tabs
    Fixes #23418

--- core/html_api.php
+++ core/html_api.php
@@ -894,7 +894,10 @@ function print_summary_menu( $p_page = '', array $p_filter = null ) {
 
 	# Plugins menu items - these are cooked links
 	foreach ( $t_menu_options as $t_menu_item ) {
-		echo '<li>' . $t_menu_item . '</li>';
+		$t_active = $p_page && strpos( $t_menu_item, $p_page ) !== false
+			? ' class="active"'
+			: '';
+		echo "<li{$t_active}>{$t_menu_item}</li>";
 	}
 
 	echo '</ul>' . "\n";
