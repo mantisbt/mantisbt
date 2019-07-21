bf92303017cbf4319b6de5e9f30d0a109abfe540
Victor Boctor <victor@mantishub.net>
2019-07-20 20:50:04 -0700

    Enable adding attachments with private notes

--- js/common.js
+++ js/common.js
@@ -531,10 +531,8 @@ $(document).ready( function() {
 	$('input[name=private].ace').bind("click", function() {
 		if ($(this).is(":checked")){
 			$('textarea[name=bugnote_text]').addClass("bugnote-private");
-			$('tr[id=bugnote-attach-files]').hide();
 		} else {
 			$('textarea[name=bugnote_text]').removeClass("bugnote-private");
-			$('tr[id=bugnote-attach-files]').show();
 		}
 	});
 
