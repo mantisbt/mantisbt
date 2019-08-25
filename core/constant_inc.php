7e4b9486dd3e952aa8e7c94784222016401808b8
Victor Boctor <victor@mantishub.net>
2019-08-25 09:42:27 -0700

    Change version to 2.23.0-dev

--- core/constant_inc.php
+++ core/constant_inc.php
@@ -21,7 +21,7 @@
 /**
  * Mantis Version
  */
-define( 'MANTIS_VERSION', '2.22.0' );
+define( 'MANTIS_VERSION', '2.23.0-dev' );
 define( 'FILTER_VERSION', 'v9' );
 
 # --- constants -------------------
