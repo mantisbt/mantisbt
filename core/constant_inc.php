9fd3b1ff59fe2eed5af2e4f545ccd94dcd87de1b
Victor Boctor <victor@mantishub.net>
2020-03-15 12:21:32 -0700

    Update release to 2.25.0-dev
    Signed-off-by: Victor Boctor <victor@mantishub.net>

--- core/constant_inc.php
+++ core/constant_inc.php
@@ -21,7 +21,7 @@
 /**
  * Mantis Version
  */
-define( 'MANTIS_VERSION', '2.24.0' );
+define( 'MANTIS_VERSION', '2.25.0-dev' );
 define( 'FILTER_VERSION', 'v9' );
 
 # --- constants -------------------
