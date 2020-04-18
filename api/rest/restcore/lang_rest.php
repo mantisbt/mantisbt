6bdd833d27a4ed5b7a3d6b4aa999a416d5ad0f9d
Victor Boctor <victor@mantishub.net>
2020-04-18 13:25:19 -0700

    Implement `LocalizedStringsGetCommand`
    Fixes #26890
    
    Signed-off-by: Victor Boctor <victor@mantishub.net>

--- api/rest/restcore/lang_rest.php
+++ api/rest/restcore/lang_rest.php
@@ -46,23 +46,14 @@ $g_app->group('/lang', function() use ( $g_app ) {
  * @return \Slim\Http\Response The augmented response.
  */
 function rest_lang_get( \Slim\Http\Request $p_request, \Slim\Http\Response $p_response, array $p_args ) {
-	$t_strings = $p_request->getParam( 'string' );
-	if( !is_array( $t_strings ) ) {
-		$t_strings = array( $t_strings );
-	}
+	$t_data = array(
+		'query' => array(
+			'string' => $p_request->getParam( 'string' )
+		)
+	);
 
-	$t_current_language = lang_get_current();
-	$t_localized_strings = array();
-	foreach( $t_strings as $t_string ) {
-		if( !lang_exists( $t_string, $t_current_language) ) {
-			continue;
-		}
-
-		$t_localized_strings[] = array( 'name' => $t_string, 'localized' => lang_get( $t_string ) );
-	}
-
-	$t_result = array( 'strings' => $t_localized_strings );
-	$t_result['language'] = $t_current_language;
+	$t_command = new LocalizedStringsGetCommand( $t_data );
+	$t_result = $t_command->execute();
 
 	return $p_response->withStatus( HTTP_STATUS_SUCCESS )->withJson( $t_result );
 }
