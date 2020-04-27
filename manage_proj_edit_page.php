f94b0786e9c36853b2662e336a5c55ffb7b4d888
Marc-Antoine TURBET-DELOF <28108356+maturbet@users.noreply.github.com>
2020-04-27 09:28:52 +0200

    redundance

--- manage_proj_edit_page.php
+++ manage_proj_edit_page.php
@@ -503,7 +503,6 @@ if ( config_get( 'subprojects_enabled') == ON ) {
 				<div class="pull-right">
     <form id="manage-project-version-filter" method="post" action="manage_proj_edit_page.php?project_id=<?php echo $f_project_id ?>" class="form-inline">
         <fieldset>
-			<input type="hidden" class="form-control input-sm" name="project_id" value="<?php echo $f_project_id ?>" />
 			<label class="inline">
 				<input type="checkbox" class="ace" name="hideobsolet" value="<?php echo ON ?>" <?php check_checked( (int)$c_hide_obsolet, ON ); ?> />
 				<span class="lbl padding-6"><?php echo lang_get( 'hide_obsolete' ) ?></span>
