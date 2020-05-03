3152defe0866255a2e8db40306ba347f0041fe9c
Damien Regad <dregad@mantisbt.org>
2020-05-03 11:45:38 +0200

    python3 migration: buildrelease.py
    Fixes #27384

--- build/buildrelease.py
+++ build/buildrelease.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -u
+#!/usr/bin/python3 -u
 
 import getopt
 import os
@@ -45,9 +45,9 @@ exclude_list = (
 
 
 def usage():
-    print '''Builds a release (zip/tarball)
+    print('''Builds a release (zip/tarball)
 
-Usage: %s [options] /path/for/tarballs [/path/to/mantisbt]
+Usage: {0} [options] /path/for/tarballs [/path/to/mantisbt]
 
 Options:
     -h | --help               Show this usage message
@@ -56,41 +56,41 @@ Options:
     -d | --docbook            Build and include the docbook manuals
     -v | --version <version>  Override version name detection
     -s | --suffix <suffix>    Include version suffix in config file
-''' % path.basename(__file__)
+'''.format(path.basename(__file__)))
 # end usage()
 
 
 def gpg_sign_tarball(filename):
     ''' Sign the file using GPG '''
-    gpgsign = "gpg -b -a %s" + path.abspath(path.join(os.curdir, filename))
+    gpgsign = "gpg -b -a {}" + path.abspath(path.join(os.curdir, filename))
     try:
-        subprocess.check_call(gpgsign % '--batch --yes ', shell=True)
+        subprocess.check_call(gpgsign.format('--batch --yes '), shell=True)
     except subprocess.CalledProcessError:
-        print "WARNING: GPG signature failed; to sign manually, run\n" \
-            "         %s" % (gpgsign % '')
+        print("WARNING: GPG signature failed; to sign manually, run")
+        print("         " + gpgsign.format(''))
 
 
 def generate_checksum(filename):
     ''' Generate MD5 and SHA1 checksums for the file '''
-    f = open("%s.digests" % filename, 'w')
+    f = open(filename + ".digests", 'w')
     for method in ("md5", "sha1"):
-        checksum_cmd = "%ssum --binary " % method
+        checksum_cmd = "{}sum --binary ".format(method)
         checksum = os.popen(checksum_cmd + filename).read()
         f.write(checksum)
-        print "      %s: %s" % (method, checksum.rstrip())
+        print("      {}: {}".format(method, checksum.rstrip()))
     f.close()
 
 
 def remove_build_dir(release_dir):
-    print "Removing build directory..."
+    print("Removing build directory...")
     shutil.rmtree(release_dir)
 
 
 def main():
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], options, long_options)
-    except getopt.GetoptError, err:
-        print str(err)
+    except getopt.GetoptError as err:
+        print(err)
         usage()
         sys.exit(2)
 
@@ -127,15 +127,15 @@ def main():
         mantis_path = args[1]
 
     # 'Standard' umask
-    old_umask = os.umask(0002)
+    old_umask = os.umask(0o002)
 
     # Check paths
     if not path.isdir(release_path):
-        print "Creating release path..."
+        print("Creating release path...")
         os.mkdir(release_path)
 
     if not path.isdir(mantis_path):
-        print "Error: mantis path is not a directory or does not exist."
+        print("Error: mantis path is not a directory or does not exist.")
         sys.exit(3)
 
     if (
@@ -143,8 +143,8 @@ def main():
         not path.isdir(path.join(mantis_path, "core")) or
         not path.isfile(path.join(mantis_path, "core", "constant_inc.php"))
     ):
-        print "Error: '%s' does not appear to be a valid Mantis directory." % \
-            mantis_path
+        print("Error: '{}' does not appear to be a valid Mantis directory."
+              .format(mantis_path))
         sys.exit(3)
 
     # Find Mantis version
@@ -164,41 +164,44 @@ def main():
     # Copy to release path, excluding unwanted files
     release_dir = path.abspath(path.join(release_path, release_name))
 
-    print "\nBuilding release '%s' in path '%s'" % (release_name, release_dir)
-    print "  Source repository: '%s'\n" % mantis_path
+    print("\nBuilding release '{}' in path '{}'".format(
+        release_name,
+        release_dir
+    ))
+    print("  Source repository: '{}'\n".format(mantis_path))
 
     if path.exists(release_dir):
-        print "Error: release path already contains %s." % (release_name)
+        print("Error: release path already contains {}.".format(release_name))
         sys.exit(3)
 
     # Generate temp file with list of exclusions
-    fp = tempfile.NamedTemporaryFile(delete=False)
-    print "  Excluded files and directories:"
+    fp = tempfile.NamedTemporaryFile(mode='wt', delete=False)
+    print("  Excluded files and directories:")
     for name in exclude_list:
-        print "    " + name
+        print("    " + name)
         fp.write(name + "\n")
     fp.close()
 
     # Copy the files from the source repo, then delete temp file
-    rsync = "rsync -rltD --exclude-from=%s %s/ %s" % (
-        fp.name,
-        mantis_path,
-        release_dir
+    rsync = "rsync -rltD --exclude-from={exclude} {source}/ {target}".format(
+        exclude=fp.name,
+        source=mantis_path,
+        target=release_dir
     )
     subprocess.check_call(rsync, shell=True)
 
     os.unlink(fp.name)
-    print "  Copy complete.\n"
+    print("  Copy complete.\n")
 
     # Apply version suffix
     if version_suffix:
-        print "Applying version suffix..."
-        sed_cmd = "s/(%s\s*=\s*)'.*'/\\1'%s'/" % (
+        print("Applying version suffix...")
+        sed_cmd = "s/({}\s*=\s*)'.*'/\\1'{}'/".format(
             'g_version_suffix',
             version_suffix
         )
         subprocess.call(
-            'sed -r -i.bak "%s" %s' % (
+            'sed -r -i.bak "{}" {}'.format(
                 sed_cmd,
                 path.join(release_dir, "config_defaults_inc.php")
             ),
@@ -207,9 +210,9 @@ def main():
 
     # Build documentation for release
     if build_docbook:
-        print "Building docbook manuals...\n"
+        print("Building docbook manuals...\n")
         subprocess.call(
-            manualscript + " --release %s %s" % (
+            manualscript + " --release {} {}".format(
                 path.join(mantis_path, "docbook"),
                 path.join(release_dir, "doc")
             ),
@@ -217,37 +220,37 @@ def main():
         )
 
     # Create tarballs and sign them
-    print "Creating release tarballs..."
+    print("Creating release tarballs...")
     os.chdir(release_path)
     tarball_ext = ("tar.gz", "zip")
 
     for ext in tarball_ext:
-        tarball = "%s.%s" % (release_name, ext)
-        print "  " + tarball
+        tarball = "{}.{}".format(release_name, ext)
+        print("  " + tarball)
 
         if ext == "tar.gz":
             tar_cmd = "tar -czf"
         elif ext == "zip":
             tar_cmd = "zip -rq"
-        tar_cmd += " %s %s"
+        tar_cmd += " {} {}"
 
-        subprocess.call(tar_cmd % (tarball, release_name), shell=True)
+        subprocess.call(tar_cmd.format(tarball, release_name), shell=True)
 
-        print "    Signing the tarball"
+        print("    Signing the tarball")
         gpg_sign_tarball(tarball)
 
-        print "    Generating checksums..."
+        print("    Generating checksums...")
         generate_checksum(tarball)
 
     # Cleanup
     if clean_build:
-        print
+        print()
         remove_build_dir(release_dir)
 
     # Restore previous umask
     os.umask(old_umask)
 
-    print "Done!\n"
+    print("Done!\n")
 
 # end main()
 
