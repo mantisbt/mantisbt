ec07d9c24b7411948edf43172199cb9eec95a0da
Victor Boctor <victor@mantishub.net>
2020-05-16 14:14:11 -0700

    Password managers shouldn’t trigger on monitor box
    Fixes #26963

--- bug_view_inc.php
+++ bug_view_inc.php
@@ -690,8 +690,7 @@ if( $t_flags['monitor_show'] ) {
 			<form method="get" action="bug_monitor_add.php" class="form-inline noprint">
 			<?php echo form_security_field( 'bug_monitor_add' ) ?>
 				<input type="hidden" name="bug_id" value="<?php echo (integer)$f_issue_id; ?>" />
-				<label for="bug_monitor_list_username"><?php echo lang_get( 'username' ) ?></label>
-				<input type="text" class="input-sm" id="bug_monitor_list_username" name="username" />
+				<input type="text" class="input-sm" id="bug_monitor_list_user_to_add" name="user_to_add" />
 				<input type="submit" class="btn btn-primary btn-sm btn-white btn-round" value="<?php echo lang_get( 'add_user_to_monitor' ) ?>" />
 			</form>
 			<?php } ?>
