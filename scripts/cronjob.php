85c9d0f84423071d4c31833796d067edc7f1e3dc
Victor Boctor <victor@mantishub.net>
2020-06-11 22:25:38 -0700

    Create a Cronjob script and plugin event
    This way there is one cronjob script to execute which triggers an event, then all plugins can hook into it.
    
    Fixes #27882

---
+++ scripts/cronjob.php
@@ -0,0 +1,38 @@
+#!/usr/bin/php -q
+<?php
+# MantisBT - A PHP based bugtracking system
+
+# MantisBT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# MantisBT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
+# See the README and LICENSE files for details
+
+/**
+ * Cron Script to trigger cronjob work for plugins
+ */
+
+/**
+ * Global Bypass http headers
+ */
+global $g_bypass_headers;
+$g_bypass_headers = 1;
+
+require_once( dirname( dirname( __FILE__ ) ) . '/core.php' );
+
+# Make sure this script doesn't run via the webserver
+if( php_sapi_name() != 'cli' ) {
+	echo "cronjob.php is not allowed to run through the webserver.\n";
+	exit( 1 );
+}
+
+event_signal( 'EVENT_CRONJOB' );
+exit( 0 );
