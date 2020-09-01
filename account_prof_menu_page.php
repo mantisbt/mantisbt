102e2e0ce01534a0aaefef2da0e900261bafe032
Damien Regad <dregad@mantisbt.org>
2020-09-01 19:08:47 +0200

    Add "Default Profile" column
    Users can select their default profile, but the UI does not indicate
    which one of the profiles is their default one.
    
    Fixes #27256

--- account_prof_menu_page.php
+++ account_prof_menu_page.php
@@ -181,6 +181,7 @@ if( $g_global_profiles ) {
 								<th><?php echo lang_get( 'os' ) ?></th>
 								<th><?php echo lang_get( 'os_build' ) ?></th>
 								<th><?php echo lang_get( 'global_profile' ) ?></th>
+								<th><?php echo lang_get( 'default_profile' ) ?></th>
 								<th class="center"><?php echo lang_get( 'actions' ) ?></th>
 							</tr>
 						</thead>
@@ -208,6 +209,11 @@ if( $g_global_profiles ) {
 								<td><?php echo string_display_line( $v_os_build );  ?></td>
 								<td class="center">
 									<?php if( $t_is_global_profile ) { ?>
+									<i class="ace-icon fa fa-check fa-lg"></i>
+									<?php } ?>
+								</td>
+								<td class="center">
+									<?php if( $t_is_default_profile ) { ?>
 									<i class="ace-icon fa fa-check fa-lg"></i>
 									<?php } ?>
 								</td>
