d50092f42bfcff52a34c2bb28f5b381da311043e
Damien Regad <dregad@mantisbt.org>
2020-09-10 19:27:55 +0200

    Move Delete button to form footer: Version
    Using the HTML5 button's `formaction` property we can get rid of the
    separate form that caused the button to be displayed below the Edit box,
    and put the Delete button in the main form's footer.
    
    Fixes #27274

--- manage_proj_ver_delete.php
+++ manage_proj_ver_delete.php
@@ -71,9 +71,6 @@ version_remove( $f_version_id );
 form_security_purge( 'manage_proj_ver_delete' );
 
 layout_page_header( null, $t_redirect_url );
-
 layout_page_begin( 'manage_overview_page.php' );
-
 html_operation_successful( $t_redirect_url );
-
 layout_page_end();
