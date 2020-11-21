67482a701400656d23f1e3c0e683499ce4dafca2
Damien Regad <dregad@mantisbt.org>
2020-11-22 00:20:41 +0100

    Fix invalid HTML
    The form was incorrectly defined in the middle of the table. It is now
    wrapping it.

--- manage_user_edit_page.php
+++ manage_user_edit_page.php
@@ -319,6 +319,7 @@ if( $t_reset || $t_unlock || $t_delete || $t_impersonate ) {
 <div class="widget-body">
 <div class="widget-main no-padding">
 <div class="form-container">
+<form id="manage-user-project-add-form" method="post" action="manage_user_proj_add.php">
 <div class="table-responsive">
 	<table class="table table-bordered table-condensed table-striped">
         <tr>
@@ -327,7 +328,6 @@ if( $t_reset || $t_unlock || $t_delete || $t_impersonate ) {
             </td>
             <td><?php print_project_user_list( $t_user['id'] ) ?></td>
         </tr>
-        <form id="manage-user-project-add-form" method="post" action="manage_user_proj_add.php">
         <fieldset>
             <?php echo form_security_field( 'manage_user_proj_add' ) ?>
             <input type="hidden" name="user_id" value="<?php echo $t_user['id'] ?>" />
@@ -357,9 +357,9 @@ if( $t_reset || $t_unlock || $t_delete || $t_impersonate ) {
             </td>
         </tr>
         </fieldset>
-        </form>
 	</table>
 </div>
+</form>
 </div>
 </div>
 </div>
