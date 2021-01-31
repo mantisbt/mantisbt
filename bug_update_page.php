mantisbt/mantisbt

commit 5e509923a9c58ec277f77c1e5ebc56ca147e6a61

Use new icon API functions
Replace hardcoded fontawesome <i> tags with new print_icon() or
icon_get() calls.

Fixes #27828

bug_update_page.php
@@ -168,7 +168,7 @@ layout_page_begin();
 		<div class="widget-box widget-color-blue2">
 		<div class="widget-header widget-header-small">
 			<h4 class="widget-title lighter">
-				<i class="ace-icon fa fa-comments"></i>
+				<?php print_icon( 'fa-comments', 'ace-icon' ); ?>
 				<?php echo lang_get( 'updating_bug_advanced_title' ) ?>
 			</h4>
 			<div class="widget-toolbar no-border">
@@ -341,7 +341,7 @@ if( $t_show_reporter || $t_show_handler || $t_show_due_date ) {
 			echo '<input ' . helper_get_tab_index() . ' type="text" id="due_date" name="due_date" class="datetimepicker input-sm" size="16" ' .
 				'data-picker-locale="' . lang_get_current_datetime_locale() .  '" data-picker-format="' . config_get( 'datetime_picker_format' ) . '" ' .
 				'maxlength="16" value="' . $t_date_to_display . '" />';
-			echo '<i class="fa fa-calendar fa-xlg datetimepicker"></i>';
+			print_icon( 'fa-calendar', 'fa-xlg datetimepicker' );
 		} else {
 			if( !date_is_null( $t_bug->due_date ) ) {
 				echo date( config_get( 'short_date_format' ), $t_bug->due_date );
@@ -424,7 +424,7 @@ if( $t_show_status || $t_show_resolution ) {
 		$t_status_css = html_get_status_css_fg( $t_bug->status );
 
 		echo '<td class="bug-status">';
-		echo '<i class="fa fa-square fa-status-box ' . $t_status_css . '"></i> ';
+		print_icon( 'fa-square', 'fa-status-box ' . $t_status_css );
 		print_status_option_list( 'status', $t_bug->status,
 			access_can_close_bug( $t_bug ),
 			$t_bug->project_id );

commit 1c904c981176c4bbf79b3296ebfdba30bfd5dbc9

Add missing space between status icon and select
Regression from 5e509923a9c58ec277f77c1e5ebc56ca147e6a61

Issue #27828

bug_update_page.php
@@ -425,6 +425,7 @@ if( $t_show_status || $t_show_resolution ) {
 
 		echo '<td class="bug-status">';
 		print_icon( 'fa-square', 'fa-status-box ' . $t_status_css );
+		echo '&nbsp;';
 		print_status_option_list( 'status', $t_bug->status,
 			access_can_close_bug( $t_bug ),
 			$t_bug->project_id );