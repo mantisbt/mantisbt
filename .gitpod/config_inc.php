194170dac742d622158bb92d3edf4f87f8795ff4
Victor Boctor <vboctor@gmail.com>
2021-02-20 23:24:32 +0000

    Add config file used in gitpod

---
+++ .gitpod/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'UTC';
+
+$g_crypto_master_salt     = 'OBPqDjBNK5cMvsM50FJ3k1bzZb4GwLUf/SE+ZdD169I=';
