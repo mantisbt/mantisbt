281f0d6a35445b006bedc044f40feb1e48282a8f
Victor Boctor <vboctor@gmail.com>
2021-02-24 05:18:45 +0000

    Update test seed data to match current CI

--- .gitpod/setup-db.sh
+++ .gitpod/setup-db.sh
@@ -6,12 +6,12 @@ php ./admin/upgrade_unattended.php
 mysql -u root bugtracker -e "INSERT INTO mantis_api_token_table (user_id, name, hash, date_created, date_used) VALUES (1, 'test api token', 'test-token', 0, 0)"
 
 # Create Project
-mysql -u root bugtracker -e "INSERT INTO mantis_project_table (name, enabled, view_state, access_min, category_id, inherit_global, description) VALUES ('Test', 1, 10, 10, 1, 1, '')"
+mysql -u root bugtracker -e "INSERT INTO mantis_project_table (name, enabled, view_state, access_min, category_id, inherit_global, description) VALUES ('Test Project', 1, 10, 10, 1, 1, 'Test Project')"
 
 # Create 3 versions as needed by test cases
-mysql -u root bugtracker -e "INSERT INTO mantis_project_version_table (project_id, version, description, released) VALUES (1, 'v1', 'version 1', 1)"
-mysql -u root bugtracker -e "INSERT INTO mantis_project_version_table (project_id, version, description, released) VALUES (1, 'v2', 'version 2', 1)"
-mysql -u root bugtracker -e "INSERT INTO mantis_project_version_table (project_id, version, description, released) VALUES (1, 'v3', 'version 3', 0)"
+mysql -u root bugtracker -e "INSERT INTO mantis_project_version_table (project_id, version, description, released, obsolete) VALUES (1, '1.0.0', 'Obsolete Version', 1, 1)"
+mysql -u root bugtracker -e "INSERT INTO mantis_project_version_table (project_id, version, description, released, obsolete) VALUES (1, '1.1.0', 'Released Version', 1, 0)"
+mysql -u root bugtracker -e "INSERT INTO mantis_project_version_table (project_id, version, description, released, obsolete) VALUES (1, '2.0.0', 'Future Version', 0, 0)"
 
 # Create 2 tags as needed by test cases
 mysql -u root bugtracker -e "INSERT INTO mantis_tag_table (user_id, name, description, date_created, date_updated) VALUES (0, 'modern-ui', '', 1, 1)"
