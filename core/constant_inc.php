c5bd4a3a1d5283796e6e4a14880961cfab697364
Damien Regad <dregad@mantisbt.org>
2021-03-08 00:17:40 +0100

    Set version to 2.26.0-dev

--- core/constant_inc.php
+++ core/constant_inc.php
@@ -21,7 +21,7 @@
 /**
  * Mantis Version
  */
-define( 'MANTIS_VERSION', '2.25.0' );
+define( 'MANTIS_VERSION', '2.26.0-dev' );
 define( 'FILTER_VERSION', 'v9' );
 
 # --- constants -------------------
