b86a832873d9bb14f21c7540f1eed9591dcf5d18
Damien Regad <dregad@mantisbt.org>
2022-05-10 15:35:29 +0200

    Bump version to 2.25.4
    Signed-off-by: Damien Regad <dregad@mantisbt.org>

--- core/constant_inc.php
+++ core/constant_inc.php
@@ -21,7 +21,7 @@
 /**
  * Mantis Version
  */
-define( 'MANTIS_VERSION', '2.25.4-dev' );
+define( 'MANTIS_VERSION', '2.25.4' );
 define( 'FILTER_VERSION', 'v9' );
 
 # --- constants -------------------
