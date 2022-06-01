d105eb4f659c3079226399ae812283479da5c90d
Damien Regad <dregad@mantisbt.org>
2022-06-01 17:43:03 +0200

    Jump to Categories section after update
    Use existing `#categories` anchor (div id) to jump straight to the
    Categories section when returning from an update operation in the Edit
    Project page.
    
    Fixes #30435

--- manage_proj_cat_copy.php
+++ manage_proj_cat_copy.php
@@ -80,7 +80,7 @@ form_security_purge( 'manage_proj_cat_copy' );
 if( $f_project_id == ALL_PROJECTS ) {
 	$t_redirect_url = 'manage_proj_page.php';
 } else {
-	$t_redirect_url = 'manage_proj_edit_page.php?project_id=' . $f_project_id;
+	$t_redirect_url = 'manage_proj_edit_page.php?project_id=' . $f_project_id . '#categories';
 }
 
 print_header_redirect( $t_redirect_url );
