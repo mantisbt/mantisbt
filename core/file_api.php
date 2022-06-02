c3cbfe2c90240d15bf0e450254cbeb68050655ba
Damien Regad <dregad@mantisbt.org>
2022-06-02 11:31:55 +0200

    Fix deprecated usage of ${} string interpolation
    The "Deprecate ${} string interpolation" RFC [1] has been acceoted and
    will be implemented in PHP 8.2.
    
    The Mantis code was using the deprecated syntax in a few places, this
    fixes these occurrences.
    
    Thanks to ChrisG for the original patch.
    
    Fixes #30429
    
    [1]: https://wiki.php.net/rfc/deprecate_dollar_brace_string_interpolation

--- core/file_api.php
+++ core/file_api.php
@@ -795,7 +795,7 @@ function diskfile_is_name_unique( $p_name, $p_filepath ) {
  * @return boolean true if unique
  */
 function file_is_name_unique( $p_name, $p_bug_id, $p_table = 'bug' ) {
-	$t_file_table = db_get_table( "${p_table}_file" );
+	$t_file_table = db_get_table( "{$p_table}_file" );
 
 	db_param_push();
 	$t_query = 'SELECT COUNT(*) FROM ' . $t_file_table . ' WHERE filename=' . db_param();
