488fff8e9acfeaeee595785b8a5655ac9f226bf1
Chris Graham <chris@ocproducts.com>
2023-01-02 06:27:05 -0600

    Add canonical meta tag to view issue page
    It is possible for issue URLs to have additional parameters in some 
    situations. Search engines such as Google will index better if they 
    know which are the set of canonical URLs. It reduces load through 
    over-indexing, allows the search engine to prioritise the indexing of 
    URLs that are correct, and avoids extraneous or non-ideal URLs getting 
    into the index.
    
    Fixes #31833, PR https://github.com/mantisbt/mantisbt/pull/1862

--- core/layout_api.php
+++ core/layout_api.php
@@ -47,15 +47,19 @@ require_api( 'utility_api.php' );
 
 /**
  * Print the page header section
- * @param string $p_page_title   Html page title.
- * @param string $p_redirect_url URL to redirect to if necessary.
- * @param string $p_page_id      The page id.
+ * @param string $p_page_title    Html page title.
+ * @param string $p_redirect_url  URL to redirect to if necessary: has to be relative to the install path {@see $g_path}.
+ * @param string $p_page_id       The page id.
+ * @param string $p_canonical_url Canonical URL if necessary: has to be relative to the install path {@see $g_path}.
  * @return void
  */
-function layout_page_header( $p_page_title = null, $p_redirect_url = null, $p_page_id = null ) {
+function layout_page_header( $p_page_title = null, $p_redirect_url = null, $p_page_id = null, $p_canonical_url = null ) {
 	layout_page_header_begin( $p_page_title );
 	if( $p_redirect_url !== null ) {
 		html_meta_redirect( $p_redirect_url );
+	}
+	if( $p_canonical_url !== null ) {
+		html_meta_canonical( $p_canonical_url );
 	}
 
 	layout_page_header_end( $p_page_id );
