mantisbt/mantisbt

commit d9464fe8132f6cfd694625c8d050575aed95d5ba

Restore missing version date on Bug View page
Fixing a regression introduced with IssueViewPageCommand, which removed
the prepare_version_string() calls and only displayed the version name,
as returned by the Command (see 2b163046849d3445894537f7a7b4762edd41ff8b).

The Command now returns the formatted version string in $t_issue_view
array.

Fixes #31889

core/commands/IssueViewPageCommand.php
@@ -131,6 +131,19 @@ class IssueViewPageCommand extends Command {
 			in_array( 'target_version', $t_fields ) &&
 			access_has_bug_level( config_get( 'roadmap_view_threshold' ), $t_issue_id );
 
+		$t_issue_view['product_version'] = prepare_version_string( $t_project_id,
+			$t_issue['version']['id'],
+			false
+		);
+		$t_issue_view['target_version'] = prepare_version_string( $t_project_id,
+			$t_issue['target_version']['id'],
+			false
+		);
+		$t_issue_view['fixed_in_version'] = prepare_version_string( $t_project_id,
+			$t_issue['fixed_in_version']['id'],
+			false
+		);
+
 		$t_issue_view['form_title'] = lang_get( 'bug_view_title' );
 
 		if( config_get_global( 'wiki_enable' ) == ON ) {

bug_view_inc.php
@@ -472,7 +472,7 @@ if( ( $t_flags['versions_product_version_show'] && isset( $t_issue['version'] ) 
 	# Product Version
 	if( $t_flags['versions_product_version_show'] && isset( $t_issue['version'] ) ) {
 		echo '<th class="bug-product-version category">', lang_get( 'product_version' ), '</th>';
-		echo '<td class="bug-product-version">', string_display_line( $t_issue['version']['name'] ), '</td>';
+		echo '<td class="bug-product-version">', string_display_line( $t_issue_view['product_version'] ), '</td>';
 	} else {
 		$t_spacer += 2;
 	}
@@ -505,7 +505,7 @@ if( ( $t_flags['versions_target_version_show'] && isset( $t_issue['target_versio
 	if( $t_flags['versions_target_version_show'] && isset( $t_issue['target_version'] ) ) {
 		# Target Version
 		echo '<th class="bug-target-version category">', lang_get( 'target_version' ), '</th>';
-		echo '<td class="bug-target-version">', string_display_line( $t_issue['target_version']['name'] ), '</td>';
+		echo '<td class="bug-target-version">', string_display_line( $t_issue_view['target_version'] ), '</td>';
 	} else {
 		$t_spacer += 2;
 	}
@@ -513,7 +513,7 @@ if( ( $t_flags['versions_target_version_show'] && isset( $t_issue['target_versio
 	# fixed in version
 	if( $t_flags['versions_fixed_in_version_show'] && isset( $t_issue['fixed_in_version'] ) ) {
 		echo '<th class="bug-fixed-in-version category">', lang_get( 'fixed_in_version' ), '</th>';
-		echo '<td class="bug-fixed-in-version">', string_display_line( $t_issue['fixed_in_version']['name'] ), '</td>';
+		echo '<td class="bug-fixed-in-version">', string_display_line( $t_issue_view['fixed_in_version'] ), '</td>';
 	} else {
 		$t_spacer += 2;
 	}

commit 6671bc5c851b1dc0860871d3f9b882a79a352d13

Fix error when displaying Issue without Version
Commit d9464fe8132f6cfd694625c8d050575aed95d5ba introduced a regression
when displaying an Issue without Product, Target or Fixed In Version:

APPLICATION ERROR 1601 - Version "" not found
PHP Notice (or Warning on 8.1+): Undefined array key "version"

This is because mci_issue_data_as_array() removes null keys and empty
arrays from the Issue data, so the IssueViewPageCommand needs to check
for array key existence before calling prepared_version_string().

Fixes #32086, #31889

core/commands/IssueViewPageCommand.php
@@ -131,18 +131,16 @@ class IssueViewPageCommand extends Command {
 			in_array( 'target_version', $t_fields ) &&
 			access_has_bug_level( config_get( 'roadmap_view_threshold' ), $t_issue_id );
 
-		$t_issue_view['product_version'] = prepare_version_string( $t_project_id,
-			$t_issue['version']['id'],
-			false
-		);
-		$t_issue_view['target_version'] = prepare_version_string( $t_project_id,
-			$t_issue['target_version']['id'],
-			false
-		);
-		$t_issue_view['fixed_in_version'] = prepare_version_string( $t_project_id,
-			$t_issue['fixed_in_version']['id'],
-			false
-		);
+		# Formatted version strings for display
+		$t_issue_view['product_version'] = isset( $t_issue['version'] )
+			? prepare_version_string( $t_project_id, $t_issue['version']['id'], false )
+			: '';
+		$t_issue_view['target_version'] = isset( $t_issue['target_version'] )
+			? prepare_version_string( $t_project_id, $t_issue['target_version']['id'], false )
+			: '';
+		$t_issue_view['fixed_in_version'] = isset( $t_issue['fixed_in_version'] )
+			? prepare_version_string( $t_project_id, $t_issue['fixed_in_version']['id'], false )
+			: '';
 
 		$t_issue_view['form_title'] = lang_get( 'bug_view_title' );
 