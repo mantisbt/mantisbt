2379768f0f39b005a13dce6a1bf9ff310f34e188
Victor Boctor <vboctor@gmail.com>
2023-03-26 20:00:38 -0700

    Update Web UI to use command for config delete
    Fixes #32258

--- adm_config_delete.php
+++ adm_config_delete.php
@@ -34,15 +34,11 @@
  */
 
 require_once( 'core.php' );
-require_api( 'access_api.php' );
-require_api( 'config_api.php' );
-require_api( 'constant_inc.php' );
 require_api( 'form_api.php' );
 require_api( 'gpc_api.php' );
 require_api( 'helper_api.php' );
 require_api( 'lang_api.php' );
 require_api( 'print_api.php' );
-require_api( 'project_api.php' );
 
 form_security_validate( 'adm_config_delete' );
 
@@ -50,15 +46,23 @@ $f_user_id = gpc_get_int( 'user_id' );
 $f_project_id = gpc_get_int( 'project_id' );
 $f_config_option = gpc_get_string( 'config_option' );
 
-access_ensure_global_level( config_get( 'set_configuration_threshold' ) );
-
-if( $f_project_id != ALL_PROJECTS ) {
-	project_ensure_exists( $f_project_id );
-}
-
 helper_ensure_confirmed( lang_get( 'delete_config_sure_msg' ), lang_get( 'delete' ) );
 
-config_delete( $f_config_option, $f_user_id, $f_project_id );
+$t_data = array(
+	'payload' => array(
+		'user' => array( 'id' => $f_user_id ),
+		'project' => array( 'id' => $f_project_id ),
+		'configs' => array(
+			array(
+				'option' => $f_config_option,
+				'value' => null,
+			),
+		),
+	),
+);
+
+$t_command = new ConfigsSetCommand( $t_data );
+$t_command->execute();
 
 form_security_purge( 'adm_config_delete' );
 
