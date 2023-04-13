e6def156d1f4783f83aa72b4e083d4887add75dc
Damien Regad <dregad@mantisbt.org>
2023-04-13 14:59:40 +0200

    Bump version to 2.25.8-dev

--- core/constant_inc.php
+++ core/constant_inc.php
@@ -21,7 +21,7 @@
 /**
  * Mantis Version
  */
-define( 'MANTIS_VERSION', '2.25.7' );
+define( 'MANTIS_VERSION', '2.25.8-dev' );
 define( 'FILTER_VERSION', 'v9' );
 
 # --- constants -------------------
