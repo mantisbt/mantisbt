aadf1787da021909ab7637b04288ea287ba568ca
Victor Boctor <vboctor@gmail.com>
2023-04-30 21:54:00 -0700

    Update Web UI to use ProjectUsersDeleteCommand
    Fixes #32467

--- manage_proj_user_remove.php
+++ manage_proj_user_remove.php
@@ -23,30 +23,24 @@
  * @link http://www.mantisbt.org
  *
  * @uses core.php
- * @uses access_api.php
  * @uses authentication_api.php
- * @uses config_api.php
  * @uses form_api.php
  * @uses gpc_api.php
  * @uses helper_api.php
  * @uses html_api.php
  * @uses lang_api.php
  * @uses print_api.php
- * @uses project_api.php
  * @uses user_api.php
  */
 
 require_once( 'core.php' );
-require_api( 'access_api.php' );
 require_api( 'authentication_api.php' );
-require_api( 'config_api.php' );
 require_api( 'form_api.php' );
 require_api( 'gpc_api.php' );
 require_api( 'helper_api.php' );
 require_api( 'html_api.php' );
 require_api( 'lang_api.php' );
 require_api( 'print_api.php' );
-require_api( 'project_api.php' );
 require_api( 'user_api.php' );
 
 form_security_validate( 'manage_proj_user_remove' );
@@ -55,21 +49,20 @@ auth_reauthenticate();
 $f_project_id = gpc_get_int( 'project_id' );
 $f_user_id = gpc_get_int( 'user_id', 0 );
 
-# We should check both since we are in the project section and an
-#  admin might raise the first threshold and not realize they need
-#  to raise the second
-access_ensure_project_level( config_get( 'manage_project_threshold' ), $f_project_id );
-access_ensure_project_level( config_get( 'project_user_threshold' ), $f_project_id );
+$t_data = array(
+	'payload' => array(
+		'project' => array( 'id' => $f_project_id ),
+		'user' => array( 'id' => $f_user_id )
+	)
+);
+
+$t_command = new ProjectUsersDeleteCommand( $t_data );
+$t_command->validate();
 
 if( 0 == $f_user_id ) {
 	# Confirm with the user
 	helper_ensure_confirmed( lang_get( 'remove_all_users_sure_msg' ), lang_get( 'remove_all_users_button' ) );
-
-	project_remove_all_users( $f_project_id, access_get_project_level( $f_project_id ) );
 } else {
-	# Don't allow removal of users from the project who have a higher access level than the current user
-	access_ensure_project_level( access_get_project_level( $f_project_id, $f_user_id ), $f_project_id );
-
 	$t_user = user_get_row( $f_user_id );
 
 	# Confirm with the user
@@ -80,9 +73,9 @@ if( 0 == $f_user_id ) {
 		),
 		lang_get( 'remove_user_button' )
 	);
-
-	project_remove_user( $f_project_id, $f_user_id );
 }
+
+$t_command->execute();
 
 form_security_purge( 'manage_proj_user_remove' );
 
