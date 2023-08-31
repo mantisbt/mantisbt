4a20beb2d08ba7655c1db2fef5c2a5c6963e0eff
Victor Boctor <vboctor@gmail.com>
2023-08-31 00:01:50 -0700

    Show operation successful for pages that may be confusing
    Some pages redirect to themselves and they are not refreshed in a way that indicate changes have been applied.
    
    Fixes #5189

--- account_prefs_update.php
+++ account_prefs_update.php
@@ -139,5 +139,5 @@ user_pref_set( $f_user_id, $t_prefs, ALL_PROJECTS );
 
 form_security_purge( 'account_prefs_update' );
 
-print_header_redirect( $f_redirect_url );
+print_successful_redirect( $f_redirect_url, /* force_show */ true );
 
