8cfd108762e005118623fbd8b40cec280ac87151
tarip18 <taripinem18@gmail.com>
2024-02-07 10:08:48 +0700

    feat:updating my view page.php adding space

--- my_view_page.php
+++ my_view_page.php
@@ -55,6 +55,8 @@ require_api( 'user_api.php' );
 require_api( 'layout_api.php' );
 require_css( 'status_config.php' );
 
+
+
 auth_ensure_user_authenticated();
 
 $t_current_user_id = auth_get_current_user_id();
