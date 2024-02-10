99686319246e471451b7389fcbda8c5d9c715d30
Damien Regad <dregad@mantisbt.org>
2024-02-10 15:03:53 +0100

    Add pg_connect test

--- build/test_db_connect.php
+++ build/test_db_connect.php
@@ -4,10 +4,16 @@ include 'vendor/autoload.php';
 $f_db_type = getenv( 'DB_TYPE' );
 $f_hostname = getenv( 'DB_HOSTNAME' );
 $f_admin_username = getenv( 'DB_USER' );
-$f_admin_password = getenv( 'DB_PASSWORD' );
+$f_admin_password = (string)getenv( 'DB_PASSWORD' );
 
-printf("type=%s\nhost=%s\nuser=%s\npass=%s\n\n",
-	$f_db_type, $f_hostname, $f_admin_username, $f_admin_password );
+[$host, $port] = explode(':', $f_hostname);
+$str = sprintf("host='%s' port='%d' user='%s' password='%s'",
+	addcslashes($host, "'\\"),
+	$port,
+	addcslashes($f_admin_username, "'\\"),
+	addcslashes($f_admin_password, "'\\")
+);
+var_dump($str, pg_connect( $str ) );
 
 $g_db = ADONewConnection( $f_db_type );
 $g_db->debug = true;
