mantisbt/mantisbt

commit e3f572c0f9dd8e1820d76df23281a99e07636bea

Display warning if Issue has disabled category
Warning icon with descriptive tooltip is displayed when

- viewing an issue and allowed to edit it
- editing issue

Issue #31017

core/category_api.php
@@ -715,3 +715,14 @@ function category_ensure_can_delete( $p_category_id ) {
 		trigger_error( ERROR_CATEGORY_CANNOT_DELETE_HAS_ISSUES, ERROR );
 	}
 }
+
+/**
+ * Check if category is enabled.
+ *
+ * @param int $p_category_id Category identifier.
+ *
+ * @return bool True if enabled, false otherwise
+ */
+function category_is_enabled( $p_category_id ) {
+	return category_get_field( $p_category_id, 'status' ) == CATEGORY_STATUS_ENABLED;
+}

bug_update_page.php
@@ -234,6 +234,13 @@ if( $t_show_id || $t_show_project || $t_show_category || $t_show_view_state || $
 	echo '<td>';
 
 	if( $t_show_category ) {
+        if( !category_is_enabled( $t_bug->category_id ) ) {
+			print_icon( 'warning',
+				'fa-status-box bigger-125 red',
+				lang_get( 'category_disabled' )
+			);
+			echo "&nbsp;";
+		}
 		echo '<select ' . helper_get_tab_index()
 			. ( $t_allow_no_category ? '' : ' required' )
 			. ' id="category_id" name="category_id" class="input-sm">';

bug_view_inc.php
@@ -256,11 +256,18 @@ if( $t_flags['id_show'] || $t_flags['project_show'] || $t_flags['category_show']
 	echo '<td class="bug-project">', $t_flags['project_show'] && isset( $t_issue['project']['name'] ) ? string_display_line( $t_issue['project']['name'] ) : '', '</td>';
 
 	# Category
-	echo '<td class="bug-category">',
-		$t_flags['category_show'] && isset( $t_issue['category']['name'] )
-			? string_display_line( $t_issue['category']['name'] )
-			: '',
-		'</td>';
+	echo '<td class="bug-category">';
+	if( $t_flags['category_show'] && isset( $t_issue['category']['name'] ) ) {
+		if( $t_flags['can_update'] && !category_is_enabled( $t_issue['category']['id'] ) ) {
+			print_icon( 'warning',
+				'bigger-125 red',
+				lang_get( 'category_disabled' )
+			);
+			echo "&nbsp;";
+		}
+		echo string_display_line( $t_issue['category']['name'] );
+	}
+	echo '</td>';
 
 	# View Status
 	echo '<td class="bug-view-status">', $t_flags['view_state_show'] && isset( $t_issue['view_state']['label'] ) ? string_display_line( $t_issue['view_state']['label'] ) : '', '</td>';

commit a16fa867276dc15ff68e622bf6a4b57bce7b11f4

Always consider Category 0 as enabled
Fixes a regression introduced by e3f572c0f9dd8e1820d76df23281a99e07636bea.

APPLICATION ERROR 1502 (Category not found) prevents editing an issue
without a category.

Issue #31017

core/category_api.php
@@ -729,10 +729,13 @@ function category_ensure_can_delete( $p_category_id ) {
 /**
  * Check if category is enabled.
  *
+ * Category 0 (no category) is always considered as enabled.
+ *
  * @param int $p_category_id Category identifier.
  *
  * @return bool True if enabled, false otherwise
  */
 function category_is_enabled( $p_category_id ) {
-	return category_get_field( $p_category_id, 'status' ) == CATEGORY_STATUS_ENABLED;
+	return $p_category_id == 0
+		|| category_get_field( $p_category_id, 'status' ) == CATEGORY_STATUS_ENABLED;
 }