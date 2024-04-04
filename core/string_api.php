100427709e55e5d4da11e1dbb8d6816418b64646
Damien Regad <dregad@mantisbt.org>
2024-04-04 19:08:05 +0200

    Fix handling of hex-encoded character references
    This is necessary when Markdown processing is switched off.
    
    Fixes #34393

--- core/string_api.php
+++ core/string_api.php
@@ -901,7 +901,7 @@ function string_html_specialchars( $p_string ) {
 	# achumakov: @ added to avoid warning output in unsupported codepages
 	# e.g. 8859-2, windows-1257, Korean, which are treated as 8859-1.
 	# This is VERY important for Eastern European, Baltic and Korean languages
-	return preg_replace( '/&amp;(#[0-9]+|[a-z]+);/i', '&$1;', @htmlspecialchars( $p_string, ENT_COMPAT, 'utf-8' ) );
+	return preg_replace( '/&amp;(#x?[0-9]+|[a-z]+);/i', '&$1;', @htmlspecialchars( $p_string, ENT_COMPAT, 'utf-8' ) );
 }
 
 /**
