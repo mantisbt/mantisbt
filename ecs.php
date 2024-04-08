f24b8de8e15254cb4095ad7a0d059c7b032b1074
grummbeer <kde@streber24.de>
2024-04-09 01:25:09 +0200

    Add some more fixers, this commit will be removed

--- ecs.php
+++ ecs.php
@@ -29,16 +29,135 @@ return ECSConfig::configure()
 	])
     ->withRules([
 		/**
-		 * Operator: binary_operator_spaces
+		 * Basic: Encoding
 		 *
-		 * Configurable. Default value: 'single_space'
-		 * Different operators can be configured separately.
+		 * PHP code MUST use only UTF-8 without BOM (remove BOM).
 		 *
-		 * "$a=1+2;" > "$a = 1 + 2;"
-		 *
-		 * @see https://cs.symfony.com/doc/rules/operator/binary_operator_spaces.html
+		 * @see https://cs.symfony.com/doc/rules/basic/encoding.html
 		 */
-		\PhpCsFixer\Fixer\Operator\BinaryOperatorSpacesFixer::class,
+		\PhpCsFixer\Fixer\Basic\EncodingFixer::class,
+
+		/**
+		 * Whitespace: Line encoding
+		 *
+		 * All PHP files must use same line ending. Default is "\n"
+		 *
+		 * Sniff: LineEndingsSniff with ['eolChar' => "\n"]
+		 *
+		 * @see https://cs.symfony.com/doc/rules/whitespace/line_ending.html
+		 */
+		\PhpCsFixer\Fixer\Whitespace\LineEndingFixer::class,
+
+		/**
+		 * Whitespace: No trailing whitespaces
+		 *
+		 * Remove trailing whitespace at the end of non-blank lines.
+		 *
+		 * "$foo = 'bar'···" > "$foo = 'bar'"
+		 *
+		 * @see https://cs.symfony.com/doc/rules/whitespace/no_trailing_whitespace.html
+		 */
+		\PhpCsFixer\Fixer\Whitespace\NoTrailingWhitespaceFixer::class,
+
+		/**
+		 * Whitespace: Single blank line at eof
+		 *
+		 * A PHP file without end tag must always end with a single empty line feed.
+		 *
+		 * Sniff: PSR2.Files.EndFileNewline
+		 *
+		 * @see https://cs.symfony.com/doc/rules/whitespace/single_blank_line_at_eof.html
+		 */
+		\PhpCsFixer\Fixer\Whitespace\SingleBlankLineAtEofFixer::class,
+
+		/**
+		 * Whitespaces: No whitespace in blank lines
+		 *
+		 * Remove trailing whitespace at the end of blank lines.
+		 *
+		 * <input>
+		 * ···
+		 * $a = 1;"
+		 * </input>
+		 * <output>
+		 *
+		 *  $a = 1;"
+		 * </output>
+		 *
+		 * @see https://cs.symfony.com/doc/rules/whitespace/no_whitespace_in_blank_line.html
+		 */
+		\PhpCsFixer\Fixer\Whitespace\NoWhitespaceInBlankLineFixer::class,
+
+		/**
+		 * String: Single quotes
+		 *
+		 * Convert double quotes to single quotes for simple strings.
+		 *
+		 * Configurable. Default is keep double-quoted strings if they contain a
+		 * single-quoted string.
+		 *
+		 * $a = "sample"                       > $a = 'sample'
+		 * $b = "sample with 'single-quotes'"  > $b = "sample with 'single-quotes'"
+		 *
+		 * @see https://cs.symfony.com/doc/rules/string_notation/single_quote.html
+		 */
+		\PhpCsFixer\Fixer\StringNotation\SingleQuoteFixer::class,
+
+		/**
+		 * PHP tag: Full opening tag
+		 *
+		 * "<?" > "<?php"
+		 *
+		 * @see https://cs.symfony.com/doc/rules/php_tag/full_opening_tag.html
+		 */
+		\PhpCsFixer\Fixer\PhpTag\FullOpeningTagFixer::class,
+
+		/**
+		 * PHP tag: Echo tag syntax
+		 *
+		 * Configurable. Default is "format" = "short", "long_function" = "echo"
+		 *
+		 * "<?= …" > "<?php echo …"
+		 *
+		 * @see https://cs.symfony.com/doc/rules/php_tag/echo_tag_syntax.html
+		 */
+		\PhpCsFixer\Fixer\PhpTag\EchoTagSyntaxFixer::class,
+
+		/**
+		 * PHP tag: No closing tag
+		 *
+		 * The closing ?> tag MUST be omitted from files containing only PHP.
+		 *
+		 * @see https://cs.symfony.com/doc/rules/php_tag/no_closing_tag.html
+		 */
+		\PhpCsFixer\Fixer\PhpTag\NoClosingTagFixer::class,
+
+		/**
+		 * Casing: Constant case: lower
+		 *
+		 * The PHP constants true, false, and null MUST be written
+		 * using the correct casing.
+		 *
+		 * Configurable. Default is "lower"
+		 *
+		 * "$a = FALse" > "a = false"
+		 *
+		 * @see https://cs.symfony.com/doc/rules/casing/constant_case.html
+		 */
+		\PhpCsFixer\Fixer\Casing\ConstantCaseFixer::class,
+
+		/**
+		 * Casing: Lowercase keywords
+		 *
+		 * PHP keywords MUST be in lower case.
+		 *
+		 * "FOREACH( $a AS $B )" > "foreach( $a as $B )"
+		 *
+		 * Sniff: Generic.PHP.LowerCaseKeyword
+		 *
+		 * @see https://cs.symfony.com/doc/rules/casing/lowercase_keywords.html
+		 */
+		\PhpCsFixer\Fixer\Casing\LowercaseKeywordsFixer::class,
 
 		/**
 		 * Import: no_unused_imports
