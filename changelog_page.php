e4377be367e1dae6d2d0f0812418dc67652445a0
Pasquale Pizzuti <produzionepp@infotelsistemi.com>
2024-04-19 09:50:48 +0200

    prevent show all obsolete versions

--- changelog_page.php
+++ changelog_page.php
@@ -263,7 +263,7 @@ foreach( $t_project_ids as $t_project_id ) {
 	$t_resolved = config_get( 'bug_resolved_status_threshold' );
 
 	# grab versions info for later use, excluding obsolete ones
-	$t_version_rows = version_get_all_rows( $t_project_id, VERSION_ALL, $f_obsolete );
+	$t_version_rows = version_get_all_rows( $t_project_id, VERSION_ALL, $f_version_id == -1 ? false : $f_obsolete );
 
 	# cache category info, but ignore the results for now
 	category_get_all_rows( $t_project_id );
