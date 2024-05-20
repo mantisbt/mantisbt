a80c51a9e0e37f019814ea1ff5f5cade1110a474
Roland Becker <roland@atrol.de>
2024-05-20 20:05:00 +0200

    Use string_attribute to display custom field names
    Fixes #34463

--- bug_report_page.php
+++ bug_report_page.php
@@ -652,9 +652,9 @@ if( $t_show_attachments ) {
 			<?php if( $t_def['require_report'] ) {?><span class="required">*</span><?php } ?>
 			<?php if( $t_def['type'] != CUSTOM_FIELD_TYPE_RADIO && $t_def['type'] != CUSTOM_FIELD_TYPE_CHECKBOX ) { ?>
 				<label for="custom_field_<?php echo string_attribute( $t_def['id'] ) ?>">
-					<?php echo string_display_line( lang_get_defaulted( $t_def['name'] ) ) ?>
+					<?php echo string_attribute( lang_get_defaulted( $t_def['name'] ) ) ?>
 				</label>
-			<?php } else { echo string_display_line( lang_get_defaulted( $t_def['name'] ) ); } ?>
+			<?php } else { echo string_attribute( lang_get_defaulted( $t_def['name'] ) ); } ?>
 		</th>
 		<td>
 			<?php print_custom_field_input( $t_def, ( $f_master_bug_id === 0 ) ? null : $f_master_bug_id, $t_def['require_report'] ) ?>
--- print_all_bug_page_word.php
+++ print_all_bug_page_word.php
@@ -393,7 +393,7 @@ foreach( $t_related_custom_field_ids as $t_custom_field_id ) {
 ?>
 <tr>
 	<td class="bold">
-		<?php echo string_display_line( sprintf( lang_get( 'label' ), lang_get_defaulted( $t_def['name'] ) ) ) ?>
+		<?php echo string_attribute( sprintf( lang_get( 'label' ), lang_get_defaulted( $t_def['name'] ) ) ) ?>
 	</td>
 	<td colspan="5">
 		<?php print_custom_field_value( $t_def, $t_custom_field_id, $t_id ); ?>
