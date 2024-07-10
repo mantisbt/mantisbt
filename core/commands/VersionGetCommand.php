b7fc06f00afd8397a3ccb55aa233909b2a713be1
Roland Becker <roland@atrol.de>
2024-07-10 23:20:06 +0200

    Fix INTERNAL APPLICATION ERROR when adding or editing versions
    Regression introduced by changes in issue #34468

--- core/commands/VersionGetCommand.php
+++ core/commands/VersionGetCommand.php
@@ -20,6 +20,9 @@ require_api( 'helper_api.php' );
 
 use Mantis\Exceptions\ClientException;
 
+$t_soap_dir = dirname( __DIR__, 2 ) . '/api/soap/';
+require_once( $t_soap_dir . 'mc_api.php' );
+
 /**
  * A command that gets project versions.
  */
