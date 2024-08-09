mantisbt/mantisbt

commit 91b0533876247b35a4a2ce02e266240f5875cd5a

Replace dirname( __FILE__ ) by __DIR__ (2. Commands)
Some commands require MantisConnect API functions.

Instead of repeating the same dirname() call for each mc_*.php include,
we define the path to SOAP API directory in a variable.

require_once statements defining functions not used in the Command have
been removed.

Fixes #34468

core/commands/UserUpdateCommand.php
@@ -22,8 +22,8 @@ require_api( 'user_api.php' );
 
 use Mantis\Exceptions\ClientException;
 
-require_once( dirname( __FILE__ ) . '/../../api/soap/mc_account_api.php' );
-require_once( dirname( __FILE__ ) . '/../../api/soap/mc_api.php' );
+$t_soap_dir = dirname( __DIR__, 2 ) . '/api/soap/';
+require_once( $t_soap_dir . 'mc_api.php' );
 
 /**
  * A command that updates a user account.

core/commands/ConfigsSetCommand.php
@@ -16,7 +16,8 @@
 
 use Mantis\Exceptions\ClientException;
 
-require_once( __DIR__ . '/../../api/soap/mc_api.php' );
+$t_soap_dir = dirname( __DIR__, 2 ) . '/api/soap/';
+require_once( $t_soap_dir . 'mc_api.php' );
 
 /**
  * A command that sets config options.

core/commands/IssueAddCommand.php
@@ -33,10 +33,11 @@ require_api( 'relationship_api.php' );
 require_api( 'string_api.php' );
 require_api( 'user_api.php' );
 
-require_once( dirname( __FILE__ ) . '/../../api/soap/mc_api.php' );
-require_once( dirname( __FILE__ ) . '/../../api/soap/mc_enum_api.php' );
-require_once( dirname( __FILE__ ) . '/../../api/soap/mc_issue_api.php' );
-require_once( dirname( __FILE__ ) . '/../../api/soap/mc_project_api.php' );
+$t_soap_dir = dirname( __DIR__, 2 ) . '/api/soap/';
+require_once( $t_soap_dir . 'mc_api.php' );
+require_once( $t_soap_dir . 'mc_enum_api.php' );
+require_once( $t_soap_dir . 'mc_issue_api.php' );
+require_once( $t_soap_dir . 'mc_project_api.php' );
 
 use Mantis\Exceptions\ClientException;
 

core/commands/IssueViewPageCommand.php
@@ -33,11 +33,9 @@ require_api( 'relationship_api.php' );
 require_api( 'string_api.php' );
 require_api( 'user_api.php' );
 
-require_once( dirname( __FILE__ ) . '/../../api/soap/mc_account_api.php' );
-require_once( dirname( __FILE__ ) . '/../../api/soap/mc_api.php' );
-require_once( dirname( __FILE__ ) . '/../../api/soap/mc_enum_api.php' );
-require_once( dirname( __FILE__ ) . '/../../api/soap/mc_issue_api.php' );
-require_once( dirname( __FILE__ ) . '/../../api/soap/mc_project_api.php' );
+$t_soap_dir = dirname( __DIR__, 2 ) . '/api/soap/';
+require_once( $t_soap_dir . 'mc_api.php' );
+require_once( $t_soap_dir . 'mc_issue_api.php' );
 
 use Mantis\Exceptions\ClientException;
 

core/commands/ProjectAddCommand.php
@@ -16,8 +16,8 @@
 
 require_api( 'project_api.php' );
 
-require_once( __DIR__ . '/../../api/soap/mc_api.php' );
-require_once( __DIR__ . '/../../api/soap/mc_enum_api.php' );
+$t_soap_dir = dirname( __DIR__, 2 ) . '/api/soap/';
+require_once( $t_soap_dir . 'mc_api.php' );
 
 use Mantis\Exceptions\ClientException;
 

core/commands/ProjectHierarchyAddCommand.php
@@ -20,7 +20,8 @@ require_api( 'helper_api.php' );
 require_api( 'project_api.php' );
 require_api( 'project_hierarchy_api.php' );
 
-require_once( __DIR__ . '/../../api/soap/mc_api.php' );
+$t_soap_dir = dirname( __DIR__, 2 ) . '/api/soap/';
+require_once( $t_soap_dir . 'mc_api.php' );
 
 use Mantis\Exceptions\ClientException;
 

core/commands/ProjectUpdateCommand.php
@@ -16,8 +16,8 @@
 
 require_api( 'project_api.php' );
 
-require_once( dirname( __FILE__ ) . '/../../api/soap/mc_api.php' );
-require_once( dirname( __FILE__ ) . '/../../api/soap/mc_enum_api.php' );
+$t_soap_dir = dirname( __DIR__, 2 ) . '/api/soap/';
+require_once( $t_soap_dir . 'mc_api.php' );
 
 use Mantis\Exceptions\ClientException;
 

core/commands/ProjectUsersAddCommand.php
@@ -19,9 +19,8 @@ use Mantis\Exceptions\ClientException;
 require_api( 'authentication_api.php' );
 require_api( 'user_pref_api.php' );
 
-require_once( dirname( __FILE__, 3 ) . '/api/soap/mc_account_api.php' );
-require_once( dirname( __FILE__, 3 ) . '/api/soap/mc_api.php' );
-require_once( dirname( __FILE__, 3 ) . '/api/soap/mc_enum_api.php' );
+$t_soap_dir = dirname( __DIR__, 2 ) . '/api/soap/';
+require_once( $t_soap_dir . 'mc_api.php' );
 
 /**
  * Sample:

commit 233f7c44c96c6ba207b0b1b29acb1e4e16451a84

Add missing require_once mc_account_api.php
Regression introduced by 91b0533876247b35a4a2ce02e266240f5875cd5a

Fixes #34468

core/commands/UserUpdateCommand.php
@@ -24,6 +24,7 @@ use Mantis\Exceptions\ClientException;
 
 $t_soap_dir = dirname( __DIR__, 2 ) . '/api/soap/';
 require_once( $t_soap_dir . 'mc_api.php' );
+require_once( $t_soap_dir . 'mc_account_api.php' );
 
 /**
  * A command that updates a user account.
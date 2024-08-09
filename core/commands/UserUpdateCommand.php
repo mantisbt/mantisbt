233f7c44c96c6ba207b0b1b29acb1e4e16451a84
Damien Regad <dregad@mantisbt.org>
2024-08-09 12:08:05 +0200

    Add missing require_once mc_account_api.php
    Regression introduced by 91b0533876247b35a4a2ce02e266240f5875cd5a
    
    Fixes #34468

--- core/commands/UserUpdateCommand.php
+++ core/commands/UserUpdateCommand.php
@@ -24,6 +24,7 @@ use Mantis\Exceptions\ClientException;
 
 $t_soap_dir = dirname( __DIR__, 2 ) . '/api/soap/';
 require_once( $t_soap_dir . 'mc_api.php' );
+require_once( $t_soap_dir . 'mc_account_api.php' );
 
 /**
  * A command that updates a user account.
