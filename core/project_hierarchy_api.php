mantisbt/mantisbt

commit 5ff35d16e85c1aa58cc80fea260cd0f3cfc39910

Fix project_hierarchy_cache()
Currently, project_hierarchy_cache() caches either all projects or
enabled project only, whichever was requested last.

Project edit page calls project_hierarchy_cache() for each subproject
in turn, once for the whole hierarchy, and once for the enabled projects
hierarchy. This causes a cache reload on each call, defeating its
purpose.

The code now caches both hierarchies independently, so that alternating
calls will not cause cache reloads anymore.

Fixes #34526, PR https://github.com/mantisbt/mantisbt/pull/2020

Signed-off-by: Damien Regad <dregad@mantisbt.org>

Original commit message edited, fixed incorrect Issue reference.

core/project_hierarchy_api.php
@@ -31,9 +31,15 @@
 require_api( 'constant_inc.php' );
 require_api( 'database_api.php' );
 
+# We may need to build caches for either all projects or enabled projects or both
+$g_cache_project_hierarchy_enabled = null;
+$g_cache_project_hierarchy_all = null;
+$g_cache_project_inheritance_enabled = null;
+$g_cache_show_disabled_all = null;
+# To make it simpler for callers of project_hierarchy_cache(), we provide
+# these two aliases for the correct pair of caches above
 $g_cache_project_hierarchy = null;
 $g_cache_project_inheritance = null;
-$g_cache_show_disabled = null;
 
 /**
  * Add project to project hierarchy
@@ -146,14 +152,27 @@ function project_hierarchy_get_parent( $p_project_id, $p_show_disabled = false )
  * @return void
  */
 function project_hierarchy_cache( $p_show_disabled = false ) {
+	global $g_cache_project_hierarchy_enabled, $g_cache_project_inheritance_enabled;
+	global $g_cache_project_hierarchy_all, $g_cache_project_inheritance_all;
 	global $g_cache_project_hierarchy, $g_cache_project_inheritance;
-	global $g_cache_show_disabled;
 
-	if( !is_null( $g_cache_project_hierarchy ) && ( $g_cache_show_disabled == $p_show_disabled ) ) {
+	# Should disabled projects be shown and do we already cache the whole hierarchy?
+	if( ( !is_null( $p_show_disabled ) ) && !is_null( $g_cache_project_hierarchy_all ) ) {
+		# Set aliases to the whole hierarchy cache and return
+		$g_cache_project_hierarchy = $g_cache_project_hierarchy_all;
+		$g_cache_project_inheritance = $g_cache_project_inheritance_all;
 		return;
 	}
-	$g_cache_show_disabled = $p_show_disabled;
 
+	# Should disabled projects be hidden and do we already cache the enabled hierarchy?
+	if( ( is_null( $p_show_disabled ) ) && !is_null( $g_cache_project_hierarchy_enabled ) ) {
+		# Set aliases to the enabled hierarchy cache and return
+		$g_cache_project_hierarchy = $g_cache_project_hierarchy_enabled;
+		$g_cache_project_inheritance = $g_cache_project_inheritance_enabled;
+		return;
+	}
+
+	# Build the missing cache
 	db_param_push();
 	$t_enabled_clause = $p_show_disabled ? '1=1' : 'p.enabled = ' . db_param();
 
@@ -186,6 +205,15 @@ function project_hierarchy_cache( $p_show_disabled = false ) {
 		if( $t_row['inherit_parent'] ) {
 			$g_cache_project_inheritance[$t_project_id][$t_parent_id] = $t_parent_id;
 		}
+	}
+
+	# Copy aliases into the right pair of cache variables
+	if( is_null( $p_show_disabled ) ) {
+		$g_cache_project_hierarchy_enabled = $g_cache_project_hierarchy;
+		$g_cache_project_inheritance_enabled = $g_cache_project_inheritance;
+	} else {
+		$g_cache_project_hierarchy_all = $g_cache_project_hierarchy;
+		$g_cache_project_inheritance_all = $g_cache_project_inheritance;
 	}
 }
 

commit 0c73e36e280e5af0b5011b62b909d24a6654c109

Fix disabled projets' inclusion in hierarchy cache
The logic to check $p_show_disabled using is_null() was incorrect,
causing disabled projects to be excluded from the cache when the param's
value true.

This resulted in disabled project not being listed on Manage Projects
Page. Changing code to perform a boolean test fixes the problem.

Regression introduced by 5ff35d16e85c1aa58cc80fea260cd0f3cfc39910
(issue #34526).

Fixes #34618

core/project_hierarchy_api.php
@@ -156,19 +156,19 @@ function project_hierarchy_cache( $p_show_disabled = false ) {
 	global $g_cache_project_hierarchy_all, $g_cache_project_inheritance_all;
 	global $g_cache_project_hierarchy, $g_cache_project_inheritance;
 
-	# Should disabled projects be shown and do we already cache the whole hierarchy?
-	if( ( !is_null( $p_show_disabled ) ) && !is_null( $g_cache_project_hierarchy_all ) ) {
-		# Set aliases to the whole hierarchy cache and return
+	# Set Cache aliases...
+	if( $p_show_disabled ) {
+		# ... to the whole hierarchy cache
 		$g_cache_project_hierarchy = $g_cache_project_hierarchy_all;
 		$g_cache_project_inheritance = $g_cache_project_inheritance_all;
-		return;
-	}
-
-	# Should disabled projects be hidden and do we already cache the enabled hierarchy?
-	if( ( is_null( $p_show_disabled ) ) && !is_null( $g_cache_project_hierarchy_enabled ) ) {
-		# Set aliases to the enabled hierarchy cache and return
+	} else {
+		# ... to the enabled hierarchy cache
 		$g_cache_project_hierarchy = $g_cache_project_hierarchy_enabled;
 		$g_cache_project_inheritance = $g_cache_project_inheritance_enabled;
+	}
+
+	# Cache is already built
+	if( !is_null( $g_cache_project_hierarchy ) ) {
 		return;
 	}
 
@@ -208,12 +208,12 @@ function project_hierarchy_cache( $p_show_disabled = false ) {
 	}
 
 	# Copy aliases into the right pair of cache variables
-	if( is_null( $p_show_disabled ) ) {
-		$g_cache_project_hierarchy_enabled = $g_cache_project_hierarchy;
-		$g_cache_project_inheritance_enabled = $g_cache_project_inheritance;
-	} else {
+	if( $p_show_disabled ) {
 		$g_cache_project_hierarchy_all = $g_cache_project_hierarchy;
 		$g_cache_project_inheritance_all = $g_cache_project_inheritance;
+	} else {
+		$g_cache_project_hierarchy_enabled = $g_cache_project_hierarchy;
+		$g_cache_project_inheritance_enabled = $g_cache_project_inheritance;
 	}
 }
 