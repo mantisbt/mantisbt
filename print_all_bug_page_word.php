9664197d3dede82cc9d5a35228ec5256d9f3039d
Damien Regad <dregad@mantisbt.org>
2024-09-26 01:00:38 +0200

    Refactor Profile API - new ProfileData class
    Using an object to handle Profiles avoids repeated SQL queries to
    retrieve the same Profile information from the database.
    
    This initial implementation only supports read-only operations for now;
    add, update and delete are still managed with legacy functions.
    
    These 3 functions have been replaced by ProfileData methods, which they
    are now simple proxies for. They have been marked as deprecated, and
    will be removed in a future release.
    - profile_get_row()
    - profile_get_name()
    - profile_is_global()
    
    Fixes #34824

--- print_all_bug_page_word.php
+++ print_all_bug_page_word.php
@@ -450,8 +450,8 @@ foreach( $t_related_custom_field_ids as $t_custom_field_id ) {
 <?php
 	# account profile description
 	if( $t_bug->profile_id > 0 ) {
-		$t_profile_row = profile_get_row( $t_bug->profile_id );
-		$t_profile_description = string_display( $t_profile_row['description'] );
+		$t_profile = new ProfileData( $t_bug->profile_id );
+		$t_profile_description = string_display( $t_profile->description );
 
 ?>
 <tr>
