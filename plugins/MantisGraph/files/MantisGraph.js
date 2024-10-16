mantisbt/mantisbt

commit fa96f4399fc9c2f84bba815fcc230585087af525

Fix and improve fill color for Issue Trends chart
With chart.js 3.x, datasets' fill attribute is false by default.

This restores the fill color, with the following change:
- "Resolved" dataset is filled from the x axis
- "Opened" dataset (= the total) is filled from the Resolved dataset (to
  avoid overlap)
- Remove fill color for the "Still open" dataset since it's the  delta
  from the other 2 sets it makes more sense to only display the line.

Fixes #34847

plugins/MantisGraph/files/MantisGraph.js
@@ -74,15 +74,18 @@ $(function() {
                 datasets: [
                     {
                         label: $(this).data('opened-label'),
-                        data: $(this).data('opened-values')
+                        data: $(this).data('opened-values'),
+                        fill: +1, // Fill since Resolved dataset
                     },
                     {
                         label: $(this).data('resolved-label'),
-                        data: $(this).data('resolved-values')
+                        data: $(this).data('resolved-values'),
+                        fill: 'origin',
                     },
                     {
                         label: $(this).data('still-open-label'),
-                        data: $(this).data('still-open-values')
+                        data: $(this).data('still-open-values'),
+                        fill: false,
                     }
                 ]
             },

commit da8699c0c5c030e5a90a9eabdfb17ed6fd5200e4

Use a stacked y axis for Issue Trend chart
The modified visualization introduced in commit  fa96f4399fc9c2f84bba815fcc230585087af525
was confusing, due to the `Still open` dataset being the delta of
`Reported` - `Resolved`.

We now display the areas under the `resolved` and `still open` datasets
as stacked data. Since the sum of the 2 is equal to the `reported` set,
its line overlaps and we avoid confusing display.

Fixes #34847

plugins/MantisGraph/files/MantisGraph.js
@@ -75,7 +75,8 @@ $(function() {
                     {
                         label: $(this).data('opened-label'),
                         data: $(this).data('opened-values'),
-                        fill: +1, // Fill since Resolved dataset
+                        fill: false,
+                        stack: 'total', // Display on a separate stack
                     },
                     {
                         label: $(this).data('resolved-label'),
@@ -85,14 +86,15 @@ $(function() {
                     {
                         label: $(this).data('still-open-label'),
                         data: $(this).data('still-open-values'),
-                        fill: false,
-                    }
+                        fill: '-1', // Fill since "resolved" dataset
+                    },
                 ]
             },
             options: {
                 scales: {
                     y: {
                         beginAtZero: true,
+                        stacked: true,
                         ticks: {
                             precision: 0,
                         }
@@ -104,6 +106,10 @@ $(function() {
                     }
                 },
                 plugins: {
+                    // legend: {
+                    //     display: true,
+                    //     fillStyle: "#cccccc",
+                    // },
                     tooltip: {
                         mode: 'index',
                         position: 'nearest',
@@ -111,7 +117,7 @@ $(function() {
                     colorschemes: {
                         scheme: 'brewer.Set1-3',
                         reverse: true,
-                        fillAlpha: 0.15
+                        fillAlpha: 0.15,
                     }
                 }
             }