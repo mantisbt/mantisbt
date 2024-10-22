aacff8f6d736568379bda7bfd4f651ba11ac4e26
Damien Regad <dregad@mantisbt.org>
2024-10-22 14:35:37 +0200

    Remove legacy row-N classes
    These were removed from CSS files in 823266db247f4c2efb3f73a602522c9f2ce42e31
    as they were no longer used since deprecation of helper_alternate_class()
    (commit 6aa5e9b81fb2fbd2bf0ad46b416d5f1445c7f98a).

--- plugins/MantisGraph/pages/issues_trend_bycategory_table.php
+++ plugins/MantisGraph/pages/issues_trend_bycategory_table.php
@@ -190,7 +190,7 @@ foreach ( $t_category as $t_cat ) {
 }
 echo '</tr>';
 for( $t_ptr=0; $t_ptr<$t_bin_count; $t_ptr++ ) {
-	echo '<tr class="row-'.($t_ptr%2+1).'"><td>'.$t_ptr.' ('. date( $t_date_format, $t_marker[$t_ptr] ) .')'.'</td>';
+	echo '<tr><td>'.$t_ptr.' ('. date( $t_date_format, $t_marker[$t_ptr] ) .')'.'</td>';
 	foreach ( $t_category as $t_cat ) {
 		echo '<td>' . ( $t_data[$t_ptr][$t_cat] ?? 0 ) . '</td>';
 	}
