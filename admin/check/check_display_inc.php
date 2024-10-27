060d424819e42725035e61f23cc633fbe6ea56d3
Roland Becker <roland@atrol.de>
2024-10-27 20:51:14 +0100

    On Windows, GraphViz tools need '.exe' extension
    Fixes #34917

--- admin/check/check_display_inc.php
+++ admin/check/check_display_inc.php
@@ -82,9 +82,10 @@ if( config_get( 'relationship_graph_enable' ) ) {
 	);
 
 	# Check each tool's availability
+	$t_extension = is_windows_server() ? '.exe' : '';
 	$t_unavailable = [];
 	foreach( $t_tools as $t_tool ) {
-		$t_tool_path = $t_graphviz_path . $t_tool;
+		$t_tool_path = $t_graphviz_path . $t_tool . $t_extension;
 		if( !is_executable( $t_tool_path ) ) {
 			$t_unavailable[] = $t_tool;
 		}
