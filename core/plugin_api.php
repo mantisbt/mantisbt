44d3fecca03280029f1ca5333bd770551e5faddd
Nikolay Raspopov <raspopov@cherubicsoft.com>
2024-12-06 19:51:20 +0300

    Improve plugin_file_path() function
    - Automatically detect the current plugin when $p_base_name = ''
    - Return the path to the plugin's "files" directory when $p_filename = ''
    
    This will allow plugins to dynamically provide files from their 'files'
    directory. Currently, the directory is hard-coded but not easily 
    accessible.
    
    Fixes 35082, PR https://github.com/mantisbt/mantisbt/pull/2054
    
    Signed-off-by: Damien Regad <dregad@mantisbt.org>

--- core/plugin_api.php
+++ core/plugin_api.php
@@ -185,15 +185,20 @@ function plugin_route_group( $p_base_name = null ) {
 }
 
 /**
- * Return a path to a plugin file.
- * @param string $p_filename  File name.
- * @param string $p_base_name Plugin base name.
- * @return mixed File path or false if FNF
+ * Returns the path to a plugin file, or the plugin's files directory.
+ * @param string $p_filename  File name; if not set, the function will return the plugin's files directory.
+ * @param string $p_base_name Plugin base name (defaults to the current plugin).
+ * @return string|false File path or false if file cannot be found.
  */
-function plugin_file_path( $p_filename, $p_base_name ) {
-	$t_file_path = config_get_global( 'plugin_path' );
-	$t_file_path .= $p_base_name . DIRECTORY_SEPARATOR;
-	$t_file_path .= 'files' . DIRECTORY_SEPARATOR . $p_filename;
+function plugin_file_path( $p_filename = '', $p_base_name = '' ) {
+	$t_file_path = config_get_global( 'plugin_path' )
+		. ( $p_base_name ?: plugin_get_current() ) . DIRECTORY_SEPARATOR
+		. 'files' . DIRECTORY_SEPARATOR;
+
+	if( !$p_filename ) {
+		return $t_file_path;
+	}
+	$t_file_path .= $p_filename;
 
 	return( is_file( $t_file_path ) ? $t_file_path : false );
 }
