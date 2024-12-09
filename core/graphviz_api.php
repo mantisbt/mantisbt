b84319e79d18e576e7f8a8a7c5c40d81b3f5f792
raspopov <raspopov@cherubicsoft.com>
2024-12-09 18:49:48 +0300

    New Graph::graphviz_path() function avoids duplicate code
    Issue #35039

--- core/graphviz_api.php
+++ core/graphviz_api.php
@@ -113,11 +113,6 @@ class Graph {
 	protected $edges = array();
 
 	/**
-	 * @var string Graphviz path
-	 */
-	protected $graphviz_path;
-
-	/**
 	 * @var string Graphviz tool
 	 */
 	protected $graphviz_tool;
@@ -238,7 +233,6 @@ class Graph {
 
 		$this->set_attributes( $p_attributes );
 
-		$this->graphviz_path = realpath( config_get_global( 'graphviz_path' ) ) . DIRECTORY_SEPARATOR;
 		$this->graphviz_tool = $p_tool;
 
 		if( is_windows_server() ) {
@@ -375,8 +369,9 @@ class Graph {
 		}
 
 		# Graphviz tool missing or not executable
-		if( !is_executable( $this->tool_path() ) ) {
-			error_parameters( $this->graphviz_tool );
+		$t_tool_path = $this->tool_path();
+		if( !is_executable( $t_tool_path ) ) {
+			error_parameters( $t_tool_path );
 			trigger_error( ERROR_GRAPH_TOOL_NOT_FOUND, ERROR );
 		}
 
@@ -386,7 +381,7 @@ class Graph {
 		$t_dot_source = ob_get_clean();
 
 		# Start dot process
-		$t_command = escapeshellarg( $this->tool_path() ) . ' -T' . $p_format;
+		$t_command = escapeshellarg( $t_tool_path ) . ' -T' . $p_format;
 		$t_stderr = tempnam( sys_get_temp_dir(), 'graphviz' );
 		$t_descriptors = array(
 			0 => array( 'pipe', 'r', ),
@@ -512,12 +507,21 @@ class Graph {
 	}
 
 	/**
+	 * Gets the path to the Graphviz tools directory.
+	 *
+	 * @return string
+	 */
+	public static function graphviz_path() {
+		return realpath( config_get_global( 'graphviz_path' ) ) . DIRECTORY_SEPARATOR;
+	}
+
+	/**
 	 * Gets the path to the Graphviz tool.
 	 *
 	 * @return string
 	 */
 	protected function tool_path() {
-		return $this->graphviz_path . $this->graphviz_tool;
+		return self::graphviz_path() . $this->graphviz_tool;
 	}
 }
 
