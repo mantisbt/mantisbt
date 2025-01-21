738f7385c55039a107a6c3c15398e92d5a6e02db
CasN <cas@nuy.info>
2025-01-21 14:10:25 +0100

    Updated config_defaults_inc.php

--- config_defaults_inc.php
+++ config_defaults_inc.php
@@ -518,19 +518,50 @@ $g_email_notifications_verbose = OFF;
 /**
  * Sets the default email notifications values for different user categories.
  *
- * In combination with {@see $g_notify_flags}, this config option controls
+ * In combination with *notify_flags* (see below), this config option controls
  * who should get email notifications on different actions/statuses.
  *
  * The user categories are:
- # - `reporter`:      the Issue's reporter
- * - `handler`:       the user assigned to the Issue
- * - `monitor`:       users who are monitoring the Issue
- * - `bugnotes`:      users who have added a bugnote to the Issue
- * - `category`:      category owners
- * - `explicit`:      users who are explicitly specified by the code based on the
- *                    action (e.g. user added to monitor list).
- * - `threshold_max`: all users with access level <= max
- * - `threshold_min`: ...and with access level >= min
+ *
+ *      'reporter': the reporter of the bug
+ *       'handler': the handler of the bug
+ *       'monitor': users who are monitoring a bug
+ *      'bugnotes': users who have added a bugnote to the bug
+ *      'category': category owners
+ *      'explicit': users who are explicitly specified by the code based on the
+ *                  action (e.g. user added to monitor list).
+ * 'threshold_max': all users with access <= max
+ * 'threshold_min': ..and with access >= min
+ *
+ * The second config option (notify_flags) sets overrides for specific
+ * actions/statuses. If a user category is not listed for an action, the
+ * default from the config option above is used.  The possible actions are:
+ *
+ *             'new': a new bug has been added
+ *           'owner': a bug has been assigned to a new owner
+ *        'reopened': a bug has been reopened
+ *         'deleted': a bug has been deleted
+ *         'updated': a bug has been updated
+ *         'bugnote': a bugnote has been added to a bug
+ *         'sponsor': sponsorship has changed on this bug
+ *        'relation': a relationship has changed on this bug
+ *         'monitor': an issue is monitored.
+ *        '<status>': eg: 'resolved', 'closed', 'feedback', 'acknowledged', etc.
+ *                     this list corresponds to $g_status_enum_string
+ *
+ * Examples:
+ * - If you wanted to have all developers get notified of new bugs you might
+ *   add the following lines to your config file:
+ *
+ *   $g_notify_flags['new']['threshold_min'] = DEVELOPER;
+ *   $g_notify_flags['new']['threshold_max'] = DEVELOPER;
+ *
+ * - You might want to do something similar so all managers are notified when a
+ *   bug is closed.
+ * - If you did not want reporters to be notified when a bug is closed
+ *   (only when it is resolved) you would use:
+ *
+ *   $g_notify_flags['closed']['reporter'] = OFF;
  *
  * @see $g_notify_flags
  * @global array $g_default_notify_flags
@@ -549,40 +580,12 @@ $g_default_notify_flags = array(
 /**
  * Sets notifications overrides for specific actions/statuses.
  *
- * If a user category is not listed for an action, the default defined by
- * {@see $g_default_notify_flags} is used.
- *
- * Possible actions are:
- * - `new`:      a new Issue has been added
- * - `owner`:    an Issue has been assigned to a new owner
- * - `reopened`: an Issue has been reopened
- * - `deleted`:  an Issue has been deleted
- * - `updated`:  an Issue has been updated
- * - `bugnote`:  a bugnote has been added to an Issue
- * - `sponsor`:  sponsorship has changed on this Issue
- * - `relation`: a relationship has changed on this Issue
- * - `monitor`:  an Issue is monitored.
- * - `<status>`: A status code, as defined in {@see $g_status_enum_string},
- *               eg: 'resolved', 'closed', 'feedback', 'acknowledged`, etc.
- *
- * Examples:
- *  - If you wanted to have all developers get notified of new bugs you could
- *    add the following lines to your config file:
- *
- *    $g_notify_flags['new']['threshold_min'] = DEVELOPER;
- *    $g_notify_flags['new']['threshold_max'] = DEVELOPER;
- *
- *  - You might want to do something similar so all managers are notified when a
- *    bug is closed.
- *  - If you did not want reporters to be notified when a bug is closed
- *    (only when it is resolved) you would use:
- *
- *    $g_notify_flags['closed']['reporter'] = OFF;
- *
- * The default setting
+ * See above for detailed information. As an example of how to use this config
+ * option, the default setting
  * - disables bugnotes notifications on new bugs (not needed in this case)
- * - disables all notifications for the monitoring event, except explicit
- *
+ * - disables all notifications for monitoring event, except explicit
+ * example of how to use this config option.
+
  * @see $g_default_notify_flags
  * @global array $g_notify_flags
  */
@@ -944,7 +947,7 @@ $g_email_retry_in_days = 7;
  * this functionality can be dis/enabled with the $g_use_emailtemplate. Default value  = OFF meaning use standard mantisbt emails
  *
  * @global int $g_email_use_template
-  */
+ */
 $g_use_mailtemplate = OFF;
 
 /**
@@ -1095,12 +1098,6 @@ $g_language_choices_arr = array(
 /**
  * Browser language mapping for 'auto' language selection.
  *
- * The mapping between a comma-separated list of BCP 47 language codes, usually
- * ending with the most generic one (i.e. the primary language without extended
- * subtag), and the corresponding MantisBT language code.
- *
- * @see lang_get_current_lang()
- * @see lang_get_current_datetime_locale()
  * @global array $g_language_auto_map
  */
 $g_language_auto_map = array(
@@ -2367,22 +2364,18 @@ $g_file_download_xsendfile_header_name = 'X-Sendfile';
  *
  * The options below can be combined using bitwise operators (not all
  * possible combinations make sense):
- * - OFF                      Do not convert URLs or emails
- * - LINKS_SAME_WINDOW        Convert to links that open in the current window (DEFAULT)
- * - LINKS_NEW_WINDOW         Convert to links that open in a new window (overrides LINKS_SAME_WINDOW)
- * - LINKS_NOOPENER           Links have the `noopener` type (DEFAULT)
- *                            {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/rel/noopener}
- * - LINKS_NOREFERRER         Links have the `noreferrer` type, i.e. they omit the *Referer*
- *                            header (implies LINKS_NOOPENER)
- *                            {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/rel/noreferrer}
- * - LINKS_NOFOLLOW_EXTERNAL  Links to external sites (i.e. having a different root domain)
- *                            have the `nofollow` type, instructing search engines
- *                            not to follow these links (DEFAULT)
- *                            {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/rel#nofollow}
+ * - OFF                Do not convert URLs or emails
+ * - LINKS_SAME_WINDOW  Convert to links that open in the current window (DEFAULT)
+ * - LINKS_NEW_WINDOW   Convert to links that open in a new window (overrides LINKS_SAME_WINDOW)
+ * - LINKS_NOOPENER     Links have the `noopener` type (DEFAULT)
+ *                      {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types/noopener}
+ * - LINKS_NOREFERRER   Links have the `noreferrer` type, i.e. they omit the *Referer*
+ *                      header (implies LINKS_NOOPENER)
+ *                      {@link https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types/noreferrer}
  *
  * @global int $g_html_make_links
  */
-$g_html_make_links = LINKS_SAME_WINDOW | LINKS_NOOPENER | LINKS_NOFOLLOW_EXTERNAL;
+$g_html_make_links = LINKS_SAME_WINDOW | LINKS_NOOPENER;
 
 /**
  * Valid html tags for multi-line fields (e.g. description).
@@ -4549,22 +4542,6 @@ $g_relationship_graph_enable = OFF;
 $g_graphviz_path = '/usr/bin/';
 
 /**
- * Graphviz output format.
- *
- * Can be `svg`, `png` (default) or any other
- * {@link https://www.graphviz.org/docs/outputs/ supported format}.
- *
- * NOTE: svg produces higher quality images compared to png, but it requires
- * Graphviz >= 2.42.4 due to a
- * {@link https://gitlab.com/graphviz/graphviz/-/issues/1687 bug in earlier versions}.
- * The fix will be included in Ubuntu 26.04 LTS; the default can be reconsidered
- * when it is released.
- *
- * @global string $g_graph_format
- */
-$g_graph_format = 'png';
-
-/**
  * Font name and size, as required by Graphviz.
  *
  * If Graphviz fails to run for you, you are probably using a font name that
@@ -4910,13 +4887,11 @@ $g_manage_plugin_threshold = ADMINISTRATOR;
  */
 $g_plugin_mime_types = array(
 	'css' => 'text/css',
-	'js'  => 'application/javascript',
+	'js'  => 'text/javascript',
 	'gif' => 'image/gif',
 	'png' => 'image/png',
 	'jpg' => 'image/jpeg',
-	'jpeg' => 'image/jpeg',
-	'svg' => 'image/svg+xml',
-	'webp' => 'image/webp'
+	'jpeg' => 'image/jpeg'
 );
 
 /**
