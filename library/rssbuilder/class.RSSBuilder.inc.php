efd7f6a0fd33898d30b2499c4c8169d76318fa1e
Damien Regad <dregad@mantisbt.org>
2025-02-21 23:34:50 +0100

    Convert RSSBuilder scripts from ISO-8859-1 to UTF-8

--- library/rssbuilder/class.RSSBuilder.inc.php
+++ library/rssbuilder/class.RSSBuilder.inc.php
@@ -5,7 +5,7 @@ require_once 'class.RSSBase.inc.php';
 * Class for creating an RSS-feed
 * @author Michael Wimmer <flaimo@gmail.com>
 * @category flaimo-php
-* @copyright Copyright © 2002-2008, Michael Wimmer
+* @copyright Copyright Â© 2002-2008, Michael Wimmer
 * @license GNU General Public License v3
 * @link http://code.google.com/p/flaimo-php/
 * @package RSS
