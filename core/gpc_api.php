mantisbt/mantisbt

commit c939e5574556a2986bdd23d2ba9369d03595c269

PHP 8.4: Implicitly nullable parameter declaration
Adjust function declarations to fix deprecation notices, and adapt some
PHPDoc blocks accordingly.

Fixes #35215

core/gpc_api.php
@@ -211,11 +211,7 @@ function gpc_get_custom_field( $p_var_name, $p_custom_field_type, $p_default = n
 				$p_default = array( $p_default );
 			}
 			$t_values = gpc_get_string_array( $p_var_name, $p_default );
-			if( is_array( $t_values ) ) {
-				return implode( '|', $t_values );
-			} else {
-				return '';
-			}
+			return implode( '|', $t_values );
 		case CUSTOM_FIELD_TYPE_DATE:
 			$t_day = gpc_get_int( $p_var_name . '_day', 0 );
 			$t_month = gpc_get_int( $p_var_name . '_month', 0 );
@@ -245,7 +241,7 @@ function gpc_get_custom_field( $p_var_name, $p_custom_field_type, $p_default = n
  *
  * @return array
  */
-function gpc_get_string_array( $p_var_name, array $p_default = null ) {
+function gpc_get_string_array( string $p_var_name, array $p_default = [] ): array {
 	# Don't pass along a default unless one was given to us
 	# otherwise we prevent an error being triggered
 	$t_args = func_get_args();
@@ -282,7 +278,7 @@ function gpc_get_string_array( $p_var_name, array $p_default = null ) {
  *
  * @return array
  */
-function gpc_get_int_array( $p_var_name, array $p_default = null ) {
+function gpc_get_int_array( string $p_var_name, array $p_default = [] ): array {
 	# Don't pass along a default unless one was given to us
 	# otherwise we prevent an error being triggered
 	$t_args = func_get_args();
@@ -313,7 +309,7 @@ function gpc_get_int_array( $p_var_name, array $p_default = null ) {
  *
  * @return array
  */
-function gpc_get_bool_array( $p_var_name, array $p_default = null ) {
+function gpc_get_bool_array( string $p_var_name, array $p_default = [] ): array {
 	# Don't pass along a default unless one was given to us
 	# otherwise we prevent an error being triggered
 	$t_args = func_get_args();

api/soap/mc_issue_api.php
@@ -386,14 +386,17 @@ function mci_issue_get_due_date( BugData $p_bug ) {
 /**
  * Sets the supplied array of custom field values to the specified issue id.
  *
- * @param integer $p_issue_id       Issue id to apply custom field values to.
- * @param array   &$p_custom_fields The array of custom field values as described in the webservice complex types.
- * @param boolean $p_log_insert     Create history logs for new values.
- * @return boolean|SoapFault|RestFault true for success, otherwise fault.
+ * @param int         $p_issue_id      Issue id to apply custom field values to.
+ * @param array|null &$p_custom_fields The array of custom field values as described
+ *                                     in the webservice complex types.
+ * @param bool        $p_log_insert    Create history logs for new values.
+ *
+ * @return bool|SoapFault|RestFault true for success, otherwise fault.
+ * @throws ClientException
  */
-function mci_issue_set_custom_fields( $p_issue_id, array &$p_custom_fields = null, $p_log_insert = true ) {
+function mci_issue_set_custom_fields( $p_issue_id, ?array &$p_custom_fields, $p_log_insert = true ) {
 	# set custom field values on the submitted issue
-	if( isset( $p_custom_fields ) && is_array( $p_custom_fields ) ) {
+	if( isset( $p_custom_fields ) ) {
 		foreach( $p_custom_fields as $t_custom_field ) {
 			$t_custom_field = ApiObjectFactory::objectToArray( $t_custom_field );
 

core/access_api.php
@@ -348,14 +348,14 @@ function access_has_project_level( $p_access_level, $p_project_id = null, $p_use
  *                                              - array: for an array threshold
  *                                              - string: for a threshold option which will be evaluated
  *                                                for each project context
- * @param array                 $p_project_ids  Array of project ids to check access against, default to null
+ * @param array|null            $p_project_ids  Array of project ids to check access against, default to null
  *                                               to use all user accessible projects
  * @param integer|null          $p_user_id      Integer representing user id, defaults to null to use current user.
  * @param integer               $p_limit        Maximum number of results, default is 0 for all results
  *
  * @return array                The filtered array of project ids
  */
-function access_project_array_filter( $p_access_level, array $p_project_ids = null, $p_user_id = null, $p_limit = 0 ) {
+function access_project_array_filter( $p_access_level, ?array $p_project_ids = null, ?int $p_user_id = null, int $p_limit = 0 ): array {
 	# Short circuit the check in this case
 	if( NOBODY == $p_access_level ) {
 		return array();
@@ -419,7 +419,7 @@ function access_project_array_filter( $p_access_level, array $p_project_ids = nu
  * @return bool            True if user has the specified access level in any of the projects.
  * @access public
  */
-function access_has_any_project_level( $p_access_level, array $p_project_ids = null, $p_user_id = null ) {
+function access_has_any_project_level( $p_access_level, ?array $p_project_ids = null, ?int $p_user_id = null ): bool {
 	# We only need 1 matching project to return positive
 	$t_matches = access_project_array_filter( $p_access_level, $p_project_ids, $p_user_id, 1 );
 	return !empty( $t_matches );
@@ -434,7 +434,7 @@ function access_has_any_project_level( $p_access_level, array $p_project_ids = n
  * @param array|null       $p_project_ids  Array of project ids to check access against.
  * @param int|null         $p_user_id      User id, defaults to null to use current user.
  */
-function access_ensure_any_project_level( $p_access_level, array $p_project_ids = null, $p_user_id = null ) {
+function access_ensure_any_project_level( $p_access_level, ?array $p_project_ids = null, ?int $p_user_id = null ): void {
 	if( !access_has_any_project_level( $p_access_level, $p_project_ids, $p_user_id ) ) {
 		access_denied();
 	}

core/bug_group_action_api.php
@@ -207,8 +207,8 @@ function bug_group_action_process( $p_action, $p_bug_id ) {
  *
  * @return array
  */
-function bug_group_action_get_commands( array $p_project_ids = null ) {
-	if( $p_project_ids === null || count( $p_project_ids ) == 0 ) {
+function bug_group_action_get_commands( array $p_project_ids = [] ) {
+	if( empty( $p_project_ids ) ) {
 		$p_project_ids = array( ALL_PROJECTS );
 	}
 

core/category_api.php
@@ -400,7 +400,7 @@ function category_get_row( $p_category_id, $p_error_if_not_exists = true ) {
  * @return int|null An integer representing sort order.
  * @access public
  */
-function category_sort_rows_by_project( $p_category1, array $p_category2 = null ) {
+function category_sort_rows_by_project( $p_category1, ?array $p_category2 = null ) {
 	static $s_project_id = null;
 	if( is_null( $p_category2 ) ) {
 		# Set a target project

core/classes/BugFilterQuery.class.php
@@ -240,7 +240,7 @@ class BugFilterQuery extends DbQuery {
 	 * @param integer $p_offset		Offset value
 	 * @return IteratorAggregate|boolean ADOdb result set or false if the query failed.
 	 */
-	public function execute( array $p_bind_array = null, $p_limit = null, $p_offset = null ) {
+	public function execute( array $p_bind_array = [], $p_limit = null, $p_offset = null ) {
 		if( $this->needs_rebuild ) {
 			$this->build_query();
 		}
@@ -1609,7 +1609,7 @@ class BugFilterQuery extends DbQuery {
 	 *
 	 * @return string
 	 */
-	protected function helper_convert_legacy_clause( $p_string, array $p_params = null ) {
+	protected function helper_convert_legacy_clause( $p_string, array $p_params = [] ) {
 		if( empty( $p_params ) ) {
 			# shortcut, if there are no parameters, there's no need to translate
 			return $p_string;

core/classes/DbQuery.class.php
@@ -166,18 +166,9 @@ class DbQuery {
 	 *
 	 * @return void
 	 */
-	public function __construct( $p_query_string = null, array $p_bind_array = null ) {
-		# Itialization
-		if( null === $p_query_string ) {
-			$this->query_string = '';
-		} else {
-			$this->query_string = $p_query_string;
-		}
-		if( null === $p_bind_array ) {
-			$this->query_bind_array = array();
-		} else {
-			$this->query_bind_array = $p_bind_array;
-		}
+	public function __construct( string $p_query_string = '', array $p_bind_array = [] ) {
+		$this->query_string = $p_query_string;
+		$this->query_bind_array = $p_bind_array;
 	}
 
 	/**
@@ -246,7 +237,7 @@ class DbQuery {
 	 *
 	 * @return IteratorAggregate|boolean ADOdb result set or false if the query failed.
 	 */
-	public function execute( array $p_bind_array = null, $p_limit = null, $p_offset = null ) {
+	public function execute( array $p_bind_array = [], $p_limit = null, $p_offset = null ) {
 		# For backwards compatibility with legacy code still relying on DB API,
 		# we need to save the parameters count before binding otherwise it will
 		# be reset after query execution, which will cause issues on RDBMS with
@@ -254,7 +245,7 @@ class DbQuery {
 		db_param_push();
 
 		# bind values if provided
-		if( null !== $p_bind_array ) {
+		if( !empty( $p_bind_array ) ) {
 			$this->bind_values( $p_bind_array );
 		}
 
@@ -785,16 +776,12 @@ class DbQuery {
 	 *
 	 * @return IteratorAggregate|false ADOdb result set or false if the query failed
 	 */
-	public static function compat_db_query( $p_query, array $p_arr_parms = null, $p_limit = -1, $p_offset = -1, $p_pop_param = true ) {
+	public static function compat_db_query( string $p_query, array $p_params = [], int $p_limit = -1, int $p_offset = -1, bool $p_pop_param = true ) {
 		global $g_db_param;
-
-		if( !is_array( $p_arr_parms ) ) {
-			$p_arr_parms = array();
-		}
 
 		$t_query = new DbQuery();
 		$t_query->db_query_string = $p_query;
-		$t_query->db_param_array = $p_arr_parms;
+		$t_query->db_param_array = $p_params;
 
 		$t_query->process_sql_syntax();
 
@@ -806,7 +793,7 @@ class DbQuery {
 		# Restore ADOdb parameter count
 		$g_db_param->pop();
 
-		if( $p_pop_param && !empty( $p_arr_parms ) ) {
+		if( $p_pop_param && !empty( $p_params ) ) {
 			$g_db_param->pop();
 		}
 

core/custom_field_api.php
@@ -138,19 +138,18 @@ function custom_field_cache_row( $p_field_id, $p_trigger_errors = true ) {
 /**
  * Cache custom fields contained within an array of field id's.
  *
- * If ids parameter is omitted, all fields will be cached.
- *
- * @param array|null $p_cf_id_array Array of custom field ids.
+ * @param array $p_cf_id_array Array of custom field ids;
+ *                             if empty, all fields will be cached.
  *
  * @return void
  *
  * @access public
  */
-function custom_field_cache_array_rows( array $p_cf_id_array = null ) {
+function custom_field_cache_array_rows( array $p_cf_id_array = [] ): void {
 	global $g_cache_custom_field, $g_cache_name_to_id_map;
 
 	$c_cf_id_array = array();
-	$t_cache_all = ( null === $p_cf_id_array );
+	$t_cache_all = empty( $p_cf_id_array );
 
 	# cache main data
 	if( $t_cache_all ) {

commit c076bc567a3cdf4ebab4206229d451c9252a64d8

Fix PHP TypeError calling gpc_get_string_array()
Argument 2 passed to gpc_get_string_array() must be of the type array,
null given, called in .../core/gpc_api.php on line 213

Regression introduced by c939e5574556a2986bdd23d2ba9369d03595c269.

Fixes #35215

core/gpc_api.php
@@ -206,8 +206,10 @@ function gpc_get_custom_field( $p_var_name, $p_custom_field_type, $p_default = n
 	switch( $p_custom_field_type ) {
 		case CUSTOM_FIELD_TYPE_MULTILIST:
 		case CUSTOM_FIELD_TYPE_CHECKBOX:
-			# ensure that the default is an array, if set
-			if( ( $p_default !== null ) && !is_array( $p_default ) ) {
+			# Ensure that the default is an array
+			if( $p_default === null ) {
+				$p_default = [];
+			} elseif( !is_array( $p_default ) ) {
 				$p_default = array( $p_default );
 			}
 			$t_values = gpc_get_string_array( $p_var_name, $p_default );