mantisbt/mantisbt

commit 9cdc361a80b2a3fa614ca2648a97342bd0a18236

Fix Login URL from access denied page login button
When access_denied() is called from an admin page, the Login button
returns a 404 because auth_login_page() returns a relative URL and the
login page does not exist under /admin.

Fixes #35587

core/access_api.php
@@ -83,14 +83,14 @@ function access_denied() {
 	}
 
 	if( !auth_is_user_authenticated() ) {
-		print_header_redirect( auth_login_page( $t_return ) );
+		print_header_redirect( helper_mantis_url( auth_login_page( $t_return ) ) );
 	} else {
 		$t_buttons = [];
 		
 		# Login button
 		if( current_user_is_anonymous() ) {
 			$t_buttons[] = [
-				auth_login_page( $t_return ),
+				helper_mantis_url( auth_login_page( $t_return ) ),
 				lang_get( 'login' )
 			];
 		}

commit 2f75ab6d8e1d829d094985662faf7dcf1aa24102

Fix login page redirection from admin page
When an unauthenticated user tries to access the admin page, they are
redirected to the login page, but this was broken by use of absolute URL
introduced in commit 9cdc361a80b2a3fa614ca2648a97342bd0a18236.

Fixes #35587

core/access_api.php
@@ -83,7 +83,7 @@ function access_denied() {
 	}
 
 	if( !auth_is_user_authenticated() ) {
-		print_header_redirect( helper_mantis_url( auth_login_page( $t_return ) ) );
+		print_header_redirect( helper_mantis_url( auth_login_page( $t_return ) ), false, false, true );
 	} else {
 		$t_buttons = [];
 		