mantisbt/mantisbt

commit 10971ed15a2f8476130c996f54dda6dc068828f9

Fix Nu Html Checker issues
Fix various Nu Html Checker issues in dropzone (print_api.php):

- Error: Element img is missing required attribute src.
- Error: An img element must have an alt attribute, except under certain
  conditions.

In bug_view_inc.php:

- Error: Element tbody not allowed as child of element table in this
  context.
- Error: Duplicate ID history.

In bug_update_page.php and bugnote_view_inc.php:

- Error: The width attribute on the td element is obsolete.
- Warning: A table row was 0 columns wide, which is less than the column
  count established by the first row (6).
- Error: Element option without attribute label must not be empty.
- Error: Duplicate ID bugnotes.

In print_all_bug_page.php:

- Error: Stray end tag td.
- Error: Row 2 of a row group established by a tbody element has no
  cells beginning on it.
- Warning: A table row was 10 columns wide and exceeded the column count
  established by the first row (5).

In print_all_bug_page_word.php:

- Error: The width attribute on the td element is obsolete.
- Error: Row 2 of a row group established by a tbody element has no
  cells beginning on it.

core/print_api.php
@@ -2230,7 +2230,7 @@ function print_dropzone_template(){
 	<div id="dropzone-preview-template" class="hidden">
 		<div class="dz-preview dz-file-preview">
 			<div class="dz-filename"><span data-dz-name></span></div>
-			<img data-dz-thumbnail />
+			<img src="#" alt="" data-dz-thumbnail>
 			<div class="dz-error-message">
 				<div class="dz-error-mark"><span><?php print_icon('fa-times-circle'); ?></span></div>
 				<span data-dz-errormessage></span>

bug_update_page.php
@@ -204,18 +204,18 @@ if( $t_show_id || $t_show_project || $t_show_category || $t_show_view_state || $
 	#
 
 	echo '<tr>';
-	echo '<td width="15%" class="category">', $t_show_id ? lang_get( 'id' ) : '', '</td>';
-	echo '<td width="20%" class="category">', $t_show_project ? lang_get( 'email_project' ) : '', '</td>';
-	echo '<td width="15%" class="category">';
+	echo '<td class="category">', $t_show_id ? lang_get( 'id' ) : '', '</td>';
+	echo '<td class="category">', $t_show_project ? lang_get( 'email_project' ) : '', '</td>';
+	echo '<td class="category">';
 	if( $t_show_category ) {
 		$t_allow_no_category = config_get( 'allow_no_category' );
 		echo $t_allow_no_category ? '' : '<span class="required">*</span> ';
 		echo '<label for="category_id">' . lang_get( 'category' ) . '</label>';
 	}
 	echo '</td>';
-	echo '<td width="20%" class="category">', $t_show_view_state ? '<label for="view_state">' . lang_get( 'view_status' ) . '</label>' : '', '</td>';
-	echo '<td width="15%" class="category">', $t_show_date_submitted ? lang_get( 'date_submitted' ) : '', '</td>';
-	echo '<td width="15%" class="category">', $t_show_last_updated ? lang_get( 'last_update' ) : '', '</td>';
+	echo '<td class="category">', $t_show_view_state ? '<label for="view_state">' . lang_get( 'view_status' ) . '</label>' : '', '</td>';
+	echo '<td class="category">', $t_show_date_submitted ? lang_get( 'date_submitted' ) : '', '</td>';
+	echo '<td class="category">', $t_show_last_updated ? lang_get( 'last_update' ) : '', '</td>';
 	echo '</tr>';
 
 	#
@@ -314,7 +314,7 @@ if( $t_show_reporter || $t_show_handler || $t_show_due_date ) {
 
 		if( access_has_project_level( config_get( 'update_bug_assign_threshold', config_get( 'update_bug_threshold' ) ) ) ) {
 			echo '<select ' . helper_get_tab_index() . ' id="handler_id" name="handler_id" class="input-sm">';
-			echo '<option value="0"></option>';
+			echo '<option value="0">&nbsp;</option>';
 			print_assign_to_option_list( $t_bug->handler_id, $t_bug->project_id );
 			echo '</select>';
 		} else {

bug_view_inc.php
@@ -222,15 +222,6 @@ if( $t_top_buttons_enabled ) {
 	echo '</thead>';
 }
 
-if( $t_bottom_buttons_enabled ) {
-	echo '<tfoot>';
-	echo '<tr class="noprint"><td colspan="6">';
-	/** @noinspection PhpUnhandledExceptionInspection */
-	bug_view_action_buttons( $f_issue_id, $t_flags );
-	echo '</td></tr>';
-	echo '</tfoot>';
-}
-
 echo '<tbody>';
 
 if( $t_flags['id_show'] || $t_flags['project_show'] || $t_flags['category_show'] ||
@@ -653,7 +644,19 @@ if( isset( $t_issue['custom_fields'] ) ) {
 	print_table_spacer( 6 );
 }
 
-echo '</tbody></table>';
+echo '</tbody>';
+
+if( $t_bottom_buttons_enabled ) {
+	echo '<tfoot>';
+	echo '<tr class="noprint"><td colspan="6">';
+	/** @noinspection PhpUnhandledExceptionInspection */
+	bug_view_action_buttons( $f_issue_id, $t_flags );
+	echo '</td></tr>';
+	echo '</tfoot>';
+}
+
+echo '</table>';
+
 echo '</div></div></div></div></div>';
 
 # User list sponsoring the bug
@@ -791,7 +794,7 @@ if( $t_flags['history_show'] && $f_history ) {
 	$t_block_icon = $t_collapse_block ? 'fa-chevron-down' : 'fa-chevron-up';
 	$t_history = history_get_events_array( $f_issue_id );
 ?>
-		<div id="history" class="widget-box widget-color-blue2 <?php echo $t_block_css ?>">
+		<div class="widget-box widget-color-blue2 <?php echo $t_block_css ?>">
 			<div class="widget-header widget-header-small">
 				<h4 class="widget-title lighter">
 					<?php print_icon( 'fa-history', 'ace-icon' ); ?>

bugnote_view_inc.php
@@ -109,7 +109,7 @@ $t_block_css = $t_collapse_block ? 'collapsed' : '';
 $t_block_icon = $t_collapse_block ? 'fa-chevron-down' : 'fa-chevron-up';
 
 ?>
-<div id="bugnotes" class="widget-box widget-color-blue2 <?php echo $t_block_css ?>">
+<div class="widget-box widget-color-blue2 <?php echo $t_block_css ?>">
 <div class="widget-header widget-header-small">
 	<h4 class="widget-title lighter">
 		<?php print_icon( 'fa-comments', 'ace-icon' ); ?>
@@ -130,7 +130,7 @@ $t_block_icon = $t_collapse_block ? 'fa-chevron-down' : 'fa-chevron-up';
 	if( 0 == $t_activities_count ) {
 ?>
 <tr class="bugnotes-empty">
-	<td class="center" colspan="2">
+	<td class="center">
 		<?php echo lang_get( 'no_bugnotes_msg' ) ?>
 	</td>
 </tr>
@@ -154,7 +154,7 @@ $t_block_icon = $t_collapse_block ? 'fa-chevron-down' : 'fa-chevron-up';
 		}
 ?>
 <tr class="bugnote visible-on-hover-toggle" id="c<?php echo $t_activity['id'] ?>">
-		<td class="category">
+	<td class="category">
 		<div class="pull-left padding-2"><?php print_avatar( $t_activity['user_id'], 'bugnote', 80 ); ?>
 		</div>
 		<div class="pull-left padding-2">

print_all_bug_page.php
@@ -115,7 +115,7 @@ layout_page_header();
 	<input type="hidden" name="<?php echo FILTER_PROPERTY_SORT_FIELD_NAME; ?>" value="<?php echo $f_sort ?>" />
 	<input type="hidden" name="<?php echo FILTER_PROPERTY_SORT_DIRECTION; ?>" value="<?php echo $f_dir ?>" />
 
-<table class="table table-striped table-bordered table-condensed no-margin">
+<table class="table table-striped table-bordered table-condensed no-margin noprint">
 <?php
 #<SQLI> Excel & Print export
 #$f_bug_array stores the number of the selected rows
@@ -134,9 +134,8 @@ for( $i=0; $i < $t_row_count; $i++ ) {
 $f_export = implode( ',', $f_bug_arr );
 
 ?>
-
 <tr>
-	<td colspan="<?php echo $t_num_of_columns ?>">
+	<td>
 <?php
 	if( 'DESC' == $f_dir ) {
 		$t_new_dir = 'ASC';
@@ -168,7 +167,6 @@ $f_export = implode( ',', $f_bug_arr );
 		echo '</a> ';
 	}
 ?>
-
 	</td>
 </tr>
 </table>
@@ -184,8 +182,9 @@ if( filter_is_temporary( $t_filter ) ) {
 <?php # CSRF protection not required here - form does not result in modifications ?>
 
 <table id="buglist" class="table table-striped table-bordered table-condensed no-margin">
+<thead>
 <tr>
-    <td class="bold bigger-110" colspan="<?php echo $t_num_of_columns / 2 + $t_num_of_columns % 2; ?>">
+    <td class="bold bigger-110" colspan="<?php echo $t_num_of_columns ?>">
 		<?php
 			echo lang_get( 'viewing_bugs_title' );
 
@@ -198,8 +197,6 @@ if( filter_is_temporary( $t_filter ) ) {
 			}
 			echo '( ' . $v_start . ' - ' . $v_end . ' )';
 		?>
-	</td>
-<tr>
 	</td>
 </tr>
 <tr class="row-category">

print_all_bug_page_word.php
@@ -193,22 +193,22 @@ for( $j=0; $j < $t_row_count; $j++ ) {
 </tr>
 <?php print_table_spacer( 6 ) ?>
 <tr class="bold">
-	<td width="16%">
+	<td>
 		<?php echo sprintf( lang_get( 'label' ), $t_lang_id ) ?>
 	</td>
-	<td width="16%">
+	<td>
 		<?php echo sprintf( lang_get( 'label' ), $t_lang_category ) ?>
 	</td>
-	<td width="16%">
+	<td>
 		<?php echo sprintf( lang_get( 'label' ), $t_lang_severity ) ?>
 	</td>
-	<td width="16%">
+	<td>
 		<?php echo sprintf( lang_get( 'label' ), $t_lang_reproducibility ) ?>
 	</td>
-	<td width="16%">
+	<td>
 		<?php echo sprintf( lang_get( 'label' ), $t_lang_date_submitted ) ?>
 	</td>
-	<td width="16%">
+	<td>
 		<?php echo sprintf( lang_get( 'label' ), $t_lang_last_update ) ?>
 	</td>
 </tr>
@@ -506,7 +506,7 @@ $t_bugnotes = bugnote_get_all_visible_bugnotes( $t_id, $t_user_bugnote_order, $t
 	if( 0 == count( $t_bugnotes ) ) {
 	?>
 <tr>
-	<td class="bold" colspan="2">
+	<td class="bold">
 		<?php echo $t_lang_no_bugnotes_msg ?>
 	</td>
 </tr>
@@ -527,7 +527,7 @@ $t_bugnotes = bugnote_get_all_visible_bugnotes( $t_id, $t_user_bugnote_order, $t
 			$t_note = string_display_links( $t_bugnote->note );
 	?>
 <tr>
-	<td width="12%">
+	<td>
 				(<?php echo bugnote_format_id( $t_bugnote->id ) ?>)
 			<br />
 				<?php print_user( $t_bugnote->reporter_id, false ) ?>&#160;&#160;&#160;

commit 01621bbe5839466cd9fdea2dea816098b13c1433

Fix dropzone extra page load
Regression introduced by 10971ed15a2f8476130c996f54dda6dc068828f9.

Fixes #35644, PR https://github.com/mantisbt/mantisbt/pull/2128

core/print_api.php
@@ -2243,7 +2243,7 @@ function print_dropzone_template(){
 	<div id="dropzone-preview-template" class="hidden">
 		<div class="dz-preview dz-file-preview">
 			<div class="dz-filename"><span data-dz-name></span></div>
-			<img src="#" alt="" data-dz-thumbnail>
+			<img src="data:image/png;base64," alt="" data-dz-thumbnail>
 			<div class="dz-error-message">
 				<div class="dz-error-mark"><span><?php print_icon('fa-times-circle'); ?></span></div>
 				<span data-dz-errormessage></span>