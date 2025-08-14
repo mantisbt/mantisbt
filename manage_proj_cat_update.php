mantisbt/mantisbt

commit 05c1639f45bfc0c3392288930f37f351bfcfc7b9

Use enabled/disabled for category status
Since we currently only use a binary state (enabled/disabled) for the
status, we do not need to make things more complex than they have to be.

This simplifies @cas's original contribution by:
- using a checkbox for input instead of radio buttons
- display a check icon instead of relying on language strings in lists
- standardize on enabled/disabled terminology

This also fixes the "inverted logic" (status == 0 was previously treated
as enabled, which is the reverse of one would expect.

Fixes #31017

manage_proj_cat_update.php
@@ -56,7 +56,7 @@ auth_reauthenticate();
 $f_category_id     = gpc_get_int( 'category_id' );
 $f_name            = trim( gpc_get_string( 'name' ) );
 $f_assigned_to     = gpc_get_int( 'assigned_to', 0 );
-$f_category_status = gpc_get_int( 'category_status', CATEGORY_STATUS_DISABLED );
+$f_status          = gpc_get_bool( 'status', CATEGORY_STATUS_DISABLED );
 
 if( is_blank( $f_name ) ) {
 	error_parameters( 'name' );
@@ -74,7 +74,7 @@ if( mb_strtolower( $f_name ) != mb_strtolower( $t_old_name ) ) {
 	category_ensure_unique( $t_project_id, $f_name );
 }
 
-category_update( $f_category_id, $f_name, $f_assigned_to, $f_category_status );
+category_update( $f_category_id, $f_name, $f_assigned_to, $f_status );
 
 form_security_purge( 'manage_proj_cat_update' );
 

bug_report_page.php
@@ -280,7 +280,7 @@ if( $t_show_attachments ) {
 				echo $t_allow_no_category ? '' : ' required';
 			?>>
 				<?php
-					print_category_option_list( $f_category_id, /* project_id */ null, /* active_only */ true );
+					print_category_option_list( $f_category_id, null, true );
 				?>
 			</select>
 		</td>

bug_update_page.php
@@ -237,7 +237,7 @@ if( $t_show_id || $t_show_project || $t_show_category || $t_show_view_state || $
 		echo '<select ' . helper_get_tab_index()
 			. ( $t_allow_no_category ? '' : ' required' )
 			. ' id="category_id" name="category_id" class="input-sm">';
-		print_category_option_list( $t_bug->category_id, $t_bug->project_id, /* active_only */ true );
+		print_category_option_list( $t_bug->category_id, $t_bug->project_id, true );
 		echo '</select>';
 	}
 

core/category_api.php
@@ -500,12 +500,12 @@ function category_get_filter_list( $p_project_id = null ) {
  * @param bool $p_inherit         Indicates whether to inherit categories from parent projects,
  *                                or null to use configuration default.
  * @param bool $p_sort_by_project Whether to sort by project.
- * @param bool $p_active_only     False to select all categories or True for just the active ones.
+ * @param bool $p_enabled_only    False to select all categories or True for just the active ones.
  *
  * @return array array of categories
  * @access public
  */
-function category_get_all_rows( $p_project_id, $p_inherit = null, $p_sort_by_project = false, $p_active_only = false ) {
+function category_get_all_rows( $p_project_id, $p_inherit = null, $p_sort_by_project = false, $p_enabled_only = false ) {
 	global $g_category_cache, $g_cache_category_project;
 
 	if( isset( $g_cache_category_project[(int)$p_project_id] ) ) {
@@ -545,8 +545,8 @@ function category_get_all_rows( $p_project_id, $p_inherit = null, $p_sort_by_pro
 		$t_project_where = ' project_id=' . $p_project_id . ' ';
 	}
 
-	if( $p_active_only ) {
-		$t_project_where .= ' and c.status = ' . CATEGORY_STATUS_DISABLED;
+	if( $p_enabled_only ) {
+		$t_project_where .= ' and c.status = ' . CATEGORY_STATUS_ENABLED;
 	}
 	
 	$t_query = 'SELECT c.*, p.name AS project_name FROM {category} c

core/print_api.php
@@ -758,20 +758,20 @@ function print_profile_option_list_from_profiles( array $p_profiles, $p_select_i
  * We check in the project category table and in the bug table
  * We put them all in one array and make sure the entries are unique.
  *
- * @param integer $p_category_id A category identifier.
- * @param null    $p_project_id  A project identifier.
- * @param bool    $p_active_only True to exclude inactive categories.
+ * @param integer $p_category_id  A category identifier.
+ * @param null    $p_project_id   A project identifier.
+ * @param bool    $p_enabled_only True to exclude disabled categories.
  *
  * @return void
  */
-function print_category_option_list( $p_category_id = 0, $p_project_id = null, $p_active_only = false ) {
+function print_category_option_list( $p_category_id = 0, $p_project_id = null, $p_enabled_only = false ) {
 	if( null === $p_project_id ) {
 		$t_project_id = helper_get_current_project();
 	} else {
 		$t_project_id = $p_project_id;
 	}
 
-	$t_cat_arr = category_get_all_rows( $t_project_id, null, true, $p_active_only );
+	$t_cat_arr = category_get_all_rows( $t_project_id, null, true, $p_enabled_only );
 
 	if( config_get( 'allow_no_category' ) ) {
 		echo '<option value="0"';

manage_proj_cat_edit_page.php
@@ -58,7 +58,7 @@ $t_row = category_get_row( $f_category_id );
 $t_assigned_to = (int)$t_row['user_id'];
 $t_project_id = (int)$t_row['project_id'];
 $t_name = $t_row['name'];
-$t_category_status = $t_row['status'];
+$t_status = (int)$t_row['status'];
 
 access_ensure_project_level( config_get( 'manage_project_threshold' ), $t_project_id );
 
@@ -97,22 +97,15 @@ print_manage_menu( 'manage_proj_cat_edit_page.php' );
 			</tr>
 			<tr>
 				<td class="category">
-					<?php echo lang_get( 'category_status' ) ?>
+					<label for="category-status">
+						<?php echo lang_get( 'enabled' ) ?>
+					</label>
 				</td>
 				<td>
-					<label>
-						<input type="radio" name="category_status" value="0"
-							<?php echo ( $t_category_status == CATEGORY_STATUS_DISABLED ) ? 'checked="checked" ' : '' ?>
-						/>
-						<?php echo lang_get( 'category_active' ) ?>
-					</label>
-					&nbsp; &nbsp; &nbsp;
-					<label>
-						<input type="radio" name="category_status" value="1"
-							<?php echo ( $t_category_status == CATEGORY_STATUS_ENABLED ) ? 'checked="checked" ' : '' ?>
-						/>
-						<?php echo lang_get( 'category_not_active' ) ?>
-					</label>
+					<input type="checkbox" id="proj-category-status" name="status" class="ace"
+						<?php check_checked( $t_status, CATEGORY_STATUS_ENABLED ); ?>
+					/>
+					<span class="lbl"></span>
 				</td>
 			</tr>
 			<tr>

manage_proj_edit_page.php
@@ -462,7 +462,7 @@ print_manage_menu( 'manage_proj_edit_page.php' );
 		<thead>
 			<tr>
 				<th><?php echo lang_get( 'category' ) ?></th>
-				<td><?php echo lang_get( 'category_status' ) ?></td>
+				<th class="center"><?php echo lang_get( 'enabled' ) ?></th>
 				<th><?php echo lang_get( 'assign_to' ) ?></th>
 				<th colspan="2" class="center"><?php echo lang_get( 'actions' ) ?></th>
 			</tr>
@@ -473,15 +473,10 @@ print_manage_menu( 'manage_proj_edit_page.php' );
 		foreach ( $t_categories as $t_category ) {
 			$t_id = $t_category['id'];
 			$t_inherited = ( $t_category['project_id'] != $f_project_id );
-			if( $t_category['status'] == OFF ){
-				$t_category_status = lang_get( 'category_active' ) ;
-			} else {
-				$t_category_status = lang_get( 'category_not_active' ) ;
-			}
 ?>
 			<tr>
 				<td><?php echo string_display_line( category_full_name( $t_id, $t_inherited, $f_project_id ) )  ?></td>
-				<td><?php echo $t_category_status ?></td>
+				<td class="center"><?php echo trans_bool( $t_category['status'] ) ?></td>
 				<td><?php echo prepare_user_name( $t_category['user_id'] ) ?></td>
 				<td class="center">
 					<div class="inline">

manage_proj_page.php
@@ -128,7 +128,7 @@ print_manage_menu( 'manage_proj_page.php' );
 					);
 					print_sort_icon( $t_direction, $f_sort, 'status' ); ?>
 				</th>
-				<th><?php
+				<th class="center"><?php
 					print_manage_project_sort_link(
 						'manage_proj_page.php',
 						lang_get( 'enabled' ),
@@ -244,7 +244,7 @@ print_manage_menu( 'manage_proj_page.php' );
 		<thead>
 			<tr>
 				<td><?php echo lang_get( 'category' ) ?></td>
-				<td><?php echo lang_get( 'category_status' ) ?></td>
+				<td class="center"><?php echo lang_get( 'enabled' ) ?></td>
 				<td><?php echo lang_get( 'assign_to' ) ?></td>
 				<?php if( $t_can_update_global_cat ) { ?>
 				<td class="center"><?php echo lang_get( 'actions' ) ?></td>
@@ -256,15 +256,10 @@ print_manage_menu( 'manage_proj_page.php' );
 <?php
 			foreach( $t_categories as $t_category ) {
 				$t_id = $t_category['id'];
-				if( $t_category['status'] == OFF ){
-					$t_category_status = lang_get( 'category_active' ) ;
-				} else {
-					$t_category_status = lang_get( 'category_not_active' ) ;
-				}
 ?>
 			<tr>
 				<td><?php echo string_display_line( category_full_name( $t_id, false ) )  ?></td>
-				<td><?php echo $t_category_status ?></td>
+				<td class="center"><?php echo trans_bool( $t_category['status'] ) ?></td>
 				<td><?php echo prepare_user_name( $t_category['user_id'] ) ?></td>
 				<?php if( $t_can_update_global_cat ) { ?>
 				<td class="center">

commit 38a569855832c9d5cc63ee8bdf5a96c9b0c60e84

Fix DB error updating category on PostgreSQL
ERROR: invalid input syntax for type integer: "TRUE" for the query:
UPDATE mantis_category_table SET name=$1, user_id=$2, status=$3
WHERE id=$4.

Regression introduced by commit 05c1639f45bfc0c3392288930f37f351bfcfc7b9
(issue #31017).

Fixes #36263

manage_proj_cat_update.php
@@ -74,7 +74,7 @@ if( mb_strtolower( $f_name ) != mb_strtolower( $t_old_name ) ) {
 	category_ensure_unique( $t_project_id, $f_name );
 }
 
-category_update( $f_category_id, $f_name, $f_assigned_to, $f_status );
+category_update( $f_category_id, $f_name, $f_assigned_to, (int)$f_status );
 
 form_security_purge( 'manage_proj_cat_update' );
 