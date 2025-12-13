mantisbt/mantisbt

commit 97f7ad7a629044d1d04971c910a2bf8572b9233f

Push current plugin in plugin_needs_upgrade()
Without this, the plugins' schema() methods will return incorrect table
names (i.e. without the plugin's name as a prefix) if they rely on
plugin_table() to build them.

This is really picking nits, because an incorrect table name is very
unlikely to cause issues since the function only checks the number of
entries in the plugin's schema array.

core/plugin_api.php
@@ -669,7 +669,9 @@ function plugin_install( $p_plugin ) {
  * @return bool True if plugin needs schema ugrades.
  */
 function plugin_needs_upgrade( $p_plugin ) {
+	plugin_push_current( $p_plugin->name );
 	$t_plugin_schema = $p_plugin->schema();
+	plugin_pop_current( $p_plugin->name );
 	if( is_null( $t_plugin_schema ) ) {
 		return false;
 	}

commit efd35e33e2f19274200b7e0f1c64115804996151

Fix incorrect plugin_push_current() call
The plugin stack expects the plugin's basename, not its name.

Bug introduced in 97f7ad7a629044d1d04971c910a2bf8572b9233f.

Fixes #36765

core/plugin_api.php
@@ -79,8 +79,11 @@ $g_plugin_cache_priority = array();
 $g_plugin_cache_protected = array();
 
 /**
- * Current plugin stack
- * @global MantisPlugin[] $g_plugin_current
+ * Current plugin stack.
+ *
+ * Stores the plugin basename.
+ * @see plugin_push_current(), plugin_get_current(), plugin_pop_current()
+ * @global string[] $g_plugin_current
  */
 $g_plugin_current = array();
 
@@ -703,7 +706,7 @@ function plugin_install( MantisPlugin $p_plugin ) {
  * @return boolean True if plugin needs schema upgrades.
  */
 function plugin_needs_upgrade( MantisPlugin $p_plugin ) {
-	plugin_push_current( $p_plugin->name );
+	plugin_push_current( $p_plugin->basename );
 	$t_plugin_schema = $p_plugin->schema();
 	plugin_pop_current();
 	if( is_null( $t_plugin_schema ) ) {