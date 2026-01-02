mantisbt/mantisbt

commit 2f75ab6d8e1d829d094985662faf7dcf1aa24102

Fix login page redirection from admin page
When an unauthenticated user tries to access the admin page, they are
redirected to the login page, but this was broken by use of absolute URL
introduced in commit 9cdc361a80b2a3fa614ca2648a97342bd0a18236.

Fixes #35587

core/access_api.php
@@ -83,7 +83,7 @@ function access_denied() {
 	}
 
 	if( !auth_is_user_authenticated() ) {
-		print_header_redirect( helper_mantis_url( auth_login_page( $t_return ) ) );
+		print_header_redirect( helper_mantis_url( auth_login_page( $t_return ) ), false, false, true );
 	} else {
 		$t_buttons = [];
 		

commit 2f86882766aa143fb6dd192d45832151b0531fe9

Fix access_denied() for anonymous user when disabled
Going to /bug_report_page.php as an unauthenticated user when
$g_allow_anonymous_login = OFF resulted in a blank page

Regression introduced by commit 2f75ab6d8e1d829d094985662faf7dcf1aa24102.

Setting $p_die parameter to true fixes the problem.

Fixes #36810

core/access_api.php
@@ -84,7 +84,7 @@ function access_denied() {
 	}
 
 	if( !auth_is_user_authenticated() ) {
-		print_header_redirect( helper_mantis_url( auth_login_page( $t_return ) ), false, false, true );
+		print_header_redirect( helper_mantis_url( auth_login_page( $t_return ) ), true, false, true );
 	} else {
 		$t_buttons = [];
 		