mantisbt/mantisbt

commit 30ed1ea36df25114daae6e45726ff47c4d6f45e8

Do not set default $g_path from CLI
The set_default_path() function relies on $_SERVER['SCRIPT_NAME'], which
does not contain an URL when running from CLI.

This results in a most likely invalid URL being stored in $g_path, which
could lead to unexpected behavior or errors later on.

Simply refuse to set $g_path in this case, and print a warning on
screen. Note that this should normally not happen, except in development
environments.

Fixes #37313

core.php
@@ -359,6 +359,12 @@ function set_default_path() {
 		return false;
 	}
 
+	$t_message = 'Please set it manually in ' . $g_config_path . 'config_inc.php' . PHP_EOL;
+	if(php_sapi_name() == 'cli') {
+		echo 'WARNING: cannot initialize $g_path from CLI. ' . $t_message;
+		return false;
+	}
+
 	$t_protocol = 'http';
 	$t_host = 'localhost';
 	if( isset( $_SERVER['SCRIPT_NAME'] ) ) {
@@ -409,7 +415,7 @@ function set_default_path() {
 		$t_path = rtrim( $t_path, '/\\' ) . '/';
 
 		if( strpos( $t_path, '&#' ) ) {
-			echo 'Can not safely determine $g_path. Please set $g_path manually in ' . $g_config_path . 'config_inc.php';
+			echo 'Can not safely determine $g_path.' . $t_message;
 			die;
 		}
 	} else {

commit 4eba32908f782c8363adb3dea39940267ebb4705

Remove Warning echoed in set_default_path()
This caused the PHPUnit tests to fail.

Fixing regression from 30ed1ea36df25114daae6e45726ff47c4d6f45e8.

core.php
@@ -359,9 +359,9 @@ function set_default_path() {
 		return false;
 	}
 
-	$t_message = 'Please set it manually in ' . $g_config_path . 'config_inc.php' . PHP_EOL;
+	# $_SERVER['SCRIPT_NAME'], does not contain an URL when running from CLI
+	# Do not set $g_path in this case.
 	if(php_sapi_name() == 'cli') {
-		echo 'WARNING: cannot initialize $g_path from CLI. ' . $t_message;
 		return false;
 	}
 
@@ -415,7 +415,9 @@ function set_default_path() {
 		$t_path = rtrim( $t_path, '/\\' ) . '/';
 
 		if( strpos( $t_path, '&#' ) ) {
-			echo 'Can not safely determine $g_path.' . $t_message;
+			echo 'Can not safely determine $g_path.'
+				. "Please set it manually in $g_config_path config_inc.php"
+				. PHP_EOL;
 			die;
 		}
 	} else {