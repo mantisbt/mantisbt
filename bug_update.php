mantisbt/mantisbt

commit 0ac87c39fb4746a2f8c276730fa689baf47ba8ba

Prevent unauthorized update of `version` field (2)
Users below report_issues_for_unreleased_versions_threshold should not
be allowed to set an Issue's product version to an unreleased one.

This addresses the problem in the MantisBT UI - users could send a
crafted request to bug_report.php or bug_update.php

Added the necessary access checks to prevent that.

Fixes #37065, GHSA-3v2j-6fw9-f57c

bug_update.php
@@ -42,6 +42,8 @@
  * @uses relationship_api.php
  */
 
+use Mantis\Exceptions\ClientException;
+
 require_once( 'core.php' );
 require_api( 'access_api.php' );
 require_api( 'authentication_api.php' );
@@ -325,6 +327,19 @@ if( $t_existing_bug->resolution != $t_updated_bug->resolution && (
 	);
 	trigger_error( ERROR_INVALID_RESOLUTION, ERROR );
 }
+
+# If version is unreleased, ensure user is allowed to change it.
+if( $t_existing_bug->version != $t_updated_bug->version
+	&& !version_is_released( version_get_id( $t_updated_bug->version ) )
+	&& !access_has_project_level( config_get( 'report_issues_for_unreleased_versions_threshold' ) )
+) {
+	throw new ClientException(
+		'User not allowed to assign unreleased versions',
+		ERROR_INVALID_FIELD_VALUE,
+		['version']
+	);
+}
+
 
 # Ensure that the user has permission to change the target version of the issue.
 if( $t_existing_bug->target_version !== $t_updated_bug->target_version ) {

core/commands/IssueAddCommand.php
@@ -277,6 +277,13 @@ class IssueAddCommand extends Command {
 
 		$t_version_id = isset( $t_issue['version'] ) ? mci_get_version_id( $t_issue['version'], $t_project_id, 'version' ) : 0;
 		if( $t_version_id != 0 ) {
+			if( !access_has_project_level( config_get( 'report_issues_for_unreleased_versions_threshold' ) ) ) {
+				throw new ClientException(
+					'User not allowed to assign unreleased versions',
+					ERROR_INVALID_FIELD_VALUE,
+					['version']
+				);
+			}
 			$this->issue->version = version_get_field( $t_version_id, 'version' );
 		}
 

commit d93329e1797ca4392a057439f30c938bb53fc6c6

Fix regression preventing to unset product version
Introduced by 0ac87c39fb4746a2f8c276730fa689baf47ba8ba.

Check that the version is set before calling version_is_released().

Fixes #37348

bug_update.php
@@ -328,8 +328,9 @@ if( $t_existing_bug->resolution != $t_updated_bug->resolution && (
 	trigger_error( ERROR_INVALID_RESOLUTION, ERROR );
 }
 
-# If version is unreleased, ensure user is allowed to change it.
-if( $t_existing_bug->version != $t_updated_bug->version
+# If version is set and unreleased, ensure user is allowed to change it.
+if( $t_updated_bug->version
+	&& $t_existing_bug->version != $t_updated_bug->version
 	&& !version_is_released( version_get_id( $t_updated_bug->version ) )
 	&& !access_has_project_level( config_get( 'report_issues_for_unreleased_versions_threshold' ) )
 ) {