5f021fa20e11dbf8b5b9cee1710947b3e62e24c9
Victor Boctor <vboctor@gmail.com>
2026-08-20 11:05:37 -0700

    Document issue move test members
    Fixes #37352

--- tests/Mantis/IssueMoveCommandTest.php
+++ tests/Mantis/IssueMoveCommandTest.php
@@ -1,4 +1,18 @@
 <?php
+# MantisBT - A PHP based bugtracking system
+
+# MantisBT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# MantisBT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MantisBT.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace Mantis\tests\Mantis;
 
@@ -8,9 +22,13 @@ use Mantis\Exceptions\ClientException;
  * Tests for IssueMoveCommand.
  */
 class IssueMoveCommandTest extends MantisCoreBase {
+	/** @var int Created issue identifier. */
 	private $issue_id;
+	/** @var int Source-project category identifier. */
 	private $source_category_id;
+	/** @var int Created target-project identifier. */
 	private $target_project_id;
+	/** @var int Target-project category identifier. */
 	private $target_category_id;
 
 	/**
