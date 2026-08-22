ff40be0601d8eb2260f3451bfbb73a11341e8aa4
Victor Boctor <vboctor@gmail.com>
2026-08-22 01:39:46 -0700

    Use command for web attachment deletion
    Fixes #34425

--- bug_file_delete.php
+++ bug_file_delete.php
@@ -35,10 +35,6 @@
  */
 
 require_once( 'core.php' );
-require_api( 'access_api.php' );
-require_api( 'bug_api.php' );
-require_api( 'config_api.php' );
-require_api( 'file_api.php' );
 require_api( 'form_api.php' );
 require_api( 'gpc_api.php' );
 require_api( 'helper_api.php' );
@@ -51,23 +47,16 @@ $f_file_id = gpc_get_int( 'file_id' );
 
 $t_bug_id = file_get_field( $f_file_id, 'bug_id' );
 
-$t_bug = bug_get( $t_bug_id, true );
-if( $t_bug->project_id != helper_get_current_project() ) {
-	# in case the current project is not the same project of the bug we are viewing...
-	# ... override the current project. This to avoid problems with categories and handlers lists etc.
-	$g_project_override = $t_bug->project_id;
-}
-
-$t_attachment_owner = file_get_field( $f_file_id, 'user_id' );
-$t_current_user_is_attachment_owner = $t_attachment_owner == auth_get_current_user_id();
-if( !$t_current_user_is_attachment_owner || ( $t_current_user_is_attachment_owner && !config_get( 'allow_delete_own_attachments' ) ) ) {
-	access_ensure_bug_level( config_get( 'delete_attachments_threshold' ), $t_bug_id );
-}
-
 helper_ensure_confirmed( lang_get( 'delete_attachment_sure_msg' ), lang_get( 'delete' ) );
 
-file_delete( $f_file_id, 'bug' );
+$t_command = new IssueFileDeleteCommand( array(
+	'query' => array(
+		'issue_id' => $t_bug_id,
+		'file_id' => $f_file_id,
+	)
+) );
+$t_result = $t_command->execute();
 
 form_security_purge( 'bug_file_delete' );
 
-print_header_redirect_view( $t_bug_id );
+print_header_redirect_view( $t_result['issue_id'] );
