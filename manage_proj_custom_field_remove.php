0c50e723031976f034b3f3ff6c1e7a9bc8b073e5
Victor Boctor <vboctor@gmail.com>
2026-08-29 21:26:21 -0700

    Use command for custom field unlinking
    Fixes #32449

--- manage_proj_custom_field_remove.php
+++ manage_proj_custom_field_remove.php
@@ -23,28 +23,22 @@
  * @link http://www.mantisbt.org
  *
  * @uses core.php
- * @uses access_api.php
  * @uses authentication_api.php
- * @uses config_api.php
  * @uses custom_field_api.php
  * @uses form_api.php
  * @uses gpc_api.php
  * @uses helper_api.php
- * @uses html_api.php
  * @uses lang_api.php
  * @uses print_api.php
  * @uses string_api.php
  */
 
 require_once( 'core.php' );
-require_api( 'access_api.php' );
 require_api( 'authentication_api.php' );
-require_api( 'config_api.php' );
 require_api( 'custom_field_api.php' );
 require_api( 'form_api.php' );
 require_api( 'gpc_api.php' );
 require_api( 'helper_api.php' );
-require_api( 'html_api.php' );
 require_api( 'lang_api.php' );
 require_api( 'print_api.php' );
 require_api( 'string_api.php' );
@@ -57,11 +51,13 @@ $f_field_id = gpc_get_int( 'field_id' );
 $f_project_id = gpc_get_int( 'project_id' );
 $f_return = gpc_get_string( 'return', '' );
 
-# We should check both since we are in the project section and an
-# admin might raise the first threshold and not realize they need
-# to raise the second
-access_ensure_project_level( config_get( 'manage_project_threshold' ), $f_project_id );
-access_ensure_project_level( config_get( 'custom_field_link_threshold' ), $f_project_id );
+$t_command = new ProjectFieldUnlinkCommand( [
+	'query' => [
+		'field_id' => $f_field_id,
+		'project_id' => $f_project_id,
+	],
+] );
+$t_command->validate();
 
 $t_definition = custom_field_get_definition( $f_field_id );
 
@@ -80,7 +76,7 @@ if( $f_return == 'custom_field' ) {
 	$t_redirect_url = 'manage_proj_edit_page.php?project_id=' . $f_project_id . '#customfields';
 }
 
-custom_field_unlink( $f_field_id, $f_project_id );
+$t_command->execute();
 
 form_security_purge( 'manage_proj_custom_field_remove' );
 
