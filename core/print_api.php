mantisbt/mantisbt

commit 5dbd46b9e322f7140e7c7b56db19f88c1784faae

Fix static analysis warnings
Foreach variable $t_locations used both as iterable and value.

core/print_api.php
@@ -2198,14 +2198,11 @@ function print_bug_attachment_preview_audio_video( array $p_attachment, $p_file_
 function print_timezone_option_list( $p_timezone ) {
 	$t_identifiers = timezone_identifiers_list( DateTimeZone::ALL );
 
+	$t_locations_list = [];
 	foreach( $t_identifiers as $t_identifier ) {
 		$t_zone = explode( '/', $t_identifier, 2 );
-		if( isset( $t_zone[1] ) ) {
-			$t_id = $t_zone[1];
-		} else {
-			$t_id = $t_identifier;
-		}
-		$t_locations[$t_zone[0]][$t_identifier] = array(
+		$t_id = $t_zone[1] ?? $t_identifier;
+		$t_locations_list[$t_zone[0]][$t_identifier] = array(
 			str_replace( '_', ' ', $t_id ),
 			$t_identifier
 		);

commit 34ee5422ad3476451adc6c75d118e11a43b8fa69

Fix Undefined variable $t_locations warning
Regression introduced by 5dbd46b9e322f7140e7c7b56db19f88c1784faae.

core/print_api.php
@@ -2208,7 +2208,7 @@ function print_timezone_option_list( $p_timezone ) {
 		);
 	}
 
-	foreach( $t_locations as $t_continent => $t_locations ) {
+	foreach( $t_locations_list as $t_continent => $t_locations ) {
 		echo "\t" . '<optgroup label="' . $t_continent . '">' . "\n";
 		foreach ( $t_locations as $t_location ) {
 			echo "\t\t" . '<option value="' . $t_location[1] . '"';